aa57d35a72f05c80e69b8b4d8905a8b84a921420
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:30 +0000

    Add package django-lionheart-helpers/2.1.1/django-lionheart-helpers-2.1.1.tar.gz

---
+++ packages/django-lionheart-helpers/django-lionheart-helpers-2.1.1.tar.gz/django-lionheart-helpers-2.1.1/lionheart/metadata.py
@@ -0,0 +1,7 @@
+__version__ = "2.1.1"
+__author__ = "Dan Loewenherz"
+__copyright__ = "Copyright 2015, Lionheart Software"
+__maintainer__ = "Dan Loewenherz"
+__email__ = "dan@lionheartsw.com"
+__license__ = "Apache 2.0"
+

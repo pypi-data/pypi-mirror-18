37638402321098e9cba6932af8058ccb4ac45616
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:46 +0000

    Add package Kuyruk/6.0.0/Kuyruk-6.0.0.tar.gz

---
+++ packages/Kuyruk/Kuyruk-6.0.0.tar.gz/Kuyruk-6.0.0/tests/unit/test_various.py
@@ -0,0 +1,36 @@
+import logging
+import unittest
+
+from mock import patch
+
+from kuyruk import Task
+from tests import tasks
+
+
+logger = logging.getLogger(__name__)
+
+
+class KuyrukTestCase(unittest.TestCase):
+
+    def test_task_decorator(self):
+        """Task decorator is applied successfully"""
+        # Decorator without args
+        self.assertTrue(isinstance(tasks.echo, Task))
+        # Decorator with args
+        self.assertTrue(isinstance(tasks.echo_another, Task))
+
+    @patch('tests.tasks.must_be_called')
+    def test_eager(self, mock_func):
+        """Task.apply() is called successfully on eager mode"""
+        tasks.eager_task()
+        mock_func.assert_called_once_with()
+
+    @patch('tests.tasks.must_be_called')
+    def test_apply(self, mock_func):
+        """Task.apply() is called successfully"""
+        tasks.echo.apply("hello")
+        mock_func.assert_called_once_with()
+
+    def test_task_name(self):
+        """Task.name is correct"""
+        self.assertEqual(tasks.echo.name, 'tests.tasks:echo')

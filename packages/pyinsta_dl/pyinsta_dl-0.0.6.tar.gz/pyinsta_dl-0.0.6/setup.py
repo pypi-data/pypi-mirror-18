0ad6d178e770094d3f7aa81703723320ce92a66b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:38 +0000

    Add package pyinsta_dl/0.0.6/pyinsta_dl-0.0.6.tar.gz

---
+++ packages/pyinsta_dl/pyinsta_dl-0.0.6.tar.gz/pyinsta_dl-0.0.6/setup.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+setup(name='pyinsta_dl',
+      py_modules=['pyinsta_dl'],
+      version='0.0.6',
+      description='Python implementation for download images and videos from Instagram',
+      url='https://github.com/natanocr/pyinsta_dl',
+      author='Natan Oliveira',
+      author_email='natanoliveiracruz@gmail.com',
+      keywords='instagram download media image video',
+      long_description=open('README.rst').read(),
+      license='MIT',
+      platforms='OS Independent',
+      packages=['pyinsta_dl'],
+      install_requires=[
+          'bs4',
+      ],
+      test_suite='test',
+      zip_safe=False)

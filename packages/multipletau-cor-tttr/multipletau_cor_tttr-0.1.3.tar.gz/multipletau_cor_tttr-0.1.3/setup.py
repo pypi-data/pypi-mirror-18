6a13303331f1bed6fe8bb2580d0b7979a5f55b06
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:28 +0000

    Add package multipletau-cor-tttr/0.1.3/multipletau_cor_tttr-0.1.3.tar.gz

---
+++ packages/multipletau-cor-tttr/multipletau_cor_tttr-0.1.3.tar.gz/multipletau_cor_tttr-0.1.3/setup.py
@@ -0,0 +1,23 @@
+from distutils.core import setup, Extension
+
+module = Extension('multipletau_cor_tttr/CCF',
+                   sources=['multipletau_cor_tttr/CCF.c'])
+
+setup(
+    name='multipletau_cor_tttr',
+    version='0.1.3',
+    author='Anders Barth',
+    license='MIT',
+    author_email='anders.barth@gmail.com',
+    packages=['multipletau_cor_tttr'],
+    package_data={'multipletau_cor_tttr': ['sample/sample_data.npy', 'sample/multipletau_cor_tttr_example.py']},
+    url='http://testpypi.python.org/pypi/multipletau_cor_tttr/',
+    description='Library for correlation of time-tagged time-resolved photon data for'
+                ' fluorescence correlation spectroscopy (FCS) analysis.',
+    long_description=open('README').read(),
+    ext_modules=[module],
+    install_requires=['numpy', 'matplotlib'],
+    classifiers=['Programming Language :: Python :: 2.7',
+                 'Programming Language :: Python :: 3.5'],
+    keywords=['FCS Fluorescence Correlation Spectroscopy Single Photon Counting TTTR Multiple-tau algorithm']
+)

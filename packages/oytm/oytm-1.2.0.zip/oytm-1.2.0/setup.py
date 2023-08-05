f85ef74ef908f874a53d6fad9eec6dea6a40cf9f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:23 +0000

    Add package oytm/1.2.0/oytm-1.2.0.zip

---
+++ packages/oytm/oytm-1.2.0.zip/oytm-1.2.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name = "oytm",
+    version = "1.2.0",
+    py_modules = ["oytm"],
+    author = "ouym",
+    author_email = "ouym@qq.com",
+    url = "http://www.oymit.top",
+    discription = "print list",
+)

bb3bd09e157baa763de02523da36a69bc619045f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:33 +0000

    Add package django-webp/1.0.7/django-webp-1.0.7.tar.gz

---
+++ packages/django-webp/django-webp-1.0.7.tar.gz/django-webp-1.0.7/setup.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+from setuptools import find_packages
+
+setup(
+    name='django-webp',
+    version='1.0.7',
+    author=u'Andre Farzat',
+    author_email='andrefarzat@gmail.com',
+    packages=find_packages(),
+    url='http://pypi.python.org/pypi/django-webp/',
+    license='MIT',
+    description='Returns a webp image instead of jpg, gif or png to browsers which have support',
+    # long_description=open('README.rst').read(),
+    install_requires=open('requirements.txt').readlines(),
+    include_package_data=True,
+    zip_safe=False,
+)

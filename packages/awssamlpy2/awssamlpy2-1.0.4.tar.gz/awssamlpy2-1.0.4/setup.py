a6f3dbf8e58db8486a36dc5f230ead5b328a890f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:53 +0000

    Add package awssamlpy2/1.0.4/awssamlpy2-1.0.4.tar.gz

---
+++ packages/awssamlpy2/awssamlpy2-1.0.4.tar.gz/awssamlpy2-1.0.4/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+ 
+setup(
+    name = 'awssamlpy2',
+    version = '1.0.4',
+    description = 'SAML federated API access for AWS',
+    author='Neeharika',
+    author_email='neeharika.mm@gmail.com',
+    classifiers=[
+        'Programming Language :: Python',
+        'Environment :: Console',
+    ],
+	install_requires=['beautifulsoup4','requests','html5lib','boto'],
+	scripts=['aws-saml']
+)

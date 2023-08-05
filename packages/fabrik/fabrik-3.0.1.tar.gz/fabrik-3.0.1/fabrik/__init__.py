f8793f945991acdd98a862e2965360b4fbf9a804
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:44 +0000

    Add package fabrik/3.0.1/fabrik-3.0.1.tar.gz

---
+++ packages/fabrik/fabrik-3.0.1.tar.gz/fabrik-3.0.1/fabrik/__init__.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+fabrik
+------------
+
+:copyright: (c) 2014-2016 by Fröjd Interactive AB
+:license: MIT, see LICENSE for more details.
+"""
+
+
+__title__ = "fabrik"
+__version__ = "3.0.1"
+__build__ = 301
+__license__ = "MIT"
+__copyright__ = "Copyright 2014-2016 Fröjd Interactive AB"

6cac48bbc07346d6a38b49a4ea0929eb411455f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:56:53 +0000

    Add package jsonapi-requests/0.1/jsonapi-requests-0.1.tar.gz

---
+++ packages/jsonapi-requests/jsonapi-requests-0.1.tar.gz/jsonapi-requests-0.1/CHANGELOG.md
@@ -0,0 +1,15 @@
+Changelog for kubepy
+=================
+
+0.1 (2016-11-04)
+----------------
+
+- Append slash to API root if needed. ([#2](https://github.com/socialwifi/jsonapi-requests/pull/2))
+- Allow to automatically append slash. ([#4](https://github.com/socialwifi/jsonapi-requests/pull/4))
+- Set headers according to JSON:API specification. ([#3](https://github.com/socialwifi/jsonapi-requests/pull/3))
+
+
+0.0 (2016-10-27)
+----------------
+
+- Initial lowlevel api.
---
+++ packages/jsonapi-requests/jsonapi-requests-0.1.tar.gz/jsonapi-requests-0.1/setup.py
@@ -0,0 +1,27 @@
+from pip.req import parse_requirements
+from setuptools import find_packages
+from setuptools import setup
+
+
+setup(
+    name='jsonapi-requests',
+    version='0.1',
+    description='Python client implementation for json api. http://jsonapi.org/',
+    author='Jakub Skiepko',
+    author_email='it@socialwifi.com',
+    url='https://github.com/socialwifi/jsonapi-requests',
+    packages=['jsonapi_requests'],
+    install_requires=[str(ir.req) for ir in parse_requirements('base_requirements.txt', session=False)],
+    license='BSD',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ]
+)

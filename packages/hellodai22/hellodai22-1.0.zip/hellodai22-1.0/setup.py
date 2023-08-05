4db8342e92825fbdb40f91bff1b3e8f2161b92a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:55:20 +0000

    Add package hellodai22/1.0/hellodai22-1.0.zip

---
+++ packages/hellodai22/hellodai22-1.0.zip/hellodai22-1.0/setup.py
@@ -0,0 +1,6 @@
+from distutils.core import setup
+setup(name='hellodai22',
+      version='1.0',
+      description='A simple example',
+      author='Magnus Lie Hetland',
+      py_modules=['hello','nihao'])

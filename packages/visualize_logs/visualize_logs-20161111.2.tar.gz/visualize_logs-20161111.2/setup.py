7418c0c5eabbcecd49b9554ae826944f28442175
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:08 +0000

    Add package visualize_logs/20161111.2/visualize_logs-20161111.2.tar.gz

---
+++ packages/visualize_logs/visualize_logs-20161111.2.tar.gz/visualize_logs-20161111.2/setup.py
@@ -0,0 +1,32 @@
+import os
+from setuptools import setup
+
+
+# Utility function to read the README file.
+# Used for the long_description.  It's nice, because now 1) we have a top level
+# README file and 2) it's easier to type in the README file than to put a raw
+# string in below ...
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
+setup(
+    name='visualize_logs',
+    version='20161111.2',
+    author='Keith J. Jones',
+    author_email='keith@keithjjones.com',
+    packages=['Visualize_Logs', 'Visualize_Logs.objects'],
+    url='https://github.com/keithjjones/visualize_logs',
+    license='CC BY-SA',
+    description=('A Python library and command line tools to '
+                 'provide log visualization.'),
+    long_description=read('README.TXT'),
+    install_requires=['networkx', 'pandas', 'plotly>=1.9.0',
+                      'pydotplus'],
+    entry_points={
+        'console_scripts': [
+            'plotprocmoncsv = Visualize_Logs.__main__:plotprocmoncsv',
+            'plotcuckoojson = Visualize_Logs.__main__:plotcuckoojson'
+        ]
+     }
+)

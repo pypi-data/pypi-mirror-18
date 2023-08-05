4f23f547ebeb1c0d3b03fb106e3cdaae935e366f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:39 +0000

    Add package cricketscore/0.2/cricketscore-0.2.tar.gz

---
+++ packages/cricketscore/cricketscore-0.2.tar.gz/cricketscore-0.2/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(
+    name='cricketscore',  # This is the name of your PyPI-package.
+    version='0.2',  # Update the version number for new releases
+    scripts=['cricscore.py']
+)

72b83650df9d5b957bf88f4f9ac670bbbbb3c791
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:10 +0000

    Add package madsaf_list/1.0.0/madsaf_list-1.0.0.zip

---
+++ packages/madsaf_list/madsaf_list-1.0.0.zip/madsaf_list-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+
+    name = 'madsaf_list',
+    version = '1.0.0',
+    py_modules = ['madsaf_list'],
+    author = 'mateusalipy',
+    author_email = 'mateusalifontes@gmail.com',
+    description = 'A simple printer of nested list',
+    )

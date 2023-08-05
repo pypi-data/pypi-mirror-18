fcb06631ab019c8b240faaab0d8781b59327a4ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:29 +0000

    Add package phy/1.0.7/phy-1.0.7.tar.gz

---
+++ packages/phy/phy-1.0.7.tar.gz/phy-1.0.7/phy/cluster/manual/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+# flake8: noqa
+
+"""Manual clustering facilities."""
+
+from ._utils import ClusterMeta
+from .clustering import Clustering
+from .gui_component import ManualClustering
+from .views import WaveformView, TraceView, FeatureView, CorrelogramView

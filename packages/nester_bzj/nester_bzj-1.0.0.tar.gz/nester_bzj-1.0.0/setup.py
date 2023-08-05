49143ae5c8105dbb1e3974fa669355107f560adb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:15 +0000

    Add package nester_bzj/1.0.0/nester_bzj-1.0.0.tar.gz

---
+++ packages/nester_bzj/nester_bzj-1.0.0.tar.gz/nester_bzj-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+	name		= 'nester_bzj',
+	version		= '1.0.0',
+	py_modules	= ['nester'],
+	author		= 'baozijian',
+	author_email	= 'cool.baozijian@gmail.com',
+	url		= 'http://note.bz1.me',
+	description	= 'My first try of python, A simple printer of nested lists',
+     )

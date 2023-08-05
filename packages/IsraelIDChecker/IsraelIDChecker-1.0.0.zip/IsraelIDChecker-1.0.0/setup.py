e0a888785d1ee376533c650f530503bb5b992b9c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:58:15 +0000

    Add package IsraelIDChecker/1.0.0/IsraelIDChecker-1.0.0.zip

---
+++ packages/IsraelIDChecker/IsraelIDChecker-1.0.0.zip/IsraelIDChecker-1.0.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+    name='IsraelIDChecker',
+    version='1.0.0',
+    author='Izia',
+    author_email='iziaman@gmail.com',
+    url='http://www.askitzik.org',
+    description="This is a module that check if the person gave a correct ID. "
+)

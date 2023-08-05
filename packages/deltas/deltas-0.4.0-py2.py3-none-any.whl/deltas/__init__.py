e9884ba1182c0661e7e2ddbd5b0d006fe183cb77
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:45 +0000

    Add package deltas/0.4.0/deltas-0.4.0-py2.py3-none-any.whl

---
+++ packages/deltas/deltas-0.4.0-py2.py3-none-any.whl/deltas/__init__.py
@@ -0,0 +1,20 @@
+from .apply import apply
+from .operations import Operation, Insert, Delete, Equal
+from .algorithms.diff_engine import DiffEngine
+from .algorithms import segment_matcher, SegmentMatcher
+from .algorithms import sequence_matcher, SequenceMatcher
+from .tokenizers import (Token, Tokenizer, RegexTokenizer, text_split,
+                         wikitext_split)
+from .segmenters import (Segmenter, Segment, MatchableSegment,
+                         ParagraphsSentencesAndWhitespace)
+
+__version__ = "0.4.0"
+
+__all__ = [apply,
+           Operation, Insert, Delete, Equal,
+           DiffEngine,
+           segment_matcher, SegmentMatcher,
+           sequence_matcher, SequenceMatcher,
+           Token, Tokenizer, RegexTokenizer, text_split, wikitext_split,
+           Segmenter, Segment, MatchableSegment,
+           ParagraphsSentencesAndWhitespace]

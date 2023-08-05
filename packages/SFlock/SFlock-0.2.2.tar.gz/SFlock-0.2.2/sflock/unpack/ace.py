302e3ebcd059561e71ee899e873f936e698fb0d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:47 +0000

    Add package SFlock/0.2.2/SFlock-0.2.2.tar.gz

---
+++ packages/SFlock/SFlock-0.2.2.tar.gz/SFlock-0.2.2/sflock/unpack/ace.py
@@ -0,0 +1,30 @@
+# Copyright (C) 2016 Jurriaan Bremer.
+# This file is part of SFlock - http://www.sflock.org/.
+# See the file 'docs/LICENSE.txt' for copying permission.
+
+import os
+import subprocess
+import tempfile
+
+from sflock.abstracts import Unpacker
+
+class AceFile(Unpacker):
+    name = "acefile"
+    exe = "/usr/bin/unace"
+    exts = ".ace"
+    magic = "ACE archive"
+
+    def unpack(self, password=None, duplicates=None):
+        dirpath = tempfile.mkdtemp()
+        filepath = os.path.abspath(self.f.filepath)
+
+        try:
+            subprocess.check_call([
+                self.zipjail, filepath, dirpath,
+                self.exe, "x", filepath, dirpath + os.sep,
+            ], stdin=subprocess.PIPE, stdout=subprocess.PIPE)
+        except subprocess.CalledProcessError as e:
+            self.f.mode = "failed"
+            self.f.error = e
+
+        return self.process_directory(dirpath, duplicates)
---
+++ packages/SFlock/SFlock-0.2.2.tar.gz/SFlock-0.2.2/sflock/unpack/rar.py
@@ -0,0 +1,29 @@
+# Copyright (C) 2015-2016 Jurriaan Bremer.
+# This file is part of SFlock - http://www.sflock.org/.
+# See the file 'docs/LICENSE.txt' for copying permission.
+
+import subprocess
+import tempfile
+
+from sflock.abstracts import Unpacker
+
+class RarFile(Unpacker):
+    name = "rarfile"
+    exe = "/usr/bin/rar"
+    exts = ".rar"
+    magic = "RAR archive"
+
+    def unpack(self, password=None, duplicates=None):
+        dirpath = tempfile.mkdtemp()
+
+        try:
+            subprocess.check_output([
+                self.zipjail, self.f.filepath, dirpath,
+                self.exe, "x", "-mt1", "-p%s" % (password or "-"),
+                self.f.filepath, dirpath,
+            ])
+        except subprocess.CalledProcessError as e:
+            self.f.mode = "failed"
+            self.f.error = e
+
+        return self.process_directory(dirpath, duplicates, password)

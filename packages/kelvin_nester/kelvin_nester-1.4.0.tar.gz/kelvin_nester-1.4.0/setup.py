8d3cdcba81fe6a1b140d6f29ee6a47eb9ef58da4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:00 +0000

    Add package kelvin_nester/1.4.0/kelvin_nester-1.4.0.tar.gz

---
+++ packages/kelvin_nester/kelvin_nester-1.4.0.tar.gz/kelvin_nester-1.4.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+       name            = 'kelvin_nester',
+       version         = '1.4.0',
+       py_modules      = ['kelvin_nester'],
+       author          = 'kelvintong',
+       author_email    = 'tonglisir@sina.com',
+       url             = 'http://www.headfirstlabs.com',
+       description     = 'A simple printer of nested lists',
+    )

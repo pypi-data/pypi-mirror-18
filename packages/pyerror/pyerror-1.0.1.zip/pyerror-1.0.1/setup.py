5b2e24d18d67bd1f62a43437206eedd0cc2a9f9d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:09:21 +0000

    Add package pyerror/1.0.1/pyerror-1.0.1.zip

---
+++ packages/pyerror/pyerror-1.0.1.zip/pyerror-1.0.1/setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+setup(
+  name = 'pyerror',
+  packages = ['pyerror'], # this must be the same as the name above
+  version = '1.0.1',
+  description = 'Error Detection Library In Python',
+  author = 'Sepand Haghighi',
+  author_email = 'sepand.haghighi@yahoo.com',
+  url = 'https://github.com/sepandhaghighi/error_detect/', # use the URL to the github repo
+  download_url = 'https://github.com/sepandhaghighi/error_detect/tarball/v1.0', # I'll explain this in a second
+  keywords = ['free', 'python', 'detection','error','CRC','parity'], # arbitrary keywords
+  classifiers = [],
+  license='MIT',
+
+)

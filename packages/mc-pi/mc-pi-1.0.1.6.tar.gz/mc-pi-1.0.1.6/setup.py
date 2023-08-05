b59f7e7fcd3b4b49a10f80b9ad9245e8c3fdae37
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:25 +0000

    Add package mc-pi/1.0.1.6/mc-pi-1.0.1.6.tar.gz

---
+++ packages/mc-pi/mc-pi-1.0.1.6.tar.gz/mc-pi-1.0.1.6/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name = "mc-pi",
+    version = "1.0.1.6",
+    author = "Martin O'Hanlon",
+    author_email="sr.tama@outlook.com",
+    description = ("Refer to http://www.stuffaboutcode.com"),
+    keywords = "raspberry pi minecraft python tutorial",
+    url = "https://github.com/MrMenezes/mcpi",
+    packages=['mcpi'],
+)

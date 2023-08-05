4bd14ee5dc9d757deedfc4e38bd8aba18eb92a97
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:55:02 +0000

    Add package vigil/0.1/vigil-0.1.tar.gz

---
+++ packages/vigil/vigil-0.1.tar.gz/vigil-0.1/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name='vigil',
+    version='0.1',
+    author='David Dworken',
+    author_email='david@daviddworken.com',
+    packages=['vigil'],
+    url='http://pypi.python.org/pypi/vigil/',
+    license='LICENSE.txt',
+    description='vigil... as a python package',
+    long_description=open('README.md').read(),
+)

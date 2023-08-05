1a0d38986b5f4c328a6edafeff6aa161e3f1feb5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:14 +0000

    Add package matplotlib2tikz/0.5.15/matplotlib2tikz-0.5.15.tar.gz

---
+++ packages/matplotlib2tikz/matplotlib2tikz-0.5.15.tar.gz/matplotlib2tikz-0.5.15/setup.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+#
+from distutils.core import setup
+import os
+import codecs
+
+from matplotlib2tikz import __version__, __license__, __author__, __email__
+
+
+def read(fname):
+    try:
+        content = codecs.open(
+            os.path.join(os.path.dirname(__file__), fname),
+            encoding='utf-8'
+            ).read()
+    except Exception:
+        content = ''
+    return content
+
+setup(
+    name='matplotlib2tikz',
+    version=__version__,
+    packages=['matplotlib2tikz'],
+    url='https://github.com/nschloe/matplotlib2tikz',
+    download_url='https://pypi.python.org/pypi/matplotlib2tikz',
+    author=__author__,
+    author_email=__email__,
+    requires=['matplotlib (>=1.4.0)', 'numpy', 'Pillow (>= 3.0.0)'],
+    description='convert matplotlib figures into TikZ/PGFPlots',
+    long_description=read('README.rst'),
+    license=__license__,
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+        'Topic :: Multimedia :: Graphics :: Graphics Conversion',
+        'Topic :: Scientific/Engineering :: Visualization'
+        ]
+    )

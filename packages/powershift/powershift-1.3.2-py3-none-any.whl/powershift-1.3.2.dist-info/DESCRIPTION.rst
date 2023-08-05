6aa3e51783724f8f9cbb14bccdb7bd02096bede6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:10:15 +0000

    Add package powershift/1.3.2/powershift-1.3.2-py3-none-any.whl

---
+++ packages/powershift/powershift-1.3.2-py3-none-any.whl/powershift-1.3.2.dist-info/DESCRIPTION.rst
@@ -0,0 +1,11 @@
+
+This package provides a Python library for working with the open source
+OpenShift Origin project and downstream OpenShift products from Red Hat.
+
+The library will provide the capability to work with OpenShift/Kubernetes
+resource objects, as well as endpoints for communicating with the OpenShift
+REST API.
+
+The package requires Python 3 and will not work with Python 2.
+
+
---
+++ packages/powershift/powershift-1.3.2-py3-none-any.whl/powershift/endpoints/base.py
@@ -0,0 +1,107 @@
+import os
+import requests
+
+_bool_tokens = ('1', 'true', 'enabled', 'on')
+
+_internal_token = '/var/run/secrets/kubernetes.io/serviceaccount/token'
+
+class Client(object):
+
+    def __init__(self, host=None, token=None, verify=None):
+        self.host = host
+        self.token = token
+        self.verify = verify
+
+        if self.host is None:
+            self.host = os.environ.get('OPENSHIFT_API_HOST')
+
+        if self.host is None:
+            self.host = 'openshift.default.svc.cluster.local'
+            if self.token is None and os.path.exists(_internal_token):
+                with open(_internal_token) as fp:
+                    self.token = fp.read()
+                self.verify = False
+
+        if self.token is None:
+            self.token = os.environ.get('OPENSHIFT_API_TOKEN', 'unset')
+
+        if self.verify is None:
+            self.verify = (os.environ.get('OPENSHIFT_API_VERIFY', 'true').lower()
+                    in _bool_tokens)
+
+    @property
+    def api(self):
+        return EndPoint(self, '/api')
+
+    @property
+    def oapi(self):
+        return EndPoint(self, '/oapi')
+
+_endpoint_api_types = {}
+
+def registered_endpoint_types():
+    return _endpoint_api_types
+
+def register_endpoint(cls):
+    _endpoint_api_types[cls.path] = cls
+    return cls
+
+class EndPoint(object):
+
+    path = None
+
+    _delete_ = {}
+    _get_ = {}
+    _head_ = {}
+    _options_ = {}
+    _patch_ = {}
+    _post_ = {}
+    _put_ = {}
+
+    def __init__(self, _client_, _path_=None, **params):
+        self.client = _client_
+
+        if _path_:
+            self.path = _path_
+
+        self.params = params
+
+    def __getattr__(self, name):
+        path = '%s/%s' % (self.path, name)
+        if path not in _endpoint_api_types:
+            if path not in _endpoint_api_types:
+                raise AttributeError('invalid API endpoint %r' % path)
+            return _endpoint_api_types[fallbackpath](self.client, **self.params)
+        return _endpoint_api_types[path](self.client, **self.params)
+
+@register_endpoint
+class EndPoint_oapi_v1_namespaces(EndPoint):
+
+    path = '/oapi/v1/namespaces'
+
+    def __call__(self, *, namespace):
+        child = self.path + '/{namespace}'
+        params = dict(self.params)
+        params['namespace'] = namespace
+        return EndPoint(self.client, child, **params)
+
+# @register_endpoint
+# class EndPoint_api_v1_watch(EndPoint):
+# 
+#     path = '/api/v1/watch'
+# 
+# @register_endpoint
+# class EndPoint_oapi_v1_watch(EndPoint):
+# 
+#     path = '/oapi/v1/watch'
+# 
+# @register_endpoint
+# class EndPoint_oapi_v1_watch_namespaces(EndPoint):
+# 
+#     path = '/oapi/v1/watch/namespaces'
+# 
+#     def __call__(self, *, namespace):
+#         child = self.path + '/{namespace}'
+#         params = dict(self.params)
+#         params['namespace'] = namespace
+#         return EndPoint(self.client, child, **params)

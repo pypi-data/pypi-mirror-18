925842ed06847806108581cb3482e5ccf4d127fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:17 +0000

    Add package flask_sqla_debug/0.2/flask_sqla_debug-0.2.tar.gz

---
+++ packages/flask_sqla_debug/flask_sqla_debug-0.2.tar.gz/flask_sqla_debug-0.2/setup.py
@@ -0,0 +1,15 @@
+"""Setup for flask_sqla_debug."""
+
+from distutils.core import setup
+setup(
+    name='flask_sqla_debug',
+    packages=['flask_sqla_debug'],
+    version='0.2',
+    description='Helpers for debugging flask and sqlalchemy performance',
+    author='Alfred Perlstein',
+    author_email='alfred.perlstein@gmail.com',
+    url='https://github.com/splbio/flask_sqla_debug',
+    download_url='https://github.com/splbio/flask_sqla_debug/tarball/0.1',
+    keywords=['testing', 'logging', 'debug', 'flask', 'sqlalchemy'],
+    classifiers=[],
+)

0d637e80ca62afc8019463de0c07b08abc4fab78
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:30 +0000

    Add package shlib/0.3.1/shlib-0.3.1.tar.gz

---
+++ packages/shlib/shlib-0.3.1.tar.gz/shlib-0.3.1/setup.py
@@ -0,0 +1,41 @@
+try:                                                                                                                   
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+with open('README.rst') as f:
+    readme = f.read()
+
+setup(
+    name='shlib',
+    version='0.3.1',
+    description='shell library',
+    long_description=readme,
+    author="Ken Kundert",
+    author_email='shlib@nurdletech.com',
+    py_modules=['shlib'],
+    url='http://nurdletech.com/linux-utilities/shlib',
+    download_url='https://github.com/kenkundert/shlib/tarball/master',
+    license='GPLv3+',
+    zip_safe=True,
+    install_requires=[
+        'braceexpand',  # this is optional
+        'extended_pathlib>=0.1',
+    ],
+    keywords=[
+        'shlib',
+        'shell',
+        'utilities',
+    ],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
+        'Natural Language :: English',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Topic :: Utilities',
+    ],
+)

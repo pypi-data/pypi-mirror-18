4aaac618d18b21c5e767feebdba23a9c6c1b1384
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:00 +0000

    Add package django-fobi/0.9.11/django-fobi-0.9.11.tar.gz

---
+++ packages/django-fobi/django-fobi-0.9.11.tar.gz/django-fobi-0.9.11/src/fobi/__init__.py
@@ -0,0 +1,8 @@
+__title__ = 'django-fobi'
+__version__ = '0.9.11'
+__build__ = 0x00006c
+__author__ = 'Artur Barseghyan <artur.barseghyan@gmail.com>'
+__copyright__ = '2014-2016 Artur Barseghyan'
+__license__ = 'GPL 2.0/LGPL 2.1'
+
+default_app_config = 'fobi.apps.Config'

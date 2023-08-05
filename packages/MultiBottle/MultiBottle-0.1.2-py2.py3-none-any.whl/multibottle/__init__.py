b6794db23cac6a27ae56a73ea35a8c83fcc98348
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:43 +0000

    Add package MultiBottle/0.1.2/MultiBottle-0.1.2-py2.py3-none-any.whl

---
+++ packages/MultiBottle/MultiBottle-0.1.2-py2.py3-none-any.whl/multibottle/__init__.py
@@ -0,0 +1,9 @@
+#from viewmodel import viewModel,viewFields
+from multibottle.mbextend import HtmlPage, ExtReq, trail_filter, fileExt_filter
+#from multibottle.pages import FmPage
+from multibottle.subpages.fmPage import FmPage
+from multibottle.subpages.basicPage import MsgPage
+
+# from viewmodel.viewFields import (Case, IdField, TxtField, IntField,
+#                 DateField, TimeField, EnumField, EnumForeignField,
+#                 TxtListField, ObjListField )

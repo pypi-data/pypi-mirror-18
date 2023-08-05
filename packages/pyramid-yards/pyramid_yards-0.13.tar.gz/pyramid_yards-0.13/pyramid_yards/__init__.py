1a71945ff4143ce91a679a146d28ba70c3b85d47
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:54 +0000

    Add package pyramid-yards/0.13/pyramid_yards-0.13.tar.gz

---
+++ packages/pyramid-yards/pyramid_yards-0.13.tar.gz/pyramid_yards-0.13/pyramid_yards/__init__.py
@@ -0,0 +1,28 @@
+"""
+pyramid_yards is a helpers for validating for on Pyramid
+
+See the README.rst file for more information.
+"""
+
+__version__ = '0.13'
+
+from pyramid.events import NewRequest
+from pyramid.settings import asbool
+
+from .yards import Yards, RequestSchemaPredicate, ValidationFailure
+
+
+def subscribe_yards(event):
+    request = event.request
+    request.set_property(Yards(request), 'yards', reify=True)
+
+
+def includeme(config):
+    settings = config.registry.settings
+    RequestSchemaPredicate.check_csrf_token = asbool(
+        settings.get('pyramid_yards.check_csrf_token', 'true'))
+    config.add_subscriber(subscribe_yards, NewRequest)
+    config.add_view_predicate('request_schema', RequestSchemaPredicate)
+
+    config.add_translation_dirs('pyramid_yards:locales'
+                                )

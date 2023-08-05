3a811752c2e0922f622050d7198ec2b3e2aa2de3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:25 +0000

    Add package tableaudocumentapi/0.5/tableaudocumentapi-0.5-py2.py3-none-any.whl

---
+++ packages/tableaudocumentapi/tableaudocumentapi-0.5-py2.py3-none-any.whl/tableaudocumentapi/connection.py
@@ -0,0 +1,156 @@
+###############################################################################
+#
+# Connection - A class for writing connections to Tableau files
+#
+###############################################################################
+import xml.etree.ElementTree as ET
+from tableaudocumentapi.dbclass import is_valid_dbclass
+
+
+class Connection(object):
+    """
+    A class for writing connections to Tableau files.
+
+    """
+
+    ###########################################################################
+    #
+    # Public API.
+    #
+    ###########################################################################
+
+    def __init__(self, connxml):
+        """
+        Constructor.
+
+        """
+        self._connectionXML = connxml
+        self._dbname = connxml.get('dbname')
+        self._server = connxml.get('server')
+        self._username = connxml.get('username')
+        self._authentication = connxml.get('authentication')
+        self._class = connxml.get('class')
+        self._port = connxml.get('port', None)
+
+    def __repr__(self):
+        return "'<Connection server='{}' dbname='{}' @ {}>'".format(self._server, self._dbname, hex(id(self)))
+
+    @classmethod
+    def from_attributes(cls, server, dbname, username, dbclass, port=None, authentication=''):
+        root = ET.Element('connection', authentication=authentication)
+        xml = cls(root)
+        xml.server = server
+        xml.dbname = dbname
+        xml.username = username
+        xml.dbclass = dbclass
+        xml.port = port
+
+        return xml
+
+    ###########
+    # dbname
+    ###########
+    @property
+    def dbname(self):
+        return self._dbname
+
+    @dbname.setter
+    def dbname(self, value):
+        """
+        Set the connection's database name property.
+
+        Args:
+            value:  New name of the database. String.
+
+        Returns:
+            Nothing.
+
+        """
+        self._dbname = value
+        self._connectionXML.set('dbname', value)
+
+    ###########
+    # server
+    ###########
+    @property
+    def server(self):
+        return self._server
+
+    @server.setter
+    def server(self, value):
+        """
+        Set the connection's server property.
+
+        Args:
+            value:  New server. String.
+
+        Returns:
+            Nothing.
+
+        """
+        self._server = value
+        self._connectionXML.set('server', value)
+
+    ###########
+    # username
+    ###########
+    @property
+    def username(self):
+        return self._username
+
+    @username.setter
+    def username(self, value):
+        """
+        Set the connection's username property.
+
+        Args:
+            value:  New username value. String.
+
+        Returns:
+            Nothing.
+
+        """
+        self._username = value
+        self._connectionXML.set('username', value)
+
+    ###########
+    # authentication
+    ###########
+    @property
+    def authentication(self):
+        return self._authentication
+
+    ###########
+    # dbclass
+    ###########
+    @property
+    def dbclass(self):
+        return self._class
+
+    @dbclass.setter
+    def dbclass(self, value):
+
+        if not is_valid_dbclass(value):
+            raise AttributeError("'{}' is not a valid database type".format(value))
+
+        self._class = value
+        self._connectionXML.set('class', value)
+
+    ###########
+    # port
+    ###########
+    @property
+    def port(self):
+        return self._port
+
+    @port.setter
+    def port(self, value):
+        self._port = value
+        # If port is None we remove the element and don't write it to XML
+        if value is None:
+            try:
+                del self._connectionXML.attrib['port']
+            except KeyError:
+                pass
+        else:
+            self._connectionXML.set('port', value)
---
+++ packages/tableaudocumentapi/tableaudocumentapi-0.5-py2.py3-none-any.whl/tableaudocumentapi/datasource.py
@@ -0,0 +1,260 @@
+###############################################################################
+#
+# Datasource - A class for writing datasources to Tableau files
+#
+###############################################################################
+import collections
+import itertools
+import xml.etree.ElementTree as ET
+import xml.sax.saxutils as sax
+from uuid import uuid4
+
+from tableaudocumentapi import Connection, xfile
+from tableaudocumentapi import Field
+from tableaudocumentapi.multilookup_dict import MultiLookupDict
+from tableaudocumentapi.xfile import xml_open
+
+########
+# This is needed in order to determine if something is a string or not.  It is necessary because
+# of differences between python2 (basestring) and python3 (str).  If python2 support is every
+# dropped, remove this and change the basestring references below to str
+try:
+    basestring
+except NameError:  # pragma: no cover
+    basestring = str
+########
+
+_ColumnObjectReturnTuple = collections.namedtuple('_ColumnObjectReturnTupleType', ['id', 'object'])
+
+
+def _get_metadata_xml_for_field(root_xml, field_name):
+    if "'" in field_name:
+        field_name = sax.escape(field_name, {"'": "&apos;"})
+    xpath = u".//metadata-record[@class='column'][local-name='{}']".format(field_name)
+    return root_xml.find(xpath)
+
+
+def _is_used_by_worksheet(names, field):
+    return any((y for y in names if y in field.worksheets))
+
+
+class FieldDictionary(MultiLookupDict):
+
+    def used_by_sheet(self, name):
+        # If we pass in a string, no need to get complicated, just check to see if name is in
+        # the field's list of worksheets
+        if isinstance(name, basestring):
+            return [x for x in self.values() if name in x.worksheets]
+
+        # if we pass in a list, we need to check to see if any of the names in the list are in
+        # the field's list of worksheets
+        return [x for x in self.values() if _is_used_by_worksheet(name, x)]
+
+
+def _column_object_from_column_xml(root_xml, column_xml):
+    field_object = Field.from_column_xml(column_xml)
+    local_name = field_object.id
+    metadata_record = _get_metadata_xml_for_field(root_xml, local_name)
+    if metadata_record is not None:
+        field_object.apply_metadata(metadata_record)
+    return _ColumnObjectReturnTuple(field_object.id, field_object)
+
+
+def _column_object_from_metadata_xml(metadata_xml):
+    field_object = Field.from_metadata_xml(metadata_xml)
+    return _ColumnObjectReturnTuple(field_object.id, field_object)
+
+
+def base36encode(number):
+    """Converts an integer into a base36 string."""
+
+    ALPHABET = "0123456789abcdefghijklmnopqrstuvwxyz"
+
+    base36 = ''
+    sign = ''
+
+    if number < 0:
+        sign = '-'
+        number = -number
+
+    if 0 <= number < len(ALPHABET):
+        return sign + ALPHABET[number]
+
+    while number != 0:
+        number, i = divmod(number, len(ALPHABET))
+        base36 = ALPHABET[i] + base36
+
+    return sign + base36
+
+
+def make_unique_name(dbclass):
+    rand_part = base36encode(uuid4().int)
+    name = dbclass + '.' + rand_part
+    return name
+
+
+class ConnectionParser(object):
+
+    def __init__(self, datasource_xml, version):
+        self._dsxml = datasource_xml
+        self._dsversion = version
+
+    def _extract_federated_connections(self):
+        connections = list(map(Connection, self._dsxml.findall('.//named-connections/named-connection/*')))
+        connections.extend(map(Connection, self._dsxml.findall("./connection[@class='sqlproxy']")))
+        return connections
+
+    def _extract_legacy_connection(self):
+        return list(map(Connection, self._dsxml.findall('connection')))
+
+    def get_connections(self):
+        if float(self._dsversion) < 10:
+            connections = self._extract_legacy_connection()
+        else:
+            connections = self._extract_federated_connections()
+        return connections
+
+
+class Datasource(object):
+    """
+    A class for writing datasources to Tableau files.
+
+    """
+
+    ###########################################################################
+    #
+    # Public API.
+    #
+    ###########################################################################
+    def __init__(self, dsxml, filename=None):
+        """
+        Constructor.  Default is to create datasource from xml.
+
+        """
+        self._filename = filename
+        self._datasourceXML = dsxml
+        self._datasourceTree = ET.ElementTree(self._datasourceXML)
+        self._name = self._datasourceXML.get('name') or self._datasourceXML.get(
+            'formatted-name')  # TDS files don't have a name attribute
+        self._version = self._datasourceXML.get('version')
+        self._caption = self._datasourceXML.get('caption', '')
+        self._connection_parser = ConnectionParser(
+            self._datasourceXML, version=self._version)
+        self._connections = self._connection_parser.get_connections()
+        self._fields = None
+
+    @classmethod
+    def from_file(cls, filename):
+        """Initialize datasource from file (.tds)"""
+
+        dsxml = xml_open(filename, cls.__name__.lower()).getroot()
+        return cls(dsxml, filename)
+
+    @classmethod
+    def from_connections(cls, caption, connections):
+        root = ET.Element('datasource', caption=caption, version='10.0', inline='true')
+        outer_connection = ET.SubElement(root, 'connection')
+        outer_connection.set('class', 'federated')
+        named_conns = ET.SubElement(outer_connection, 'named-connections')
+        for conn in connections:
+            nc = ET.SubElement(named_conns,
+                               'named-connection',
+                               name=make_unique_name(conn.dbclass),
+                               caption=conn.server)
+            nc.append(conn._connectionXML)
+        return cls(root)
+
+    def save(self):
+        """
+        Call finalization code and save file.
+
+        Args:
+            None.
+
+        Returns:
+            Nothing.
+
+        """
+
+        # save the file
+
+        xfile._save_file(self._filename, self._datasourceTree)
+
+    def save_as(self, new_filename):
+        """
+        Save our file with the name provided.
+
+        Args:
+            new_filename:  New name for the workbook file. String.
+
+        Returns:
+            Nothing.
+
+        """
+
+        xfile._save_file(self._filename, self._datasourceTree, new_filename)
+
+    ###########
+    # name
+    ###########
+    @property
+    def name(self):
+        return self._name
+
+    ###########
+    # version
+    ###########
+    @property
+    def version(self):
+        return self._version
+
+    @property
+    def caption(self):
+        return self._caption
+
+    @caption.setter
+    def caption(self, value):
+        self._datasourceXML.set('caption', value)
+        self._caption = value
+
+    @caption.deleter
+    def caption(self):
+        del self._datasourceXML.attrib['caption']
+        self._caption = ''
+
+    ###########
+    # connections
+    ###########
+    @property
+    def connections(self):
+        return self._connections
+
+    def clear_repository_location(self):
+        tag = self._datasourceXML.find('./repository-location')
+        if tag is not None:
+            self._datasourceXML.remove(tag)
+
+    ###########
+    # fields
+    ###########
+    @property
+    def fields(self):
+        if not self._fields:
+            self._fields = self._get_all_fields()
+        return self._fields
+
+    def _get_all_fields(self):
+        column_field_objects = self._get_column_objects()
+        existing_column_fields = [x.id for x in column_field_objects]
+        metadata_only_field_objects = (x for x in self._get_metadata_objects() if x.id not in existing_column_fields)
+        field_objects = itertools.chain(column_field_objects, metadata_only_field_objects)
+
+        return FieldDictionary({k: v for k, v in field_objects})
+
+    def _get_metadata_objects(self):
+        return (_column_object_from_metadata_xml(x)
+                for x in self._datasourceTree.findall(".//metadata-record[@class='column']"))
+
+    def _get_column_objects(self):
+        return [_column_object_from_column_xml(self._datasourceTree, xml)
+                for xml in self._datasourceTree.findall('.//column')]

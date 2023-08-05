1fd2eea97396e0e201609cbd9356755d40ff84ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:54 +0000

    Add package loggable-decorator/1.0.0/loggable-decorator-1.0.0.tar.gz

---
+++ packages/loggable-decorator/loggable-decorator-1.0.0.tar.gz/loggable-decorator-1.0.0/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'loggable-decorator',
+  packages = ['loggable'],
+  version = '1.0.0',
+  description = 'Add a logger attribute to class decorated',
+  author = 'Peter Downs',
+  author_email = 'peterldowns@gmail.com',
+  url = 'https://github.com/ateliedocodigo/python-loggable-decorator',
+  download_url = 'https://github.com/ateliedocodigo/python-loggable-decorator/tarball/1.0.0',
+  keywords = ['logging'],
+  classifiers = [],
+)

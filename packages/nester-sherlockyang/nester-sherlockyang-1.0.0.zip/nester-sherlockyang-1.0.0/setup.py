db8a2281602aa44dbfd3076a109f399436954d22
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:15 +0000

    Add package nester-sherlockyang/1.0.0/nester-sherlockyang-1.0.0.zip

---
+++ packages/nester-sherlockyang/nester-sherlockyang-1.0.0.zip/nester-sherlockyang-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name = 'nester-sherlockyang',
+    version = '1.0.0',
+    py_modules = ['nester-sherlockyang'],
+    author = 'probe',
+    author_email = 'gayayzy@163.com',
+    url = 'http://www.headfirst.com',
+    description = 'a simple nested list',
+)

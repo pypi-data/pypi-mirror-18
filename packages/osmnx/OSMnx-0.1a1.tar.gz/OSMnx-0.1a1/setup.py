5f4a10fdcca5723d055ffd08ff8c8a8154de428f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:56:54 +0000

    Add package osmnx/0.1a1/OSMnx-0.1a1.tar.gz

---
+++ packages/osmnx/OSMnx-0.1a1.tar.gz/OSMnx-0.1a1/setup.py
@@ -0,0 +1,23 @@
+# OSMnx
+# See full license in LICENSE.txt
+
+from setuptools import setup
+
+setup(name='OSMnx',
+      version='0.1a1',
+      description='Retrieve and construct spatial geometries and street networks from OpenStreetMap',
+      url='https://github.com/gboeing/osmnx',
+      author='Geoff Boeing',
+      author_email='gboeing@berkeley.edu',
+      license='MIT',
+      packages=['osmnx'],
+      install_requires=['requests>=2.11',
+                        'numpy>=1.11',
+                        'pandas>=0.19',
+                        'geopandas>=0.2',
+                        'networkx>=1.11',
+                        'matplotlib>=1.5',
+                        'shapely>=1.5',
+                        'descartes>=1.0',
+                        'geopy>=1.11',
+                        'rtree>=0.8'])

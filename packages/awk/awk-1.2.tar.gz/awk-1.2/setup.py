55bf1f95e9f636c4f344bc6c1411712c2989ebf2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:30 +0000

    Add package awk/1.2/awk-1.2.tar.gz

---
+++ packages/awk/awk-1.2.tar.gz/awk-1.2/setup.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(name='awk',
+      version='1.2',
+      py_modules=['awk'],
+      author='Simone Bronzini',
+      author_email='simone.bronzini@moveax.it',
+      url='https://github.com/SimoneBronzini/awk',
+      license='http://opensource.org/licenses/MIT',
+      description='A library to provide advanced awk-like functionalities for file manipulation in Python.',
+      classifiers=[
+          'Programming Language :: Python :: 3',
+          'Programming Language :: Python :: 3.3',
+          'Programming Language :: Python :: 3.4',
+          'Programming Language :: Python :: 3.5',
+          'Topic :: Text Processing :: General',
+          'Topic :: Software Development :: Libraries :: Python Modules',
+      ],)

e376e10a61caba33b036749325bba67c8bc0cc8f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:42 +0000

    Add package askfm.py/0.6.2/askfm.py-0.6.2.tar.gz

---
+++ packages/askfm.py/askfm.py-0.6.2.tar.gz/askfm.py-0.6.2/askfm/parser.py
@@ -0,0 +1,21 @@
+import os
+from bs4 import BeautifulSoup
+from .pair import Pair
+
+def _href_to_id(href):
+    return int(os.path.basename(href))
+
+def parse(answers):
+    '''parse given HTML from crawler.'''
+    answers = answers.replace('<br />', '\n')
+    soup = BeautifulSoup(answers, 'html.parser')
+    retval = []
+    for div in soup.select('.streamItem-answer'):
+        try:
+            _id = _href_to_id(div.select('.streamItemContent-footer')[0].a.get('href'))
+            question = div.select('.streamItemContent-question')[0].h2.string
+            answer = div.select('.streamItemContent-answer')[0].string
+            retval.append(Pair(_id, question, answer))
+        except IndexError:
+            pass
+    return retval

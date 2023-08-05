8b8ffb1092053422d61b9b09f6906d63972da0ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:55:40 +0000

    Add package pytunes/12.5/pytunes-12.5.tar.gz

---
+++ packages/pytunes/pytunes-12.5.tar.gz/pytunes-12.5/pytunes/__init__.py
@@ -0,0 +1,11 @@
+"""
+Python wrapper for iTunes using appscript APIs
+
+Module version marks the last tested compatible iTunes version.
+"""
+
+__version__ = '12.5'
+
+class iTunesError(Exception):
+    pass
+

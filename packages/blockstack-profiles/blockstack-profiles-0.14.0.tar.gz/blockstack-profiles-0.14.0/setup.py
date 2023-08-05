b5e2d204b4af908bc0add17e259a7633a043c41a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:02 +0000

    Add package blockstack-profiles/0.14.0/blockstack-profiles-0.14.0.tar.gz

---
+++ packages/blockstack-profiles/blockstack-profiles-0.14.0.tar.gz/blockstack-profiles-0.14.0/setup.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+"""
+Blockstack Profiles
+==============
+
+"""
+
+from setuptools import setup, find_packages
+
+setup(
+    name='blockstack-profiles',
+    version='0.14.0',
+    url='https://github.com/blockstack/blockstack-profiles-py',
+    license='MIT',
+    author='Blockstack Developers',
+    author_email='hello@onename.com',
+    description="""Library for blockstack profile generation and validation""",
+    keywords='bitcoin blockchain blockstack profile schema',
+    packages=find_packages(),
+    zip_safe=False,
+    install_requires=[
+        'cryptography>=1.2.3',
+        'jsontokens>=0.0.2',
+        'keylib>=0.0.5',
+        'blockstack-zones>=0.14.0'
+    ],
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet',
+        'Topic :: Security :: Cryptography',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+)

eedeebd854b8ff421ce8d62a9ff42ab450117c43
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:16 +0000

    Add package chorogrid/0.0.1/chorogrid-0.0.1-py2.py3-none-any.whl

---
+++ packages/chorogrid/chorogrid-0.0.1-py2.py3-none-any.whl/chorogrid/__init__.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+# Filename: __init__.py
+
+# Author: David Taylor (@Prooffreader)
+
+"""A python script to produce choropleths and colored square- and hex-grid maps"""
+
+__version__ = '0.0.1'
+
+from chorogrid.Colorbin import Colorbin
+from chorogrid.Chorogrid import Chorogrid
+

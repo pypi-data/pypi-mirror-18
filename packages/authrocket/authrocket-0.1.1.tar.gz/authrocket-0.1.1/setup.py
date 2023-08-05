5eef4f4a6731a0ac96fb0e460052f3d7990a0b35
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:09:08 +0000

    Add package authrocket/0.1.1/authrocket-0.1.1.tar.gz

---
+++ packages/authrocket/authrocket-0.1.1.tar.gz/authrocket-0.1.1/setup.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+
+import os.path
+
+from setuptools import setup
+
+ROOT = os.path.dirname(__file__)
+
+setup(
+    version="0.1.1",
+    url="https://github.com/nathforge/authrocket",
+    name="authrocket",
+    description="Unofficial AuthRocket API client",
+    long_description=open(os.path.join(ROOT, "README.rst")).read(),
+    author="Nathan Reynolds",
+    author_email="email@nreynolds.co.uk",
+    packages=["authrocket"],
+    package_dir={
+        "": os.path.join(ROOT, "src")
+    },
+    install_requires=[
+        line.strip()
+        for line in open(os.path.join(ROOT, "requirements.txt"))
+        if not line.startswith("#")
+        and line.strip() != ""
+    ],
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development",
+        "License :: OSI Approved :: MIT License",
+    ]
+)

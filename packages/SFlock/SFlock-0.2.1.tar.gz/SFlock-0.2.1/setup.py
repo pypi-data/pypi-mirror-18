faa29b8078009a37755514dc99fd83cf5cfb5d53
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:46 +0000

    Add package SFlock/0.2.1/SFlock-0.2.1.tar.gz

---
+++ packages/SFlock/SFlock-0.2.1.tar.gz/SFlock-0.2.1/setup.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# Copyright (C) 2015-2016 Jurriaan Bremer.
+# This file is part of SFlock - http://www.sflock.org/.
+# See the file 'docs/LICENSE.txt' for copying permission.
+
+from setuptools import setup
+
+setup(
+    name="SFlock",
+    version="0.2.1",
+    author="Jurriaan Bremer",
+    author_email="jbr@cuckoo.sh",
+    packages=[
+        "sflock",
+        "sflock.unpack",
+        "sflock.data",
+    ],
+    entry_points={
+        "console_scripts": [
+            "sflock = sflock.main:main",
+        ],
+    },
+    url="http://sflock.org/",
+    license="GPLv3",
+    description="Sample staging and detonation utility",
+    include_package_data=True,
+    package_data={
+        "sflock.data": ["*"],
+        "sflock.data.win32": ["*"],
+    },
+    install_requires=[
+        "click==6.6",
+        "olefile==0.43",
+        "python-magic==0.4.12",
+    ],
+)

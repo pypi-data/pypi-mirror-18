ceb941ab8fee009a2bbecbde6d197af1066977b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:12 +0000

    Add package PennSDK/1.3.1/PennSDK-1.3.1.tar.gz

---
+++ packages/PennSDK/PennSDK-1.3.1.tar.gz/PennSDK-1.3.1/penn/__init__.py
@@ -0,0 +1,10 @@
+__version__ = '1.2'
+
+from .registrar import Registrar
+from .directory import Directory
+from .dining import Dining
+from .dining import DiningV2
+from .transit import Transit
+from .news import News
+from .laundry import Laundry
+from .map import Map

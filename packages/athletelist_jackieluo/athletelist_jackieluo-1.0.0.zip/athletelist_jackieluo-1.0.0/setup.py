b4b1306906bfa29a27fa5f5a006df927b3cbdc23
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:00 +0000

    Add package athletelist_jackieluo/1.0.0/athletelist_jackieluo-1.0.0.zip

---
+++ packages/athletelist_jackieluo/athletelist_jackieluo-1.0.0.zip/athletelist_jackieluo-1.0.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+    name='athletelist_jackieluo',
+    version='1.0.0',
+    py_modules=['athletelist'],
+    author='jackieluo',
+    autor_email='luojiachen1@qq.com',
+    url='https://github.com/luojc0517',
+    description='chapter6 athletelist', )

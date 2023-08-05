bf581662970102dc798d8753e55eca0d35b83fd1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:56:55 +0000

    Add package xsconnect/0.1.4/xsconnect-0.1.4.zip

---
+++ packages/xsconnect/xsconnect-0.1.4.zip/xsconnect-0.1.4/xsconnect/peripheralboards/StickIt_OctoADC_V1.py
@@ -0,0 +1,17 @@
+brd = {
+    'name': ('StickIt! OctoADC V1'),
+    'port': {
+        'pmod': {
+            'default' : {
+                'dout'   : 'd1',
+                'sclk'   : 'd3',
+                'din'    : 'd5',
+                'cs_n'   : 'd7',
+                'invclk0': 'd0',
+                'invclk1': 'd2',
+                'invclk2': 'd4',
+                'invclk3': 'd6',
+            }
+        }
+    }
+}

113e8fcc29225263345d272a9d68219ae14bb856
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:57:02 +0000

    Add package weasl/0.1/weasl-0.1.tar.gz

---
+++ packages/weasl/weasl-0.1.tar.gz/weasl-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='weasl',
+    version='0.1',
+    description='weakly supervised active learning framework',
+    url='https://github.com/njpataki/weasl',
+    author='Nicholas Pataki',
+    author_email='njpatakI@gmail.com',
+    license='MIT',
+    packages=['weasl'],
+    zip_safe=False)

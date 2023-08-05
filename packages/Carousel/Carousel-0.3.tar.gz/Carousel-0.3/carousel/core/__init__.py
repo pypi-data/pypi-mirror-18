c1d947b8b24f89568d2a1c6128dccb84a0652077
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:45 +0000

    Add package Carousel/0.3/Carousel-0.3.tar.gz

---
+++ packages/Carousel/Carousel-0.3.tar.gz/Carousel-0.3/carousel/core/__init__.py
@@ -0,0 +1,295 @@
+# -*- coding: utf-8 -*-
+"""
+This is the Carousel core package. Carousel is extensible through its
+built-in classes; therefore adding new data sources, formulas importers,
+calculators, simulations is accomplished by sub-classing the appropriate
+class.
+
+The built-in classes are organized into 5 categories, which make up the layers
+of a model.
+
+* Data
+* Formulas
+* Calculations
+* Simulations
+* Outputs
+
+See the :ref:`dev-intro` in the developer section for more information on each
+section.
+"""
+
+import pint
+import os
+from inspect import getargspec
+import functools
+import json
+import numpy as np
+import warnings
+import logging
+from carousel.core.exceptions import (
+    DuplicateRegItemError, MismatchRegMetaKeysError
+)
+
+warnings.simplefilter('always', DeprecationWarning)
+logging.captureWarnings(True)
+# create default logger from root logger with debug level, stream handler and
+# formatter with date-time, function name, line no and basic configuration
+LOG_DATEFMT = '%Y-%m-%d %H:%M:%S'
+LOG_FORMAT = ('\n> %(asctime)s %(funcName)s:%(lineno)d\n> ' +
+              '\n'.join(logging.BASIC_FORMAT.rsplit(':', 1)))
+logging.basicConfig(datefmt=LOG_DATEFMT, format=LOG_FORMAT)
+LOGGER = logging.getLogger(__name__)
+LOGGER.setLevel(logging.DEBUG)
+
+# unit registry, quantity constructor and extra units registry definitions
+UREG = pint.UnitRegistry()  # registry of units
+Q_ = UREG.Quantity  # quantity constructor for ambiguous quantities like degC
+UREG.define('lumen = cd * sr = lm')
+UREG.define('lux = lumen / m ** 2.0 = lx')
+UREG.define('fraction = []')  # define new dimensionless base unit for percents
+UREG.define('percent = fraction / 100.0 = pct')  # can't use "%" only ascii
+UREG.define('suns = []')  # dimensionless unit equivalent to 1000.0 [W/m/m]
+
+# define PV solar context
+_PV = pint.Context('pv')
+# define transformation of suns to power flux and vice versa
+E0 = 1000.0 * UREG.W / UREG.m / UREG.m  # 1 sun
+_PV.add_transformation('[]', '[power] / [area]', lambda ureg, x: x * E0)
+_PV.add_transformation('[power] / [area]', '[]', lambda ureg, x: x / E0)
+UREG.add_context(_PV)
+
+
+def _listify(x):
+    """
+    If x is not a list, make it a list.
+    """
+    return list(x) if isinstance(x, (list, tuple)) else [x]
+
+
+class Registry(dict):
+    """
+    Base class for a registry.
+
+    The register method can be used to add new keys to the registry only if
+    they don't already exist. A registry can also have meta data associated
+    with subsets of the registered keys. To enforce rules on meta when the keys
+    are registered, override the register method and raise exceptions before
+    calling the :func:`super` built-in function.
+
+    By default there are no meta attributes, only the register method.
+    To set meta attributes, in a subclass, set the ``meta_names`` class
+    attribute in the subclass::
+
+        class MyRegistry(Registry):
+            meta_names = ['meta1', 'meta2', ...]
+
+    The ``Registry`` superclass will check that the meta names are not already
+    attributes and then set instance attributes as empty dictionaries in the
+    subclass. To document them, use the class docstring or document them in the
+    documentation API.
+    """
+    meta_names = []
+
+    def __init__(self):
+        self.meta_names = _listify(self.meta_names)  # convert to list
+        for m in self.meta_names:
+            # check for m in cls and bases
+            if m in dir(Registry):
+                msg = ('Class %s already has %s member.' %
+                       (self.__class__.__name__, m))
+                raise AttributeError(msg)
+            setattr(self, m, {})  # create instance attribute and set to dict()
+        super(Registry, self).__init__()
+
+    def register(self, newitems, *args, **kwargs):
+        """
+        Register newitems in registry.
+
+        :param newitems: New items to add to registry. When registering new
+            items, keys are not allowed to override existing keys in the
+            registry.
+        :type newitems: mapping
+        :param args: Positional arguments with meta data corresponding to order
+            of meta names class attributes
+        :param kwargs: Maps of corresponding meta for new keys. Each set of
+            meta keys must be a subset of the new item keys.
+        :raises:
+            :exc:`~carousel.core.exceptions.DuplicateRegItemError`,
+            :exc:`~carousel.core.exceptions.MismatchRegMetaKeysError`
+        """
+        newkeys = newitems.viewkeys()  # set of the new item keys
+        if any(self.viewkeys() & newkeys):  # duplicates
+            raise DuplicateRegItemError(self.viewkeys() & newkeys)
+        self.update(newitems)  # register new item
+        # update meta fields
+        kwargs.update(zip(self.meta_names, args))
+        for k, v in kwargs.iteritems():
+            meta = getattr(self, k)  # get the meta attribute
+            if v:
+                if not v.viewkeys() <= newkeys:
+                    raise MismatchRegMetaKeysError(newkeys - v.viewkeys())
+                meta.update(v)  # register meta
+
+    def unregister(self, items):
+        """
+        Remove items from registry.
+
+        :param items:
+        """
+        items = _listify(items)
+        # get all members of Registry except private, special or class
+        meta_names = (m for m in vars(self).iterkeys()
+                      if (not m.startswith('_') and m not in dir(Registry)))
+        # check that meta names matches
+        # FIXME: this is so lame. replace this with something more robust
+        for m in meta_names:
+            if m not in self.meta_names:
+                raise AttributeError('Meta name %s not listed.')
+        # pop items from Registry and from meta
+        for it in items:
+            if it in self:
+                self.pop(it)
+            for m in (getattr(self, m_) for m_ in self.meta_names):
+                if it in m:
+                    m.pop(it)
+
+
+# decorator to use with formulas to convert argument units
+def convert_args(test_fcn, *test_args):
+    """
+    Decorator to be using in formulas to convert ``test_args`` depending on
+    the ``test_fcn``.
+
+    :param test_fcn: A test function that converts arguments.
+    :type test_fcn: function
+    :param test_args: Names of args to convert using ``test_fcn``.
+    :type test_args: str
+
+    The following test functions are available.
+    * :func:`dimensionless_to_index`
+
+    Example: Convert ``dawn_idx`` and ``eve_idx`` to indices::
+
+        @convert_args(dimensionless_to_index, 'dawn_idx', 'eve_idx')
+        def f_max_T(Tcell24, dawn_idx, eve_idx):
+            idx = dawn_idx + np.argmax(Tcell24[dawn_idx:eve_idx])
+            return Tcell24[idx], idx
+    """
+    def wrapper(origfcn):
+        @functools.wraps(origfcn)
+        def newfcn(*args, **kwargs):
+            argspec = getargspec(origfcn)  # use ``inspect`` to get arg names
+            kwargs.update(zip(argspec.args, args))  # convert args to kw
+            # loop over test args
+            for a in test_args:
+                # convert a if it's in args
+                if a in argspec.args:
+                    kwargs[a] = test_fcn(kwargs[a])  # update kwargs
+            # call original function with converted args
+            return origfcn(**kwargs)
+        # return wrapped function
+        return newfcn
+    # return the wrapper function that consumes the original function
+    return wrapper
+
+# NOTE: Preferred way to compare units is with dimensionality
+# EG: (25 * UREG.degC).dimensionality == UREG.degC.dimensionality
+# XXX: Really? because this works too, seems way better!
+# EG: (25 * UREG.degC).units = UREG.degC
+
+
+def dimensionless_to_index(index):
+    # convert dimensionless to index
+    if not index.dimensionality:
+        index = index.magnitude
+    else:
+        raise TypeError('Indices must be dimensionless.')
+    # TODO: make an exception called IndexUnitsError
+    return index
+
+
+# custom JSON encoder to serialize Quantities and NumPy arrays
+class CarouselJSONEncoder(json.JSONEncoder):
+    def default(self, o):
+        """
+        JSONEncoder default method that converts NumPy arrays and quantities
+        objects to lists.
+        """
+        if isinstance(o, Q_):
+            return o.magnitude
+        elif isinstance(o, np.ndarray):
+            return o.tolist()
+        else:
+            # raise TypeError if not serializable
+            return super(CarouselJSONEncoder, self).default(o)
+
+
+class CommonBase(type):
+    """
+    Provides common metaclass methods.
+
+    * :meth:`get_parents` ensures initialization only from subclasses of the
+      main class and not the main class itself
+    * :meth:`set_param_file_or_parameters` adds class attributes ``param_file``
+      or ``parameters`` depending on whether the path and file of the parameters
+      are given or if the parameters are listed as class attributes.
+
+    Base classes must implement the ``_path_attr`` and ``_file_attr`` as class
+    attributes::
+
+        class ExampleBase(CommonBase):
+            _path_attr = 'outputs_path'  # class attribute with parameter path
+            _file_attr = 'outputs_file'  # class attribute with parameter file
+    """
+    _path_attr = NotImplemented
+    _file_attr = NotImplemented
+
+    @classmethod
+    def set_param_file_or_parameters(mcs, attr):
+        cls_path = attr.pop(mcs._path_attr, None)
+        cls_file = attr.pop(mcs._file_attr, None)
+        # TODO: read parameters from param_file and then also update from attr
+        if None not in [cls_path, cls_file]:
+            attr['param_file'] = os.path.join(cls_path, cls_file)
+        else:
+            attr['parameters'] = dict.fromkeys(
+                k for k, v in attr.iteritems() if isinstance(v, Parameter)
+            )
+            for k in attr['parameters']:
+                attr['parameters'][k] = attr.pop(k)
+        return attr
+
+    @staticmethod
+    def get_parents(bases, parent):
+        """
+        Ensures that initialization only performed on subclasses of parent
+        https://github.com/django/django/blob/master/django/db/models/base.py
+
+        :param bases: Bases to compare against parent.
+        :type bases: list
+        :param parent: Superclass that bases should be subclassed from.
+        :return: Bases subclassed from parent.
+        :rtype: list
+        """
+        return [b for b in bases if isinstance(b, parent)]
+
+
+class Parameter(dict):
+    _attrs = []
+
+    def __init__(self, *args, **kwargs):
+        items = dict(zip(self._attrs, args))
+        extras = {}
+        for key, val in kwargs.iteritems():
+            if key in self._attrs:
+                items[key] = val
+            else:
+                extras[key] = val
+                LOGGER.warning('This key: "%s" is not an attribute.', key)
+        super(Parameter, self).__init__(items, extras=extras)
+
+    def __repr__(self):
+        fmt = ('<%s(' % self.__class__.__name__)
+        fmt += ', '.join('%s=%r' % (k, v) for k, v in self.iteritems())
+        fmt += ')>'
+        return fmt
---
+++ packages/Carousel/Carousel-0.3.tar.gz/Carousel-0.3/carousel/core/models.py
@@ -0,0 +1,336 @@
+# -*- coding: utf-8 -*-
+"""
+This is the Carousel :mod:`~carousel.core.models` module that contains
+definitions for the :class:`~carousel.core.models.Model` class.
+
+The Carousel model contains five layers:
+:class:`~carousel.core.layers.Data`,
+:class:`~carousel.core.layers.Formulas`,
+:class:`~carousel.core.layers.Calculations`,
+:class:`~carousel.core.layers.Outputs` and
+:class:`~carousel.core.layers.Simulations`. The
+:class:`~carousel.core.layers.Data` layer organizes
+:ref:`data-sources` by providing methods to add and load data for Carousel.
+The :class:`~carousel.core.layers.Formulas` layer loads
+:ref:`formulas` used by :class:`~carousel.core.layers.Calculations`
+calculations. The :class:`~carousel.core.layers.Outputs` layer
+organizes the calculated outputs for use in other calculations. Finally the
+:class:`~carousel.core.layers.Simulations` layer organizes
+options such as how long the simulation should run and takes care of actually
+running the simulation.
+"""
+
+import importlib
+import json
+import os
+from carousel.core import logging, _listify, CommonBase
+
+LOGGER = logging.getLogger(__name__)
+LAYERS_MOD = '.layers'
+LAYERS_PKG = 'carousel.core'
+
+
+class ModelBase(CommonBase):
+    """
+    Base model meta class. If model has class attributes "modelpath" and
+    "modelfile" then layer class names and model configuration will be read from
+    the file on that path. Otherwise layer class names will be read from the
+    class attributes.
+    """
+    _path_attr = 'modelpath'
+    _file_attr = 'modelfile'
+    _layers_cls_attr = 'layer_cls_names'
+
+    def __new__(mcs, name, bases, attr):
+        # use only with Model subclasses
+        if not CommonBase.get_parents(bases, ModelBase):
+            return super(ModelBase, mcs).__new__(mcs, name, bases, attr)
+        # set model file full path if model path and file specified or
+        # try to set parameters from class attributes except private/magic
+        modelpath = attr.get(mcs._path_attr)  # path to models and layers
+        modelfile = attr.pop(mcs._file_attr, None)  # model file
+        layer_cls_names = attr.get(mcs._layers_cls_attr)
+        # check bases for model parameters b/c attr doesn't include bases
+        for base in bases:
+            if layer_cls_names is None:
+                layer_cls_names = getattr(base, mcs._layers_cls_attr, None)
+            if modelpath is None:
+                modelpath = getattr(base, mcs._path_attr, None)
+            if modelfile is None:
+                modelfile = getattr(base, mcs._file_attr, None)
+        if None not in [modelpath, modelfile]:
+            attr[mcs._file_attr] = os.path.join(modelpath, 'models', modelfile)
+        elif layer_cls_names is not None:
+            attr['model'] = dict.fromkeys(layer_cls_names)
+            for k in attr['model']:
+                attr['model'][k] = attr.pop(k, None)
+        return super(ModelBase, mcs).__new__(mcs, name, bases, attr)
+
+
+class Model(object):
+    """
+    A class for models. Carousel is a subclass of the :class:`Model` class.
+
+    :param modelfile: The name of the JSON file with model data.
+    :type modelfile: str
+    """
+    __metaclass__ = ModelBase
+    # TODO: these should be in a Meta class
+    #: dictionary of layer class names
+    layer_cls_names = {'data': 'Data', 'calculations': 'Calculations',
+                       'formulas': 'Formulas', 'outputs': 'Outputs',
+                       'simulations': 'Simulations'}
+    # FIXME: doesn't work for layers in different modules
+    # TODO: should be dictionaries, combined with layer_cls_names and modelfile
+    #: module with layer class definitions
+    layers_mod = LAYERS_MOD
+    #: package with layers module
+    layers_pkg = LAYERS_PKG
+    #: simulation layer
+    cmd_layer_name = 'simulations'
+
+    def __init__(self, modelfile=None):
+        # check for modelfile in meta class, but use argument if not None
+        if modelfile is None and hasattr(self, 'modelfile'):
+            modelfile = self.modelfile
+            modelpath = self.modelpath
+            LOGGER.debug('modelfile: %s', modelfile)
+        else:
+            # modelfile was either given as arg or wasn't in metaclass
+            modelpath = None  # modelpath will be derived from modelfile
+            #: model file
+            self.modelfile = modelfile
+        # get modelpath from modelfile if not in meta class
+        if modelfile is not None and modelpath is None:
+            self.modelfile = os.path.abspath(modelfile)
+            #: model path, used to find layer files relative to model
+            self.modelpath = os.path.dirname(os.path.dirname(self.modelfile))
+            modelpath = self.modelpath  # update this field just in case
+        # check meta class for model if declared inline
+        if hasattr(self, 'model'):
+            model = self.model
+        else:
+            #: dictionary of the model
+            self.model = model = None
+        # layer attributes initialized in meta class or _initialize()
+        # for k, v in layer_cls_names.iteritems():
+        #     setattr(self, k, v)
+        # XXX: this seems bad to initialize attributes outside of constructor
+        #: dictionary of model layer classes
+        self.layers = {}
+        #: state of model, initialized or uninitialized
+        self._state = 'uninitialized'
+        # need either model file or model and layer class names to initialize
+        ready_to_initialize = ((modelfile is not None or model is not None) and
+                               self.layer_cls_names is not None)
+        if ready_to_initialize:
+            self._initialize()  # initialize using modelfile or model
+
+    @property
+    def state(self):
+        """
+        current state of the model
+        """
+        return self._state
+
+    def _load(self, layer=None):
+        """
+        Load or update all or part of :attr:`model`.
+
+        :param layer: Optionally load only specified layer.
+        :type layer: str
+        """
+        # open model file for reading and convert JSON object to dictionary
+        with open(self.modelfile, 'r') as fp:
+            _model = json.load(fp)
+        # if layer argument spec'd then only update/load spec'd layer
+        if not layer or not self.model:
+            # update/load model if layer not spec'd or if no model exists yet
+            self.model = _model
+        else:
+            # convert non-sequence to tuple
+            layers = _listify(layer)
+            # update/load layers
+            for layer in layers:
+                self.model[layer] = _model[layer]
+
+    def _update(self, layer=None):
+        """
+        Update layers in model.
+        """
+        if not layer:
+            layers = self.layers
+        else:
+            # convert non-sequence to tuple
+            layers = _listify(layer)
+        for layer in layers:
+            # relative path to layer files from model file
+            path = os.path.abspath(os.path.join(self.modelpath, layer))
+            getattr(self, layer).load(path)
+
+    def _initialize(self):
+        """
+        Initialize model and layers.
+        """
+        # read modelfile, convert JSON and load/update model
+        if self.modelfile is not None:
+            self._load()
+        LOGGER.debug('model:\n%r', self.model)
+        # initialize layers
+        # FIXME: move import inside loop for custom layers in different modules
+        mod = importlib.import_module(self.layers_mod, self.layers_pkg)
+        src_model = {}
+        for layer, value in self.model.iteritems():
+            # from layers module get the layer's class definition
+            layer_cls = getattr(mod, self.layer_cls_names[layer])  # class def
+            self.layers[layer] = layer_cls  # add layer class def to model
+            # check if model layers are classes
+            src_value = {}  # layer value generated from source classes
+            for src in value:
+                # skip if not a source class
+                if isinstance(src, basestring):
+                    continue
+                # check if source has keyword arguments
+                try:
+                    src, kwargs = src
+                except TypeError:
+                    kwargs = {}  # no key work arguments
+                # generate layer value from source class
+                src_value[src.__name__] = {'module': src.__module__,
+                                           'package': None}
+                # update layer keyword arguments
+                src_value[src.__name__].update(kwargs)
+            # use layer values generated from source class
+            if src_value:
+                value = src_model[layer] = src_value
+            # set layer attribute with model data
+            setattr(self, layer, layer_cls(value))
+        # update model with layer values generated from source classes
+        if src_model:
+            self.model.update(src_model)
+        self._update()
+        self._state = 'initialized'
+
+    def load(self, modelfile, layer=None):
+        """
+        Load or update a model or layers in a model.
+
+        :param modelfile: The name of the json file to load.
+        :type modelfile: str
+        :param layer: Optionally load only specified layer.
+        :type layer: str
+        """
+        # read modelfile, convert JSON and load/update model
+        self.modelfile = modelfile
+        self._load(layer)
+        self._update(layer)
+
+    def edit(self, layer, item, delete=False):
+        """
+        Edit model.
+
+        :param layer: Layer of model to edit
+        :type layer: str
+        :param item: Items to edit.
+        :type item: dict
+        :param delete: Flag to return
+            :class:`~carousel.core.layers.Layer` to delete item.
+        :type delete: bool
+        """
+        # get layer attribute with model data
+        if hasattr(self, layer):
+            layer_obj = getattr(self, layer)
+        else:
+            raise AttributeError('missing layer: %s', layer)
+        if delete:
+            return layer_obj
+        # iterate over items and edit layer
+        for k, v in item.iteritems():
+            if k in layer_obj.layer:
+                layer_obj.edit(k, v)  # edit layer
+            else:
+                raise AttributeError('missing layer item: %s', k)
+            # update model data
+            if k in self.model[layer]:
+                self.model[layer][k].update(v)
+            else:
+                raise AttributeError('missing model layer item: %s', k)
+
+    def add(self, layer, items):
+        """
+        Add items in model.
+        """
+        for k in items.iterkeys():
+            if k in self.model[layer]:
+                raise Exception('item %s is already in layer %s' % (k, layer))
+        self.model[layer].update(items)
+        # this should also update Layer.layer, the layer data
+        # same as calling layer constructor
+        # so now just need to add items to the layer
+        for k, v in items.iteritems():
+            getattr(self, layer).add(k, v['module'], v.get('package'))
+
+    def delete(self, layer, items):
+        """
+        Delete items in model.
+        """
+        # Use edit to get the layer obj containing item
+        items = _listify(items)  # make items a list if it's not
+        layer_obj = self.edit(layer, dict.fromkeys(items), delete=True)
+        for k in items:
+            if k in layer_obj.layer:
+                layer_obj.delete(k)
+            else:
+                raise AttributeError('item %s missing from layer %s' %
+                                     (k, layer))
+            # don't need to pop items from self.model, because, self.layer
+            # points to the same object as the item in model!
+            # for example:
+            #    (Pdb) id(self.model['data'])  # same ID as layer in data
+            #    125639560L
+            #    (Pdb) id(self.data.layer)  # same ID as data in model
+            #    125639560L
+
+    def save(self, modelfile, layer=None):
+        """
+        Save a model file.
+
+        :param modelfile: The name of the json file to save.
+        :type modelfile: str
+        :param layer: Optionally save only specified layer.
+        :type layer: str
+        """
+        if layer:
+            obj = {layer: self.model[layer]}
+        else:
+            obj = self.model
+        with open(modelfile, 'w') as fp:
+            json.dump(obj, fp, indent=2, sort_keys=True)
+
+    @property
+    def registries(self):
+        return {layer: getattr(self, layer).reg
+                for layer in self.layers}
+
+    @property
+    def cmd_layer(self):
+        return getattr(self, self.cmd_layer_name, NotImplemented)
+
+    @property
+    def commands(self):
+        return self.cmd_layer.reg.commands
+
+    def command(self, cmd, progress_hook=None, *args, **kwargs):
+        """
+        Execute a model command.
+
+        :param cmd: Name of the command.
+        :param progress_hook: A function to which progress updates are passed.
+        """
+        cmds = cmd.split(None, 1)  # split commands and simulations
+        sim_names = cmds[1:]  # simulations
+        if not sim_names:
+            sim_names = self.cmd_layer.reg.iterkeys()
+        for sim_name in sim_names:
+            sim_cmd = getattr(self.cmd_layer.reg[sim_name], cmd)
+            sim_cmd(self, progress_hook=progress_hook, *args, **kwargs)

6138631bb8ef52b1cdc2250eb7a1dc38ea0c379c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:39 +0000

    Add package initclass_mahe/0.1/initclass_mahe-0.1.zip

---
+++ packages/initclass_mahe/initclass_mahe-0.1.zip/initclass_mahe-0.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='initclass_mahe',
+      version='0.1',
+      description ='Mahe\python package',
+      url='',
+      author='Mahe',
+      author_email='mayyanar@grundfos.com',
+      license='GPL',
+      packages=['initclass_mahe'],
+      zip_safe=False)
+      

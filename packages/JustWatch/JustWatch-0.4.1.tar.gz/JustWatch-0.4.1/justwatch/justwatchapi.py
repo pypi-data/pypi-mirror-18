bd52f0fdbf1e7815f6196ad94e66cf30b86da80d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:20 +0000

    Add package JustWatch/0.4.1/JustWatch-0.4.1.tar.gz

---
+++ packages/JustWatch/JustWatch-0.4.1.tar.gz/JustWatch-0.4.1/justwatch/justwatchapi.py
@@ -0,0 +1,36 @@
+import requests
+from babel import Locale
+
+class JustWatch:
+	def __init__(self, country='AU', **kwargs):
+		self.kwargs = kwargs
+		self.country = country
+		self.language = Locale.parse('und_{}'.format(self.country)).language
+	def search_for_item(self, **kwargs):
+		if kwargs:
+			self.kwargs = kwargs
+		null = None
+		payload = {
+			"content_types":null,
+			"presentation_types":null,
+			"providers":null,
+			"genres":null,
+			"languages":null,
+			"release_year_from":null,
+			"release_year_until":null,
+			"monetization_types":null,
+			"min_price":null,
+			"max_price":null,
+			"scoring_filter_types":null,
+			"cinema_release":null,
+			"query":null
+		}
+		for key, value in self.kwargs.items():
+			if key in payload.keys():
+				payload[key] = value
+			else:
+				print('{} is not a valid keyword'.format(key))
+		header = {'User-Agent':'JustWatch Python client (github.com/dawoudt/JustWatchAPI)'}
+		api_url = 'https://api.justwatch.com/titles/{}_{}/popular'.format(self.language, self.country)
+		r = requests.post(api_url, json=payload, headers=header)
+		return r.json()

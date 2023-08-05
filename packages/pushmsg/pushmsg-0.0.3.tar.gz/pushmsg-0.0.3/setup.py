19dff0617fb67f9d197615c81db8ff1e36eb36af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:23:40 +0000

    Add package pushmsg/0.0.3/pushmsg-0.0.3.tar.gz

---
+++ packages/pushmsg/pushmsg-0.0.3.tar.gz/pushmsg-0.0.3/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+import os
+import pushmsg
+
+VERSION = pushmsg.__version__
+
+install_reqs = [
+'ipython',
+'pushbullet.py>=0.10.0'
+]
+
+setup(
+    name='pushmsg',
+    version=VERSION,
+    license='MIT',
+    description=('IPython magic function to send notifications wtih Pushbullet'),
+    author='Nicholas Youngblut',
+    author_email='nyoungb2@gmail.com',
+    url='https://github.com/nick-youngblut/pushmsg',
+    packages=find_packages(exclude=[]),
+    install_requires=install_reqs,
+    long_description="""
+An IPython magic extension for sending notifications with Pushbullet.
+It is useful for setting notfications for the completion of long jobs.
+"""
+)

4d011d66aa9fc1e31e067df9c6cfc0412f1dab25
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:12 +0000

    Add package MimirNotes/0.4.5/MimirNotes-0.4.5.tar.gz

---
+++ packages/MimirNotes/MimirNotes-0.4.5.tar.gz/MimirNotes-0.4.5/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+
+def readme():
+    with open('README.rst') as f:
+        return f.read()
+
+setup(name='MimirNotes',
+      version='0.4.5',
+      description='A simple, command line, note taking utility.',
+      keywords='note terminal command-line journal',
+      url='https://github.com/jcerise/mimir',
+      author='Jeremy Cerise',
+      author_email='jcerise06@gmail.com',
+      packages=['MimirNotes'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=['click', 'enum34', 'dateparser'],
+      entry_points='''
+            [console_scripts]
+            mimir=MimirNotes.mimir:cli
+      ''',
+      )

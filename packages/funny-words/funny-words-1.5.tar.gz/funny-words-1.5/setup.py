be2f45a34743e7b78406e02ae1cf561a56c05a87
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:10 +0000

    Add package funny-words/1.5/funny-words-1.5.tar.gz

---
+++ packages/funny-words/funny-words-1.5.tar.gz/funny-words-1.5/setup.py
@@ -0,0 +1,20 @@
+from distutils.core import setup
+
+setup(
+    name='funny-words',
+    version='1.5',
+    packages=['funny_words'],
+    author='Seth Black',
+    author_email='sblack@sethserver.com',
+    url='https://github.com/sethblack/funny-words',
+    license='Apache 2.0',
+    description='Generates a list of funny words for naming things such as app releases, internal projects, servers and children.',
+    scripts=['funny-words'],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python'
+    ]
+)

3cce4db7329a839b6acf4841559028b2b0f0d383
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:08:55 +0000

    Add package parsec/3.1/parsec-3.1.zip

---
+++ packages/parsec/parsec-3.1.zip/parsec-3.1/setup.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+setup(
+    name = 'parsec',
+    version = '3.1',
+    description = 'parser combinator.',
+    long_description = 'A univeral Python parser combinator library inspirted by Parsec library of Haskell.',
+    author = 'He Tao',
+    author_email = 'sighingnow@gmail.com',
+    url = 'https://github.com/sighignow/parsec.py',
+    license = 'MIT',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        "Topic :: Software Development :: Compilers",
+        'Operating System :: MacOS :: MacOS X',
+        'Operating System :: Microsoft :: Windows',
+        'Operating System :: POSIX',
+        'Programming Language :: Python',
+        "License :: OSI Approved :: MIT License",
+    ],
+    platforms = 'any',
+    keywords = 'monad parser combinator',
+
+    package_dir = {'': 'src'},
+    packages = find_packages('src'),
+
+    test_suite = 'tests',
+)
+

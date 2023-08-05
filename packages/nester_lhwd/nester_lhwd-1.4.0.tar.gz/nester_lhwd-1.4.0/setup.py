b40ed5c9f932ade9dfba0abeee2b437b8a4e1bfc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:57:09 +0000

    Add package nester_lhwd/1.4.0/nester_lhwd-1.4.0.tar.gz

---
+++ packages/nester_lhwd/nester_lhwd-1.4.0.tar.gz/nester_lhwd-1.4.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name= 'nester_lhwd',
+    version= '1.4.0',
+    py_modules = ['nester'],
+    author= 'lihang',
+    author_email= '418045274@qq.com',
+    url = 'http://www.cutelhwd.cn',
+    description= 'A simple printer of nested lists',
+)

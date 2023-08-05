9425c9f8863864f5a9bd11fd1110e1c2cfe055e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:08:48 +0000

    Add package xmatch/0.1.0/xmatch-0.1.0.tar.gz

---
+++ packages/xmatch/xmatch-0.1.0.tar.gz/xmatch-0.1.0/xmatch/__init__.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Copyright (c) 2016 IAS / CNRS / Univ. Paris-Sud
+# LGPL License - see attached LICENSE file
+# Author: Alexandre Beelen <alexandre.beelen@ias.u-psud.fr>
+
+from .xmatch import *
+
+r"""
+ __   ____  __       _       _
+ \ \ / /  \/  |     | |     | |
+  \ V /| \  / | __ _| |_ ___| |__
+   > < | |\/| |/ _` | __/ __| '_ \
+  / . \| |  | | (_| | || (__| | | |
+ /_/ \_\_|  |_|\__,_|\__\___|_| |_|
+===================================
+
+Cross Match
+"""
+
+__version__ = '0.1.0'

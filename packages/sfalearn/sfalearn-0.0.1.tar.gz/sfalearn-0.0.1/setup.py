236899523db023281dc9c7ed5196baac6b4df68e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:09 +0000

    Add package sfalearn/0.0.1/sfalearn-0.0.1.tar.gz

---
+++ packages/sfalearn/sfalearn-0.0.1.tar.gz/sfalearn-0.0.1/setup.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+import ez_setup
+ez_setup.use_setuptools()
+
+PROJECT = 'sfalearn'
+
+# Change docs/sphinx/conf.py too!
+VERSION = '0.0.1'
+
+from setuptools import setup, find_packages
+
+try:
+    long_description = open('README.md', 'rt').read()
+except IOError:
+    long_description = ''
+
+setup(
+    name=PROJECT,
+    version=VERSION,
+
+    description='Automata Learning',
+    long_description=long_description,
+
+    author='George Argyros, Ioannis Stais',
+    author_email='ioannis.stais@gmail.com',
+
+    url='https://github.com/GeorgeArgyros/sfalearn',
+    download_url='https://github.com/GeorgeArgyros/sfalearn/tarball/master',
+
+    classifiers=['Development Status :: 3 - Alpha',
+                 'Programming Language :: Python',
+                 'Programming Language :: Python :: 2',
+                 'Programming Language :: Python :: 2.7',
+                 'Intended Audience :: Developers',
+                 'Environment :: Console',
+                 ],
+
+    platforms=['Any'],
+    scripts=[],
+    provides=[],
+    install_requires=['symautomata'],
+    dependency_links=[
+        "git+https://github.com/GeorgeArgyros/symautomata#egg=symautomata-0.0.1"
+    ],
+    namespace_packages=[],
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+)

97cf7cb1bfd7ce5f1ea1ddfe472604b8ace5cbbf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:28 +0000

    Add package knuverse-cli/1.0.2/knuverse_cli-1.0.2-py2.py3-none-any.whl

---
+++ packages/knuverse-cli/knuverse_cli-1.0.2-py2.py3-none-any.whl/cli/__main__.py
@@ -0,0 +1,102 @@
+#!/usr/bin/env python
+
+import sys
+from inspect import classify_class_attrs
+from inspect import getdoc
+from knuverse.knufactor import HttpErrorException
+from cli.knu import verification
+from cli.lib.account import login
+from cli.lib.account import configure
+
+
+class InvalidCommandException(Exception):
+    """
+    Invalid command exception class
+    """
+
+
+def help_short():
+    help_text = """Usage: knuverse [--help] <command> [args]
+
+Commands:"""
+    print(help_text)
+
+    for f in classify_class_attrs(Knuverse):
+        if f.name.startswith("__"):
+           continue
+        docstring = f.object.__doc__
+        print("   {:15}{description}".format(f.name, description=next(s.lstrip() for s in docstring.split('\n') if s)))
+
+
+class Knuverse(object):
+
+    def __init__(self):
+        pass
+
+    def configure(self, args):
+        """
+        Configure Knuverse account credentials
+        Usage: knuverse configure
+
+        """
+        if "--help" in args:
+            raise InvalidCommandException
+        configure()
+
+    def verify(self, args):
+        """
+        Verify a user with either AudioPIN or AudioPass
+        Usage: knuverse verify [options] <NAME>
+
+        Options:
+          --audiopin Specifies to do an AudioPIN verification
+
+          --audiopass Specifies to do an AudioPass verification
+
+        """
+        if len(args) < 1 or "--help" in args:
+            raise InvalidCommandException
+        else:
+            name = args[0]
+            mode = None
+            if "--audiopin" in args:
+                mode = "audiopin"
+            elif "--audiopass" in args:
+                mode = "audiopass"
+            api = login()
+            verification(api, client_name=name, mode=mode)
+
+
+def _main():
+
+    if len(sys.argv) < 2:
+        help_short()
+    else:
+        kv = Knuverse()
+        function_mappings = {
+            'verify': kv.verify,
+            'configure': kv.configure
+        }
+        cmd = sys.argv[1]
+        f = function_mappings.get(cmd)
+        if f:
+            try:
+                f(sys.argv[2:])
+            except InvalidCommandException:
+                print(getdoc(f))
+        else:
+            help_short()
+
+
+def main():
+    try:
+        _main()
+    except HttpErrorException as e:
+        print("Server error: %s." % str(e))
+        sys.exit(1)
+    except KeyboardInterrupt:
+        print("Exiting.")
+        sys.exit(0)
+
+if __name__ == "__main__":
+    main()
---
+++ packages/knuverse-cli/knuverse_cli-1.0.2-py2.py3-none-any.whl/cli/knu.py
@@ -0,0 +1,136 @@
+from __future__ import print_function
+import sys
+import threading
+import curses
+from time import sleep
+from datetime import datetime
+
+import colorama
+from colorama import Fore
+colorama.init(autoreset=True)
+
+from cli.lib.recorder import record_to_file
+
+
+def _draw_pinpad(screen, pin_mapping):
+
+    cell_size = 15
+    pinpad_width = cell_size * 3 + 4  # 3 cells plus 4 cell dividers
+    pinpad_height = 20
+
+    y, x = screen.getmaxyx()
+    y = (y - pinpad_height) // 2
+    x = (x - pinpad_width) // 2
+
+    def draw_line(y, x):
+        screen.addstr(y, x, "-" * pinpad_width)
+        y += 1
+        return y, x
+
+    def draw_separator(y, x, separator=("", "", "")):
+        screen.addstr(y, x, "|%s|%s|%s|" % tuple([s.center(cell_size) for s in separator]))
+        y += 1
+        return y, x
+
+    def draw_words(y, x, separator):
+        screen.addstr(y, x, "|%s|%s|%s|" % tuple([pin_mapping.get(i).center(cell_size) if i != " " else " ".center(cell_size) for i in separator]))
+        y += 1
+        return y, x
+
+    pinpad = [
+        ["1", "2", "3"],
+        ["4", "5", "6"],
+        ["7", "8", "9"],
+        [" ", "0", " "]
+    ]
+
+    for row in pinpad:
+        y, x = draw_line(y, x)
+        y, x = draw_separator(y, x, separator=row)
+        y, x = draw_separator(y, x)
+        y, x = draw_words(y, x, row)
+        y, x = draw_separator(y, x)
+
+    draw_line(y, x)
+    screen.refresh()
+
+
+def _draw_word(screen, display):
+
+    y, x = screen.getmaxyx()
+    y //= 2
+    x //= 2
+
+    screen.addstr(y, x, display)
+    screen.refresh()
+
+
+def verification(api, client_name, mode=None):
+
+    client = api.get_client_info(client_name)
+
+    # Make sure client is enrolled
+    if client.get("state") != "enrolled":
+        print("%s must enroll before verifying." % client_name)
+        return
+
+    ver = api.start_verification(client.get("id"), mode=mode)
+    ver_id = ver.get("verification_id")
+
+    # Get length of verification. We need this to know how long to record
+    ver_length = 0
+    for frame in ver.get("animation"):
+        ver_length += frame.get("duration")
+    audio_length = float(ver_length) / 1000 + 1
+
+    # Begin the recorder in a separate thread
+    t = threading.Thread(target=record_to_file, args=("/tmp/verification.wav", audio_length))
+    t.daemon = True
+    t.start()
+
+    # TODO Initial pause
+    # Display the animation
+    screen = curses.initscr()
+    curses.noecho()
+    curses.cbreak()
+    try:
+        word_boundaries = []
+        animation_start = datetime.now()
+        for frame in ver.get("animation"):
+            if frame.get("is_word_boundary"):
+                word_boundaries.append((datetime.now() - animation_start).total_seconds() * 1000)
+            if frame.get("pinpad_display"):
+                _draw_pinpad(screen, frame.get("pinpad_display"))
+            else:
+                _draw_word(screen, frame.get("display"))
+            screen.erase()
+            sleep(float(frame.get("duration")) / 1000)
+    except curses.error:
+        print("Terminal window size is too small.")
+        sys.exit(1)
+    finally:
+        curses.echo()
+        curses.nocbreak()
+        curses.endwin()
+        screen.erase()
+    # Wait for the recorder thread to finish
+    t.join()
+
+    # Upload verification audio
+    api.upload_verification_resource(
+        ver_id,
+        word_boundaries,
+        audio_file="/tmp/verification.wav",
+    )
+
+    # Display the verification results
+    print("Processing...", end="")
+    ver = api.get_verification_resource(ver_id)
+    while ver.get("state") != "completed":
+        sleep(1)
+        ver = api.get_verification_resource(ver_id)
+    if ver.get("verified"):
+        print(Fore.GREEN + "Verified")
+    else:
+        print(Fore.RED + "Rejected")
+    return

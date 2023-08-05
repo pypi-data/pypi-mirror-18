34363872a4441829d7dee99ffee63a0349d2c8ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:10 +0000

    Add package gmplnotebook/0.1.1/gmplnotebook-0.1.1.tar.gz

---
+++ packages/gmplnotebook/gmplnotebook-0.1.1.tar.gz/gmplnotebook-0.1.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name='gmplnotebook',
+    version='0.1.1',
+    description='A GMPL/MathProg kernel+extension for Jupyter',
+    url='https://github.com/nelsonuhan/gmplnotebook',
+    author='Nelson Uhan',
+    author_email='nelson@uhan.me',
+    license='GNU GPLv3',
+    packages=['gmplnotebook'],
+    package_data={
+        '': ['kernel.js'],
+    },
+    install_requires=['glpk>=0.4.2', 'metakernel', 'jupyter'],
+)

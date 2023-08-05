92e62fb6b3bd69dc4d99fbf497852ea7e35ee95f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:38 +0000

    Add package social_ids/1.0.1/social_ids-1.0.1.tar.gz

---
+++ packages/social_ids/social_ids-1.0.1.tar.gz/social_ids-1.0.1/social_ids/networks/instagram.py
@@ -0,0 +1,21 @@
+import re
+import requests
+
+BASE_URL = 'https://www.instagram.com/{handler}'
+PATTERN_PUBLIC = '\"owner\"\: \{\"id\"\: \"(\d+?)\"\}'
+# Instagram private pages are not gonna show any post, so you can just search for "id": "
+PATTERN_PRIVATE = '\"id\": \"(\d+?)\"'
+
+def get_id(handler):
+    """Returns Instagram user ID for the given handler
+    """
+    req = requests.get(BASE_URL.format(handler=handler))
+
+    try:
+        return re.search(PATTERN_PUBLIC, req.text).group(1)
+    except AttributeError:
+        # Try private account pattern
+        try:
+            return re.search(PATTERN_PRIVATE, req.text).group(1)
+        except AttributeError:
+            return None

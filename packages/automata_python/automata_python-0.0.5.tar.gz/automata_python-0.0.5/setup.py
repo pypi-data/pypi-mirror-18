fe268a2f08588aea1ae066d0e6a263b7fcb7742c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:00 +0000

    Add package automata_python/0.0.5/automata_python-0.0.5.tar.gz

---
+++ packages/automata_python/automata_python-0.0.5.tar.gz/automata_python-0.0.5/setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+"""Setup logic for pip."""
+
+from setuptools import setup
+
+setup(
+    name='automata_python',
+    version='0.0.5',
+    description='Python Automata Library',
+    url='https://github.com/hemangsk/automata-python',
+    author='Hemang Kumar',
+    author_email='hemangsk@gmail.com',
+    license='GPLv3',
+    keywords='language finite automata turing machine push down automata linear bound automata',
+    packages=[
+        'automata',
+        'automata.fa',
+        'automata.data_structures'
+
+    ],
+    install_requires=[],
+    entry_points={}
+)

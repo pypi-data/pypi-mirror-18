c84c9bd215ad055bcae1a4027401aaccbd07686f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:55:57 +0000

    Add package djrtime/0.1.0a2/djrtime-0.1.0a2.tar.gz

---
+++ packages/djrtime/djrtime-0.1.0a2.tar.gz/djrtime-0.1.0a2/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='djrtime',
+    version='0.1.0a2',
+    packages=['djrtime', 'djrtime.conf'],
+    url='',
+    license='',
+    author='Vinay Karanam',
+    author_email='',
+    description='',
+    install_requires=['python-snappy==0.5', 'rtime==0.1.0a2'],
+)

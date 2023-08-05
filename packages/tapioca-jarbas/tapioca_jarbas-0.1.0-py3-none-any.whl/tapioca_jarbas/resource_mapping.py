d1c31a668f88a4d68eaf77492fce0b055e2f76fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:58:19 +0000

    Add package tapioca-jarbas/0.1.0/tapioca_jarbas-0.1.0-py3-none-any.whl

---
+++ packages/tapioca-jarbas/tapioca_jarbas-0.1.0-py3-none-any.whl/tapioca_jarbas/resource_mapping.py
@@ -0,0 +1,14 @@
+RESOURCE_MAPPING = {
+    'documents': {
+        'resource': 'document/',
+        'docs': 'https://github.com/datasciencebr/jarbas#get-apidocument'
+    },
+    'receipt': {
+        'resource': 'receipt/{document_pk}',
+        'docs': 'https://github.com/datasciencebr/jarbas#get-apireceiptdocumentpk'
+    },
+    'supplier': {
+        'resource': 'supplier/{cnpj}',
+        'docs': 'https://github.com/datasciencebr/jarbas#get-apisuppliersuppliercnpj'
+    },
+}

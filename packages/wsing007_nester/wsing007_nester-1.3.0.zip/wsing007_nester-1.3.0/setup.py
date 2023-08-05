5daec28a6590516172802483e19db0e1013bf6c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:36 +0000

    Add package wsing007_nester/1.3.0/wsing007_nester-1.3.0.zip

---
+++ packages/wsing007_nester/wsing007_nester-1.3.0.zip/wsing007_nester-1.3.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name = 'wsing007_nester',
+    version = '1.3.0',
+    py_modules = ['wsing007_nester'],
+    author = 'wsing007',
+    author_email = 'wsing007@163.com',
+    #url = 'www.baidu.com',
+    description = 'wsing007 test',
+    )
---
+++ packages/wsing007_nester/wsing007_nester-1.3.0.zip/wsing007_nester-1.3.0/wsing007_nester.py
@@ -0,0 +1,11 @@
+"""递归打印列表"""
+def print_lol(the_list,indent=False,level=0):
+#如果item为列表，就递归其中的item。
+        for each_item in the_list:
+            if isinstance(each_item,list):
+                print_lol(each_item,indent,level+1)
+            else:
+                if indent:
+                    for tab_stop in range(level):
+                        print("\t",end='')
+                print(each_item)

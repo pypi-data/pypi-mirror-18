90436c53af20b4c05041d507ca4448054820623c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:23 +0000

    Add package nester_pat/1.0.0/nester_pat-1.0.0.tar.gz

---
+++ packages/nester_pat/nester_pat-1.0.0.tar.gz/nester_pat-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+	name			= 'nester_pat',
+	version			= '1.0.0',
+	py_modules		= ['nester_pat'],
+	author			= 'Patrick',
+	author_email	= 'patrick_d@foxmail.com',
+	url				= 'http://www.jimihua.net',
+	description 	= 'A simple printer of nested lists',
+)

66866e8d8575c71cde7ec42720853f48f8cbd496
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:44 +0000

    Add package web3/3.2.0/web3-3.2.0.tar.gz

---
+++ packages/web3/web3-3.2.0.tar.gz/web3-3.2.0/web3/__init__.py
@@ -0,0 +1,23 @@
+from __future__ import absolute_import
+
+import pkg_resources
+
+
+from web3.main import Web3
+from web3.providers.rpc import (
+    RPCProvider,
+    KeepAliveRPCProvider,
+    TestRPCProvider,
+)
+from web3.providers.ipc import IPCProvider
+
+__version__ = pkg_resources.get_distribution("web3").version
+
+__all__ = [
+    "__version__",
+    "Web3",
+    "RPCProvider",
+    "TestRPCProvider",
+    "KeepAliveRPCProvider",
+    "IPCProvider",
+]

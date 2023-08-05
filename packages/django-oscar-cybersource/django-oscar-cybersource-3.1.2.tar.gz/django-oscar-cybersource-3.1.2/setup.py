392132623000c34ab5eda61116b62d2ddebc0e88
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:10:18 +0000

    Add package django-oscar-cybersource/3.1.2/django-oscar-cybersource-3.1.2.tar.gz

---
+++ packages/django-oscar-cybersource/django-oscar-cybersource-3.1.2.tar.gz/django-oscar-cybersource-3.1.2/setup.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python
+import codecs
+import os.path
+from setuptools import setup
+from versiontag import get_version, cache_git_tag
+
+
+packages = [
+    'cybersource',
+    'cybersource.migrations',
+    'cybersource.tests',
+]
+
+setup_requires = [
+    'versiontag>=1.1.0',
+]
+
+requires = [
+    'python-dateutil>=2.5.3',
+    'Django>=1.9.9',
+    'django-oscar>=1.3',
+    'django-oscar-api>=1.0.9',
+    'django-oscar-api-checkout>=0.2.1',
+    'lxml>=3.6.4',
+]
+
+def fpath(name):
+    return os.path.join(os.path.dirname(__file__), name)
+
+def read(fname):
+    return codecs.open(fpath(fname), encoding='utf-8').read()
+
+cache_git_tag()
+
+setup(
+    name='django-oscar-cybersource',
+    description="Integration between django-oscar and the Cybersource Secure Acceptance.",
+    version=get_version(pypi=True),
+    long_description=open('README.rst').read(),
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Framework :: Django :: 1.8',
+        'Framework :: Django :: 1.9',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: ISC License (ISCL)',
+        'Operating System :: Unix',
+        'Operating System :: MacOS :: MacOS X',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+    author='Craig Weber',
+    author_email='crgwbr@gmail.com',
+    url='https://gitlab.com/thelabnyc/django-oscar-cybersource',
+    license='ISC',
+    packages=packages,
+    install_requires=requires,
+    setup_requires=setup_requires
+)

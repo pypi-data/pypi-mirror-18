900e18f6e667f188542806d4beccf9fbc4dad3b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:56:41 +0000

    Add package railgun-cli/0.1.0.dev0/railgun-cli-0.1.0.dev0.tar.gz

---
+++ packages/railgun-cli/railgun-cli-0.1.0.dev0.tar.gz/railgun-cli-0.1.0.dev0/cli/railgun.py
@@ -0,0 +1,238 @@
+# coding: utf-8
+"""
+    railgun.py
+    ``````````
+
+    railgun commandline tool
+
+    :License: MIT
+    :Copyright: @neo1218
+"""
+
+# built-in
+import platform
+import click
+import os
+import shutil
+import datetime
+from functools import wraps
+# railgun
+from operators import _mkdir_p, _touch_file, _copy_files
+from templates import md
+# logging
+import logging
+from logging import StreamHandler, DEBUG
+logger = logging.getLogger(__name__)
+logger.setLevel(DEBUG)
+logger.addHandler(StreamHandler())
+log_symbol = {
+    "warning": "\033[31m{warning}\033[0m => ",
+    "info": "\033[33m{Info}\033[0m => "
+}
+
+# global path
+_platform = platform.platform().split('-')[0]
+gen_path = site_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+
+
+def run_in_root(f):
+    """
+    make sure all the cli run under site root path
+    """
+    @wraps(f)
+    def decorator(*args, **kwargs):
+        path = os.getcwd()
+        _dot_site_path = os.path.join(path, '.site')
+        if not os.path.isfile(_dot_site_path):
+            logger.warning(log_symbol['warning'] + \
+                " please run the command under site root folder."
+            )
+            exit(1)
+        else:
+            f(*args, **kwargs)
+    return decorator
+
+
+def warning_path_exist(path):
+    """
+    send warning msg if path exist
+    """
+    logger.warning(log_symbol['warning'] + \
+        '\033[32m{path}\033[0m exist.' \
+        .format(path=path)
+    )
+
+
+def start_init_info(path):
+    """
+    start init msg
+    """
+    if os.path.isdir(path):
+        warning_path_exist(path)
+        exit(1)
+    else:
+        logger.info(log_symbol['info'] + \
+            ' start init your static site [on] \033[32m{path}\033[0m.'\
+            .format(path=path)
+        )
+
+
+def finish_init_info():
+    logger.info(log_symbol['info'] + \
+        " finish init your site."
+    )
+
+
+@click.group()
+def cli():
+    pass
+
+
+@click.command()
+@click.option('--dev', is_flag=True)
+@click.argument('site_name')
+def init(dev, site_name):
+    """
+    ⚡️ generate railgun site
+    """
+    site = os.path.join(site_path, 'site')
+    gen = os.path.join(gen_path, 'gen')
+    dst = os.path.join(os.getcwd(), site_name)
+
+    start_init_info(dst)
+    _mkdir_p(dst)
+
+    for site_dir, sub_dirs, filenames in os.walk(site):
+        relative = site_dir.split(site)[1].lstrip(os.path.sep)
+        dst_dir = os.path.join(dst, relative)
+
+        _mkdir_p(dst_dir)
+
+        for filename in filenames:
+            site_file = os.path.join(site_dir, filename)
+            dst_file = os.path.join(dst_dir, filename)
+
+            shutil.copy(site_file, dst_file)
+
+    # add gen
+    dst_gen_path = os.path.join(dst, 'gen')
+    shutil.copytree(gen, dst_gen_path)
+
+    if not dev:
+        themes_path = os.path.join(dst, 'app/themes')
+        os.chdir(dst)
+        # default theme: https://github.com/neo1218/ship-theme-cat
+        os.popen('git clone https://github.com/neo1218/ship-theme-cat.git app/themes/cat')
+        os.popen('railgun upgrade cat')
+
+    finish_init_info()
+
+
+@click.command()
+@click.option('--port', default=5050)
+@run_in_root
+def server(port):
+    """
+    ⚡️ local preview server
+    """
+    logger.info(log_symbol['info'] + \
+        " \033[33m{Railgun => }\033[0m"
+    )
+    os.popen("python2 manage.py runserver --port %d" % port)
+
+
+@click.command()
+@click.argument('file_name')
+@run_in_root
+def new(file_name):
+    """
+    ⚡️ create a new post
+    """
+    current_path = os.getcwd()
+    file_path = os.path.join(current_path, 'app/pages', file_name+'.md')
+    if os.path.exists(file_path):
+        warning_path_exist(file_path)
+    else:
+        new_time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        _touch_file(file_path, md % new_time)
+        logger.info(log_symbol['info'] + \
+            " new a markdown file \033[32m{path}\033[0m".format(path=file_path)
+        )
+
+
+@click.command()
+@run_in_root
+def build():
+    """
+    ⚡️ freezes a railgun site into static files
+    """
+    build_path = os.path.join(os.getcwd(), 'app/build')
+    os.popen('python manage.py build')
+    logger.info(log_symbol['info'] + \
+        " static your site in \033[34m{path}\033[0m".format(path=build_path)
+    )
+
+
+@click.command()
+@run_in_root
+def upload():
+    """
+    ⚡️ deploy railgun site
+    """
+    current_path = os.getcwd()
+    root_path = current_path
+    harbor_folder = os.path.join(root_path, '.harbor')
+    build_folder = os.path.join(root_path, 'app/build')
+
+    if not os.path.exists(harbor_folder):
+        _mkdir_p(harbor_folder)
+        os.chdir(harbor_folder)
+        os.popen('git init')
+        os.chdir(root_path)
+        _copy_files(build_folder, harbor_folder)
+        os.popen('python manage.py first_upload')
+    else:
+        os.chdir(root_path)
+        _copy_files(build_folder, harbor_folder)
+        os.popen('python manage.py other_upload')
+
+    os.chdir(root_path)
+    logger.info(log_symbol['info'] + \
+        ' deployment done!'
+    )
+
+
+@click.command()
+@click.argument('theme_name')
+@run_in_root
+def upgrade(theme_name):
+    """
+    ⚡️ upgrade site theme
+    """
+    root_path = os.getcwd()
+    templates_target_path = os.path.join(root_path, 'app/templates')
+    static_target_path = os.path.join(root_path, 'app/static')
+
+    templates_path = os.path.join(root_path,
+                                  'app/themes/%s/templates' % theme_name)
+    static_path = os.path.join(root_path, 'app/themes/%s/static' % theme_name)
+
+    if os.path.isdir(templates_target_path):
+        shutil.rmtree(templates_target_path)
+    if os.path.isdir(static_target_path):
+        shutil.rmtree(static_target_path)
+    shutil.copytree(templates_path, templates_target_path)
+    shutil.copytree(static_path, static_target_path)
+
+    logger.info(log_symbol['info'] + \
+        " upgrade theme to \033[33m{theme}\033[0m".format(theme=theme_name)
+    )
+
+
+# railgun commands
+cli.add_command(init)
+cli.add_command(server)
+cli.add_command(build)
+cli.add_command(new)
+cli.add_command(upload)
+cli.add_command(upgrade)
---
+++ packages/railgun-cli/railgun-cli-0.1.0.dev0.tar.gz/railgun-cli-0.1.0.dev0/gen/__init__.py
@@ -0,0 +1,197 @@
+#coding: utf-8
+"""
+    gen
+    ````
+
+    static files generator for railgun
+
+    :License: MIT
+    :Copyright: @neo1218
+"""
+
+import os
+import shutil
+import collections
+import werkzeug
+try:
+    from urllib import unquote
+    from urlparse import urlsplit
+except ImportError: # python3 ~
+    from urllib.parse import urlsplit, unquote
+from contextlib import contextmanager
+from threading import Lock
+from flask import url_for
+try:
+    unicode
+except NameError:  # python3 ~
+    unicode = str
+    basestring = str
+
+
+class Gen(object):
+    """
+    static files generator class
+    """
+    def __init__(self, app=None):
+        self.url_gens = []
+        self.init_app(app)
+
+    def init_app(self, app):
+        self.app = app
+        if app:
+            self.url_fors = UrlForGen(app)  # all url_for ?
+            app.config.setdefault('GEN_OUT_DEST', 'build')
+            app.config.setdefault('GEN_BASE_URL', None)
+
+    def register_url_gens(self, function):
+        """
+        register url_gens function
+        """
+        self.url_gens.append(function)
+        return function
+
+    @property
+    def root_path(self):
+        """build root path"""
+        return os.path.join(
+            unicode(self.app.root_path),
+            unicode(self.app.config['GEN_OUT_DEST'])
+        )
+
+    def gen(self):
+        """
+        generator static files
+        """
+        if os.path.isdir(self.root_path):
+            shutil.rmtree(self.root_path) # 简单粗暴⚡️
+        os.makedirs(self.root_path)
+        built_urls = set()
+        built_endpoints = set()
+        for url, endpoint in self._gen_all_urls():
+            built_endpoints.add(endpoint)
+            if url in built_urls:
+                continue
+            built_urls.add(url)
+            filename = self._build_file(url)
+        return built_urls
+
+    def has_no_empty_params(self, rule):
+        defaults = rule.defaults if rule.defaults is not None else ()
+        arguments = rule.arguments if rule.arguments is not None else ()
+        return len(defaults) >= len(arguments)
+
+    def _gen_all_urls(self):
+        """
+        run all generators and yield (url, endpoint) tuple
+        """
+        base_url_path = self._base_url_path()
+        url_encoding = self.app.url_map.charset  # 'utf-8'
+        url_gens = list(self.url_gens)
+        # add iter_rules function
+        url_gens += [self.app.url_map.iter_rules]
+        # add iter_calls function
+        url_gens += [self.url_fors.iter_calls]
+        with self.app.test_request_context(base_url=base_url_path or None):
+            for gen in url_gens:
+                print url_gens
+                for gend in gen():
+                    if isinstance(gend, werkzeug.routing.Rule):
+                        if self.has_no_empty_params(gend):
+                            url = gend.rule
+                            endpoint = gend.endpoint
+                            values = gend.defaults or {}
+                        else: continue
+                    elif isinstance(gend, basestring):
+                        url = gend
+                        endpoint = None
+                    else:
+                        if isinstance(gend, collections.Mapping):
+                            values = gend
+                            endpoint = gen.__name__
+                        else:
+                            endpoint, values = gend
+                    url = url_for(endpoint, **values)  # 构造url
+                    assert url.startswith(base_url_path), (
+                        'url_for returned an URL %r not starting with '
+                        'base_url_path %r.'
+                        % (url, base_url_path)
+                    )
+                    url = url[len(base_url_path):]
+                    url = unquote(url)
+                    parsed_url = urlsplit(url)
+                    if parsed_url.scheme or parsed_url.netloc:
+                        raise ValueError('External URLs not supported: ' + url)
+                    url = parsed_url.path
+                    if not isinstance(url, unicode):
+                        url = url.decode(url_encoding)
+                    print "yield url, endpoint"
+                    yield url, endpoint
+
+    def _build_file(self, url):
+        """
+        simulation request on WSGI level and
+        write response to static files
+        """
+        gen_client = self.app.test_client()
+        base_url = self.app.config['GEN_BASE_URL']
+        with self.url_fors:
+            # find all app url_for and build url
+            response = gen_client.get(url, follow_redirects=True,
+                                      base_url=base_url)
+        destination_path = self.urlpath_to_filepath(url)
+        filename = os.path.join(self.root_path, *destination_path.split('/'))
+        dirname = os.path.dirname(filename)
+        if not os.path.isdir(dirname):
+            os.makedirs(dirname)
+        static_data = response.data
+        with open(filename, 'wb') as fd:
+            fd.write(static_data)
+        print "build file {file}".format(file=filename)
+        # response.close()
+        return filename
+
+    def urlpath_to_filepath(self, path):
+        """
+        convert a URL path like /admin/ to a file path like admin/index.html
+        """
+        if path.endswith('/'):
+            path += 'index.html'
+        assert path.startswith('/')
+        return path[1:]
+
+    def _base_url_path(self):
+        baseurl = self.app.config['GEN_BASE_URL']
+        return urlsplit(baseurl or '').path.rstrip('/')
+
+
+class UrlForGen(object):
+    """
+    app url_for rules
+    """
+    def __init__(self, app):
+        self.app = app
+        self.calls = collections.deque()
+        self._enabled = False
+        self._lock = Lock()  # 上锁啦, 哈哈, 有意思
+
+        def gens(endpoint, values):
+            """
+            ⚡️ 通过flask url processors获取所有url_for调用信息
+            http://flask.pocoo.org/docs/0.11/patterns/urlprocessors/
+            """
+            if self._enabled:
+                self.calls.append((endpoint, values.copy()))
+        self.app.url_default_functions.setdefault(None, []).insert(0, gens)
+        # {None: [<function gens at xxxx>]}
+
+    def __enter__(self):
+        self._lock.acquire()
+        self._enabled = True
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        self._enabled = False
+        self._lock.release()
+
+    def iter_calls(self):
+        while self.calls:
+            yield self.calls.popleft()

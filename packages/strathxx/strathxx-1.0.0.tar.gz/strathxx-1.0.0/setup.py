b4a0e7182cb7bce2a229bcb24a380adb4dfb5903
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:00 +0000

    Add package strathxx/1.0.0/strathxx-1.0.0.tar.gz

---
+++ packages/strathxx/strathxx-1.0.0.tar.gz/strathxx-1.0.0/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+	name = 'strathxx',
+	version = '1.0.0',
+	py_modules = ['emp'],
+	author = 'Troon',
+	author_email = 'jayombagi@gmail.com',
+	url = 'http://www.prewired.co.ke',
+	description = 'Demo package for @iLabAfrica ISS',
+
+)

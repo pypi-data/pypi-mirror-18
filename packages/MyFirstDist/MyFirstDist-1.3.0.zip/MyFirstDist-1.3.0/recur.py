0389a62384c352fe5f4ed9e4ff924fdc2dd81fbb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:46 +0000

    Add package MyFirstDist/1.3.0/MyFirstDist-1.3.0.zip

---
+++ packages/MyFirstDist/MyFirstDist-1.3.0.zip/MyFirstDist-1.3.0/recur.py
@@ -0,0 +1,16 @@
+''' This is a module called "recur.py".
+It provides one function called printAllArray.'''
+def printAllArray(a_list,indent=False,level=0):
+
+    for each_item in a_list:
+
+        if isinstance(each_item,list):
+            #Recursion is required here....
+            printAllArray(each_item,indent,level+1)
+
+        else:
+            if indent:
+                for each_tab in range(level):
+                    print("\t",end='')
+            print(each_item)
+                

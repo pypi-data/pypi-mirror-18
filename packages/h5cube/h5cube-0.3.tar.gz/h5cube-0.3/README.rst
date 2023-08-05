aa81129a7eb64e6e3c8293a99d712434c41b6b53
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:11 +0000

    Add package h5cube/0.3/h5cube-0.3.tar.gz

---
+++ packages/h5cube/h5cube-0.3.tar.gz/h5cube-0.3/README.rst
@@ -0,0 +1,23 @@
+Gaussian CUBE Compression via h5py
+==================================
+
+Compression/decompression command-line tool and Python package for
+Gaussian CUBE files, exploiting the capabilities of the
+`HDF5 <http://www.h5py.org/>`__ binary format via ``h5py``.
+
+Branches named with the prefix ``v.`` are volatile. The history
+there may be rewritten dramatically, without warning.
+
+Available on `PyPI <https://pypi.python.org/pypi/h5cube>`__
+(``pip install h5cube``).
+
+Source on `GitHub <https://github.com/bskinn/h5cube>`__
+(`v0.3 CHANGELOG <https://github.com/bskinn/h5cube/blob/v0.3/CHANGELOG.txt>`__).
+
+Documentation at Read the Docs:
+
+.. image:: https://readthedocs.org/projects/h5cube/badge/?version=latest
+    :target: http://h5cube.readthedocs.io/en/latest/?badge=latest
+    :alt: Documentation Status
+
+
---
+++ packages/h5cube/h5cube-0.3.tar.gz/h5cube-0.3/setup.py
@@ -0,0 +1,38 @@
+from setuptools import setup
+from os import environ
+
+def readme():
+    with open('README.rst') as f:
+        return f.read()
+
+setup(
+    name='h5cube',
+    version='0.3',
+    provides=['h5cube'],
+    requires=['h5py (>=2.4)', 'numpy (>=1.8)'],
+    packages=['h5cube'],
+    url='https://www.github.com/bskinn/h5cube',
+    license='MIT License',
+    author='Brian Skinn',
+    author_email='bskinn@alum.mit.edu',
+    description='Gaussian CUBE File Compression Utility',
+    long_description=readme(),
+    classifiers=['License :: OSI Approved :: MIT License',
+                 'Natural Language :: English',
+                 'Environment :: Console',
+                 'Intended Audience :: Science/Research',
+                 'Operating System :: OS Independent',
+                 'Programming Language :: Python :: 3 :: Only',
+                 'Programming Language :: Python :: 3.4',
+                 'Programming Language :: Python :: 3.5',
+                 'Topic :: Scientific/Engineering',
+                 'Topic :: System :: Archiving :: Compression',
+                 'Topic :: Utilities',
+                 'Development Status :: 4 - Beta'],
+    zip_safe=bool(environ.get('TOX')),
+    entry_points={
+        'console_scripts': [
+            'h5cube = h5cube.h5cube:script_run'
+                           ]
+                  }
+    )

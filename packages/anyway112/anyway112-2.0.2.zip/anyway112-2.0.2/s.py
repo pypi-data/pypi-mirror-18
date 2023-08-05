b81be33ae6772605632dbd8cfa57a8ee2eeb567f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:09:08 +0000

    Add package anyway112/2.0.2/anyway112-2.0.2.zip

---
+++ packages/anyway112/anyway112-2.0.2.zip/anyway112-2.0.2/s.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+setup(
+    name='anyway112',
+    version='2.0.2',
+    url='https://www.baidu.com',
+    author='TysonWang',
+    author_email='https://www.baidu.com',
+    py_modules=['my'],
+)

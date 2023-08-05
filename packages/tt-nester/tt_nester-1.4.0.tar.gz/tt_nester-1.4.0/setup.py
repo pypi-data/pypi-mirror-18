d4778ef80e982b9943df1241cc3b2e0745a20d8d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:01 +0000

    Add package tt-nester/1.4.0/tt_nester-1.4.0.tar.gz

---
+++ packages/tt-nester/tt_nester-1.4.0.tar.gz/tt_nester-1.4.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name         = 'tt_nester',
+    version      = '1.4.0',
+    author       = 'tongli',
+    py_modules   = ['tt_nester'],
+    author_email = 'tonglisr@sina.com',
+    url          = 'http://www.headfirstlabs.com',
+    description  = 'A simple printer of nested lists',
+)

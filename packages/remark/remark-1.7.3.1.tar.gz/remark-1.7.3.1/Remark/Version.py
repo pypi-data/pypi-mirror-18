073960369ac6f68314066e7c025a9dd1ce6bc17c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:20 +0000

    Add package remark/1.7.3.1/remark-1.7.3.1.tar.gz

---
+++ packages/remark/remark-1.7.3.1.tar.gz/remark-1.7.3.1/Remark/Version.py
@@ -0,0 +1,14 @@
+# Description: Version-specific constants
+# Documentation: implementation.txt
+
+# Remark 1.7.3
+# Copyright (c) 2009 - 2016
+# Kalle Rutanen
+# Distributed under the MIT license (see license.txt).
+
+def remarkVersion():
+    '''
+    Returns the version of Remark as a string.
+    '''
+    return '1.7.3.1'
+

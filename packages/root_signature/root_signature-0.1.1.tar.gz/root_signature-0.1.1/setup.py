d6cd4ae127fbb789e3bc0e60204ed3d185d4c8ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:03 +0000

    Add package root_signature/0.1.1/root_signature-0.1.1.tar.gz

---
+++ packages/root_signature/root_signature-0.1.1.tar.gz/root_signature-0.1.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+setup(name='root_signature',
+      version='0.1.1',
+      description='Extract data from root_files',
+      url='http://github.com/zderev/root_signature',
+      author='Igor Zderev',
+      author_email='qbeat@bk.ru',
+      license='MIT',
+      packages=['root_signature'],
+      zip_safe=False)

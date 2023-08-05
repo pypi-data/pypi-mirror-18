1fe3317ead3a0dfa472842d4abe08d6cffa4c564
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:56:25 +0000

    Add package kinto-attachment/1.0.1/kinto-attachment-1.0.1.tar.gz

---
+++ packages/kinto-attachment/kinto-attachment-1.0.1.tar.gz/kinto-attachment-1.0.1/kinto_attachment/tests/test_plugin_setup.py
@@ -0,0 +1,17 @@
+import unittest
+from kinto_attachment import __version__
+from . import BaseWebTestLocal
+
+
+class HelloViewTest(BaseWebTestLocal, unittest.TestCase):
+    def test_capability_is_exposed(self):
+        resp = self.app.get("/")
+        capabilities = resp.json["capabilities"]
+        self.assertIn("attachments", capabilities)
+        expected = {
+            "version": __version__,
+            "description": "Add file attachments to records",
+            "url": "https://github.com/Kinto/kinto-attachment/",
+            "base_url": "https://files.server.com/root/"
+        }
+        self.assertEqual(expected, capabilities["attachments"])

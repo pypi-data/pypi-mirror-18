3bf1ee9af4be0252e1ae3f9e398869a42cf1eb35
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:15 +0000

    Add package classify-chimeras/0.3/classify_chimeras-0.3.tar.gz

---
+++ packages/classify-chimeras/classify_chimeras-0.3.tar.gz/classify_chimeras-0.3/setup.py
@@ -0,0 +1,12 @@
+"""Setup file for classify_chimeras package."""
+from setuptools import setup
+
+setup(name='classify_chimeras',
+      version='0.3',
+      description='Functions to classify chimera states.',
+      url='http://github.com/fkemeth/classify_chimeras',
+      author='Felix Kemeth',
+      author_email='felix@kemeth.de',
+      license='Creative Commons Attribution 4.0 International',
+      packages=['classify_chimeras'],
+      zip_safe=False)

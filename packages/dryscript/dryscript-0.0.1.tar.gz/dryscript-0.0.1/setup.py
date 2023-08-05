7adaf9716e4da975ef47c778aefea08033f4ad31
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:58:15 +0000

    Add package dryscript/0.0.1/dryscript-0.0.1.tar.gz

---
+++ packages/dryscript/dryscript-0.0.1.tar.gz/dryscript-0.0.1/setup.py
@@ -0,0 +1,19 @@
+from distutils.core import setup
+
+# http://guide.python-distribute.org/creation.html
+# login: python setup.py register
+# update: python setup.py sdist upload
+setup( 
+    name = 'dryscript',
+    version = '0.0.1',
+    # author = '',
+    packages = [
+      'dryscript',
+    ],
+    url = 'http://github.com/dryscript/dryscript',
+    license = 'LICENSE.txt',
+    # description = '',
+    # long_description = '',
+ )
+
+

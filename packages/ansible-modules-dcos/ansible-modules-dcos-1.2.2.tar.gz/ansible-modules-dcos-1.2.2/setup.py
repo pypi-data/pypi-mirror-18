5aab1dd9531a4e6a9ad818a272152e41a56c426c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:52 +0000

    Add package ansible-modules-dcos/1.2.2/ansible-modules-dcos-1.2.2.tar.gz

---
+++ packages/ansible-modules-dcos/ansible-modules-dcos-1.2.2.tar.gz/ansible-modules-dcos-1.2.2/setup.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+from setuptools import setup
+
+files = [
+    "ansible/modules/dcos",
+]
+
+long_description = open('README.rst', 'r').read()
+
+setup(
+    name='ansible-modules-dcos',
+    version='1.2.2',
+    description='DCOS Ansible Modules',
+    long_description=long_description,
+    url='https://github.com/TerryHowe/ansible-modules-dcos',
+    author='Kevin Wood,Terry Howe',
+    author_email='kevin.wood@example.com',
+    license='MIT',
+    packages=files,
+    install_requires = [
+        'ansible>=2.0.0',
+        'dcoscli>=0.4.5',
+        'toml',
+    ],
+)

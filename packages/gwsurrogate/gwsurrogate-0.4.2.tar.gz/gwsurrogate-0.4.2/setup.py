3a7bfa161d30708b394f848ce7e02b509d20ec77
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:08:10 +0000

    Add package gwsurrogate/0.4.2/gwsurrogate-0.4.2.tar.gz

---
+++ packages/gwsurrogate/gwsurrogate-0.4.2.tar.gz/gwsurrogate-0.4.2/setup.py
@@ -0,0 +1,33 @@
+import sys
+
+try:
+  from setuptools import setup
+  setup
+except ImportError:
+  from distutils.core import setup
+  setup
+
+
+setup(name='gwsurrogate',
+      version='0.4.2',
+      author='Scott Field, Chad Galley',
+      author_email='sfield@astro.cornell.edu',
+      packages=['gwsurrogate'],
+      license='MIT',
+      contributors=[
+      # Alphabetical by last name.
+      "Jonathan Blackman"],
+      description='An easy to use interface to gravitational wave surrogate models',
+      # will start new downloads if these are installed in a non-standard location
+      # install_requires=["numpy","matplotlib","scipy"],
+      classifiers=[
+                'Intended Audience :: Other Audience',
+                'Intended Audience :: Science/Research',
+                'Natural Language :: English',
+                'License :: OSI Approved :: MIT License',
+                'Programming Language :: Python',
+                'Topic :: Scientific/Engineering',
+                'Topic :: Scientific/Engineering :: Mathematics',
+                'Topic :: Scientific/Engineering :: Physics',
+      ],
+)

e90fcbd22cf49e97a2ff43dda2feda43b4b2da22
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:24 +0000

    Add package nester_vb/1.3.1/nester_vb-1.3.1.tar.gz

---
+++ packages/nester_vb/nester_vb-1.3.1.tar.gz/nester_vb-1.3.1/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name            = 'nester_vb',
+    version         = '1.3.1',
+    py_modules      = ['nester_vb'],
+    author          = 'Vaughan Butler',
+    author_email    = 'vaughan.butler@gmail.com',
+    url             = 'http://www.winslowsystems.co.uk',
+    description     = 'A simple printer of nested lists',
+    )
+    

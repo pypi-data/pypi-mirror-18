203c994164777bc3de57d7e4237c216d4a5a3000
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:11 +0000

    Add package everypolitician/0.0.7/everypolitician-0.0.7.tar.gz

---
+++ packages/everypolitician/everypolitician-0.0.7.tar.gz/everypolitician-0.0.7/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+setup(
+    name = "everypolitician",
+    version = "0.0.7",
+    packages = find_packages(),
+    author = "Mark Longair",
+    author_email = "mark@mysociety.org",
+    description = "Navigate countries and legislatures from EveryPolitician",
+    license = "AGPL",
+    keywords = "politics data civic-tech",
+    install_requires = [
+        'requests',
+        'six >= 1.9.0',
+        'everypolitician-popolo == 0.0.6',
+    ]
+)

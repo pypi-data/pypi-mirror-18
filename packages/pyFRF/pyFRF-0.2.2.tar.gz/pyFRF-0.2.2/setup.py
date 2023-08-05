49e5f821afc449e3561951ff0a0bab10910e9045
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:19 +0000

    Add package pyFRF/0.2.2/pyFRF-0.2.2.tar.gz

---
+++ packages/pyFRF/pyFRF-0.2.2.tar.gz/pyFRF-0.2.2/setup.py
@@ -0,0 +1,24 @@
+desc = """\
+pyFRF
+======
+
+Frequency response function as used in structural dynamics.
+-----------------------------------------------------------
+This package is part of the www.openmodal.com project.
+
+For a showcase see: https://github.com/openmodal/pyFRF/blob/master/Showcase%20pyFRF.ipynb
+"""
+
+#from distutils.core import setup, Extension
+from setuptools import setup, Extension
+setup(name='pyFRF',
+      version='0.2.2',
+      author='Janko Slavič',
+      author_email='janko.slavic@fs.uni-lj.si',
+      description='Frequency response function as used in structural dynamics.',
+      url='https://github.com/openmodal/pyFRF',
+      py_modules=['pyFRF','fft_tools'],
+      #ext_modules=[Extension('lvm_read', ['data/short.lvm'])],
+      long_description=desc,
+      requires=['numpy']
+      )

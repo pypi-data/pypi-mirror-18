fe6461ab1cb218e395e7876f28c2cb801f7327c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:35 +0000

    Add package limix-util/1.0.0/limix_util-1.0.0.tar.gz

---
+++ packages/limix-util/limix_util-1.0.0.tar.gz/limix_util-1.0.0/limix_util/test/test_report.py
@@ -0,0 +1,9 @@
+import unittest
+
+class TestReport(unittest.TestCase):
+    def test_import(self):
+        from limix_util.report import ProgressBar
+
+
+if __name__ == '__main__':
+    unittest.main()

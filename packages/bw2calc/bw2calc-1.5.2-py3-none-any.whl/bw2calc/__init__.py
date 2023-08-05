89fcdcf77f00de9adaa22e0ae2b62e30885a47d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:26 +0000

    Add package bw2calc/1.5.2/bw2calc-1.5.2-py3-none-any.whl

---
+++ packages/bw2calc/bw2calc-1.5.2-py3-none-any.whl/bw2calc/__init__.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+
+__all__ = [
+    'DenseLCA',
+    'direct_solving_worker',
+    'DirectSolvingMixin',
+    'DirectSolvingMonteCarloLCA',
+    'GraphTraversal',
+    'IndepentLCAMixin',
+    'LCA',
+    'LeastSquaresLCA',
+    'load_calculation_package',
+    'MatrixBuilder',
+    'MonteCarloLCA',
+    'MultiLCA',
+    'MultiMonteCarlo',
+    'ParallelMonteCarlo',
+    'ParameterVectorLCA',
+    'save_calculation_package',
+    'TechnosphereBiosphereMatrixBuilder',
+]
+
+__version__ = (1, 5, 2)
+
+from .lca import LCA
+from .dense_lca import DenseLCA
+from .independent_lca import IndepentLCAMixin
+from .least_squares import LeastSquaresLCA
+from .monte_carlo import (
+    direct_solving_worker,
+    DirectSolvingMixin,
+    DirectSolvingMonteCarloLCA,
+    MonteCarloLCA,
+    MultiMonteCarlo,
+    ParallelMonteCarlo,
+)
+from .multi_lca import MultiLCA
+from .mc_vector import ParameterVectorLCA
+from .graph_traversal import GraphTraversal
+from .matrices import MatrixBuilder, TechnosphereBiosphereMatrixBuilder
+from .utils import save_calculation_package, load_calculation_package

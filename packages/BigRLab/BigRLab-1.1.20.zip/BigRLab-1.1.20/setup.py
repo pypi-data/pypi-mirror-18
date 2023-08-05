47d6e59bdb707c1f83de11d2d01dfc325773f15a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:31 +0000

    Add package BigRLab/1.1.20/BigRLab-1.1.20.zip

---
+++ packages/BigRLab/BigRLab-1.1.20.zip/BigRLab-1.1.20/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+setup(
+      name='BigRLab',
+      version='1.1.20',
+      py_modules=['BigRLab'],
+      author='lipd',
+      author_email='pandeng.li@163.com',
+      description='BigRLab client for users add text_class',
+
+
+      )

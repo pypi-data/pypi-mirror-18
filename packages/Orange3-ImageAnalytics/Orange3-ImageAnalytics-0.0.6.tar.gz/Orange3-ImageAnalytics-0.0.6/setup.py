6f2b553da3a0f9589f96c738347f38c4ad4cf585
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:49 +0000

    Add package Orange3-ImageAnalytics/0.0.6/Orange3-ImageAnalytics-0.0.6.tar.gz

---
+++ packages/Orange3-ImageAnalytics/Orange3-ImageAnalytics-0.0.6.tar.gz/Orange3-ImageAnalytics-0.0.6/setup.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python3
+
+from setuptools import setup, find_packages
+import io
+
+with io.open("README.rst", "r", encoding="utf-8") as f:
+    README = f.read()
+
+NAME = "Orange3-ImageAnalytics"
+VERSION = "0.0.6"
+AUTHOR = 'Bioinformatics Laboratory, FRI UL'
+AUTHOR_EMAIL = 'contact@orange.biolab.si'
+URL = 'http://orange.biolab.si/download'
+DESCRIPTION = "Orange3 add-on for image data mining."
+LONG_DESCRIPTION = README
+LICENSE = "GPL3+"
+CLASSIFIERS = [
+    "Development Status :: 1 - Planning",
+    "Intended Audience :: Education",
+    "Intended Audience :: Science/Research",
+    "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
+    "Programming Language :: Python :: 3 :: Only"
+]
+
+KEYWORDS = [
+    "orange3 add-on",
+]
+
+PACKAGES = find_packages()
+
+PACKAGE_DATA = {
+    "orangecontrib.imageanalytics.widgets": ["icons/*.svg"],
+}
+
+INSTALL_REQUIRES = [
+    "Orange3 >= 3.3.5",
+    "setuptools",
+    "numpy",
+    "pillow",
+    "requests"
+]
+
+ENTRY_POINTS = {
+    "orange.widgets":
+        ("Image Analytics = orangecontrib.imageanalytics.widgets",),
+    "orange3.addon":
+        ("Orange3-Imageanalytics = orangecontrib.imageanalytics",)
+}
+
+NAMESPACE_PACKAGES = ["orangecontrib"]
+
+
+if __name__ == "__main__":
+    setup(
+        name=NAME,
+        version=VERSION,
+        author=AUTHOR,
+        author_email=AUTHOR_EMAIL,
+        url=URL,
+        description=DESCRIPTION,
+        long_description=LONG_DESCRIPTION,
+        license=LICENSE,
+        packages=PACKAGES,
+        package_data=PACKAGE_DATA,
+        keywords=KEYWORDS,
+        classifiers=CLASSIFIERS,
+        install_requires=INSTALL_REQUIRES,
+        namespace_packages=NAMESPACE_PACKAGES,
+        entry_points=ENTRY_POINTS
+    )

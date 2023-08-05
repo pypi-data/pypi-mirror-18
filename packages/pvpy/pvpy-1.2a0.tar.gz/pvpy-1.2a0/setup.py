0fba4a19953e54d8af79888248a1561e99ce89ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:24 +0000

    Add package pvpy/1.2a0/pvpy-1.2a0.tar.gz

---
+++ packages/pvpy/pvpy-1.2a0.tar.gz/pvpy-1.2a0/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup
+
+setup(
+    name='pvpy',
+    version='1.02.alpha',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Scientific/Engineering :: Physics',
+      ],
+    description='Some routines relating photovoltaic solar cells',
+    author='Colton Bukowsky',
+    author_email='cb@caltech.edu',
+    url = 'https://github.com/soamaven/pvpy',
+    download_url = 'https://github.com/soamaven/pvpy/tarball/1.0.alpha',
+    keywords=['solar',
+              'photovoltaics',
+              'shockley',
+              'queisser',
+              'yalonovitch',
+              'short-circuit current',
+              'open-circuit voltage'],
+    license='MIT',
+    packages=[
+        'pvpy',
+    ],
+    install_requires=[
+        'numpy',
+        'scipy'
+    ],
+    include_package_data=True,
+    zip_safe=False
+)

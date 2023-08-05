ab7ee68229e690f5a9ba64afae8645b42b4fc436
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:08:23 +0000

    Add package trepan3k/0.7.2/trepan3k-0.7.2.tar.gz

---
+++ packages/trepan3k/trepan3k-0.7.2.tar.gz/trepan3k-0.7.2/setup.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python3
+from trepan.version import VERSION
+
+copyright   = '''Copyright (C) 2013, 2015-2016 Rocky Bernstein <rocky@gnu.org>.'''
+classifiers =  ['Development Status :: 4 - Beta',
+                'Environment :: Console',
+                'Intended Audience :: Developers',
+                'License :: OSI Approved :: GNU General Public License (GPL)',
+                'Operating System :: OS Independent',
+                'Programming Language :: Python :: 3',
+                'Programming Language :: Python :: 3.3',
+                'Programming Language :: Python :: 3.4',
+                'Programming Language :: Python :: 3.5',
+                'Programming Language :: Python :: 3.6',
+                'Programming Language :: Python',
+                'Topic :: Software Development :: Debuggers',
+                'Topic :: Software Development :: Libraries :: Python Modules',
+                ]
+
+# The rest in alphabetic order
+author             = "Rocky Bernstein"
+author_email       = "rocky@gnu.org"
+ftp_url            = None
+install_requires   = ['columnize >= 0.3.8',
+                      'pyficache >= 0.3.1',
+                      'pygments',
+                      'tracer >= 0.3.2']
+license            = 'GPL'
+mailing_list       = 'python-debugger@googlegroups.com'
+modname            = 'trepan3k'
+py_modules         = None
+short_desc         = 'GDB-like Python3 Debugger in the Trepan family'
+
+import os
+import os.path
+
+web                = 'http://github.com/rocky/python3-trepan/'
+
+# tracebacks in zip files are funky and not debuggable
+zip_safe = False
+
+def read(*rnames):
+    return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
+long_description   = ( read("README.rst") + '\n' )
+
+from setuptools import setup, find_packages
+
+setup(
+       author             = author,
+       author_email       = author_email,
+       classifiers        = classifiers,
+       description        = short_desc,
+       entry_points = {
+       'console_scripts': [
+           'trepan3k  = trepan.cli:main',
+       ]},
+       install_requires   = install_requires,
+       license            = license,
+       long_description   = long_description,
+       packages           = find_packages(),
+       py_modules         = py_modules,
+       name               = modname,
+       test_suite         = 'nose.collector',
+       url                = web,
+       setup_requires     = ['nose>=1.0'],
+       version            = VERSION,
+       zip_safe           = zip_safe)

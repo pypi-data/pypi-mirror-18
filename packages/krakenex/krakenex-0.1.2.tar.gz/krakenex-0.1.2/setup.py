3adbcedfcaef7668d7a93769116e0d5c8ed683ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:58:13 +0000

    Add package krakenex/0.1.2/krakenex-0.1.2.tar.gz

---
+++ packages/krakenex/krakenex-0.1.2.tar.gz/krakenex-0.1.2/setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+from distutils.core import setup
+
+setup(name='krakenex',
+      version='0.1.2',
+      description='kraken.com cryptocurrency exchange API',
+      author='Noel Maersk',
+      author_email='veox plussign spam atsign veox dotsign pw',
+      url='https://github.com/veox/python3-krakenex',
+      packages=['krakenex'],
+      classifiers=[
+          'Programming Language :: Python :: 3',
+      ],
+)

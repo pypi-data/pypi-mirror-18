173b65f628398ed7ca68dab16a4181fe3ad2daa2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:22 +0000

    Add package espa-api-client/1.0.0.dev2/espa_api_client-1.0.0.dev2-py2.py3-none-any.whl

---
+++ packages/espa-api-client/espa_api_client-1.0.0.dev2-py2.py3-none-any.whl/espa_api_client/__init__.py
@@ -0,0 +1,13 @@
+from .Clients import *
+from .Downloaders import *
+from .Order import *
+from .OrderTemplate import *
+from .parse import *
+
+import os
+
+LOCAL_PATH = os.path.dirname(os.path.realpath(__file__))
+TEMPLATE_DIR = os.path.join(LOCAL_PATH, 'templates')
+API_HOST_URL = 'https://espa.cr.usgs.gov'
+API_VERSION = 'v0'
+HEADERS = {'Content-Type': 'application/json'}

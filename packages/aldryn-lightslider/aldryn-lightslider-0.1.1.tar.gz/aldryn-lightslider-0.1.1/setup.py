84b6975876e591af38adee9621275e6bd2bda4db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:43 +0000

    Add package aldryn-lightslider/0.1.1/aldryn-lightslider-0.1.1.tar.gz

---
+++ packages/aldryn-lightslider/aldryn-lightslider-0.1.1.tar.gz/aldryn-lightslider-0.1.1/setup.py
@@ -0,0 +1,38 @@
+import os
+from setuptools import find_packages, setup
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with open(os.path.join(here, 'README.rst')) as readme:
+    README = readme.read()
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+setup(
+    name='aldryn-lightslider',
+    version='0.1.1',
+    packages=find_packages(),
+    include_package_data=True,
+    license='GNU License', 
+    description='LightSlider aldryn addon and djangocms plugin.',
+    long_description=README,
+    url='https://github.com/ninjabit/aldryn-lightslider',
+    author='Tobia Ghiraldini',
+    author_email='tobia.ghiraldini@ninjabit.com',
+    classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Framework :: Django :: 1.8',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU General Public License (GPL)',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    ],
+)

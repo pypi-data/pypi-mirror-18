37713f1a71f2a89e3467e55eb44cf1fb49965a67
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:57:04 +0000

    Add package piqq/0.0.7/piqq-0.0.7.zip

---
+++ packages/piqq/piqq-0.0.7.zip/piqq-0.0.7/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name='piqq',
+    version='0.0.7',
+    packages=['piqq', 'piqq.exceptions'],
+    url='https://git.hellotan.ru/jar3b/piqq',
+    license='MIT',
+    author='jar3b',
+    author_email='hellotan@live.ru',
+    description='PIQQ - The OOP-based add-on for aioamqp library',
+    install_requires=[
+        'aioamqp==0.8.2'
+    ]
+)

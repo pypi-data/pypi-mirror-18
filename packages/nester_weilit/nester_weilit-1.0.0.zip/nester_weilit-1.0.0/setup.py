53229d0307781d9e7c2ca46cd68ab2da4a032178
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:38 +0000

    Add package nester_weilit/1.0.0/nester_weilit-1.0.0.zip

---
+++ packages/nester_weilit/nester_weilit-1.0.0.zip/nester_weilit-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+	name        = 'nester_weilit',
+	version     = '1.0.0',
+	py_modules  = ['nester'],
+	author      = 'weilit',
+	author_email= '2792572804@qq.com',
+	url         = 'http://user.qzone.qq.com/2792572804',
+	description = 'a simple test',
+	)

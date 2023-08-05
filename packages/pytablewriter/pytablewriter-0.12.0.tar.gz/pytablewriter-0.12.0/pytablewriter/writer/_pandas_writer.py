b274927aefb4be7cf5534806118d7373e009c6e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:47 +0000

    Add package pytablewriter/0.12.0/pytablewriter-0.12.0.tar.gz

---
+++ packages/pytablewriter/pytablewriter-0.12.0.tar.gz/pytablewriter-0.12.0/pytablewriter/writer/_pandas_writer.py
@@ -0,0 +1,95 @@
+# encoding: utf-8
+
+"""
+.. codeauthor:: Tsuyoshi Hombashi <gogogo.vm@gmail.com>
+"""
+
+from __future__ import absolute_import
+from __future__ import unicode_literals
+
+import dataproperty
+
+from .._error import EmptyTableNameError
+from .._error import EmptyHeaderError
+from .._function import str_datetime_converter
+from .._function import dateutil_datetime_converter
+from ._text_writer import SourceCodeTableWriter
+
+
+class PandasDataFrameWriter(SourceCodeTableWriter):
+    """
+    Concrete class of a writer for Pandas DataFrame format.
+
+    :Examples:
+
+        :ref:`example-pandas-dataframe-writer`
+    """
+
+    @property
+    def support_split_write(self):
+        return True
+
+    def __init__(self):
+        super(PandasDataFrameWriter, self).__init__()
+
+        self.table_name = u""
+
+        self.is_write_header = False
+        self._prop_extractor.inf_value = 'numpy.inf'
+        self._prop_extractor.nan_value = 'numpy.nan'
+
+    def write_table(self):
+        """
+        |write_table| with Pandas DataFrame variable definition format.
+
+        :raises pytablewriter.EmptyHeaderError: If the |header_list| is empty.
+
+        .. note::
+
+            - |None| is written as ``None``
+            - |inf| is written as ``numpy.inf``
+            - |nan| is written as ``numpy.nan``
+            - |datetime| instance is determined by |is_datetime_instance_formatting| attribute:
+                - |True|: written by using `dateutil.parser <https://dateutil.readthedocs.io/en/stable/parser.html>`__
+                - |False|: written as |str|
+        """
+
+        self._verify_property()
+
+        if self.is_datetime_instance_formatting:
+            self._prop_extractor.datetime_converter = dateutil_datetime_converter
+        else:
+            self._prop_extractor.datetime_converter = str_datetime_converter
+
+        self.inc_indent_level()
+        super(PandasDataFrameWriter, self).write_table()
+        self.dec_indent_level()
+
+        if any([
+            dataproperty.is_empty_sequence(self.value_matrix),
+            not self.is_write_closing_row,
+        ]):
+            return
+
+        self._write_line("{}.columns = [".format(self.variable_name))
+        self.inc_indent_level()
+        for header in self.header_list:
+            self._write_line('"{}",'.format(header))
+        self.dec_indent_level()
+        self._write_line("]")
+
+    def _get_opening_row_item_list(self):
+        return [self.variable_name + " = pandas.DataFrame(["]
+
+    def _get_closing_row_item_list(self):
+        return "])"
+
+    def _verify_property(self):
+        super(PandasDataFrameWriter, self)._verify_property()
+
+        if dataproperty.is_empty_string(self.table_name):
+            raise EmptyTableNameError()
+
+    def _verify_header(self):
+        if dataproperty.is_empty_sequence(self.header_list):
+            raise EmptyHeaderError()
---
+++ packages/pytablewriter/pytablewriter-0.12.0.tar.gz/pytablewriter-0.12.0/test/test_md_writer.py
@@ -0,0 +1,336 @@
+# encoding: utf-8
+
+"""
+.. codeauthor:: Tsuyoshi Hombashi <gogogo.vm@gmail.com>
+"""
+
+from __future__ import absolute_import
+import collections
+
+import pytablereader as ptr
+import pytablewriter as ptw
+import pytest
+
+from .data import header_list
+from .data import value_matrix
+from .data import value_matrix_with_none
+from .data import mix_header_list
+from .data import mix_value_matrix
+from .data import float_header_list
+from .data import float_value_matrix
+from .data import value_matrix_iter
+
+
+Data = collections.namedtuple(
+    "Data", "table indent header value is_float_formatting expected")
+
+normal_test_data_list = [
+    Data(
+        table="",
+        indent=0,
+        header=header_list,
+        value=value_matrix,
+        is_float_formatting=True,
+        expected=""" a |  b  | c |dd | e  
+--:|----:|---|--:|----
+  1|123.1|a  |1.0|1   
+  2|  2.2|bb |2.2|2.2 
+  3|  3.3|ccc|3.0|cccc
+"""
+    ),
+    Data(
+        table="",
+        indent=0,
+        header=header_list,
+        value=None,
+        is_float_formatting=True,
+        expected=""" a | b | c |dd | e 
+---|---|---|---|---
+"""
+    ),
+    Data(
+        table="",
+        indent=0,
+        header=header_list,
+        value=[
+            ["1", 123.09999999999999, "a", "1",   1],
+            [2, 2.2000000000000002, "bb", "2.2", 2.2000000000000002],
+            [3, 3.2999999999999998, "ccc", "3.2999999999999998",   "cccc"],
+        ],
+        is_float_formatting=True,
+        expected=""" a |  b  | c |dd | e  
+--:|----:|---|--:|----
+  1|123.1|a  |1.0|1   
+  2|  2.2|bb |2.2|2.2 
+  3|  3.3|ccc|3.3|cccc
+"""
+    ),
+    Data(
+        table="tablename",
+        indent=0,
+        header=header_list,
+        value=value_matrix,
+        is_float_formatting=True,
+        expected="""# tablename
+ a |  b  | c |dd | e  
+--:|----:|---|--:|----
+  1|123.1|a  |1.0|1   
+  2|  2.2|bb |2.2|2.2 
+  3|  3.3|ccc|3.0|cccc
+"""
+    ),
+    Data(
+        table="tablename",
+        indent=0,
+        header=header_list,
+        value=value_matrix,
+        is_float_formatting=False,
+        expected="""# tablename
+ a |  b  | c |dd | e  
+--:|----:|---|--:|----
+  1|123.1|a  |  1|1   
+  2|  2.2|bb |2.2|2.2 
+  3|  3.3|ccc|  3|cccc
+"""
+    ),
+    Data(
+        table="tablename",
+        indent=1,
+        header=header_list,
+        value=value_matrix,
+        is_float_formatting=True,
+        expected="""## tablename
+ a |  b  | c |dd | e  
+--:|----:|---|--:|----
+  1|123.1|a  |1.0|1   
+  2|  2.2|bb |2.2|2.2 
+  3|  3.3|ccc|3.0|cccc
+"""
+    ),
+    Data(
+        table="",
+        indent=0,
+        header=header_list,
+        value=value_matrix_with_none,
+        is_float_formatting=True,
+        expected=""" a | b | c |dd | e  
+--:|--:|---|--:|----
+  1|   |a  |1.0|    
+   |2.2|   |2.2|2.2 
+  3|3.3|ccc|   |cccc
+   |   |   |   |    
+"""
+    ),
+    Data(
+        table="",
+        indent=0,
+        header=mix_header_list,
+        value=mix_value_matrix,
+        is_float_formatting=True,
+        expected=""" i | f  | c  | if |ifc|bool |inf|nan|mix_num|          time          
+--:|---:|----|---:|---|-----|---|---|------:|------------------------
+  1|1.10|aa  | 1.0|1  |True |inf|nan|    1.0|2017-01-01 00:00:00     
+  2|2.20|bbb | 2.2|2.2|False|inf|nan|    inf|2017-01-02 03:04:05+0900
+  3|3.33|cccc|-3.0|ccc|True |inf|nan|    nan|2017-01-01 00:00:00     
+"""
+    ),
+    Data(
+        table="",
+        indent=0,
+        header=float_header_list,
+        value=float_value_matrix,
+        is_float_formatting=True,
+        expected=""" a  |  b   |  c  
+---:|-----:|----:
+0.01|  9.12| 0.00
+1.00| 99.12| 0.01
+1.20|999.12| 0.00
+"""
+    ),
+]
+
+exception_test_data_list = [
+    Data(
+        table="",
+        indent=0,
+        header=[],
+        value=[],
+        is_float_formatting=True,
+        expected=ptw.EmptyTableDataError
+    ),
+    Data(
+        table="",
+        indent=0,
+        header=[],
+        value=value_matrix,
+        is_float_formatting=True,
+        expected=ptw.EmptyHeaderError
+    ),
+    Data(
+        table="",
+        indent=0,
+        header=None,
+        value=value_matrix,
+        is_float_formatting=True,
+        expected=ptw.EmptyHeaderError
+    ),
+]
+
+table_writer_class = ptw.MarkdownTableWriter
+
+
+class Test_MarkdownTableWriter_write_new_line:
+
+    def test_normal(self, capsys):
+        writer = table_writer_class()
+        writer.write_null_line()
+
+        out, _err = capsys.readouterr()
+        assert out == "\n"
+
+
+class Test_MarkdownTableWriter_set_table_data:
+
+    def test_normal(self, capsys):
+        writer = table_writer_class()
+
+        tabledata = ptr.TableData(
+            "tmp",
+            ["attr_a", "attr_b", "attr_c"],
+            [
+                ["1", "4",      "a"],
+                ["2", "2.1",    "bb"],
+                ["3", "120.9",  "ccc"],
+            ])
+
+        writer.set_table_data(tabledata)
+
+        assert writer.table_name == "tmp"
+        assert writer.header_list == ["attr_a", "attr_b", "attr_c"]
+        assert writer.value_matrix == [
+            ["1", "4",      "a"],
+            ["2", "2.1",    "bb"],
+            ["3", "120.9",  "ccc"],
+        ]
+
+
+class Test_MarkdownTableWriter_write_table:
+
+    @pytest.mark.parametrize(
+        ["table", "indent", "header", "value",
+            "is_float_formatting", "expected"],
+        [
+            [
+                data.table, data.indent, data.header, data.value,
+                data.is_float_formatting, data.expected
+            ]
+            for data in normal_test_data_list
+        ]
+    )
+    def test_normal(
+            self, capsys, table, indent, header, value,
+            is_float_formatting, expected):
+        writer = table_writer_class()
+        writer.table_name = table
+        writer.set_indent_level(indent)
+        writer.header_list = header
+        writer.value_matrix = value
+        writer.is_float_formatting = is_float_formatting
+        writer.write_table()
+
+        out, _err = capsys.readouterr()
+        assert out == expected
+
+    def test_normal_tabledata(self, capsys):
+        writer = table_writer_class()
+
+        tabledata = ptr.TableData(
+            table_name="loader_mapping",
+            header_list=['Format name', 'Loader'],
+            record_list=[
+                ['csv', 'CsvTableFileLoader'],
+                ['excel', 'ExcelTableFileLoader'],
+                ['html', 'HtmlTableFileLoader'],
+                ['markdown', 'MarkdownTableFileLoader'],
+                ['mediawiki', 'MediaWikiTableFileLoader'],
+                ['json', 'JsonTableFileLoader'],
+            ])
+        writer.set_table_data(tabledata)
+        writer.write_table()
+
+        expected = """# loader_mapping
+Format name|         Loader         
+-----------|------------------------
+csv        |CsvTableFileLoader      
+excel      |ExcelTableFileLoader    
+html       |HtmlTableFileLoader     
+markdown   |MarkdownTableFileLoader 
+mediawiki  |MediaWikiTableFileLoader
+json       |JsonTableFileLoader     
+"""
+        out, _err = capsys.readouterr()
+        assert out == expected
+
+    @pytest.mark.parametrize(
+        ["table", "indent", "header", "value", "expected"],
+        [
+            [data.table, data.indent, data.header, data.value, data.expected]
+            for data in exception_test_data_list
+        ]
+    )
+    def test_exception(self, capsys, table, indent, header, value, expected):
+        writer = table_writer_class()
+        writer.table_name = table
+        writer.set_indent_level(indent)
+        writer.header_list = header
+        writer.value_matrix = value
+
+        with pytest.raises(expected):
+            writer.write_table()
+
+
+class Test_MarkdownTableWriter_write_table_iter:
+
+    @pytest.mark.parametrize(["table", "header", "value", "expected"], [
+        [
+            "tablename",
+            ["ha", "hb", "hc"],
+            value_matrix_iter,
+            """# tablename
+ha |hb |hc 
+--:|--:|--:
+  1|  2|  3
+ 11| 12| 13
+  1|  2|  3
+ 11| 12| 13
+ 101| 102| 103
+1001|1002|1003
+""",
+        ],
+    ])
+    def test_normal(self, capsys, table, header, value, expected):
+        writer = table_writer_class()
+        writer.table_name = table
+        writer.header_list = header
+        writer.value_matrix = value
+        writer.iteration_length = len(value)
+        writer.write_table_iter()
+
+        out, _err = capsys.readouterr()
+        assert out == expected
+
+    @pytest.mark.parametrize(
+        ["table", "header", "value", "expected"],
+        [
+            [data.table, data.header, data.value, data.expected]
+            for data in exception_test_data_list
+        ]
+    )
+    def test_exception(self, capsys, table, header, value, expected):
+        writer = table_writer_class()
+        writer.table_name = table
+        writer.header_list = header
+        writer.value_matrix = value
+
+        with pytest.raises(expected):
+            writer.write_table_iter()

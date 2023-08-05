656f0bac672dd4fdbaccc6d101aee1fe4b8b9fdd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:32 +0000

    Add package moduleloader/0.1.9/moduleloader-0.1.9.tar.gz

---
+++ packages/moduleloader/moduleloader-0.1.9.tar.gz/moduleloader-0.1.9/moduleloader/loader.py
@@ -0,0 +1,24 @@
+import sys
+import inspect
+import os
+
+class RelativePath:
+
+    def __init__(self, relativePath,modules=[]):
+        frame = inspect.stack()[1]
+        path = os.path.join(os.path.join(os.path.dirname(frame[1]),relativePath))
+        if not modules:
+            for root, dirs, files in os.walk(path):
+                print root
+                sys.path.append(root)
+        else:
+            for root, dirs, files in os.walk(path):
+                for file in files:
+                    if file in modules:
+                        if not root in sys.path:
+                            sys.path.append(root)
+                        modules.pop(modules.index(file))
+
+        if modules:
+            for module in modules:
+                print "no process module", module

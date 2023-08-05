733a591a9e54af6a6a016dbed652ac4f3d0b68d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:40 +0000

    Add package mylib_chenzhang_kxd_test/0.1/mylib_chenzhang_kxd_test-0.1.zip

---
+++ packages/mylib_chenzhang_kxd_test/mylib_chenzhang_kxd_test-0.1.zip/mylib_chenzhang_kxd_test-0.1/setup.py
@@ -0,0 +1,12 @@
+# coding:utf8
+from setuptools import setup, find_packages
+setup(
+    name = "mylib_chenzhang_kxd_test",
+    version = "0.1",
+    packages = find_packages(),
+	author='zhangchen',
+    author_email='374201603@qq.com',
+	description='Django Basic Apps',
+	url='http://www.baidu.com',
+
+)

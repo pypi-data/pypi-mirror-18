d398a19073a4581d8bf31443d53db9bd0820db4d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:08 +0000

    Add package ricecooker/0.3.5rc12/ricecooker-0.3.5rc12.tar.gz

---
+++ packages/ricecooker/ricecooker-0.3.5rc12.tar.gz/ricecooker-0.3.5rc12/ricecooker/__main__.py
@@ -0,0 +1,35 @@
+"""Usage: ricecooker uploadchannel [-hvqru] <file_path> [--debug] [[OPTIONS] ...]
+
+Arguments:
+  file_path        Path to file with channel data
+  -u               Update all files (download all files again)
+  --debug          Run ricecooker against debug server (localhost:8000) rather than contentworkshop
+  [OPTIONS]        Extra arguments to add to command line (e.g. key='field')
+
+Options:
+  -h --help
+  -v       verbose mode
+  -q       quiet mode
+  -r       make report
+"""
+
+from ricecooker.commands import uploadchannel
+from ricecooker import config
+from ricecooker.exceptions import InvalidUsageException
+from docopt import docopt
+
+commands = ["uploadchannel"]
+
+if __name__ == '__main__':
+    arguments = docopt(__doc__)
+    domain = config.PRODUCTION_DOMAIN
+    kwargs = {}
+    for arg in arguments['OPTIONS']:
+      try:
+        kwarg = arg.split('=')
+        kwargs.update({kwarg[0].strip(): kwarg[1].strip()})
+      except IndexError:
+        raise InvalidUsageException("Invalid kwarg '{0}' found: Must format as [key]=[value] (no whitespace)".format(arg))
+    if arguments["--debug"]:
+    	domain = config.DEBUG_DOMAIN
+    uploadchannel(arguments["<file_path>"], domain, verbose=arguments["-v"], update=arguments["-u"], **kwargs)

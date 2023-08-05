6ea01de14ca4963c7cbf2b0b5b246e1b1a66a62f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:55 +0000

    Add package klak21_nester/1.3.0/klak21_nester-1.3.0.zip

---
+++ packages/klak21_nester/klak21_nester-1.3.0.zip/klak21_nester-1.3.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+	name = 'klak21_nester',
+	version = '1.3.0',
+	py_modules = ['klak21_nester'],
+	author = 'anand',
+	author_email = 'anandkumaresh@gmail.com',
+	description = 'A simple printer of nested lists',
+)

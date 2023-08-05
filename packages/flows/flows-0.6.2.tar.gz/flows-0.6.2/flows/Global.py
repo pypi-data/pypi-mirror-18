2d9ed7e1b85f77d8aedd119beb37d3886e742de3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:39 +0000

    Add package flows/0.6.2/flows-0.6.2.tar.gz

---
+++ packages/flows/flows-0.6.2.tar.gz/flows-0.6.2/flows/Global.py
@@ -0,0 +1,11 @@
+"""Global.py - singleton and utility for Flows"""
+
+import flows.ConfigManager
+import flows.FlowsLogger
+import flows.ProcessManager
+import flows.MessageDispatcher
+
+CONFIG_MANAGER = flows.ConfigManager.ConfigManager.default_instance()
+LOGGER = flows.FlowsLogger.FlowsLogger.default_instance().get_logger()
+PROCESS_MANAGER = flows.ProcessManager.ProcessManager.default_instance()
+MESSAGE_DISPATCHER = flows.MessageDispatcher.MessageDispatcher.default_instance()

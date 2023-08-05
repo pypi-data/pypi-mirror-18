6aa229cabd7c3dcff68136f647ef4a1904eb2bce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:05 +0000

    Add package pyyeelight/0.0.1-alpha/pyyeelight-0.0.1-alpha.tar.gz

---
+++ packages/pyyeelight/pyyeelight-0.0.1-alpha.tar.gz/pyyeelight-0.0.1-alpha/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+setup(
+  name = 'pyyeelight',
+  packages = ['pyyeelight'], # this must be the same as the name above
+  install_requires = ['json','socket','uuid', 'voluptuous'],
+  version = '0.0.1-alpha',
+  description = 'a simple python3 library for Yeelight Wifi Bulbs',
+  author = 'Hydreliox',
+  author_email = 'hydreliox@gmail.com',
+  url = 'https://github.com/HydrelioxGitHub/pyyeelight', # use the URL to the github repo
+  download_url = 'https://github.com/HydrelioxGitHub/pyyeelight/tarball/0.0.1-alpha',
+  keywords = ['xiaomi', 'bulb', 'yeelight', 'API'], # arbitrary keywords
+  classifiers = [],
+)

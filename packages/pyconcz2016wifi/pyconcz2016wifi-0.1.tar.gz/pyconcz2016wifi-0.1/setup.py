e2fb2f0e323224297fcc8aecd9e6968cee1b5d7d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:50 +0000

    Add package pyconcz2016wifi/0.1/pyconcz2016wifi-0.1.tar.gz

---
+++ packages/pyconcz2016wifi/pyconcz2016wifi-0.1.tar.gz/pyconcz2016wifi-0.1/setup.py
@@ -0,0 +1,18 @@
+from distutils.core import setup
+
+setup(name='pyconcz2016wifi',
+      author='Thomas Levine',
+      author_email='_@thomaslevine.com',
+      description='Connect to the internet at PyConCZ 2016.',
+      url='http://src.thomaslevine.com/pyconcz2016wifi/',
+      install_requires=[
+          'requests>=2.11.1',
+          'horetu>=0.2.7',
+      ],
+      scripts=['pyconcz2016wifi'],
+      classifiers=[
+          'Programming Language :: Python :: 3.5',
+      ],
+      version='0.1',
+      license='AGPL',
+      )

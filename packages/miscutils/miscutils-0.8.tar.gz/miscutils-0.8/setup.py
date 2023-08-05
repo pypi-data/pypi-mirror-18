96d12b9cbed4e14c4625caa88473764ce6d2c6b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:55:49 +0000

    Add package miscutils/0.8/miscutils-0.8.tar.gz

---
+++ packages/miscutils/miscutils-0.8.tar.gz/miscutils-0.8/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name='miscutils',
+    version='0.8',
+    description='Miscellaneous utilities for general use.',
+    author='Grzegorz Krason',
+    author_email='grzegorz@krason.biz',
+    url='https://github.com/gergelyk/pymiscutils',
+    download_url='https://github.com/gergelyk/pymiscutils/tarball/0.8',
+    license='MIT',
+    packages=['miscutils'],
+    keywords = ['iterating', 'head', 'tail', 'listing', 'router', 'proxy', 'prompt', 'shell'],
+    classifiers = [],
+)
+

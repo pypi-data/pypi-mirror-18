4be5ba2db51e74b0de53e9099f1f6b95e5a00b24
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:56:37 +0000

    Add package pysmash/1.2.0/pysmash-1.2.0.tar.gz

---
+++ packages/pysmash/pysmash-1.2.0.tar.gz/pysmash-1.2.0/pysmash/main.py
@@ -0,0 +1,16 @@
+import brackets
+import tournaments
+
+
+def main():
+    # 224997
+    # print(brackets.sets_played_by_player(225024, "DOM"))
+    # print(brackets.sets_played_by_player(224997, "DOM"))
+    print(tournaments.show_player_sets('hidden-bosses-4-0', 'DOM'))
+    # print(tournaments.show('hidden-bosses-4-0'))
+    # print(tournaments.show_sets('hidden-bosses-4-0'))
+    # print(tournaments.show_players('hidden-bosses-4-0'))
+
+if __name__ == "__main__":
+
+    main()

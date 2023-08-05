4e68e4887b59c98f6a8d2cacb33d92bb97501dec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:56:53 +0000

    Add package sisqo/2.0.0/sisqo-2.0.0.tar.gz

---
+++ packages/sisqo/sisqo-2.0.0.tar.gz/sisqo-2.0.0/sisqo/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+########################################################################################################################
+# Copyright © 2016 Alex Forster. All rights reserved.
+# This software is licensed under the 3-Clause ("New") BSD license.
+# See the LICENSE file for details.
+########################################################################################################################
+
+
+from ssh import SSH
+from configuration import Configuration, Line
+

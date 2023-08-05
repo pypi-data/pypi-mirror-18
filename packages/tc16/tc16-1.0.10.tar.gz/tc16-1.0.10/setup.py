43ae149daf753c4e88f310ae116c0b86285f8867
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:53 +0000

    Add package tc16/1.0.10/tc16-1.0.10.tar.gz

---
+++ packages/tc16/tc16-1.0.10.tar.gz/tc16-1.0.10/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+setup(
+    name='tc16',
+    version='1.0.10',
+    author='Tableau',
+    author_email='github@tableau.com',
+    url='https://github.com/tableau/tc16',
+    packages=['tc16'],
+    license='MIT',
+    entry_points={
+      'console_scripts': [
+        'tc16 = tc16:main'
+      ]
+    },
+    description='Make TC16 rad.',
+    package_data={
+        'tc16': ['shared/*.txt']
+    }
+)

3605d2169ba92a5c7248364132ee216af7b28993
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:56 +0000

    Add package seekpath/1.1.0/seekpath-1.1.0.tar.gz

---
+++ packages/seekpath/seekpath-1.1.0.tar.gz/seekpath-1.1.0/seekpath/__init__.py
@@ -0,0 +1,16 @@
+"""
+The seekpath module contains routines to get automatically the
+path in a 3D Brillouin zone to plot band structures.
+
+Author: Giovanni Pizzi, EPFL (2016)
+
+Licence: MIT License, see LICENSE.txt file
+"""
+
+__version__ = "1.1.0"
+__author__ = "Giovanni Pizzi, EPFL"
+__copyright__ = "Copyright (c), 2016, Giovanni Pizzi, EPFL (Theory and Simulation of Materials (THEOS) and National Centre for Computational Design and Discovery of Novel Materials (NCCR MARVEL)), Switzerland."
+__credits__ = ["Yoyo Hinuma"]
+__license__ = "MIT license"
+
+from .getpaths import get_path, get_explicit_k_path

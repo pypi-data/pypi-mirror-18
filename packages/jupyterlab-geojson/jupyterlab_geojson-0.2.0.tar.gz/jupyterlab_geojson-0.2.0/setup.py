201843d4b489f56b844074608da897bb53515c21
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:01 +0000

    Add package jupyterlab-geojson/0.2.0/jupyterlab_geojson-0.2.0.tar.gz

---
+++ packages/jupyterlab-geojson/jupyterlab_geojson-0.2.0.tar.gz/jupyterlab_geojson-0.2.0/setup.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+
+# Copyright (c) Jupyter Development Team.
+# Distributed under the terms of the Modified BSD License.
+
+import os
+import sys
+from distutils.core import setup
+
+
+class NodeModulesMissing(Exception):
+    "raised when node_modules directory is not found"
+    pass
+
+
+if 'develop' in sys.argv or any(a.startswith('bdist') for a in sys.argv):
+    import setuptools
+
+# Ensure that js has been built. This does not guaruntee that the packages
+# are update to date. In the future we might do a more expensive check
+# involving file hashes, but only on sdist and bdist builds.
+if not os.path.exists('node_modules'):
+    raise NodeModulesMissing("Before Python package can be installed or built, "
+                             "JavaScript components must be built using npm. "
+                             "Run the following and then retry: "
+                             "\nnpm install")
+
+setup_args = dict(
+    name                 = 'jupyterlab_geojson',
+    version              = '0.2.0',
+    packages             = ['jupyterlab_geojson'],
+    author               = 'Jason Grout',
+    author_email         = 'jgrout6@bloomberg.net',
+    include_package_data = True,
+    install_requires = [
+        'jupyterlab>=0.7.0',
+    ]
+)
+
+if __name__ == '__main__':
+    setup(**setup_args)

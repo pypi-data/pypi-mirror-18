7363da5783cd3ce9d0b82549a31264045cafd3a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:00 +0000

    Add package cr8/0.8.1/cr8-0.8.1.tar.gz

---
+++ packages/cr8/cr8-0.8.1.tar.gz/cr8-0.8.1/cr8/aio.py
@@ -0,0 +1,65 @@
+
+import functools
+import os
+import asyncio
+try:
+    import uvloop
+    asyncio.set_event_loop_policy(uvloop.EventLoopPolicy())
+except ImportError:
+    pass
+
+from tqdm import tqdm
+tqdm = functools.partial(
+    tqdm,
+    unit=' requests',
+    smoothing=0.1,
+    disable=os.environ.get('CR8_NO_TQDM') == 'True'
+)
+
+
+async def measure(stats, f, *args, **kws):
+    r = await f(*args, **kws)
+    duration = r['duration']
+    stats.measure(duration)
+    return r
+
+
+async def qmap(q, corof, iterable):
+    for i in iterable:
+        task = asyncio.ensure_future(corof(*i))
+        await q.put(task)
+    await q.put(None)
+
+
+async def consume(q, total=None):
+    with tqdm(total=total) as t:
+        while True:
+            task = await q.get()
+            if task is None:
+                break
+            await task
+            t.update(1)
+
+
+async def map(coro, iterable, total=None):
+    for i in tqdm(iterable, total=total):
+        await coro(*i)
+
+
+def run(coro, *args):
+    loop = asyncio.get_event_loop()
+    if args:
+        gen = coro(*args)
+    else:
+        gen = coro()
+    return loop.run_until_complete(gen)
+
+
+def run_many(coro, iterable, concurrency, num_items=None):
+    loop = asyncio.get_event_loop()
+    if concurrency == 1:
+        return loop.run_until_complete(map(coro, iterable, total=num_items))
+    q = asyncio.Queue(maxsize=concurrency)
+    loop.run_until_complete(asyncio.gather(
+        qmap(q, coro, iterable),
+        consume(q, total=num_items)))
---
+++ packages/cr8/cr8-0.8.1.tar.gz/cr8-0.8.1/cr8/run_track.py
@@ -0,0 +1,93 @@
+import argh
+import os
+import toml
+from glob import glob
+from .run_spec import run_spec
+from .run_crate import CrateNode, get_crate
+
+
+class Executor:
+    def __init__(self, track_dir, result_hosts=None, crate_root=None, output_fmt=None, fail_fast=None):
+        self.track_dir = track_dir
+        self.result_hosts = result_hosts
+        self.crate_root = crate_root
+        self.output_fmt = output_fmt
+        self.fail_fast = fail_fast
+
+    def _expand_paths(self, paths):
+        paths = (os.path.join(self.track_dir, path) for path in paths)
+        paths = (os.path.abspath(path) for path in paths)
+        return (p for path in paths for p in glob(path))
+
+    def _run_specs(self, specs, benchmark_host, action=None):
+        specs = self._expand_paths(specs)
+        for spec in specs:
+            print('### Running spec file: ', os.path.basename(spec))
+            try:
+                run_spec(
+                    spec,
+                    benchmark_host,
+                    self.result_hosts,
+                    output_fmt=self.output_fmt,
+                    action=action)
+            except:
+                if self.fail_fast:
+                    raise
+                else:
+                    print('WARNING: Spec file failed due to the following exception:')
+                    import traceback
+                    traceback.print_exc()
+
+    def _execute_specs(self, specs, benchmark_host):
+        if isinstance(specs, list):
+            self._run_specs(specs, benchmark_host)
+        else:
+            fixtures = specs.get('fixtures', [])
+            queries = specs.get('queries', [])
+            full = specs.get('full', [])
+            self._run_specs(fixtures, benchmark_host, action=['setup'])
+            self._run_specs(queries, benchmark_host, action=['queries'])
+            self._run_specs(fixtures, benchmark_host, action=['teardown'])
+            self._run_specs(full, benchmark_host)
+
+    def execute(self, track):
+        configurations = list(self._expand_paths(track['configurations']))
+        versions = track['versions']
+        for version in versions:
+            print('# Version: ', version)
+            for c, configuration in enumerate(configurations):
+                print('## Starting Crate {0}, configuration: {1}'.format(
+                    os.path.basename(version),
+                    os.path.basename(configuration)
+                ))
+                configuration = toml.load(os.path.join(self.track_dir, configuration))
+                crate_dir = get_crate(version, self.crate_root)
+                with CrateNode(crate_dir=crate_dir,
+                               env=configuration.get('env'),
+                               settings=configuration.get('settings')) as node:
+                    node.start()
+                    self._execute_specs(track['specs'], node.http_url)
+
+
+@argh.arg('-r', '--result_hosts', type=str)
+@argh.arg('-of', '--output-fmt', choices=['full', 'short'], default='full')
+@argh.arg('--failfast', action='store_true')
+@argh.wrap_errors([KeyboardInterrupt])
+def run_track(track, result_hosts=None, crate_root=None, output_fmt=None, failfast=False):
+    """Execute a track file"""
+    executor = Executor(
+        track_dir=os.path.dirname(track),
+        result_hosts=result_hosts,
+        crate_root=crate_root,
+        output_fmt=output_fmt,
+        fail_fast=failfast
+    )
+    executor.execute(toml.load(track))
+
+
+def main():
+    argh.dispatch_command(run_track)
+
+
+if __name__ == "__main__":
+    main()

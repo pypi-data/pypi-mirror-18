43911141586e603100145e661be346ed388b9602
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:42 +0000

    Add package cysignals/1.3.1/cysignals-1.3.1.tar.bz2

---
+++ packages/cysignals/cysignals-1.3.1.tar.bz2/cysignals-1.3.1/example/cysignals_example.pyx
@@ -0,0 +1,25 @@
+include "cysignals/signals.pxi"
+include "cysignals/memory.pxi"
+
+from libc.math cimport sin
+
+def sine_sum(double x, long count):
+    cdef double s = 0
+    cdef long i
+    for i in range(count):
+        sig_check()
+        s += sin(i*x)
+    return s
+
+cdef long* safe_range_long(long count) except? NULL:
+    """
+    This function can be safely called within a sig_on block.
+
+    With an ordinary malloc, this is not the case since the internal
+    state of the heap would be messed up if an interrupt happens during
+    malloc().
+    """
+    cdef long* a = <long*>check_allocarray(count, sizeof(long))
+    for i in range(count):
+        a[i] = i
+    return a

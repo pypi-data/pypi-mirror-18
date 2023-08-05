a2d9b706c4170580fc940b628c4c286a6b83f162
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:08:26 +0000

    Add package twitterbymb/0.2.9/twitterbymb-0.2.9.tar.gz

---
+++ packages/twitterbymb/twitterbymb-0.2.9.tar.gz/twitterbymb-0.2.9/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup
+
+with open('README') as f:
+    long_description = ''.join(f.readlines())
+
+setup(
+    name='twitterbymb',
+    version='0.2.9',
+    description='Simple console and web Twitter wall',
+    author='Marek Bertovic',
+    author_email='mb.bertovic@gmail.com',
+    license='Public Domain',
+    url='https://github.com/marekbert/MI-PYP1',
+    packages=['twitterbymb'],
+    package_data={'twitterbymb': ['templates/*.html']},
+    include_package_data=True,
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Topic :: Software Development :: Libraries',
+    ],
+    install_requires=['Flask', 'click>=6','requests>2.10','Jinja2>=2.8'],
+    entry_points={
+        'console_scripts':[
+            'ultimatetwitter=twitterbymb.twitwall:main'
+        ]
+    }
+)

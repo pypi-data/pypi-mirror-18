078e7ffda81509744a20c6a79c2df8a47fb446b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:10:27 +0000

    Add package cinema/0.0.1/cinema-0.0.1.zip

---
+++ packages/cinema/cinema-0.0.1.zip/cinema-0.0.1/setup.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+setup(
+    name='cinema',
+    packages=['cinema'],
+    version='0.0.1',
+    description='This python scripts retrieves the rating of a movie given its title.',
+    author='Meysam Mahfouzi',
+    author_email='maysam.mahfouzi@gmail.com',
+    url='https://github.com/meysammahfouzi/movie-rating',
+    download_url='https://github.com/meysammahfouzi/movie-rating/tarball/0.0.1',
+    license='MIT',
+    keywords=['movie', 'rating', 'imdb', 'film', 'cinema'],
+    classifiers=[],
+)

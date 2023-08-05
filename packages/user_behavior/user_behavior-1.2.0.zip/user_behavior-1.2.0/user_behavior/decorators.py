648ed76f48b763a448c354c7b7b9f731aec8efea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:23:53 +0000

    Add package user_behavior/1.2.0/user_behavior-1.2.0.zip

---
+++ packages/user_behavior/user_behavior-1.2.0.zip/user_behavior-1.2.0/user_behavior/decorators.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import
+
+from functools import wraps
+
+from django.contrib.auth.models import User
+
+from cdportal.user_behavior.serializers import ApiInfoSerializer
+from .services import get_request_data, set_response_data
+
+
+def user_behavior_extracter(func):
+
+    @wraps(func)
+    def func_wrapper(*args, **kwargs):
+        request = args[1]
+        request_data = get_request_data(request)
+
+        serializer = ApiInfoSerializer(data=request_data, partial=True)
+
+        if serializer.is_valid():
+            user = User.objects.get(id=request.user.id)
+            api_info = serializer.save(user=user)
+            request.data['api_info'] = api_info
+
+        queryset = func(*args, **kwargs)
+        request.data.pop('api_info')
+        set_response_data(api_info, queryset)
+
+        return queryset
+
+    return func_wrapper

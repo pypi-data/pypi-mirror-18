c8af1e7a30cfa088eeafc5bffe92eb12af7735a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:31 +0000

    Add package djangocms-text-mediumeditor/0.1.1/djangocms_text_mediumeditor-0.1.1-py3-none-any.whl

---
+++ packages/djangocms-text-mediumeditor/djangocms_text_mediumeditor-0.1.1-py3-none-any.whl/djangocms_text_mediumeditor/cms_plugins.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import unicode_literals
+
+from django.utils.translation import ugettext_lazy as _
+
+from cms.plugin_base import CMSPluginBase
+from cms.plugin_pool import plugin_pool
+
+from .forms import MediumEditorTextForm
+from .models import MediumEditorText
+
+
+class MediumEditorTextPlugin(CMSPluginBase):
+    model = MediumEditorText
+    form = MediumEditorTextForm
+    name = _("Text")
+    render_template = "cms/plugins/mediumeditor_text.html"
+    change_form_template = "cms/plugins/mediumeditor_text_change_form.html"
+
+    def render(self, context, instance, placeholder):
+        context.update({
+            "body": instance.body,
+            "placeholder": placeholder,
+            "object": instance,
+        })
+        return context
+
+
+plugin_pool.register_plugin(MediumEditorTextPlugin)

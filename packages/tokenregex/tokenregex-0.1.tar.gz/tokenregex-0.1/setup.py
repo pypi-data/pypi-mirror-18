8863a02ef4701068cf9364072aad094c60991904
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:54 +0000

    Add package tokenregex/0.1/tokenregex-0.1.tar.gz

---
+++ packages/tokenregex/tokenregex-0.1.tar.gz/tokenregex-0.1/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'tokenregex',
+  packages = ['tokenregex'], # this must be the same as the name above
+  version = '0.1',
+  description = 'NLP at your fingertips',
+  author = 'Ramtin Seraj',
+  author_email = 'mehdizadeh.ramtin@gmail.com',
+  url = 'https://github.com/ramtinms/tokenregex',
+  download_url = 'https://github.com/ramtinms/tokenregex/tarball/0.1',
+  keywords = ['natural language processing', 'nlp', 'regex', 'tokenizer'], # arbitrary keywords
+  classifiers = [],
+)

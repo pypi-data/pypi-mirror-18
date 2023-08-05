73a899d8239d4e89c241fc236b9d56d4e4d01a1e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:54 +0000

    Add package recipe-new-project/0.1/recipe-new-project-0.1.tar.gz

---
+++ packages/recipe-new-project/recipe-new-project-0.1.tar.gz/recipe-new-project-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='recipe-new-project',
+      version='0.1',
+      description='New python project recipe',
+      url='http://github.com/nuidi-dev/recipe',
+      author='ZZI',
+      author_email='hi@nuidi.com',
+      license='MIT',
+      packages=['recipe'],
+      zip_safe=False)

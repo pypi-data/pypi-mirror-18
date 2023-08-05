22553e98be69ff7361726792469af97d0f021cdb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:07 +0000

    Add package cover-rage-client/0.0.9/cover-rage-client-0.0.9.tar.gz

---
+++ packages/cover-rage-client/cover-rage-client-0.0.9.tar.gz/cover-rage-client-0.0.9/cover_rage_client/client.py
@@ -0,0 +1,77 @@
+#!/usr/bin/env python
+
+import sys
+
+import asyncio
+from collections import OrderedDict
+from hashlib import sha1
+import hmac
+import json
+import os
+import subprocess
+
+from aiohttp import ClientSession
+
+from .cover import PythonCoverage
+
+
+async def send_request(
+    req_server_url: str,
+    req_token: str,
+    req_overall_coverage: float,
+    req_uncovered_lines: dict,
+    req_git_commit: str,
+    loop=None
+) -> None:
+    data = OrderedDict([
+        ('git_commit', req_git_commit),
+        ('overall_coverage', req_overall_coverage),
+        ('uncovered_lines', json.dumps(req_uncovered_lines)),
+    ])
+    mac = hmac.new(req_token.encode('utf-8'), msg=json.dumps(data).encode('utf-8'), digestmod=sha1)
+    headers = {'X-CoverRage-Signature': 'sha1={}'.format(mac.hexdigest())}
+    async with ClientSession(loop=loop) as session:
+        async with session.post(req_server_url, headers=headers, data=json.dumps(data)) as response:
+            response_text = await response.text()
+            assert response.status == 200, response_text
+
+
+def main(server_url: str, token: str, git_root: str, coverage_file: str) -> None:
+    assert server_url
+    assert token
+    assert git_root
+    assert coverage_file
+    assert os.path.exists(git_root)
+    assert os.path.isdir(git_root)
+    assert os.path.exists(coverage_file)
+    assert os.path.isfile(coverage_file)
+
+    coverage = PythonCoverage(git_root, coverage_file)
+    assert coverage
+
+    overall_coverage = coverage.line_rate
+    uncovered_lines = coverage.compare_coverage_with_diff()
+    git_commit = subprocess.check_output(['git', 'rev-parse', 'HEAD'], cwd=git_root)
+    assert isinstance(git_commit, bytes)
+    git_commit = git_commit.decode("utf-8").rstrip()
+    assert git_commit
+
+    main_loop = asyncio.get_event_loop()
+    main_loop.run_until_complete(
+        send_request(
+            server_url,
+            token,
+            overall_coverage,
+            uncovered_lines,
+            git_commit
+        )
+    )
+
+
+def __main__():  # noqa
+
+    if len(sys.argv) != 5:
+        print('Usage: {} <cover_rage_server_url> <cover_rage_app_token> </path/to/git/root> </path/to/coverage.xml>'.format(sys.argv[0]))
+        exit(1)
+
+    main(*sys.argv[1:5])
---
+++ packages/cover-rage-client/cover-rage-client-0.0.9.tar.gz/cover-rage-client-0.0.9/setup.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+
+def get_version() -> str:
+    with open('VERSION', 'r') as f:
+        version_str = f.read().strip()
+    assert version_str
+    return version_str
+
+setup(
+    name='cover-rage-client',
+    version=get_version(),
+    description='Utility (client) to check coverage for the last commit / pull request while running tests on CI.',
+    long_description=open('README.rst', 'r').read(),
+    author='Alexander Ryabtsev',
+    author_email='ryabtsev.alexander@gmail.com',
+    url='https://github.com/alexryabtsev/cover_rage_client',
+    packages=['cover_rage_client'],
+    install_requires=open('requirements.txt', 'r').readlines(),
+    package_data={'': ['cover_rage_client/utils/newlines.sh', 'cover_rage_client/utils/showlinenum.awk']},
+    include_package_data=True,
+    entry_points={
+        'console_scripts': ['rage_client=cover_rage_client.client:__main__'],
+    },
+    license='MIT',
+    classifiers=(
+        b'Development Status :: 3 - Alpha',
+        b'Environment :: Console',
+        b'Intended Audience :: Developers',
+        b'License :: OSI Approved :: MIT License',
+        b'Natural Language :: English',
+        b'Operating System :: OS Independent',
+        b'Programming Language :: Python',
+        b'Programming Language :: Python :: 3.5',
+        b'Topic :: Software Development :: Testing',
+    ),
+)

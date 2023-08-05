2719646294b2f4e6f58484ebb376fca99bb834df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:56:45 +0000

    Add package gcharts/0.1.0/gcharts-0.1.0.tar.gz

---
+++ packages/gcharts/gcharts-0.1.0.tar.gz/gcharts-0.1.0/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+
+setup(
+    name = 'gcharts',
+    packages = ['gcharts'],
+    version = '0.1.0',
+    description = 'Python wrapper for Google Charts',
+    author = 'Guillermo Guirao Aguilar',
+    author_email = 'contact@guillermoguiraoaguilar.com',
+    url = 'https://github.com/Funk66/gcharts.git',
+    keywords = ['google', 'charts']
+)

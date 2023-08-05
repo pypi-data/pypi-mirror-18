6cc76bf712cf8222a875d40082cf6f88208f7a13
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:54:41 +0000

    Add package helga-yelling/0.1.2/helga-yelling-0.1.2.tar.gz

---
+++ packages/helga-yelling/helga-yelling-0.1.2.tar.gz/helga-yelling-0.1.2/helga_yelling.py
@@ -0,0 +1,44 @@
+import random
+import re
+
+from helga.db import db
+from helga.plugins import preprocessor
+
+def is_shout(message):
+    """
+    Returns `True` if message is determined to be a loud rambling of a
+    madperson.
+
+    We should verify letters and there should be atleast 2
+    """
+
+    regex = re.compile(r'[^a-zA-Z]')
+    stripped = regex.sub('', message)
+
+    if len(stripped) > 1 and stripped.upper() == regex.sub('', message):
+        return True
+
+    return False
+
+@preprocessor
+def yelling(client, channel, nick, message):
+
+    if is_shout(message):
+
+        count = db.yelling.find({
+            'channel': channel,
+        }).count()
+
+        if count:
+            random_resp = db.yelling.find({
+                'channel': channel
+            })[random.randrange(count)]
+
+            client.msg(channel, random_resp['msg'])
+
+        db.yelling.update_one({
+            'msg': message,
+            'channel': channel,
+        }, { '$set': { 'msg': message } }, upsert=True)
+
+    return (channel, nick, message)

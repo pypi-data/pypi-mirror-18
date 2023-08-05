e3f1f484af4ce91632261640818f2f5196180934
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:45 +0000

    Add package cschin888_nester/1.0.0/cschin888_nester-1.0.0.zip

---
+++ packages/cschin888_nester/cschin888_nester-1.0.0.zip/cschin888_nester-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name = 'cschin888_nester',
+    version = '1.0.0',
+    py_modules = ['nester'],
+    author = 'cschin888',
+    author_email = 'cschin888@gmail.com',
+    url = 'http://jameschin888.blogspot.com',
+    description = 'A simple printer of nested list'
+    )

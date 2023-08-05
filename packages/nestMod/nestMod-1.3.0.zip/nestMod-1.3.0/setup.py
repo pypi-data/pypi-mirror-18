e26b8fd80d0a5446f890ddf00cd6dcd7d9020141
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:58 +0000

    Add package nestMod/1.3.0/nestMod-1.3.0.zip

---
+++ packages/nestMod/nestMod-1.3.0.zip/nestMod-1.3.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name = "nestMod",
+    version = "1.3.0",
+    py_modules = ["nester"],
+    author = "UltimiHQ",
+    author_email = "Ultimi450@gmail.com",
+    url = "",
+    description = "A simple printer of nested lists"
+)

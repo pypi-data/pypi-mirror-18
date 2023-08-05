62e433cd99ac38e3a4e22455c55f6e54dc168b5d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:05:46 +0000

    Add package oss2/2.2.1/oss2-2.2.1.zip

---
+++ packages/oss2/oss2-2.2.1.zip/oss2-2.2.1/tests/test_multipart.py
@@ -0,0 +1,74 @@
+# -*- coding: utf-8 -*-
+
+import unittest
+import oss2
+
+from common import *
+
+
+class TestMultipart(OssTestCase):
+    def test_multipart(self):
+        key = self.random_key()
+        content = random_bytes(128 * 1024)
+
+        parts = []
+        upload_id = self.bucket.init_multipart_upload(key).upload_id
+
+        result = self.bucket.upload_part(key, upload_id, 1, content)
+        parts.append(oss2.models.PartInfo(1, result.etag))
+        self.assertTrue(result.crc is not None)
+
+        self.bucket.complete_multipart_upload(key, upload_id, parts)
+
+        result = self.bucket.get_object(key)
+        self.assertEqual(content, result.read())
+
+    def test_progress(self):
+        stats = {'previous': -1}
+
+        def progress_callback(bytes_consumed, total_bytes):
+            self.assertTrue(bytes_consumed <= total_bytes)
+            self.assertTrue(bytes_consumed > stats['previous'])
+
+            stats['previous'] = bytes_consumed
+
+        key = self.random_key()
+        content = random_bytes(128 * 1024)
+
+        upload_id = self.bucket.init_multipart_upload(key).upload_id
+        self.bucket.upload_part(key, upload_id, 1, content, progress_callback=progress_callback)
+        self.assertEqual(stats['previous'], len(content))
+
+        self.bucket.abort_multipart_upload(key, upload_id)
+
+    def test_upload_part_copy(self):
+        src_object = self.random_key()
+        dst_object = self.random_key()
+
+        content = random_bytes(200 * 1024)
+
+        # 上传源文件
+        self.bucket.put_object(src_object, content)
+
+        # part copy到目标文件
+        parts = []
+        upload_id = self.bucket.init_multipart_upload(dst_object).upload_id
+
+        result = self.bucket.upload_part_copy(self.bucket.bucket_name, src_object,
+                                              (0, 100 * 1024 - 1), dst_object, upload_id, 1)
+        parts.append(oss2.models.PartInfo(1, result.etag))
+
+        result = self.bucket.upload_part_copy(self.bucket.bucket_name, src_object,
+                                              (100*1024, None), dst_object, upload_id, 2)
+        parts.append(oss2.models.PartInfo(2, result.etag))
+
+        self.bucket.complete_multipart_upload(dst_object, upload_id, parts)
+
+        # 验证
+        content_got = self.bucket.get_object(dst_object).read()
+        self.assertEqual(len(content_got), len(content))
+        self.assertEqual(content_got, content)
+
+
+if __name__ == '__main__':
+    unittest.main()

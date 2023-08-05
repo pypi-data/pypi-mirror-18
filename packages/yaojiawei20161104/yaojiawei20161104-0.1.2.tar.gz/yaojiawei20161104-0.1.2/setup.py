c404e72a853f43b7c3954dcdebc2640a2ea66a5e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:55:35 +0000

    Add package yaojiawei20161104/0.1.2/yaojiawei20161104-0.1.2.tar.gz

---
+++ packages/yaojiawei20161104/yaojiawei20161104-0.1.2.tar.gz/yaojiawei20161104-0.1.2/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup, find_packages
+
+setup(name="yaojiawei20161104",version='0.1.2',
+    entry_points="""
+    [console_scripts]
+    yaojiawei=yaojiawei20161104.skels.yaojiawei:hehe
+    """)

5a3c4fc359864aad338a1c96aefbfbd2cc785f6e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:53 +0000

    Add package kinto-amo/0.3.0/kinto_amo-0.3.0-py2.py3-none-any.whl

---
+++ packages/kinto-amo/kinto_amo-0.3.0-py2.py3-none-any.whl/kinto_amo/tests/support.py
@@ -0,0 +1,27 @@
+import os
+import unittest
+
+try:
+    import ConfigParser as configparser
+except ImportError:
+    import configparser
+
+from kinto import main as kinto_main
+from kinto.core.testing import BaseWebTest
+
+HERE = os.path.dirname(os.path.abspath(__file__))
+
+
+class AMOTestCase(BaseWebTest, unittest.TestCase):
+    api_prefix = "v1"
+    entry_point = kinto_main
+    config = 'config.ini'
+
+    def get_app_settings(self, extras=None):
+        ini_path = os.path.join(HERE, self.config)
+        config = configparser.ConfigParser()
+        config.read(ini_path)
+        settings = dict(config.items('app:main'))
+        if extras:
+            settings.update(extras)
+        return settings

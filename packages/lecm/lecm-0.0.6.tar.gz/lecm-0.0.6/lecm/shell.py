81f96d880049a51f8f513be4e591566623d5a9fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:08:33 +0000

    Add package lecm/0.0.6/lecm-0.0.6.tar.gz

---
+++ packages/lecm/lecm-0.0.6.tar.gz/lecm-0.0.6/lecm/shell.py
@@ -0,0 +1,110 @@
+# Copyright 2016 Yanis Guenane <yguenane@redhat.com>
+# Author: Yanis Guenane <yguenane@redhat.com>
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from lecm import certificate
+from lecm import configuration
+from lecm import lists
+from lecm import parser
+from lecm import utils
+
+import logging
+import os
+import sys
+
+
+def should_reload(cert, global_configuration):
+
+    if cert.service_name != global_configuration.get('service_name', 'httpd'):
+        return True
+
+    return False
+
+
+def main():
+
+    options = parser.parse()
+
+    if isinstance(options, int):
+        sys.stderr.write(
+            'USAGE: lecm [--generate,--renew,--list,--list-details]\n'
+        )
+        return 1
+
+    if options.debug:
+        logging.basicConfig(level=logging.DEBUG)
+    else:
+        logging.basicConfig(level=logging.INFO)
+
+    _CHANGE = False
+    _CONF = {}
+    if options.conf:
+        _CONF['file_path'] = options.conf
+
+    global_configuration = configuration.load_configuration(_CONF)
+    certificates = configuration.expand_configuration(global_configuration)
+    certificates = utils.filter_certificates(options.items, certificates)
+
+    if options.list:
+        lists.list(certificates)
+    elif options.list_details:
+        lists.list_details(certificates)
+    else:
+        noop_holder = {}
+        for name, parameters in certificates.items():
+            cert = certificate.Certificate(parameters)
+            if options.generate:
+                if options.noop:
+                    if not os.path.exists('%s/pem/%s.pem' %
+                                          (cert.path, cert.name)):
+                        noop_holder[name] = parameters
+                else:
+                    if not os.path.exists('%s/pem/%s.pem' %
+                                          (cert.path, cert.name)):
+                        cert.generate()
+                        _CHANGE = True
+                        # If the service for a specific certificate is
+                        # different than the 'default' service_name or
+                        # no default is specified then reload the service
+                        # right after certificate generation, else reload
+                        # it just once at the end
+                        if should_reload(cert, global_configuration):
+                            cert.reload_service()
+            elif options.renew:
+                if options.noop:
+                    if cert.days_before_expiry <= cert.remaining_days:
+                        noop_holder[name] = parameters
+                else:
+                    if cert.days_before_expiry <= cert.remaining_days:
+                        cert.renew()
+                        _CHANGE = True
+                        # If the service for a specific certificate is
+                        # different than the 'default' service_name or
+                        # no default is specified then reload the service
+                        # right after certificate generation, else reload
+                        # it just once at the end
+                        if should_reload(cert, global_configuration):
+                            cert.reload_service()
+
+        if not options.noop and _CHANGE:
+            utils.reload_service(
+                global_configuration.get('service_name', 'httpd'),
+                global_configuration.get('service_provider', 'systemd')
+            )
+        if options.noop:
+            lists.list(noop_holder)
+
+
+if __name__ == "__main__":
+    main()
---
+++ packages/lecm/lecm-0.0.6.tar.gz/lecm-0.0.6/lecm/utils.py
@@ -0,0 +1,80 @@
+# Copyright 2016 Yanis Guenane <yguenane@redhat.com>
+# Author: Yanis Guenane <yguenane@redhat.com>
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from prettytable import PrettyTable
+
+import copy
+import logging
+import os
+import platform
+import subprocess
+
+LOG = logging.getLogger(__name__)
+
+
+def output_informations(data):
+
+    x = PrettyTable()
+    for column in data:
+        x.add_column(column[0], column[1])
+    print(x)
+
+
+def filter_certificates(items, certificates):
+
+    certificates_to_return = copy.deepcopy(certificates)
+    if isinstance(items, list):
+        for name in certificates.keys():
+            if name not in items:
+                del certificates_to_return[name]
+
+    return certificates_to_return
+
+
+def enforce_selinux_context(output_directory):
+
+    if platform.dist()[0] in ['fedora', 'centos', 'redhat']:
+        if os.path.exists('/sbin/semanage'):
+            FNULL = open(os.devnull, 'w')
+
+            # Set new selinux so it is persistent over reboot
+            command = 'semanage fcontext -a -t cert_t %s(/.*?)' % (
+                output_directory
+            )
+            p = subprocess.Popen(command.split(), stdout=FNULL,
+                                 stderr=subprocess.STDOUT)
+            p.wait()
+
+            # Ensure file have the right context applied
+            command = 'restorecon -Rv %s' % output_directory
+            p = subprocess.Popen(command.split(), stdout=FNULL,
+                                 stderr=subprocess.STDOUT)
+            p.wait()
+
+
+def reload_service(service_name, service_provider):
+
+    if service_name:
+        if not isinstance(service_name, list):
+            service_name = [service_name]
+
+        for service in service_name:
+            LOG.info('Reloading service specified: %s' % service)
+            if service_provider == 'sysv':
+                command = 'service %s reload' % service
+            else:
+                command = 'systemctl reload %s' % service
+            p = subprocess.Popen(command.split())
+            p.wait()

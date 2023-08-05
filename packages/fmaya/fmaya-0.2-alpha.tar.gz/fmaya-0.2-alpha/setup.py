d5aeabe0eb845a53551b4fa521f95959effdb06e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:27 +0000

    Add package fmaya/0.2-alpha/fmaya-0.2-alpha.tar.gz

---
+++ packages/fmaya/fmaya-0.2-alpha.tar.gz/fmaya-0.2-alpha/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name = 'fmaya',
+    packages = ['fmaya'],
+    version = '0.2-alpha',
+    description = 'Functional expressions for Autodesk Maya.',
+    author = 'Gary Fixler',
+    author_email = 'gfixler@gmail.com',
+    url = 'https://github.com/gfixler/fmaya',
+    download_url = 'https://github.com/gfixler/fmaya/archive/v0.2-alpha.tar.gz',
+    license = 'GNU GPLv3',
+)
+

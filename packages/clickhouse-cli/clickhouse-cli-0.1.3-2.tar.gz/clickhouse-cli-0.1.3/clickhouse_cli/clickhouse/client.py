1f5de0f86e3424d1e3644dab428dedcccf454545
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:00 +0000

    Add package clickhouse-cli/0.1.3/clickhouse-cli-0.1.3-2.tar.gz

---
+++ packages/clickhouse-cli/clickhouse-cli-0.1.3-2.tar.gz/clickhouse-cli-0.1.3/clickhouse_cli/clickhouse/client.py
@@ -0,0 +1,128 @@
+import logging
+import re
+
+import requests
+
+from .definitions import FORMATTABLE_QUERIES
+
+
+logger = logging.getLogger('main')
+
+
+class DBException(Exception):
+    regex = (
+        r'Code: (?P<code>\d+), e\.displayText\(\) = ([\w:]+: )?(?P<text>[\w\W]+),\s+'
+        r'e\.what\(\) = (?P<what>[\w:]+)(,\s+)?'
+        r'(Stack trace:\n\n(?P<stacktrace>[\w\W]*)\n)?'
+    )
+
+    def __init__(self, response, query):
+        self.response = response
+        self.query = query
+        self.error_code = 0
+        self.error = ''
+        self.stacktrace = ''
+
+        try:
+            info = re.search(self.regex, response.text).groupdict()
+            self.error_code = info['code']
+            self.error = info['text']
+            self.stacktrace = info['stacktrace'] or ''
+        except:
+            self.error = self.response.text
+
+    def __str__(self):
+        return 'Query:\n{0}\n\nResponse:\n{1}'.format(self.query, self.response.text)
+
+
+class TimeoutError(Exception):
+    pass
+
+
+class ConnectionError(Exception):
+    pass
+
+
+class Response(object):
+
+    def __init__(self, query, fmt, response='', message=''):
+        self.query = query
+        self.message = message
+        self.format = fmt
+        self.time_elapsed = None
+        self.rows = None
+
+        if isinstance(response, requests.Response):
+            self.data = response.text[:-1]
+            self.time_elapsed = response.elapsed.total_seconds()
+
+            lines = len(self.data.split('\n'))
+
+            if self.data == '' or not lines:
+                self.rows = 0
+            elif fmt in ('TabSeparated', 'CSV'):
+                self.rows = lines
+            elif fmt in ('TabSeparatedWithNames', ):
+                self.rows = lines - 1
+            elif fmt in ('PrettyCompactMonoBlock', 'TabSeparatedWithNamesAndTypes'):
+                self.rows = lines - 2
+
+            if fmt in ('PrettyCompactMonoBlock',) and self.rows >= 10001:
+                self.rows = 10000
+        else:
+            self.data = response
+
+
+class Client(object):
+
+    def __init__(self, url, user='default', password=None, database='default', stacktrace=False):
+        self.url = url
+        self.user = user
+        self.password = password or ''
+        self.database = database
+        self.stacktrace = stacktrace
+
+    def query(self, query, data=None, fmt='PrettyCompactMonoBlock', **kwargs):
+        query = query.strip().rstrip(';').rstrip()
+
+        query_split = query.split()
+
+        if len(query_split) == 0:
+            return Response(query, fmt, message='Empty query.'.format(self.database))
+
+        # A `USE database;` kind of query that we should handle ourselves since sessions aren't supported over HTTP
+        if query_split[0].upper() == 'USE' and len(query_split) == 2:
+            self.database = query_split[1]
+            return Response(query, fmt, message='Changed the current database to {0}.'.format(self.database))
+
+        if query_split[0].upper() in FORMATTABLE_QUERIES and len(query_split) >= 2:
+            if query_split[-2].upper() == 'FORMAT':
+                fmt = query_split[-1]
+            elif query_split[-2].upper() != 'FORMAT':
+                if query_split[0].upper() != 'INSERT':
+                    query = query + ' FORMAT {fmt}'.format(fmt=fmt)
+
+        params = {'query': query}
+
+        if self.database != 'default':
+            params['database'] = self.database
+
+        if self.stacktrace:
+            params['stacktrace'] = 1
+
+        params['enable_http_compression'] = 1
+
+        response = None
+        try:
+            response = requests.post(
+                self.url, data=data, params=params, auth=(self.user, self.password), **kwargs
+            )
+        except requests.exceptions.ConnectTimeout:
+            raise TimeoutError
+        except requests.exceptions.ConnectionError:
+            raise ConnectionError
+
+        if response is not None and response.status_code != 200:
+            raise DBException(response, query=query)
+
+        return Response(query, fmt, response)
---
+++ packages/clickhouse-cli/clickhouse-cli-0.1.3-2.tar.gz/clickhouse-cli-0.1.3/clickhouse_cli/ui/prompt.py
@@ -0,0 +1,54 @@
+import os
+
+from prompt_toolkit.buffer import AcceptAction, Buffer
+from prompt_toolkit.filters import Condition
+from prompt_toolkit.history import FileHistory
+from prompt_toolkit.key_binding.manager import KeyBindingManager
+from prompt_toolkit.token import Token
+
+from clickhouse_cli.clickhouse.definitions import INTERNAL_COMMANDS
+from clickhouse_cli.ui.completer import CHCompleter
+
+
+KeyBinder = KeyBindingManager(enable_abort_and_exit_bindings=True, enable_search=True)
+
+
+class CLIBuffer(Buffer):
+
+    def __init__(self, client, multiline, *args, **kwargs):
+        @Condition
+        def is_multiline():
+            if not multiline:
+                return False
+
+            text = self.document.text
+            return not query_is_finished(text, multiline)
+
+        super(CLIBuffer, self).__init__(
+            *args,
+            completer=CHCompleter(client),
+            history=FileHistory(filename=os.path.expanduser('~/.clickhouse-cli_history')),
+            enable_history_search=True,
+            accept_action=AcceptAction.RETURN_DOCUMENT,
+            is_multiline=is_multiline,
+            **kwargs
+        )
+
+
+def query_is_finished(text, multiline=False):
+    text = text.strip()
+    return (
+        (not multiline and text == '') or
+        text.endswith(';') or
+        text in INTERNAL_COMMANDS
+    )
+
+
+def get_prompt_tokens(cli):
+    return [
+        (Token.Prompt, ' :) '),
+    ]
+
+
+def get_continuation_tokens(cli, width):
+    return [(Token.Prompt, '  ] ')]

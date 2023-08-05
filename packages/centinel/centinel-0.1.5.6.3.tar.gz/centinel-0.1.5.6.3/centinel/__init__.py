f0815a11646d72885f43180d6e9f394787efcbf4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:10:12 +0000

    Add package centinel/0.1.5.6.3/centinel-0.1.5.6.3.tar.gz

---
+++ packages/centinel/centinel-0.1.5.6.3.tar.gz/centinel-0.1.5.6.3/centinel/__init__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+__title__ = 'centinel'
+__version__ = '0.1.5.6.3'
+
+import centinel.backend
+import centinel.client
+import centinel.command
+import centinel.config
+import centinel.cli
+import centinel.daemonize
+import centinel.utils

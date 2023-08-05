d2d826916d7ed6e5ca6d9e5b503ba12c6e70baca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:44 +0000

    Add package alignak-module-nrpe-booster/0.1.0/alignak_module_nrpe_booster-0.1.0.tar.gz

---
+++ packages/alignak-module-nrpe-booster/alignak_module_nrpe_booster-0.1.0.tar.gz/alignak_module_nrpe_booster-0.1.0/version.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+    Alignak - Poller module to boost NRPE checks
+"""
+# Package name
+__pkg_name__ = u"alignak_module_nrpe_booster"
+
+# Module type for PyPI keywords
+# Used for:
+# - PyPI keywords
+__module_types__ = u"checks"
+
+# Application manifest
+__version__ = u"0.1.0"
+__author__ = u"Alignak team"
+__author_email__ = u"frederic.mohier@gmail.com"
+__copyright__ = u"(c) 2015-2016 - %s" % __author__
+__license__ = u"GNU Affero General Public License, version 3"
+__url__ = u"https://github.com/Alignak-monitoring-contrib/alignak-module-ws"
+__description__ = u"Alignak - Receiver module for Alignak Web Services"

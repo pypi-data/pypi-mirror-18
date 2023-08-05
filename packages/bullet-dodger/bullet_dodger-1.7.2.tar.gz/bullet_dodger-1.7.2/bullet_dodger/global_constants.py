4d558b6cdccca2080e22e582ef751c1b2956cac4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:56 +0000

    Add package bullet-dodger/1.7.2/bullet_dodger-1.7.2.tar.gz

---
+++ packages/bullet-dodger/bullet_dodger-1.7.2.tar.gz/bullet_dodger-1.7.2/bullet_dodger/global_constants.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+# Bullet dodger: a game where you must dodge bullets
+# Copyright (C) 2016 Jorge Maldonado Ventura
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+# Resolution
+WIDTH = 800
+HEIGHT = 600
+
+# Colors
+BLACK = (0, 0, 0)
+GREEN = (0, 255, 0)
+DARK_ALLOY_ORANGE = (196, 121, 67)
+RED = (255, 0, 0)
+YELLOW = (255, 255, 12)

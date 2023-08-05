69f32d2873b0643e9baeda8157b317b097929900
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:00 +0000

    Add package my_clock/0.0.5/my_clock-0.0.5.tar.gz

---
+++ packages/my_clock/my_clock-0.0.5.tar.gz/my_clock-0.0.5/setup.py
@@ -0,0 +1,41 @@
+from setuptools import setup
+from my_clock.my_clock import __VERSION__
+
+classifiers = [
+    'Development Status :: 4 - Beta',
+    'Environment :: Console',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.3',
+    'Programming Language :: Python :: 3.4',
+    'Topic :: Utilities',
+]
+
+classifiers = [
+    'Development Status :: 2 - Pre-Alpha',
+    'Environment :: Console',
+    'License :: OSI Approved :: MIT License',
+    'Operating System :: Unix',
+    'Programming Language :: Python',
+    'Programming Language :: Python :: 3',
+    'Topic :: Terminals'
+]
+
+setup(
+    name='my_clock',
+    version=__VERSION__,
+    description='my clock',
+    long_description=open('README.rst').read(),
+    author='Yassu',
+    author_email='mathyassu@gmail.com',
+    url='https://github.com/yassu/MyClock',
+    packages=['my_clock'],
+    install_requires = ['json5'],
+    classifiers=classifiers,
+    entry_points="""
+       [console_scripts]
+       my_clock = my_clock.my_clock:main
+    """,
+)
+

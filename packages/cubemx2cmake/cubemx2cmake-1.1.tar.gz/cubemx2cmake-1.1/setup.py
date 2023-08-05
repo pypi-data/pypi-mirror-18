afdd15833cdaf3a44293daa9b9067c84cc136a2d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:11 +0000

    Add package cubemx2cmake/1.1/cubemx2cmake-1.1.tar.gz

---
+++ packages/cubemx2cmake/cubemx2cmake-1.1.tar.gz/cubemx2cmake-1.1/setup.py
@@ -0,0 +1,31 @@
+""" Package meta """
+
+from setuptools import setup
+
+
+def readme():
+    """ Load the contents of README.rst """
+    with open('README.rst') as readme_file:
+        return readme_file.read()
+
+setup(
+    name='cubemx2cmake',
+    version='1.1',
+    description='Command line tool to generate CMakeLists.txt from STM32CubeMX projects.',
+    long_description=readme(),
+    classifiers=[
+        'Topic :: Software Development :: Code Generators',
+        'Topic :: Utilities'
+    ],
+    keywords='st stm32 cube cubemx stm32cubemx cmake code generator',
+    url='https://github.com/eugene-babichenko/cubemx2cmake',
+    author='Yevhenii Babichenko',
+    author_email='eugene.babichenko@gmail.com',
+    license='MIT',
+    packages=['cubemx2cmake'],
+    zip_safe=False,
+    include_package_data=True,
+    entry_points={
+        'console_scripts': ['cubemx2cmake=cubemx2cmake.command_line:main']
+    }
+)

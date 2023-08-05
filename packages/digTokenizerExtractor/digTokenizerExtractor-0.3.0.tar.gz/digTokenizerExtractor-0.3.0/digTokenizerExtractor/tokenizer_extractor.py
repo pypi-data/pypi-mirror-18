8598db62a4f33a991d8d5d65ddfafa37e750dd95
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:07:53 +0000

    Add package digTokenizerExtractor/0.3.0/digTokenizerExtractor-0.3.0.tar.gz

---
+++ packages/digTokenizerExtractor/digTokenizerExtractor-0.3.0.tar.gz/digTokenizerExtractor-0.3.0/digTokenizerExtractor/tokenizer_extractor.py
@@ -0,0 +1,35 @@
+from digExtractor.extractor import Extractor
+from digCrfTokenizer.crf_tokenizer import CrfTokenizer
+import copy
+
+class TokenizerExtractor(Extractor):
+
+    def __init__(self):
+        tokenizer = CrfTokenizer()
+        tokenizer.setRecognizePunctuation(True)
+        tokenizer.setRecognizeHtmlTags(True)
+        tokenizer.setSkipHtmlEntities(True)
+        tokenizer.setRecognizeHtmlEntities(True)
+        tokenizer.setSkipHtmlTags(True)
+        self.tokenizer = tokenizer
+        self.metadata = {'extractor': 'tokenizer'}
+        self.renamed_input_fields = 'text'
+
+    def get_metadata(self):
+        return copy.copy(self.metadata)
+
+    def set_metadata(self, metadata):
+        self.metadata = metadata
+        return self
+
+    def set_tokenizer(self, tokenizer):
+        self.tokenizer = tokenizer
+        return self
+
+    def extract(self, doc):
+        results = list()
+        results.append(self.tokenizer.tokenize(doc['text'].lower()))
+        return results
+
+    def get_renamed_input_fields(self):
+        return self.renamed_input_fields;

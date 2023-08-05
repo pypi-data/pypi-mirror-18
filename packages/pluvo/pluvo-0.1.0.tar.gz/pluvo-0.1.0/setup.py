39d7063be59e038dca1851633a4cb322e817a7b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:10 +0000

    Add package pluvo/0.1.0/pluvo-0.1.0.tar.gz

---
+++ packages/pluvo/pluvo-0.1.0.tar.gz/pluvo-0.1.0/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+
+setup(
+    name='pluvo',
+    packages=['pluvo'],
+    package_data={},
+    version='0.1.0',
+    description='Python library to access the Pluvo REST API.',
+    author='Daan Porru (Wend)',
+    author_email='daan@wend.nl',
+    license='MIT',
+    url='https://github.com/wendbv/pluv-python',
+    keywords=['REST', 'API', 'Pluvo'],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+    install_requires=["coverage", "flake8==2.6.2", "pytest>=2.7", "pytest-cov",
+                      "pytest-flake8==0.2", "pytest-mock", "requests"],
+)

cc7c2d595c645868bea3a7d3d21d6500bf3a5f4e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:56:42 +0000

    Add package kasami/0.0.4/kasami-0.0.4.tar.gz

---
+++ packages/kasami/kasami-0.0.4.tar.gz/kasami-0.0.4/kasami/tests/test_tree_scorer.py
@@ -0,0 +1,29 @@
+from math import log
+
+from nose.tools import eq_
+
+from .. import tree
+from ..tree_scorer import TreeScorer
+
+
+def test_pcfg():
+    trees = [
+        tree.parse("(S (NN 'foo') (VBZ 'bar')))"),
+        tree.parse("(S (NN 'foo') (VBZ 'baz')))"),
+        tree.parse("(S (NN 'foo') (VP (DET 'a') (VBZ 'baz')))")
+    ]
+
+    foo_ts = TreeScorer.from_tree_bank(trees)
+
+    t = tree.parse("(S (NN 'foo') (VP (DET 'a') (VBZ 'bar')))")
+    eq_(round(foo_ts.score(t), 3),
+        round(log(1/3, 10) + log(1, 10) + log(1, 10) + log(1/3, 10), 3))
+
+    t = tree.parse("(S (NN 'bar') (VP (DET 'a') (VBZ 'bar')))")
+    eq_(round(foo_ts.score(t), 3),
+        round(log(1/3, 10) + log(0.5/3, 10) + log(1, 10) + log(1/3, 10), 3))
+
+    t = tree.parse("(S (NN 'bar') (VP (DET 'every') (VBZ 'foo')))")
+    eq_(round(foo_ts.score(t), 3),
+        round(log(1/3, 10) + log(0.5/3, 10) +
+              log(0.5/1, 10) + log(0.5/3, 10), 3))

cbe6f54351e9184cd57d0403d446eca17367a371
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:23:39 +0000

    Add package tabulator/0.10.1/tabulator-0.10.1.tar.gz

---
+++ packages/tabulator/tabulator-0.10.1.tar.gz/tabulator-0.10.1/tabulator/loaders/stream.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+from __future__ import division
+from __future__ import print_function
+from __future__ import absolute_import
+from __future__ import unicode_literals
+
+import io
+from .. import exceptions
+from .. import helpers
+from . import api
+
+
+# Module API
+
+class StreamLoader(api.Loader):
+    """Loader to load source from file-like byte stream.
+    """
+
+    # Public
+
+    options = []
+
+    def load(self, source, encoding, mode):
+
+        # Raise if in text mode
+        if hasattr(source, 'encoding'):
+            message = 'Only byte streams are supported.'
+            raise exceptions.SourceError(message)
+
+        # Prepare bytes
+        bytes = source
+
+        # Prepare encoding
+        encoding = helpers.detect_encoding(bytes, encoding)
+
+        # Return or raise
+        if mode == 'b':
+            return bytes
+        else:
+            chars = io.TextIOWrapper(bytes, encoding)
+            return chars

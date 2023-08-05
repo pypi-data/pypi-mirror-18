30df05a33e3870cdb6977ad78f0889a94224bfc6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:41 +0000

    Add package nester_HF_test/1.2.0/nester_HF_test-1.2.0.tar.gz

---
+++ packages/nester_HF_test/nester_HF_test-1.2.0.tar.gz/nester_HF_test-1.2.0/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+
+    name            = 'nester_HF_test',
+    version         = '1.2.0',
+    py_modules      = ['nester_HF_test'],
+    author          = 'totb',
+    author_email    = 'tougholdtechbroad@gmail.com',
+    url             = 'http://tougholdtechbroad.wordpress.com',
+    description     = 'A simple printer of nested lists',
+    )

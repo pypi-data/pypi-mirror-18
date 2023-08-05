8fd329775dc9a4a8a10a282f13df942500e5420a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:10 +0000

    Add package crds/7.0.8/crds-7.0.8.tar.gz

---
+++ packages/crds/crds-7.0.8.tar.gz/crds-7.0.8/crds/git_version.py
@@ -0,0 +1,6 @@
+__version__ = '36829fc'
+
+__full_version_info__ = '''
+branch: master
+sha1: 36829fc
+'''
---
+++ packages/crds/crds-7.0.8.tar.gz/crds-7.0.8/setup.py
@@ -0,0 +1,74 @@
+#! /usr/bin/env python
+import sys
+import glob
+
+from distutils.core import setup
+
+setup_pars = {
+    "packages" : [
+        'crds',
+        'crds.client',
+        'crds.hst',
+        'crds.jwst',
+        'crds.submit',
+        'crds.tobs',
+        'crds.tests',
+        ],
+    "package_dir" : {
+        'crds' : 'crds',
+        'crds.client' : 'crds/client',
+        'crds.hst' : 'crds/hst',
+        'crds.jwst' : 'crds/jwst',
+        'crds.submit' : 'crds/submit',
+        'crds.tobs' : 'crds/tobs',
+        'crds.tests' : 'crds/tests',
+        },
+    "package_data" : {
+        'crds.hst': [
+            '*.dat',
+            'tpns/*.tpn',
+            'specs/*.spec',
+            'specs/*.rmap',
+            'specs/*.json',
+            ],
+        'crds.jwst': [
+            '*.dat',
+            'tpns/*.tpn',
+            'specs/*.spec',
+            'specs/*.rmap',
+            'specs/*.json',
+            ],
+        'crds.tobs': [
+            '*.dat',
+            'tpns/*.tpn',
+            'specs/*.spec',
+            'specs/*.rmap',
+            'specs/*.json',
+            ],
+        },
+    "scripts" : glob.glob("scripts/*"),
+    }
+
+import crds   #  local subdirectory...  ew...
+
+setup(name="crds",
+      provides=["crds","crds.hst","crds.jwst","crds.client","crds.tobs"],
+      version=crds.__version__,
+      description="Calibration Reference Data System,  HST/JWST reference file management",
+      long_description=open('README.rst').read(),
+      author="Todd Miller",
+      author_email="jmiller@stsci.edu",
+      url="https://hst-crds.stsci.edu",
+      license="BSD",
+      requires=["numpy","astropy"],
+      classifiers=[
+          'Intended Audience :: Science/Research',
+          'License :: OSI Approved :: BSD License',
+          'Operating System :: POSIX :: Linux',
+          'Operating System :: MacOS :: MacOS X',
+          'Programming Language :: Python :: 2.7',
+          'Programming Language :: Python :: 3',
+          'Topic :: Scientific/Engineering :: Astronomy',
+      ],
+      **setup_pars
+      )

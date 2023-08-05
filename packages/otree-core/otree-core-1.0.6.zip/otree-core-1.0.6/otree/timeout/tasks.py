4d8c507edb03240af6842419cfa3b7ddc4d41111
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:56 +0000

    Add package otree-core/1.0.6/otree-core-1.0.6.zip

---
+++ packages/otree-core/otree-core-1.0.6.zip/otree-core-1.0.6/otree/timeout/tasks.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import django.test
+from huey.contrib.djhuey import db_task
+
+from otree import constants_internal
+
+
+test_client = django.test.Client()
+
+
+@db_task()
+def submit_expired_url(participant_code, original_index_in_pages, url):
+    from otree.models import Participant
+
+    result = Participant.objects.filter(
+            code=participant_code).values_list(
+        '_index_in_pages', flat=True)
+    if result:
+        latest_index_in_pages = result[0]
+        if latest_index_in_pages <= original_index_in_pages:
+            test_client.post(
+                url, data={constants_internal.auto_submit: True}, follow=True)
+        # otherwise, the participant already proceeded past the page.
+    # maybe was deleted from the DB
+
+
+@db_task()
+def ensure_pages_visited(participant_pk_set, wait_page_index):
+    """This is necessary when a wait page is followed by a timeout page.
+    We can't guarantee the user's browser will properly continue to poll
+    the wait page and get redirected, so after a grace period we load the page
+    automatically, to kick off the expiration timer of the timeout page.
+    """
+
+    from otree.models import Participant
+
+    unvisited_participants = Participant.objects.filter(
+        pk__in=participant_pk_set,
+        _index_in_pages__lte=wait_page_index,
+    )
+    for participant in unvisited_participants:
+
+        # if the wait page is the first page,
+        # then _current_form_page_url could be null.
+        # in this case, use the start_url() instead,
+        # because that will redirect to the current wait page.
+        # (alternatively we could define _current_page_url or
+        # current_wait_page_url)
+        url = participant._current_form_page_url or participant._start_url()
+        test_client.get(url, follow=True)
---
+++ packages/otree-core/otree-core-1.0.6.zip/otree-core-1.0.6/tests/test_bots.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from otree.bots.bot import ParticipantBot
+from .base import TestCase
+import otree.session
+from otree.bots.runner import session_bot_runner_factory
+import logging
+
+
+class TestBots(TestCase):
+    def test_bot_runs(self):
+
+        session = otree.session.create_session(
+            session_config_name='bots',
+            num_participants=2,
+            use_cli_bots=True,
+            bot_case_number=0,
+        )
+
+        bot_runner = session_bot_runner_factory(session)
+
+        with self.assertRaises(ZeroDivisionError):
+            bot_runner.play()
+
+    def test_bots_check_html(self):
+
+        session = otree.session.create_session(
+            session_config_name='bots_check_html',
+            num_participants=1,
+            use_cli_bots=True,
+            bot_case_number=0,
+        )
+
+        with self.settings(BOTS_CHECK_HTML=True):
+            from django.conf import settings
+            self.assertEqual(settings.BOTS_CHECK_HTML, True)
+
+            bot_runner = session_bot_runner_factory(session)
+
+            try:
+                bot_runner.play()
+            except AssertionError as exc:
+                # AssertionError should say something about check_html
+                raises_correct_message = 'check_html' in str(exc)
+            else:
+                raises_correct_message = False
+            if not raises_correct_message:
+                raise AssertionError('bots check_html not working properly')
+
+    def test_bot_bad_post(self):
+        """
+        Test that posting bad data without using SubmitInvalid
+        will raise an error.
+        """
+
+        session = otree.session.create_session(
+            session_config_name='bots_bad_post',
+            num_participants=1,
+            use_cli_bots=True,
+            bot_case_number=0,
+        )
+
+        bot_runner = session_bot_runner_factory(session)
+
+        with self.assertRaises(AssertionError):
+            # need to disable log output, because this triggers an exception
+            # that is logged to stdout
+            logging.disable(logging.CRITICAL)
+            bot_runner.play()
+            logging.disable(logging.NOTSET)

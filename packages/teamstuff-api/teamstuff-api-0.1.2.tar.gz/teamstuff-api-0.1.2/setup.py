f8811dd860b7911d790177398d09c8dc14a57b19
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:28 +0000

    Add package teamstuff-api/0.1.2/teamstuff-api-0.1.2.tar.gz

---
+++ packages/teamstuff-api/teamstuff-api-0.1.2.tar.gz/teamstuff-api-0.1.2/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+
+setup(
+    name='teamstuff-api',
+    version='0.1.2',
+    author='Tom Werner',
+    author_email='hello@tomwerner.me.uk',
+    packages=['teamstuff-api'],
+    url='https://gitlab.com/tomwerneruk/teamstuff-api',
+    license='MIT',
+    description='',
+    long_description=open('README.txt').read(),
+)

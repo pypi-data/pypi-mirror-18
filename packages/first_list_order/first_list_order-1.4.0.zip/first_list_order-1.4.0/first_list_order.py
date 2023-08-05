68959c663063b1c95501603872bcce7cd8de5212
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:37 +0000

    Add package first_list_order/1.4.0/first_list_order-1.4.0.zip

---
+++ packages/first_list_order/first_list_order-1.4.0.zip/first_list_order-1.4.0/first_list_order.py
@@ -0,0 +1,17 @@
+"""Esse módulo nester.py fornece um função chamada print_lol que permite processar e imprimir uma lista ela estando aninhadas ou não."""
+
+def print_lol(the_list, indent=False, level=0):
+
+    """Essa função requer um argumento posicional chamado "the_list", que é qualquer lista em python. Cada item de dados na lista fornecida é
+(recursivamente) impresso na tela em sua propria linha e o argumento "level" que determina a tabulação de cada aninhamento"""
+    
+    for each_item in the_list:
+        if isinstance(each_item, list):
+            print_lol(each_item, indent, level+1)
+        else:
+            if indent:
+                for tab_stop in range(level):
+                    print("\t", end='')
+            print(each_item)
+
+            

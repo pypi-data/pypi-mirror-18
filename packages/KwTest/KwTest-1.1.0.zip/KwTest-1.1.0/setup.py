1fb6314db07cee9b9fe544132e8769dc3a929ac1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:58:31 +0000

    Add package KwTest/1.1.0/KwTest-1.1.0.zip

---
+++ packages/KwTest/KwTest-1.1.0.zip/KwTest-1.1.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name = 'KwTest',
+    version = '1.1.0',
+    py_modules = ['KwTest'],
+    author = 'kwakcom',
+    author_email = 'malefic76@gmail.com',
+    url = '',
+    description = 'Test module for python',
+    )

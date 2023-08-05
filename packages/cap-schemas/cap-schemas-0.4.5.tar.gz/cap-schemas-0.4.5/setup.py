291ef96a0921de56acb5ad98258424aa5d9d1361
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:43 +0000

    Add package cap-schemas/0.4.5/cap-schemas-0.4.5.tar.gz

---
+++ packages/cap-schemas/cap-schemas-0.4.5.tar.gz/cap-schemas-0.4.5/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    author = 'Lukas Heinrich',
+    author_email = 'lukas.heinrich@gmail.com',
+    name = 'cap-schemas',
+    version = '0.4.5',
+    description = 'schemas for analysis preservation',
+    include_package_data = True,
+    packages = find_packages(),
+    install_requires = [
+        'jsonref',
+        'pyyaml',
+        'requests[security]',
+        'jsonschema'
+    ]
+)

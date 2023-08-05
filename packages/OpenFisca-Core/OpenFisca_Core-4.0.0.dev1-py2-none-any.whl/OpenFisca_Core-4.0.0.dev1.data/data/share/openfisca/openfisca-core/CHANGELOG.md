f16c4c281074f3f6a5c1b61c01f1c0a7bd6f47cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:08:32 +0000

    Add package OpenFisca-Core/4.0.0.dev1/OpenFisca_Core-4.0.0.dev1-py2-none-any.whl

---
+++ packages/OpenFisca-Core/OpenFisca_Core-4.0.0.dev1-py2-none-any.whl/OpenFisca_Core-4.0.0.dev1.data/data/share/openfisca/openfisca-core/CHANGELOG.md
@@ -0,0 +1,106 @@
+# Changelog
+
+## 3.0.3
+
+* Fix `cerfa_field` validation in `Column`, `Formula` and `AbstractConversionVariable`.
+  Previously, some variables having `cerfa_field` as a `dict` were converted to `unicode` by mistake.
+  See https://github.com/openfisca/openfisca-france/issues/543
+
+## 3.0.2
+
+* Move `calmar.py` to [OpenFisca-Survey-Manager](https://github.com/openfisca/openfisca-survey-manager).
+
+  No incidence on users since it was only needed for dataframes.
+
+## 3.0.1
+
+* Adapt requested_period_last_value and last_duration_last_value to extra params
+
+## 3.0.0
+
+* Update introspection data. This allows to enhance data served by the web API which itself feeds the Legislation Explorer.
+
+## 2.2.2
+
+* Update travis procedures
+
+## 2.2.1
+
+* Remove conda from travis config
+
+## 2.2.0
+
+* Implement simulation.calculate `print_trace=True` argument. Options: `max_depth` and `show_default_values`
+
+  Examples:
+  ```python
+  simulation = scenario.new_simulation(trace=True)
+  simulation.calculate('irpp', 2014, print_trace=True)
+  simulation.calculate('irpp', 2014, print_trace=True, max_depth=-1)
+  simulation.calculate('irpp', 2014, print_trace=True, max_depth=-1, show_default_values=False)
+  ```
+
+## 2.1.0 – [diff](https://github.com/openfisca/openfisca-core/compare/2.0.3...2.0.4)
+
+* Load extensions from pip packages
+
+## 2.0.4 – [diff](https://github.com/openfisca/openfisca-core/compare/2.0.3...2.0.4)
+
+* Use DEFAULT_DECOMP_FILE attribute from reference TB system
+
+## 2.0.3 – [diff](https://github.com/openfisca/openfisca-core/compare/2.0.2...2.0.3)
+
+* Explicit the error when a variable is not found
+
+## 2.0.2 – [diff](https://github.com/openfisca/openfisca-core/compare/2.0.1...2.0.2)
+
+* Update numpy dependency to 1.11
+
+## 2.0.1 – [diff](https://github.com/openfisca/openfisca-core/compare/2.0.0...2.0.1)
+
+* Force updating version number and CHANGELOG.md before merging on master
+* Release tag and Pip version automatically
+
+## 2.0.0 – [diff](https://github.com/openfisca/openfisca-core/compare/1.1.0...2.0.0)
+
+* Variables are not added to the TaxBenefitSystem when the entities class are imported, but explicitely when the TaxBenefitSystem is instanciated.
+  * Metaclasses are not used anymore.
+* New API for TaxBenefitSystem
+  * Columns are now stored in the TaxBenefitSystem, not in entities.
+* New API for rerforms.
+* XmlBasedTaxBenefitSystem is deprecated, and MultipleXmlBasedTaxBenefitSystem renamed to TaxBenefitSystem
+
+## 1.1.0 – [diff](https://github.com/openfisca/openfisca-core/compare/1.0.0...1.1.0)
+
+* Implement cache opt out system
+
+## 1.0.0 – [diff](https://github.com/openfisca/openfisca-core/compare/0.5.4...1.0.0)
+
+* Remove `build_column` obsolete function. Use `Variable` class instead.
+
+## 0.5.4 – [diff](https://github.com/openfisca/openfisca-core/compare/0.5.3...0.5.4)
+
+* Merge pull request #382 from openfisca/fix-sum-by-entity
+* The result size must be the targent entity'one
+
+## 0.5.3 – [diff](https://github.com/openfisca/openfisca-core/compare/0.5.2...0.5.3)
+
+* Many updates
+
+## 0.5.2 – [diff](https://github.com/openfisca/openfisca-core/compare/0.5.1...0.5.2)
+
+* Add trim option to marginal rate computation
+* Compute stop date of whole tax scale instead of a single bracket.
+* Use bracket stop date instead of legislation stop date inside a bracket.
+* Introduce reference periods
+* Fix roles_count bug : was set to 1 for scenarios vectorially defined
+
+## 0.5.1 – [diff](https://github.com/openfisca/openfisca-core/compare/0.5.0...0.5.1)
+
+* Move docs to gitbook repo
+* Upgrade to new Travis infrastructure
+* Enhance errors display when a reform modifies the legislation
+
+## 0.5.0
+
+* First release uploaded to PyPI
---
+++ packages/OpenFisca-Core/OpenFisca_Core-4.0.0.dev1-py2-none-any.whl/openfisca_core/entities.py
@@ -0,0 +1,185 @@
+# -*- coding: utf-8 -*-
+
+import numpy as np
+import warnings
+
+from enumerations import Enum
+from formulas import ADD, DIVIDE
+
+
+class Entity(object):
+    key = None
+    plural = None
+    label = None
+    roles = None
+    is_person = False
+
+    def __init__(self, simulation):
+        self.simulation = simulation
+        self.count = 0
+        self.step_size = 0
+
+    # Calculations
+
+    def check_variable_defined_for_entity(self, variable_name):
+        if not (self.simulation.get_variable_entity(variable_name) == self):
+            variable_entity = self.simulation.get_variable_entity(variable_name)
+            raise Exception(
+                "Variable {} is not defined for {} but for {}".format(
+                    variable_name, self.key, variable_entity.key)
+                )
+
+    def check_array_compatible_with_entity(self, array):
+        if not self.count == array.size:
+            raise Exception("Input {} is not a valid value for the entity {}".format(array, self.key))
+
+    def __call__(self, variable_name, period = None, options = []):
+        self.check_variable_defined_for_entity(variable_name)
+
+        if ADD in options and DIVIDE in options:
+            return self.simulation.calculate_add_divide(variable_name, period)
+        elif ADD in options:
+            return self.simulation.calculate_add(variable_name, period)
+        elif DIVIDE in options:
+            return self.simulation.calculate_divide(variable_name, period)
+        else:
+            return self.simulation.calculate(variable_name, period)
+
+    # Helpers
+
+    def empty_array(self):
+        return np.zeros(self.count)
+
+    def filled_array(self, value):
+        with warnings.catch_warnings():  # Avoid a non-relevant warning
+            warnings.simplefilter("ignore")
+            return np.full(self.count, value)
+
+
+class PersonEntity(Entity):
+    is_person = True
+
+    # Projection person -> person
+
+    def role_in(self, entity):
+        entity = self.simulation.get_entity(entity)
+        return entity.members_role
+
+    def value_from_partner(self, array, entity, role):
+        # Make sure there is only two people with the role
+        self.check_array_compatible_with_entity(array)
+        entity_filter = entity.nb_persons(role) == 2
+        higher_value = entity_filter * entity.max(array, role)
+        lower_value = entity_filter * entity.min(array, role)
+        higher_value_i = entity.project(higher_value, role)
+        lower_value_i = entity.project(lower_value, role)
+        return (array == higher_value_i) * lower_value_i + (array == lower_value_i) * higher_value_i
+
+
+class GroupEntity(Entity):
+
+    @classmethod
+    def get_role_enum(cls):
+        return Enum(map(lambda role: role['key'], cls.roles))
+
+    def __init__(self, simulation):
+        Entity.__init__(self, simulation)
+        self.members_entity_id = None
+        self.members_role = None
+        self.members_legacy_role = None
+        self.members_position = None
+
+    @property
+    def members(self):
+        return self.simulation.persons
+
+    #  Aggregation persons -> entity
+
+    def sum(self, array, role = None):
+        self.simulation.persons.check_array_compatible_with_entity(array)
+        result = self.empty_array()
+        if role is not None:
+            role_filter = (self.members_role == role)
+
+            # Entities for which one person at least has the given role
+            entity_has_role_filter = np.bincount(self.members_entity_id, weights = role_filter) > 0
+
+            result[entity_has_role_filter] += np.bincount(self.members_entity_id[role_filter], weights = array[role_filter])
+        else:
+            result += np.bincount(self.members_entity_id, weights = array)
+        return result
+
+    def any(self, array, role = None):
+        sum_in_entity = self.sum(array, role = role)
+        return (sum_in_entity > 0)
+
+    def reduce(self, array, reducer, neutral_element, role = None):
+        self.simulation.persons.check_array_compatible_with_entity(array)
+        position_in_entity = self.members_position
+        role_in_entity = self.members_role
+        role_filter = (role_in_entity == role) if role is not None else True
+
+        result = self.filled_array(neutral_element)  # Neutral value that will be returned if no one with the given role exists.
+
+        # We loop over the positions in the entity
+        # Looping over the entities is tempting, but potentielly slow if there are a lot of entities
+        nb_positions = np.max(position_in_entity) + 1
+        for p in range(nb_positions):
+            filter = (position_in_entity == p) * role_filter
+            entity_filter = self.any(filter)
+            result[entity_filter] = reducer(result[entity_filter], array[filter])
+
+        return result
+
+    def all(self, array, role = None):
+        return self.reduce(array, neutral_element = True, reducer = np.logical_and, role = role)
+
+    def max(self, array, role = None):
+        return self.reduce(array, neutral_element = - np.infty, reducer = np.maximum, role = role)
+
+    def min(self, array, role = None):
+        return self.reduce(array, neutral_element = np.infty, reducer = np.minimum, role = role)
+
+    def nb_persons(self, role = None):
+            role_condition = (self.members_role == role)
+            return self.sum(role_condition)
+
+    # Projection person -> entity
+
+    def value_from_person(self, array, role, default = 0):
+        # TODO: Make sure the role is unique
+        self.simulation.persons.check_array_compatible_with_entity(array)
+        result = self.filled_array(default)
+        role_filter = (self.members_role == role)
+        entity_filter = self.any(role_filter)
+
+        result[entity_filter] = array[role_filter]
+
+        return result
+
+    # Projection entity -> person(s)
+
+    def project(self, array, role = None):
+        self.check_array_compatible_with_entity(array)
+        role_condition = (self.members_role == role) if role is not None else True
+        return array[self.members_entity_id] * role_condition
+
+    def project_on_first_person(self, array):
+        self.check_array_compatible_with_entity(array)
+        entity_position_array = self.members_position
+        entity_index_array = self.members_entity_id
+        boolean_filter = (entity_position_array == 0)
+        return array[entity_index_array] * boolean_filter
+
+    def share_between_members(self, array, role = None):
+        self.check_array_compatible_with_entity(array)
+        nb_persons_per_entity = self.nb_persons(role)
+        return self.project(array / nb_persons_per_entity, role = role)
+
+    # Projection entity -> entity
+
+    def transpose(self, array, origin_entity):
+        origin_entity = self.simulation.get_entity(origin_entity)
+        origin_entity.check_array_compatible_with_entity(array)
+        input_projected = origin_entity.project_on_first_person(array)
+        return self.sum(input_projected)

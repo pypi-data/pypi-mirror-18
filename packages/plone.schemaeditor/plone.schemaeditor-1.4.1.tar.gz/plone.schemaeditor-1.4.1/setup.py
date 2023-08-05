8b54dd4afa0a638c18051aa129c0e77084adad2e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:50 +0000

    Add package plone.schemaeditor/1.4.1/plone.schemaeditor-1.4.1.tar.gz

---
+++ packages/plone.schemaeditor/plone.schemaeditor-1.4.1.tar.gz/plone.schemaeditor-1.4.1/setup.py
@@ -0,0 +1,48 @@
+from setuptools import setup, find_packages
+
+version = '1.4.1'
+
+setup(name='plone.schemaeditor',
+      version=version,
+      description="Provides through-the-web editing of a zope schema/interface.",
+      long_description=open("README.rst").read() + "\n" +
+      open("CHANGES.rst").read(),
+      classifiers=[
+          "Framework :: Zope2",
+          "Framework :: Plone",
+          "Framework :: Plone :: 4.3",
+          "Programming Language :: Python",
+          "Programming Language :: Python :: 2.6",
+          "Programming Language :: Python :: 2.7",
+          "Topic :: Software Development :: Libraries :: Python Modules",
+          "License :: OSI Approved :: BSD License",
+      ],
+      keywords='plone schema ttw',
+      author='David Glick',
+      author_email='dglick@gmail.com',
+      url='http://svn.plone.org/svn/plone/plone.schemaeditor',
+      license='BSD',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['plone'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          'Zope2',
+          'zope.component',
+          'zope.container',
+          'zope.interface',
+          'zope.lifecycleevent',
+          'zope.schema',
+          'zope.publisher',
+          'z3c.form',
+          'plone.z3cform',
+          'plone.autoform',
+      ],
+      extras_require={'test': [
+          'plone.app.dexterity',
+          'plone.app.testing',
+          'plone.app.robotframework',
+          'lxml',
+      ]},
+      )

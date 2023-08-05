e80007fc9863f964da0c9de781cf42c386094e2a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:10 +0000

    Add package prefixeddict/0.3/prefixeddict-0.3.tar.gz

---
+++ packages/prefixeddict/prefixeddict-0.3.tar.gz/prefixeddict-0.3/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name='prefixeddict',
+    packages=['prefixeddict'],
+    version='0.3',
+    description='Prefixed dictionaries for Python 2.6+/3.2+.',
+    author='Martin Jungblut Schreiner',
+    author_email='martinjungblut@gmail.com',
+    url='https://github.com/martinjungblut/prefixeddict',
+    download_url='https://github.com/martinjungblut/prefixeddict/tarball/0.3',
+    keywords=['dictionary', 'dict', 'prefixed', 'prefix', 'collections'],
+    classifiers=[],
+)

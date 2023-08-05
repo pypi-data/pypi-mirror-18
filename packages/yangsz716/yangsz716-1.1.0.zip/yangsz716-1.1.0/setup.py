105f0ea0e288e89906928d697e252bb56859d452
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:34 +0000

    Add package yangsz716/1.1.0/yangsz716-1.1.0.zip

---
+++ packages/yangsz716/yangsz716-1.1.0.zip/yangsz716-1.1.0/setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+
+setup(name='yangsz716',
+      version='1.1.0',
+      py_modules=['nester'],
+      author='yangsz716',
+      
+      description='A simple printer of nested lists')

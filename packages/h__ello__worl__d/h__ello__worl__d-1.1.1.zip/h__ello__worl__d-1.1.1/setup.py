64d45664b2ef7826418869ce1618cb488e07c3fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:48 +0000

    Add package h__ello__worl__d/1.1.1/h__ello__worl__d-1.1.1.zip

---
+++ packages/h__ello__worl__d/h__ello__worl__d-1.1.1.zip/h__ello__worl__d-1.1.1/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+	name		= 'h__ello__worl__d',
+	version		= '1.1.1',
+	py_modules	= ['h__ello__worl__d'],
+	author		= 'zheyang',
+	author_email	= 'hfpython@headfirstlabs.com',
+	url		= 'http://www.headfirstlabs.com',
+	description	= 'A simple printer of nested lists',
+)

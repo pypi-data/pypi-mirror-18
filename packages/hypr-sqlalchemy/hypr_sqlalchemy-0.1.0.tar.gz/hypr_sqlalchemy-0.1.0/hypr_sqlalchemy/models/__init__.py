f2a66376388d801b5bd7bc947c422e0a11f8b315
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:36 +0000

    Add package hypr-sqlalchemy/0.1.0/hypr_sqlalchemy-0.1.0.tar.gz

---
+++ packages/hypr-sqlalchemy/hypr_sqlalchemy-0.1.0.tar.gz/hypr_sqlalchemy-0.1.0/hypr_sqlalchemy/models/__init__.py
@@ -0,0 +1,13 @@
+# Copyright 2014-2016 Morgan Delahaye-Prat. All Rights Reserved.
+#
+# Licensed under the Simplified BSD License (the "License");
+# you may not use this file except in compliance with the License.
+"""Models provided by the Hypr.SQLAlchemy extension."""
+
+
+from hypr_sqlalchemy.models.sqlalchemy import SqlAlchemyModel
+
+
+__version__ = '0.1.0'
+__all__ = ('SqlAlchemyModel',)
+

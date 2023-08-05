ad9ef6b346751b668e67b7d57a4b8e1c67dd659e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:06 +0000

    Add package stamps-python/0.0.2/stamps-python-0.0.2.tar.gz

---
+++ packages/stamps-python/stamps-python-0.0.2.tar.gz/stamps-python-0.0.2/stamps/memberships.py
@@ -0,0 +1,43 @@
+from .base_api import BaseAPI
+
+
+class Memberships(BaseAPI):
+
+    def status(self, user=None):
+        url = self.client.base_url + "/memberships/status"
+
+        payload = {
+            'token': self.client.token,
+            'merchant': self.client.merchant_id,
+            'user': user
+        }
+        return self.client._call('GET', url, payload)
+
+    def register(self, name=None, email=None, birthday=None, gender=None,
+                 member_id=None, phone=None, address=None, password=None):
+
+        url = self.client.base_url + "/memberships/register"
+        payload = {
+            'token': self.client.token,
+            'merchant': self.client.merchant_id,
+            'name': name,
+            'email': email,
+            'gender': gender,
+            'birthday': birthday,
+            'member_id': member_id,
+            'phone': phone,
+            'address': address,
+            'password': password
+        }
+        return self.client._call('POST', url, payload)
+
+    def add_stamps(self, user=None, stamps=None, note=None):
+        url = self.client.base_url + "/memberships/add-stamps"
+        payload = {
+            'token': self.client.token,
+            'merchant': self.client.merchant_id,
+            'user': user,
+            'note': note,
+            'stamps': stamps
+        }
+        return self.client._call('POST', url, payload)
---
+++ packages/stamps-python/stamps-python-0.0.2.tar.gz/stamps-python-0.0.2/stamps/redemptions.py
@@ -0,0 +1,27 @@
+from .base_api import BaseAPI
+
+
+class Redemptions(BaseAPI):
+
+    def add(self, user=None, id=None, type=None):
+        url = self.client.base_url + "/redemptions/add"
+
+        payload = {
+            'token': self.client.token,
+            'merchant': self.client.merchant_id,
+            'store': self.store_id,
+            'user': user,
+            'reward': id,
+            'type': type,
+        }
+
+        return self.client._call('POST', url, payload)
+
+    def cancel(self, id=None):
+        url = self.client.base_url + "/redemptions/cancel"
+
+        payload = {
+            'token': self.client.token,
+            'id': id
+        }
+        return self.client._call('POST', url, payload)

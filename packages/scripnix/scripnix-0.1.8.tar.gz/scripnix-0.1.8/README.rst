d4ea104ae3a6104c3294050a4cdfaffe7d4e8f17
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:30 +0000

    Add package scripnix/0.1.8/scripnix-0.1.8.tar.gz

---
+++ packages/scripnix/scripnix-0.1.8.tar.gz/scripnix-0.1.8/README.rst
@@ -0,0 +1,84 @@
+Scripnix
+========
+
+Useful Python3 and bash shell scripts for macOS/BSD and \*NIX. Useful to
+me, at any rate. YMMV.
+
+|status| |buildstatus| |codecov| |pypiversion| |pyversions| |licence|
+
+Replaces the old `Scripnix0 <https://github.com/yukondude/Scripnix0>`__
+project which had grown crufty and was not macOS-friendly.
+
+Licence
+-------
+
+Licensed under the `GNU General Public License, version
+3 <https://www.gnu.org/licenses/gpl-3.0.en.html>`__. Refer to the
+attached LICENSE file or see http://www.gnu.org/licenses/ for details.
+
+Change Log
+----------
+
+The current version is 0.1.8. Scripnix is
+`semver <http://semver.org/>`__-ish in its versioning scheme.
+
+Scripnix is currently an alpha release, so expect many many breaking
+changes. Once ready for prime time, the major version number will jump
+to 2 to reflect that this is actually the second incarnation of
+Scripnix.
+
+Installation
+------------
+
+Following installation using one of the methods below, you may want to
+run the ``install-scripnix`` command as the root user to setup the
+system-wide configuration. Any other users that also wish to use
+Scripnix should also run that command.
+
+Homebrew (macOS)
+~~~~~~~~~~~~~~~~
+
+On macOS, Homebrew will take care of installing any dependencies,
+including Python 3.
+
+::
+
+    brew tap yukondude/tap
+    brew install scripnix
+
+PyPI (POSIX)
+~~~~~~~~~~~~
+
+On \*NIX, you will first need to install Python 3.3 (or higher) using
+your preferred method.
+
+::
+
+    pip3 install scripnix
+
+Development Setup
+-----------------
+
+1. Install Scripnix, as above, so that all of its dependencies are
+   available.
+2. Create a Python 3 virtualenv for Scripnix.
+3. Clone the Scripnix repo.
+4. Install dependencies:
+   ``pip install click coverage pytest pytest-cov``
+5. Install the project in development mode: ``./setup.py develop``
+6. Run the unit tests to make sure everything is copacetic:
+   ``./setup.py test``
+7. Pour a snifter of Martell XO and light up a Bolivar Belicoso.
+
+.. |status| image:: https://img.shields.io/pypi/status/Scripnix.svg
+   :target: https://pypi.python.org/pypi/Scripnix/
+.. |buildstatus| image:: https://travis-ci.org/yukondude/Scripnix.svg?branch=master
+   :target: https://travis-ci.org/yukondude/Scripnix
+.. |codecov| image:: https://codecov.io/gh/yukondude/Scripnix/branch/master/graph/badge.svg
+   :target: https://codecov.io/gh/yukondude/Scripnix
+.. |pypiversion| image:: https://img.shields.io/pypi/v/Scripnix.svg
+   :target: https://pypi.python.org/pypi/Scripnix/
+.. |pyversions| image:: https://img.shields.io/pypi/pyversions/Scripnix.svg
+   :target: https://pypi.python.org/pypi/Scripnix/
+.. |licence| image:: https://img.shields.io/pypi/l/Scripnix.svg
+   :target: https://www.gnu.org/licenses/gpl-3.0.en.html
---
+++ packages/scripnix/scripnix-0.1.8.tar.gz/scripnix-0.1.8/setup.py
@@ -0,0 +1,133 @@
+#!/usr/bin/env python
+""" Scripnix setuptools configuration.
+"""
+
+# This file is part of Scripnix. Copyright 2016 Dave Rogers <info@yukondude.com>. Licensed under the GNU General Public License, version 3.
+# Refer to the attached LICENSE file or see <http://www.gnu.org/licenses/> for details.
+
+import os
+from setuptools import setup, find_packages
+from setuptools.command.develop import develop as DevelopCommand
+from setuptools.command.install import install as InstallCommand
+from setuptools.command.test import test as TestCommand
+import subprocess
+import sys
+import scripnix
+
+
+if sys.version_info < (3, 3):
+    sys.stderr.write("Scripnix requires Python 3.3 or higher.\n")
+    sys.exit(1)
+
+
+HERE = os.path.abspath(os.path.dirname(__file__))
+
+
+with open(os.path.join(HERE, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read().strip()
+
+
+def gather_console_scripts():
+    """ Return the list of the project's console script entry points.
+    """
+    console_scripts = []
+    command_path = 'scripnix/pycommand'
+    full_command_path = os.path.join(HERE, command_path)
+    package_path = command_path.replace('/', '.')
+
+    for file_name in [fn for fn in os.listdir(full_command_path) if os.path.isfile(os.path.join(full_command_path, fn))]:
+        if file_name in ('__init__.py', 'command.py'):
+            continue
+
+        command_module = os.path.splitext(file_name)[0]
+        command_name = command_module.replace('_', '-')
+        console_scripts.append("{}={}.{}:main".format(command_name, package_path, command_module))
+
+    return console_scripts
+
+
+def gather_requirements(requirements_file_name):
+    """ Return the list of required packages and versions from requirements.txt.
+    """
+    return [pkg.strip() for pkg in open(os.path.join(HERE, requirements_file_name), 'r').readlines()]
+
+
+class PostDevelopCommand(DevelopCommand):
+    """Post-installation for development mode."""
+    def run(self):
+        DevelopCommand.run(self)
+
+        if not self.uninstall:
+            sys.stdout.write("Running install-scripnix\n")
+            subprocess.call(["install-scripnix", "--yes", "--verbose"])
+
+
+# noinspection PyClassHasNoInit
+class PostInstallCommand(InstallCommand):
+    """Post-installation for installation mode."""
+    def run(self):
+        InstallCommand.run(self)
+        sys.stdout.write("Running install-scripnix\n")
+        subprocess.call(["install-scripnix", "--yes"])
+
+
+# noinspection PyAttributeOutsideInit
+class PyTest(TestCommand):
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+        self.test_args = ["--cov=scripnix/pycommand", "--cov=tests", "--cov-report=term-missing", "--cov-fail-under=80"]
+        self.test_suite = True
+
+    def run_tests(self):
+        # noinspection PyPackageRequirements
+        import pytest
+        errcode = pytest.main(self.test_args)
+        sys.exit(errcode)
+
+
+setup(
+    author="Dave Rogers",
+    author_email="thedude@yukondude.com",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "Intended Audience :: System Administrators",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Natural Language :: English",
+        "Operating System :: MacOS",
+        "Operating System :: POSIX",
+        "Operating System :: POSIX :: Linux",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Unix Shell",
+        "Topic :: System",
+        "Topic :: System :: Archiving",
+        "Topic :: System :: Systems Administration",
+        "Topic :: Utilities",
+    ],
+    cmdclass={
+        'develop': PostDevelopCommand,
+        'install': PostInstallCommand,
+        'test': PyTest
+    },
+    description=scripnix.__doc__.strip(),
+    entry_points={
+        'console_scripts': gather_console_scripts(),
+    },
+    extras_require={
+        'testing': gather_requirements("requirements-test.txt"),
+    },
+    include_package_data=True,
+    install_requires=gather_requirements("requirements.txt"),
+    license="GPLv3",
+    long_description=long_description,
+    name="scripnix",
+    packages=find_packages(),
+    platforms=["MacOS", "Linux"],
+    tests_require=gather_requirements("requirements-test.txt"),
+    url="https://yukondude.github.io/Scripnix/",
+    version=scripnix.__version__
+)

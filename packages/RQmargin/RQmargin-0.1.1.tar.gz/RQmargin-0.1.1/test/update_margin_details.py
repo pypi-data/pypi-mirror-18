8abb43be594cc689e19e7848fb3af967d09b33d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:03 +0000

    Add package RQmargin/0.1.1/RQmargin-0.1.1.tar.gz

---
+++ packages/RQmargin/RQmargin-0.1.1.tar.gz/RQmargin-0.1.1/test/update_margin_details.py
@@ -0,0 +1,25 @@
+from store.store import *
+from config import get_config_one
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("update_mode", help="input 'total' or 'daily' .'total':it means that"
+                                        "it is the first time to update")
+args = parser.parse_args()
+
+database_url = get_config_one("../config.json").get('tushare')
+store_margin_details = StoringMarginDetails(database_url)
+if args.update_mode == 'total':
+    flag=input("are you sure to totally update ? it will drop the existed collection.(y/n)")
+    if flag== 'y':
+        store_margin_details.drop_margin_details()
+        store_margin_details.create_index()
+        store_margin_details.update_margin_details()
+elif args.update_mode == 'daily':
+    store_margin_details.update_margin_details()
+else:
+    raise RuntimeError("please add correct argument. 'total' or 'daily'.")
+print('begin to fetch data..')
+
+
+print('store successfully.')

7ecfe7ece77efc6c2922674835bcfdd24d53432d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:39 +0000

    Add package lambdautils/0.9.8/lambdautils-0.9.8.tar.gz

---
+++ packages/lambdautils/lambdautils-0.9.8.tar.gz/lambdautils-0.9.8/lambdautils/__init__.py
@@ -0,0 +1,5 @@
+"""Utilities for AWS Lambda functions."""
+
+
+__version__ = "0.9.8"
+__author__ = "German Gomez-Herrero, Arnaud Charpentier, FindHotel BV"
---
+++ packages/lambdautils/lambdautils-0.9.8.tar.gz/lambdautils-0.9.8/lambdautils/exception.py
@@ -0,0 +1,31 @@
+"""Exceptions."""
+
+
+class CriticalError(Exception):
+
+    """A blocking critical exception."""
+
+    pass
+
+
+class ProcessingError(Exception):
+
+    """Error processing event."""
+
+    def __init__(self, events, *args, **kwargs):
+        super(self.__class__, self).__init__(*args, **kwargs)
+        self.events = events
+
+
+class OutOfOrderError(Exception):
+
+    """An event is out of order: not really an error."""
+
+    pass
+
+
+class StateTableError(Exception):
+
+    """Unable to retrieve the name of a processor state table."""
+
+    pass

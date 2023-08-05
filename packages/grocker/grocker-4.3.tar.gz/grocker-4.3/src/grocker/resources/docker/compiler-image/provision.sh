bc88b5407306266966423b0f2e1a42d4391e851b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:09:21 +0000

    Add package grocker/4.3/grocker-4.3.tar.gz

---
+++ packages/grocker/grocker-4.3.tar.gz/grocker-4.3/src/grocker/resources/docker/compiler-image/provision.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+set -xe
+
+cd $(dirname $0)  # Go to Grocker build dir
+[ -e provision.env ] && source provision.env  # Retrieve config vars
+
+apt install -qy ${SYSTEM_DEPS}
+install --mode=0555 --owner=grocker /tmp/grocker/compile.py /home/grocker/compile.py
+
+cp grocker_sudoers /etc/sudoers.d/grocker_sudoers
+
+rm -r $(dirname $0)

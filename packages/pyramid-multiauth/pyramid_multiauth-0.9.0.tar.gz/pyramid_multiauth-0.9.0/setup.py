d91d640d1892e7dcc90c782ba67e5a5c0d0ded60
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:56:55 +0000

    Add package pyramid-multiauth/0.9.0/pyramid_multiauth-0.9.0.tar.gz

---
+++ packages/pyramid-multiauth/pyramid_multiauth-0.9.0.tar.gz/pyramid_multiauth-0.9.0/setup.py
@@ -0,0 +1,36 @@
+
+import os
+import sys
+from setuptools import setup, find_packages
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with open(os.path.join(here, 'README.rst')) as f:
+    README = f.read()
+
+with open(os.path.join(here, 'CHANGES.txt')) as f:
+    CHANGES = f.read()
+
+requires = ['pyramid']
+
+setup(name='pyramid_multiauth',
+      version='0.9.0',
+      description='pyramid_multiauth',
+      long_description=README + '\n\n' + CHANGES,
+      classifiers=[
+          "Programming Language :: Python",
+          "Framework :: Pylons",
+          "Topic :: Internet :: WWW/HTTP",
+          "License :: OSI Approved :: Mozilla Public License 2.0 (MPL 2.0)",
+      ],
+      author='Mozilla Services',
+      author_email='services-dev@mozilla.org',
+      url='https://github.com/mozilla-services/pyramid_multiauth',
+      keywords='web pyramid pylons authentication',
+      packages=find_packages(),
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=requires,
+      tests_require=requires,
+      test_suite="pyramid_multiauth",
+      paster_plugins=['pyramid'])

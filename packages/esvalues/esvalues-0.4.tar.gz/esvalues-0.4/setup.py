12837d215b7deb969549e2058060be78d6b8ca6c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:56:39 +0000

    Add package esvalues/0.4/esvalues-0.4.tar.gz

---
+++ packages/esvalues/esvalues-0.4.tar.gz/esvalues-0.4/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+
+setup(name='esvalues',
+      version='0.4',
+      description='Estimates Expectation Shapley (ES) values for arbirary functions.',
+      url='http://github.com/interpretable-ml/esvalues.py',
+      author='Scott Lundberg',
+      author_email='slund1@cs.washington.edu',
+      license='MIT',
+      packages=['esvalues'],
+      install_requires=['numpy', 'scipy'],
+      test_suite='nose.collector',
+      tests_require=['nose'],
+      zip_safe=False)

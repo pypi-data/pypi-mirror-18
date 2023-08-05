70889a77379b167e7c6321ce9057f09a87a11eca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:29 +0000

    Add package pandalone/0.1.13/pandalone-0.1.13-py2.py3-none-any.whl

---
+++ packages/pandalone/pandalone-0.1.13-py2.py3-none-any.whl/pandalone/_version.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+#
+# Copyright 2014-2015 European Commission (JRC);
+# Licensed under the EUPL (the 'Licence');
+# You may not use this work except in compliance with the Licence.
+# You may obtain a copy of the Licence at: http://ec.europa.eu/idabc/eupl
+"""Authoritative project's version-info"""
+__version__ = version = "0.1.13"  # PEP 440 versioning. Also update README.rst's.
+__updated__ = "2016-11-01 20:45:19"

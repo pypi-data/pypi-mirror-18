3d20b912a095215f5de26d9fc912226a6279df75
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:49 +0000

    Add package em73xx/0.10/em73xx-0.10.tar.gz

---
+++ packages/em73xx/em73xx-0.10.tar.gz/em73xx-0.10/setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup
+
+current_version = '0.10'
+
+# convert from github markdown to rst
+try:
+    import pypandoc
+    long_description = pypandoc.convert('README.md', 'rst')
+except(IOError, ImportError):
+    long_description = open('README.md').read()
+
+setup(
+    name = 'em73xx',
+    packages = [ 'em73xx', 'em73xx.test' ],
+    version = current_version,
+    description = 'python wrapper for the em73xx series of 4G modems present in recent Lenovo Thinkpads',
+    author = 'Sean McLemon',
+    author_email = 'sean.mclemon@gmail.com',
+    url = 'https://github.com/smcl/py-em73xx',
+    download_url = 'https://github.com/smcl/py-em73xx/tarball/%s' % (current_version),
+    keywords = ['thinkpad', 'em7345', 'em73xx'],
+    classifiers = [],
+    test_suite='em73xx.test.all',
+    install_requires=[
+        'unittest2',
+        'pyserial',
+        'python-dateutil'
+    ],
+    setup_requires=[
+        'unittest2',
+        'pyserial',
+        'python-dateutil'
+    ],
+    long_description=long_description
+)

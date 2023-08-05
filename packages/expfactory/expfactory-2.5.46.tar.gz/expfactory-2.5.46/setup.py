aff2a6be205a2020fcbe95f0764de2f2e4638a72
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:26 +0000

    Add package expfactory/2.5.46/expfactory-2.5.46.tar.gz

---
+++ packages/expfactory/expfactory-2.5.46.tar.gz/expfactory-2.5.46/setup.py
@@ -0,0 +1,40 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+setup(
+    # Application name:
+    name="expfactory",
+
+    # Version number (initial):
+    version="2.5.46",
+
+    # Application author details:
+    author="Vanessa Sochat",
+    author_email="vsochat@stanford.edu",
+
+    # Packages
+    packages=find_packages(),
+
+    # Data files
+    include_package_data=True,
+    zip_safe=False,
+
+    # Details
+    url="http://www.github.com/expfactory",
+
+    license="LICENSE",
+    description="Python module for managing experiment factory experiments, for deployment to a psiturk battery or docker container.",
+    keywords='psiturk behavior neuroscience experiment factory docker',
+
+    install_requires = ['requests','numpy','Flask','gitpython','flask-restful','selenium==2.53.6','cognitiveatlas','scipy','numexpr','seaborn'],
+
+    entry_points = {
+        'console_scripts': [
+            'expfactory=expfactory.scripts:main',
+        ],
+    },
+
+)

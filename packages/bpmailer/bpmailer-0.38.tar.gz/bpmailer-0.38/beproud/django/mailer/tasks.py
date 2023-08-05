1f6d9d98c03729aacde54987e1555460620c1154
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:08:05 +0000

    Add package bpmailer/0.38/bpmailer-0.38.tar.gz

---
+++ packages/bpmailer/bpmailer-0.38.tar.gz/bpmailer-0.38/beproud/django/mailer/tasks.py
@@ -0,0 +1,114 @@
+#:coding=utf-8:
+from django.conf import settings
+
+try:
+    from celery import shared_task
+except ImportError:
+    from celery.task import task as shared_task
+
+import celery
+
+if celery.VERSION < (3, 1):
+    try:
+        import djcelery  # NOQA
+    except ImportError:
+        from django.core.exceptions import ImproperlyConfigured
+        raise ImproperlyConfigured("when used celery<3.1, djcelery is required!")
+
+    if 'djcelery' not in settings.INSTALLED_APPS:
+        from django.core.exceptions import ImproperlyConfigured
+        raise ImproperlyConfigured("djcelery not in INSTALLED_APPS!")
+
+from beproud.django.mailer import api as mailer_api
+
+__all__ = (
+    'send_mail',
+    'send_template_mail',
+    'send_mass_mail',
+    'mail_managers',
+    'mail_managers_template',
+    'mail_admins',
+)
+
+
+@shared_task
+def send_mail(*args, **kwargs):
+    max_retries = kwargs.pop('max_retries', 3)
+    retry_countdown = kwargs.pop('retry_countdown', 10)
+    try:
+        mailer_api.send_mail(*args, **kwargs)
+    except Exception, e:
+        return send_mail.retry(
+            exc=e,
+            countdown=retry_countdown,
+            max_retries=max_retries,
+        )
+
+@shared_task
+def send_template_mail(*args, **kwargs):
+    max_retries = kwargs.pop('max_retries', 3)
+    retry_countdown = kwargs.pop('retry_countdown', 10)
+    try:
+        mailer_api.send_template_mail(*args, **kwargs)
+    except Exception, e:
+        return send_template_mail.retry(
+            exc=e,
+            countdown=retry_countdown,
+            max_retries=max_retries,
+        )
+
+
+@shared_task
+def send_mass_mail(*args, **kwargs):
+    max_retries = kwargs.pop('max_retries', 3)
+    retry_countdown = kwargs.pop('retry_countdown', 10)
+    try:
+        mailer_api.send_mass_mail(*args, **kwargs)
+    except Exception, e:
+        return send_mass_mail.retry(
+            exc=e,
+            countdown=retry_countdown,
+            max_retries=max_retries,
+        )
+
+
+@shared_task
+def mail_managers(*args, **kwargs):
+    max_retries = kwargs.pop('max_retries', 3)
+    retry_countdown = kwargs.pop('retry_countdown', 10)
+    try:
+        mailer_api.mail_managers(*args, **kwargs)
+    except Exception, e:
+        return mail_managers.retry(
+            exc=e,
+            countdown=retry_countdown,
+            max_retries=max_retries,
+        )
+
+
+@shared_task
+def mail_managers_template(*args, **kwargs):
+    max_retries = kwargs.pop('max_retries', 3)
+    retry_countdown = kwargs.pop('retry_countdown', 10)
+    try:
+        mailer_api.mail_managers_template(*args, **kwargs)
+    except Exception, e:
+        return mail_managers_template.retry(
+            exc=e,
+            countdown=retry_countdown,
+            max_retries=max_retries,
+        )
+
+
+@shared_task
+def mail_admins(*args, **kwargs):
+    max_retries = kwargs.pop('max_retries', 3)
+    retry_countdown = kwargs.pop('retry_countdown', 10)
+    try:
+        mailer_api.mail_admins(*args, **kwargs)
+    except Exception, e:
+        return mail_admins.retry(
+            exc=e,
+            countdown=retry_countdown,
+            max_retries=max_retries,
+        )
---
+++ packages/bpmailer/bpmailer-0.38.tar.gz/bpmailer-0.38/setup.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+#:coding=utf-8:
+
+# XXX: Hack to prevent stupid "TypeError: 'NoneType' object is not callable" error
+# in multiprocessing/util.py _exit_function when running `python
+# setup.py test` (see
+# http://www.eby-sarna.com/pipermail/peak/2010-May/003357.html)
+try:
+    import multiprocessing  # NOQA
+except ImportError, e:
+    pass
+
+import sys
+import os
+import fnmatch
+
+try:
+    current_dir = os.path.dirname(os.path.abspath(__file__))
+    for fn in os.listdir(current_dir):
+        if fnmatch.fnmatch(fn, 'billiard-*.egg'):
+            sys.path.append(os.path.join(current_dir, fn))
+
+    import billiard  # NOQA
+except ImportError, e:
+    pass
+
+############## End Hack ################
+
+from setuptools import setup, find_packages
+
+setup(
+    name='bpmailer',
+    version='0.38',
+    description='Mailing utility for Django',
+    author='BeProud Inc.',
+    author_email='ian@beproud.jp',
+    url='https://github.com/beproud/bpmailer/',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Plugins',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+    include_package_data=True,
+    packages=find_packages(),
+    namespace_packages=['beproud', 'beproud.django'],
+    install_requires=['Django>=1.2'],
+    tests_require=['celery>=2.2.7', 'mock>=0.7.2'],
+    test_suite='tests.main',
+    zip_safe=False,
+)

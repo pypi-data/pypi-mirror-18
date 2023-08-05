0dbdc392716a56dbf009a4b7ff77f87e20369d0a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:45 +0000

    Add package PySciter/0.4.5/PySciter-0.4.5.zip

---
+++ packages/PySciter/PySciter-0.4.5.zip/PySciter-0.4.5/sciter/capi/sctiscript.py
@@ -0,0 +1,15 @@
+"""TIScript Virtual Machine Runtime.
+
+Incomplete.
+"""
+import ctypes
+
+HVM = ctypes.c_void_p
+value = ctypes.c_uint64
+
+
+class tiscript_native_interface(ctypes.Structure):
+    """."""
+    _fields_ = [
+        ("create_vm", ctypes.c_void_p),
+        ]

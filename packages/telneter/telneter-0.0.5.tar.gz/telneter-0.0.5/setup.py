3d5ffca11149c9bf4d689c07f025232773393b65
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:56:10 +0000

    Add package telneter/0.0.5/telneter-0.0.5.tar.gz

---
+++ packages/telneter/telneter-0.0.5.tar.gz/telneter-0.0.5/setup.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+setup(
+    name     = 'telneter',
+    version  = '0.0.5',
+    packages = [''],
+    requires = ['python (>=2.5)'],
+    description  = 'Telnet interface for popular network devices',
+    author       = 'Pavel Polyakov',
+    author_email = 'polyakov.pavel.xyz@gmail.com',
+    url          = 'https://github.com/Pavel-Polyakov/telneter',
+    download_url = 'https://github.com/Pavel-Polyakov/telneter/archive/master',
+    license      = 'MIT License',
+    keywords     = 'telnet',
+    classifiers  = [
+        'Intended Audience :: Developers',
+        'Programming Language :: Python :: 2',
+    ],
+)

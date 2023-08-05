151fef1339a1acc049dea199c60295d05540d96d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:21 +0000

    Add package Ciw/0.2.7/Ciw-0.2.7.tar.gz

---
+++ packages/Ciw/Ciw-0.2.7.tar.gz/Ciw-0.2.7/ciw/__init__.py
@@ -0,0 +1,16 @@
+from __future__ import absolute_import
+
+from .auxiliary import *
+from .simulation import Simulation
+from .data_record import DataRecord
+from .server import Server
+from .individual import Individual
+from .arrival_node import ArrivalNode
+from .exit_node import ExitNode
+from .node import Node
+from .state_tracker import *
+from .exactnode import *
+from .import_params import *
+from .network import *
+
+__version__ = '0.2.7'
---
+++ packages/Ciw/Ciw-0.2.7.tar.gz/Ciw-0.2.7/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+with open('README.rst') as readme_file:
+    readme = readme_file.read()
+
+with open('CHANGES.rst') as changes_file:
+    changes = changes_file.read()
+
+with open('AUTHORS.rst') as authors_file:
+    authors = authors_file.read()
+
+setup(
+    name='Ciw',
+    version='0.2.7',
+    url='https://github.com/geraintpalmer/Ciw',
+    author='Geraint Palmer, Vincent Knight',
+    author_email='palmer.geraint@googlemail.com',
+    packages=['ciw'],
+    description='A discrete event simulation library for open queueing networks',
+    long_description=readme + '\n\n' + changes + '\n\n' + authors,
+    install_requires=["PyYAML", "networkx", "hypothesis", "numpy", "tqdm"]
+)

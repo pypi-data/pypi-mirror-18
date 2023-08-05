5afd2ef26b61a56c27431db68b800ebfbc356619
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:09 +0000

    Add package seriously/2.0.63/seriously-2.0.63.tar.gz

---
+++ packages/seriously/seriously-2.0.63.tar.gz/seriously-2.0.63/setup.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+"""Seriously - a Python-based golfing language"""
+
+from setuptools import setup, find_packages
+
+setup(
+    name='seriously',
+
+    version='2.0.63',
+
+    description='A Python-based golfing language',
+    long_description='Seriously is a Python-based golfing language. See the GitHub page for more details.',
+
+    url='https://github.com/Mego/Seriously',
+
+    author='Mego',
+
+    license='MIT',
+
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+    
+    install_requires = ['pycryptodome', 'stats'],
+    
+    packages = ['seriously', 'lib'],
+
+    keywords='codegolf recreational',
+    entry_points={
+        'console_scripts': [
+            'seriously=seriously:main',
+        ],
+    },
+)

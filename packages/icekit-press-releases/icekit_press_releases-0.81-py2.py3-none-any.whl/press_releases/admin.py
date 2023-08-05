c47ad8b781dd3b6be1624cc37e4078a1d8bbd7a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:35 +0000

    Add package icekit-press-releases/0.81/icekit_press_releases-0.81-py2.py3-none-any.whl

---
+++ packages/icekit-press-releases/icekit_press_releases-0.81-py2.py3-none-any.whl/press_releases/admin.py
@@ -0,0 +1,22 @@
+from django.contrib import admin
+from icekit.publishing.admin import PublishingAdmin
+
+from icekit.admin_mixins import FluentLayoutsMixin
+from . import models
+
+class PressReleaseAdmin(PublishingAdmin, FluentLayoutsMixin):
+    list_display = PublishingAdmin.list_display + ('released', 'modified',)
+    date_hierarchy = 'released'
+    list_filter = PublishingAdmin.list_filter + ('category',)
+    search_fields = ('title', 'id', 'slug',)
+    ordering = ('-released', )
+    prepopulated_fields = {'slug': ('title',)}
+
+
+class PressReleaseCategoryAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(models.PressRelease, PressReleaseAdmin)
+admin.site.register(models.PressReleaseCategory, PressReleaseCategoryAdmin)
+

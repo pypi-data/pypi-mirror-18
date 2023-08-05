4556355bfe8f81be18d0ebf70fb37144cf588da1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:55:35 +0000

    Add package moduleloader/0.1.10/moduleloader-0.1.10.tar.gz

---
+++ packages/moduleloader/moduleloader-0.1.10.tar.gz/moduleloader-0.1.10/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'moduleloader',
+  packages = ['moduleloader'], # this must be the same as the name above
+  version = '0.1.10',
+  description = 'Load Modules from external directories',
+  author = 'Sergio D Fdez',
+  author_email = 'sergiodfdez@gmail.com',
+  url = 'https://github.com/Sergiodfdez/python-module-loader', # use the URL to the github repo
+  download_url = 'https://github.com/Sergiodfdez/python-moduleloader/tarball/0.1.10', # I'll explain this in a second
+  keywords = ['module', 'load', 'external', 'directory'], # arbitrary keywords
+  classifiers = []
+)

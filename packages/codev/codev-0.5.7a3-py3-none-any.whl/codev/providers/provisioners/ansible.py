12db4f6124df1dcbb74a0d87d1755ad941b0eef2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:26 +0000

    Add package codev/0.5.7a3/codev-0.5.7a3-py3-none-any.whl

---
+++ packages/codev/codev-0.5.7a3-py3-none-any.whl/codev/providers/provisioners/ansible.py
@@ -0,0 +1,106 @@
+from codev.provisioner import Provisioner
+from codev.settings import BaseSettings
+from codev.isolator import Isolator
+# from os import environ
+import configparser
+
+from logging import getLogger
+logger = getLogger(__name__)
+
+
+class AnsibleProvisionerSettings(BaseSettings):
+    @property
+    def playbook(self):
+        return self.data.get('playbook', None)
+
+    @property
+    def version(self):
+        return self.data.get('version', None)
+
+    @property
+    def extra_vars(self):
+        return self.data.get('extra_vars', {})
+
+    @property
+    def env_vars(self):
+        return self.data.get('env_vars', {})
+
+
+class AnsibleProvisioner(Provisioner):
+    provider_name = 'ansible'
+    settings_class = AnsibleProvisionerSettings
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.isolator = Isolator(
+            'virtualenv',
+            self.performer,
+            settings_data=dict(python='2'),
+            ident='codev_ansible')
+
+    def install(self):
+        # TODO requirements - python-dev, python-virtualenv
+        self.isolator.create()
+        self.isolator.execute('pip install setuptools')
+        self.isolator.execute('pip install --upgrade markupsafe paramiko PyYAML Jinja2 httplib2 six ecdsa==0.11')
+
+        version_add = ''
+        if self.settings.version:
+            version_add = '==%s' % self.settings.version
+        self.isolator.execute('pip install --upgrade ansible%s' % version_add)
+
+    def run(self, machines_groups, info):
+        inventory = configparser.ConfigParser(allow_no_value=True, delimiters=('',))
+        for machines_group, machines in machines_groups.items():
+            inventory.add_section(machines_group)
+            for machine in machines:
+                # ansible node additional requirements
+                machine.install_packages('python')
+                inventory.set(machines_group, machine.ip, '')
+
+        inventory_filepath = '/tmp/codev.ansible.inventory'
+
+        with open(inventory_filepath, 'w+') as inventoryfile:
+            inventory.write(inventoryfile)
+
+        template_vars = {
+            'source_directory': self.performer.execute('pwd')
+        }
+        template_vars.update(info)
+
+        if self.settings.extra_vars:
+            extra_vars = ' --extra-vars "{joined_extra_vars}"'.format(
+                joined_extra_vars=' '.join(
+                    [
+                        '{key}={value}'.format(
+                            key=key,
+                            value=value.format(**template_vars) if isinstance(value, str) else value
+                        ) for key, value in self.settings.extra_vars.items()
+                    ]
+                )
+            )
+        else:
+            extra_vars = ''
+
+        if self.settings.env_vars:
+            env_vars = '{joined_env_vars} '.format(
+                joined_env_vars=' '.join(
+                    [
+                        '{key}="{value}"'.format(
+                            key=key,
+                            value=value.format(**template_vars) if isinstance(value, str) else value
+                        ) for key, value in self.settings.env_vars.items()
+                    ]
+                )
+            )
+        else:
+            env_vars = ''
+
+        self.isolator.execute('{env_vars}ansible-playbook -i {inventory} {playbook}{extra_vars}'.format(
+            inventory=inventory_filepath,
+            playbook=self.settings.playbook,
+            extra_vars=extra_vars,
+            env_vars=env_vars
+        ))
+
+        return True
---
+++ packages/codev/codev-0.5.7a3-py3-none-any.whl/codev/providers/sources/git.py
@@ -0,0 +1,93 @@
+from codev.source import Source
+from git import Repo
+
+
+class Git(object):
+    def __init__(self, version=None, commit=None, repository_url=None, directory=None):
+        self.branch = version
+        self.tag = version
+        self.commit = commit
+        self.directory = directory
+        self.repository = Repo()
+        self.repository_url = repository_url or self.repository.remotes.origin.url
+
+    def install(self, performer):
+        """
+        Install project to directory
+
+        :param performer:
+        :type performer: codev.performer.BasePerformer
+        :return:
+        """
+        # TODO requirements
+        # performer.install_packages('git')
+
+        # TODO checking fingerprints instead of copying known_hosts
+        # http://serverfault.com/questions/132970/can-i-automatically-add-a-new-host-to-known-hosts
+        # http://serverfault.com/questions/447028/non-interactive-git-clone-ssh-fingerprint-prompt
+        # http://unix.stackexchange.com/questions/94448/how-to-add-an-ip-range-to-known-hosts
+        # https://help.github.com/articles/what-are-github-s-ssh-key-fingerprints/
+        # ssh-keyscan -t rsa,dsa github.com 2> /dev/null > /tmp/key && ssh-keygen -lf /tmp/key
+        # ssh-keygen -H -F github.com
+        # github.com,192.30.252.*,192.30.253.*,192.30.254.*,192.30.255.* ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEAq2A7hRGmdnm9tUDbO9IDSwBK6TbQa+PXYPCPy6rbTrTtw7PHkccKrpp0yVhp5HdEIcKr6pLlVDBfOLX9QUsyCOV0wzfjIJNlGEYsdlLJizHhbn2mUjvSAHQqZETYP81eFzLQNnPHt4EVVUh7VfDESU84KezmD5QlWpXLmvU31/yMf+Se8xhHTvKSCZIFImWwoG6mbUoWf9nzpIoaSjB+weqqUUmpaaasXVal72J+UX2B+2RPW3RcT0eOzQgqlJL3RKrTJvdsjE3JEAvGq3lGHSZXy28G3skua2SmVi/w4yCE6gbODqnTWlg7+wC604ydGXA8VJiS5ap43JXiUFFAaQ==
+
+        performer.execute('mkdir -p ~/.ssh')
+        performer.send_file('~/.ssh/known_hosts', '~/.ssh/known_hosts')
+
+        if self.directory:
+            if performer.check_execute('[ -d {directory} ]'.format(directory=self.directory)):
+                performer.check_execute('rm -rf {directory}'.format(directory=self.directory))
+
+        if self.directory:
+            directory = self.directory
+        else:
+            directory = '.'
+
+        if self.branch or self.tag:
+
+            performer.execute('git clone {url} --branch {object} --single-branch {directory}'.format(
+                url=self.repository_url,
+                directory=directory,
+                object=self.branch or self.tag
+            ))
+        elif self.commit:
+            performer.execute('git init {directory}'.format(directory=directory))
+            with performer.change_directory(directory):
+                performer.execute('git remote add origin {url}'.format(url=self.repository_url))
+                performer.execute('git fetch origin {commit}'.format(commit=self.commit))
+                performer.execute('git reset --hard FETCH_HEAD')
+
+
+class GitSource(Source):
+    provider_name = 'git'
+
+    def __init__(self, *args, **kwargs):
+        self.git = Git(directory='directory')
+        super().__init__(*args, **kwargs)
+
+    def process_options(self, options):
+        if not options:
+            raise ValueError('Repository options must be specified.')
+
+        remote = self.git.repository.remote()
+        remote.fetch()
+
+        # branch
+        if options in remote.refs:
+            self.branch = options
+
+        # tag
+        elif options in self.git.repository.tags:
+            self.tag = options
+
+        # commit
+        else:
+            for commit in self.git.repository.iter_commits():
+                if options == commit:
+                    self.commit = commit
+            else:
+                raise ValueError("Branch, tag or commit '{options}' not found.".format(options=options))
+
+        return self.branch or self.tag or self.commit
+
+

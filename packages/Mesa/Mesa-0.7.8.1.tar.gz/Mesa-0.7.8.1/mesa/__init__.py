36ddcab9e7f68e9afb083aea2565e5eabc02095f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:36 +0000

    Add package Mesa/0.7.8.1/Mesa-0.7.8.1.tar.gz

---
+++ packages/Mesa/Mesa-0.7.8.1.tar.gz/Mesa-0.7.8.1/mesa/__init__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Mesa Agent-Based Modeling Framework
+
+Core Objects: Model, and Agent.
+
+"""
+
+from .model import Model
+from .agent import Agent
+
+
+__all__ = ["Model", "Agent"]
+
+__title__ = 'mesa'
+__version__ = '0.7.8.1'
+__license__ = 'Apache 2.0'
+__copyright__ = 'Copyright 2015 Project Mesa Team'

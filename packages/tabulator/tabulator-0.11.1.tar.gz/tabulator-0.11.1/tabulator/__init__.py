032bfbb9fca8032094ef02f7f18325ca84fee111
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:09:10 +0000

    Add package tabulator/0.11.1/tabulator-0.11.1.tar.gz

---
+++ packages/tabulator/tabulator-0.11.1.tar.gz/tabulator-0.11.1/tabulator/__init__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
+
+from .stream import Stream
+from . import exceptions
+
+# Deprecated
+from .topen import topen
+from .stream import Stream as Table

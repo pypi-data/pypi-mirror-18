7a0594054d4fddca0d0d35f7a3148c334db98b72
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:53 +0000

    Add package lineprotocol/0.1.dev0/lineprotocol-0.1.dev0.tar.gz

---
+++ packages/lineprotocol/lineprotocol-0.1.dev0.tar.gz/lineprotocol-0.1.dev0/setup.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+setup(
+    name='lineprotocol',
+    version='0.1.dev0',
+    description='Export data to InfluxDB\'s Line Protocol format',
+    url='https://github.com/qn7o/lineprotocol',
+    author='Antonin Bourguignon',
+    author_email='antonin.bourguignon@gmail.com',
+    license='MIT',
+    packages=['lineprotocol'],
+    zip_safe=False
+)

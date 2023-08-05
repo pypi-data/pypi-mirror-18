f20585265ed9142575688ad5047c46ea0d59faea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:59 +0000

    Add package exp10it/2.3.25/exp10it-2.3.25.tar.gz

---
+++ packages/exp10it/exp10it-2.3.25.tar.gz/exp10it-2.3.25/newscript.py
@@ -0,0 +1,22 @@
+#############################################################
+###
+###  mm,            .m   .m,   W
+### ]""W,           PW   W"W   "   ][
+###    d['W W`]bWb   W  ][ ][ WW  ]WWW
+###  ]WW  ]W[ ]P T[  W  ][W][  W   ][
+###    T[ .W, ][ ][  W  ][ ][  W   ][
+### ]mmW` d"b ]WmW`.mWm, WmW .mWm, ]bm
+###  ""` '" "`]["` '"""` '"` '"""`  ""
+###           ][
+###
+###
+### name: newscript.py
+### function: write new script easily
+### date: 2016-08-05
+### author: quanyechavshuo
+### blog: https://3xp10it.github.io
+#############################################################
+# -*- coding: utf-8 -*-
+from exp10it import *
+
+newscript()

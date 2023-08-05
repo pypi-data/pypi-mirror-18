9ae4973c64f3522042ce36122ec84265dfb6c718
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:20 +0000

    Add package ovp-organizations/0.1.1/ovp-organizations-0.1.1.tar.gz

---
+++ packages/ovp-organizations/ovp-organizations-0.1.1.tar.gz/ovp-organizations-0.1.1/setup.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+
+setup(
+    name='ovp-organizations',
+    version='0.1.1',
+    author=u'Atados',
+    author_email='arroyo@atados.com.br',
+    packages=find_packages(),
+    url='https://github.com/OpenVolunteeringPlatform/django-ovp-organizations',
+    download_url = 'https://github.com/OpenVolunteeringPlatform/django-ovp-organizations/tarball/0.1.1',
+    license='AGPL',
+    description='This module has core functionality for' + \
+                ' ovp organization, such as: creating, listing,' + \
+                ' editing and retrieving.',
+    long_description=open('README.rst', encoding='utf-8').read(),
+    zip_safe=False,
+    install_requires = [
+      'Django>=1.10.1,<1.11.0',
+      'djangorestframework>=3.4.7,<3.5.0',
+      'codecov>=2.0.5,<2.1.0',
+      'coverage>=4.2,<4.3.0',
+      'ovp-users>=1.0.9,<2.0.0',
+      'ovp-core>=0.1.4,<1.0.0'
+    ]
+)

24077c920c9a633c86620ce462de37f7e0d07509
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:55:03 +0000

    Add package vigil/0.1.3/vigil-0.1.3.tar.gz

---
+++ packages/vigil/vigil-0.1.3.tar.gz/vigil-0.1.3/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+import pypandoc
+
+setup(
+    name='vigil',
+    version='0.1.3',
+    author='David Dworken',
+    author_email='david@daviddworken.com',
+    packages=['vigil'],
+    url='http://pypi.python.org/pypi/vigil/',
+    license='LICENSE.txt',
+    description='vigil... as a python package',
+    long_description=pypandoc.convert("README.md", "rst"),
+)

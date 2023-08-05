cc29846dfa30b4f1b0affbc6acf18aacaf768e63
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:05:53 +0000

    Add package Slowloris_IMproved/0.3.2/Slowloris_IMproved-0.3.2.tar.gz

---
+++ packages/Slowloris_IMproved/Slowloris_IMproved-0.3.2.tar.gz/Slowloris_IMproved-0.3.2/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name="Slowloris_IMproved",
+    py_modules=["sim"],
+    entry_points={"console_scripts": ["sim=sim:main"]},
+    version="0.3.2",
+    description="IMproved low bandwidth DoS tool. Slowloris rewrite in Python, original by Gokberk Yaltirakli.",
+    author="Robin Kanters",
+    author_email="robin@robinkanters.nl",
+    url="https://github.com/robinkanters/slowloris",
+    keywords=["dos", "http", "slowloris"]
+)

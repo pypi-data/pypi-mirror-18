49c0992535a7ad96549540721a55bc51d4458705
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:09:09 +0000

    Add package nesterMM/1.0.0/nesterMM-1.0.0.zip

---
+++ packages/nesterMM/nesterMM-1.0.0.zip/nesterMM-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name  = 'nesterMM',
+    version='1.0.0',
+    py_modules = ['nester'],
+    author='lfm',
+    author_email='347023623@qq.com',
+    url='http://www.baidu.com',
+    description='A simple printer of nested list',
+)

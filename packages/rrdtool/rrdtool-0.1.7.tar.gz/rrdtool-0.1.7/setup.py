3dadccc34a4d89cabaadc1c7fb523d22a56c7314
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:08:08 +0000

    Add package rrdtool/0.1.7/rrdtool-0.1.7.tar.gz

---
+++ packages/rrdtool/rrdtool-0.1.7.tar.gz/rrdtool-0.1.7/setup.py
@@ -0,0 +1,32 @@
+try:
+    from setuptools import setup
+    from setuptools.extension import Extension
+except ImportError:
+    from distutils.core import setup, Extension
+
+
+def main():
+    module = Extension('rrdtool',
+                       sources=['rrdtoolmodule.c'],
+                       include_dirs=['/usr/local/include'],
+                       library_dirs=['/usr/local/lib'],
+                       libraries=['rrd'])
+
+    kwargs = dict(
+        name='rrdtool',
+        version='0.1.7',
+        description='Python bindings for rrdtool',
+        keywords=['rrdtool'],
+        author='Christian Kroeger, Hye-Shik Chang',
+        author_email='commx@commx.ws',
+        license='LGPL',
+        url='https://github.com/commx/python-rrdtool',
+        ext_modules=[module],
+        test_suite="tests"
+    )
+
+    setup(**kwargs)
+
+
+if __name__ == '__main__':
+    main()

111c5b23b57b9ad35ffdc11ceb3f5662f3835e5f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:17 +0000

    Add package sdklib/1.3/sdklib-1.3.tar.gz

---
+++ packages/sdklib/sdklib-1.3.tar.gz/sdklib-1.3/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+from sdklib import __version__
+
+EXCLUDE_FROM_PACKAGES = []
+
+REQUIRES = ["urllib3 >= 1.10"]
+
+
+setup(
+    name='sdklib',
+    version=__version__,
+    description='SDK helper library',
+    author='Ivan Martin',
+    author_email='ivanprjcts@gmail.com',
+    url='https://github.com/ivanprjcts/sdklib',
+    install_requires=REQUIRES,
+    license='BSD',
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+    packages=find_packages(exclude=EXCLUDE_FROM_PACKAGES),
+    zip_safe=False
+)

44ebad450416155558e6ea0a5a61942860932b73
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:16 +0000

    Add package scrapinghub-entrypoint-scrapy/0.8.9/scrapinghub-entrypoint-scrapy-0.8.9.tar.gz

---
+++ packages/scrapinghub-entrypoint-scrapy/scrapinghub-entrypoint-scrapy-0.8.9.tar.gz/scrapinghub-entrypoint-scrapy-0.8.9/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='scrapinghub-entrypoint-scrapy',
+    version='0.8.9',
+    license='BSD',
+    description='Scrapy entrypoint for Scrapinghub job runner',
+    long_description=open('README.rst').read(),
+    packages=find_packages(),
+    install_requires=[
+        'Scrapy>=1.0',
+        'hubstorage>=0.23.2',
+    ],
+    entry_points={
+        'console_scripts': [
+            'start-crawl = sh_scrapy.crawl:main',
+            'list-spiders = sh_scrapy.crawl:list_spiders',
+        ],
+    },
+)

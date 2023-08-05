bc87d993fb4f9f32472dfce2b2d29ab8a768a75c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:07:11 +0000

    Add package pyjo/0.6.1/pyjo-0.6.1.tar.gz

---
+++ packages/pyjo/pyjo-0.6.1.tar.gz/pyjo-0.6.1/pyjo/fields/listfield.py
@@ -0,0 +1,31 @@
+from pyjo.fields.field import Field
+
+
+class ListField(Field):
+    def __init__(self, subtype=None, **kwargs):
+        """
+        :type model: T
+        :rtype: list[T]
+        """
+        super(ListField, self).__init__(type=list, **kwargs)
+        self._subtype = subtype or object
+
+    def to_pyjson(self, value):
+        if self._to_pyjson is not None:
+            return self._to_pyjson(value)
+        res = []
+        for x in value:
+            if hasattr(x, 'to_pyjson'):
+                x = x.to_pyjson()
+            res.append(x)
+        return res
+
+    def from_pyjson(self, value):
+        if self._from_pyjson is not None:
+            return self._from_pyjson(value)
+        res = []
+        for x in value:
+            if hasattr(self._subtype, 'from_pyjson'):
+                x = self._subtype.from_pyjson(x)
+            res.append(x)
+        return res

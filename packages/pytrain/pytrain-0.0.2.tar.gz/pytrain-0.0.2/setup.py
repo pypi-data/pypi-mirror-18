044438433cd1aca76a9f3401444d08db69c2b2f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:09:05 +0000

    Add package pytrain/0.0.2/pytrain-0.0.2.tar.gz

---
+++ packages/pytrain/pytrain-0.0.2.tar.gz/pytrain-0.0.2/setup.py
@@ -0,0 +1,21 @@
+from distutils.core import setup
+
+setup(
+        name='pytrain',
+        version='0.0.2',
+        packages = ['pytrain'],
+        author ='becxer',
+        author_email='becxer87@gmail.com',
+        url = 'https://github.com/becxer/pytrain',
+        description ='Machinelearning library for python',
+        long_description ='Machinelearning library for python',
+        license='MIT',
+        install_requires=['numpy'],
+        classifiers=[
+                'Development Status :: 3 - Alpha',
+                'Topic :: Software Development :: Build Tools',
+                'License :: OSI Approved :: MIT License',
+                'Programming Language :: Python :: 2.7',
+        ]
+)
+

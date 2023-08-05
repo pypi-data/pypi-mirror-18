92d596dc6c7b01adb9c646a4c1403ad94df89e69
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:03 +0000

    Add package tabulator/0.9.0/tabulator-0.9.0-py2.py3-none-any.whl

---
+++ packages/tabulator/tabulator-0.9.0-py2.py3-none-any.whl/tabulator/loaders/file.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+from __future__ import division
+from __future__ import print_function
+from __future__ import absolute_import
+from __future__ import unicode_literals
+
+import io
+from .. import exceptions
+from .. import helpers
+from . import api
+
+
+# Module API
+
+class FileLoader(api.Loader):
+    """Loader to load source from filesystem.
+    """
+
+    # Public
+
+    options = []
+
+    def load(self, source, encoding, mode):
+
+        # Prepare source
+        scheme = 'file://'
+        if source.startswith(scheme):
+            source = source.replace(scheme, '', 1)
+
+        # Prepare bytes
+        bytes = io.open(source, 'rb')
+
+        # Prepare encoding
+        encoding = helpers.detect_encoding(bytes, encoding)
+
+        # Return or raise
+        if mode == 'b':
+            return bytes
+        elif mode == 't':
+            chars = io.TextIOWrapper(bytes, encoding)
+            return chars
+        else:
+            message = 'Mode %s is not supported' % mode
+            raise exceptions.LoadingError(message)
---
+++ packages/tabulator/tabulator-0.9.0-py2.py3-none-any.whl/tabulator/loaders/stream.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+from __future__ import division
+from __future__ import print_function
+from __future__ import absolute_import
+from __future__ import unicode_literals
+
+import io
+from .. import exceptions
+from .. import helpers
+from . import api
+
+
+# Module API
+
+class StreamLoader(api.Loader):
+    """Loader to load source from file-like byte stream.
+    """
+
+    # Public
+
+    options = []
+
+    def load(self, source, encoding, mode):
+
+        # Raise if in text mode
+        if hasattr(source, 'encoding'):
+            message = 'Only byte streams are supported.'
+            raise exceptions.LoadingError(message)
+
+        # Prepare bytes
+        bytes = source
+
+        # Prepare encoding
+        encoding = helpers.detect_encoding(bytes, encoding)
+
+        # Return or raise
+        if mode == 'b':
+            return bytes
+        elif mode == 't':
+            chars = io.TextIOWrapper(bytes, encoding)
+            return chars
+        else:
+            message = 'Mode %s is not supported' % mode
+            raise exceptions.LoadingError(message)

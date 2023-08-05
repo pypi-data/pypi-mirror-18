698f72368fb23d4920f7a5498067dac23da1e5dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:05 +0000

    Add package jupyter_utils/1.2.1/jupyter_utils-1.2.1-py2.py3-none-any.whl

---
+++ packages/jupyter_utils/jupyter_utils-1.2.1-py2.py3-none-any.whl/jupyter_utils/tests/test_jupyter_utils.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+test_jupyter_utils
+----------------------------------
+Tests for `jupyter_utils` module.
+"""
+
+import sys
+import unittest
+
+
+class TestJupyter_utils(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_000_something(self):
+        pass
+
+if __name__ == '__main__':
+    sys.exit(unittest.main())

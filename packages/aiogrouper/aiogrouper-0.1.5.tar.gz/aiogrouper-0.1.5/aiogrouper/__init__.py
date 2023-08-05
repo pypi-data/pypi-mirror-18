2a1d0b39290b07a16bcec357ba99f4c61dded591
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:57:33 +0000

    Add package aiogrouper/0.1.5/aiogrouper-0.1.5.tar.gz

---
+++ packages/aiogrouper/aiogrouper-0.1.5.tar.gz/aiogrouper-0.1.5/aiogrouper/__init__.py
@@ -0,0 +1,28 @@
+from pkg_resources import get_distribution, DistributionNotFound
+import os.path
+
+__distribution__ = 'aiogrouper'
+
+# Kudos to http://stackoverflow.com/a/17638236
+
+try:
+    _dist = get_distribution(__distribution__)
+    # Normalize case for Windows systems
+    _dist_loc = os.path.normcase(_dist.location)
+    _here = os.path.normcase(__file__)
+    if not _here.startswith(os.path.join(_dist_loc, __distribution__)):
+        # not installed, but there is another version that *is*
+        raise DistributionNotFound
+except DistributionNotFound:
+    __version__ = 'dev'
+else:
+    __version__ = _dist.version
+
+del os, get_distribution, DistributionNotFound
+del _dist, _dist_loc, _here
+
+from .grouper import *
+from .group import *
+from .query import *
+from .stem import *
+from .subject import *
---
+++ packages/aiogrouper/aiogrouper-0.1.5.tar.gz/aiogrouper-0.1.5/aiogrouper/subject.py
@@ -0,0 +1,43 @@
+__all__ = ['Subject', 'everyone']
+
+class Subject:
+    def __init__(self, *, id=None, identifier=None, source=None, name=None, grouper=None):
+        assert id or identifier
+        self.id = id
+        self.identifier, self.source = identifier, source
+        self.name = name
+        self.grouper = grouper
+
+    @classmethod
+    def from_json(cls, value, grouper=None):
+        if isinstance(value, dict):
+            return cls(id=value['id'],
+                       identifier=value.get('identifierLookup'),
+                       source=value.get('sourceId'),
+                       name=value.get('name'),
+                       grouper=grouper)
+        elif isinstance(value, cls):
+            return value
+        raise ValueError
+
+    def to_json(self, lookup=False):
+        if lookup:
+            data = {}
+            if self.id:
+                data['subjectId'] = self.id
+            if self.identifier:
+                data['subjectIdentifier'] = self.identifier
+            if self.source:
+                data['subjectSourceId'] = self.source
+            return data
+        data = {}
+        if self.id: data['id'] = self.id
+        if self.source: data['sourceId'] = self.source
+        if self.name: data['name'] = self.name
+        return data
+
+    def __str__(self):
+        return '<Subject {}: {!r}>'.format(self.id, self.name)
+    __repr__ = __str__
+
+everyone = Subject(id='GrouperAll', source='g:isa')

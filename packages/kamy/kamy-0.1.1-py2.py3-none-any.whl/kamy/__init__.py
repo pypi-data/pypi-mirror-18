3df669749634e4253de780e0f6f156c3d81d961a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:34 +0000

    Add package kamy/0.1.1/kamy-0.1.1-py2.py3-none-any.whl

---
+++ packages/kamy/kamy-0.1.1-py2.py3-none-any.whl/kamy/__init__.py
@@ -0,0 +1,10 @@
+"""This is my first package"""
+
+__version__ = "0.1.1"
+
+
+from . import commands as cmd
+
+
+def hello_world():
+    print("Hello world!")
---
+++ packages/kamy/kamy-0.1.1-py2.py3-none-any.whl/kamy/commands.py
@@ -0,0 +1,5 @@
+
+
+
+def testing():
+    print("hello, this is test!")

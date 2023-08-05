1dc13fe5c75bd0b45ff827caf2ec0315d7fde1f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:57:18 +0000

    Add package fabric-anaconda/0.0.3/fabric-anaconda-0.0.3.tar.gz

---
+++ packages/fabric-anaconda/fabric-anaconda-0.0.3.tar.gz/fabric-anaconda-0.0.3/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+try:
+    import pypandoc
+    long_description = pypandoc.convert('README.md', 'rst')
+except(IOError, ImportError):
+    long_description = open('README.md').read()
+
+setup(
+    name="fabric-anaconda",
+    version="0.0.3",
+    author='Barnaby Gray',
+    author_email='barnaby@pickle.me.uk',
+    url='http://pypi.python.org/pypi/fabric-anaconda/',
+    description=(
+        "Some additional functions for working with anaconda environments "
+        "in Fabric."
+    ),
+    long_description=long_description,
+    packages=find_packages(),
+    install_requires=[
+        'Fabric'
+    ],
+    data_files=[('', ['README.md'])]
+)

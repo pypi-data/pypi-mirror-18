f6d4651ec5d7962e046956ea08db87abe0051c76
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:14 +0000

    Add package docraptor/1.2.0/docraptor-1.2.0.tar.gz

---
+++ packages/docraptor/docraptor-1.2.0.tar.gz/docraptor-1.2.0/setup.py
@@ -0,0 +1,59 @@
+# coding: utf-8
+
+import sys
+from setuptools import setup, find_packages
+
+NAME = "docraptor"
+VERSION = "1.2.0"
+
+
+
+# To install the library, run the following
+#
+# python setup.py install
+#
+# prerequisite: setuptools
+# http://pypi.python.org/pypi/setuptools
+
+REQUIRES = ["urllib3 >= 1.10", "six >= 1.9", "certifi", "python-dateutil"]
+
+setup(
+    name=NAME,
+    version=VERSION,
+    description="A wrapper for the DocRaptor HTML to PDF/XLS service.",
+    author="Elijah Miller",
+    author_email="elijah.miller@gmail.com",
+    maintainer="DocRaptor Support",
+    maintainer_email="support@docraptor.com",
+    url="https://github.com/docraptor/docraptor-python",
+    keywords=["DocRaptor", "API", "PDF", "XLS", "XLSX", "HTML"],
+    classifiers=[ # see https://pypi.python.org/pypi?%3Aaction=list_classifiers
+        "Development Status :: 5 - Production/Stable",
+
+        "Environment :: Web Environment",
+
+        "Intended Audience :: Developers",
+        "Intended Audience :: Information Technology",
+
+        "License :: OSI Approved :: Apache Software License",
+
+        "Operating System :: Microsoft :: Windows",
+        "Operating System :: POSIX",
+        "Operating System :: Unix",
+
+        "Topic :: Internet :: WWW/HTTP :: Dynamic Content",
+        "Topic :: Multimedia :: Graphics :: Presentation",
+        "Topic :: Office/Business",
+        "Topic :: Office/Business :: Financial :: Spreadsheet",
+
+        "Topic :: Printing",
+        "Topic :: Text Processing :: Markup :: HTML",
+    ],
+    license="Apache V2.0 License",
+    install_requires=REQUIRES,
+    packages=find_packages(),
+    include_package_data=True,
+    long_description="""\
+    A native client library for the DocRaptor HTML to PDF/XLS service.
+    """
+)

448cc858b00f4125030ce7bb97f33f5a94a2c83d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:09:00 +0000

    Add package HeadFirstTest1/1.1.0/HeadFirstTest1-1.1.0.tar.gz

---
+++ packages/HeadFirstTest1/HeadFirstTest1-1.1.0.tar.gz/HeadFirstTest1-1.1.0/setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+setup(
+    name = 'HeadFirstTest1',
+    version = '1.1.0',
+    py_modules = ['nester'],
+    author = 'westchen',
+    author_email = 'westchennn@gmail.com',
+    description = 'nested list v2'
+    )

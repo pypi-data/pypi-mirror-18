681d40780714d38e88d037d7f39187d5dd478c00
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:56 +0000

    Add package arvados-cwl-runner/1.0.20161025170023/arvados-cwl-runner-1.0.20161025170023.tar.gz

---
+++ packages/arvados-cwl-runner/arvados-cwl-runner-1.0.20161025170023.tar.gz/arvados-cwl-runner-1.0.20161025170023/arvados_cwl/_version.py
@@ -0,0 +1,1 @@
+__version__ = '4b037a38dbe848a77224dd05dd035736b5d0c0c1'
---
+++ packages/arvados-cwl-runner/arvados-cwl-runner-1.0.20161025170023.tar.gz/arvados-cwl-runner-1.0.20161025170023/arvados_cwl/arvdocker.py
@@ -0,0 +1,47 @@
+import logging
+import sys
+
+import cwltool.docker
+from cwltool.errors import WorkflowException
+import arvados.commands.keepdocker
+
+
+logger = logging.getLogger('arvados.cwl-runner')
+
+def arv_docker_get_image(api_client, dockerRequirement, pull_image, project_uuid):
+    """Check if a Docker image is available in Keep, if not, upload it using arv-keepdocker."""
+
+    if "dockerImageId" not in dockerRequirement and "dockerPull" in dockerRequirement:
+        dockerRequirement["dockerImageId"] = dockerRequirement["dockerPull"]
+
+    sp = dockerRequirement["dockerImageId"].split(":")
+    image_name = sp[0]
+    image_tag = sp[1] if len(sp) > 1 else None
+
+    images = arvados.commands.keepdocker.list_images_in_arv(api_client, 3,
+                                                            image_name=image_name,
+                                                            image_tag=image_tag)
+
+    if not images:
+        # Fetch Docker image if necessary.
+        cwltool.docker.get_image(dockerRequirement, pull_image)
+
+        # Upload image to Arvados
+        args = ["--project-uuid="+project_uuid, image_name]
+        if image_tag:
+            args.append(image_tag)
+        logger.info("Uploading Docker image %s", ":".join(args[1:]))
+        try:
+            arvados.commands.keepdocker.main(args, stdout=sys.stderr)
+        except SystemExit:
+            raise WorkflowException()
+
+        images = arvados.commands.keepdocker.list_images_in_arv(api_client, 3,
+                                                                image_name=image_name,
+                                                                image_tag=image_tag)
+
+    if not images:
+        raise WorkflowException("Could not find Docker image %s:%s" % (image_name, image_tag))
+
+    pdh = api_client.collections().get(uuid=images[0][0]).execute()["portable_data_hash"]
+    return pdh

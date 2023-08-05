66ab39b0d38a564d6a25695836588348bcbaa733
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:36 +0000

    Add package reflectutils/0.24/reflectutils-0.24.tar.gz

---
+++ packages/reflectutils/reflectutils-0.24.tar.gz/reflectutils-0.24/setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+from distutils.core import setup
+
+setup(name='reflectutils',
+    description='Reflection utilities for python',
+    version='0.24',
+    author='Manhattan Engineering Inc.',
+    packages=['reflectutils'],
+    package_dir={'reflectutils': '.'},
+)

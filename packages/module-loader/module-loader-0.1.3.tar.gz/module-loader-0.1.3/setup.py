c9fa3e0a63f5ef172ae535de5f097e4224066b8f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:26 +0000

    Add package module-loader/0.1.3/module-loader-0.1.3.tar.gz

---
+++ packages/module-loader/module-loader-0.1.3.tar.gz/module-loader-0.1.3/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'module-loader',
+  packages = ['module-loader'], # this must be the same as the name above
+  version = '0.1.3',
+  description = 'Load Modules from external directories',
+  author = 'Sergio D Fdez',
+  author_email = 'sergiodfdez@gmail.com',
+  url = 'https://github.com/Sergiodfdez/python-module-loader', # use the URL to the github repo
+  download_url = 'https://github.com/Sergiodfdez/python-module-loader/tarball/0.1.3', # I'll explain this in a second
+  keywords = ['module', 'load', 'external', 'directory'], # arbitrary keywords
+  classifiers = []
+)

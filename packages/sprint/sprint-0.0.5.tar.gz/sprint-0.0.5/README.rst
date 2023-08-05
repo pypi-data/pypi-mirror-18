102b44722839ac4696e77963bad5a084c7a08e89
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:46 +0000

    Add package sprint/0.0.5/sprint-0.0.5.tar.gz

---
+++ packages/sprint/sprint-0.0.5.tar.gz/sprint-0.0.5/README.rst
@@ -0,0 +1,7 @@
+#SPRINT
+
+#Feng Zhang (15110700005@fudan.edu.cn)
+
+#Please use pip to install sprint  ("pip install -e .")
+
+#Please read pipeline files in http://sprint.tianlab.cn
---
+++ packages/sprint/sprint-0.0.5.tar.gz/sprint-0.0.5/sprint/__init__.py
@@ -0,0 +1,22 @@
+# coding: utf-8
+import re
+from tools_fq import *
+from tools_sam import *
+from tools_bed import *
+from tools_zf import *
+
+def main():
+	"""Entry point for the application script"""
+	print("SPRINT-0.0.1")
+
+
+
+
+
+
+
+
+
+
+
+

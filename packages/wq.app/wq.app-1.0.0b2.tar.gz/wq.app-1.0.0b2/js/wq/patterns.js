f28f1ea9cdb08ec0b2baaa6ce590b476944953ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:07:13 +0000

    Add package wq.app/1.0.0b2/wq.app-1.0.0b2.tar.gz

---
+++ packages/wq.app/wq.app-1.0.0b2.tar.gz/wq.app-1.0.0b2/js/wq/patterns.js
@@ -0,0 +1,65 @@
+/*
+ * wq.app 1.0.0b2 - wq/patterns.js
+ * wq/app.js plugin to handle dynamically adding nested forms
+ * (c) 2016, S. Andrew Sheppard
+ * https://wq.io/license
+ */
+
+define(['jquery', 'wq/template'], function($, tmpl) {
+
+var patterns = {
+    'name': 'patterns'
+};
+
+var _templates, _pageContext;
+
+patterns.init = function(conf) {
+    _templates = (
+        conf && conf.templates ||
+        this.app.config.template.templates
+    );
+};
+
+patterns.context = function(context) {
+    _pageContext = context;
+};
+
+patterns.run = function($page, routeInfo) {
+    $page.find('button[data-wq-action=addattachment]').click(add);
+    function add(evt) {
+        var $button = $(evt.target),
+            section = $button.data('wq-section'),
+            count = $page.find('.section-' + section).length;
+        patterns.addAttachment(
+            routeInfo.page, section, count, $button, routeInfo.mode
+        );
+    }
+};
+
+patterns.addAttachment = function(page, section, index, $button, mode) {
+    var template = _templates[page + '_' + (mode ? mode : 'edit')],
+        pattern = '{{#' + section + '}}([\\s\\S]+){{/' + section + '}}',
+        match, $attachment, context;
+    if (!template) {
+         return;
+    }
+    match = template.match(pattern);
+    if (!match) {
+        return;
+    }
+
+    context = {
+        '@index': index
+    };
+    for (var key in _pageContext) {
+        context[key.replace(section + '.', '')] = _pageContext[key];
+    }
+    $attachment = $(tmpl.render(match[1], context));
+    $button.parents('li').before($attachment);
+    $attachment.enhanceWithin();
+    $button.parents('ul').listview('refresh');
+};
+
+return patterns;
+
+});

95079b9097570436ce6499ed48f9d95f0a534e03
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:48 +0000

    Add package limix-lsf/1.0.1/limix_lsf-1.0.1.tar.gz

---
+++ packages/limix-lsf/limix_lsf-1.0.1.tar.gz/limix_lsf-1.0.1/limix_lsf/config.py
@@ -0,0 +1,8 @@
+import os
+import configparser
+
+def stdoe_folder():
+    home = os.path.join(os.path.expanduser('~'))
+    conf = configparser.ConfigParser()
+    conf.read(os.path.join(home, '.config', 'lsf', 'config'))
+    return conf.get('default', 'stdoe_folder')

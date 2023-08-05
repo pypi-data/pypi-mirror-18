70ee29e0b64f046161a8eeb03d09cc2e64fe8b93
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:08:52 +0000

    Add package evento/1.0.1/evento-1.0.1.tar.gz

---
+++ packages/evento/evento-1.0.1.tar.gz/evento-1.0.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='evento',
+      version='1.0.1',
+      description='Observer pattern made muy facil',
+      url='http://github.com/markkorput/pyevento',
+      author='Mark van de Korput',
+      author_email='dr.theman@gmail.com',
+      license='MIT',
+      packages=['evento'],
+      zip_safe=True,
+      test_suite='tests')

d8eae67fe84e212f1db0f85392b867a8b76c6eed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:32 +0000

    Add package FSTA/0.0.1/FSTA-0.0.1-py2.7.egg

---
+++ packages/FSTA/FSTA-0.0.1-py2.7.egg/FSTA/scenario.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*
+
+from FSTA.installation import *
+
+
+class scenario(object):
+	"""Scenario if * or * or ... then ...
+	"""
+	exactly = 2
+	contains = 1
+	def __init__(self, phrases = [], actions = [], text = ""):
+		"""Initialisation
+			- phrases		list of unicode text
+			- actions		list of actions obtect
+			- text			explain the action (for logging)
+		"""
+		if isinstance(phrases, list):
+			self.phrases = phrases
+		else:
+			self.phrases = [phrases]
+		if isinstance(actions, list):
+			self.actions = actions
+		else:
+			self.actions = [actions]	
+		self.valide = False
+		
+	def run(self, text):
+		"""Function called if phrases match
+		- text		phrase dicted
+		"""
+		for action in self.actions:
+			action.run(text)
+	
+	def init(self, group):
+		""" Initialisation of the order with the installation datas
+		"""
+		for action in self.actions:
+			if isinstance(action, action_mqtt):
+				action.installation = group.installation
+	
+	def test(self, text):
+		"""test si le text correspond a une phrase.
+		"""
+		self.valide = False
+		for phrase in self.phrases:
+			if phrase.lower() == text.lower():
+				self.valide = max(self.valide, scenario.exactly)
+			elif phrase.lower() in text.lower():
+				self.valide = max(self.valide, scenario.contains)
+		return self.valide
+		
+		

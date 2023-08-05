f87bb5935551bef07e6f7889257b9bfd9ac8c47c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:57:04 +0000

    Add package exp10it/2.3.28/exp10it-2.3.28.tar.gz

---
+++ packages/exp10it/exp10it-2.3.28.tar.gz/exp10it-2.3.28/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+#from setuptools import setup, find_packages
+setup(
+    name='exp10it',
+    version='2.3.28',
+    packages=['cms_identify','cms_scan','dicts','dirsearch','exps','sql_inject','subDomainsBrute','Sublist3r'],
+    url='https://3xp10it.github.io',
+    license='',
+    author='quanyechavshuo',
+    author_email='quanyechavshuo@gmail.com',
+    description='My pipy module'
+)

bb7ec5ae11c371e096a5abe7287059dc039cb0b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:08:38 +0000

    Add package dc_ui/0.0.1/dc_ui-0.0.1.zip

---
+++ packages/dc_ui/dc_ui-0.0.1.zip/dc_ui-0.0.1/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name = 'dc_ui',
+    version = '0.0.1',
+    py_modules = ['dc_ui'],
+    author = 'vaniff',
+    author_email = 'zhouyong0206@163.com',
+    description = 'dc simple ui module',
+	url='http://pypi.python.org/pypi/dc_type/',
+	license='LICENSE.txt',
+    long_description=open('README.txt').read(),
+    )

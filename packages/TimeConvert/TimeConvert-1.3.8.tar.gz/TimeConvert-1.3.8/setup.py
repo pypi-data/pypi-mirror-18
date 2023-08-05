c47973d581865670dd18bb324475892a075771b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:15 +0000

    Add package TimeConvert/1.3.8/TimeConvert-1.3.8.tar.gz

---
+++ packages/TimeConvert/TimeConvert-1.3.8.tar.gz/TimeConvert-1.3.8/setup.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+
+version = '1.3.8'
+
+
+setup(
+    name='TimeConvert',
+    version=version,
+    keywords='',
+    description='Time Convert for Humans™',
+    long_description=open('README.rst').read(),
+
+    url='https://github.com/Brightcells/TimeConvert',
+
+    author='Hackathon',
+    author_email='kimi.huang@brightcells.com',
+
+    py_modules=['TimeConvert'],
+    install_requires=['pytz', ],
+
+    classifiers=[
+        "License :: OSI Approved :: BSD License",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.5",
+        "Programming Language :: Python :: 2.6",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.2",
+        "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+)

cc9140df31e663414bcb8450b2a88452e50b74ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:30 +0000

    Add package lemmes/1.1.5/lemmes-1.1.5.tar.gz

---
+++ packages/lemmes/lemmes-1.1.5.tar.gz/lemmes-1.1.5/lemmes/__init__.py
@@ -0,0 +1,7 @@
+import os
+from lemmes import *
+
+
+__version__ = '1.1.5'
+__license__ = 'MIT'
+__author__ = 'Edgar Castanheda'
---
+++ packages/lemmes/lemmes-1.1.5.tar.gz/lemmes-1.1.5/lemmes/rules.py
@@ -0,0 +1,8 @@
+# -*- coding: UTF-8 -*-
+RULES = {
+  'muriend': 'muerte',
+  'querid': 'querer',
+  'mirando': 'mirar',
+  'miradit': 'mirar',
+  'pudier': 'poder'
+}

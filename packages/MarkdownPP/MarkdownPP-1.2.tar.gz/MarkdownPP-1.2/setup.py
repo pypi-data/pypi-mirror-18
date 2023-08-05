5ce0c3b6c30ce95cde9ed66617558b46840a1f24
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:30 +0000

    Add package MarkdownPP/1.2/MarkdownPP-1.2.tar.gz

---
+++ packages/MarkdownPP/MarkdownPP-1.2.tar.gz/MarkdownPP-1.2/setup.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+from os import path
+import shutil
+
+if path.isfile('README.md'):
+    shutil.copyfile('README.md', 'README')
+
+setup(name="MarkdownPP",
+      description="Markdown preprocessor",
+      version="1.2",
+      author="John Reese",
+      author_email="john@noswap.com",
+      url="https://github.com/jreese/markdown-pp",
+      classifiers=['License :: OSI Approved :: MIT License',
+                   'Operating System :: OS Independent',
+                   'Programming Language :: Python',
+                   'Programming Language :: Python :: 2.6',
+                   'Programming Language :: Python :: 2.7',
+                   'Programming Language :: Python :: 3',
+                   'Programming Language :: Python :: 3.2',
+                   'Programming Language :: Python :: 3.3',
+                   'Programming Language :: Python :: 3.4',
+                   'Topic :: Utilities',
+                   'Development Status :: 4 - Beta',
+                   ],
+      license='MIT License',
+      scripts=['bin/markdown-pp'],
+      packages=['MarkdownPP', 'MarkdownPP/Modules'],
+      install_requires=[
+            "Watchdog >= 0.8.3",
+      ],
+      )

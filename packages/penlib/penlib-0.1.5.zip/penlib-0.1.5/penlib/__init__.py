f5490adbb7f0adaf5d0b5d6fad1a96c6e80f5138
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:47 +0000

    Add package penlib/0.1.5/penlib-0.1.5.zip

---
+++ packages/penlib/penlib-0.1.5.zip/penlib-0.1.5/penlib/__init__.py
@@ -0,0 +1,9 @@
+from . import postexploitation
+from . import reconnaissance
+from . import exploitation
+from . import scanning
+
+__version__ = "0.1.5"
+__author__ = 'George Sauceda'
+__license__ = 'GPL'
+__title__ = 'penlib'

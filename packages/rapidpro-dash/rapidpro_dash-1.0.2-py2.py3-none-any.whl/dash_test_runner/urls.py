fc27e8f0780c48aefc7d15ce67730d3c3b36dd32
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:20 +0000

    Add package rapidpro-dash/1.0.2/rapidpro_dash-1.0.2-py2.py3-none-any.whl

---
+++ packages/rapidpro-dash/rapidpro_dash-1.0.2-py2.py3-none-any.whl/dash_test_runner/urls.py
@@ -0,0 +1,13 @@
+from __future__ import unicode_literals
+
+from django.conf.urls import include, url
+from django.contrib import admin
+
+urlpatterns = [
+    url(r'^manage/', include('dash.orgs.urls')),
+    url(r'^manage/', include('dash.stories.urls')),
+    url(r'^manage/', include('dash.dashblocks.urls')),
+    url(r'^manage/', include('dash.categories.urls')),
+    url(r'^users/', include('dash.users.urls')),
+    url(r'^admin/', include(admin.site.urls)),
+]

21e39e81251a4c0cdac9df1358aa2cd44937c92c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:20 +0000

    Add package hcache/1.0.0/hcache-1.0.0.tar.gz

---
+++ packages/hcache/hcache-1.0.0.tar.gz/hcache-1.0.0/setup.py
@@ -0,0 +1,51 @@
+import os
+import sys
+from setuptools import setup
+from setuptools import find_packages
+
+
+def setup_package():
+    src_path = os.path.dirname(os.path.abspath(sys.argv[0]))
+    old_path = os.getcwd()
+    os.chdir(src_path)
+    sys.path.insert(0, src_path)
+
+    needs_pytest = {'pytest', 'test', 'ptr'}.intersection(sys.argv)
+    pytest_runner = ['pytest-runner'] if needs_pytest else []
+
+    setup_requires = pytest_runner
+    install_requires = ['six']
+    tests_require = install_requires
+
+    metadata = dict(
+        name='hcache',
+        version='1.0.0',
+        maintainer="Danilo Horta",
+        maintainer_email="horta@ebi.ac.uk",
+        license="BSD",
+        url='http://github.com/Horta/hcache',
+        packages=find_packages(),
+        zip_safe=False,
+        install_requires=install_requires,
+        setup_requires=setup_requires,
+        tests_require=tests_require,
+        include_package_data=True,
+    )
+
+    try:
+        from distutils.command.bdist_conda import CondaDistribution
+    except ImportError:
+        pass
+    else:
+        metadata['distclass'] = CondaDistribution
+        metadata['conda_buildnum'] = 1
+        metadata['conda_features'] = ['mkl']
+
+    try:
+        setup(**metadata)
+    finally:
+        del sys.path[0]
+        os.chdir(old_path)
+
+if __name__ == '__main__':
+    setup_package()

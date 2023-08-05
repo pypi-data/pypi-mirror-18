7cd6c43a72d6d715bf584a05b18ab09b8cdd9e8d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:08:14 +0000

    Add package django_db/0.0.7/django_db-0.0.7.tar.gz

---
+++ packages/django_db/django_db-0.0.7.tar.gz/django_db-0.0.7/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+
+setup(
+    name='django_db',
+    version='0.0.7',
+    url='https://github.com/jdxin0/django_db',
+    description='A simple way to use django orm not in a classic django project',
+    author='jdxin0',
+    author_email='jdxin00@gmail.com',
+    license='MIT',
+    keywords='django orm db',
+    packages=[
+        'django_db',
+    ],
+    install_requires=[
+        'django',
+        'pymysql',
+    ],
+)

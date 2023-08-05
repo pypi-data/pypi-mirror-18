4a715c535e3d1cd88e0353fe856eb40dd5021e54
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:16 +0000

    Add package passflip/0.7/passflip-0.7.tar.gz

---
+++ packages/passflip/passflip-0.7.tar.gz/passflip-0.7/passflip/arguments.py
@@ -0,0 +1,24 @@
+from argparse import ArgumentParser
+
+
+class PassflipArgumentParser(ArgumentParser):
+
+    def __init__(self):
+        super(PassflipArgumentParser, self).__init__()
+        self.init_arguments()
+
+    def init_arguments(self):
+        self.add_argument(
+            "-c",
+            "--check",
+            action="store_true",
+            help="prompt for password & salt twice."
+        )
+        self.add_argument(
+            "-l",
+            "--length",
+            action="store",
+            nargs="?",
+            help="limit the length of the output password"
+        )
+
---
+++ packages/passflip/passflip-0.7.tar.gz/passflip-0.7/passflip/passflip.py
@@ -0,0 +1,44 @@
+import sys
+from .mutation import mutate
+from .arguments import PassflipArgumentParser
+from .input import prompt_password, prompt_salt
+
+def main():
+    argument_parser = PassflipArgumentParser()
+    arguments = argument_parser.parse_args()
+    
+    if arguments.check is True:
+        input_ = prompt_with_double_check()
+    else:
+        input_ = prompt_in_default_mode() 
+    
+    if input_ is None:
+        return
+
+    output = mutate(input_[0], input_[1])
+    
+    if arguments.length is not None:
+        output = output[:int(arguments.length)]
+
+    print(output)
+
+
+def prompt_with_double_check():
+    password = prompt_password()
+    password_again = prompt_password(again=True)
+    if password != password_again:
+        print("Passwords do not match.")
+        return
+    salt = prompt_salt()
+    salt_again = prompt_salt(again=True)
+    if salt != salt_again:
+        print("Salts do not match.")
+        return
+    return (password, salt)
+
+
+def prompt_in_default_mode():
+    password = prompt_password()
+    salt = prompt_salt()
+    return (password, salt)
+

228628861ff621312916d643a72e4fcffb46c9fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:08:27 +0000

    Add package django-exchange-maploom/1.5.11/django-exchange-maploom-1.5.11.tar.gz

---
+++ packages/django-exchange-maploom/django-exchange-maploom-1.5.11.tar.gz/django-exchange-maploom-1.5.11/setup.py
@@ -0,0 +1,27 @@
+import os
+from setuptools import setup, find_packages
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+setup(
+    name='django-exchange-maploom',
+    version='1.5.11',
+    author='GeoNode Development Team - Prominent Edge, Terranodo, Boundless Spatial',
+    author_email='exchange@boundlessgeo.com',
+    url='https://github.com/boundlessgeo/django-exchange-maploom',
+    download_url="https://github.com/boundlessgeo/django-exchange-maploom",
+    description="Use MapLoom in your django projects.",
+    long_description=open(os.path.join(here, 'README.md')).read(),
+    license='See LICENSE file.',
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    classifiers=['Topic :: Utilities',
+                 'Natural Language :: English',
+                 'Operating System :: OS Independent',
+                 'Intended Audience :: Developers',
+                 'Environment :: Web Environment',
+                 'Framework :: Django',
+                 'Development Status :: 1 - Planning',
+                 'Programming Language :: Python :: 2.7'],
+)

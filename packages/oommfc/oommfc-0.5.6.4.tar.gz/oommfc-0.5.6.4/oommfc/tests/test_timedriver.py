a2da6156621c441158b9ad0fdedc067ce0beb072
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:57 +0000

    Add package oommfc/0.5.6.4/oommfc-0.5.6.4.tar.gz

---
+++ packages/oommfc/oommfc-0.5.6.4.tar.gz/oommfc-0.5.6.4/oommfc/tests/test_timedriver.py
@@ -0,0 +1,93 @@
+import os
+import glob
+import pytest
+import oommfc as oc
+from .test_driver import TestDriver
+
+
+class TestTimeDriver(TestDriver):
+    def test_script(self):
+        driver = oc.TimeDriver()
+        t = 1e-9
+        n = 120
+        script = driver.script(self.system, t=t, n=n)
+
+        assert script[0] == "#"
+        assert script[-1] == "1"
+        assert script.count("#") == 3
+        assert script.count("Specify") == 2
+        assert script.count("Destination") == 2
+        assert script.count("Schedule") == 2
+        assert script.count("mmArchive") == 2
+        assert script.count("Stage") == 2
+
+        assert "Oxs_RungeKuttaEvolve" in script
+        assert "Oxs_TimeDriver" in script
+        assert "Oxs_FileVectorField" in script
+
+        lines = script.split("\n")
+        alpha = self.system.dynamics.damping.alpha
+        assert lines[2] == "  alpha {}".format(alpha)
+        gamma = self.system.dynamics.precession.gamma
+        assert lines[3] == "  gamma_G {}".format(gamma)
+        assert lines[9] == "  stopping_time {}".format(t/n)
+        assert lines[11] == "  stage_count {}".format(n)
+        assert lines[12] == "  Ms {}".format(8e5)
+        assert lines[17] == "      file m0.omf"
+        assert lines[20] == "  basename tds"
+
+    def test_save_mif(self):
+        driver = oc.TimeDriver()
+        t = 1e-9
+        n = 120
+        driver._makedir(self.system)
+        driver._save_mif(self.system, t=t, n=n)
+
+        assert os.path.isfile("tds/tds.mif")
+
+        lines = open("tds/tds.mif", "r").readlines()
+        assert lines[0] == "# MIF 2.1\n"
+        assert lines[-1][-1] == "1"
+
+        os.system("rm -r tds/")
+
+    @pytest.mark.oommf
+    def test_drive(self):
+        md = oc.TimeDriver()
+
+        md.drive(self.system, t=0.1e-9, n=10)
+
+        assert os.path.exists("tds/")
+        assert os.path.isfile("tds/tds.mif")
+
+        omf_files = list(glob.iglob("tds/*.omf"))
+        odt_files = list(glob.iglob("tds/*.odt"))
+
+        assert len(omf_files) == 11
+        assert "tds/m0.omf" in omf_files
+
+        assert len(odt_files) == 1
+
+        os.system("rm -r tds/")
+
+    def test_drive_exception(self):
+        md = oc.TimeDriver()
+        with pytest.raises(ValueError):
+            md.drive(self.system, t=-0.1e-9, n=10)
+        with pytest.raises(ValueError):
+            md.drive(self.system, t=0.1e-9, n=-10)
+
+    def test_script_missing_terms(self):
+        # Missing Gilbert damping alpha
+        self.system.dynamics = oc.Precession(gamma=2.2)
+        td = oc.TimeDriver()
+        script = td.script(self.system, t=1e-9, n=20)
+        assert "alpha 0" in script
+        assert "gamma_G 2.2" in script
+
+        # Missing gamma
+        self.system.dynamics = oc.Damping(alpha=5)
+        td = oc.TimeDriver()
+        script = td.script(self.system, t=1e-9, n=20)
+        assert "alpha 5" in script
+        assert "gamma_G 221100" in script

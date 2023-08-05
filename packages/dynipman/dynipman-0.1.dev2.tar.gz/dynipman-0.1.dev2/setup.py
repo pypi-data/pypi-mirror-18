c5907b617bb0493aeb98eac0cbabcdcd8f8c55c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:03 +0000

    Add package dynipman/0.1.dev2/dynipman-0.1.dev2.tar.gz

---
+++ packages/dynipman/dynipman-0.1.dev2.tar.gz/dynipman-0.1.dev2/setup.py
@@ -0,0 +1,18 @@
+from distutils.core import setup
+VERSION = '0.1.dev2'
+setup(
+  name = 'dynipman',
+  install_requires = ['tornado', 'pycrypto'],
+  packages = ['dynipman',],
+  package_data = {'dynipman':['scripts/*', 'index.html']},
+  scripts = ['dynipman/scripts/dynipman_cd','dynipman/scripts/dynipman_sd',],
+  version = VERSION,
+  description = 'Personal IP address name service for Developers who move around a lot',
+  author = 'Kumseok Jung',
+  author_email = 'jungkumseok@gmail.com',
+  license = 'LICENSE.txt',
+  url = 'https://github.com/jungkumseok/dynipman',
+  download_url = 'https://github.com/jungkumseok/dynipman/archive/'+VERSION+'.tar.gz',
+  keywords = ['remote', 'ip address', 'dns'], 
+  classifiers = [],
+)

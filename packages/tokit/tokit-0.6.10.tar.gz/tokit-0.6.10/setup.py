35a91e24f31649429046170a95a1b661aeca29f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:08:35 +0000

    Add package tokit/0.6.10/tokit-0.6.10.tar.gz

---
+++ packages/tokit/tokit-0.6.10.tar.gz/tokit-0.6.10/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup
+
+# Thanks to http://peterdowns.com/posts/first-time-with-pypi.html
+tokit_version = '0.6.10'
+
+setup(
+    name='tokit',
+    packages=['tokit'],
+    version=tokit_version,
+    description='A kit for development with Tornado web framework',
+    author='Giang Manh',
+    author_email='manhgd@yahoo.com',
+    url='https://github.com/manhg/tokit',
+    download_url='https://github.com/manhg/tokit/tarball/' + tokit_version,
+    keywords=['tornado', 'web', 'tokit'],
+    install_requires = [
+        "tornado>=4.3",
+        "shortuuid"
+    ],
+    package_data={'tokit': [
+        'js/*.js', 'skeleton/*',
+        'skeleton/config/*', 'skeleton/src/*', 'skeleton/src/home/*'
+    ]},
+    classifiers=[
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ]
+)

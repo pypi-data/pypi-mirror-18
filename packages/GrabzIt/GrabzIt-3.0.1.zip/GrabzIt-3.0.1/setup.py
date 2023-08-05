7d137d4fe5e619be500a4262f6d5ecdbaf499b33
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:06 +0000

    Add package GrabzIt/3.0.1/GrabzIt-3.0.1.zip

---
+++ packages/GrabzIt/GrabzIt-3.0.1.zip/GrabzIt-3.0.1/setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='GrabzIt',
+      version='3.0.1',
+      summary='Convert URLs or HTML to images and PDFs',
+      author='GrabzIt',
+      url='http://grabz.it/api/python/',
+      license='MIT Licence',
+      description='This library allows you to programmatically convert web pages and HTML into images, PDFs, CSVs and spreadsheets. Additionally GrabzIt allows you to convert online videos into animated GIFs',
+      packages=['GrabzIt']      
+)

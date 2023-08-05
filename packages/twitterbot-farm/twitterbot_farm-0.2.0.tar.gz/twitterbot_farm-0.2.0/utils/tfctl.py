78e2d436576db8301dd8bc86050e5929e7087d96
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:08:09 +0000

    Add package twitterbot-farm/0.2.0/twitterbot_farm-0.2.0.tar.gz

---
+++ packages/twitterbot-farm/twitterbot_farm-0.2.0.tar.gz/twitterbot_farm-0.2.0/utils/tfctl.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python2.7
+# coding: utf-8
+
+""" ctl for everything that can't be done without python """
+
+from os import getenv
+from sys import argv
+from json import dumps
+from requests import post
+from dictator import Dictator
+from twitterbot_farm import Reader, Writer
+
+HOST = getenv('HOST', '127.0.0.1')
+INFLUXDB_URL = getenv('INFLUXDB_URL', 'http://127.0.0.1:8086/write?db=twitterbot_farm')
+
+
+def create():
+    Dictator(host=HOST)[argv[2]] = argv[3:]
+
+
+def error():
+    raise ValueError(argv)
+
+
+def dump():
+    print dumps(Dictator(host=HOST).items(), indent=4, ensure_ascii=False)
+
+
+def __stats__():
+    connect = Dictator(host=HOST)
+    bots = [bot.replace('@', '') for bot in connect.keys() if bot != '__app__']
+    output = dict()
+    for bot_name in bots:
+        role = connect['@' + bot_name][0]
+        if role == 'reader':
+            output[bot_name] = len(Reader(bot_name, HOST).connection.keys())
+        elif role == 'writer':
+            output[bot_name] = len(Writer(bot_name, HOST).unprocessed_lines())
+    return output
+
+
+def stats():
+    for bot_name, value in __stats__().items():
+        print "{0}={1}".format(bot_name, value)
+
+
+def send_stats():
+    for bot_name, value in __stats__().items():
+        data = "{0} value={1}".format(bot_name, value)
+        print INFLUXDB_URL, '-d', data
+        post(INFLUXDB_URL, data)
+
+
+def tweet():
+    bot_name = argv[2]
+    text = " ".join(argv[3:])
+    bot = Writer(bot_name, HOST)
+    bot.twibot.api.update_status(text)
+
+
+def main():
+    commands = {
+        'create': create,
+        'dump': dump,
+        'stats': stats,
+        'send_stats': send_stats,
+        'tweet': tweet,
+    }
+
+    assert argv[1] in commands, "Unknown command {0}".format(argv[1])
+    commands.get(argv[1])()
+
+
+if __name__ == '__main__':
+    main()

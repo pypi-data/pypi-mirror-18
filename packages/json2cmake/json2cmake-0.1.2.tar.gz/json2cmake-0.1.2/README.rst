d8c6349d0ff4eadb17297efa1ee3ad440fb247a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:38 +0000

    Add package json2cmake/0.1.2/json2cmake-0.1.2.tar.gz

---
+++ packages/json2cmake/json2cmake-0.1.2.tar.gz/json2cmake-0.1.2/README.rst
@@ -0,0 +1,15 @@
+json2cmake
+==========
+
+``json2cmake`` converts `JSON compilation
+database <http://clang.llvm.org/docs/JSONCompilationDatabase.html>`__
+files into `CMake <https://cmake.org/>`__ files. The resulting
+``CMakeLists.txt`` file can then be used to recompile the same object
+files with less overhead via `Ninja <https://ninja-build.org/>`__, used
+as an IDE project file for `CLion <https://www.jetbrains.com/clion/>`__,
+or for integration into a larger CMake project.
+
+The output files only include `object
+library <https://cmake.org/Wiki/CMake/Tutorials/Object_Library>`__
+definitions, as a ``compile_commands.json`` file typically doesn't
+contain any linker commands.
---
+++ packages/json2cmake/json2cmake-0.1.2.tar.gz/json2cmake-0.1.2/setup.py
@@ -0,0 +1,43 @@
+from setuptools import setup, find_packages
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='json2cmake',
+    version='0.1.2',
+    description='Generate CMakeLists.txt from a compile_commands.json',
+    long_description=long_description,
+    url='https://github.com/AbigailBuccaneer/json2cmake',
+    author='AbigailBuccaneer',
+    author_email='abigailbuccaneer@gmail.com',
+    license='MIT',
+
+
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+
+        'License :: OSI Approved :: MIT License',
+
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+    keywords='c c++ cmake development',
+    packages=find_packages(),
+
+    entry_points={'console_scripts': ['json2cmake=json2cmake:main']},
+
+)

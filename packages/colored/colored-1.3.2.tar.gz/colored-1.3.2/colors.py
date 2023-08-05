8de1833dcd7c63b2ab3e5347a6107b3b88fa6fac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:36 +0000

    Add package colored/1.3.2/colored-1.3.2.tar.gz

---
+++ packages/colored/colored-1.3.2.tar.gz/colored-1.3.2/colors.py
@@ -0,0 +1,77 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+"""
+colors.py is a part of colored.
+
+Copyright 2014-2016 Dimitris Zlatanidis <d.zlatanidis@gmail.com>
+All rights reserved.
+
+Colored is very simple Python library for color and formatting in terminal.
+
+https://github.com/dslackw/colored
+
+colored is free software: you can redistribute it and/or
+modify it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+You should have received a copy of the GNU General Public License
+along with this program. If not, see <http://www.gnu.org/licenses/>.
+"""
+
+
+names = [
+        'BLACK',
+        'RED',
+        'GREEN',
+        'YELLOW',
+        'BLUE',
+        'MAGENTA',
+        'CYAN',
+        'LIGHT_GRAY',
+        'DARK_GRAY',
+        'LIGHT_RED',
+        'LIGHT_GREEN',
+        'LIGHT_YELLOW',
+        'LIGHT_BLUE',
+        'LIGHT_MAGENTA',
+        'LIGHT_CYAN',
+        'WHITE',
+        'GREY_0',
+        'NAVY_BLUE',
+        'DARK_BLUE',
+        'BLUE_3A',
+        'BLUE_3B',
+        'BLUE_1',
+        'DARK_GREEN',
+        'DEEP_SKY_BLUE_4A',
+        'DEEP_SKY_BLUE_4B',
+        'DEEP_SKY_BLUE_4C',
+        'DODGER_BLUE_3',
+        'DODGER_BLUE_2',
+        'GREEN_4',
+        'SPRING_GREEN_4',
+        'TURQUOISE_4',
+        'DEEP_SKY_BLUE_3A',
+        'DEEP_SKY_BLUE_3B',
+        'DODGER_BLUE_1',
+        'GREEN_3A',
+        'SPRING_GREEN_3A',
+        'DARK_CYAN',
+        'LIGHT_SEA_GREEN',
+        'DEEP_SKY_BLUE_2',
+        'DEEP_SKY_BLUE_1',
+        'GREEN_3B',
+        'SPRING_GREEN_3B',
+        'SPRING_GREEN_2A',
+        'CYAN_3',
+        'DARK_TURQUOISE',
+        'TURQUOISE_2',
+        'GREEN_1',
+        'SPRING_GREEN_2B',
+        'SPRING_GREEN_1',
+]

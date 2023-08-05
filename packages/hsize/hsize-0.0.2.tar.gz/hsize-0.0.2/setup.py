755087e24d3178a8853d043fa0731ba47b62d2f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:03 +0000

    Add package hsize/0.0.2/hsize-0.0.2.tar.gz

---
+++ packages/hsize/hsize-0.0.2.tar.gz/hsize-0.0.2/setup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+setup(
+    name = 'hsize',
+    version = '0.0.2',
+    keywords = ('human', 'size'),
+    description = 'Convert the size between human size and bytes on python3',
+    license = 'MIT License',
+
+    author = 'micahel tsui',
+    author_email = 'bwangel.me@gmail.com',
+
+    packages = find_packages(),
+    platforms = 'any',
+)

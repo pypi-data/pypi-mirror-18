3d0a6219cad0b023ebf5499aa1f2bc7e2082d98d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:22 +0000

    Add package vincent_dxk/1.0.0/vincent_dxk-1.0.0.zip

---
+++ packages/vincent_dxk/vincent_dxk-1.0.0.zip/vincent_dxk-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+setup(
+    name="vincent_dxk",
+    version ="1.0.0",
+    py_modules =["vincent_dxk"],
+    author="VincentAndy",
+    author_email="yunishenai@aliyun.com",
+    url="http://www.women92.com",
+    description="VincentAndy first demo"
+
+)

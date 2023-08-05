606779ba2d8eb8b257057313e7925506d497d6db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:21 +0000

    Add package gocdpb/7.3/gocdpb-7.3.tar.gz

---
+++ packages/gocdpb/gocdpb-7.3.tar.gz/gocdpb-7.3/src/main/goserver_adapter.py
@@ -0,0 +1,209 @@
+# -*- coding: utf-8 -*-
+from __future__ import print_function
+import sys
+import json
+import yaml
+import requests
+from collections import OrderedDict
+
+from goserver_config import CruiseTree
+
+
+class Goserver(object):
+    """
+    Manages HTTP communication with the Go server.
+    """
+    config_xml_rest_path = "/go/admin/restful/configuration/file/{}/xml"
+
+    def __init__(self, config, verbose, config_overrides):
+        self.__config = {}
+        if config is not None:
+            self.__config.update(yaml.load(config))
+        self.__config.update(config_overrides)
+        self.verbose = verbose
+        self._cruise_config_md5 = None
+        self.tree = None
+        self._initial_xml = None
+        self.need_to_download_config = True
+
+    def check_config(self):
+        for param in (
+            'url',
+        ):
+            assert param in self.__config, param
+
+    def init(self):
+        """
+        Fetch configuration from Go server
+        """
+        if self.need_to_download_config:
+            try:
+                self.tree = CruiseTree.fromstring(self.xml_from_url())
+                self._initial_xml = self.cruise_xml
+            except RuntimeError as error:
+                print('Could not get XML configuration. That might not be a problem...')
+                print(error)
+            self.need_to_download_config = False
+
+    @property
+    def need_to_upload_config(self):
+        return self.cruise_xml != self._initial_xml
+
+    @property
+    def __auth(self):
+        if 'username' in self.__config:
+            return self.__config['username'], self.__config['password']
+
+    @property
+    def cruise_xml(self):
+        return self.tree.tostring()
+
+    @property
+    def cruise_xml_subset(self):
+        return self.tree.config_subset_tostring()
+
+    def request(self, action, path, **kwargs):
+        action = action.upper()
+        if self._changing_call(action):
+            # If we change via REST API, we need to fetch the config
+            # again if we need to understand the state.
+            # If we uploaded the config XML, we need to fetch it
+            # again to get a new md5 checksum in case we want to
+            # change some more...
+            self.need_to_download_config = True
+        url = self.__config['url'] + path
+        if self.__auth:
+            kwargs['auth'] = self.__auth
+        response = requests.request(action, url, **kwargs)
+        if response.status_code != 200:
+            sys.stderr.write("Failed to {} {}\n".format(action, path))
+            sys.stderr.write("status-code: {}\n".format(response.status_code))
+            sys.stderr.write(u"text: {}\n".format(response.text))
+        return response
+
+    @staticmethod
+    def _changing_call(action):
+        return action not in ('HEAD', 'GET')
+
+    def xml_from_url(self):
+        action = 'GET'
+        path = self.config_xml_rest_path.format(action)
+        response = self.request(action, path)
+        if response.status_code != 200:
+            raise RuntimeError(str(response.status_code))
+        self._cruise_config_md5 = response.headers['x-cruise-config-md5']
+        return response.text
+
+    def create_a_pipeline(self, pipeline):
+        """
+        Add a pipeline to the Go server configuration using the REST API:
+        https://api.go.cd/current/#create-a-pipeline
+
+        :param pipeline: Json object as describe in API above.
+        """
+        path = "/go/api/admin/pipelines"
+        data = json.dumps(pipeline)
+        headers = {
+            'Accept': 'application/vnd.go.cd.v1+json',
+            'Content-Type': 'application/json'
+        }
+        response = self.request('post', path, data=data, headers=headers)
+        if response.status_code != 200:
+            raise RuntimeError(str(response.status_code))
+
+    def get_pipeline_config(self, pipeline_name):
+        path = "/go/api/admin/pipelines/" + pipeline_name
+        headers = {
+            'Accept': 'application/vnd.go.cd.v1+json'
+        }
+        response = self.request('get', path, headers=headers)
+        if response.status_code != 200:
+            raise RuntimeError(str(response.status_code))
+        json_data = json.loads(response.text.replace("\\'", "'"),
+                               object_pairs_hook=OrderedDict)
+        etag = response.headers['etag']
+        return etag, json_data
+
+    def edit_pipeline_config(self, pipeline_name, etag, pipeline):
+        path = "/go/api/admin/pipelines/" + pipeline_name
+        data = json.dumps(pipeline)
+        headers = {
+            'Accept': 'application/vnd.go.cd.v1+json',
+            'Content-Type': 'application/json',
+            'If-Match': etag
+        }
+        response = self.request('put', path, data=data, headers=headers)
+        if response.status_code != 200:
+            print(response.text)
+            raise RuntimeError(str(response.status_code))
+
+    def unpause(self, pipeline_name):
+        headers = {
+            'Confirm': 'true'
+        }
+        path = "/go/api/pipelines/" + pipeline_name + "/unpause"
+        self.request('post', path, headers=headers)
+
+    def get_pipeline_status(self, pipeline_name):
+        path = "/go/api/pipelines/" + pipeline_name + "/status"
+        headers = {
+            'Accept': 'application/json'
+        }
+        response = self.request('get', path, headers=headers)
+        if response.status_code != 200:
+            raise RuntimeError(str(response.status_code))
+        json_data = json.loads(response.text.replace("\\'", "'"),
+                               object_pairs_hook=OrderedDict)
+        return json_data
+
+    def get_pipeline_groups(self):
+        path = "/go/api/config/pipeline_groups"
+        headers = {
+            'Accept': 'application/json'
+        }
+        response = self.request('get', path, headers=headers)
+        if response.status_code != 200:
+            raise RuntimeError(str(response.status_code))
+        json_data = json.loads(response.text.replace("\\'", "'"),
+                               object_pairs_hook=OrderedDict)
+        return json_data
+
+    def get_pipeline_instance(self, pipeline, instance):
+        path = "/go/api/pipelines/" + pipeline + "/instance/" + instance
+        headers = {
+            'Accept': 'application/json'
+        }
+        response = self.request('get', path, headers=headers)
+        if response.status_code != 200:
+            raise RuntimeError(str(response.status_code))
+        json_data = json.loads(response.text.replace("\\'", "'"),
+                               object_pairs_hook=OrderedDict)
+        return json_data
+
+    def upload_config(self):
+        """
+        This method pushes a new cruise-config.xml to the go server.
+        It's used when there is no REST API for the changes we want to do.
+
+        Make sure to refresh the cruise-config by using the .init() method
+        if the Go server config was just changed through the REST API.
+        """
+        if self.cruise_xml == self._initial_xml:
+            print("No changes done. Not uploading config.")
+        else:
+            data = {'xmlFile': self.cruise_xml, 'md5': self._cruise_config_md5}
+            action = 'POST'
+            response = self.request(action,
+                                    self.config_xml_rest_path.format(action),
+                                    data=data)
+            if response.status_code != 200:
+                sys.stderr.write("status-code: %s\n" % response.status_code)
+                # GoCD produces broken JSON???, see
+                # https://github.com/gocd/gocd/issues/1472
+                json_data = json.loads(response.text.replace("\\'", "'"),
+                                       object_pairs_hook=OrderedDict)
+                sys.stderr.write("result: %s\n" % json_data["result"])
+                sys.stderr.write(
+                    "originalContent:\n%s\n" % json_data["originalContent"])
+                raise RuntimeError(response.status_code)
+
---
+++ packages/gocdpb/gocdpb-7.3.tar.gz/gocdpb-7.3/src/main/tagrepos.py
@@ -0,0 +1,193 @@
+# -*- coding: utf-8 -*-
+from __future__ import division, print_function
+from os import path, chdir, getcwd
+from shutil import rmtree
+import collections
+import argparse
+import sys
+import json
+import functools
+import subprocess
+import pipes
+
+
+class Git(object):
+    def __init__(self, verbose=False):
+        self.verbose = verbose
+
+    def _call_git(self, *args):
+        if self.verbose:
+            print("git", " ".join(map(pipes.quote, args)))
+        try:
+            result = subprocess.check_output(['git'] + list(args), stderr=subprocess.STDOUT)
+        except subprocess.CalledProcessError as e:
+            # Make sure we always see what the stdout before re-raising the exception.
+            # Otherwise it gets lost.
+            print(e.output)
+            raise
+
+        if self.verbose:
+            print(result)
+        return result
+
+    def __getattr__(self, name):
+        return functools.partial(self._call_git, name)
+
+
+class GitTagger(object):
+    def __init__(self, directory, verbose=False):
+        self.directory = directory
+        self.repo_name = None
+        self.msg = None
+        self.start_dir = None
+        self.git = None
+        self.verbose = verbose
+
+    @staticmethod
+    def get_repo_name(url):
+        repo_name = path.basename(url)
+        if url.endswith('.git'):
+            repo_name = path.splitext(repo_name)[0]
+        return repo_name
+
+    def clone(self, repo, branch):
+        self.start_dir = getcwd()
+        chdir(self.directory)
+        self.git = Git(verbose=self.verbose)
+        self.git.clone(repo)
+        self.repo_name = self.get_repo_name(repo)
+        chdir(self.repo_name)
+        self.git.checkout(branch)
+        chdir(self.start_dir)
+
+    def tag(self, name, revision):
+        chdir(path.join(self.directory, self.repo_name))
+        try:
+            self.git.tag(name, revision)
+        except subprocess.CalledProcessError as e:
+            # if the tag already exists, we should just carry on
+            if "already exists" in e.output:
+                print("tag {} already exists, continuing".format(name))
+            else:
+                raise
+
+        self.msg = 'Tagged {}:{} with {}'.format(self.repo_name, revision, name)
+        chdir(self.start_dir)
+
+    def branch(self, name, revision):
+        chdir(path.join(self.directory, self.repo_name))
+        self.git.branch(name, revision)
+        self.msg = 'Branched {}:{} with {}'.format(self.repo_name, revision, name)
+        chdir(self.start_dir)
+
+    def push(self, tag_or_branch):
+        chdir(path.join(self.directory, self.repo_name))
+        self.git.push('origin', tag_or_branch)
+        chdir(self.start_dir)
+
+    def clean(self):
+        chdir(self.directory)
+        rmtree(self.repo_name)
+        chdir(self.start_dir)
+
+
+def tag_repos(directory, name, structure, branch_list=None, push=False, clean=False, verbose=False):
+    if branch_list is None:
+        branch_list = []
+    for repo in structure:
+        if repo['type'] != 'Git':
+            print("Don't know how to handle material type " + repo['type'])
+            print(repo)
+            continue
+        should_branch = False
+        for pipeline in repo['pipelines']:
+            if pipeline['name'] in branch_list:
+                should_branch = True
+        desc = repo['description']
+        url_part, branch_part = desc.split(',')
+        url = url_part.split(':', 1)[1].strip()
+        branch = branch_part.split(':', 1)[1].strip()
+        rev = repo['revision']
+        tagger = GitTagger(directory, verbose)
+        tagger.clone(url, branch)
+        if should_branch:
+            tagger.branch(name, rev)
+        else:
+            tagger.tag(name, rev)
+        if push:
+            tagger.push(name)
+        if clean:
+            tagger.clean()
+
+
+def check_consistent(structure, label):
+    revisions = collections.defaultdict(list)
+    pipelines = collections.defaultdict(list)
+    for repository in structure:
+        description = repository['description']
+        revision = repository['revision']
+        revisions[description].append(revision)
+        for pipeline in repository['pipelines']:
+            name = pipeline['name']
+            counter = pipeline['counter']
+            pipelines[(description, revision)] = "{}/{}".format(name, counter)
+    bad = False
+    for description, revision_list in revisions.items():
+        if len(revision_list) > 1:
+            bad = True
+            print('Repository {} used with more than one revision:'.format(description))
+            for revision in revision_list:
+                print('Revision {} used in build {}'.format(revision, pipelines[(description, revision)]))
+    if bad:
+        raise ValueError('Inconsistencies found in {}'.format(label))
+
+
+def main():
+    parser = argparse.ArgumentParser(
+        description="Tag and/or branch a set of Git repositories as provided by json data.")
+    parser.add_argument(
+        'jsonfile',
+        help='Json file as produced by gocdrepos. Read from stdin if no  filename given.',
+        nargs='?',
+        type=argparse.FileType('r'),
+        default=sys.stdin)
+    parser.add_argument(
+        '-d', '--directory',
+        default='/tmp',
+        help="Parent directory of repository clones clones. Default to /tmp."
+    )
+    parser.add_argument(
+        '-t', '--tag-name',
+        required=True,
+        help="Name of tag / branch to create."
+    )
+    parser.add_argument(
+        '-b', '--branch-list',
+        help="Comma-separated list of pipeline names. Create branches for these."
+    )
+    parser.add_argument(
+        '-p', '--push',
+        action='store_true',
+        help="Push changes to remote repo."
+    )
+    parser.add_argument(
+        '-c', '--clean',
+        action='store_true',
+        help="Remove cloned repo."
+    )
+    parser.add_argument(
+        '-v', '--verbose',
+        action='store_true',
+        help="Verbose Git output."
+    )
+
+    pargs = parser.parse_args()
+    branch_list = [] if not pargs.branch_list else pargs.branch_list.split(',')
+    structure = json.load(pargs.jsonfile)
+
+    check_consistent(structure, pargs.jsonfile.name)
+    tag_repos(pargs.directory, pargs.tag_name, structure, branch_list, pargs.push, pargs.clean, pargs.verbose)
+
+
+if __name__ == '__main__':
+    main()

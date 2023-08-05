a89c306177491257b279c984b3c1bc75e81db7e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:43 +0000

    Add package netdisco/0.7.5/netdisco-0.7.5.tar.gz

---
+++ packages/netdisco/netdisco-0.7.5.tar.gz/netdisco-0.7.5/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(name='netdisco',
+      version='0.7.5',
+      description='Discover devices on your local network',
+      url='https://github.com/home-assistant/netdisco',
+      author='Paulus Schoutsen',
+      author_email='Paulus@PaulusSchoutsen.nl',
+      license='MIT',
+      install_requires=['netifaces>=0.10.0', 'requests>=2.0',
+                        'zeroconf==0.17.6'],
+      packages=find_packages(),
+      zip_safe=False)

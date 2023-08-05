15d6febe8e75d059c5faf4e99237e5ff244da037
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:19 +0000

    Add package apidev-coop-cms/1.1.24/apidev-coop_cms-1.1.24.tar.gz

---
+++ packages/apidev-coop-cms/apidev-coop_cms-1.1.24.tar.gz/apidev-coop_cms-1.1.24/coop_cms/views/homepage.py
@@ -0,0 +1,63 @@
+# -*- coding: utf-8 -*-
+"""homepage management"""
+
+from django.conf import settings
+from django.contrib.auth.decorators import login_required
+from django.contrib.sites.models import Site
+from django.core.exceptions import PermissionDenied
+from django.core.urlresolvers import reverse
+from django.http import Http404, HttpResponseRedirect
+from django.shortcuts import render_to_response, get_object_or_404
+from django.template import RequestContext
+from django.utils.translation import ugettext as _
+
+from colorbox.decorators import popup_redirect
+
+from coop_cms import models
+from coop_cms.settings import cms_no_homepage, get_article_class
+
+
+def homepage(request):
+    """view homepage"""
+    if cms_no_homepage():
+        raise Http404
+
+    site = Site.objects.get_current()
+
+    # Try site settings
+    try:
+        site_settings = models.SiteSettings.objects.get(site=site)
+        if site_settings.homepage_url:
+            return HttpResponseRedirect(site_settings.homepage_url)
+    except models.SiteSettings.DoesNotExist:
+        pass
+
+    return HttpResponseRedirect(reverse('coop_cms_view_all_articles'))
+
+
+@login_required
+@popup_redirect
+def set_homepage(request, article_id):
+    """use the article as homepage"""
+    article = get_object_or_404(get_article_class(), id=article_id)
+
+    if not request.user.has_perm('can_edit_article', article):
+        raise PermissionDenied
+
+    if request.method == "POST":
+        site_settings = models.SiteSettings.objects.get_or_create(site=Site.objects.get_current())[0]
+        site_settings.homepage_url = article.get_absolute_url()
+        site_settings.save()
+
+        return HttpResponseRedirect(reverse('coop_cms_homepage'))
+
+    context_dict = {
+        'article': article,
+        'title': _(u"Do you want to use this article as homepage?"),
+    }
+
+    return render_to_response(
+        'coop_cms/popup_set_homepage.html',
+        context_dict,
+        context_instance=RequestContext(request)
+    )
---
+++ packages/apidev-coop-cms/apidev-coop_cms-1.1.24.tar.gz/apidev-coop_cms-1.1.24/coop_cms/widgets.py
@@ -0,0 +1,77 @@
+# -*- coding: utf-8 -*-
+"""widgets"""
+
+from floppyforms.widgets import ClearableFileInput, Select, SelectMultiple, Input
+
+from coop_cms.utils import get_text_from_template
+
+
+class ReadOnlyInput(Input):
+    """readonly input"""
+    template_name = 'coop_cms/widgets/readonlyinput.html'
+
+
+class ImageEdit(ClearableFileInput):
+    """image edit"""
+    template_name = 'coop_cms/widgets/imageedit.html'
+    
+    def __init__(self, update_url, thumbnail_src, *args, **kwargs):
+        super(ImageEdit, self).__init__(*args, **kwargs)
+        self._extra_context = {
+            'update_url': update_url,
+            'thumbnail_src': thumbnail_src,
+            'extra_classes': get_text_from_template("coop_cms/widgets/_imageedit_cssclass.html"),
+        }
+        
+    def get_context(self, *args, **kwargs):
+        """get context"""
+        context = super(ImageEdit, self).get_context(*args, **kwargs)
+        context.update(self._extra_context)
+        return context
+
+
+class ChosenWidgetMixin(object):
+    """chosen jquery widget"""
+
+    class Media:
+        """css and js required by widget"""
+        js = (
+            "{0}?v=1".format("chosen/chosen.jquery.min.js"),
+        )
+        css = {
+            "all": ("{0}?v=1".format("chosen/chosen.css"),),
+        }
+
+    def __init__(self, attrs=None, *args, **kwargs):
+        
+        self._extra_context = {}
+        if kwargs.pop("force_template", False):
+            #chosen inherit from super template
+            self._extra_context['super_template'] = self.template_name
+            self.template_name = 'coop_cms/widgets/chosen.html'
+
+        if not attrs:
+            attrs = {}
+        attrs['data-placeholder'] = kwargs.pop('overlay', None)
+        super(ChosenWidgetMixin, self).__init__(attrs, *args, **kwargs)
+
+
+class ChosenSelectMultiple(ChosenWidgetMixin, SelectMultiple):
+    """chosen select multiple"""
+    #template_name = 'coop_cms/widgets/chosen.html'
+    
+    def get_context(self, *args, **kwargs):
+        """context"""
+        context = super(ChosenSelectMultiple, self).get_context(*args, **kwargs) # pylint: disable=E1002
+        context.update(self._extra_context)
+        return context
+
+
+class ChosenSelect(ChosenWidgetMixin, Select):
+    """chosen select"""
+
+    def get_context(self, *args, **kwargs):
+        """context"""
+        context = super(ChosenSelect, self).get_context(*args, **kwargs) # pylint: disable=E1002
+        context.update(self._extra_context)
+        return context

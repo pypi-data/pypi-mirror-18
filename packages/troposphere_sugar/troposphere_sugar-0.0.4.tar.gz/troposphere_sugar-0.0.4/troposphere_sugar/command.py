cb4389f9082a3cddef720b39ae9094f8e6e2fb4c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:07:24 +0000

    Add package troposphere_sugar/0.0.4/troposphere_sugar-0.0.4.tar.gz

---
+++ packages/troposphere_sugar/troposphere_sugar-0.0.4.tar.gz/troposphere_sugar-0.0.4/troposphere_sugar/command.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+from skel import Skel
+from runner import Runner
+import argparse
+import re
+
+#Shameless copy
+#http://stackoverflow.com/questions/1175208/elegant-python-function-to-convert-camelcase-to-camel-case
+def camelcase_to_dashed(name):
+    s1 = re.sub('(.)([A-Z][a-z]+)', r'\1-\2', name)
+    return re.sub('([a-z0-9])([A-Z])', r'\1-\2', s1).lower()
+
+class Command(object):
+    def execute(self, stack_name, iam_capability=False, session=None):
+        self.process()
+        params = self.params_args_dict
+        self._runner = Runner(self.template, stack_name=stack_name,
+                params=params, iam_capability=iam_capability, session=session)
+        self._runner.perform()
+
+    @property
+    def parser(self):
+        if not hasattr(self, "_parser"):
+            self._parser = argparse.ArgumentParser(description=self.template.description)
+        return self._parser
+
+    @parser.setter
+    def parser(self, value):
+        self._parser = value
+
+    @property
+    def parsed_args(self):
+        self.set_params_args_in_parser()
+        return vars(self.parser.parse_args())
+
+    @property
+    def params_args_dict(self):
+        result = {}
+        for (k,v) in self.parsed_args.items():
+            altk = re.sub("_","-",k)
+            title = self.args_to_params.get(altk, None)
+            if title:
+                result[title] = v
+        return result
+
+    @staticmethod
+    def param_to_dict(p):
+        default = p.properties.get("Default", None)
+        required = not bool(default)
+        help=p.properties["Description"]
+        return dict(default=p.properties.get("Default", None),
+                required=not(bool(default)),
+                help=help)
+
+    def set_params_args_in_parser(self):
+        if not getattr(self, "args_to_params", None):
+            self.args_to_params = {}
+
+        for param in self.cfparams:
+            p = param[0]
+            dashed = camelcase_to_dashed(p.title)
+            self.args_to_params[dashed]=p.title
+            dashed_arg = "--{}".format(dashed)
+            self.parser.add_argument(dashed_arg, **self.param_to_dict(p))
---
+++ packages/troposphere_sugar/troposphere_sugar-0.0.4.tar.gz/troposphere_sugar-0.0.4/troposphere_sugar/skel.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+from decorators import *
+from troposphere import Template
+from random import randint
+
+class Skel(object):
+    def __init__(self, **kwargs):
+        self.__dict__.update(self._defaults())
+        self.__dict__.update(kwargs)
+
+    def _defaults(self):
+        return {
+                "template": Template(),
+                "_processed": False
+                }
+
+    def _get_all_decorated_for_class(self, clazz, ttype):
+        all = clazz.__dict__.values()
+        for base_clazz in clazz.__bases__:
+            all += self._get_all_decorated_for_class(base_clazz, ttype)
+        return [prop for prop in all if isinstance(prop, ttype)]
+
+    def _get_all_decorated(self, ttype):
+        ds = [prop for prop in self._get_all_decorated_for_class(self.__class__, ttype)]
+        return [[prop.__get__(self, self.__class__), prop.fget.__name__] for prop in ds]
+
+    @property
+    def cfparams(self):
+        return self._get_all_decorated(cfparam)
+
+    @property
+    def cfresources(self):
+        return self._get_all_decorated(cfresource)
+
+    @property
+    def cfoutputs(self):
+        return self._get_all_decorated(cfoutput)
+
+    @property
+    def cfconditions(self):
+        return self._get_all_decorated(cfcondition)
+
+    def process(self):
+        if not self._processed:
+            self._processed = True
+            [self.template.add_parameter(p[0]) for p in self.cfparams]
+            [self.template.add_resource(r[0]) for r in self.cfresources]
+            [self.template.add_output(o[0]) for o in self.cfoutputs]
+            [self.template.add_condition(c[1], c[0]) for c in self.cfconditions]
+
+    @property
+    def output(self):
+        self.process()
+        return self.template.to_json()

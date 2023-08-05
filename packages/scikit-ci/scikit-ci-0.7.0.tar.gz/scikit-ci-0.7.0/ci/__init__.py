f237dbdc6d7e8132d4e10ce91c34705d2c10d416
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:18 +0000

    Add package scikit-ci/0.7.0/scikit-ci-0.7.0.tar.gz

---
+++ packages/scikit-ci/scikit-ci-0.7.0.tar.gz/scikit-ci-0.7.0/ci/__init__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+"""
+scikit-ci enables a centralized and simpler CI configuration for Python
+extensions.
+"""
+
+from .constants import STEPS
+from .driver import execute_step
+
+__author__ = 'The scikit-build team'
+__email__ = 'scikit-build@googlegroups.com'
+__version__ = '0.7.0'
+
+__all__ = ["execute_step", "STEPS"]

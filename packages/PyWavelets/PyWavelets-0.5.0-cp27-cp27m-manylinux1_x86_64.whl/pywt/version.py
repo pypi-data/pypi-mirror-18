b73ff9174631ba0826a0fd241326269ec1d61666
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:35 +0000

    Add package PyWavelets/0.5.0/PyWavelets-0.5.0-cp27-cp27m-manylinux1_x86_64.whl

---
+++ packages/PyWavelets/PyWavelets-0.5.0-cp27-cp27m-manylinux1_x86_64.whl/pywt/version.py
@@ -0,0 +1,10 @@
+
+# THIS FILE IS GENERATED FROM PYWAVELETS SETUP.PY
+short_version = '0.5.0'
+version = '0.5.0'
+full_version = '0.5.0'
+git_revision = ''
+release = True
+
+if not release:
+    version = full_version

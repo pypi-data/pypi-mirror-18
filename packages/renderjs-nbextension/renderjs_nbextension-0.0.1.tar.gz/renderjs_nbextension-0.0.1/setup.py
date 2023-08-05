cb40feefe4539f2577e9bf9d7f44db173c32569c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:09:04 +0000

    Add package renderjs-nbextension/0.0.1/renderjs_nbextension-0.0.1.tar.gz

---
+++ packages/renderjs-nbextension/renderjs_nbextension-0.0.1.tar.gz/renderjs_nbextension-0.0.1/setup.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+try:
+    from jupyterpip import cmdclass
+except:
+    print("!!!! NO JUPYTER PIP !!!!")
+
+setup(
+    name='renderjs_nbextension',
+    version='0.0.1',
+    packages=['renderjs_nbextension'],
+    install_requires=['jupyter-pip'],
+    cmdclass=cmdclass('renderjs_nbextension', 'renderjs_nbextension/main'
+                      # Flag for user install
+                      #,True
+    ),
+
+    # Not sure if needed
+    #package_data = {
+    #    '': ['*.js', '*.html']
+    #},
+
+    # PyPI Data
+    author='Sebastian Kreisel',
+    author_email='sebastian.kreisel@nexedi.com',
+    description='RenderJS gadgets for jupyter (frontend-part)',
+    keywords='renderjs jupyter nbextension',
+    license='GPL 2',
+    url='https://lab.nexedi.com/Kreisel/renderjs_extension'
+)

a362992273bda3bb3d1ba998a2e2f223e0ce5cb1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:57 +0000

    Add package awssamlpy3/1.0.3/awssamlpy3-1.0.3.tar.gz

---
+++ packages/awssamlpy3/awssamlpy3-1.0.3.tar.gz/awssamlpy3-1.0.3/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+ 
+setup(
+    name = 'awssamlpy3',
+    version = '1.0.3',
+    description = 'SAML federated API access for AWS',
+    author='Neeharika',
+    author_email='neeharika.mm@gmail.com',
+    classifiers=[
+        'Programming Language :: Python',
+        'Environment :: Console',
+    ],
+	install_requires=['beautifulsoup4','requests','html5lib','boto','configparser'],
+	scripts=['aws-saml']
+)

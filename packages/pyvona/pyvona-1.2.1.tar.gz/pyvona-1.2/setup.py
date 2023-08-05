5f841c109e11894f019d30517e5ba0cbcc8b2f79
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:05 +0000

    Add package pyvona/1.2.1/pyvona-1.2.1.tar.gz

---
+++ packages/pyvona/pyvona-1.2.1.tar.gz/pyvona-1.2/setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+import os
+from distutils.core import setup
+
+setup(name='pyvona',
+      version='1.2.1',
+      description='Python text-to-speech IVONA Wrapper',
+      author='Zachary Bears',
+      author_email='bears.zachary@gmail.com',
+      url='http://www.zacharybears.com/pyvona',
+      py_modules=['pyvona'],
+      install_requires=['requests']
+    )
+

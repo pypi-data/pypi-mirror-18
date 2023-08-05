303309b9524cbfb8653d2d18b316c6cca98e2e2a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:20 +0000

    Add package taski/0.1.7/taski-0.1.7.tar.gz

---
+++ packages/taski/taski-0.1.7.tar.gz/taski-0.1.7/app/base.py
@@ -0,0 +1,25 @@
+class Task():
+    def __init__(self):
+        self.id = 0
+        self.name = ""
+        self.pid = 0
+        self.done = False
+        self.ts_done = -1
+        self.ts_added = -1
+        self.meta_data = None
+
+    def __repr__(self):
+        return "[Task](" + self.name.encode('utf-8') + ")"
+
+class Project():
+    def __init__(self):
+        self.id = 0
+        self.name = ""
+        self.tasks = []
+        self.priority = 0
+
+    def __repr__(self):
+        return "[Project](" + self.name.encode('utf-8') + ")"
+
+def dlog(msg):
+    print(msg.encode('utf-8'))

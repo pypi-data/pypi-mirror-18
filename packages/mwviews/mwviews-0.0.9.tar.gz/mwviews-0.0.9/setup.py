5c0b1753e57057a9daacfcb41dd1a0143fa4edc8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:08:20 +0000

    Add package mwviews/0.0.9/mwviews-0.0.9.tar.gz

---
+++ packages/mwviews/mwviews-0.0.9.tar.gz/mwviews-0.0.9/setup.py
@@ -0,0 +1,27 @@
+import os
+
+from setuptools import find_packages, setup
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
+def requirements(fname):
+    return [line.strip()
+            for line in open(os.path.join(os.path.dirname(__file__), fname))]
+
+setup(
+    name='mwviews',
+    version='0.0.9',
+    author='Dan Andreescu',
+    author_email='dandreescu@wikimedia.org',
+    description='Tools for parsing and querying Wikimedia Foundation' +
+                ' pageview data, from both static dumps and online API',
+    license='MIT',
+    url='https://github.com/mediawiki-utilities/python-mwviews',
+    download_url='https://github.com/mediawiki-utilities/python-mwviews/tarball/0.0.9',
+    packages=find_packages(),
+    keywords=['wikimedia', 'wikipedia', 'pageview', 'api'],
+    install_requires=['requests', 'futures']
+)

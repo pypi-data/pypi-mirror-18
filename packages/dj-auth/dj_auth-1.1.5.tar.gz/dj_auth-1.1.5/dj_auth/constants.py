b94611521282521b056e5f440c72785eb5a1024c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:58 +0000

    Add package dj-auth/1.1.5/dj_auth-1.1.5.tar.gz

---
+++ packages/dj-auth/dj_auth-1.1.5.tar.gz/dj_auth-1.1.5/dj_auth/constants.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+from django.utils.translation import ugettext_lazy as _
+
+FILTER_ID = 0
+FILTERT_TEXT = _(u'Equals')
+
+EXCLUDE_ID = 1
+EXCLUDE_TEXT = _(u'Not Equals')
+
+FILTER_TYPE = (
+    (FILTER_ID, FILTERT_TEXT),
+    (EXCLUDE_ID, EXCLUDE_TEXT),
+)

cbaebed3ebabeaaae70b957e8a91ada6c5c5389f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:34 +0000

    Add package sigfox/0.1.0/sigfox-0.1.0.zip

---
+++ packages/sigfox/sigfox-0.1.0.zip/sigfox-0.1.0/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'sigfox',
+  packages = ['sigfox'],
+  version = '0.1.0',
+  description = 'Library for the RC1692HP-SIG Sigfox Module',
+  author = 'SkyGrid',
+  author_email = 'hey@skygrid.io',
+  license='MIT',
+  url = 'https://github.com/skygridio/sigfox-python',
+  keywords = ["skygrid", "iot", "internet of things", "sigfox"],
+  classifiers = [],
+)

1e9c30e909423289b2a72c8e52297483a40d4b56
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:16 +0000

    Add package django-gstorage/0.4.1/django-gstorage-0.4.1.tar.gz

---
+++ packages/django-gstorage/django-gstorage-0.4.1.tar.gz/django-gstorage-0.4.1/HISTORY.rst
@@ -0,0 +1,29 @@
+.. :changelog:
+
+Release history
+---------------
+
+0.4.1 (2016-10-30)
+++++++++++++++++++
+
+- Updated docstrings to sphinx style
+- Added documentation for gstorage.utils
+
+0.4.0 (2016-10-30)
+++++++++++++++++++
+
+**Features**
+
+- Refactored tests and added seperate test files for each module.
+- Updated documentation on readthedocs.io to include section about
+  bucket.
+
+0.3.0 (2016-10-28)
+++++++++++++++++++
+
+**Features**
+
+- ``gstorage.bucket.Bucket.get_default`` allowing easy access to
+  default bucket.
+- ``gstorage.bucket.Bucket.copydir`` allowing easy upload of files
+  to Google cloud.
---
+++ packages/django-gstorage/django-gstorage-0.4.1.tar.gz/django-gstorage-0.4.1/setup.py
@@ -0,0 +1,48 @@
+"""
+    django-gstorage
+    ~~~~~~~~~~~~~~~
+"""
+import re
+
+from setuptools import setup
+
+version = ''
+with open('gstorage/__init__.py', 'r') as fd:
+    version = re.search(r'^__version__\s*=\s*[\'"]([^\'"]*)[\'"]',
+        fd.read(), re.MULTILINE).group(1)
+
+with open('README.rst', 'r') as f:
+    readme = f.read()
+with open('HISTORY.rst', 'r') as f:
+    history = f.read()
+
+setup(
+    name='django-gstorage',
+    version=version,
+    description='Allow easy integration with Google storage for Django projects',
+    long_description=readme + '\n\n' + history,
+    author='Pradip Caulagi',
+    author_email='caulagi@gmail.com',
+    url='http://github.com/fyndiq/django-gstorage/',
+    packages=['gstorage'],
+    package_data={'': ['LICENSE', 'HISTORY.rst']},
+    include_package_data=True,
+    install_requires=[
+        'Django',
+        'gcloud',
+    ],
+    license='MIT',
+    zip_safe=False,
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ],
+)

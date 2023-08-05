91e59497a9b74110d1e88fb44de9a7757d96f8d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:15 +0000

    Add package shub-image/0.2.5/shub-image-0.2.5.tar.gz

---
+++ packages/shub-image/shub-image-0.2.5.tar.gz/shub-image-0.2.5/shub_image/tool.py
@@ -0,0 +1,43 @@
+import click
+import importlib
+import shub_image
+from shub_image.utils import missing_modules
+
+
+def missingmod_cmd(modules):
+    modlist = ", ".join(modules)
+
+    @click.command(help="*DISABLED* - requires %s" % modlist)
+    @click.pass_context
+    def cmd(ctx):
+        click.echo("Error: '%s' command requires %s" %
+                   (ctx.info_name, modlist))
+        ctx.exit(1)
+    return cmd
+
+
+@click.group(help="Scrapinghub release tool")
+@click.version_option(shub_image.__version__)
+def cli():
+    pass
+
+
+module_deps = {
+    "init": [],
+    "build": ["docker"],
+    "list": ["docker"],
+    "test": ["docker"],
+    "push": ["docker"],
+    "deploy": ["docker"],
+    "upload": ["docker"],
+    "check": [],
+}
+
+for command, modules in module_deps.items():
+    m = missing_modules(*modules)
+    if m:
+        cli.add_command(missingmod_cmd(m), command)
+    else:
+        module_path = "shub_image." + command
+        command_module = importlib.import_module(module_path)
+        cli.add_command(command_module.cli, command)

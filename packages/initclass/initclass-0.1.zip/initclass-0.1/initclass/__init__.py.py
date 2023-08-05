04c6f12fb7104abec39235a50312c73b09312aca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:36 +0000

    Add package initclass/0.1/initclass-0.1.zip

---
+++ packages/initclass/initclass-0.1.zip/initclass-0.1/initclass/__init__.py.py
@@ -0,0 +1,29 @@
+
+#Class with object
+
+class initclass:
+
+    num = 100;#publicvariable. visible outside. to access within objects, use self prefix
+  
+    #no parameter
+    def func(self):
+        print "hello"
+
+    #single parameter method
+    def retfunc(self, num2):        
+        #return self.num+num2+10 #use self / classname to refer local variables
+        return objclass.num+num2+10
+
+
+      #single parameter method
+    def retfunc(self, num2):        
+        #return self.num+num2+10 #use self / classname to refer local variables
+        return self.x+self.y+ +num2
+    
+
+    #init object
+    ##constructor . Use __Init__(self, realpart , imaginarypart)
+    def __init__(self, xval, yval):        
+        self.x = xval #public variable. can be used in other functions
+        self.y = yval #public variable. instead of x, y..can use any name
+        
---
+++ packages/initclass/initclass-0.1.zip/initclass-0.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='initclass',
+      version='0.1',
+      description ='Mahe\python package',
+      url='',
+      author='Mahe',
+      author_email='mayyanar@grundfos.com',
+      license='GPL',
+      packages=['initclass'],
+      zip_safe=False)
+      

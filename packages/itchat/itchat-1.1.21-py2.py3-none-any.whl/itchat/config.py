4b6a31bec23d5ff314ec018131f982e8d3132ef7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:09:20 +0000

    Add package itchat/1.1.21/itchat-1.1.21-py2.py3-none-any.whl

---
+++ packages/itchat/itchat-1.1.21-py2.py3-none-any.whl/itchat/config.py
@@ -0,0 +1,8 @@
+import os, platform
+
+DIR = os.getcwd()
+BASE_URL = 'https://login.weixin.qq.com'
+OS = platform.system() #Windows, Linux, Darwin
+
+WELCOME_WORDS = 'Hello World!'
+USER_AGENT = u'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.71 Safari/537.36'

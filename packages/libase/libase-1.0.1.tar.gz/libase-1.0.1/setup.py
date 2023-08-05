77448d0d72321321cc3c50489269be2cf9d24b6e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:30 +0000

    Add package libase/1.0.1/libase-1.0.1.tar.gz

---
+++ packages/libase/libase-1.0.1.tar.gz/libase-1.0.1/setup.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+# -*- encoding=utf8 -*-
+
+'''
+FileName:   setup.py
+Author:     Fasion Chan
+@contact:   fasionchan@gmail.com
+@version:   $Id$
+
+Description:
+
+Changelog:
+
+'''
+
+VERSION = '1.0.1'
+
+from setuptools import (
+    setup,
+    )
+
+setup(
+    name='libase',
+    version=VERSION,
+    author='Fasion Chan',
+    author_email='fasionchan@gmail.com',
+    packages=[
+        'libase',
+        #'libase.multirun',
+        #'libase.platform',
+        #'libase.platform.syscall',
+        #'libase.server',
+        #'libase.util',
+        ],
+    scripts=[
+        ],
+    package_data={
+        },
+    install_requires=[
+        ],
+    )

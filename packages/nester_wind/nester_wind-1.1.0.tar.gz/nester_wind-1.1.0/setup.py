ba9cada2d3ad7bc2b7eddef568238f87891d8620
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:49 +0000

    Add package nester_wind/1.1.0/nester_wind-1.1.0.tar.gz

---
+++ packages/nester_wind/nester_wind-1.1.0.tar.gz/nester_wind-1.1.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+	name = 'nester_wind',
+	version = '1.1.0',
+	py_modules = ['nester_wind'],
+	author = 'windpenguin',
+	author_email = 'windpenguin@gmail.com',
+	url = 'http://windpenguin.com',
+	description = 'A simple printer of nested lists',
+	) 

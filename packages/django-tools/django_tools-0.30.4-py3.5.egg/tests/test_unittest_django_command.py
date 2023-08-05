ad114c76130df4fe10fbf2ea971a0da54823e283
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:31 +0000

    Add package django-tools/0.30.4/django_tools-0.30.4-py3.5.egg

---
+++ packages/django-tools/django_tools-0.30.4-py3.5.egg/tests/test_unittest_django_command.py
@@ -0,0 +1,43 @@
+# coding: utf-8
+
+"""
+    Test django_tools.unittest_utils.django_command
+"""
+
+from __future__ import unicode_literals, print_function
+
+
+import os
+
+from unittest import TestCase
+
+import django_tools
+from django_tools.unittest_utils.django_command import DjangoCommandMixin
+
+
+MANAGE_DIR = os.path.abspath(os.path.join(os.path.dirname(django_tools.__file__), ".."))
+
+
+class TestDjangoCommand(DjangoCommandMixin, TestCase):
+    def test_help(self):
+        output = self.call_manage_py(["--help"], manage_dir=MANAGE_DIR)
+
+        self.assertNotIn("ERROR", output)
+        self.assertIn("[django]", output)
+        self.assertIn("Type 'manage.py help <subcommand>' for help on a specific subcommand.", output)
+
+    def test_set_env(self):
+        """
+        Test if we can set "DJANGO_SETTINGS_MODULE"
+        """
+        env = dict(os.environ)
+        env["DJANGO_SETTINGS_MODULE"]="does-not-exist"
+
+        with self.assertRaises(AssertionError) as cm:
+            self.call_manage_py(["--help"], manage_dir=MANAGE_DIR, env=env)
+
+        output = "\n".join(cm.exception.args)
+        # print(output)
+
+        self.assertIn("subprocess exist status == 1", output)
+        self.assertIn("ImportError: No module named 'does-not-exist'", output)

8648f669bb1494e4f38bc54c716d55d9cf9204ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:02 +0000

    Add package ciscosparkapi/0.4/ciscosparkapi-0.4.tar.gz

---
+++ packages/ciscosparkapi/ciscosparkapi-0.4.tar.gz/ciscosparkapi-0.4/ciscosparkapi/__init__.py
@@ -0,0 +1,120 @@
+# -*- coding: utf-8 -*-
+"""Python API wrapper for the Cisco Spark APIs."""
+
+
+from __future__ import absolute_import
+from builtins import object
+from six import string_types
+
+import os
+
+from .exceptions import ciscosparkapiException, SparkApiError
+from .restsession import RestSession
+from .api.accesstokens import AccessToken, AccessTokensAPI
+from .api.people import Person, PeopleAPI
+from .api.rooms import Room, RoomsAPI
+from .api.memberships import Membership, MembershipsAPI
+from .api.messages import Message, MessagesAPI
+from .api.teams import Team, TeamsAPI
+from .api.teammemberships import TeamMembership, TeamMembershipsAPI
+from .api.webhooks import Webhook, WebhooksAPI
+
+
+__author__ = "Chris Lunsford"
+__author_email__ = "chrlunsf@cisco.com"
+__copyright__ = "Copyright (c) 2016 Cisco Systems, Inc."
+__license__ = "MIT"
+
+
+# Versioneer version control
+from ._version import get_versions
+__version__ = get_versions()['version']
+del get_versions
+
+
+DEFAULT_BASE_URL = 'https://api.ciscospark.com/v1/'
+
+
+class CiscoSparkAPI(object):
+    """Cisco Spark API wrapper class."""
+
+    def __init__(self, access_token=None, base_url=DEFAULT_BASE_URL,
+                 timeout=60):
+        """Init a new CiscoSparkAPI object.
+
+        An access token must be used when interacting with the Cisco Spark API.
+        This package supports two methods for you to provide that access token:
+
+          1. You may manually specify the access token via the access_token
+             argument, when creating a new CiscoSparkAPI object.
+
+          2. If an access_token argument is not supplied, the package checks
+             for a SPARK_ACCESS_TOKEN environment variable, and if available,
+             it uses the value of this environment variable as the access_token
+             when new CiscoSparkAPI objects are created.
+
+        A ciscosparkapiException is raised if an access token is not provided
+        via one of these two methods.
+
+        Args:
+            access_token(string_types): The access token to be used for API
+                calls to the Cisco Spark service.  Defaults to checking for a
+                SPARK_ACCESS_TOKEN environment variable.
+            base_url(string_types): The base URL to be prefixed to the
+                individual API endpoint suffixes.
+                Defaults to ciscosparkapi.DEFAULT_BASE_URL.
+            timeout(int): Timeout (in seconds) for RESTful HTTP requests.
+                Defaults to 60 seconds.
+
+        Returns:
+            CiscoSparkAPI: A new CiscoSparkAPI connection object.
+
+        Raises:
+            AssertionError: If the parameter types are incorrect.
+            ciscosparkapiException: If an access token is not provided via the
+                access_token argument or SPARK_ACCESS_TOKEN environment
+                variable.
+
+        """
+        # Process args
+        assert access_token is None or isinstance(access_token, string_types)
+        assert isinstance(base_url, string_types)
+        assert isinstance(timeout, int)
+        spark_access_token = os.environ.get('SPARK_ACCESS_TOKEN', None)
+        access_token = access_token if access_token else spark_access_token
+        if not access_token:
+            error_message = "You must provide an access token to interact " \
+                            "with the Cisco Spark APIs, either via the " \
+                            "access_token argument or via a " \
+                            "SPARK_ACCESS_TOKEN environment variable.  " \
+                            "None provided."
+            raise ciscosparkapiException(error_message)
+        session_args = {u'timeout': timeout}
+
+        # Create the API session
+        # All of the API calls associated with a CiscoSparkAPI object will
+        # leverage a single RESTful 'session' connecting to the Cisco Spark
+        # cloud.
+        self.session = RestSession(access_token, base_url, **session_args)
+
+        # Spark API wrappers
+        self.access_tokens = AccessTokensAPI(self.base_url, timeout=timeout)
+        self.people = PeopleAPI(self.session)
+        self.rooms = RoomsAPI(self.session)
+        self.memberships = MembershipsAPI(self.session)
+        self.messages = MessagesAPI(self.session)
+        self.teams = TeamsAPI(self.session)
+        self.team_memberships = TeamMembershipsAPI(self.session)
+        self.webhooks = WebhooksAPI(self.session)
+
+    @property
+    def access_token(self):
+        return self.session.access_token
+
+    @property
+    def base_url(self):
+        return self.session.base_url
+
+    @property
+    def timeout(self):
+        return self.session.timeout
---
+++ packages/ciscosparkapi/ciscosparkapi-0.4.tar.gz/ciscosparkapi-0.4/ciscosparkapi/api/people.py
@@ -0,0 +1,183 @@
+# -*- coding: utf-8 -*-
+"""Cisco Spark People-API wrapper classes.
+
+Classes:
+    Person: Models a Spark 'person' JSON object as a native Python object.
+    PeopleAPI: Wrappers the Cisco Spark People-API and exposes the API calls as
+        Python method calls that return native Python objects.
+
+"""
+
+
+from builtins import object
+from six import string_types
+
+from ciscosparkapi.exceptions import ciscosparkapiException
+from ciscosparkapi.helper import generator_container
+from ciscosparkapi.restsession import RestSession
+from ciscosparkapi.sparkdata import SparkData
+
+
+__author__ = "Chris Lunsford"
+__author_email__ = "chrlunsf@cisco.com"
+__copyright__ = "Copyright (c) 2016 Cisco Systems, Inc."
+__license__ = "MIT"
+
+
+class Person(SparkData):
+    """Model a Spark 'person' JSON object as a native Python object."""
+
+    def __init__(self, json):
+        """Init a new Person data object from a JSON dictionary or string.
+
+        Args:
+            json(dict, string_types): Input JSON object.
+
+        Raises:
+            TypeError: If the input object is not a dictionary or string.
+
+        """
+        super(Person, self).__init__(json)
+
+    @property
+    def id(self):
+        return self._json['id']
+
+    @property
+    def emails(self):
+        return self._json['emails']
+
+    @property
+    def displayName(self):
+        return self._json['displayName']
+
+    @property
+    def avatar(self):
+        return self._json['avatar']
+
+    @property
+    def created(self):
+        return self._json['created']
+
+    @property
+    def lastActivity(self):
+        return self._json['lastActivity']
+
+    @property
+    def status(self):
+        return self._json['status']
+
+
+class PeopleAPI(object):
+    """Cisco Spark People-API wrapper class.
+
+    Wrappers the Cisco Spark People-API and exposes the API calls as Python
+    method calls that return native Python objects.
+
+    Attributes:
+        session(RestSession): The RESTful session object to be used for API
+            calls to the Cisco Spark service.
+
+    """
+
+    def __init__(self, session):
+        """Init a new PeopleAPI object with the provided RestSession.
+
+        Args:
+            session(RestSession): The RESTful session object to be used for
+                API calls to the Cisco Spark service.
+
+        Raises:
+            AssertionError: If the parameter types are incorrect.
+
+        """
+        assert isinstance(session, RestSession)
+        super(PeopleAPI, self).__init__()
+        self.session = session
+
+    @generator_container
+    def list(self, email=None, displayName=None, max=None):
+        """List people by email or displayName.
+
+        An email address or displayName must be provided.
+
+        This method supports Cisco Spark's implementation of RFC5988 Web
+        Linking to provide pagination support.  It returns a generator
+        container that incrementally yield all people returned by the
+        query.  The generator will automatically request additional 'pages' of
+        responses from Spark as needed until all responses have been returned.
+        The container makes the generator safe for reuse.  A new API call will
+        be made, using the same parameters that were specified when the
+        generator was created, every time a new iterator is requested from the
+        container.
+
+        Args:
+            email(string_types): The e-mail address of the person to be found.
+            displayName(string_types): The complete or beginning portion of
+                the displayName to be searched.
+            max(int): Limits the maximum number of people returned from the
+                Spark service per request.
+
+        Yields:
+            Person: The the next person from the Cisco Spark query.
+
+        Raises:
+            AssertionError: If the parameter types are incorrect.
+            ciscosparkapiException: If neither an email or displayName argument
+                is specified.
+            SparkApiError: If the Cisco Spark cloud returns an error.
+
+        """
+        # Process args
+        assert email is None or isinstance(email, string_types)
+        assert displayName is None or isinstance(displayName, string_types)
+        assert max is None or isinstance(max, int)
+        params = {}
+        if email:
+            params['email'] = email
+        elif displayName:
+            params['displayName'] = displayName
+        else:
+            error_message = "An email or displayName argument must be " \
+                            "specified."
+            raise ciscosparkapiException(error_message)
+        if max:
+            params['max'] = max
+        # API request - get items
+        items = self.session.get_items('people', params=params)
+        # Yield Person objects created from the returned items JSON objects
+        for item in items:
+            yield Person(item)
+
+    def get(self, personId):
+        """Get person details, by personId.
+
+        Args:
+            personId(string_types): The personID of the person.
+
+        Returns:
+            Person: With the details of the requested person.
+
+        Raises:
+            AssertionError: If the parameter types are incorrect.
+            SparkApiError: If the Cisco Spark cloud returns an error.
+
+        """
+        # Process args
+        assert isinstance(personId, string_types)
+        # API request
+        json_obj = self.session.get('people/'+personId)
+        # Return a Person object created from the response JSON data
+        return Person(json_obj)
+
+    def me(self):
+        """Get the person details of the account accessing the API 'me'.
+
+        Raises:
+            SparkApiError: If the Cisco Spark cloud returns an error.
+
+        """
+        # API request
+        json_obj = self.session.get('people/me')
+        # Return a Person object created from the response JSON data
+        return Person(json_obj)

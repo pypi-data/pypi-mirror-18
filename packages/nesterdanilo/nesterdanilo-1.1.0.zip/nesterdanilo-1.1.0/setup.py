baa86afa64c16a0d124617c2ff36df6a326a3b7d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:02 +0000

    Add package nesterdanilo/1.1.0/nesterdanilo-1.1.0.zip

---
+++ packages/nesterdanilo/nesterdanilo-1.1.0.zip/nesterdanilo-1.1.0/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+        name='nesterdanilo',
+        version='1.1.0',
+        py_modules=['nesterdanilo'],
+        author='Danilo Setúbal',
+        author_email='danilosetubal@gmail.com',
+        url='http://www.danilosetubal.com.br',
+        description='Uma função simples que processa e exibe os'
+            'itens de uma lista com aninhamento ou não.'
+)

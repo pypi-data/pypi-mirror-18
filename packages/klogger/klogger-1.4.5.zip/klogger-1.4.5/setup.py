931718c4e37d36b12219e9ff2e397759e706b909
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:07:58 +0000

    Add package klogger/1.4.5/klogger-1.4.5.zip

---
+++ packages/klogger/klogger-1.4.5.zip/klogger-1.4.5/setup.py
@@ -0,0 +1,3 @@
+from distutils.core import setup
+
+setup(name="klogger", version="1.4.5", description="Use minimal efforts to log simple scripts to multi-process scripts.", author="Kang Min Yoo", author_email="kaniblurous@gmail.com", py_modules=["klogger"])

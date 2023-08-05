d20f5dcc165972b6e3d9cf43651f51feb906c7e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:48 +0000

    Add package PyWavelets/0.5.0/PyWavelets-0.5.0.tar.gz

---
+++ packages/PyWavelets/PyWavelets-0.5.0.tar.gz/PyWavelets-0.5.0/pywt/version.py
@@ -0,0 +1,10 @@
+
+# THIS FILE IS GENERATED FROM PYWAVELETS SETUP.PY
+short_version = '0.5.0'
+version = '0.5.0'
+full_version = '0.5.0'
+git_revision = '2d2ca0a658f1aba2f4213463c341ea2458e3cd00'
+release = True
+
+if not release:
+    version = full_version

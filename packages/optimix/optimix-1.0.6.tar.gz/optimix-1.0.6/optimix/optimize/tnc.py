13270ecfc4e89b71238a3357c9780b107c54eb05
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:39 +0000

    Add package optimix/1.0.6/optimix-1.0.6.tar.gz

---
+++ packages/optimix/optimix-1.0.6.tar.gz/optimix-1.0.6/optimix/optimize/tnc.py
@@ -0,0 +1,52 @@
+from numpy import asarray
+from numpy import concatenate
+
+from scipy.optimize import fmin_tnc
+
+from ..util import as_data_function
+
+
+def do_flatten(x):
+    if isinstance(x, list) or isinstance(x, tuple):
+        return concatenate([asarray(xi).ravel() for xi in x])
+    return concatenate(x)
+
+
+class ProxyFunction(object):
+
+    def __init__(self, function, purpose, progress, negative):
+        self._function = as_data_function(function, purpose)
+        self._signal = -1 if negative else +1
+
+    def value(self):
+        return self._signal * self._function.value()
+
+    def gradient(self):
+        return [self._signal * gi for gi in self._function.gradient()]
+
+    def __call__(self, x):
+        x = asarray(x).ravel()
+        self._function.variables().from_flat(x)
+        v = self.value()
+        g = do_flatten(self.gradient())
+        return v, g
+
+    def set_solution(self, x):
+        self._function.variables().from_flat(asarray(x).ravel())
+
+    def get_solution(self):
+        return self._function.variables().flatten()
+
+
+def _minimize(proxy_function):
+    x0 = proxy_function.get_solution()
+    r = fmin_tnc(proxy_function, x0, disp=0)
+    proxy_function.set_solution(r[0])
+
+
+def minimize(function, purpose='learn', progress=None):
+    return _minimize(ProxyFunction(function, purpose, progress, False))
+
+
+def maximize(function, purpose='learn', progress=None):
+    return _minimize(ProxyFunction(function, purpose, progress, True))

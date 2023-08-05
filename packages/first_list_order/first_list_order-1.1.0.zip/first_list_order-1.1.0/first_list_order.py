a2524f26e76c12ff89d3db468d324027dfbd3712
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:30 +0000

    Add package first_list_order/1.1.0/first_list_order-1.1.0.zip

---
+++ packages/first_list_order/first_list_order-1.1.0.zip/first_list_order-1.1.0/first_list_order.py
@@ -0,0 +1,16 @@
+"""Esse módulo nester.py fornece um função chamada print_lol que permite processar e imprimir uma lista ela estando aninhadas ou não."""
+
+def print_lol(the_list, level):
+
+    """Essa função requer um argumento posicional chamado "the_list", que é qualquer lista em python. Cada item de dados na lista fornecida é
+(recursivamente) impresso na tela em sua propria linha"""
+    
+    for each_item in the_list:
+        if isinstance(each_item, list):
+            print_lol(each_item, level+1)
+        else:
+            for tab_stop in range(level):
+                print("\t", end='')
+            print(each_item)
+
+            
---
+++ packages/first_list_order/first_list_order-1.1.0.zip/first_list_order-1.1.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+    name    ='first_list_order',
+    version ='1.1.0',
+    py_modules = ['first_list_order'],
+    author      = 'mateusalipy',
+    author_email = 'mateusalifontes@gmail.com',
+    description  = 'A simple printer of nested list',
+    )

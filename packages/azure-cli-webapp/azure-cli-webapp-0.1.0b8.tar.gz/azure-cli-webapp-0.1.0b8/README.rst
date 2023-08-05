c6a119fb39f3c904f2ef616e0e4a681a250f7228
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:26 +0000

    Add package azure-cli-webapp/0.1.0b8/azure-cli-webapp-0.1.0b8.tar.gz

---
+++ packages/azure-cli-webapp/azure-cli-webapp-0.1.0b8.tar.gz/azure-cli-webapp-0.1.0b8/README.rst
@@ -0,0 +1,7 @@
+Microsoft Azure CLI 'webapp' Command Module
+==================================
+
+This package is for the 'webapp' module.
+i.e. 'az webapp'
+
+
---
+++ packages/azure-cli-webapp/azure-cli-webapp-0.1.0b8.tar.gz/azure-cli-webapp-0.1.0b8/azure/cli/command_modules/webapp/__init__.py
@@ -0,0 +1,10 @@
+#---------------------------------------------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License. See License.txt in the project root for license information.
+#---------------------------------------------------------------------------------------------
+
+# pylint: disable=unused-import
+
+import azure.cli.command_modules.webapp._params
+import azure.cli.command_modules.webapp.generated
+import azure.cli.command_modules.webapp._help

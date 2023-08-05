d9ce4edefa63be19a9c5976499adf66e1d25e8c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:59 +0000

    Add package scaleapi/0.1.5/scaleapi-0.1.5.tar.gz

---
+++ packages/scaleapi/scaleapi-0.1.5.tar.gz/scaleapi-0.1.5/setup.py
@@ -0,0 +1,18 @@
+from distutils.core import setup
+setup(
+    name = 'scaleapi',
+    packages = ['scaleapi'],
+    version = '0.1.5',
+    description = 'The official Python client library for the Scale API, the API for human labor.',
+    author = 'Calvin Huang',
+    author_email = 'c@lvin.me',
+    url = 'https://github.com/scaleapi/scaleapi-python-client',
+    download_url = 'https://github.com/scaleapi/scaleapi-python-client/tarball/0.1.2',
+    keywords = ['scale', 'scaleapi', 'humans', 'tasks', 'categorization', 'transcription', 'annotation', 'comparison', 'data collection', 'phone call'],
+    install_requires = ['requests', 'enum34'],
+    classifiers = ['Programming Language :: Python :: 2.7',
+                   'Programming Language :: Python :: 3.5',
+                   'License :: OSI Approved :: MIT License',
+                   'Intended Audience :: Developers',
+                   'Topic :: Software Development :: Libraries']
+)

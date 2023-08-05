a7444989b2a6f354183cd81deb010155a7ab2952
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:52 +0000

    Add package array-to-latex/0.30b/array_to_latex-0.30b.tar.gz

---
+++ packages/array-to-latex/array_to_latex-0.30b.tar.gz/array_to_latex-0.30b/setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='array_to_latex',
+      version='0.30b',
+      description='Numpy arrays converted to formatted LaTeX array',
+      author='Joseph C. Slater',
+      author_email='joseph.c.slater@gmail.com',
+      url='https://github.com/josephcslater/array_to_latex/',
+      download_url='https://github.com/josephcslater/array_to_latex/archive/0.30b.tar.gz',
+      packages=['array_to_latex'],
+      keywords=['latex','array','format']
+      )

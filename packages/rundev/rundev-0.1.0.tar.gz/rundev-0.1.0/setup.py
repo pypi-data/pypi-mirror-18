a35d26d918e6a7c3cda6d86a15dfd56111c50189
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:55:41 +0000

    Add package rundev/0.1.0/rundev-0.1.0.tar.gz

---
+++ packages/rundev/rundev-0.1.0.tar.gz/rundev-0.1.0/setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# coding=utf-8
+from distutils.core import setup
+
+setup(
+    name='rundev',
+    version='0.1.0',
+    description='Develop and deploy multiple process applications',
+    author='Michał Zieliński',
+    author_email='michal@zielinscy.org.pl',
+    url='https://github.com/zielmicha/rundev',
+    scripts=['rundev'],
+    install_requires=[],
+)

b3d50b17aa54917154fa7008a3191feb80e5c80e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:10 +0000

    Add package myo/0.27.0/myo-0.27.0.tar.gz

---
+++ packages/myo/myo-0.27.0.tar.gz/myo-0.27.0/myo/command.py
@@ -0,0 +1,214 @@
+import abc
+from functools import singledispatch  # type: ignore
+
+from amino import Path, __, Just, List, L, Maybe, _, Map
+
+from ribosome.record import list_field, field, maybe_field, bool_field
+from ribosome.util.callback import parse_callback_spec
+
+from myo.record import Record, Named
+from myo.util import ident_field, Ident
+from myo.logging import Logging
+from myo.util.ident import Key
+
+
+default_signals = List('int', 'term', 'kill')
+
+
+class Line(Logging, metaclass=abc.ABCMeta):
+
+    def __init__(self, line) -> None:
+        self.line = line
+
+    @abc.abstractmethod
+    def resolve(self, vim, args: List) -> Maybe[str]:
+        ...
+
+
+class StrictLine(Line):
+
+    def resolve(self, vim, *args):
+        return Just(self.line)
+
+
+class EvalLine(Line):
+
+    def resolve(self, vim, *args):
+        return parse_callback_spec(self.line) // __(vim, *args)
+
+
+class Command(Named):
+    log_path = maybe_field(Path)
+    parser = maybe_field(str)
+    transient = bool_field()
+    langs = list_field()
+    kill = bool_field()
+    signals = list_field(str, initial=default_signals)
+    eval = bool_field()
+    args = list_field()
+
+    @property
+    def _str_extra(self):
+        return super()._str_extra.cat_m(self.langs.empty.no.m(self.langs))
+
+    @property
+    def _str_extra_named(self):
+        return Map(eval=self.eval)
+
+    @property
+    def desc(self):
+        info = self._info.mk_string(' ∘ ')
+        tail = ': {}'.format(info) if info else ''
+        return '{} {}{}'.format(self._type_desc, self.name, tail)
+
+    @property
+    def _info_fields(self):
+        return List('langs', 'parser', 'log_path')
+
+    @property
+    def _info(self) -> List[str]:
+        return self._info_fields // self._field_info
+
+    def _field_info(self, name: str):
+        v = getattr(self, name)
+        fmt = '⎡{} -> {}⎤'.format
+        if isinstance(v, Maybe):
+            return v / L(fmt)(name, _)
+        elif isinstance(v, List):
+            return v.empty.no.maybe(fmt(name, v.mk_string(',')))
+        else:
+            return Just(fmt(name, v))
+
+    @property
+    def _type_desc(self):
+        return 'C'
+
+    @property
+    def main_id(self) -> Ident:
+        return self.uuid
+
+    @property
+    def main_name(self):
+        return self.name
+
+    @property
+    def main_key(self):
+        return Key(uuid=self.main_id, name=self.main_name)
+
+    @property
+    def effective(self):
+        return self
+
+
+class VimCommand(Command):
+
+    @property
+    def _type_desc(self):
+        return 'VC'
+
+
+class ShellCommand(Command):
+    line = field(str)
+    shell = ident_field()
+
+    @property
+    def _type_desc(self):
+        return 'SC'
+
+    @property
+    def _info_fields(self):
+        return super()._info_fields + List('shell')
+
+    @property
+    def effective_line(self):
+        tpe = EvalLine if self.eval else StrictLine
+        return tpe(self.line)
+
+
+class Shell(ShellCommand):
+    target = ident_field()
+
+    @property
+    def _type_desc(self):
+        return 'S'
+
+
+class TransientCommand(Command):
+    command = field(Command)
+    line = field(str)
+
+    def __new__(cls, prefix='transient', transient=None, command=None,
+                name=None, **kw):
+        name = name or '{}_{}_{}'.format(prefix, command.name,
+                                         List.random_string(5))
+        return super().__new__(cls, name=name, transient=True, command=command,
+                               **kw)
+
+    @property
+    def _str_extra(self):
+        return super()._str_extra.cat(self.command)
+
+    @property
+    def effective(self):
+        return self.command.set(name=self.name, line=self.line)
+
+    @property
+    def main_id(self):
+        return self.command.main_id
+
+    @property
+    def main_name(self):
+        return self.command.name
+
+
+@singledispatch
+def _history_cmp(cmd):
+    return cmd.name
+
+
+@_history_cmp.register(TransientCommand)
+def _history_cmp_transient(cmd):
+    return '{}|{}'.format(cmd.command.name, cmd.line)
+
+
+class Commands(Record):
+    no_such_command_error = 'no such command: {}'
+    no_such_shell_error = 'so such shell: {}'
+    no_latest_command_error = 'history is empty'
+
+    commands = list_field()
+    history = list_field()
+    transient_cmd = maybe_field(Command)
+
+    def __add__(self, command: Command):
+        return self.append1.commands(command)
+
+    def __getitem__(self, ident: Ident):
+        return (
+            self.commands.find(__.has_ident(ident))
+            .o(lambda: self.history.find(__.has_ident(ident)))
+            .to_either(self.no_such_command_error.format(ident))
+        )
+
+    @property
+    def _str_extra(self):
+        return super()._str_extra.cat(self.commands)
+
+    def shell(self, ident):
+        pred = lambda a: isinstance(a, Shell) and a.has_ident(ident)
+        return (self.commands.find(pred)
+                .to_either(self.no_such_shell_error.format(ident)))
+
+    def add_history(self, cmd):
+        new = self.history.cons(cmd).distinct_by(_history_cmp)
+        return self.set(history=new)
+
+    def transient_command(self, cmd):
+        return self.set(transient_cmd=Just(cmd))
+
+    @property
+    def latest_command(self):
+        return self.history.head.to_either(self.no_latest_command_error)
+
+__all__ = ('Commands', 'Command', 'VimCommand', 'ShellCommand', 'Shell',
+           'TransientCommand')
---
+++ packages/myo/myo-0.27.0.tar.gz/myo-0.27.0/myo/plugins/command/main.py
@@ -0,0 +1,243 @@
+from collections import namedtuple
+
+from ribosome.machine import may_handle, handle
+from ribosome.machine.transition import Fatal
+from ribosome.util.callback import parse_callback_spec
+from ribosome.machine.base import UnitTask
+from ribosome.record import decode_json, encode_json
+
+from myo.state import MyoComponent, MyoTransitions
+
+from amino import L, _, List, Try, __, Maybe, Map, I, Task, Just
+from myo.command import (Command, VimCommand, ShellCommand, Shell,
+                         TransientCommand)
+from myo.util import amend_options
+from myo.plugins.core.message import Parse, ParseOutput, StageI
+from myo.plugins.command.message import (Run, ShellRun, Dispatch, AddCommand,
+                                         AddShellCommand, AddShell,
+                                         AddVimCommand, SetShellTarget,
+                                         CommandExecuted, RunTest, RunVimTest,
+                                         CommandAdded, CommandShow, RunLatest,
+                                         LoadHistory, StoreHistory, RunLine)
+from myo.plugins.command.util import assemble_vim_test_line
+
+RunInShell = namedtuple('RunInShell', ['shell'])
+
+
+class CommandTransitions(MyoTransitions):
+    _test_cmd_name = '<test>'
+
+    def _add(self, tpe: type, **strict):
+        return self._from_opt(tpe, **strict) / self._add_command
+
+    def _add_command(self, cmd: Command):
+        return List(
+            self.data.cat(cmd), CommandAdded(cmd, options=self.msg.options)
+        )
+
+    @may_handle(StageI)
+    def stage_i(self):
+        line = self.vim.vars(self._last_test_line_var) | ''
+        shell = self.vim.vars(self._last_test_shell_var)
+        opt1 = Map(line=line)
+        opt = shell / (lambda a: opt1.cat(('shell', a))) | opt1
+        return (
+            AddShellCommand(name=self._test_cmd_name, options=opt),
+            LoadHistory()
+        )
+
+    @property
+    def _history_var(self):
+        return 'Myo_history'
+
+    @property
+    def _last_test_line_var(self):
+        return 'Myo_last_test_line'
+
+    @property
+    def _last_test_shell_var(self):
+        return 'Myo_last_test_shell'
+
+    @handle(LoadHistory)
+    def load_history(self):
+        return (
+            self.vim.vars.s(self._history_var) //
+            L(decode_json)(_).to_either(Fatal('failed to load history')) /
+            self.data.commands.setter.history /
+            self.data.setter.commands
+        )
+
+    @may_handle(StoreHistory)
+    def store_history(self):
+        return UnitTask(
+            Task.delay(encode_json, self.data.commands.history).join_either /
+            L(self).vim.vars.set(self._history_var, _)
+        )
+
+    @handle(AddCommand)
+    def add_command(self):
+        return self._add(Command, name=self.msg.name)
+
+    @may_handle(AddVimCommand)
+    def add_vim_command(self):
+        return self._add(VimCommand, name=self.msg.name)
+
+    @handle(AddShellCommand)
+    def add_shell_command(self):
+        return self._add(ShellCommand, name=self.msg.name)
+
+    @handle(AddShell)
+    def add_shell(self):
+        return self._from_opt(Shell, name=self.msg.name) / (
+            lambda shell: (
+                self._add_command(shell) +
+                (shell.target / L(SetShellTarget)(shell, _) / _.pub).to_list
+            )
+        )
+
+    @may_handle(CommandAdded)
+    def command_added(self):
+        if self.msg.options.get('start').exists(I):
+            return Run(self.msg.command.uuid)
+
+    @handle(Run)
+    def run(self):
+        return (
+            self._command_fatal(self.msg.command) /
+            L(Dispatch)(_, self.msg.options)
+        )
+
+    @may_handle(RunLine)
+    def run_line(self):
+        return Dispatch(ShellCommand(name='run_line',
+                                     line=self.msg.args.join_tokens),
+                        Map(target=self.msg.target))
+
+    @handle(ShellRun)
+    def run_in_shell(self):
+        line = self.msg.options.get('line') | ''
+        def send(cmd):
+            return (
+                self.data.transient_command(cmd),
+                Dispatch(cmd, self.msg.options)
+            )
+        # double Maybe is necessary - the outer indicates success, the
+        # inner is the type of the ShellCommand field
+        return (
+            self._shell_fatal(self.msg.shell) /
+            _.ident /
+            Maybe /
+            (lambda a: ShellCommand(name='shell_run', shell=a, line=line,
+                                    transient=True)) /
+            send
+        )
+
+    @handle(RunLatest)
+    def run_latest(self):
+        return self._latest_command_fatal / Dispatch
+
+    @handle(Dispatch)
+    def dispatch(self):
+        cmd = self.msg.command
+        self.vim.vars.set_p('last_command', dict(name=cmd.name))
+        self.vim.pautocmd('RunCommand')
+        return self.data.dispatch_message(cmd, self.msg.options) / _.pub
+
+    @may_handle(CommandExecuted)
+    def command_executed(self):
+        cmd = self.msg.command
+        def set_log(path, cmd_lens):
+            def run(cmd):
+                self.log.debug('setting {} log to {}'.format(cmd.name, path))
+                return cmd.set(log_path=Just(path))
+            return cmd_lens.modify(run)
+        data = (
+            (self.msg.log_path.to_either('no log path') &
+             self.data.command_lens(cmd.main_key))
+            .map2(set_log) |
+            self.data
+        )
+        new = data.commands.add_history(cmd)
+        return data.set(commands=new), StoreHistory()
+
+    @handle(Parse)
+    def parse(self):
+        def find_log(c):
+            return ((self.data.command(c.main_key) // _.log_path)
+                    .to_either('{} has no log'.format(c)))
+        def parse(c, l):
+            self.log.debug('parsing {} from {}'.format(c, l))
+            return (
+                Try(l.read_text) /
+                List.lines /
+                L(ParseOutput)(c, _, l, self.msg.options) /
+                _.pub
+            )
+        cmd = (
+            self.msg.options.get('command')
+            .cata(self.data.command, lambda: self._latest_command_fatal)
+        )
+        log_path = cmd // find_log
+        return (cmd & log_path).flat_map2(parse).lmap(Fatal)
+
+    @handle(RunTest)
+    def run_test(self):
+        opt = self.msg.options
+        return (
+            opt.get('ctor')
+            .to_either('no test ctor specified') //
+            self._assemble //
+            L(self._run_test_line)(opt - 'ctor', _)
+        ).to_either('failed to setup test line').lmap(Fatal)
+
+    @handle(RunVimTest)
+    def run_vim_test(self):
+        return (
+            assemble_vim_test_line(self.vim) //
+            L(self._run_test_line)(self.msg.options, _)
+        ).lmap(Fatal)
+
+    @may_handle(CommandShow)
+    def show(self):
+        msg = self.data.commands.commands / _.desc
+        self.log.info(msg.join_lines)
+
+    def _command_fatal(self, ident):
+        return self.data.command(self.msg.command).lmap(Fatal)
+
+    def _shell_fatal(self, ident):
+        return self.data.shell(ident).lmap(Fatal)
+
+    @property
+    def _latest_command_fatal(self):
+        return self.data.commands.latest_command.lmap(Fatal)
+
+    def _assemble(self, ctor):
+        return parse_callback_spec(ctor) // __(self.vim)
+
+    def _run_test_line(self, options, line):
+        glangs = self.vim.buffer.vars.pl('test_langs')
+        langs = options.get('langs').or_else(glangs) | List()
+        shell = self.vim.buffer.pvar_or_global('test_shell')
+        target = self.vim.buffer.pvar_or_global('test_target')
+        opt = amend_options(options, 'shell', shell)
+        opt2 = amend_options(opt, 'target', target) + ('langs', langs)
+        self.vim.vars.set(self._last_test_line_var, line)
+        shell / L(self.vim.vars.set)('Myo_last_test_shell', _)
+        return (
+            self.data.command(self._test_cmd_name)
+            .to_either('no test command') /
+            (lambda a: TransientCommand(
+                prefix='test_line', command=a, line=line, langs=langs)) /
+            L(Dispatch)(_, opt2)
+        )
+
+
+class Plugin(MyoComponent):
+    Transitions = CommandTransitions
+
+    def new_state(self):
+        pass
+
+__all__ = ('Plugin', 'AddVimCommand', 'AddCommand', 'Run', 'AddShell',
+           'AddShellCommand', 'SetShellTarget')

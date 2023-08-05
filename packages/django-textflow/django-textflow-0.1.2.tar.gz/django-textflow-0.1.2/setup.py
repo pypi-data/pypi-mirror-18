8b647f6c1ff6fde86c10f97706b18fa3e8a94968
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:55:00 +0000

    Add package django-textflow/0.1.2/django-textflow-0.1.2.tar.gz

---
+++ packages/django-textflow/django-textflow-0.1.2.tar.gz/django-textflow-0.1.2/setup.py
@@ -0,0 +1,39 @@
+# coding: utf-8
+import os
+from setuptools import find_packages, setup
+
+with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as readme:
+    README = readme.read()
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+setup(
+    name='django-textflow',
+    version='0.1.2',
+    packages=find_packages(),
+    include_package_data=True,
+    license='MIT License',  # example license
+    description='A simple Django app to animate texts.',
+    long_description=README,
+    url='https://github.com/mhaze4/jquery-textflow/',
+    author='Michael Jünger',
+    author_email='michael@xn--jnger-kva.name',
+    classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Framework :: Django :: 1.8',  # replace "X.Y" as appropriate
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',  # example license
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        # Replace these appropriately if you are stuck on Python 2.
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    ],
+)

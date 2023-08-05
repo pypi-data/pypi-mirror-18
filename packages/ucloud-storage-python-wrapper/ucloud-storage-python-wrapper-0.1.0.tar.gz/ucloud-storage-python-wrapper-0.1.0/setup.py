29fa22f7dffc465fb6e6c4c4c04a86202cfa0b56
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:09:07 +0000

    Add package ucloud-storage-python-wrapper/0.1.0/ucloud-storage-python-wrapper-0.1.0.tar.gz

---
+++ packages/ucloud-storage-python-wrapper/ucloud-storage-python-wrapper-0.1.0.tar.gz/ucloud-storage-python-wrapper-0.1.0/setup.py
@@ -0,0 +1,22 @@
+# -*- coding:utf-8 -*-
+
+from distutils.core import setup
+
+setup(
+    # package name
+    name='ucloud-storage-python-wrapper',
+    # package version
+    version='0.1.0',
+
+    py_modules=['uspw', ],
+
+    # package struct
+    packages=['uspw', ],
+    license='GNU GPL 3.0',
+
+    author='yonggill Lee',
+    author_email='yonggill@wishket.com',
+    url='http://blog.yonggari.net',
+    description='python wrapper for KT ucloud storage service.',
+    keywords=['kt', 'ucloud', 'storage', 'python', 'wrapper']
+)

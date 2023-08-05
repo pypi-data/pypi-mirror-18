36b7d6f1dc92748cf657f7e07a263c9f1d12aed2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:54 +0000

    Add package prox/0.0.16/prox-0.0.16.tar.gz

---
+++ packages/prox/prox-0.0.16.tar.gz/prox-0.0.16/setup.py
@@ -0,0 +1,43 @@
+from setuptools import setup, find_packages
+import os
+
+ROOT = os.path.dirname(os.path.realpath(__file__))
+
+setup(
+    name = 'prox',
+    version = '0.0.16',
+    description = 'The tool to check the health of proxy list',
+    long_description = open(os.path.join(ROOT, 'README.rst')).read(),
+    url = 'http://github.com/lorien/prox',
+    author = 'Gregory Petukhov',
+    author_email = 'lorien@lorien.name',
+
+    packages = find_packages(),
+    include_package_data = True,
+    scripts = [
+        'bin/prox_check',
+        'bin/prox_task',
+        'bin/prox_geo',
+    ],
+    install_requires = [
+        'urllib3>=1.18.1',
+        'pysocks',
+        'peewee',
+        'bottle',
+        'pyyaml',
+        'python-geoip-python3',
+    ],
+    license = "MIT",
+    keywords = "proxy proxylist",
+    classifiers = [
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.4',
+        'License :: OSI Approved :: MIT License',
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Utilities',
+    ],
+)

878b32a9e4fe5487baf6b6758152df6b47e74493
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:24 +0000

    Add package switchDiscovery/1.0/switchDiscovery-1.0.tar.gz

---
+++ packages/switchDiscovery/switchDiscovery-1.0.tar.gz/switchDiscovery-1.0/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+   name='switchDiscovery',
+   version='1.0',
+   description='Switch Devices Discovery tool',
+   author='Aniket Gole',
+   author_email='roshan3133@gmail.com',
+   license='MIT',
+   packages=['switchDiscovery'],  #same as name
+   install_requires=['commands', 'paramiko', 'netaddr', 'getpass', 'sys', 'socket', 'argparse', 'logging'], #external packages as dependencies
+   scripts=[],
+   include_package_data=True,
+   zip_safe=False
+)

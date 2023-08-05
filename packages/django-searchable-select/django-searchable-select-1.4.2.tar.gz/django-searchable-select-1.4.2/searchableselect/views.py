2b629c7717f41393bb4f8fb999d25bff202b7160
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:48 +0000

    Add package django-searchable-select/1.4.2/django-searchable-select-1.4.2.tar.gz

---
+++ packages/django-searchable-select/django-searchable-select-1.4.2.tar.gz/django-searchable-select-1.4.2/searchableselect/views.py
@@ -0,0 +1,28 @@
+try:
+    # Django <=1.9
+    from django.db.models.loading import get_model
+except ImportError:
+    # Django 1.10+
+    from django.apps import apps
+    get_model = apps.get_model
+
+from django.http import JsonResponse
+from django.contrib.admin.views.decorators import staff_member_required
+
+
+@staff_member_required
+def filter_models(request):
+    model_name = request.GET.get('model')
+    search_field = request.GET.get('search_field')
+    value = request.GET.get('q')
+
+    model = get_model(model_name)
+
+    values = model.objects.filter(**{'{}__icontains'.format(search_field): value})[:10]
+    values = [
+        dict(pk=value.pk, name=unicode(value))
+        for value
+        in values
+    ]
+
+    return JsonResponse(dict(result=values))

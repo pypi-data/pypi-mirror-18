942cb23557f33527d54a3704b5e0ca97ab7b43ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:20 +0000

    Add package ecache/0.0.2/ecache-0.0.2.tar.gz

---
+++ packages/ecache/ecache-0.0.2.tar.gz/ecache-0.0.2/setup.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+
+import os
+import re
+
+from setuptools import setup, find_packages
+
+
+def _get_version():
+    v_file_path = os.path.join(os.path.dirname(os.path.abspath(__file__)),
+                               'ecache', '__init__.py')
+    ver_info_str = re.compile(r".*version_info = \((.*?)\)", re.S). \
+        match(open(v_file_path).read()).group(1)
+    return re.sub(r'(\'|"|\s+)', '', ver_info_str).replace(',', '.')
+
+with open("README.rst") as f:
+    long_description = f.read()
+
+requires = []
+
+
+setup(
+    name="ecache",
+    version=_get_version(),
+    description="Cache integration with sqlalchemy.",
+    long_description=long_description,
+    author='Mrkiven',
+    author_email="kiven.mr@gmail.com",
+    packages=find_packages(),
+    package_data={'include': 'meepo/*'},
+    license='MIT',
+    install_requires=[
+        'SQLAlchemy==0.9.3',
+        'redis>=2.10.5',
+        'flask>=0.10.1',
+        'Flask-SQLAlchemy==2.1',
+        'dogpile.cache==0.5.4'
+    ]
+)

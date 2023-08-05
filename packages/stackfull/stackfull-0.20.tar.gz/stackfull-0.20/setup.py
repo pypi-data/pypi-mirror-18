a4f8b2efd6a82401b90d031b8e9621409541eefa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:07:02 +0000

    Add package stackfull/0.20/stackfull-0.20.tar.gz

---
+++ packages/stackfull/stackfull-0.20.tar.gz/stackfull-0.20/setup.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+# Author: João S. O. bueno
+
+from setuptools import setup
+
+import os
+
+version = '0.20'
+description = "Tools for stack-usage in Python expressions"
+
+def filedata(name):
+    try:
+        return open(name).read()
+    except Exception as error:
+        return ""
+
+long_description = "\n".join((filedata("README.txt"),
+    filedata("docs/INSTALL.txt"),
+    filedata("docs/CREDITS.txt"),
+    filedata("docs/HISTORY.txt"),
+    ))
+
+setup(
+    name='stackfull',
+    version=version,
+    description=description,
+    long_description=long_description,
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: End Users/Desktop",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
+        "License :: OSI Approved :: GNU Lesser General Public License v3 or later (LGPLv3+)",
+        'License :: OSI Approved :: Python Software Foundation License',
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: Implementation :: PyPy",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+    keywords='python expressions stack acelerator utils push pop',
+    author='João S. O. Bueno',
+    author_email='gwidion@gmail.com',
+    url='https://github.com/jsbueno/stackfull',
+    license='License :: OSI Approved :: Python Software Foundation License',
+    py_modules=['stackfull'],
+    zip_safe=False,
+    install_requires=[],
+    extras_require={},
+    entry_points="",
+)

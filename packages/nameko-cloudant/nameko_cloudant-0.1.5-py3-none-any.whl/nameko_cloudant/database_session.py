bf4537c0efebaf48a6bfe13c629372c08939f27e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:03 +0000

    Add package nameko-cloudant/0.1.5/nameko_cloudant-0.1.5-py3-none-any.whl

---
+++ packages/nameko-cloudant/nameko_cloudant-0.1.5-py3-none-any.whl/nameko_cloudant/database_session.py
@@ -0,0 +1,36 @@
+from weakref import WeakKeyDictionary
+from nameko.extensions import DependencyProvider
+from cloudant.client import Cloudant
+
+DATABASE_CONFIG = 'DATABASE'
+
+
+class DatabaseSession(DependencyProvider):
+    def __init__(self, database, config=None):
+        self.database = database
+        self.config = config
+        self.clients = WeakKeyDictionary()
+
+    def setup(self):
+        if not self.config:
+            self.config = self.container.config[DATABASE_CONFIG]
+
+    def stop(self):
+        for client in self.clients:
+            client.disconnect()
+            del client
+
+    def get_dependency(self, worker_ctx):
+        client = Cloudant(
+            self.config['username'],
+            self.config['password'],
+            account=self.config['account'],
+        )
+
+        self.clients[worker_ctx] = client
+        client.connect()
+        return client[self.database]
+
+    def worker_teardown(self, worker_ctx):
+        client = self.clients.pop(worker_ctx)
+        client.disconnect()

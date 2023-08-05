591056c0c031e5e8fb989159624ab8bb0696a9d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:17 +0000

    Add package ez-spotify-dl/0.3/ez_spotify_dl-0.3.tar.gz

---
+++ packages/ez-spotify-dl/ez_spotify_dl-0.3.tar.gz/ez_spotify_dl-0.3/setup.py
@@ -0,0 +1,29 @@
+# -*- coding:utf-8 -*-
+
+from setuptools import setup
+
+requirements = []
+with open('requirements.txt') as f:
+    requirements = [line.rstrip('\n') for line in f.readlines()]
+
+readme = ''
+with open('README.md') as f:
+    readme = f.read()
+
+setup(
+    name='ez_spotify_dl',
+    version='0.3',
+    license='GPL',
+    description='Spotify downloader tool',
+    long_description=readme,
+    url='https://bitbucket.org/sacanix/spotify_dl',
+    author='Tony Kamillo',
+    author_email='tonysacanix@gmail.com',
+    keywords='spotify download tool',
+    install_requires=requirements,
+    py_modules=['spotify_dl'],
+    package_data={'': ['*.md']},
+    entry_points={
+        'console_scripts': ['spotify_dl = spotify_dl']
+    }
+)

080f554a35512d7ce3b7c00b570f12cb73331ec5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:43 +0000

    Add package sellester/1.3.4/sellester-1.3.4.tar.gz

---
+++ packages/sellester/sellester-1.3.4.tar.gz/sellester-1.3.4/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup (
+        name         = 'sellester',
+        version      = '1.3.4',
+        py_modules   = ['sellester'],
+        author       = 'sellester',
+        author_email = 'sellester@naver.com',
+        url          = 'http://www.headfirstlabs.com',
+        description  = 'A simple printer of nested lists',
+      )

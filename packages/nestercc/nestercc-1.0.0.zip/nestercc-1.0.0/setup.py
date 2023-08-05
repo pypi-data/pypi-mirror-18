eff3bcd518a4b2afa915b2c04426c5aa67bb3d1e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:08:57 +0000

    Add package nestercc/1.0.0/nestercc-1.0.0.zip

---
+++ packages/nestercc/nestercc-1.0.0.zip/nestercc-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+	name 		='nestercc',
+	version		='1.0.0',
+	py_modules	=['nestercc'],
+	auther		='jqm',
+	auther_email='951448670@qq.com',
+	url			='http://www.headfirstlabs.com',
+	description	='A simple printer of nested lists',
+	)

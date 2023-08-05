fed856c9dd43bcdf854c7098b84cc1ddaf8b51ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:58 +0000

    Add package chefpy/0.321/chefpy-0.321.tar.gz

---
+++ packages/chefpy/chefpy-0.321.tar.gz/chefpy-0.321/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(name="chefpy",
+      version="0.321",
+      description="Unofficial Codechef Api for user's status,rank,solutions etc.",
+      url="http://github.com/zuck007/chefpy",
+      author="Pradeep Khileri",
+      author_email="pradeepchoudhary009@gmail.com",
+      license="MIT",
+      packages=["chefpy"],
+      scripts=["./bin/chefpy"],
+      keywords='api codechef automation command-line linux web-scraping',
+      install_requires=[
+          "bs4",
+          "requests",
+      ],
+      zip_safe=False)
+

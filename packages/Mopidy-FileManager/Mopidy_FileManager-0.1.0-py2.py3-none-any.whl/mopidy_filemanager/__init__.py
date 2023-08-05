d02a83bc9bac090017f7f5fe80fbeae6ff9935e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:49 +0000

    Add package Mopidy-FileManager/0.1.0/Mopidy_FileManager-0.1.0-py2.py3-none-any.whl

---
+++ packages/Mopidy-FileManager/Mopidy_FileManager-0.1.0-py2.py3-none-any.whl/mopidy_filemanager/__init__.py
@@ -0,0 +1,41 @@
+from __future__ import unicode_literals
+
+import logging
+import os
+
+from mopidy import config, ext
+
+
+__version__ = '0.1.0'
+
+logger = logging.getLogger(__name__)
+
+
+class Extension(ext.Extension):
+
+    dist_name = 'Mopidy-FileManager'
+    ext_name = 'filemanager'
+    version = __version__
+
+    def get_default_config(self):
+        conf_file = os.path.join(os.path.dirname(__file__), 'ext.conf')
+        return config.read(conf_file)
+
+    def setup(self, registry):
+
+        registry.add('http:static', {
+            'name': self.ext_name,
+            'path': os.path.join(os.path.dirname(__file__), 'static'),
+        })
+
+        registry.add('http:app', {
+            'name': self.ext_name,
+            'factory': self.factory,
+        })
+
+    def factory(self, config, core):
+        from .file_manager import FileManagerHandler
+
+        return [
+            ("/fs", FileManagerHandler, {'root': '/root'}),
+        ]

10e3b11a6f57dd3a84a948533f7b8106d2159641
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:07:42 +0000

    Add package ggps/0.1.11/ggps-0.1.11.tar.gz

---
+++ packages/ggps/ggps-0.1.11.tar.gz/ggps-0.1.11/setup.py
@@ -0,0 +1,41 @@
+
+import codecs
+import os
+
+#from setuptools import setup
+from distutils.core import setup
+
+def description():
+    return 'ggps is a python library for parsing Garmin gpx and tcx files'
+
+def fpath(name):
+    return os.path.join(os.path.dirname(__file__), name)
+
+def readme():
+    filename = fpath('README.rst')
+    with codecs.open(filename, encoding='utf-8') as f:
+        return f.read()
+
+setup(
+    name='ggps',
+    version='0.1.11',
+    description='ggps is a python library for parsing Garmin gpx and tcx files',
+    long_description=readme(),
+    url='https://github.com/cjoakim/ggps',
+    author='Christopher Joakim',
+    author_email='christopher.joakim@gmail.com',
+    license='MIT',
+    packages=['ggps'],
+    zip_safe=False,
+    install_requires=['arrow','m26'],
+    test_suite="tests",
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Topic :: Utilities',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ]
+)

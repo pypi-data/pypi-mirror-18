92ea4140095eaeb43352c2ce6756253eac4f524d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:01 +0000

    Add package phonenumbers/7.7.3/phonenumbers-7.7.3.tar.gz

---
+++ packages/phonenumbers/phonenumbers-7.7.3.tar.gz/phonenumbers-7.7.3/tests/testdata/region_AD.py
@@ -0,0 +1,16 @@
+"""Auto-generated file, do not edit by hand. AD metadata"""
+from phonenumbers.phonemetadata import NumberFormat, PhoneNumberDesc, PhoneMetadata
+
+PHONE_METADATA_AD = PhoneMetadata(id='AD', country_code=376, international_prefix='00',
+    general_desc=PhoneNumberDesc(national_number_pattern='\\d{6}', possible_number_pattern='\\d{6}', possible_length=(6,)),
+    fixed_line=PhoneNumberDesc(national_number_pattern='\\d{6}', possible_number_pattern='\\d{6}', possible_length=(6,)),
+    mobile=PhoneNumberDesc(national_number_pattern='\\d{6}', possible_number_pattern='\\d{6}', possible_length=(6,)),
+    toll_free=PhoneNumberDesc(national_number_pattern='NA', possible_number_pattern='NA'),
+    premium_rate=PhoneNumberDesc(national_number_pattern='NA', possible_number_pattern='NA'),
+    shared_cost=PhoneNumberDesc(national_number_pattern='NA', possible_number_pattern='NA'),
+    personal_number=PhoneNumberDesc(national_number_pattern='NA', possible_number_pattern='NA'),
+    voip=PhoneNumberDesc(national_number_pattern='NA', possible_number_pattern='NA'),
+    pager=PhoneNumberDesc(national_number_pattern='NA', possible_number_pattern='NA'),
+    uan=PhoneNumberDesc(national_number_pattern='NA', possible_number_pattern='NA'),
+    voicemail=PhoneNumberDesc(national_number_pattern='NA', possible_number_pattern='NA'),
+    no_international_dialling=PhoneNumberDesc(national_number_pattern='NA', possible_number_pattern='NA'))

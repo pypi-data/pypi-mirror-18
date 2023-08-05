2606dea51430c6f682ce1ba5eb3ea330663861a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:09:21 +0000

    Add package pytchuka/0.0.3/pytchuka-0.0.3.tar.gz

---
+++ packages/pytchuka/pytchuka-0.0.3.tar.gz/pytchuka-0.0.3/pytchuka/route_loader.py
@@ -0,0 +1,37 @@
+import json
+import re
+
+from marshmallow import Schema
+from marshmallow import fields
+
+
+class RouteSchema(Schema):
+    url = fields.Str(required=True)
+    status = fields.Int(required=True)
+    body = fields.Dict(required=True)
+
+
+class RouteLoader:
+    def __init__(self):
+        self.routes = {}
+
+    def load_from_file(self, filename):
+        with open(filename, 'r') as file:
+            json_info = json.load(file)
+            self.load_routes(json_info)
+
+    def load_routes(self, routes):
+        routes_list = RouteSchema(many=True).load(routes)
+        for route in routes_list.data:
+            self.routes[route['url']] = route
+
+    def get_route(self, url):
+        return self.routes.get(url, None)
+
+    def match_route(self, url):
+        for key, value in self.routes.items():
+            if re.match(key, url):
+                return value
+
+        return {'url': url, 'status': 200, 'body': {}}
+

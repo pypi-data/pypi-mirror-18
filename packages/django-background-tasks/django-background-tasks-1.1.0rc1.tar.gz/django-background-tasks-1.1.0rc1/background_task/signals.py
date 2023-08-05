02357260b73753809bd2ec5adb8286c64d3694ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:08:55 +0000

    Add package django-background-tasks/1.1.0rc1/django-background-tasks-1.1.0rc1.tar.gz

---
+++ packages/django-background-tasks/django-background-tasks-1.1.0rc1.tar.gz/django-background-tasks-1.1.0rc1/background_task/signals.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+import django.dispatch
+
+task_created = django.dispatch.Signal(providing_args=['task'])
+task_error = django.dispatch.Signal(providing_args=['task'])
+task_rescheduled = django.dispatch.Signal(providing_args=['task'])
+task_failed = django.dispatch.Signal(providing_args=['task_id', 'completed_task'])
+task_successful = django.dispatch.Signal(providing_args=['task_id', 'completed_task'])
---
+++ packages/django-background-tasks/django-background-tasks-1.1.0rc1.tar.gz/django-background-tasks-1.1.0rc1/background_task/tests/test_settings.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+DEBUG = True
+TEMPLATE_DEBUG = DEBUG
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': ':memory:',
+        'TEST':  # This will force django to create a real sqlite database on
+                 # the disk, instead of creating it in memory.
+                 # We need this to test the async behavior.
+            {
+                'NAME': 'test_db',
+            },
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': '',
+        'PORT': '',
+    }
+}
+
+INSTALLED_APPS = [
+    'django.contrib.contenttypes',
+    'django.contrib.auth',
+    'background_task',
+]
+
+SECRET_KEY = 'foo'
+
+USE_TZ = True
+BACKGROUND_TASK_RUN_ASYNC = False

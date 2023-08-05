4924414ab5c26130bb2ddf3d95425a640833cda0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:23:53 +0000

    Add package pyshtrih/1.3.1/pyshtrih-1.3.1.tar.gz

---
+++ packages/pyshtrih/pyshtrih-1.3.1.tar.gz/pyshtrih-1.3.1/pyshtrih/__init__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+
+from .protocol import Protocol
+from .device import ShtrihFRK, ShtrihComboFRK, ShtrihComboPTK, ShtrihAllCommands
+from .excepts import ProtocolError, NoConnectionError, UnexpectedResponseError, Error, CheckError, \
+    OpenCheckError, ItemSaleError, CloseCheckError
+
+
+__version__ = '1.3.1'
+__all__ = (
+    'Protocol',
+    'ShtrihFRK', 'ShtrihComboFRK', 'ShtrihComboPTK', 'ShtrihAllCommands',
+    'ProtocolError', 'NoConnectionError', 'UnexpectedResponseError', 'Error', 'CheckError',
+    'OpenCheckError', 'ItemSaleError', 'CloseCheckError'
+)

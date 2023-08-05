501619ac3bb7266a292017e8de9411ec0ea5f42a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:08:05 +0000

    Add package larry_nester/0.0.0/larry_nester-0.0.0.tar.gz

---
+++ packages/larry_nester/larry_nester-0.0.0.tar.gz/larry_nester-0.0.0/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+	name		= 'larry_nester',
+        vresion		='1.0.0',
+	py_modules	=['larry_nester'],
+	author		='larry',
+	author_email	='136105956@qq.com',
+	url		='http://headfirstlabs.com',
+	description	='A simple printer of nested lists',
+      )
+

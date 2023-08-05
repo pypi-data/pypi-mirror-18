89f6f2333247d9d1679d183e4bd02bb6eace0a90
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:35 +0000

    Add package credits.common/1.0.3/credits.common-1.0.3.tar.gz

---
+++ packages/credits.common/credits.common-1.0.3.tar.gz/credits.common-1.0.3/setup.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from __future__ import division
+from __future__ import print_function
+
+import setuptools
+
+version = "1.0.3"
+
+packages = [
+    "base58==0.2.2",
+    "ed25519",
+    "logbook==0.12.5",
+]
+
+setuptools.setup(
+    name="credits.common",
+    version=version,
+    author="Credits Developers",
+    author_email="admin@credits.vision",
+    install_requires=packages,
+    namespace_packages=['credits'],
+    packages=setuptools.find_packages(),
+    url="https://github.com/CryptoCredits/credits-common",
+    download_url="https://github.com/CryptoCredits/credits-common/tarball/%s" % version,
+)

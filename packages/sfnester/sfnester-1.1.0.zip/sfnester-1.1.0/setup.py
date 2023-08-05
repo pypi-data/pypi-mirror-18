a699e9a2298b1511f2a01c9d2579c2a173eff2e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:34 +0000

    Add package sfnester/1.1.0/sfnester-1.1.0.zip

---
+++ packages/sfnester/sfnester-1.1.0.zip/sfnester-1.1.0/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+
+setup(
+         name              = 'sfnester',
+         version           = '1.1.0',
+         py_modules        = ['sfnester'],
+         author            = 'hfpython',
+         author_email      = 'hfpython@headfirstlabs.com',
+         url               = 'http://www.headfirstlabs.com',
+         description       = 'A simple printer of nested lists',
+         )

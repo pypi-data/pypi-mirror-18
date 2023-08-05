0b636469ed0aacd5eace1b629bb8f7ee05632703
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:45 +0000

    Add package psst/0.1.1/psst-0.1.1.tar.gz

---
+++ packages/psst/psst-0.1.1.tar.gz/psst-0.1.1/psst/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+
+__author__ = 'Dheepak Krishnamurthy'
+__email__ = 'kdheepak89@gmail.com'
+__version__ = '0.1.1'
+
+print("This version is outdated. Please contact the developer for more information")

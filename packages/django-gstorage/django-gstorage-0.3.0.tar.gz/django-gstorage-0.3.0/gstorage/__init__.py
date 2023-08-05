ecc19657c1ff938cd7ffb3796e214a6c185b5862
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:52 +0000

    Add package django-gstorage/0.3.0/django-gstorage-0.3.0.tar.gz

---
+++ packages/django-gstorage/django-gstorage-0.3.0.tar.gz/django-gstorage-0.3.0/gstorage/__init__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+    django-gstorage
+    ~~~~~~~~~~~~~~~
+
+    A plug-and-play replacement for FileSystemStorage but
+    using Google storage for persistence
+"""
+
+__version__ = '0.3.0'
+__author__ = 'Fyndiq'
+__license__ = 'MIT'
+__copyright__ = 'Copyright 2016 Fyndiq AB'
+
+
+default_app_config = 'gstorage.apps.GStorageConfig'

5b1f3fdbf2e86efd8c545b8f64dac864468f2fca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:17 +0000

    Add package beecloud/3.3.0/beecloud-3.3.0.zip

---
+++ packages/beecloud/beecloud-3.3.0.zip/beecloud-3.3.0/beecloud/__init__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+    package init.
+    :created by xuanzhui on 2015/12/24.
+    :copyright (c) 2015 BeeCloud.
+    :license: MIT, see LICENSE for more details.
+"""
+
+BEECLOUD_PYTHON_SDK_VERSION = '3.3.0'
+BEECLOUD_HOSTS = ['https://apibj.beecloud.cn', 'https://apisz.beecloud.cn',
+                  'https://apiqd.beecloud.cn', 'https://apihz.beecloud.cn']
+
+BEECLOUD_RESTFUL_VERSION = '/2/'
+
+# common result code
+NETWORK_ERROR_CODE = -1
+NETWORK_ERROR_NAME = 'NETWORK_ERROR'
+NETWORK_ERROR_DETAIL = 'Network Error'
+
+NOT_SUPPORTED_CODE = -2
+NOT_SUPPORTED_NAME = 'NOT_SUPPORTED_CODE'

28f9ed0cc94d85f339016aa1ad8af02f5f8cc889
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:20 +0000

    Add package stix/1.2.0.2/stix-1.2.0.2.zip

---
+++ packages/stix/stix-1.2.0.2.zip/stix-1.2.0.2/stix/xmlconst.py
@@ -0,0 +1,5 @@
+# Copyright (c) 2015, The MITRE Corporation. All rights reserved.
+# See LICENSE.txt for complete terms.
+
+# STIX TAGS
+TAG_STIX_PACKAGE = "{http://stix.mitre.org/stix-1}STIX_Package"

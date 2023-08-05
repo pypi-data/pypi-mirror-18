e98aa460898fed948fc4778668b5bbc542f25af9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:09 +0000

    Add package windflow/0.1.16/windflow-0.1.16-py3.5.egg

---
+++ packages/windflow/windflow-0.1.16-py3.5.egg/windflow/services/assets.py
@@ -0,0 +1,59 @@
+import simplejson
+from windflow.services import Service
+
+
+class Assets(dict):
+    def get_style(self, name):
+        try:
+            bundle = self[name]
+        except KeyError as e:
+            return ''
+
+        try:
+            return '<link href="' + bundle['css'] + '" rel="stylesheet">'
+        except KeyError as e:
+            return ''
+
+    def get_script(self, name):
+        try:
+            bundle = self[name]
+        except KeyError as e:
+            return ''
+
+        try:
+            return '<script src="' + bundle['js'] + '" type="text/javascript"></script>'
+        except KeyError as e:
+            return ''
+
+
+class UnavailableAssets(Assets):
+    def get_style(self, name):
+        return ''
+
+    def get_script(self, name):
+        return ('<script type="text/javascript">document.write("Assets are not available. It may means that the '
+                'bundling process is still running, or that the webpack AssetsPlugin did not run. Look for the '
+                'assets.json file in your static directory (and look at webpack output).")</script>')
+
+        # def __getattr__(self, item):
+        #    return self
+
+
+class WebpackAssets(Service):
+    """
+    Webpack assets service.
+
+    Simply reads the output of the assets plugin (json) so it can be used in a templating context.
+
+    """
+
+    filename = 'static/assets.json'
+
+    def get(self):
+        try:
+            with open(self.filename) as f:
+                return Assets(simplejson.load(f))
+        except:
+            # todo this is evil, but we'd need to find out errors e do want to "ignore" or how to tell user about it.
+            # at the very minimum: simplejson.scanner.JSONDecodeError and probably missing file
+            return UnavailableAssets()

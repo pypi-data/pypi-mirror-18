637179e880e3519bdb1fa46c1c417a3ff5cf1467
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:05:48 +0000

    Add package lyf111111/1.0.0/lyf111111-1.0.0.tar.gz

---
+++ packages/lyf111111/lyf111111-1.0.0.tar.gz/lyf111111-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+       name        =  'lyf111111',
+       version     =  '1.0.0',
+       py_modules  =  ['lyf111111'],
+       author      =  'liuyufei',
+       author_email=  '2868393672@qq.com',
+       url         =  'http://www.headfirstlabs.com',
+       description =  'A simple printer of nested lists',
+     )

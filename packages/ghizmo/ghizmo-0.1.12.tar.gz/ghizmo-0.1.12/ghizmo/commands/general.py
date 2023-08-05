ca8feb465e541dcb9453e4bace4af64be879cab3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:07:19 +0000

    Add package ghizmo/0.1.12/ghizmo-0.1.12.tar.gz

---
+++ packages/ghizmo/ghizmo-0.1.12.tar.gz/ghizmo-0.1.12/ghizmo/commands/general.py
@@ -0,0 +1,29 @@
+from __future__ import print_function
+
+
+def rate_limit(config, args):
+  """
+  Current rate limit information.
+  """
+  yield config.github.rate_limit()
+
+
+def emojis(config, args):
+  """
+  List available emojis.
+  """
+  yield config.github.emojis()
+
+
+def octocat(config, args):
+  """
+  Easter egg.
+  """
+  yield config.github.octocat(say=args.get("say"))
+
+
+def zen(config, args):
+  """
+  Easter egg.
+  """
+  yield config.github.zen()

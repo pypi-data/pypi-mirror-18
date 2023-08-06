02a88bd991cde63fa88cb60f047c383e9a7d69a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:12:18 +0000

    Add package pymicroservice/0.0.1/pymicroservice-0.0.1-py3-none-any.whl

---
+++ packages/pymicroservice/pymicroservice-0.0.1-py3-none-any.whl/pymicroservice/core/microservice.py
@@ -0,0 +1,145 @@
+import inspect
+from abc import ABC, abstractmethod
+import os
+from concurrent.futures import ThreadPoolExecutor
+import functools
+
+from tornado.web import RequestHandler, StaticFileHandler
+from tornado.gen import coroutine
+from tornado.ioloop import IOLoop
+from tornado.web import Application
+from tornado.log import enable_pretty_logging
+
+from pymicroservice.util import init_default_logger
+from pymicroservice.errors import ServiceConfigurationError, AccessDeniedError
+from pymicroservice.core.handlers import TornadoJsonRpcHandler
+from pymicroservice.core.decorators import public_method
+
+__all__ = [
+    'PyMicroService'
+]
+
+
+class PyMicroService(ABC):
+    """
+
+    The base class for defining a microservice.
+
+    """
+    name = None
+
+    host = "127.0.0.1"
+    port = 8000
+
+    template_dir = "."
+    static_dirs = []
+
+    extra_handlers = []
+
+    api_token_header = "X-Api-Token"
+
+    max_parallel_blocking_tasks = os.cpu_count()
+    _executor = None
+
+    def __init__(self):
+        self.app = None
+        init_default_logger()
+
+        # name
+        if self.name is None:
+            raise ServiceConfigurationError("No name defined for the microservice")
+
+        # methods
+        self.methods = {}
+        self.gather_exposed_methods()
+
+        if len(self.methods) == 0:
+            raise ServiceConfigurationError("No exposed methods for the microservice")
+
+        # executor
+        if self.max_parallel_blocking_tasks <= 0:
+            raise ServiceConfigurationError("Invalid max_parallel_blocking_tasks value")
+
+        self._executor = ThreadPoolExecutor(self.max_parallel_blocking_tasks)
+
+    @public_method
+    def get_service_specs(self):
+        """
+        A default exposed method that returns the current microservice specifications. The returned information is
+        in the format:
+
+        ::
+
+            {
+                "host": "127.0.0.1",
+                "port": 9000.
+                "name": "service.example",
+                "max_parallel_blocking_tasks": 8,
+                "methods": {
+                    "get_service_specs": "...",
+                    "method1": "method1's docstring",
+                    ...
+                }
+            }
+
+        :return:
+        """
+        return {
+            "host": self.host,
+            "port": self.port,
+            "name": self.name,
+            "max_parallel_blocking_tasks": self.max_parallel_blocking_tasks,
+            "methods": {m: self.methods[m].__doc__ for m in self.methods}
+        }
+
+    def start(self):
+        """
+        The main method that starts the service. This is blocking.
+
+        """
+        self.app = self.make_tornado_app()
+        enable_pretty_logging()
+        self.app.listen(self.port, address=self.host)
+        IOLoop.current().start()
+
+    def make_tornado_app(self):
+
+        handlers = [
+            (r"/api", TornadoJsonRpcHandler,
+             {
+                 "methods": self.methods,
+                 "executor": self._executor,
+                 "api_token_header":
+                     self.api_token_header,
+                 "api_token_handler": self.api_token_is_valid
+             })
+        ]
+
+        self.add_extra_handlers(handlers)
+        self.add_static_handlers(handlers)
+
+        return Application(handlers, template_path=self.template_dir)
+
+    def add_extra_handlers(self, handlers):
+        handlers.extend(self.extra_handlers)
+
+    def add_static_handlers(self, handlers):
+        for url, path in self.static_dirs:
+            handlers.append((url.rstrip("/") + "/(.*)", StaticFileHandler, {"path": path}))
+
+    def api_token_is_valid(self, api_token):
+        """
+        Method that must be overridden by subclasses in order to implement the API token validation logic.
+        Should return ``True`` if the api token is valid, or ``False`` otherwise.
+
+        :param api_token: a string representing the received api token value
+        :return: ``True`` if the api_token is valid, ``False`` otherwise
+        """
+        return True
+
+    def gather_exposed_methods(self):
+        for itemname in dir(self):
+            item = getattr(self, itemname)
+            if getattr(item, "__is_exposed_method__", False) is True or \
+                            getattr(item, "__private_api_method__", False) is True:
+                self.methods[item.__name__] = item

d97d4029b7e9de5e0e3f6fa079c540bbfd686882
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:18:26 +0000

    Add package wwg_nester/1.1.0/wwg_nester-1.1.0.zip

---
+++ packages/wwg_nester/wwg_nester-1.1.0.zip/wwg_nester-1.1.0/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup( 
+        name         = 'wwg_nester', 
+        version      = '1.1.0', 
+        ## TODO: be sure to change these next few lines to match your details!
+        py_modules   = ['wwg_nester'],
+        author       = 'tiemobuguilu',
+        author_email = '1121319023@qq.com',
+        url          = 'http://www.headfirstlabs.com',
+        description  = 'A simple printer of nested lists',
+     )

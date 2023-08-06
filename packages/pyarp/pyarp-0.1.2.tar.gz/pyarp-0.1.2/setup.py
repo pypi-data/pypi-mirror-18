ce9d3892ff7822a780cc88bd9bb2170574c3deb4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:46:09 +0000

    Add package pyarp/0.1.2/pyarp-0.1.2.tar.gz

---
+++ packages/pyarp/pyarp-0.1.2.tar.gz/pyarp-0.1.2/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+setup(
+    name="pyarp",
+    packages=["pyarp"],
+    entry_points={
+        'console_scripts': [
+            'pyarp = pyarp.__main__:main'
+        ]
+    },
+    version='0.1.2',
+    license='MIT',
+    description="A user-friendly ARP spoofing tool",
+    author="Imad Hsissou",
+    author_email="imad.hsissou@gmail.com",
+    url="https://github.com/7BISSO/python-arp-spoofer",
+    download_url="https://github.com/7BISSO/python-arp-spoofer/tarball/0.1.1",
+    keywords=['arp', 'scapy'],
+    include_package_data=True,
+    install_requires=[
+        "scapy",
+        "netifaces"
+    ],
+)

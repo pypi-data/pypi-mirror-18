ff2fa690ab709af34b2402ff83f6c7ac807c9d9d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:44:50 +0000

    Add package nester_flame/1.0.0/nester_flame-1.0.0.zip

---
+++ packages/nester_flame/nester_flame-1.0.0.zip/nester_flame-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+        name            = 'nester_flame',
+        version         = '1.0.0',
+        py_modules      = ['nester_flame'],
+        author          = 'burningFlame',
+        author_eamil    = 'shuiwen150615@163.com',
+        url             = '',
+        description     = '',
+    )

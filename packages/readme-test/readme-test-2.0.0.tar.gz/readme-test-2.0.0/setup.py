14f910c4e4d186f52e614dbb7666725733858417
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:16:09 +0000

    Add package readme-test/2.0.0/readme-test-2.0.0.tar.gz

---
+++ packages/readme-test/readme-test-2.0.0.tar.gz/readme-test-2.0.0/setup.py
@@ -0,0 +1,18 @@
+import codecs
+from random import randint
+from setuptools import setup
+
+def long_description():
+    with codecs.open('README.rst', encoding='utf8') as f:
+        return f.read()
+
+def random_version():
+    return '{}.{}.{}'.format(randint(0, 9), randint(0, 9), randint(0, 9))
+
+setup(
+    name='readme-test',
+    version=random_version(),
+    description='testing readme rendering on pypi',
+    long_description=long_description(),
+    url='http://httpie.org/',
+)

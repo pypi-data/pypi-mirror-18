37528b0b6239dd6b472012f56000f5dca8383a63
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:13:19 +0000

    Add package enable/4.6.0/enable-4.6.0.tar.gz

---
+++ packages/enable/enable-4.6.0.tar.gz/enable-4.6.0/kiva/_version.py
@@ -0,0 +1,8 @@
+# THIS FILE IS GENERATED FROM ENABLE SETUP.PY
+version = '4.6.0'
+full_version = '4.6.0'
+git_revision = '9bc2198'
+is_released = True
+
+if not is_released:
+    version = full_version

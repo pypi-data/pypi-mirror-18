b998a24f9bd5f7a6f16b2be6347d4141dd57412a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:17:02 +0000

    Add package itculate-sdk/0.8.1/itculate_sdk-0.8.1.tar.gz

---
+++ packages/itculate-sdk/itculate_sdk-0.8.1.tar.gz/itculate_sdk-0.8.1/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+from os import path
+import itculate_sdk
+
+here = path.abspath(path.dirname(__file__))
+
+setup(
+    name="itculate_sdk",
+    version=itculate_sdk.__version__,
+    description="ITculate SDK",
+    url="https://bitbucket.org/itculate/itculate-sdk",
+    author="Ophir",
+    author_email="opensource@itculate.io",
+    license="MIT",
+    keywords=["ITculate", "sdk", "graph", "topology"],
+    package_data={'itculate_sdk/*': ['*.csv']},
+    packages=find_packages(),
+)

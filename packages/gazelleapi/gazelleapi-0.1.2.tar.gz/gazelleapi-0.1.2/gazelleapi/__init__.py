03fb03f2e2c848517d18f41ff44057d94c898ab9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:45:16 +0000

    Add package gazelleapi/0.1.2/gazelleapi-0.1.2.tar.gz

---
+++ packages/gazelleapi/gazelleapi-0.1.2.tar.gz/gazelleapi-0.1.2/gazelleapi/__init__.py
@@ -0,0 +1,6 @@
+from .gazelleapi import GazelleAPI
+
+__version__ = "0.1.2"
+__author__ = "itismadness"
+__author_email__ = "it.is.madness@gmail.com"
+__url__ = "https://github.com/itismadness/gazelleapi"
---
+++ packages/gazelleapi/gazelleapi-0.1.2.tar.gz/gazelleapi-0.1.2/setup.py
@@ -0,0 +1,40 @@
+import os
+from setuptools import setup, find_packages
+from gazelleapi import __version__, __author__, __author_email__, __url__
+
+
+if os.path.isfile('README.md'):
+    with open('README.md') as f:
+        readme_file = f.read()
+else:
+    readme_file = ''
+
+setup(
+    name='gazelleapi',
+    version=__version__,
+    description='Gazelle API',
+    long_description=readme_file,
+    author=__author__,
+    author_email=__author_email__,
+    url=__url__,
+    license='MIT',
+    install_requires=[
+        "future",
+        "requests"
+    ],
+    packages=find_packages(exclude=('tests', 'docs')),
+    package_data={
+        '': ['*.txt']
+    },
+    zip_safe=True,
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3'
+    ]
+)

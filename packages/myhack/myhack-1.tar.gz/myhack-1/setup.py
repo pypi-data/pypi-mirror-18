072b78fc9b3771b6a6cc70f8fadb28c4a2d987db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:12:59 +0000

    Add package myhack/1/myhack-1.tar.gz

---
+++ packages/myhack/myhack-1.tar.gz/myhack-1/setup.py
@@ -0,0 +1,16 @@
+from distutils.core import setup
+import sys, os
+
+
+setup(name='myhack',
+      version='1',
+      description="trying",
+      long_description="trying",
+      keywords='myhack',
+      author='minus79',
+      author_email='gergely06@gmail.com',
+      url='https://github.com/minus79/xenon',
+      download_url = 'https://github.com/minus79/xenon/tarball/0.1',
+      include_package_data=True,
+      zip_safe=False
+      )

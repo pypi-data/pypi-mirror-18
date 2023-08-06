36f7a2a7809f81946107e5ff06d5644ad0042e2a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:46:56 +0000

    Add package koresz_nester/1.0.0/koresz_nester-1.0.0.tar.gz

---
+++ packages/koresz_nester/koresz_nester-1.0.0.tar.gz/koresz_nester-1.0.0/setup.py
@@ -0,0 +1,12 @@
+# setup.py
+
+from distutils.core import setup
+setup(
+	name = 'koresz_nester',
+	version = '1.0.0',
+	py_module = ['koresz_nester'],
+	author = 'koresz',
+	author_email = 'koresz@gmail.com',
+	url = 'http://my_firstpythonlab.com',
+	description = 'A simple nested list printer ',
+	) 

086415d8af74aa9b433d88319b8d7e2e3931271c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:09:13 +0000

    Add package mastercard-parameter-management-services/1.0.2/mastercard_parameter_management_services-1.0.2-py2.py3-none-any.whl

---
+++ packages/mastercard-parameter-management-services/mastercard_parameter_management_services-1.0.2-py2.py3-none-any.whl/mastercardparametermanagementservices/partnerstatus.py
@@ -0,0 +1,86 @@
+#
+# Copyright (c) 2016 MasterCard International Incorporated
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without modification, are
+# permitted provided that the following conditions are met:
+#
+# Redistributions of source code must retain the above copyright notice, this list of
+# conditions and the following disclaimer.
+# Redistributions in binary form must reproduce the above copyright notice, this list of
+# conditions and the following disclaimer in the documentation and/or other materials
+# provided with the distribution.
+# Neither the name of the MasterCard International Incorporated nor the names of its
+# contributors may be used to endorse or promote products derived from this software
+# without specific prior written permission.
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY
+# EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+# OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT
+# SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+# INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
+# TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+# OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER
+# IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING
+# IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+# SUCH DAMAGE.
+#
+
+
+from mastercardapicore.core.model import BaseObject
+from mastercardapicore.core.model import RequestMap
+from mastercardapicore.core.model import OperationConfig
+from mastercardapicore.core.model import OperationMetadata
+from sdkconfig import SDKConfig
+
+
+class PartnerStatus(BaseObject):
+    """
+    
+    """
+
+    __config = {
+        "7086f2de-c35d-4e94-950f-fb89157aa566" : OperationConfig("/send/v1/partners/{partnerId}/partner-status", "query", [], []),
+        "071c19a5-85e8-4d7c-8794-5249cef06c9f" : OperationConfig("/send/v1/partners/{partnerId}/partner-status", "update", [], []),
+        
+    }
+
+    def getOperationConfig(self,operationUUID):
+        if operationUUID not in self.__config:
+            raise Exception("Invalid operationUUID: "+operationUUI)
+
+        return self.__config[operationUUID]
+
+    def getOperationMetadata(self):
+        return OperationMetadata(SDKConfig.getVersion(), SDKConfig.getHost())
+
+
+
+
+
+
+    @classmethod
+    def read(cls,criteria):
+        """
+        Query objects of type PartnerStatus by id and optional criteria
+        @param type criteria
+        @return PartnerStatus object representing the response.
+        """
+
+        return BaseObject.execute("7086f2de-c35d-4e94-950f-fb89157aa566", PartnerStatus(criteria))
+
+
+
+
+    def update(self):
+        """
+        Updates an object of type PartnerStatus
+
+        @return PartnerStatus object representing the response.
+        """
+        return BaseObject.execute("071c19a5-85e8-4d7c-8794-5249cef06c9f", self)
+
+
+
+
+
+

ecce7c07f61bc91d3418a6f054b57de7ce1568cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:08:23 +0000

    Add package osmium/2.10.2/osmium-2.10.2.tar.gz

---
+++ packages/osmium/osmium-2.10.2.tar.gz/osmium-2.10.2/osmium/version.py
@@ -0,0 +1,11 @@
+"""
+Version information.
+"""
+
+# the major version
+pyosmium_major = '2.10'
+# current release (Pip version)
+pyosmium_release = '2.10.2'
+
+# libosmium version shipped with the Pip release
+libosmium_version = '2.10.2'

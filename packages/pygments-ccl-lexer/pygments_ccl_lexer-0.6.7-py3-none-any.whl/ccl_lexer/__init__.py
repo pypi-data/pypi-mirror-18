693b8898251eb1041be32480c1e3935e89d6f2fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:10:30 +0000

    Add package pygments-ccl-lexer/0.6.7/pygments_ccl_lexer-0.6.7-py3-none-any.whl

---
+++ packages/pygments-ccl-lexer/pygments_ccl_lexer-0.6.7-py3-none-any.whl/ccl_lexer/__init__.py
@@ -0,0 +1,30 @@
+from pygments.lexers.sql import SqlLexer
+from pygments.token import Keyword, Name
+
+__all__ = ['CCL_Lexer']
+
+
+CCL_KEYWORDS = ['select', 'from', 'where', 'with', 'order by']
+CCL_BUILTINS = ['in', 'between', 'and', 'or', 'distinct']
+CCL_FUNCTIONS = ['substring', 'cnvtdatetime']
+class CCL_Lexer(SqlLexer):
+    name = 'CCL'
+    aliases = ['ccl', 'prg']
+    filenames = ['*.ccl']
+
+    def get_tokens_unprocessed(self, text):
+        extra_content = [(CCL_KEYWORDS, Keyword),
+                         (CCL_BUILTINS, Name.Builtin),
+                         (CCL_FUNCTIONS, Name.Function)]
+
+        for index, token, value in SqlLexer.get_tokens_unprocessed(self, text):
+            if token is Name:
+                for i in extra_content:
+                    if value in i[0]:
+                        yield index, i[1], value
+                        break;
+                else:
+                    yield index, token, value
+            else:
+                yield index, token, value
+

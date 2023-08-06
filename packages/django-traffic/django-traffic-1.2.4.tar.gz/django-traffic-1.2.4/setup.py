41fa48c4830323273599c6016171692a4726e6c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:20:59 +0000

    Add package django-traffic/1.2.4/django-traffic-1.2.4.tar.gz

---
+++ packages/django-traffic/django-traffic-1.2.4.tar.gz/django-traffic-1.2.4/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name='django-traffic',
+    packages=['django_traffic'],
+    version='1.2.4',
+    description='Django middleware that helps visualize your app\'s traffic in Kibana',
+    author='Konstantinos Livieratos',
+    author_email='livieratos.konstantinos@gmail.com',
+    url='https://github.com/koslibpro/django-traffic',
+    download_url='https://github.com/koslibpro/django-traffic/tarball/1.2.4',
+    keywords=['django', 'kibana', 'elasticsearch', 'traffic'],
+    classifiers=[],
+)

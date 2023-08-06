cea14fd690d99dec9b410a84464dad430f31b1e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:52:14 +0000

    Add package twisted-platform-support/16.12.3/twisted_platform_support-16.12.3-cp35-cp35m-manylinux1_x86_64.whl

---
+++ packages/twisted-platform-support/twisted_platform_support-16.12.3-cp35-cp35m-manylinux1_x86_64.whl/_twisted_platform_support/test/test_api.py
@@ -0,0 +1,53 @@
+# Copyright (c) Twisted Matrix Laboratories.
+# See LICENSE for details.
+
+from twisted.trial.unittest import TestCase
+from twisted.python.compat import _PY3
+from twisted.python.runtime import platform
+
+
+class NoAPITestCase(TestCase):
+
+    def test_no_api(self):
+        """
+        Nothing can be star imported from this package.
+        """
+        import _twisted_platform_support
+        self.assertEqual(_twisted_platform_support.__all__, [])
+
+    def test_sendmsg(self):
+        """
+        Sendmsg can be imported.
+        """
+        from _twisted_platform_support import sendmsg
+        sendmsg
+
+    if _PY3 or platform.isWindows():
+        test_sendmsg.skip = "Not relevant on this platform."
+
+    def test_raiser(self):
+        """
+        Raiser can be imported.
+        """
+        from _twisted_platform_support import raiser
+        raiser
+
+    def test_portmap(self):
+        """
+        Sendmsg can be imported.
+        """
+        from _twisted_platform_support import portmap
+        portmap
+
+    if _PY3 or platform.isWindows():
+        test_portmap.skip = "Not relevant on this platform."
+
+    def test_iocp(self):
+        """
+        IOCP can be imported.
+        """
+        from _twisted_platform_support import iocp
+        iocp
+
+    if not platform.isWindows():
+        test_iocp.skip = "Not relevant on this platform."

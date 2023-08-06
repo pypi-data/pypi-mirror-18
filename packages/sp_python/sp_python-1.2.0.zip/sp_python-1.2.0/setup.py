c31cb17dae4b75dc231f11584a0562014c7c290f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:08:51 +0000

    Add package sp_python/1.2.0/sp_python-1.2.0.zip

---
+++ packages/sp_python/sp_python-1.2.0.zip/sp_python-1.2.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+setup(
+    name='sp_python',
+    version='1.2.0',
+    py_modules=['sp_python1'],
+    author='sp',
+    author_email='497798161@qq.com',
+    url='http://www.sp.com',
+    description='a simple printer of lists',
+    )
+    

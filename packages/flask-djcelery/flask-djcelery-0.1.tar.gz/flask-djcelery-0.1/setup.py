3613614daf39bfb2b6d57d500d92f8fd39d9246c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:07:44 +0000

    Add package flask-djcelery/0.1/flask-djcelery-0.1.tar.gz

---
+++ packages/flask-djcelery/flask-djcelery-0.1.tar.gz/flask-djcelery-0.1/setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+
+setup(
+  name = 'flask-djcelery',
+  packages = ['flask-djcelery'],
+  version = '0.1',
+  description = 'An integration of djcelery with flask application',
+  author = 'Waqas Javed',
+  author_email = 'waqasjaved160@gmail.com',
+  url = 'https://github.com/waqasjaved160/flask-djcelery',
+  download_url = 'https://github.com/waqasjaved160/flask-djcelery/tarball/0.1',
+  keywords = ['flask', 'django', 'djcelery', 'celery', 'dynamic', 'periodic'],
+  classifiers = [],
+)
+

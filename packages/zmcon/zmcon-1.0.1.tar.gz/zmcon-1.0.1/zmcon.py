232c91faac929474fceed18e0c42886cac1df38b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:09:28 +0000

    Add package zmcon/1.0.1/zmcon-1.0.1.tar.gz

---
+++ packages/zmcon/zmcon-1.0.1.tar.gz/zmcon-1.0.1/zmcon.py
@@ -0,0 +1,10 @@
+def printl(each_flick, level):
+
+    
+    for i in each_flick:
+        if isinstance(i,list):
+            printl(i, level+1)
+        else:
+            for tab_stop in range(level):
+                print('\t',end='')
+            print(i)

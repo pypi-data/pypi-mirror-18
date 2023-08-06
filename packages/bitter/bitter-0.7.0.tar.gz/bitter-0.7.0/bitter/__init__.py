750e8246f7f18e20a59117210192de95b013cab1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:22:21 +0000

    Add package bitter/0.7.0/bitter-0.7.0.tar.gz

---
+++ packages/bitter/bitter-0.7.0.tar.gz/bitter-0.7.0/bitter/__init__.py
@@ -0,0 +1,10 @@
+"""
+Bitter module. A library and cli for Twitter using python-twitter.
+http://github.com/balkian/bitter
+"""
+
+import os
+
+from .version import __version__
+
+__all__ = ['cli', 'config', 'crawlers', 'models', 'utils' ]

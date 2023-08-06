d5c62b5581a01b75a70050573ced517bd9c2523e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:15:12 +0000

    Add package pook/0.1.3/pook-0.1.3.tar.gz

---
+++ packages/pook/pook-0.1.3.tar.gz/pook-0.1.3/pook/matchers/base.py
@@ -0,0 +1,109 @@
+import functools
+from copy import deepcopy
+from abc import abstractmethod, ABCMeta
+from ..compare import compare
+from ..regex import isregex, isregex_expr, strip_regex
+
+
+class BaseMatcher(object):
+    """
+    BaseMatcher implements the basic HTTP request matching interface.
+    """
+
+    __metaclass__ = ABCMeta
+
+    # Negate matching if necessary
+    negate = False
+    # Defines if the matching supports regular expression matching
+    regexp = False
+
+    def __init__(self, expectation, negate=False):
+        if not expectation:
+            raise ValueError('expectation argument cannot be empty')
+
+        self.negate = negate
+        self._expectation = expectation
+
+    @property
+    def name(self):
+        return type(self).__name__
+
+    @property
+    def expectation(self):
+        return self._expectation if hasattr(self, '_expectation') else None
+
+    @abstractmethod
+    def match(self, request):
+        """
+        Match performs the value matching.
+        This is an abstract method that must be implemented by child classes.
+
+        Arguments:
+            request (pook.Request): request object to match.
+        """
+        pass
+
+    @expectation.setter
+    def expectation(self, value):
+        self._expectation = value
+
+    def match_regexp(self, re, value):
+        """
+        Matches a regular expression value.
+
+        Arguments:
+            re (str|regex): regular expression value to use.
+            value (str): value to match.
+
+        Returns:
+            bool
+        """
+        if not isregex(re):
+            return False
+
+        if isregex_expr(re):
+            re = strip_regex(re)
+
+        try:
+            return bool(re.compile(re).match(value))
+        except:
+            return False
+
+    def compare(self, value, expectation):
+        """
+        Compares two values with regular expression matching support.
+
+        Arguments:
+            value (mixed): value to compare.
+            expectation (mixed): value to match.
+
+        Returns:
+            bool
+        """
+        if not value:
+            return False
+
+        return compare(value, expectation)
+
+    def to_dict(self):
+        """
+        Returns the current matcher representation as dictionary.
+
+        Returns:
+            dict
+        """
+        return {self.name: deepcopy(self.expectation)}
+
+    def __repr__(self):
+        return '{}({})'.format(self.name, self.expectation)
+
+    def __str__(self):
+        return self.expectation
+
+    @staticmethod
+    def matcher(fn):
+        @functools.wraps(fn)
+        def wrapper(self, *args):
+            result = fn(self, *args)
+            return not result if self.negate else result
+        return wrapper
---
+++ packages/pook/pook-0.1.3.tar.gz/pook-0.1.3/pook/matchers/url.py
@@ -0,0 +1,61 @@
+import re
+import sys
+from .base import BaseMatcher
+from .path import PathMatcher
+from .query import QueryMatcher
+
+if sys.version_info < (3,):     # Python 2
+    from urlparse import urlparse
+else:                           # Python 3
+    from urllib.parse import urlparse
+
+# URI protocol test regular expression
+protoregex = re.compile('^http[s]?://', re.IGNORECASE)
+
+
+class URLMatcher(BaseMatcher):
+    """
+    URLMatcher implements an URL schema matcher.
+    """
+
+    def __init__(self, url):
+        if not url:
+            raise ValueError('url argument cannot be empty')
+        if not isinstance(url, str):
+            raise TypeError('url most be a string')
+
+        # Add protocol prefix in the URL
+        if not protoregex.match(url):
+            url = 'http://{}'.format(url)
+
+        self.url = url
+        self.expectation = urlparse(url)
+
+    def match_path(self, req):
+        path = self.expectation.path
+        if not path:
+            return True
+        return PathMatcher(path).match(req)
+
+    def match_query(self, req):
+        query = self.expectation.query
+        if not query:
+            return True
+        return QueryMatcher(query).match(req)
+
+    @BaseMatcher.matcher
+    def match(self, req):
+        url = self.expectation
+        return all([
+            self.compare(url.scheme, req.url.scheme),
+            self.compare(url.hostname, req.url.hostname),
+            self.compare(url.port or req.url.port, req.url.port),
+            self.match_path(req),
+            self.match_query(req)
+        ])
+
+    def __str__(self):
+        return self.url
+
+    def __repr__(self):
+        return '{}({})'.format(self.name, self.url)

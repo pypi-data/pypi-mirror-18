f9494257a1285123a809b8bfd9570f6d34f11bd6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:31:21 +0000

    Add package tmpr/0.4.4/tmpr-0.5.0.tar.gz

---
+++ packages/tmpr/tmpr-0.5.0.tar.gz/tmpr-0.5.0/setup.py
@@ -0,0 +1,40 @@
+import os
+import re
+import glob
+from setuptools import setup
+
+def read(filename):
+    return open(os.path.join(os.path.dirname(__file__), filename)).read()
+
+try:
+    readme = read('README.md')
+except IOError as e:
+    readme = ''
+
+templates = glob.glob('templates/*')
+
+setup(name='tmpr',
+      license='MIT License',
+      author='Matt Bierbaum',
+      url='https://github.com/mattbierbaum/tmpr',
+      version='0.5.0',
+
+      install_requires=[
+          "tornado>=4.3",
+          "parsedatetime>=2.1",
+          "bcrypt>=3.1"
+        ],
+      packages=['tmpr'],
+      entry_points={
+        'console_scripts': [
+            'tmpr = tmpr.__main__:main'
+        ]
+      },
+      data_files=[('', templates)],
+      package_data={'': ['README.md']},
+
+      platforms='osx, posix, linux, windows',
+      description='Temporary file sharing using simple two digit codes.',
+      long_description=readme,
+      zip_safe=False,
+)

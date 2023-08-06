9fe22170df2c64882ca661cbcc2b92bae2d80896
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:17:20 +0000

    Add package ratom/2.0.9/ratom-2.0.9-py2-none-any.whl

---
+++ packages/ratom/ratom-2.0.9-py2-none-any.whl/ratom/__init__.py
@@ -0,0 +1,11 @@
+"""Rage Against The Outdated Machine"""
+
+# File: ratom/__init__.py
+# Version: 2.0.9
+# Date: 2016-12-08
+# Author: qtfkwk <qtfkwk+ratom@gmail.com>
+# Copyright: (C) 2016 by qtfkwk
+# License: BSD 2-Clause License (https://opensource.org/licenses/BSD-2-Clause)
+
+from common import *
+

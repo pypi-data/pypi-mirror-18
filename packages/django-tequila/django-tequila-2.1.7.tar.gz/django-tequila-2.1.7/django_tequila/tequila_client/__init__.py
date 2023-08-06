7d1299226eaf2375e8ca3e53e1d9068f2f838e69
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:11:16 +0000

    Add package django-tequila/2.1.7/django-tequila-2.1.7.tar.gz

---
+++ packages/django-tequila/django-tequila-2.1.7.tar.gz/django-tequila-2.1.7/django_tequila/tequila_client/__init__.py
@@ -0,0 +1,3 @@
+from __future__ import absolute_import
+from .client import TequilaClient
+from .config import Config, EPFLConfig
---
+++ packages/django-tequila/django-tequila-2.1.7.tar.gz/django-tequila-2.1.7/django_tequila/urls.py
@@ -0,0 +1,8 @@
+from django.conf.urls import url
+from django_tequila.tequila_auth_views import login, logout, not_allowed
+
+urlpatterns = [
+    url(r'^login/?$', login, name='login_view'),
+    url(r'^logout/$', logout, name='logout'),
+    url(r'^not_allowed/$', not_allowed, name='not_allowed'),
+]

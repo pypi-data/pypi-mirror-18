00e95482b52b290407ea6c264301e61f2c9e7b00
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:45:41 +0000

    Add package case_class/0.0.8/case_class-0.0.8.tar.gz

---
+++ packages/case_class/case_class-0.0.8.tar.gz/case_class-0.0.8/setup.py
@@ -0,0 +1,36 @@
+import os
+from setuptools import setup
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
+setup(
+    name="case_class",
+    version="0.0.8",
+
+    url="https://github.com/tkw1536/PythonCaseClass",
+    author="Tom Wiesing",
+    author_email="tkw01536@gmail.com",
+
+    py_modules=['case_class', 'case_class.case_class', 'case_class.clsutils',
+                'case_class.exceptions', 'case_class.signature',
+                'case_class.utils'],
+
+    description=("Scala-like CaseClasses for Python"),
+    long_description=read('README.rst'),
+
+    license="MIT",
+
+    classifiers=[
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 3",
+
+        "License :: OSI Approved :: MIT License",
+
+        "Topic :: Software Development :: Libraries",
+        "Intended Audience :: Developers",
+        "Topic :: Utilities",
+    ]
+)

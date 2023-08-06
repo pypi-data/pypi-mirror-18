93dc77c295fb7b676599b3c63e6c19408c08a7f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:42:42 +0000

    Add package pyLocalIP/1.0.0/pyLocalIP-1.0.0.tar.gz

---
+++ packages/pyLocalIP/pyLocalIP-1.0.0.tar.gz/pyLocalIP-1.0.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+setup(
+    name="pyLocalIP",
+    description="Get Local IP using Python",
+    author="Ethan-Zhang",
+    author_email = "unpeeled_onion@outlook.com",
+    license="http://www.apache.org/licenses/LICENSE-2.0",
+    url="https://github.com/Ethan-Zhang/PyLocalIP",
+    download_url="https://github.com/Ethan-Zhang/PyLocalIP/tree/v1.0",
+    keywords=["localip", "local ip", "local_ip", "LocalIP"],
+    version="1.0.0",
+    )

526a76c70ed394e6fe6cd7fcc29d0755d1b1e65b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:08:15 +0000

    Add package hackcmds/3.6/hackcmds-3.6.tar.gz

---
+++ packages/hackcmds/hackcmds-3.6.tar.gz/hackcmds-3.6/setup.py
@@ -0,0 +1,26 @@
+
+
+from setuptools import setup, find_packages
+
+
+setup(name='hackcmds',
+    version='3.6',
+    description='a simple way to use mongo db, let db like dict',
+    url='https://github.com/Qingluan/.git',
+    author='Qing luan',
+    author_email='darkhackdevil@gmail.com',
+    license='MIT',
+    zip_safe=False,
+    packages=find_packages(),
+    install_requires=['mroylib-min'],
+    include_package_data=True,
+    entry_points={
+        'console_scripts': ['Hacker=Hacker.libs.cmd:main']
+    },
+		scripts = [
+        'init.sh'
+    ]
+
+)
+
+

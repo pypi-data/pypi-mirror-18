87b7a3eb4ba9a286c257c125784821f1187f9297
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:32:07 +0000

    Add package lens-bson/0.0.2/lens_bson-0.0.2.tar.gz

---
+++ packages/lens-bson/lens_bson-0.0.2.tar.gz/lens_bson-0.0.2/setup.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+import os
+from setuptools import setup, find_packages
+
+setup(
+    name='lens_bson',
+    author='Veit Heller',
+    version='0.0.2',
+    license='MIT',
+    url='https://github.com/port-zero/lens_bson',
+    download_url = 'https://github.com/port-zero/lens_bson/tarball/0.0.2',
+    description='A BSON parser for lens',
+    packages=find_packages('.'),
+    install_requires=[
+        "pygments",
+        "lens",
+        "bson",
+    ],
+)
+

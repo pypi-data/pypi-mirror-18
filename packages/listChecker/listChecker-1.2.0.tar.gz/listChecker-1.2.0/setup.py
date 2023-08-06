d271c21fca800ccbde31ecb330be4da6c3366b64
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:50:33 +0000

    Add package listChecker/1.2.0/listChecker-1.2.0.tar.gz

---
+++ packages/listChecker/listChecker-1.2.0.tar.gz/listChecker-1.2.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(
+    name='listChecker',
+    version='1.2.0',
+    py_modules=['listChecker'],
+    author='tony_zhai',
+    author_email='horacezhai@gmail.com',
+    url="",
+    description="",
+)

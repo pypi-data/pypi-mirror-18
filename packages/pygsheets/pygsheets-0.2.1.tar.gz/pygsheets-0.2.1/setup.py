f724efbfd0db512838216d0adaddc6dea2c3639e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:46:09 +0000

    Add package pygsheets/0.2.1/pygsheets-0.2.1.tar.gz

---
+++ packages/pygsheets/pygsheets-0.2.1.tar.gz/pygsheets-0.2.1/setup.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python
+
+import os.path
+import re
+import sys
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+if sys.argv[-1] == 'publish':
+    os.system('python setup.py sdist upload -r pypi')
+    sys.exit()
+
+def read(filename):
+    return open(os.path.join(os.path.dirname(__file__), filename)).read()
+
+description = 'Google Spreadsheets Python API v4'
+
+long_description = """
+{index}
+
+License
+-------
+MIT
+
+Download
+========
+"""
+
+version = re.search(r'^__version__\s*=\s*[\'"]([^\'"]*)[\'"]',
+                    read('pygsheets/__init__.py'), re.MULTILINE).group(1)
+
+setup(
+    name='pygsheets',
+    packages=['pygsheets'],
+    description=description,
+    version=version,
+    author='Nithin Murali',
+    author_email='imnmfotmal@gmail.com',
+    url='https://github.com/nithinmurali/pygsheets',
+    keywords=['spreadsheets', 'google-spreadsheets', 'pygsheets'],
+    install_requires=['google-api-python-client', 'enum'],
+    download_url='https://github.com/nithinmurali/pygsheets/tarball/'+version,
+    classifiers=[
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Intended Audience :: End Users/Desktop",
+        "Intended Audience :: Science/Research",
+        "Topic :: Office/Business :: Financial :: Spreadsheet",
+        "Topic :: Software Development :: Libraries :: Python Modules"
+        ],
+    license='MIT'
+    )

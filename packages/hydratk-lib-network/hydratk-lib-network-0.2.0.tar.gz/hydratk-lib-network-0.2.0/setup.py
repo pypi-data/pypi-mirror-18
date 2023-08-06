a9122d604cd2f096cdbdbbb9262f4af290cfec88
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:45:08 +0000

    Add package hydratk-lib-network/0.2.0/hydratk-lib-network-0.2.0.tar.gz

---
+++ packages/hydratk-lib-network/hydratk-lib-network-0.2.0.tar.gz/hydratk-lib-network-0.2.0/setup.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+from sys import argv
+from install.pre_install import run_pre_install
+from install.post_install import run_post_install
+
+with open("README.rst", "r") as f:
+    readme = f.read()
+    
+run_pre_install(argv)       
+    
+classifiers = [
+    "Development Status :: 5 - Production/Stable",
+    "Environment :: Console",
+    "Environment :: Other Environment",
+    "Intended Audience :: Developers",
+    "License :: Freely Distributable",
+    "Operating System :: OS Independent",   
+    "License :: OSI Approved :: BSD License",
+    "Programming Language :: Python",    
+    "Programming Language :: Python :: 2.6",
+    "Programming Language :: Python :: 2.7",
+    "Programming Language :: Python :: 3.3",
+    "Programming Language :: Python :: 3.4",
+    "Programming Language :: Python :: 3.5",
+    "Programming Language :: Python :: Implementation",
+    "Programming Language :: Python :: Implementation :: CPython", 
+    "Programming Language :: Python :: Implementation :: PyPy",
+    "Topic :: Software Development :: Libraries :: Application Frameworks",
+    "Topic :: Utilities"
+]                        
+          
+setup(
+      name='hydratk-lib-network',
+      version='0.2.0',
+      description='Clients/API for many network protocols and technologies',
+      long_description=readme,
+      author='Petr Rašek, HydraTK team',
+      author_email='bowman@hydratk.org, team@hydratk.org',
+      url='http://libraries.hydratk.org/network',
+      license='BSD',
+      packages=find_packages('src'),
+      package_dir={'' : 'src'},
+      classifiers=classifiers,
+      zip_safe=False
+     )        
+
+run_post_install(argv)

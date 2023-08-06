3a550da3452ba18003726c8acdbcd5bf54200806
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:11:05 +0000

    Add package proso-apps/3.12.11/proso-apps-3.12.11.tar.gz

---
+++ packages/proso-apps/proso-apps-3.12.11.tar.gz/proso-apps-3.12.11/proso_subscription/admin.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+
+from .models import Subscription, DiscountCode
+from django.contrib import admin
+
+
+class SubscriptionAdmin(admin.ModelAdmin):
+
+    list_display = (
+        'user',
+        'plan_description',
+        'created',
+        'expiration',
+        'payment_status'
+    )
+    search_fields = (
+        'user__username',
+    )
+    list_filter = (
+        'plan_description',
+    )
+    raw_id_fields = (
+        "session",
+        "user",
+        "referral",
+    )
+
+    def payment_status(self, s):
+        return s.payment.status['state'] if s.payment is not None else 'FREE'
+
+
+class DiscountCodeAdmin(admin.ModelAdmin):
+
+    list_display = (
+        'identifier',
+        'code',
+        'discount_percentage',
+        'plan',
+        'usage_limit',
+    )
+    list_filter = (
+        'plan',
+        'discount_percentage',
+    )
+
+
+admin.site.register(Subscription, SubscriptionAdmin)
+admin.site.register(DiscountCode, DiscountCodeAdmin)

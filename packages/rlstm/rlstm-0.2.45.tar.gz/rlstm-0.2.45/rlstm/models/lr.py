8e5b14d52780632b438d91be7015fd03a6f7aa72
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:12:47 +0000

    Add package rlstm/0.2.45/rlstm-0.2.45.tar.gz

---
+++ packages/rlstm/rlstm-0.2.45.tar.gz/rlstm-0.2.45/rlstm/models/lr.py
@@ -0,0 +1,43 @@
+import pdb
+import autograd.numpy as np
+import autograd.numpy.random as npr
+from autograd import grad, value_and_grad
+
+from rlstm.common_util import safe_log
+from rlstm.nn_util import sigmoid, flatten_io
+
+
+def cross_entropy(outputs, proba):
+    return outputs * proba + (1 - outputs) * (1 - proba)
+
+
+def logistic_predictions(weights, input_set, fence_set, in_dim_count, out_dim_count):
+    # input should be ( dims by data ) or vertical columns
+    # Adds in the row of ones, so we don't have to when passing things in!
+    # outputs probability of a label being true according to logistic model.
+
+    # flatten but we already have flattened version in farray
+    my_input_set = np.vstack((input_set, np.ones((1, input_set.shape[1]))))
+    weights = np.reshape(weights, (out_dim_count, in_dim_count + 1))
+    return sigmoid(np.dot(weights, my_input_set))
+
+
+def logistic_log_predictions(weights, input_set, fence_set, in_dim_count, out_dim_count):
+    return safe_log(logistic_predictions(weights, input_set, fence_set, in_dim_count, out_dim_count))
+
+
+def log_likelihood(weights, input_set, fence_set, output_set):
+    # Training loss is the negative log-likelihood of the training labels.
+    in_dim_count = input_set.shape[0]
+    out_dim_count = output_set.shape[0]
+
+    pred_set = logistic_predictions(
+        weights,
+        input_set,
+        fence_set,
+        in_dim_count,
+        out_dim_count)
+
+    lprobs = safe_log(cross_entropy(output_set, pred_set))
+    ll = np.sum(lprobs)
+    return ll

569cdf5a204da12381eba16647d129d9de14a0e4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:22:35 +0000

    Add package dcode/0.1.1/dcode-0.1.1.tar.gz

---
+++ packages/dcode/dcode-0.1.1.tar.gz/dcode-0.1.1/dcode/install.py
@@ -0,0 +1,38 @@
+import os
+import sys
+import pkg_resources
+from subprocess import check_call
+
+
+def installMac():
+    # Just opening the mac app will register the handler
+    path = pkg_resources.resource_filename("dcode", "macos/")
+    print("Installing from " + path)
+    check_call(["open", path])
+
+
+def installLinux():
+    path = pkg_resources.resource_filename("dcode", "linux/dcode.desktop")
+    if os.geteuid() == 0:
+        destination = "/usr/share/applications/"
+    else:
+        destination = "~/.local/share/applications/"
+    print("Installing from {} into {}".format(path, destination))
+
+    check_call("""
+        mkdir -p {destination}
+        cp {path} {destination}
+        update-desktop-database {destination}
+    """.format(path=path, destination=destination),
+        shell=True)
+
+
+def install():
+    if sys.platform == "darwin":
+        installMac()
+    elif sys.platform == "linux":
+        installLinux()
+    else:
+        print("Platform not supported.")
+        return
+    print("URL handler installed")

7153f69692ae10203e5863ed6f61989f338098f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:46:50 +0000

    Add package mambo/1.0.0b2/Mambo-1.0.0b2.tar.gz

---
+++ packages/mambo/Mambo-1.0.0b2.tar.gz/Mambo-1.0.0b2/mambo/contrib/auth/exceptions.py
@@ -0,0 +1,6 @@
+
+class AuthError(Exception): pass
+class LoginError(AuthError): pass
+class InvalidLoginError(LoginError): pass
+class InvalidLoginPassword(LoginError): pass
+class VerifyEmailError(LoginError): pass

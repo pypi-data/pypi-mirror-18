80403c188b310d640c2f8522902378ad3be3fe91
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:22:35 +0000

    Add package sqlitestructure/0.2.0/sqlitestructure-0.2.0-py2.py3-none-any.whl

---
+++ packages/sqlitestructure/sqlitestructure-0.2.0-py2.py3-none-any.whl/sqlitestructure/__init__.py
@@ -0,0 +1,13 @@
+# encoding: utf-8
+
+"""
+.. codeauthor:: Tsuyoshi Hombashi <gogogo.vm@gmail.com>
+"""
+
+from ._writer import (
+    TableStructureWriter,
+    TableStructureWriterV0,
+    TableStructureWriterV1,
+    TableStructureWriterV2,
+    TableStructureWriterV3
+)

cf234e7826e18757541f3910a8e9c5d631a8d4ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:44:50 +0000

    Add package thug/0.8.11/thug-0.8.11.tar.gz

---
+++ packages/thug/thug-0.8.11.tar.gz/thug-0.8.11/thug/Logging/BaseLogging.py
@@ -0,0 +1,92 @@
+#!/usr/bin/env python
+#
+# BaseLogging.py
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License version 2 as
+# published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+# MA  02111-1307  USA
+
+import os
+import errno
+import hashlib
+import datetime
+import logging
+
+log = logging.getLogger("Thug")
+
+class BaseLogging(object):
+    def __init__(self):
+        pass
+
+    def check_module(self, module, config):
+        if not getattr(log.ThugOpts, "%s_logging" % (module, ), True):
+            return False
+
+        try:
+            section = config.options(module)
+        except: #pylint:disable=bare-except
+            return True
+
+        for option in section:
+            if option not in ('enable', ):
+                continue
+
+            enable = config.get(module, option)
+            if enable.lower() in ('false', ):
+                return False
+
+        return True
+
+    def set_basedir(self, url):
+        if self.baseDir:
+            return
+
+        t = datetime.datetime.now()
+        m = hashlib.md5()
+        m.update(url.encode('utf8'))
+
+        base = os.getenv('THUG_LOGBASE', os.pardir if os.access(os.pardir, os.W_OK) else '/tmp/thug')
+        self.baseDir = os.path.join(base, 'logs', m.hexdigest(), t.strftime("%Y%m%d%H%M%S"))
+
+        if not log.ThugOpts.file_logging:
+            return
+
+        try:
+            os.makedirs(self.baseDir)
+        except OSError as e:
+            if e.errno == errno.EEXIST:
+                pass
+            else:
+                raise
+
+        with open(os.path.join(base, 'logs', 'thug.csv'), 'at+') as fd:
+            csv_line = '%s,%s\n' % (m.hexdigest(), url, )
+            for l in fd.readlines():
+                if l == csv_line:
+                    return
+
+            fd.write(csv_line)
+
+    def set_absbasedir(self, basedir):
+        self.baseDir = basedir
+
+        if not log.ThugOpts.file_logging:
+            return
+
+        try:
+            os.makedirs(self.baseDir)
+        except OSError as e:
+            if e.errno == errno.EEXIST:
+                pass
+            else:
+                raise
---
+++ packages/thug/thug-0.8.11.tar.gz/thug-0.8.11/thug/__init__.py
@@ -0,0 +1,2 @@
+__version__            = "0.8.11"
+__configuration_path__ = "/etc/thug"

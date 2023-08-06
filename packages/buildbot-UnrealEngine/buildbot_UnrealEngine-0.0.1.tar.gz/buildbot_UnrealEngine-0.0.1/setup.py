4cb16ec857748e5b1b61261f8536eed94e9634ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:22:37 +0000

    Add package buildbot-UnrealEngine/0.0.1/buildbot_UnrealEngine-0.0.1.tar.gz

---
+++ packages/buildbot-UnrealEngine/buildbot_UnrealEngine-0.0.1.tar.gz/buildbot_UnrealEngine-0.0.1/setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+VERSION="0.0.1"
+
+setup(
+    name="buildbot_UnrealEngine",
+    version=VERSION,
+    description="Easy configuration for the Unreal Automation Tool",
+    author="Marvin Pohl",
+    author_email="mp120@hdm-stuttgart.de",
+    url="https://github.com/pampersrocker/ue4-buildbot",
+    packages=["buildbot_UnrealEngine"],
+    requires=["Buildbot (==0.9.1)"],
+    setup_requires=['pytest-runner'],
+    tests_require=['pytest'],
+    entry_points= {
+      'buildbot.steps': [
+        'BuildCookRun = buildbot_UnrealEngine.UAT:BuildCookRun'
+      ]
+    }
+)

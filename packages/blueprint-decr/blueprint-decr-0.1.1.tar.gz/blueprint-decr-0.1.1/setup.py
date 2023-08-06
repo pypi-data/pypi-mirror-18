ac67e249d9db0d54929b03d188e4164a84077b6a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:12:02 +0000

    Add package blueprint-decr/0.1.1/blueprint-decr-0.1.1.tar.gz

---
+++ packages/blueprint-decr/blueprint-decr-0.1.1.tar.gz/blueprint-decr-0.1.1/setup.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+
+long_description = ""
+
+try:
+    import pypandoc
+    long_description = pypandoc.convert('README.md', 'rst')
+except:
+    pass
+
+sdict = {
+    'name': 'blueprint-decr',
+    'version': "0.1.1",
+    'license': 'MIT',
+    'packages': ['blueprint_decr'],
+    'zip_safe': False,
+    'install_requires': ['six'],
+    'long_description': long_description,
+    'author': 'Lichun',
+    'url': 'https://github.com/socrateslee/blueprint-decr',
+    'classifiers': [
+        'Environment :: Console',
+        'Framework :: Flask',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python']
+}
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+setup(**sdict)

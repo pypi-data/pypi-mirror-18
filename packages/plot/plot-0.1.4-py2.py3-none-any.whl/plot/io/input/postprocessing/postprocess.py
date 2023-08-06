22d83d4f8c8d51290ea960cc3a95665d4921ab9b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:10:19 +0000

    Add package plot/0.1.4/plot-0.1.4-py2.py3-none-any.whl

---
+++ packages/plot/plot-0.1.4-py2.py3-none-any.whl/plot/io/input/postprocessing/postprocess.py
@@ -0,0 +1,32 @@
+"""
+Postprocess the internal parameter dictionary
+"""
+from typing import Dict
+from ....tk.fnTK import compose
+from .set_data_id import set_data_id
+from .set_panel_color_order import set_panel_color_order
+from .set_data_color import set_data_color
+from .assort_plots_by_type import assort_plots_by_type
+from .set_data_columns import set_data_columns
+from .set_panel_minmax import set_panel_minmax
+
+
+def postprocess(params):
+    # type: (Dict) -> Dict
+    """Postprocess the internal parameter dictionary
+
+    Args:
+        params(dict): internal parameter dictionary
+
+    Returns:
+        enhanced internal parameter dictionary
+    """
+    aux = compose([
+        set_data_id,
+        set_panel_color_order,
+        set_data_color,
+        assort_plots_by_type,
+        set_data_columns,
+        set_panel_minmax
+        ])
+    return aux(params)

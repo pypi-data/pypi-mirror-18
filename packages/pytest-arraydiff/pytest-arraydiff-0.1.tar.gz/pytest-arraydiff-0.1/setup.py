adc4f148a0f92f7d6620d6501f1b19dcaab664aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:46:38 +0000

    Add package pytest-arraydiff/0.1/pytest-arraydiff-0.1.tar.gz

---
+++ packages/pytest-arraydiff/pytest-arraydiff-0.1.tar.gz/pytest-arraydiff-0.1/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+from pytest_arraydiff import __version__
+
+# IMPORTANT: we deliberately use rst here instead of markdown because long_description
+# needs to be in rst, and requiring pandoc to be installed to convert markdown to rst
+# on-the-fly is over-complicated and sometimes the generated rst has warnings that
+# cause PyPI to not display it correctly.
+
+with open('README.rst') as infile:
+    long_description = infile.read()
+
+setup(
+    version=__version__,
+    url="https://github.com/astrofrog/pytest-fits",
+    name="pytest-arraydiff",
+    description='pytest plugin to help with comparing array output from tests',
+    long_description=long_description,
+    packages = ['pytest_arraydiff'],
+    license='BSD',
+    author='Thomas Robitaille',
+    author_email='thomas.robitaille@gmail.com',
+    entry_points = {'pytest11': ['pytest_arraydiff = pytest_arraydiff.plugin',]},
+)

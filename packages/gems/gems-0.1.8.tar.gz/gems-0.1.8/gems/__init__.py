1c5bda55c2b58cfee4ef5b6e1b67aa289194fca3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:49:13 +0000

    Add package gems/0.1.8/gems-0.1.8.tar.gz

---
+++ packages/gems/gems-0.1.8.tar.gz/gems-0.1.8/gems/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+__author__ = 'Blake Printy'
+__email__ = 'bprinty@gmail.com'
+__version__ = '0.1.8'
+
+
+from .datatypes import composite
+from .datatypes import filetree
+

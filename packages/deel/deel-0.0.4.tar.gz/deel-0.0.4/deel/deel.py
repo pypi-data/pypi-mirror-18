8ac8d62d5dea01433a3a6a457ee9739bbd937dbb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:11:51 +0000

    Add package deel/0.0.4/deel-0.0.4.tar.gz

---
+++ packages/deel/deel-0.0.4.tar.gz/deel-0.0.4/deel/deel.py
@@ -0,0 +1,71 @@
+import chainer.functions as F
+from chainer import Variable, FunctionSet, optimizers
+from chainer.links import caffe
+from chainer import computational_graph
+from chainer import cuda
+from chainer import optimizers
+from chainer import serializers
+from tensor import *
+import json
+import os
+import multiprocessing
+import threading
+import time
+import six
+import numpy as np
+import os.path
+from PIL import Image
+#import six.moves.cPickle as pickle
+from six.moves import queue
+import pickle
+import hashlib
+import datetime
+import sys
+import random
+
+
+class Deel(object):
+	singlton = None
+	train = False
+	val = None
+	root = '.'
+	epoch=1000
+	gpu=-1
+	mean=None
+	labels=None
+	xp = np
+	optimizer_lr=0.1
+	trainCount=0
+	lstm_train=None
+	defferedTuning=False
+	def __init__(self,gpu=-1):
+		Deel.singleton = self
+		Deel.gpu=gpu
+		if gpu>=0:
+			cuda.get_device(gpu).use()
+			Deel.xp = cuda.cupy if gpu >= 0 else np
+
+	@staticmethod
+	def getInstance():
+		return Deel.singleton
+
+'''
+	Trainer
+'''
+class BatchTrainer(object):
+	batchsize=32
+	val_batchsize=32
+	data_q=None
+	res_q=None
+	loaderjob=20
+	train_list=None
+	val_list=None
+	lstm_train=None
+	num_of_vocab=0
+	mode='CNN'
+	vocab=None
+	in_size=256
+	def __init__(self,in_size=256):
+		BatchTrainer.data_q = queue.Queue(maxsize=1)
+		BatchTrainer.res_q = queue.Queue()
+
---
+++ packages/deel/deel-0.0.4.tar.gz/deel-0.0.4/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+install_requires = [
+    'chainer>=1.16.0',
+    'filelock',
+    'nose',
+    'numpy>=1.9.0',
+    'protobuf',
+    'six>=1.9.0',
+]
+setup(
+    name = "deel",
+    version = "0.0.4",
+    author = "UEI corporation",
+    author_email="info@uei.co.jp",
+    description = ("Deel; A High level deep learning description language"),
+    license = "MIT",
+    keywords = "chainer",
+    url = "https://github.com/uei/deel",
+    packages=[  'deel',
+                'deel.network',
+                'deel.model',
+                ],
+)

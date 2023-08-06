8ade77b688035dbb32c55f32d3c01d2f6c735a4d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:24 +0000

    Add package nesterhhh/1.0.0/nesterhhh-1.0.0.tar.gz

---
+++ packages/nesterhhh/nesterhhh-1.0.0.tar.gz/nesterhhh-1.0.0/setup.py
@@ -0,0 +1,13 @@
+movies=("012","123","234")
+
+from distutils.core import setup
+
+setup(
+       name='nesterhhh',
+       version='1.0.0',
+       py_modules=['nesterhhh'],
+       author='hfpython',
+       author_email='hfpython@headfirstlabs.com',
+       url='http://www.headfirstlabs.com',
+       description='A simple printer of nester lists',
+     )

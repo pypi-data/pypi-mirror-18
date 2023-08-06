0fb9a1bf88e5ff24fc965bb586a7696f928fdd74
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:30:26 +0000

    Add package nesterxhd/1.0.0/nesterxhd-1.0.0.zip

---
+++ packages/nesterxhd/nesterxhd-1.0.0.zip/nesterxhd-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name = 'nesterxhd',
+    version = '1.0.0',
+    py_modules = ['nester'],
+    author = 'hfpython',
+    author_email = 'mrxiehaodong@163.com',
+    url = 'http://www.headfirstlabs.com',
+    description = 'A simple printer or nester lists',
+    )

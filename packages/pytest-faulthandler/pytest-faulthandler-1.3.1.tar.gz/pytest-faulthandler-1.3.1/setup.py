3b1c9d3abb2681e7e0faf1ef3ed22114bbcf450c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:11:16 +0000

    Add package pytest-faulthandler/1.3.1/pytest-faulthandler-1.3.1.tar.gz

---
+++ packages/pytest-faulthandler/pytest-faulthandler-1.3.1.tar.gz/pytest-faulthandler-1.3.1/setup.py
@@ -0,0 +1,40 @@
+from setuptools import setup
+
+
+with open('README.rst') as f:
+    long_description = f.read()
+
+setup(
+    name='pytest-faulthandler',
+    version='1.3.1',
+    py_modules=['pytest_faulthandler'],
+    url='https://github.com/pytest-dev/pytest-faulthandler',
+    license='MIT',
+    install_requires=['pytest>=2.6'],
+    test_requires=['pytest-mock>=0.6'],
+    author='Bruno Oliveira',
+    author_email='nicoddemus@gmail.com',
+    description='py.test plugin that activates the fault handler module for tests',
+    long_description=long_description,
+    extras_require={
+        ':python_version=="2.6" or python_version=="2.7"': ['faulthandler'],
+    },
+    entry_points={
+        'pytest11': ['pytest_faulthandler = pytest_faulthandler'],
+    },
+    keywords='pytest faulthandler',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Framework :: Pytest',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Topic :: Software Development :: Testing',
+    ]
+)

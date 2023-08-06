708d3d991639eccae72ef2562d69f73906b66461
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:18:46 +0000

    Add package run-lambda/0.1.3/run_lambda-0.1.3-py3-none-any.whl

---
+++ packages/run-lambda/run_lambda-0.1.3-py3-none-any.whl/run_lambda/utils.py
@@ -0,0 +1,22 @@
+import binascii
+import datetime
+import os
+
+
+def random_aws_request_id():
+    return "-".join(random_hex(l) for l in [8, 4, 4, 4, 12])
+
+
+def random_log_stream_name():
+    today = datetime.date.today()
+    return "{t}/[$LATEST]{h}".format(t=today.strftime("%Y/%m/%d"),
+                                     h=random_hex(32))
+
+
+def random_hex(length):
+    result = binascii.b2a_hex(os.urandom((length + 1) // 2))
+    if not isinstance(result, str):
+        result = result.decode("ascii")
+    if length % 2 == 1:
+        return result[:-1]
+    return result

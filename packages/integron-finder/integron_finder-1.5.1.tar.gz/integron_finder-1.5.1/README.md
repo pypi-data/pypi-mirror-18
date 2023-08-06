f5068a944debdcb1e8da03ae4bceb1a994b596e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:10:51 +0000

    Add package integron-finder/1.5.1/integron_finder-1.5.1.tar.gz

---
+++ packages/integron-finder/integron_finder-1.5.1.tar.gz/integron_finder-1.5.1/README.md
@@ -0,0 +1,126 @@
+[![install](https://img.shields.io/github/downloads/gem-pasteur/Integron_finder/total.svg)](https://github.com/gem-pasteur/Integron_Finder/releases/latest) [![Doc] (https://readthedocs.org/projects/integronfinder/badge/?version=latest)](http://integronfinder.readthedocs.io/en/v1.5/) [![License (GPL version 3)](https://img.shields.io/badge/license-GNU%20GPL%20version%203-blue.svg?style=flat-square)](https://opensource.org/licenses/GPL-3.0)
+
+Integron Finder
+===============
+
+Find integrons in DNA sequences
+
+See the [latest release](https://github.com/gem-pasteur/Integron_Finder/releases/latest) to download the program. Or, use it online on the [Mobyle Webserver](http://mobyle.pasteur.fr/cgi-bin/portal.py#forms::integron_finder)
+
+See Documentation for how to install it and how to use it: [![Doc] (https://readthedocs.org/projects/integronfinder/badge/?version=latest)](http://integronfinder.readthedocs.io/en/v1.5/)
+
+# Licence:
+
+Integron Finder is under [open source licence GPLv3](https://opensource.org/licenses/GPL-3.0)
+
+# Dependencies :
+
+- Python 2.7
+   - Pandas 0.18.0
+   - Numpy 1.9.1
+   - Biopython 1.65
+   - Matplotlib 1.4.3
+   - psutils 2.1.3
+- HMMER 3.1b1
+- INFERNAL 1.1
+- Prodigal V2.6.2
+
+# Usage
+
+```
+usage: integron_finder [-h] [--local_max] [--func_annot] [--cpu CPU]
+                       [-dt DISTANCE_THRESH] [--outdir .] [--linear]
+                       [--union_integrases] [--cmsearch CMSEARCH]
+                       [--hmmsearch HMMSEARCH] [--prodigal PRODIGAL]
+                       [--path_func_annot bank_hmm] [--gembase]
+                       [--attc_model file.cm] [--evalue_attc 1]
+                       [--keep_palindromes] [--no_proteins]
+                       [--max_attc_size 200] [--min_attc_size 40]
+                       [--eagle_eyes] [-V]
+                       replicon
+
+positional arguments:
+  replicon              Path to the replicon file (in fasta format), eg :
+                        path/to/file.fst or file.fst
+
+optional arguments:
+  -h, --help            show this help message and exit
+  --local_max           Allows thorough local detection (slower but more
+                        sensitive and do not increase false positive rate).
+  --func_annot          Functional annotation of CDS associated with integrons
+                        HMM files are needed in Func_annot folder.
+  --cpu CPU             Number of CPUs used by INFERNAL and HMMER
+  -dt DISTANCE_THRESH, --distance_thresh DISTANCE_THRESH
+                        Two elements are aggregated if they are distant of
+                        DISTANCE_THRESH [4kb] or less
+  --outdir .            Set the output directory (default: current)
+  --linear              Consider replicon as linear. If replicon smaller than
+                        20kb, it will be considered as linear
+  --union_integrases    Instead of taking intersection of hits from Phage_int
+                        profile (Tyr recombinases) and integron_integrase
+                        profile, use the union of the hits
+  --cmsearch CMSEARCH   Complete path to cmsearch if not in PATH. eg:
+                        /usr/local/bin/cmsearch
+  --hmmsearch HMMSEARCH
+                        Complete path to hmmsearch if not in PATH. eg:
+                        /usr/local/bin/hmmsearch
+  --prodigal PRODIGAL   Complete path to prodigal if not in PATH. eg:
+                        /usr/local/bin/prodigal
+  --path_func_annot bank_hmm
+                        Path to file containing all hmm bank paths (one per
+                        line)
+  --gembase             Use gembase formatted protein file instead of
+                        Prodigal. Folder structure must be preserved
+  --attc_model file.cm  path or file to the attc model (Covariance Matrix)
+  --evalue_attc 1       set evalue threshold to filter out hits above it
+                        (default: 1)
+  --keep_palindromes    for a given hit, if the palindromic version is found,
+                        don't remove the one with highest evalue
+  --no_proteins         Don't annotate CDS and don't find integrase, just look
+                        for attC sites.
+  --max_attc_size 200   set maximum value fot the attC size (default: 200bp)
+  --min_attc_size 40    set minimum value fot the attC size (default: 40bp)
+  --eagle_eyes          Synonym of --local_max. Like a soaring eagle in the
+                        sky, catching rabbits(or attC sites) by surprise.
+  -V, --version         show program's version number and exit
+
+```
+
+
+### Example
+
+    integron_finder myfastafile.fst --local_max --func_annot
+
+## Output :
+
+A folder name Results\_id\_genome, inside there are different files :
+
+- *.gbk : contains the input sequence with all integrons and features found.
+- *.integrons : contain list of all element detected (attc, protein near attC, integrase, Pc, attI, Pint) with position, strand, evalue, etc...
+- *.pdf : representation of complete integrons detected (with integrase (redish) and at least one attc (blueish)). If a protein has a hit with an antibiotic resistance gene, it's yellow, otherwise grey.
+
+ and one folder, `other`, containing the different outputs of the different steps of the program.
+
+ # Mobyle
+
+You can use this program whithout installing it, through a webserver:
+
+http://mobyle.pasteur.fr/cgi-bin/portal.py#forms::integron_finder
+
+# Citation
+
+ The paper is published in Nucleic Acid Research.
+
+Identification and analysis of integrons and cassette arrays in bacterial genomes
+Jean Cury; Thomas Jove; Marie Touchon; Bertrand Neron; Eduardo PC Rocha
+Nucleic Acids Research 2016; [doi: 10.1093/nar/gkw319](http://nar.oxfordjournals.org/cgi/content/full/gkw319)
+
+ Please cite also the following articles:
+
+ - Nawrocki, E.P. and Eddy, S.R. (2013) Infernal 1.1: 100-fold faster RNA homology searches. Bioinformatics, 29, 2933-2935.
+ - Eddy, S.R. (2011) Accelerated Profile HMM Searches. PLoS Comput Biol, 7, e1002195.
+ - Hyatt, D., Chen, G.L., Locascio, P.F., Land, M.L., Larimer, F.W. and Hauser, L.J. (2010) Prodigal: prokaryotic gene recognition and translation initiation site identification. BMC Bioinformatics, 11, 119.
+
+ and if you use the function `--func_annot` which uses Resfams:
+
+ - Gibson, M.K., Forsberg, K.J. and Dantas, G. (2015) Improved annotation of antibiotic resistance determinants reveals microbial resistomes cluster by ecology. ISME J, 9, 207-216.
---
+++ packages/integron-finder/integron_finder-1.5.1.tar.gz/integron_finder-1.5.1/setup.py
@@ -0,0 +1,231 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import sys
+if sys.version_info[0] == 3:
+    sys.exit("Sorry, Python 3 is not supported yet")
+
+import os
+import sysconfig
+
+try:
+    import warnings
+except ImportError:
+    warnings = None
+
+from distutils.errors import DistutilsFileError
+from distutils.util import subst_vars as distutils_subst_vars
+
+from setuptools import setup
+from setuptools.dist import Distribution
+from setuptools.command.install_scripts import install_scripts as _install_scripts
+
+
+class install_scripts(_install_scripts):
+
+    def finalize_options(self):
+        inst = self.distribution.command_options.get('install', {})
+        _install_scripts.finalize_options(self)
+
+    def run(self):
+        if os.path.exists(self.build_dir):
+            installer = 'pip'
+        else:
+            installer = 'fucking_setuptools'
+
+        script_tmp_dir = self.build_dir if installer != 'fucking_setuptools' else self.install_dir
+
+        def subst_file(_file, vars_2_subst):
+            input_file = os.path.join(script_tmp_dir, _file)
+            output_file = input_file + '.tmp'
+            subst_vars(input_file, output_file, vars_2_subst)
+            os.unlink(input_file)
+            self.move_file(output_file, input_file)
+
+        # if setup.py install is used without using setup.py build
+        # setuptools creates a directory build/bdist.linux-x86_64/egg/EGG-INFO/scripts/
+        # but not build/scripts
+        # and without attribute to grasp this directory :-(((
+        # so we need to run _install_scripts first
+        # to create build/scripts (self.build_dir)
+        # and then substitute variables in this dir
+
+        if installer == 'fucking_setuptools':
+            _install_scripts.run(self)
+
+        inst = self.distribution.command_options.get('install', {})
+        if self.distribution.fix_scripts is not None:
+            vars_2_subst = {'PREFIX': inst['prefix'][1] if 'prefix' in inst else '',
+                            'PREFIXSHARE': os.path.join(get_install_data_dir(inst), 'integron_finder'),
+                            'VERSION': self.distribution.get_version(),
+                            }
+            for _file in self.distribution.fix_scripts:
+                subst_file(_file, vars_2_subst)
+                pass
+        if installer == 'pip':
+            _install_scripts.run(self)
+
+
+class UsageDistribution(Distribution):
+
+    def __init__(self, attrs=None):
+        #It's important to define potions before to call __init__
+        #otherwise AttributeError: UsageDistribution instance has no attribute 'conf_files'
+        #self.doc_files = None
+        self.fix_prefix = None
+        self.fix_scripts = None
+        Distribution.__init__(self, attrs=attrs)
+        self.common_usage = """\
+Common commands: (see '--help-commands' for more)
+
+  setup.py build      will build the package underneath 'build/'
+  setup.py install    will install the package
+"""
+
+
+def get_install_data_dir(inst):
+    """
+    :param inst: installation option
+    :type inst: dict
+    :return: the prefix where to install data
+    :rtype: string
+    """
+
+    if 'VIRTUAL_ENV' in os.environ:
+        inst['prefix'] = ('environment', os.environ['VIRTUAL_ENV'])
+    elif 'user' in inst:
+        import site
+        inst['prefix'] = ('command line', site.USER_BASE)
+    elif 'root' in inst:
+        inst['prefix'] = ('command line',
+                          os.path.join(inst['root'][1],
+                                       sysconfig.get_path('data').strip(os.path.sep)
+                                       )
+                          )
+
+    if 'install_data' in inst:
+        install_dir = inst['install_data'][1]
+    elif 'prefix' in inst:
+        install_dir = os.path.join(inst['prefix'][1], 'share')
+    else:
+        install_dir = os.path.join(sysconfig.get_path('data'), 'share')
+    return install_dir
+
+
+def subst_vars(src, dst, vars):
+    """
+    substitute variables (string starting with $) in file
+    :param src: the file containing variable to substitute
+    :type src: string
+    :param dst: the destination file
+    :type dst: string
+    :param vars: the variables to substitute in dict key are variable name
+    :type vars: dict
+    """
+    try:
+        src_file = open(src, "r")
+    except os.error as err:
+        raise DistutilsFileError("could not open '{0}': {1)".format(src, err))
+    try:
+        dest_file = open(dst, "w")
+    except os.error as err:
+        raise DistutilsFileError("could not create '{0}': {1}".format(dst, err))
+    with src_file:
+        with dest_file:
+            for line in src_file:
+                new_line = distutils_subst_vars(line, vars)
+                dest_file.write(new_line)
+
+
+def expand_data(data_to_expand):
+    """
+    From data structure like setup.py data_files (see http://)
+     [(directory/where/to/copy/the/file, [path/to/file/in/archive/file1, ...]), ...]
+    but instead of the original struct this one accept to specify a directory in elements to copy.
+
+    This function will generate one entry for all *content* of the directory and subdirectory
+    recursively, to in fine copy the tree in archive in dest on the host
+
+    the first level of directory itself is not include (which allow to rename it)
+    :param data_to_expand:
+    :type  data_to_expand: list of tuple
+    :return: list of tuple
+    """
+    def remove_prefix(prefix, path):
+        prefix = os.path.normpath(prefix)
+        path = os.path.normpath(path)
+        to_remove = len([i for i in prefix.split(os.path.sep) if i])
+        truncated = [i for i in path.split(os.path.sep) if i][to_remove:]
+        truncated = os.path.sep.join(truncated)
+        return truncated
+
+    data_struct = []
+    for base_dest_dir, src in data_to_expand:
+        base_dest_dir = os.path.normpath(base_dest_dir)
+        for one_src in src:
+            if os.path.isdir(one_src):
+                for path, _, files in os.walk(one_src):
+                    if not files:
+                        continue
+                    path_2_create = remove_prefix(one_src, path)
+                    data_struct.append(
+                        (os.path.join(base_dest_dir, path_2_create), [os.path.join(path, f) for f in files]))
+            if os.path.isfile(one_src):
+                data_struct.append((base_dest_dir, [one_src]))
+    return data_struct
+
+
+###################################################
+#                                                 #
+# the configuration of the installer start bellow #
+#                                                 #
+###################################################
+
+setup(name='integron_finder',
+      version="1.5.1",
+      description="Integron Finder aims at detecting integrons in DNA sequences",
+      long_description="""Integron Finder aims at detecting integrons in DNA sequences
+by finding particular features of the integron:
+  - the attC sites
+  - the integrase
+  - and when possible attI site and promoters.
+
+integron_finder has been describe in a paper published in Nucleic Acid Research.
+
+Identification and analysis of integrons and cassette arrays in bacterial genomes.
+Jean Cury; Thomas Jove; Marie Touchon; Bertrand Néron; Eduardo PC Rocha
+Nucleic Acids Research 2016; doi:10.1093/nar/gkw319
+""",
+      author="Jean Cury",
+      author_email="jean.cury@normalesup.org",
+      url="https://github.com/gem-pasteur/Integron_Finder/",
+      download_url='https://github.com/gem-pasteur/Integron_Finder/archive/v1.5.1.tar.gz',
+      license="GPLv3",
+      classifiers=[
+          'Development Status :: 5 - Production/Stable',
+          'Environment :: Console',
+          'Operating System :: POSIX',
+          'Programming Language :: Python :: 2',
+          'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+          'Intended Audience :: Science/Research',
+          'Topic :: Scientific/Engineering :: Bio-Informatics'
+          ],
+      install_requires=['numpy>=1.7.0',
+                        'matplotlib>=1.4.2',
+                        'pandas>=0.18.0',
+                        'biopython>=1.65',
+                        ],
+
+      scripts=['integron_finder'],
+      #file where some variable must be fix by install
+      fix_scripts=['integron_finder'],
+      #(dataprefix +'where to put the data in the install, [where to find the data in the tar ball]
+      data_files=expand_data([('share/integron_finder/data/', ['data']),
+                              ('share/integron_finder/doc/html', ['doc/build/html']),
+                              ('share/integron_finder/doc/pdf', ['doc/build/latex/IntegronFinder.pdf'])
+                             ]),
+      cmdclass={'install_scripts': install_scripts},
+      distclass=UsageDistribution
+      )
+
+

520a87ec029a9582dbdf4fa529a8836c1d4ef20f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:10:52 +0000

    Add package fricles/0.32/fricles-0.32.tar.gz

---
+++ packages/fricles/fricles-0.32.tar.gz/fricles-0.32/fricles/sentiments.py
@@ -0,0 +1,34 @@
+""" Module provides sentiment for the given text.
+
+        >> How to install ?
+            pip install fricles
+
+        >> How to use ?
+            from fricles import sentiments
+        sent_op = sentiments().get_sentiments("text")
+@author: Chetan G
+@contact: chetan.ganjihal@gmail.com
+@since: Created on 2016-02-04
+@copyright: Copyright (C) 2016 Chetan Team. All rights reserved.
+@license: http://www.apache.org/licenses/LICENSE-2.0 Apache License
+"""
+
+#'
+
+import copy
+import requests
+
+class sentiments:
+
+    url = "http://fricles.com:8080/fricles/api/v1/get_sentiments"
+    data = { "text" : "" } 
+    def get_sentiments(self, text):
+        inputData = copy.deepcopy(self.data.copy())
+        try:
+            data['text'] = text
+            r = requests.post(self.url, data=inputData)
+            print r.status_code
+            print r.text
+        except Exception, err:
+            return {"status" : "Failed", "http_code" : err.code, "message" : err.message}
+
---
+++ packages/fricles/fricles-0.32.tar.gz/fricles-0.32/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+    name = 'fricles',
+    packages = ['fricles'], # this must be the same as the name above
+    version = '0.32',
+    description = 'Client library to extract sentiments from text',
+    author = 'Chetan Ganjihal',
+    author_email = 'chetan@humdingerlabs.com',
+    url = 'https://github.com/supertramped/fricles-client', # use the URL to the github repo
+    download_url='https://github.com/supertramped/fricles-client/releases/tag/0.32',
+    keywords = ['fricles'], # arbitrary keywords
+    classifiers = [],
+)

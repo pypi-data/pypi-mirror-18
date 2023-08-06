f2cabea504a011802c5a2422cb08b206bf2fc69d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:18:38 +0000

    Add package blockade/0.3.1/blockade-0.3.1.tar.gz

---
+++ packages/blockade/blockade-0.3.1.tar.gz/blockade-0.3.1/blockade/cli.py
@@ -0,0 +1,427 @@
+#
+#  Copyright (C) 2014 Dell, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+from clint.textui import puts, puts_err, colored, columns
+
+import argparse
+import errno
+import json
+import sys
+import traceback
+import yaml
+
+from .api import rest
+from .config import BlockadeConfig
+from .core import Blockade
+from .errors import BlockadeError
+from .errors import InsufficientPermissionsError
+from .net import BlockadeNetwork
+from .state import BlockadeState
+from .utils import check_docker
+
+
+
+def load_config(config_file=None):
+    error = None
+    try:
+        # if a path was specified, load only from there and fail if not found
+        if config_file is not None:
+            with open(config_file) as f:
+                d = yaml.safe_load(f)
+                return BlockadeConfig.from_dict(d)
+
+        # otherwise, try the default paths
+        for path in ("blockade.yaml", "blockade.yml"):
+            try:
+                with open(path) as f:
+                    d = yaml.safe_load(f)
+                    return BlockadeConfig.from_dict(d)
+            except IOError as e:
+                if e.errno != errno.ENOENT:
+                    raise
+
+        # finally, return a "blank" config. This may be an empty blockade that
+        # containers will later be added to
+        return BlockadeConfig()
+    except Exception as e:
+        error = e
+    raise BlockadeError("Failed to load config (from --config, "
+                        "./blockade.yaml, or ./blockade.yml)" +
+                        (": " + str(error) if error else ""))
+
+
+def get_blockade(config, opts):
+    blockade_id = opts.name if hasattr(opts, 'name') else None
+    state = BlockadeState(blockade_id=blockade_id,
+                          data_dir=opts.data_dir)
+    return Blockade(config,
+                    blockade_id=blockade_id,
+                    state=state,
+                    network=BlockadeNetwork(config))
+
+
+def print_containers(containers, to_json=False):
+    containers = sorted(containers, key=lambda c: c.name)
+
+    if to_json:
+        d = [c.to_dict() for c in containers]
+        puts(json.dumps(d, indent=2, sort_keys=True, separators=(',', ': ')))
+
+    else:
+        puts(colored.blue(columns(["NODE",               15],
+                                  ["CONTAINER ID",       15],
+                                  ["STATUS",              7],
+                                  ["IP",                 15],
+                                  ["NETWORK",            10],
+                                  ["PARTITION",          10])))
+
+        def partition_label(c):
+            if c.holy:
+                return "H"
+            elif c.partition:
+                if c.neutral:
+                    return str(c.partition) + " [N]"
+                else:
+                    return str(c.partition)
+            elif c.neutral:
+                return "N"
+            else:
+                return ""
+
+        for container in containers:
+            puts(columns([container.name,                15],
+                         [container.container_id[:12],   15],
+                         [container.status,               7],
+                         [container.ip_address or "",    15],
+                         [container.network_state,       10],
+                         [partition_label(container),    10]))
+
+
+def _add_output_options(parser):
+    parser.add_argument('--json', action='store_true',
+                        help='Output in JSON format')
+
+
+def _add_container_selection_options(parser):
+    parser.add_argument('containers', metavar='CONTAINER', nargs='*',
+                        help='Container to select')
+    parser.add_argument('--all', action='store_true',
+                        help='Select all containers')
+    parser.add_argument('--random', action='store_true',
+                        help='Select a random container')
+
+
+def _check_container_selections(opts):
+    if opts.containers and opts.all:
+        raise BlockadeError("Either specify individual containers "
+                            "or --all, but not both")
+    elif opts.all and opts.random:
+        raise BlockadeError("Specify either --all or --random, but not both")
+    elif not (opts.containers or opts.all or opts.random):
+        raise BlockadeError("Specify individual containers or --all or --random")
+
+    return (opts.containers or None, opts.all, opts.random)
+
+
+def cmd_up(opts):
+    """Start the containers and link them together
+    """
+    config = load_config(opts.config)
+    b = get_blockade(config, opts)
+    containers = b.create(verbose=opts.verbose, force=opts.force)
+    print_containers(containers, opts.json)
+
+
+def cmd_destroy(opts):
+    """Destroy all containers and restore networks
+    """
+    config = load_config(opts.config)
+    b = get_blockade(config, opts)
+    b.destroy()
+
+
+def cmd_status(opts):
+    """Print status of containers and networks
+    """
+    config = load_config(opts.config)
+    b = get_blockade(config, opts)
+    containers = b.status()
+    print_containers(containers, opts.json)
+
+
+def __with_containers(opts, func, **kwargs):
+    containers, select_all, select_random = _check_container_selections(opts)
+    config = load_config(opts.config)
+    b = get_blockade(config, opts)
+    b.state.load()
+
+    configured_containers = set(b.state.containers.keys())
+    container_names = configured_containers if select_all or (select_random and not containers) else configured_containers.intersection(containers)
+
+    if len(container_names) > 0:
+        kwargs['select_random'] = select_random
+        return func(b, container_names, **kwargs)
+    else:
+        raise BlockadeError('selection does not match any container')
+
+
+def cmd_start(opts):
+    """Start some or all containers
+    """
+    __with_containers(opts, Blockade.start)
+
+
+def cmd_kill(opts):
+    """Kill some or all containers
+    """
+    signal = opts.signal if hasattr(opts, 'signal') else "SIGKILL"
+    __with_containers(opts, Blockade.kill, signal=signal)
+
+
+def cmd_stop(opts):
+    """Stop some or all containers
+    """
+    __with_containers(opts, Blockade.stop)
+
+
+def cmd_restart(opts):
+    """Restart some or all containers
+    """
+    __with_containers(opts, Blockade.restart)
+
+
+def cmd_flaky(opts):
+    """Make the network flaky for some or all containers
+    """
+    __with_containers(opts, Blockade.flaky)
+
+
+def cmd_slow(opts):
+    """Make the network slow for some or all containers
+    """
+    __with_containers(opts, Blockade.slow)
+
+
+def cmd_fast(opts):
+    """Restore network speed and reliability for some or all containers
+    """
+    __with_containers(opts, Blockade.fast)
+
+
+def cmd_duplicate(opts):
+    """Introduce packet duplication into the network of some or all containers
+    """
+    __with_containers(opts, Blockade.duplicate)
+
+
+def cmd_partition(opts):
+    """Partition the network between containers
+
+    Replaces any existing partitions outright. Any containers NOT specified
+    in arguments will be globbed into a single implicit partition. For
+    example if you have three containers: c1, c2, and c3 and you run:
+
+        blockade partition c1
+
+    The result will be a partition with just c1 and another partition with
+    c2 and c3.
+
+    Alternatively, --random may be specified, and zero or more random
+    partitions will be generated by blockade.
+    """
+    config = load_config(opts.config)
+    b = get_blockade(config, opts)
+
+    if opts.random:
+        if opts.partitions:
+            raise BlockadeError("Either specify individual partitions "
+                                "or --random, but not both")
+        b.random_partition()
+
+    else:
+        partitions = []
+        for partition in opts.partitions:
+            names = []
+            for name in partition.split(","):
+                name = name.strip()
+                if name:
+                    names.append(name)
+            partitions.append(names)
+        if not partitions:
+            raise BlockadeError("Either specify individual partitions "
+                                "or random")
+        b.partition(partitions)
+
+
+def cmd_join(opts):
+    """Restore full networking between containers
+    """
+    config = load_config(opts.config)
+    b = get_blockade(config, opts)
+    b.join()
+
+
+def cmd_logs(opts):
+    """Fetch the logs of a container
+    """
+    config = load_config(opts.config)
+    b = get_blockade(config, opts)
+    puts(b.logs(opts.container).decode(encoding='UTF-8'))
+
+
+def cmd_daemon(opts):
+    """Start the Blockade REST API
+    """
+    if opts.data_dir is None:
+        raise BlockadeError("You must supply a data directory for the daemon")
+    rest.start(data_dir=opts.data_dir, port=opts.port, debug=opts.debug)
+
+
+def cmd_add(opts):
+    """Add one or more existing Docker containers to a Blockade group
+    """
+    config = load_config(opts.config)
+    b = get_blockade(config, opts)
+    b.add_container(opts.containers)
+
+
+def cmd_version(opts):
+    """Show the Blockade version information
+    """
+    import blockade.version
+    puts("Blockade " + blockade.version.__version__)
+
+
+_CMDS = (("up", cmd_up),
+         ("destroy", cmd_destroy),
+         ("status", cmd_status),
+         ("start", cmd_start),
+         ("restart", cmd_restart),
+         ("stop", cmd_stop),
+         ("kill", cmd_kill),
+         ("logs", cmd_logs),
+         ("flaky", cmd_flaky),
+         ("slow", cmd_slow),
+         ("duplicate", cmd_duplicate),
+         ("fast", cmd_fast),
+         ("partition", cmd_partition),
+         ("join", cmd_join),
+         ("daemon", cmd_daemon),
+         ("add", cmd_add),
+         ("version", cmd_version))
+
+
+def setup_parser():
+    parser = argparse.ArgumentParser(description='Blockade')
+    parser.add_argument("--config", "-c", metavar="blockade.yaml",
+                        help="Config YAML. Looks in CWD if not specified.")
+    parser.add_argument("--data-dir", "-d", metavar="DIR", action="store",
+                        help="Base directory for state data. CWD if not specified.")
+    parser.add_argument("-n", "--name", metavar="NAME",
+                        help="Unique name for blockade. "
+                        "Default: basename of working directory")
+    parser.add_argument("--verbose", "-v", action="store_true",
+                        help="Print verbose output")
+
+    subparsers = parser.add_subparsers(title="commands")
+
+    command_parsers = {}
+    for command, func in _CMDS:
+        subparser = subparsers.add_parser(
+            command,
+            description=func.__doc__,
+            formatter_class=argparse.RawDescriptionHelpFormatter)
+        subparser.set_defaults(func=func)
+        command_parsers[command] = subparser
+
+    # add additional parameters to some commands
+    up_parser = command_parsers["up"]
+    _add_output_options(up_parser)
+    up_parser.add_argument("-f", "--force",
+                           action="store_true",
+                           help="Try to remove any conflicting containers if necessary")
+
+    _add_output_options(command_parsers["status"])
+
+    _add_container_selection_options(command_parsers["start"])
+    _add_container_selection_options(command_parsers["kill"])
+    _add_container_selection_options(command_parsers["stop"])
+    _add_container_selection_options(command_parsers["restart"])
+    _add_container_selection_options(command_parsers["flaky"])
+    _add_container_selection_options(command_parsers["slow"])
+    _add_container_selection_options(command_parsers["fast"])
+    _add_container_selection_options(command_parsers["duplicate"])
+
+    command_parsers["logs"].add_argument("container", metavar='CONTAINER',
+                                         help="Container to fetch logs for")
+
+    command_parsers["partition"].add_argument('partitions', nargs='*', metavar='PARTITION',
+        help='Comma-separated partition')
+    command_parsers["partition"].add_argument("-z", "--random", action='store_true',
+        help='Randomly select zero or more partitions')
+
+    command_parsers["kill"].add_argument("-s", "--signal", action="store", default="SIGKILL",
+        help="Specify the signal to be sent (str or int). Defaults to SIGKILL.")
+
+    command_parsers["daemon"].add_argument("--debug", action='store_true',
+        help="Enable debug for the REST API")
+    command_parsers["daemon"].add_argument("-p", "--port", action='store',
+        type=int, default=5000, help="REST API port. Default is 5000.")
+
+    command_parsers["add"].add_argument("containers", nargs="*", metavar='CONTAINER',
+        help="Docker container to add to the Blockade group")
+
+    return parser
+
+
+def main(args=None):
+    if sys.version_info >= (3, 2) and sys.version_info < (3, 3):
+        puts_err(colored.red("\nFor Python 3, Flask requires Python >= 3.3\n"))
+        sys.exit(1)
+
+    parser = setup_parser()
+    opts = parser.parse_args(args=args)
+
+    rc = 0
+
+    try:
+
+        # don't bother pinging docker for a version command
+        if opts.func != cmd_version:
+            check_docker()
+
+        opts.func(opts)
+    except InsufficientPermissionsError as e:
+        puts_err(colored.red("\nInsufficient permissions error:\n") + str(e) + "\n")
+        rc = 1
+    except BlockadeError as e:
+        puts_err(colored.red("\nError:\n") + str(e) + "\n")
+        rc = 1
+
+    except KeyboardInterrupt:
+        puts_err(colored.red("Caught Ctrl-C. exiting!"))
+
+    except:
+        puts_err(
+            colored.red("\nUnexpected error! This may be a Blockade bug.\n"))
+        traceback.print_exc()
+        rc = 2
+
+    sys.exit(rc)
+
+
+if __name__ == '__main__':
+    main()
---
+++ packages/blockade/blockade-0.3.1.tar.gz/blockade-0.3.1/blockade/net.py
@@ -0,0 +1,365 @@
+#
+#  Copyright (C) 2014 Dell, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+import docker
+
+import collections
+import itertools
+import re
+
+from .errors import BlockadeError
+from .utils import docker_run
+
+
+BLOCKADE_CHAIN_PREFIX = "blockade-"
+# iptables chain names are a max of 29 characters so we truncate the prefix
+MAX_CHAIN_PREFIX_LENGTH = 25
+IPTABLES_DOCKER_IMAGE = "vimagick/iptables:latest"
+
+_ERROR_NET_IFACE = "Failed to find container network interface"
+
+
+class NetworkState(object):
+    NORMAL = "NORMAL"
+    SLOW = "SLOW"
+    FLAKY = "FLAKY"
+    DUPLICATE = "DUPLICATE"
+    UNKNOWN = "UNKNOWN"
+
+
+class BlockadeNetwork(object):
+    def __init__(self, config):
+        self.config = config
+
+    def network_state(self, device):
+        return network_state(device)
+
+    def flaky(self, device):
+        flaky_config = self.config.network['flaky'].split()
+        traffic_control_netem(device, ["loss"] + flaky_config)
+
+    def slow(self, device):
+        slow_config = self.config.network['slow'].split()
+        traffic_control_netem(device, ["delay"] + slow_config)
+
+    def duplicate(self, device):
+        duplicate_config = self.config.network['duplicate'].split()
+        traffic_control_netem(device, ["duplicate"] + duplicate_config)
+
+    def fast(self, device):
+        traffic_control_restore(device)
+
+    def restore(self, blockade_id):
+        clear_iptables(blockade_id)
+
+    def partition_containers(self, blockade_id, partitions):
+        clear_iptables(blockade_id)
+        partition_containers(blockade_id, partitions)
+
+    def get_ip_partitions(self, blockade_id):
+        return iptables_get_source_chains(blockade_id)
+
+    def get_container_device(self, docker_client, container_id):
+        container_idx = get_container_device_index(docker_client, container_id)
+
+        # all my experiments showed the host device index was
+        # one greater than its associated container device index
+        host_idx = container_idx + 1
+
+        cmd = 'ip link'
+        try:
+            host_res = docker_run(cmd, image=IPTABLES_DOCKER_IMAGE,
+                                  docker_client=docker_client)
+        except Exception as e:
+            raise BlockadeError(
+                "%s:\nerror listing host network interfaces with: "
+                "'docker run --network=host %s %s':\n%s" %
+                (_ERROR_NET_IFACE, IPTABLES_DOCKER_IMAGE, cmd, str(e)))
+
+        host_rgx = '^%d: ([^:@]+)[:@]' % host_idx
+        host_match = re.search(host_rgx, host_res, re.M)
+        if host_match:
+            return host_match.group(1)
+
+        raise BlockadeError(
+            "%s:\ncould not find expected host link %s for container %s."
+            "'ip link' output:\n %s\n\nThis may be a Blockade bug, or "
+            "there may be something unusual about your container network."
+            % (_ERROR_NET_IFACE, host_idx, container_id, host_res))
+
+
+def get_container_device_index(docker_client, container_id):
+    cmd_args = ['cat', '/sys/class/net/eth0/ifindex']
+    res = None
+    try:
+        exec_handle = docker_client.exec_create(container_id, cmd_args)
+        res = docker_client.exec_start(exec_handle).decode('utf-8').strip()
+    except Exception as e:
+        if (isinstance(e, docker.errors.APIError) or
+                isinstance(e, docker.errors.DockerException)):
+            error_type = "Docker"
+        else:
+            error_type = "Unknown"
+        raise BlockadeError(
+            "%s:\n%s error attempting to exec '%s' in container %s:\n%s"
+            % (_ERROR_NET_IFACE, error_type, ' '.join(cmd_args),
+                container_id, str(e)))
+
+    try:
+        return int(res)
+    except (ValueError, TypeError):
+        raise BlockadeError(
+            "%s:\nexec '%s' in container %s returned:\n%s\n\n"
+            "Ensure the container is alive and supports this exec command."
+            % (_ERROR_NET_IFACE, ' '.join(cmd_args),
+                container_id, res))
+
+
+def parse_partition_index(blockade_id, chain):
+    prefix = "%s-p" % partition_chain_prefix(blockade_id)
+    if chain and chain.startswith(prefix):
+        try:
+            return int(chain[len(prefix):])
+        except ValueError:
+            pass
+    raise ValueError("chain %s is not a blockade partition" % (chain,))
+
+
+def partition_chain_name(blockade_id, partition_index):
+    return "%s-p%s" % (partition_chain_prefix(blockade_id), partition_index)
+
+
+def partition_chain_prefix(blockade_id):
+    prefix = "%s%s" % (BLOCKADE_CHAIN_PREFIX, blockade_id)
+    return prefix[:MAX_CHAIN_PREFIX_LENGTH]
+
+
+def iptables_call_output(*args):
+    cmd = ["iptables", "-n"] + list(args)
+    output = docker_run(' '.join(cmd), image=IPTABLES_DOCKER_IMAGE)
+    return output.split('\n')
+
+
+def iptables_call(*args):
+    cmd = ["iptables"] + list(args)
+    return docker_run(' '.join(cmd), image=IPTABLES_DOCKER_IMAGE)
+
+
+def iptables_get_chain_rules(chain):
+    if not chain:
+        raise ValueError("invalid chain")
+    lines = iptables_call_output("-L", chain)
+    if len(lines) < 2:
+        raise BlockadeError("Can't understand iptables output: \n%s" %
+                            "\n".join(lines))
+
+    chain_line, header_line = lines[:2]
+    if not (chain_line.startswith("Chain " + chain) and
+            header_line.startswith("target")):
+        raise BlockadeError("Can't understand iptables output: \n%s" %
+                            "\n".join(lines))
+    return lines[2:]
+
+
+def iptables_get_source_chains(blockade_id):
+    """Get a map of blockade chains IDs -> list of IPs targeted at them
+
+    For figuring out which container is in which partition
+    """
+    result = {}
+    if not blockade_id:
+        raise ValueError("invalid blockade_id")
+    lines = iptables_get_chain_rules("FORWARD")
+
+    for line in lines:
+        parts = line.split()
+        if len(parts) < 4:
+            continue
+        try:
+            partition_index = parse_partition_index(blockade_id, parts[0])
+        except ValueError:
+            continue  # not a rule targetting a blockade chain
+
+        source = parts[3]
+        if source:
+            result[source] = partition_index
+    return result
+
+
+def iptables_delete_rules(chain, predicate):
+    if not chain:
+        raise ValueError("invalid chain")
+    if not isinstance(predicate, collections.Callable):
+        raise ValueError("invalid predicate")
+
+    lines = iptables_get_chain_rules(chain)
+
+    # TODO this is susceptible to check-then-act races.
+    # better to ultimately switch to python-iptables if it becomes less buggy
+    for index, line in reversed(list(enumerate(lines, 1))):
+        line = line.strip()
+        if line and predicate(line):
+            iptables_call("-D", chain, str(index))
+
+
+def iptables_delete_blockade_rules(blockade_id):
+    def predicate(rule):
+        target = rule.split()[0]
+        try:
+            parse_partition_index(blockade_id, target)
+        except ValueError:
+            return False
+        return True
+    iptables_delete_rules("FORWARD", predicate)
+
+
+def iptables_delete_blockade_chains(blockade_id):
+    if not blockade_id:
+        raise ValueError("invalid blockade_id")
+
+    lines = iptables_call_output("-L")
+    for line in lines:
+        parts = line.split()
+        if len(parts) >= 2 and parts[0] == "Chain":
+            chain = parts[1]
+            try:
+                parse_partition_index(blockade_id, chain)
+            except ValueError:
+                continue
+            # if we are a valid blockade chain, flush and delete
+            iptables_call("-F", chain)
+            iptables_call("-X", chain)
+
+
+def iptables_insert_rule(chain, src=None, dest=None, target=None):
+    """Insert a new rule in the chain
+    """
+    if not chain:
+        raise ValueError("Invalid chain")
+    if not target:
+        raise ValueError("Invalid target")
+    if not (src or dest):
+        raise ValueError("Need src, dest, or both")
+
+    args = ["-I", chain]
+    if src:
+        args += ["-s", src]
+    if dest:
+        args += ["-d", dest]
+    args += ["-j", target]
+    iptables_call(*args)
+
+
+def iptables_create_chain(chain):
+    """Create a new chain
+    """
+    if not chain:
+        raise ValueError("Invalid chain")
+    iptables_call("-N", chain)
+
+
+def clear_iptables(blockade_id):
+    """Remove all iptables rules and chains related to this blockade
+    """
+    # first remove refererences to our custom chains
+    iptables_delete_blockade_rules(blockade_id)
+
+    # then remove the chains themselves
+    iptables_delete_blockade_chains(blockade_id)
+
+
+def _get_chain_groups(partitions):
+    chains = []
+
+    def find_partition(name):
+        for idx, parts in enumerate(chains):
+            in_partition = any(c.name == name for c in parts)
+            if in_partition:
+                return idx
+        return None
+
+    for partition in partitions:
+        new_part = []
+        for part in partition:
+            in_partition = find_partition(part.name)
+            if not in_partition is None:
+                chains[in_partition].remove(part)
+                chains.append(set([part]))
+            else:
+                new_part.append(part)
+        if new_part:
+            chains.append(set(new_part))
+
+    # prune empty partitions
+    return [x for x in chains if len(x) > 0]
+
+
+def partition_containers(blockade_id, partitions):
+    if not partitions or len(partitions) == 1:
+        return
+
+    # partitions without IP addresses can't be part of any
+    # iptables rule anyway
+    ip_partitions = [[c for c in parts if c.ip_address] for parts in partitions]
+
+    all_nodes = frozenset(itertools.chain(*ip_partitions))
+
+    for idx, chain_group in enumerate(_get_chain_groups(ip_partitions)):
+        # create a new chain
+        chain_name = partition_chain_name(blockade_id, idx+1)
+        iptables_create_chain(chain_name)
+
+        # direct all traffic of the chain group members to this chain
+        for container in chain_group:
+            iptables_insert_rule("FORWARD", src=container.ip_address, target=chain_name)
+
+        def in_group(container):
+            return any(container.name == x.name for x in chain_group)
+
+        # block all transfer of partitions the containers of this chain group  are NOT part of
+        chain_partition_members = set(itertools.chain(*[parts for parts in ip_partitions
+                                                        if any(in_group(c) for c in parts)]))
+        to_block = all_nodes - chain_partition_members
+
+        for container in to_block:
+            iptables_insert_rule(chain_name, dest=container.ip_address, target="DROP")
+
+
+def traffic_control_restore(device):
+    cmd = ["tc", "qdisc", "del", "dev", device, "root"]
+    docker_run(' '.join(cmd), image=IPTABLES_DOCKER_IMAGE)
+
+
+def traffic_control_netem(device, params):
+    cmd = ["tc", "qdisc", "replace", "dev", device,
+           "root", "netem"] + params
+    docker_run(' '.join(cmd), image=IPTABLES_DOCKER_IMAGE)
+
+
+def network_state(device):
+    cmd = ["tc", "qdisc", "show", "dev", device]
+    try:
+        output = docker_run(' '.join(cmd), image=IPTABLES_DOCKER_IMAGE)
+        # sloppy but good enough for now
+        if " delay " in output:
+            return NetworkState.SLOW
+        if " loss " in output:
+            return NetworkState.FLAKY
+        if " duplicate " in output:
+            return NetworkState.DUPLICATE
+        return NetworkState.NORMAL
+    except Exception:
+        return NetworkState.UNKNOWN

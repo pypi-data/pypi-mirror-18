ab50cee328e1cc9c898e7db1c8d23c56ea03c6ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:46:26 +0000

    Add package alignak_module_backend/0.2.15/alignak_module_backend-0.2.15.tar.gz

---
+++ packages/alignak_module_backend/alignak_module_backend-0.2.15.tar.gz/alignak_module_backend-0.2.15/version.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+    Alignak - Arbiter, Scheduler and Broker modules for the Alignak backend
+"""
+# Package name
+__pkg_name__ = u"alignak_module_backend"
+
+# Module type for PyPI keywords
+# Used for:
+# - PyPI keywords
+__module_type__ = u"backend"
+
+# Application manifest
+__version__ = u"0.2.15"
+__author__ = u"Alignak team"
+__author_email__ = u"d.durieux@siprossii.com"
+__copyright__ = u"(c) 2015-2016 - %s" % __author__
+__license__ = u"GNU Affero General Public License, version 3"
+__url__ = u"https://github.com/Alignak-monitoring-contrib/alignak-module-backend"
+__description__ = u"Alignak - Backend modules for Arbiter, Scheduler and Broker"

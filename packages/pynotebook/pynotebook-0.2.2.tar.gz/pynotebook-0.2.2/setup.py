9161f9e3986247daccaafbd67772e9f83dbff1c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:46:38 +0000

    Add package pynotebook/0.2.2/pynotebook-0.2.2.tar.gz

---
+++ packages/pynotebook/pynotebook-0.2.2.tar.gz/pynotebook-0.2.2/setup.py
@@ -0,0 +1,24 @@
+# -*- coding: latin-1 -*-
+
+
+from setuptools import setup
+
+# NOTE: if there is a problem with symlinks, update setuptools:
+# http://stackoverflow.com/questions/27459888/how-to-make-setuptools-follow-symlinks
+
+with open('README') as file:
+    long_description = file.read()
+
+setup(name='pynotebook',
+      version='0.2.2',
+      description = 'A notebook interface for wxpython',
+      long_description = long_description,
+      author='C. Ecker',
+      author_email='textmodelview@gmail.com',
+      url='https://pypi.python.org/pypi/pynotebook/',
+      license='BSD',
+      packages=['pynotebook', 'pynotebook/textmodel', 'pynotebook/wxtextview'],
+      scripts=['bin/pynb'],
+      platforms = ['any'],
+     )
+

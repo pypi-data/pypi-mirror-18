68cad7cb94d55378820276cf6e26121dcf2cc0c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:20:19 +0000

    Add package mhctools/0.4.1/mhctools-0.4.1.tar.gz

---
+++ packages/mhctools/mhctools-0.4.1.tar.gz/mhctools-0.4.1/mhctools/__init__.py
@@ -0,0 +1,41 @@
+from .alleles import normalize_allele_name
+from .binding_prediction import BindingPrediction
+from .epitope_collection import EpitopeCollection
+from .iedb import (
+    IedbNetMHCcons,
+    IedbNetMHCpan,
+    IedbSMM,
+    IedbSMM_PMBEC,
+    IedbNetMHCIIpan,
+)
+from .netmhc import NetMHC
+from .netmhc3 import NetMHC3
+from .netmhc4 import NetMHC4
+from .netmhc_cons import NetMHCcons
+from .netmhc_pan import NetMHCpan
+from .netmhc_pan28 import NetMHCpan28
+from .netmhc_pan3 import NetMHCpan3
+from .netmhcii_pan import NetMHCIIpan
+from .random_predictor import RandomBindingPredictor
+
+__version__ = "0.4.1"
+
+__all__ = [
+    "normalize_allele_name",
+    "BindingPrediction",
+    "EpitopeCollection",
+    "IedbNetMHCcons",
+    "IedbNetMHCpan",
+    "IedbSMM",
+    "IedbSMM_PMBEC",
+    "IedbNetMHCIIpan",
+    "NetMHC",
+    "NetMHC3",
+    "NetMHC4",
+    "NetMHCcons",
+    "NetMHCpan",
+    "NetMHCpan28",
+    "NetMHCpan3",
+    "NetMHCIIpan",
+    "RandomBindingPredictor",
+]

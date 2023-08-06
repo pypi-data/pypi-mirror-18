efa4b363c229717dc8e903cee7745c6dcbaefff4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:12:23 +0000

    Add package padsniff/0.9.3/padsniff-0.9.3.tar.gz

---
+++ packages/padsniff/padsniff-0.9.3.tar.gz/padsniff-0.9.3/padsniff/cli.py
@@ -0,0 +1,41 @@
+from importlib.util import module_from_spec, spec_from_file_location
+import logging
+
+import click
+
+from .proxy import Proxy
+
+
+log = logging.getLogger(__name__)
+
+
+def configure_logging(level):
+    logging.basicConfig(format='[%(levelname)s] :: %(message)s', level=level)
+
+
+def load_script(path):
+    spec = spec_from_file_location('script', path)
+    module = module_from_spec(spec)
+    spec.loader.exec_module(module)
+    log.info('Loaded script %s.', path)
+
+
+@click.group()
+def cli():
+    pass
+
+
+@cli.command()
+@click.option('--debug', '-d', default=False, is_flag=True)
+@click.option('--port', '-p', default=8080)
+@click.option('--script', '-s', multiple=True, type=click.Path(exists=True))
+def run(debug, port, script):
+
+    configure_logging(logging.INFO if debug else logging.WARNING)
+
+    # load scripts before instantiating the proxy to allow the use of the `on` decorator
+    for path in script:
+        load_script(path)
+
+    proxy = Proxy(host='0.0.0.0', port=port)
+    proxy.run()
---
+++ packages/padsniff/padsniff-0.9.3.tar.gz/padsniff-0.9.3/setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup
+
+with open('requirements.txt') as f:
+    requires = f.readlines()
+
+setup(
+    name='padsniff',
+    version='0.9.3',
+    description='Command-line tool to sniff Puzzle & Dragons data.',
+    url='https://bitbucket.org/necromanteion/padsniff',
+    author='Will Medlar',
+    author_email='wmmedlar@gmail.com',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3 :: Only',
+        'Topic :: Games/Entertainment :: Puzzle Games',
+    ],
+    keywords=[
+        'puzzle & dragons',
+        'man-in-the-middle',
+        'reverse engineering',
+    ],
+    install_requires=requires,
+    packages=['padsniff'],
+    entry_points={
+        'console_scripts': [
+            'padsniff = padsniff.cli:cli'
+        ]
+    }
+)

51130cdd0880ae8576b7e194596a810f43df97d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:33:07 +0000

    Add package liu111_nester/1.0.0/liu111_nester-1.0.0.tar.gz

---
+++ packages/liu111_nester/liu111_nester-1.0.0.tar.gz/liu111_nester-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+	name = 'liu111_nester',
+	version = '1.0.0',
+	py_modules = ['nester'],
+	author = 'liutest',
+	author_email = 'liutest@local.com',
+	url = 'http://liutest.local.com',
+	description = 'A Test of python module of mine',
+)

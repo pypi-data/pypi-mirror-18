22a9aedb7fe1c4501a3fed85a5623128dd9f88cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:09:34 +0000

    Add package merk-api/0.0.6/merk-api-0.0.6.tar.gz

---
+++ packages/merk-api/merk-api-0.0.6.tar.gz/merk-api-0.0.6/merkapi/__init__.py
@@ -0,0 +1,91 @@
+import sys
+import certifi
+import logging
+import urllib3
+
+from . import utils
+
+log = logging.getLogger(__name__)
+
+COUNTRY_CZ = 'cz'
+COUNTRY_SK = 'sk'
+COUNTRY_ALLOWED = (COUNTRY_CZ, COUNTRY_SK)
+
+PY2 = bool(sys.version_info.major < 3)
+
+
+class Api(object):
+    token = None
+    _enums = {}
+
+    def __init__(self, token, content_type='application/msgpack'):
+
+        headers = urllib3.make_headers(keep_alive=True, accept_encoding=True, user_agent='merk-api-py')
+        headers.update({
+            'authorization': 'Token %s' % token,
+            'accept': content_type,
+            'content-type': content_type
+        })
+        self.loads = getattr(utils, 'loads_%s' % content_type.split('/')[1])
+        self.dumps = getattr(utils, 'dumps_%s' % content_type.split('/')[1])
+        self.http = urllib3.HTTPSConnectionPool(
+            "api.merk.cz", cert_reqs='CERT_REQUIRED', ca_certs=certifi.where(), headers=headers,
+            timeout=urllib3.Timeout(total=5), retries=urllib3.Retry(total=3))
+
+    @property
+    def subscriptions(self):
+        """
+        :return: Your subscription information for each country set (cz, sk)
+        """
+
+        r = self.http.request('GET', '/subscriptions/')
+        r.encdata = self.loads(r.data) if r.data else None
+        return r
+
+    def suggest(self, query, by='regno', country_code=COUNTRY_CZ):
+        """
+        Suggest company info by regno, by email or by name
+
+        :param query:
+        :param by: one of: 'regno', 'email', 'name'
+        :param country_code: one of 'cz', 'sk'
+        :return:
+        """
+
+        if PY2 and not isinstance(query, int):
+            query = query.encode('utf-8')
+        r = self.http.request('GET', '/suggest/',
+                              fields={by: query, 'country_code': country_code})
+
+        r.encdata = self.loads(r.data) if r.data else None
+        return r
+
+    def company(self, regno, country_code=COUNTRY_CZ):
+        """
+        :param regno: Valid regno
+        :param country_code: one of 'cz', 'sk'
+        :return: Company info
+        """
+
+        r = self.http.request('GET', '/company/',
+                              fields={'regno': regno, 'country_code': country_code})
+
+        r.encdata = self.loads(r.data) if r.data else None
+        return r
+
+    def companies(self, regnos, country_code=COUNTRY_CZ):
+
+        r = self.http.request('POST', '/company/mget/',
+                              body=self.dumps({'regnos': regnos, 'country_code': country_code}))
+
+        r.encdata = self.loads(r.data) if r.data else None
+        return r
+
+    def get_enums(self, country_code=COUNTRY_CZ):
+
+        if self._enums.get(country_code, None):
+            return self._enums[country_code]
+        else:
+            r = self.http.request('GET', '/enums/', fields={country_code: country_code})
+            self._enums[country_code] = self.loads(r.data)
+            return self._enums[country_code]
---
+++ packages/merk-api/merk-api-0.0.6.tar.gz/merk-api-0.0.6/setup.py
@@ -0,0 +1,66 @@
+import sys
+from os.path import join, dirname
+from setuptools import setup
+from setuptools.command.test import test
+
+VERSION = '0.0.6'
+
+
+def parse_reqs(f='requirements.pip'):
+    ret = []
+    with open(join(dirname(__file__), f)) as fp:
+        for l in fp.readlines():
+            l = l.strip()
+            if l and not l.startswith('#'):
+                ret.append(l)
+    return ret
+
+setup_requires = ['setuptools']
+install_requires, tests_require = parse_reqs(), parse_reqs('requirements-test.pip')
+
+with open('README.md') as readmefile:
+    long_description = readmefile.read()
+
+
+class PyTest(test):
+    def finalize_options(self):
+        test.finalize_options(self)
+        self.test_args = ['tests_merkapi']
+        self.test_suite = True
+
+    def run_tests(self):
+        # import here, cause outside the eggs aren't loaded
+        import pytest
+        errno = pytest.main(self.test_args)
+        sys.exit(errno)
+
+
+setup(
+    name='merk-api',
+    version=VERSION,
+    description='Merk API Python client',
+    long_description=long_description,
+    author='Vitek Pliska',
+    author_email='vitek@creatiweb.cz',
+    license='BSD',
+    url='https://api.merk.cz/docs/',
+
+    packages=('merkapi', 'tests_merkapi'),
+    include_package_data=True,
+
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 3",
+        "Topic :: Utilities",
+    ],
+
+    setup_requires=setup_requires,
+    install_requires=install_requires,
+    tests_require=tests_require,
+
+    cmdclass={'test': PyTest},
+)

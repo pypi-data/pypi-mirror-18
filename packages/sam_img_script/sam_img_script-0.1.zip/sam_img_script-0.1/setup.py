3f758789fe8f1daf9c20d292c2a1363ea82d2ee6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:09:21 +0000

    Add package sam_img_script/0.1/sam_img_script-0.1.zip

---
+++ packages/sam_img_script/sam_img_script-0.1.zip/sam_img_script-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+setup(
+    name='sam_img_script',    # This is the name of your PyPI-package.
+    version='0.1',                          # Update the version number for new releases
+    scripts=['sam_img_script.py'],
+    author = 'Sambridhi',                  # The name of your scipt, and also the command you'll be using for calling it
+    author_email = 'sambridhi.shakya@gmail.com'
+)

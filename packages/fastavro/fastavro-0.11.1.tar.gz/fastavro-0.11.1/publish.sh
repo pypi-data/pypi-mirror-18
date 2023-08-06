c9f9c10053f17926edf52350bd6e9af30cb3ac68
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:45:48 +0000

    Add package fastavro/0.11.1/fastavro-0.11.1.tar.gz

---
+++ packages/fastavro/fastavro-0.11.1.tar.gz/fastavro-0.11.1/publish.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# Push to pypi, tag and push to bitbucket
+
+pyver=$(python -c 'import sys; print("%s%s" % sys.version_info[:2])')
+ver=$(python setup.py --version)
+pkg=fastavro-${ver}-py${pyver}_0.tar.bz2
+
+
+# Fail on 1'st error
+set -e
+set -x
+
+make
+#python3 setup.py bdist_egg upload
+python2 setup.py bdist_egg upload
+/opt/anaconda/bin/conda build .
+/opt/anaconda/bin/anaconda upload /opt/anaconda/conda-bld/linux-64/${pkg}
+rm -fr build dist fastavro.egg-info/
+git tag -f $(python setup.py --version)
+git push
+git push --tags

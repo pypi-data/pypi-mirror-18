a1861320f1d3b92d63630fed7373a9f01a1a9048
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:12:00 +0000

    Add package pook/0.1.2/pook-0.1.2.tar.gz

---
+++ packages/pook/pook-0.1.2.tar.gz/pook-0.1.2/pook/matchers/body.py
@@ -0,0 +1,27 @@
+from ..types import isregex
+from .base import BaseMatcher
+
+
+class BodyMatcher(BaseMatcher):
+    """
+    BodyMatchers matches the request body via strict value comparison or
+    regular expression based matching.
+    """
+
+    @BaseMatcher.matcher
+    def match(self, req):
+        body = req.body
+
+        if body == self.expectation:
+            return True
+
+        if isregex(self.expectation):
+            return self.expectation.match(body or '') is not None
+
+        if not body and self.expectation:
+            return False
+
+        if not isinstance(body, str):
+            return False
+
+        return self.compare(self.expectation, body)

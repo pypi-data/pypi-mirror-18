0bffb6b2b611d8745da3b708d7e4437d1e1a4d75
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:21:03 +0000

    Add package QuickDictionary/0.9/QuickDictionary-0.9.tar.gz

---
+++ packages/QuickDictionary/QuickDictionary-0.9.tar.gz/QuickDictionary-0.9/setup.py
@@ -0,0 +1,16 @@
+from distutils.core import setup
+setup(
+		name = 'QuickDictionary',
+		packages = ['QuickDictionary'],
+		version = '0.9',
+		description = 'just copy the word for to know its meaning,must be connected to internet',
+		author = 'Hirendra Thakur',
+		author_email = 'hirendrathakur1993@gmail.com',
+		url = 'https://github.com/hirendrathakur/quickDictionary',
+		download_url = 'https://github.com/hirendrathakur/quickDictionary/tarball/0.9',
+		keywords = ['dictionary','quick','search','copy'],
+		license = 'MIT',
+		scripts = ['bin/quickdictionary'],
+		install_requires = ['pyperclip','bs4'],
+		classifiers = [],
+		)

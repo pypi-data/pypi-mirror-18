f596933b1a1326d5ae4a78cf24c4cc9c4e4b61cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:07:14 +0000

    Add package botstory/0.0.35/botstory-0.0.35-py3-none-any.whl

---
+++ packages/botstory/botstory-0.0.35-py3-none-any.whl/botstory/integrations/mockdb/db.py
@@ -0,0 +1,30 @@
+import aiohttp
+from ... import utils
+
+
+class MockDB:
+    type = 'interface.session_storage'
+
+    def __init__(self):
+        self.session = None
+        self.user = None
+        self.setup = aiohttp.test_utils.make_mocked_coro()
+
+    async def get_session(self, **kwargs):
+        return self.session
+
+    async def set_session(self, session):
+        self.session = session
+
+    async def new_session(self, **kwargs):
+        return kwargs
+
+    async def get_user(self, **kwargs):
+        return self.user
+
+    async def set_user(self, user):
+        self.user = user
+
+    async def new_user(self, **kwargs):
+        self.user = utils.JSDict({**kwargs})
+        return self.user

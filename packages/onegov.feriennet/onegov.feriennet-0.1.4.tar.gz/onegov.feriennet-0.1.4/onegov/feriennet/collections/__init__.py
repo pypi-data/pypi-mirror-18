c0b912f8961c515481da9bbd431445bf659f4d79
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:19:37 +0000

    Add package onegov.feriennet/0.1.4/onegov.feriennet-0.1.4.tar.gz

---
+++ packages/onegov.feriennet/onegov.feriennet-0.1.4.tar.gz/onegov.feriennet-0.1.4/onegov/feriennet/collections/__init__.py
@@ -0,0 +1,13 @@
+from onegov.feriennet.collections.activity import VacationActivityCollection
+from onegov.feriennet.collections.billing import BillingCollection
+from onegov.feriennet.collections.billing import BillingDetails
+from onegov.feriennet.collections.match import MatchCollection
+from onegov.feriennet.collections.occasion import OccasionAttendeeCollection
+
+__all__ = [
+    'BillingCollection',
+    'BillingDetails',
+    'MatchCollection',
+    'OccasionAttendeeCollection',
+    'VacationActivityCollection'
+]
---
+++ packages/onegov.feriennet/onegov.feriennet-0.1.4.tar.gz/onegov.feriennet-0.1.4/onegov/feriennet/forms/__init__.py
@@ -0,0 +1,17 @@
+from onegov.feriennet.forms.activity import VacationActivityForm
+from onegov.feriennet.forms.attendee import AttendeeForm
+from onegov.feriennet.forms.billing import BillingForm
+from onegov.feriennet.forms.occasion import OccasionForm
+from onegov.feriennet.forms.period import PeriodForm
+from onegov.feriennet.forms.userprofile import UserProfileForm
+from onegov.feriennet.forms.match import MatchForm
+
+__all__ = [
+    'AttendeeForm',
+    'BillingForm',
+    'MatchForm',
+    'OccasionForm',
+    'PeriodForm',
+    'UserProfileForm',
+    'VacationActivityForm'
+]

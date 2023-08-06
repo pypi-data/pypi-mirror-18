d8a0b6c3ddcd9dbb44c0baf3357b58db4a078450
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:46:09 +0000

    Add package pyOfx/0.1.3/pyOfx-0.1.3.tar.gz

---
+++ packages/pyOfx/pyOfx-0.1.3.tar.gz/pyOfx-0.1.3/setup.py
@@ -0,0 +1,35 @@
+import os
+from setuptools import setup
+
+README = os.path.join(os.path.dirname(__file__), 'README.md')
+try:
+    import pypandoc
+
+    LONG = pypandoc.convert(README, to="rst", format='md')
+except ImportError as e:
+    LONG = open(README).read()
+
+setup(
+        name="pyOfx",
+        version="0.1.3",
+        author="Steven Rossiter",
+        author_email="steve@flexsis.co.uk",
+        description=("A wrapper around OrcFxAPI by Orcina "
+                     "to add extra functionality."),
+        license="MIT",
+        keywords="orcaflex api wrapper subsea engineering",
+        url="https://pythonhosted.org/pyOfx/",
+        packages=['pyofx'],
+        long_description=LONG,
+        classifiers=[
+            "Development Status :: 4 - Beta",
+            "Environment :: Win32 (MS Windows)",
+            "Topic :: Utilities",
+            "Natural Language :: English",
+            "License :: OSI Approved :: MIT License",
+            "Operating System :: Microsoft :: Windows",
+            "Programming Language :: Python :: 2.7",
+            "Programming Language :: Python :: 3.4",
+            "Topic :: Scientific/Engineering :: Physics"
+        ],
+)

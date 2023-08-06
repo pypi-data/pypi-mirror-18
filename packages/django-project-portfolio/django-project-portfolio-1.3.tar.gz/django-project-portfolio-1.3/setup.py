ded7b620a90cf6680b692877fb50df6e6ec511f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:10:20 +0000

    Add package django-project-portfolio/1.3/django-project-portfolio-1.3.tar.gz

---
+++ packages/django-project-portfolio/django-project-portfolio-1.3.tar.gz/django-project-portfolio-1.3/setup.py
@@ -0,0 +1,36 @@
+import os
+
+from setuptools import setup
+
+
+setup(name='django-project-portfolio',
+      version='1.3',
+      zip_safe=False,  # eggs are the devil.
+      description='A Django application for listing your software projects.',
+      long_description=open(os.path.join(os.path.dirname(__file__),
+                                         'README.rst')).read(),
+      author='James Bennett',
+      author_email='james@b-list.org',
+      url='https://github.com/ubernostrum/django-projects/',
+      packages=['projects', 'projects.tests', 'projects.migrations'],
+      include_package_data=True,
+      test_suite='projects.runtests.run_tests',
+      classifiers=['Development Status :: 5 - Production/Stable',
+                   'Environment :: Web Environment',
+                   'Framework :: Django',
+                   'Framework :: Django :: 1.8',
+                   'Framework :: Django :: 1.9',
+                   'Framework :: Django :: 1.10',
+                   'Intended Audience :: Developers',
+                   'License :: OSI Approved :: BSD License',
+                   'Operating System :: OS Independent',
+                   'Programming Language :: Python',
+                   'Programming Language :: Python :: 2',
+                   'Programming Language :: Python :: 2.7',
+                   'Programming Language :: Python :: 3',
+                   'Programming Language :: Python :: 3.2',
+                   'Programming Language :: Python :: 3.3',
+                   'Programming Language :: Python :: 3.4',
+                   'Programming Language :: Python :: 3.5',
+                   'Topic :: Utilities'],
+      )

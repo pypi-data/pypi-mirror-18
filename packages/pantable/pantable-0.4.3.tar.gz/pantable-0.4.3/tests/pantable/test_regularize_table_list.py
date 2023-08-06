8a221964b51e0eceb04625a90edbd5c592eae986
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:09:31 +0000

    Add package pantable/0.4.3/pantable-0.4.3.tar.gz

---
+++ packages/pantable/pantable-0.4.3.tar.gz/pantable-0.4.3/tests/pantable/test_regularize_table_list.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+
+"""
+from .context import regularize_table_list
+
+
+def test_regularize_table_list():
+    raw_table_list = [['1'], ['2', '3', '4', '5', '6', '7']]
+    regularize_table_list(raw_table_list)
+    assert raw_table_list == [
+        ['1', '', '', '', '', ''],
+        ['2', '3', '4', '5', '6', '7']
+    ]
+    return

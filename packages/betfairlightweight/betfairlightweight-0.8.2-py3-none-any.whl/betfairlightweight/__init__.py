3ead087bd6157f88094f43f4d3ec3c47e709c8eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:08:49 +0000

    Add package betfairlightweight/0.8.2/betfairlightweight-0.8.2-py3-none-any.whl

---
+++ packages/betfairlightweight/betfairlightweight-0.8.2-py3-none-any.whl/betfairlightweight/__init__.py
@@ -0,0 +1,8 @@
+from .apiclient import APIClient
+from .exceptions import BetfairError
+from .streaming import StreamListener
+
+
+__title__ = 'betfairlightweight'
+__version__ = '0.8.2'
+__author__ = 'Liam Pauling'

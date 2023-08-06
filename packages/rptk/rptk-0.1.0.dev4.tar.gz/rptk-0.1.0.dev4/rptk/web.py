27ceb83c22a88cd64e50850f6f7772a1e7c43ed4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:20:44 +0000

    Add package rptk/0.1.0.dev4/rptk-0.1.0.dev4.tar.gz

---
+++ packages/rptk/rptk-0.1.0.dev4.tar.gz/rptk-0.1.0.dev4/rptk/web.py
@@ -0,0 +1,25 @@
+from flask_api import FlaskAPI
+from flask_api.renderers import BaseRenderer, BrowsableAPIRenderer
+from flask_api.decorators import set_renderers
+from rptk.api import Rptk
+
+app = FlaskAPI(__name__)
+
+
+class TextRenderer(BaseRenderer):
+    media_type = 'text/plain'
+
+    def render(self, data, media_type, **options):
+        return "%s\n" % data
+
+
+@app.route("/<string:fmt>/<string:obj>", methods=['GET'])
+@set_renderers(TextRenderer, BrowsableAPIRenderer)
+def get(fmt=None, obj=None):
+    rptk = Rptk(object=obj, format=fmt)
+    result = rptk.query()
+    return result
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=8080)

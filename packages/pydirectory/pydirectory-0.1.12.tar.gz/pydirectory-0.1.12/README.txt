ce87dd48803d7c4e34963813ff609086f160a3c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:19:56 +0000

    Add package pydirectory/0.1.12/pydirectory-0.1.12.tar.gz

---
+++ packages/pydirectory/pydirectory-0.1.12.tar.gz/pydirectory-0.1.12/README.txt
@@ -0,0 +1,1 @@
+Python frameWork to managing multiples LDAP service
---
+++ packages/pydirectory/pydirectory-0.1.12.tar.gz/pydirectory-0.1.12/setup.py
@@ -0,0 +1,35 @@
+from distutils.core import setup
+
+requires = []
+
+try:
+	import ldap3
+except:
+	requires.append('ldap3')
+
+setup(
+	name="pydirectory",
+	version="0.1.12",
+	description="Python frameWork to managing multiples LDAP services - (gamma)",
+	author="Jonas Delgado Mesa",
+	author_email="jdelgado@yohnah.net",
+	url="https://github.com/Wengex/PyDirectory",
+	license="GPLv2",
+	packages=[
+		'pydirectory',
+		'pydirectory.directory',
+		'pydirectory.directory.settings',
+		'pydirectory.directory.engine',
+		'pydirectory.directory.objects',
+		'pydirectory.ldap',
+		'pydirectory.ldap.settings',
+		'pydirectory.ldap.engine',
+		'pydirectory.ldap.objects',
+		'pydirectory.activedirectory',
+		'pydirectory.activedirectory.settings',
+		'pydirectory.activedirectory.engine',
+		'pydirectory.activedirectory.objects',
+	],
+	long_description=open('README.txt').read(),
+	install_requires = requires
+)

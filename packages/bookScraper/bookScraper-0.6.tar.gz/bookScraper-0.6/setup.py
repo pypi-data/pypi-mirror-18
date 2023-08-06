555494484f1d5ac4a3f570d581959c4a3272fe71
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:10:36 +0000

    Add package bookScraper/0.6/bookScraper-0.6.tar.gz

---
+++ packages/bookScraper/bookScraper-0.6.tar.gz/bookScraper-0.6/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='bookScraper',
+      version='0.6',
+      description='The funniest joke in the world',
+      packages=['bookScraper'],
+      install_requires=['scrapy'],
+      zip_safe=False)

9c7bd00191df1b6564e0f694b63acfa2fe9976d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:09:56 +0000

    Add package pros-cli/2.1.7/pros_cli-2.1.7-py3-none-any.whl

---
+++ packages/pros-cli/pros_cli-2.1.7-py3-none-any.whl/proscli/main.py
@@ -0,0 +1,27 @@
+import click
+#from pkg_resources import get_distribution
+import proscli
+from proscli.utils import default_options
+
+
+def main():
+    # the program name should always be pros. don't care if it's not...
+    try:
+        cli.main(prog_name='pros')
+    except KeyboardInterrupt:
+        click.echo('Aborted!')
+        pass
+
+
+@click.command('pros',
+               cls=click.CommandCollection,
+               context_settings=dict(help_option_names=['-h', '--help']),
+               sources=[proscli.terminal_cli, proscli.build_cli, proscli.flasher_cli, proscli.conductor_cli])
+@click.version_option(version='2.1.7', prog_name='pros')
+@default_options
+def cli():
+    pass
+
+
+if __name__ == '__main__':
+    main()

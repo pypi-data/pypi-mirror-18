a9e546b6882349b3b0e0f2bad7b789a50eb681a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:30:38 +0000

    Add package Ming_nester/1.2.0/Ming_nester-1.2.0.zip

---
+++ packages/Ming_nester/Ming_nester-1.2.0.zip/Ming_nester-1.2.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+        name            = 'Ming_nester',
+        version         = '1.2.0',
+        py_modules      = ['nester'],
+        author          = 'kljkl',
+        author_email    = 'asdkl@126.com',
+        url             = 'http://www.mkl.com',
+        description     = 'A simple printer of nester lists',
+      )

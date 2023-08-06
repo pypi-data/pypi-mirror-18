18303e24df7ed3d8126acc821446862a8e699d49
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:32:45 +0000

    Add package onegov.ballot/1.0.2/onegov.ballot-1.0.2.tar.gz

---
+++ packages/onegov.ballot/onegov.ballot-1.0.2.tar.gz/onegov.ballot-1.0.2/onegov/ballot/__init__.py
@@ -0,0 +1,30 @@
+from onegov.ballot.models import (
+    Ballot,
+    BallotResult,
+    Candidate,
+    CandidateResult,
+    Election,
+    ElectionResult,
+    List,
+    ListConnection,
+    ListResult,
+    Vote
+)
+from onegov.ballot.collection import ElectionCollection
+from onegov.ballot.collection import BallotCollection, VoteCollection
+
+__all__ = [
+    'Ballot',
+    'BallotCollection',
+    'BallotResult',
+    'Candidate',
+    'CandidateResult',
+    'Election',
+    'ElectionCollection',
+    'ElectionResult',
+    'List',
+    'ListConnection',
+    'ListResult',
+    'Vote',
+    'VoteCollection'
+]

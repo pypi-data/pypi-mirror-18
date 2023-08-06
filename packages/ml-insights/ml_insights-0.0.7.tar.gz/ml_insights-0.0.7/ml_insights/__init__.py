74287106f5b2b16f9768b535c9091dd5b9fc7560
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:12:46 +0000

    Add package ml-insights/0.0.7/ml_insights-0.0.7.tar.gz

---
+++ packages/ml-insights/ml_insights-0.0.7.tar.gz/ml_insights-0.0.7/ml_insights/__init__.py
@@ -0,0 +1,10 @@
+"""
+Package Docuemntation
+"""
+# -*- coding: utf-8 -*-
+
+from .insights import ModelXRay, prob_calibration_function
+
+__all__ = ["ModelXRay","prob_calibration_function"]
+
+__version__ = '0.0.7'

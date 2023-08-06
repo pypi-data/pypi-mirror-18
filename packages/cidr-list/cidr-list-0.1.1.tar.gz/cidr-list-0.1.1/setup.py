9b54d6ff3241e17ca61aa3e4204fcb6e455d8ab1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:08:53 +0000

    Add package cidr-list/0.1.1/cidr-list-0.1.1.tar.gz

---
+++ packages/cidr-list/cidr-list-0.1.1.tar.gz/cidr-list-0.1.1/setup.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+from __future__ import absolute_import
+from __future__ import print_function
+
+import io
+from os.path import join
+from os.path import dirname
+
+from setuptools import find_packages, setup
+
+
+def read(*names, **kwargs):
+    return io.open(
+        join(dirname(__file__), *names),
+        encoding=kwargs.get('encoding', 'utf8')
+    ).read()
+
+
+setup(
+    name='cidr-list',
+    version='0.1.1',
+    url='https://github.com/mpicard/python-cidr-list',
+    license='MIT',
+    author='Martin Picard',
+    author_email='map@cix.ie',
+    description='Convert CIDR to list of IPs',
+    long_description=read('README.md'),
+    packages=find_packages(exclude=['tests']),
+    include_package_data=True,
+    zip_safe=False,
+    platforms='any',
+    install_requires=['click', 'netaddr'],
+    entry_points={
+        'console_scripts': [
+            'cidr-list = cidr_list.cli:main',
+        ],
+    },
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: POSIX',
+        'Operating System :: MacOS',
+        'Operating System :: Unix',
+        'Operating System :: Microsoft :: Windows',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ]
+)

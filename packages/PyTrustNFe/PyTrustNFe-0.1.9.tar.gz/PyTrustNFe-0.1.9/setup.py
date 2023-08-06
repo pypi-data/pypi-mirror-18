03ed1b8c2bb31c49abb493139b647b404c0a41d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:19:59 +0000

    Add package PyTrustNFe/0.1.9/PyTrustNFe-0.1.9.tar.gz

---
+++ packages/PyTrustNFe/PyTrustNFe-0.1.9.tar.gz/PyTrustNFe-0.1.9/setup.py
@@ -0,0 +1,39 @@
+# coding=utf-8
+import pypandoc
+from setuptools import setup, find_packages
+
+long_description = pypandoc.convert('README.md', 'rst')
+
+setup(
+    name="PyTrustNFe",
+    version="0.1.9",
+    author="Danimar Ribeiro",
+    author_email='danimaribeiro@gmail.com',
+    keywords=['nfe', 'mdf-e'],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Plugins',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU Lesser General Public License v2 or \
+later (LGPLv2+)',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+    packages=find_packages(exclude=['*test*']),
+    package_data={'pytrustnfe': ['nfe/templates/*xml',
+                                 'nfse/paulistana/templates/*xml']},
+    url='https://github.com/danimaribeiro/PyTrustNFe',
+    license='LGPL-v2.1+',
+    description='PyTrustNFe é uma biblioteca para envio de NF-e',
+    long_description=long_description,
+    install_requires=[
+        'Jinja2 >= 2.8',
+        'signxml >= 2.0.0',
+    ],
+    test_suite='nose.collector',
+    tests_require=[
+        'nose',
+        'mock',
+    ],
+)

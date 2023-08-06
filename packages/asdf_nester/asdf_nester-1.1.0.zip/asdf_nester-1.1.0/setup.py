54b75d15427f2a72c24e97d500fea5b0e85d1584
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:12:50 +0000

    Add package asdf_nester/1.1.0/asdf_nester-1.1.0.zip

---
+++ packages/asdf_nester/asdf_nester-1.1.0.zip/asdf_nester-1.1.0/setup.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Dec  1 21:01:21 2016
+
+@author: zhongjian
+"""
+
+from distutils.core import setup
+
+setup(
+      name    ='asdf_nester',
+      version ='1.1.0',
+      py_modules=['asdf_nester'],
+      author = 'zhongjian',
+      author_email='15320279492m@sina.cn',      
+      description='a simple example for test'
+	)

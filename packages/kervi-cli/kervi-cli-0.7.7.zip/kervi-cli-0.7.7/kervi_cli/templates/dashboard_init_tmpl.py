08d16f0411e5544d509c1b7e9b9f23441478a308
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:50:38 +0000

    Add package kervi-cli/0.7.7/kervi-cli-0.7.7.zip

---
+++ packages/kervi-cli/kervi-cli-0.7.7.zip/kervi-cli-0.7.7/kervi_cli/templates/dashboard_init_tmpl.py
@@ -0,0 +1,4 @@
+""" bootstrap your kervi dashboards here """
+from kervi.dashboard import Dashboard
+
+Dashboard("main", "Main", "dashboard", True)
---
+++ packages/kervi-cli/kervi-cli-0.7.7.zip/kervi-cli-0.7.7/kervi_cli/templates/sensors_init_tmpl.py
@@ -0,0 +1,5 @@
+""" 
+Include the sensor modules that you have created here.
+Kervi will load the sensors that are defined here.
+ """
+import my_sensor

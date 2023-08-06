4ec2afc7c1adc6d77f088d985edc9229ba244346
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:22:34 +0000

    Add package nirum/0.3.1/nirum-0.3.1-py3-none-any.whl

---
+++ packages/nirum/nirum-0.3.1-py3-none-any.whl/nirum/exc.py
@@ -0,0 +1,57 @@
+""":mod:nirum.exc`
+~~~~~~~~~~~~~~~~~~
+
+"""
+from six.moves import urllib
+
+__all__ = (
+    'InvalidNirumServiceMethodNameError',
+    'InvalidNirumServiceMethodTypeError',
+    'NirumHttpError',
+    'NirumProcedureArgumentError',
+    'NirumProcedureArgumentRequiredError',
+    'NirumProcedureArgumentValueError',
+    'NirumServiceError',
+    'NirumUrlError',
+    'UnexpectedNirumResponseError',
+)
+
+
+class NirumServiceError(Exception):
+    """Base nirum service error"""
+
+
+class InvalidNirumServiceMethodNameError(ValueError, NirumServiceError):
+    """Raised when nirum service has invalid method name."""
+
+
+class InvalidNirumServiceMethodTypeError(TypeError, NirumServiceError):
+    """Raised when nirum service method is not callable."""
+
+
+class NirumProcedureArgumentError(ValueError, NirumServiceError):
+    """WIP"""
+
+
+class NirumProcedureArgumentRequiredError(NirumProcedureArgumentError):
+    """WIP"""
+
+
+class NirumProcedureArgumentValueError(NirumProcedureArgumentError):
+    """WIP"""
+
+
+class NirumHttpError(urllib.error.HTTPError, NirumServiceError):
+    """TODO"""
+
+
+class NirumUrlError(urllib.error.URLError, NirumServiceError):
+    """TODO"""
+
+    def __init__(self, exc):
+        self.text = exc.read()
+        super().__init__(exc.reason)
+
+
+class UnexpectedNirumResponseError(NirumHttpError):
+    """TODO"""

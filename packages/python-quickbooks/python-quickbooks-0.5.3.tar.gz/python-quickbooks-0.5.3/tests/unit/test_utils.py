e28194668ed47c78b5a7020c6561a62aeff45018
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:07:40 +0000

    Add package python-quickbooks/0.5.3/python-quickbooks-0.5.3.tar.gz

---
+++ packages/python-quickbooks/python-quickbooks-0.5.3.tar.gz/python-quickbooks-0.5.3/tests/unit/test_utils.py
@@ -0,0 +1,23 @@
+import unittest
+from quickbooks import utils
+
+
+class UtilsTests(unittest.TestCase):
+    def test_build_where_clause(self):
+        where_clause = utils.build_where_clause(field1=1, field2="Someone's Company")
+        self.assertEqual(where_clause, "field2 = 'Someone\\\'s Company' AND field1 = 1")
+
+    def test_build_where_clause_integers(self):
+        where_clause = utils.build_choose_clause(choices=[1, 2], field="field1")
+
+        self.assertEqual(where_clause, "field1 in (1, 2)")
+
+    def test_build_where_clause_strings(self):
+        where_clause = utils.build_choose_clause(choices=["val1", "val2"], field="field1")
+
+        self.assertEqual(where_clause, "field1 in ('val1', 'val2')")
+
+    def test_build_where_clause_quoted_value(self):
+        where_clause = utils.build_choose_clause(choices=["val1", "Someone's Company"], field="field1")
+
+        self.assertEqual(where_clause, "field1 in ('val1', 'Someone\\\'s Company')")

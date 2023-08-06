37d2ee1451e4ff02b2953890329980645129a389
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:09:05 +0000

    Add package sloth-ci/2.1.2/sloth_ci-2.1.2-py3-none-any.whl

---
+++ packages/sloth-ci/sloth_ci-2.1.2-py3-none-any.whl/sloth_ci/__init__.py
@@ -0,0 +1,11 @@
+﻿'''Lightweight tool that runs commands when you push to GitHub, Bitbucket, or Gitlab.
+
+Just type the commands you'd run manually and let Sloth CI run them for you!
+'''
+
+__title__ = 'sloth-ci'
+__description__ = 'Lightweight CI tool'
+__version__ = '2.1.2'
+__author__ = 'Konstantin Molchanov'
+__author_email__ = 'moigagoo@live.com'
+__license__ = 'MIT'

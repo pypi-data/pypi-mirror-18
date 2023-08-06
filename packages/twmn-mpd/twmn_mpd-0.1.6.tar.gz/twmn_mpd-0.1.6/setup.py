da90d81e28bfef025e0f583844841d4e5aa33fb7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:12:46 +0000

    Add package twmn-mpd/0.1.6/twmn_mpd-0.1.6.tar.gz

---
+++ packages/twmn-mpd/twmn_mpd-0.1.6.tar.gz/twmn_mpd-0.1.6/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup(name='twmn_mpd',
+      version='0.1.6',
+      description='A simple mpd notifier for twmn',
+      url='http://github.com/mrkgnao/twmn-mpd',
+      author='Soham Chowdhury',
+      author_email='chow.soham@gmail.com',
+      license='MIT',
+      packages=['twmn_mpd', 'twmn_mpd.providers'],
+      install_requires=[
+          'python-mpd2'
+      ],
+      entry_points={
+          'console_scripts': [
+              'mpd_twmn_notifier = twmn_mpd.TWMNSocketClient:start_notifs'
+          ]
+      },
+      zip_safe=False)

66fcdd03fa67c21e29ab718349f6e6534e3d7db9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:45:47 +0000

    Add package HoleCardHandicapper/1.1.0/HoleCardHandicapper-1.1.0.tar.gz

---
+++ packages/HoleCardHandicapper/HoleCardHandicapper-1.1.0.tar.gz/HoleCardHandicapper-1.1.0/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+setup(
+    name = 'HoleCardHandicapper',
+    version = '1.1.0',
+    author = 'ishikota',
+    author_email = 'ishikota086@gmail.com',
+    description = 'NeuralNet for poker hands evaluation ',
+    license = 'MIT',
+    keywords = 'python poker hand predict evaluate evaluator',
+    url = 'https://github.com/ishikota/HoleCardHandicapper',
+    packages = ['holecardhandicapper', 'holecardhandicapper.model', ],
+    package_data = {'holecardhandicapper' : ['model/weights/*.h5', 'model/data/preflop_winrate.csv']},
+    install_requires = ['Keras==1.0.4', 'h5py==2.6.0', 'PyPokerEngine==0.0.1'],
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Topic :: Scientific/Engineering :: Artificial Intelligence",
+        "License :: OSI Approved :: MIT License",
+    ],
+)

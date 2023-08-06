2aa878b907bb6c61d2da9695fd372d55f93e56d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:10:38 +0000

    Add package assignment/0.1/assignment-0.1.tar.gz

---
+++ packages/assignment/assignment-0.1.tar.gz/assignment-0.1/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+		name = 'assignment' , 
+		version = '0.1' , 
+		description = 'calculation of minimum price of given book' ,
+		url = 'https://github.com/ptyma/assignment' ,
+		author = 'Pratima Chettri' , 
+		author_email = 'chettripratima@gmail.com' , 
+		packages = ['assignment'] ,
+		zip_safe=False
+	)
+		
+		

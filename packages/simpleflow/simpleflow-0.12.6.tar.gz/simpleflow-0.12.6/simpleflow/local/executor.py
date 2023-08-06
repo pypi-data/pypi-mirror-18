b42357c7cddc0a948351f501c6e4b46db4330f60
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:19:00 +0000

    Add package simpleflow/0.12.6/simpleflow-0.12.6.tar.gz

---
+++ packages/simpleflow/simpleflow-0.12.6.tar.gz/simpleflow-0.12.6/simpleflow/local/executor.py
@@ -0,0 +1,50 @@
+import logging
+
+from simpleflow import (
+    exceptions,
+    executor,
+    futures,
+)
+from ..task import ActivityTask
+
+
+logger = logging.getLogger(__name__)
+
+
+class Executor(executor.Executor):
+    """
+    Executes all tasks synchronously in a single local process.
+
+    """
+    def submit(self, func, *args, **kwargs):
+        logger.info('executing task {}(args={}, kwargs={})'.format(
+            func, args, kwargs))
+
+        future = futures.Future()
+
+        task = ActivityTask(func, *args, **kwargs)
+
+        try:
+            future._result = task.execute()
+        except Exception as err:
+            future._exception = err
+            if func.raises_on_failure:
+                message = err.args[0] if err.args else ''
+                raise exceptions.TaskFailed(func.name, message)
+        finally:
+            future._state = futures.FINISHED
+
+        return future
+
+    def run(self, input=None):
+        if input is None:
+            input = {}
+        args = input.get('args', ())
+        kwargs = input.get('kwargs', {})
+
+        self.before_replay()
+        result = self.run_workflow(*args, **kwargs)
+        self.after_replay()
+        self.on_completed()
+        self.after_closed()
+        return result

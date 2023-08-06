8c73da0c9b8218793223efaee34ebcf6e173117f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:09:03 +0000

    Add package einder/0.3.1/einder-0.3.1.tar.gz

---
+++ packages/einder/einder-0.3.1.tar.gz/einder-0.3.1/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(name='einder',
+      version='0.3.1',
+      author='Auke Willem Oosterhoff',
+      author_email='auke@orangetux.nl',
+      description='API wrapper for Samsumgs set-top boxes SMT-G7400 and SMT-G7401.',  # NOQA
+      url='https://github.com/OrangeTux/einder',
+      packages=[
+        'einder'
+      ],
+      license='MIT',
+      classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Topic :: Multimedia']
+     )

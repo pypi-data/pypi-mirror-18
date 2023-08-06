f5b6f1a235c9bb41d3fc62488ec69f090f39eec2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:12:51 +0000

    Add package asdf_nester/1.3.0/asdf_nester-1.3.0.zip

---
+++ packages/asdf_nester/asdf_nester-1.3.0.zip/asdf_nester-1.3.0/asdf_nester.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Dec  1 20:55:12 2016
+
+@author: zhongjian
+"""
+
+
+"""从嵌套多层的列表中打印出单个数据"""
+def diedai_list(the_list,tab=0):
+    """用递归函数解决"""
+    for i in the_list:
+        if isinstance(i,list):
+            print(diedai_list(i,tab+1))
+        else:
+            for add_tab in range(tab):
+                print('\t',end='')
+            print(i)

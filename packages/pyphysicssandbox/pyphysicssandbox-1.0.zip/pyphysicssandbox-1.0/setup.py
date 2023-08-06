1b526bacd3200bbf7c02e2cbac3b93785f65e5fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:17:29 +0000

    Add package pyphysicssandbox/1.0/pyphysicssandbox-1.0.zip

---
+++ packages/pyphysicssandbox/pyphysicssandbox-1.0.zip/pyphysicssandbox-1.0/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+setup(
+    name = 'pyphysicssandbox',
+    packages = ['pyphysicssandbox', 'py2d'],
+    version = '1.0',
+    description = 'A simple Python physics sandbox for intro programming students',
+    author = 'Jay Shaffstall',
+    author_email = 'jshaffstall@gmail.com',
+    url = 'https://github.com/jshaffstall/PyPhysicsSandbox',
+    download_url = 'https://github.com/jshaffstall/PyPhysicsSandbox/tarball/0.1',
+    keywords = ['physics'],
+    classifiers = [],
+    requires=['pygame','pymunk'],
+)

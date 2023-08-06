4e5163fe67e5858a127d18580495e6a63faa4566
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:19:32 +0000

    Add package tf-literature-based-discovery/0.0.4/tf_literature_based_discovery-0.0.4.tar.gz

---
+++ packages/tf-literature-based-discovery/tf_literature_based_discovery-0.0.4.tar.gz/tf_literature_based_discovery-0.0.4/setup.py
@@ -0,0 +1,39 @@
+import os
+from setuptools import setup
+
+with open(os.path.join(os.path.dirname(__file__), 'README.md')) as readme:
+    README = readme.read()
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+setup(
+    name='tf_literature_based_discovery',
+    version='0.0.4',
+    packages=['tf_literature_based_discovery'],
+    include_package_data=True,
+    license='MIT License',
+    description='TextFlows module for literature-based cross-context discovery',
+    long_description=README,
+    url='https://github.com/xflows/tf_literature_based_discovery',
+    author='Matic Perovsek',
+    author_email='matic.perovsek@ijs.si',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Scientific/Engineering :: Artificial Intelligence',
+        'Topic :: Software Development :: Libraries'
+    ],
+    install_requires=[
+        "tf_core",
+        "tf_latino",
+    ]
+)

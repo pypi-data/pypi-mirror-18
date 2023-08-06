b2aaaa1d11f0e3ea47ac749f3ab5fea0ca6d8ad9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:32:24 +0000

    Add package buddy/0.0.3/buddy-0.0.3.tar.gz

---
+++ packages/buddy/buddy-0.0.3.tar.gz/buddy-0.0.3/setup.py
@@ -0,0 +1,48 @@
+from setuptools import find_packages, setup
+
+dependencies = ['boto3', 'click', 'pyyaml', 'tabulate', 'Arrow']
+
+classifiers = [
+    'Development Status :: 3 - Alpha',
+    'Environment :: Console',
+    'Intended Audience :: Developers',
+    'License :: OSI Approved :: MIT License',
+    'Natural Language :: English',
+    'Operating System :: POSIX :: Linux',
+    'Programming Language :: Python',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: Implementation :: CPython',
+    'Topic :: System',
+    'Topic :: Software Development',
+]
+
+
+entry_points = {
+    'console_scripts': [
+        'bcluster = buddy.command.cluster:cli',
+        'bstack = buddy.command.stack:cli',
+    ],
+}
+
+
+setup(
+    name='buddy',
+    version='0.0.3',
+
+    description='Buddy, your Cloudformation/ECS valet',
+    long_description=open('README.rst').read(),
+    keywords='aws cloudformation ecs deployment',
+    license='MIT License',
+    platforms='Linux',
+    classifiers=classifiers,
+
+    url='https://github.com/pior/buddy',
+    author='Pior Bastida',
+    author_email='pior@pbastida.net',
+
+    packages=find_packages(exclude=['tests']),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=dependencies,
+    entry_points=entry_points,
+)

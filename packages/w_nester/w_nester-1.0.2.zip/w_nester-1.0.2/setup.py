b4e5d57e03e09e3317d9fc55f0dc023738280e05
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:10:24 +0000

    Add package w_nester/1.0.2/w_nester-1.0.2.zip

---
+++ packages/w_nester/w_nester-1.0.2.zip/w_nester-1.0.2/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+	name		= 'w_nester',
+	version		= '1.0.2',
+	py_modules	= ['w_nester'],	
+	author		= 'whybe woo',
+	author_email	= 'whybewoo@gmail.com',
+	url		= 'http://gmail.com',
+	description	= 'test: input list is write for list_out("v=list",b=False,n=0,fh)function with text file./v=list variable, b(bool)= Do list indent?, n=number of tab./'
+	)
---
+++ packages/w_nester/w_nester-1.0.2.zip/w_nester-1.0.2/w_nester.py
@@ -0,0 +1,11 @@
+﻿import sys
+def list_out(_list, indent=False, level=0, fh=sys.stdout):
+	for x in _list:
+		if isinstance(x, list):
+			list_out(x, indent, level+1, fh)
+		else:
+			if indent:
+				for y in range(level):
+					print("\t", end='', file=fh)
+					print(y, file=fh)
+			print(x, file=fh)

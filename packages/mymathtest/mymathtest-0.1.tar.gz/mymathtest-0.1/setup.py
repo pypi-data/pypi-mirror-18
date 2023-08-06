f4007af5c7edabed58f9cd7603b381b91122249c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:17:58 +0000

    Add package mymathtest/0.1/mymathtest-0.1.tar.gz

---
+++ packages/mymathtest/mymathtest-0.1.tar.gz/mymathtest-0.1/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'mymathtest',
+  packages = ['mymathtest'], # this must be the same as the name above
+  version = '0.1',
+  description = 'A tutorial lib',
+  author = 'Chilcote',
+  author_email = 'chilcote@gmail.com',
+  url = 'https://github.com/chilcote',
+  download_url = 'https://github.com/chilcote',
+  keywords = ['add', 'sub', 'tests'],
+  classifiers = [],
+)

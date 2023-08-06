e21634ba31eee68ee7f3e39e91f01d9658afe08a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:09:56 +0000

    Add package elasticsearch-django/0.3.3/elasticsearch-django-0.3.3.tar.gz

---
+++ packages/elasticsearch-django/elasticsearch-django-0.3.3.tar.gz/elasticsearch-django-0.3.3/setup.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+from os import path, chdir, pardir
+from setuptools import setup, find_packages
+
+README = open(path.join(path.dirname(__file__), 'README.rst')).read()
+
+# allow setup.py to be run from any path
+chdir(path.normpath(path.join(path.abspath(__file__), pardir)))
+
+setup(
+    name="elasticsearch-django",
+    version="0.3.3",
+    packages=find_packages(),
+    install_requires=[
+        'django>=1.8,<1.9',
+        'elasticsearch-dsl>=2.0',
+        'simplejson>=3.0',
+    ],
+    include_package_data=True,
+    description='Elasticsearch Django app',
+    long_description=README,
+    url='https://github.com/yunojuno/elasticsearch-django',
+    author='Hugo Rodger-Brown',
+    author_email='code@yunojuno.com',
+    maintainer='Hugo Rodger-Brown',
+    maintainer_email='hugo@yunojuno.com',
+    classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    ],
+)

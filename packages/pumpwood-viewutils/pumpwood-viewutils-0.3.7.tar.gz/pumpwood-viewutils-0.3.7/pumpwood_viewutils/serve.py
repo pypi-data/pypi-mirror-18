7cea106842c5f40123f89437e7acc9e934884b00
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:08:54 +0000

    Add package pumpwood-viewutils/0.3.7/pumpwood-viewutils-0.3.7.tar.gz

---
+++ packages/pumpwood-viewutils/pumpwood-viewutils-0.3.7.tar.gz/pumpwood-viewutils-0.3.7/pumpwood_viewutils/serve.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+from rest_framework.decorators import permission_classes, authentication_classes, api_view
+from rest_framework.permissions import IsAuthenticated
+from django.http import HttpResponse
+
+@api_view(['GET'])
+@permission_classes((IsAuthenticated,))
+def serve_X_Accel_protected(request, path, **kwargs):
+    '''
+        Verify if user is logged and serve files in Ngnix using X-Accel to avoid django overhead
+    '''
+    print 'request.path: ', request.path
+    print 'path: ', path
+    response = HttpResponse()
+    response['X-Accel-Redirect'] = 'protected/' + path
+    return response
+
+@api_view(['GET'])
+def serve_X_Accel_unprotected(request, path, **kwargs):
+    '''
+        Serve files in Ngnix using X-Accel to avoid django overhead
+    '''
+    print 'request.path: ', request.path
+    print 'path: ', path
+    response = HttpResponse()
+    response['X-Accel-Redirect'] = 'unprotected/' + path
+    print response

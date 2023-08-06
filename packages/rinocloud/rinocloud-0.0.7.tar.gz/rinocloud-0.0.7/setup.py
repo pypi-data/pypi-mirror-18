28544e6f1c491409d70dd41a740ebf6bdb2ce2a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:12:09 +0000

    Add package rinocloud/0.0.7/rinocloud-0.0.7.tar.gz

---
+++ packages/rinocloud/rinocloud-0.0.7.tar.gz/rinocloud-0.0.7/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+setup(
+    name="rinocloud",
+    version="0.0.7",
+    packages=["rinocloud"],
+    description='Rinocloud python bindings',
+    author='Eoin Murray',
+    author_email='eoin@rinocloud.com',
+    url='https://github.com/rinocloud/rinocloud-python',  # use the URL to the github repo
+    download_url='https://github.com/rinocloud/rinocloud-python/tarball/0.1',  # I'll explain this in a second
+    keywords=['rinocloud', 'data', 'api'],  # arbitrary keywords
+    classifiers=[],
+    test_suite='rinocloud.test.all',
+    tests_require=['mock'],
+    install_requires=[
+        "requests",
+        "requests_toolbelt",
+        "arrow",
+        "clint"
+    ],
+)

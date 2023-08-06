73e0fc9dbb74f5a4192a169940175a3988ba0edc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:45:07 +0000

    Add package fixtureupper/0.2.4/fixtureupper-0.2.4.tar.gz

---
+++ packages/fixtureupper/fixtureupper-0.2.4.tar.gz/fixtureupper-0.2.4/fixtureupper/__init__.py
@@ -0,0 +1,11 @@
+from __future__ import (absolute_import, division,
+                        print_function, unicode_literals)
+
+from fixtureupper.register import UpperRegister
+from fixtureupper.defaults import inherit
+
+
+__all__ = [
+    'UpperRegister',
+    'inherit',
+]

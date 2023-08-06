5435a309a9d1221d548d26a8ea2d9b2e6ad00475
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:45:35 +0000

    Add package dj_nester/1.0.0/dj_nester-1.0.0.zip

---
+++ packages/dj_nester/dj_nester-1.0.0.zip/dj_nester-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+        name            = 'dj_nester',
+        version         = '1.0.0',
+        py_modules      = ['dj_nester'],
+        author          = 'daijuan',
+        author_email    = '1034965697@qq.com',
+        url             = 'https://mail.qq.com/',
+        description     = 'A simple printer of nested list',
+        )

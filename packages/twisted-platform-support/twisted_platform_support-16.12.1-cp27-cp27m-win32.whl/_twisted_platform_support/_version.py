1d8e6e721c3d20f3c3cb535752f125b8793a4cba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:15:20 +0000

    Add package twisted-platform-support/16.12.1/twisted_platform_support-16.12.1-cp27-cp27m-win32.whl

---
+++ packages/twisted-platform-support/twisted_platform_support-16.12.1-cp27-cp27m-win32.whl/_twisted_platform_support/_version.py
@@ -0,0 +1,11 @@
+"""
+Provides _twisted_platform_support version information.
+"""
+
+# This file is auto-generated! Do not edit!
+# Use `python -m incremental.update _twisted_platform_support` to change this file.
+
+from incremental import Version
+
+__version__ = Version('_twisted_platform_support', 16, 12, 1)
+__all__ = ["__version__"]

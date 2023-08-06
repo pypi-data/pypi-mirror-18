cd386aaa9787892c70eececb5e2154f526f3d8d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:20:37 +0000

    Add package nester_agunbiade/1.0.0/nester_agunbiade-1.0.0.zip

---
+++ packages/nester_agunbiade/nester_agunbiade-1.0.0.zip/nester_agunbiade-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+		name	= 'nester_agunbiade',
+		version	= '1.0.0',
+		py_modules	= ['nester_agunbiade'],
+		author		= 'Agunbiade',
+		author_email = 'abdulrasheedagunbiade@gmail.com',
+		url			=	'',
+		description = 'A simpple printer for nested lists',
+		)

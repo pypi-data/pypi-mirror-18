0ce311b9fcfac872bde2a35f083f272be5ba2502
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:45:35 +0000

    Add package dj-twilio-sms/1.1.0/dj-twilio-sms-1.1.0.tar.gz

---
+++ packages/dj-twilio-sms/dj-twilio-sms-1.1.0.tar.gz/dj-twilio-sms-1.1.0/setup.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+version = "1.1.0"
+
+with open('README.rst', 'r') as f:
+    readme = f.read()
+
+setup(
+    name="dj-twilio-sms",
+    version=version,
+    description="Twilio SMS Integration for Django",
+    keywords='django twilio sms',
+    license="MIT",
+    author="Emin Mastizada",
+    author_email="emin@linux.com",
+    url="https://github.com/mastizada/dj-twilio-sms",
+    download_url="https://github.com/mastizada/dj-twilio-sms/zipball/master",
+    long_description=readme,
+    package_dir={"dj_twilio_sms": "dj_twilio_sms"},
+    packages=["dj_twilio_sms"],
+    include_package_data=True,
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "License :: OSI Approved :: MIT License",
+        "Environment :: Web Environment",
+        "Framework :: Django",
+        "Intended Audience :: Developers",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.5",
+        "Topic :: Software Development :: Libraries :: Python Modules"
+    ],
+    install_requires=[
+        "Django",
+        "djangorestframework>=2.4.7",
+        "twilio>=5.6.0",
+    ]
+)

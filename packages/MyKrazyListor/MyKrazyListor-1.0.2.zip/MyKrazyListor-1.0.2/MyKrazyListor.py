2b64aec4efe237acd35ce0ec80d61231f202b954
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:45:23 +0000

    Add package MyKrazyListor/1.0.2/MyKrazyListor-1.0.2.zip

---
+++ packages/MyKrazyListor/MyKrazyListor-1.0.2.zip/MyKrazyListor-1.0.2/MyKrazyListor.py
@@ -0,0 +1,13 @@
+""" This is my first python module to iterate a list of items.
+Going good so far.
+"""
+
+def list_recursion(inputList, level=0):
+# This iterates the inputList collection.
+	for listItem in inputList:
+		if isinstance(listItem, list):
+			list_recursion(listItem, level+1)
+		else :
+			for tab_stop in range(level):
+				print("\t", end='')
+			print(listItem)

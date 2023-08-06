fa0a1ca960d0bc2e041554e873a71428b1f43658
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:10:31 +0000

    Add package plandf/0.1.131/plandf-0.1.131.tar.gz

---
+++ packages/plandf/plandf-0.1.131.tar.gz/plandf-0.1.131/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup(name='plandf',
+    version='0.1.131',
+    description='Pandas DataFrame for computing Value-Over-Time for lists of tuples (Step.investables, Step.deliverables) of a Plan.',
+    url='https://github.com/WeFindX/PlanDF',
+    author='Mindey I.',
+    author_email='desk@infty.xyz',
+    license='MIT',
+    packages=['plandf'],
+    install_requires=[
+        'stepio>=0.1',
+        'pandas>=0.17.1',
+    ],
+    extras_require = {
+        'conversion_rates':  ["fred==3.1",
+                              "requests>=2.9.1"],
+    },
+    zip_safe=False)

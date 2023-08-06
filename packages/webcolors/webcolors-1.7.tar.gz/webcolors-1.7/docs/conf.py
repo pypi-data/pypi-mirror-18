b59307353ff8e04a845cab47471d7bb52286cb7f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:46:14 +0000

    Add package webcolors/1.7/webcolors-1.7.tar.gz

---
+++ packages/webcolors/webcolors-1.7.tar.gz/webcolors-1.7/docs/conf.py
@@ -0,0 +1,25 @@
+import os
+
+on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
+
+extensions = []
+templates_path = ['_templates']
+source_suffix = '.rst'
+master_doc = 'index'
+project = u'webcolors'
+copyright = u'2008-2016, James Bennett'
+version = '1.7'
+release = '1.7'
+exclude_trees = ['_build']
+pygments_style = 'sphinx'
+html_static_path = ['_static']
+htmlhelp_basename = 'webcolorsdoc'
+latex_documents = [
+  ('index', 'webcolors.tex', u'webcolors Documentation',
+   u'James Bennett', 'manual'),
+]
+if not on_rtd:
+    import sphinx_rtd_theme
+    html_theme = 'sphinx_rtd_theme'
+    html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+

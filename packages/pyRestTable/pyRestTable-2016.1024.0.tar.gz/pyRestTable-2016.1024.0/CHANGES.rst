115f0e9a9fe2d72c6ec6f38747f4866aed53a889
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:06:27 +0000

    Add package pyRestTable/2016.1024.0/pyRestTable-2016.1024.0.tar.gz

---
+++ packages/pyRestTable/pyRestTable-2016.1024.0.tar.gz/pyRestTable-2016.1024.0/CHANGES.rst
@@ -0,0 +1,15 @@
+..
+  This file describes user-visible changes between the versions.
+
+Change History
+##############
+
+Production
+**********
+
+:2016.1024.0: unit tests, repaired python 3 support
+:2016.1003.1: support python 3 AND python 2
+:2015.1115.0: add support methods, such as `addLabel` and `addRow`
+:2015.1111.01: support output as ReST `list-table` directive
+:2014.0710.01: provide docs at http://pyRestTable.readthedocs.org
+:2014.0430.01: release after forking from previous home
---
+++ packages/pyRestTable/pyRestTable-2016.1024.0.tar.gz/pyRestTable-2016.1024.0/setup.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+
+#-----------------------------------------------------------------------------
+# :author:    Pete R. Jemian
+# :email:     prjemian@gmail.com
+# :copyright: (c) 2014-2016, Pete R. Jemian
+#
+# Distributed under the terms of the Creative Commons Attribution 4.0 International Public License.
+#
+# The full license is in the file LICENSE, distributed with this software.
+#-----------------------------------------------------------------------------
+
+from setuptools import setup, find_packages
+import os
+import re
+import sys
+
+# pull in some definitions from the package's __init__.py file
+sys.path.insert(0, os.path.join('.', 'src', ))
+import pyRestTable
+
+
+verbose=1
+long_description = open('README.rst', 'r').read()
+
+
+setup (name =             pyRestTable.__package_name__,        # pyRestTable
+       version =          pyRestTable.__version__,
+       license =          pyRestTable.__license__,
+       description =      pyRestTable.__description__,
+       long_description = long_description,
+       author =           pyRestTable.__author_name__,
+       author_email =     pyRestTable.__author_email__,
+       url =              pyRestTable.__url__,
+       download_url =     pyRestTable.__download_url__,
+       keywords =         pyRestTable.__keywords__,
+       platforms =        'any',
+       package_dir =      {'': 'src'},
+       packages =         ['pyRestTable', ],
+       #packages=find_packages(),
+       package_data     = {
+                            'pyRestTable': [
+			        'LICENSE.txt',
+			        'VERSION',
+			    ],
+                         },
+       classifiers =      pyRestTable.__classifiers__,
+      )

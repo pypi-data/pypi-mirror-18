25979ca7f800535653be0a2c28c11532f242e4be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:12:52 +0000

    Add package steven_nester/1.2.0/steven_nester-1.2.0.zip

---
+++ packages/steven_nester/steven_nester-1.2.0.zip/steven_nester-1.2.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup       #从发布工具导入setup函数  
+  
+setup(  
+    name        ='steven_nester',  
+    version     ='1.2.0',  
+    py_modules  =['steven_nester'],  
+    author      ='steven',  
+    author_email='matchbook@live.com',  
+    url         ='http://www.headfirstlabs.com',  
+    description ='A simple printer of nested lists',  
+    )  

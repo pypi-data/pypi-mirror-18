0abeef9fe2d96b5f6c588a558d5f08ccbfdd36cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:10:53 +0000

    Add package yeelight/0.0.6/yeelight-0.0.6-py2-none-any.whl

---
+++ packages/yeelight/yeelight-0.0.6-py2-none-any.whl/yeelight/__init__.py
@@ -0,0 +1,10 @@
+#  flake8: noqa
+from yeelight.main import Bulb
+from yeelight.flow import (
+        Flow,
+        RGBTransition,
+        TemperatureTransition,
+        SleepTransition
+)
+
+__version__ = '0.0.6'

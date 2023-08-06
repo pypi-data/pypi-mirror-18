c931021471043e0e94c66d899d586817485991c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:16:18 +0000

    Add package pyoa/0.101/pyoa-0.101.tar.gz

---
+++ packages/pyoa/pyoa-0.101.tar.gz/pyoa-0.101/pyoa/ara.py
@@ -0,0 +1,18 @@
+import re
+
+
+# deNoise(text) deletes short vowels from Arabic text
+def deNoise(text):
+    noise = re.compile(""" ّ    | # Tashdid
+                             َ    | # Fatha
+                             ً    | # Tanwin Fath
+                             ُ    | # Damma
+                             ٌ    | # Tanwin Damm
+                             ِ    | # Kasra
+                             ٍ    | # Tanwin Kasr
+                             ْ    | # Sukun
+                             ـ     # Tatwil/Kashida
+                         """, re.VERBOSE)
+    text = re.sub(noise, '', text)
+    return(text)
+

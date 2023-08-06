f4e5a24fde95df5d6ad97153a7c1698c4f77d327
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:20:02 +0000

    Add package beetmoverscript/0.0.10/beetmoverscript-0.0.10.tar.gz

---
+++ packages/beetmoverscript/beetmoverscript-0.0.10.tar.gz/beetmoverscript-0.0.10/beetmoverscript/test/__init__.py
@@ -0,0 +1,17 @@
+from beetmoverscript.utils import load_json
+
+
+def get_fake_valid_config():
+    return load_json(path="beetmoverscript/test/fake_config.json")
+
+
+def get_fake_valid_task():
+    return load_json(path="beetmoverscript/test/test_work_dir/task.json")
+
+
+def get_fake_balrog_props():
+    return load_json(path="beetmoverscript/test/fake_balrog_props.json")
+
+
+def get_fake_balrog_manifest():
+    return load_json(path="beetmoverscript/test/fake_balrog_manifest.json")

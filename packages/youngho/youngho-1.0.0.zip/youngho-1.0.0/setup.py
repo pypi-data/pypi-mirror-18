8c2b3bedbd3cbf72ac931ccd9d85fb8c30b16bbd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:29:59 +0000

    Add package youngho/1.0.0/youngho-1.0.0.zip

---
+++ packages/youngho/youngho-1.0.0.zip/youngho-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name = 'youngho',
+    version = '1.0.0',
+    py_modules = ['youngho'],
+    author = 'youngho',
+    author_email = 'luckypenny79@gmail.com',
+    url = 'http://www.luckypenny79.com',
+    description = 'A simple printer of nested lists',
+    )

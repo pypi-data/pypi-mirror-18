1477a57ea86d94a51eae9f0083fd7605d552528c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:10:48 +0000

    Add package extradict/0.2.5/extradict-0.2.5.tar.gz

---
+++ packages/extradict/extradict-0.2.5.tar.gz/extradict-0.2.5/extradict/__init__.py
@@ -0,0 +1,13 @@
+# coding:utf-8
+from .version_dict import VersionDict
+from .version_dict import OrderedVersionDict
+from .map_getter import MapGetter
+from .reciprocal_dict import BijectiveDict
+from .extratuple import namedtuple
+from .extratuple import defaultnamedtuple
+from .extratuple import fastnamedtuple
+
+
+__author__ = "João S. O. Bueno"
+__version__ = "0.2.5"
+

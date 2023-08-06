f8273353d5f90286c5b05a9ad6651ceccfb3c426
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:08:42 +0000

    Add package yoyotestpags00/1.2.0/yoyotestpags00-1.2.0.zip

---
+++ packages/yoyotestpags00/yoyotestpags00-1.2.0.zip/yoyotestpags00-1.2.0/yoyotestpags00.py
@@ -0,0 +1,8 @@
+def print_list(list_name,tab_num=0):
+        for each_item in list_name:
+                if isinstance(each_item,list):
+                        print_list(each_item,tab_num+1)
+                else:
+                        for num in range(tab_num):
+                                print("\t",end="")
+                        print(each_item)

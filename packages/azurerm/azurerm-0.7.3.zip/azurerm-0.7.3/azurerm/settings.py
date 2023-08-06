9b72b9699c7a78f286fd0829ae7f13c1b2923754
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:32 +0000

    Add package azurerm/0.7.3/azurerm-0.7.3.zip

---
+++ packages/azurerm/azurerm-0.7.3.zip/azurerm-0.7.3/azurerm/settings.py
@@ -0,0 +1,13 @@
+# settings.py - place to store constants for azurerm
+
+azure_rm_endpoint = 'https://management.azure.com'
+
+ACS_API = '2016-03-30'
+BASE_API = '2015-01-01'
+COMP_API = '2016-03-30'
+INSIGHTS_API = '2015-04-01'
+INSIGHTS_METRICS_API = '2016-03-01'
+INSIGHTS_PREVIEW_API = '2016-06-01'
+MEDIA_API = '2015-10-01'
+NETWORK_API = '2016-06-01'
+STORAGE_API = '2016-01-01'

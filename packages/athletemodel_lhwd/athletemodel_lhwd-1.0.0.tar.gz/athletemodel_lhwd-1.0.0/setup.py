354d59ad438a18cc374009e64dbecd7033b93055
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:10:42 +0000

    Add package athletemodel_lhwd/1.0.0/athletemodel_lhwd-1.0.0.tar.gz

---
+++ packages/athletemodel_lhwd/athletemodel_lhwd-1.0.0.tar.gz/athletemodel_lhwd-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name= 'athletemodel_lhwd',
+    version= '1.0.0',
+    py_modules = ['athletemodel'],
+    author= 'lihang',
+    author_email= '418045274@qq.com',
+    url = 'http://www.cutelhwd.cn',
+    description= 'athlete model by lhwd',
+)

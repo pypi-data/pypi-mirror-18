951778dee75a4e1efe4e21be0eee0fad673278a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:50:28 +0000

    Add package nester_learn_1/1.0.0/nester_learn_1-1.0.0.tar.gz

---
+++ packages/nester_learn_1/nester_learn_1-1.0.0.tar.gz/nester_learn_1-1.0.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup (
+        name = 'nester_learn_1',
+        version = '1.0.0',
+        py_modules = ['nester_learn_1'],
+        author = 'hfpython',
+        author_email = '2683731045@qq.com',
+        url = 'http://www.qq.com',
+        description = 'A simple printer of nested lists',
+        )

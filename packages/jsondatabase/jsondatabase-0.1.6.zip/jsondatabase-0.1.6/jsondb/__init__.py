29557806f1d96f35d85f4b939bec2fe313af1b8d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:17:29 +0000

    Add package jsondatabase/0.1.6/jsondatabase-0.1.6.zip

---
+++ packages/jsondatabase/jsondatabase-0.1.6.zip/jsondatabase-0.1.6/jsondb/__init__.py
@@ -0,0 +1,10 @@
+"""
+A flat file database for json objects.
+"""
+
+from .db import Database
+
+__version__ = '0.1.6'
+__author__ = 'Gunther Cox'
+__email__ = 'gunthercx@gmail.com'
+__url__ = 'https://github.com/gunthercox/jsondb'

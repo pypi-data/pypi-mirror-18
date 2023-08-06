ed869df5f846e47322afb1721b4b469a7f0d0098
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:50:41 +0000

    Add package rancher/0.1.1/rancher-0.1.1.tar.gz

---
+++ packages/rancher/rancher-0.1.1.tar.gz/rancher-0.1.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+
+setup(
+    name="rancher",
+    version="0.1.1",
+    author="Miguel Ángel Durán González",
+    author_email="hi@mangel.me",
+    url="https://github.com/mangeld/rancher_api",
+    packages=['rancher',],
+)

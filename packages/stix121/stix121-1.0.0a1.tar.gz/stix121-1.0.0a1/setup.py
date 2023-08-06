61c81bcf6af5582334f1805160dd4b83db323134
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:09:09 +0000

    Add package stix121/1.0.0a1/stix121-1.0.0a1.tar.gz

---
+++ packages/stix121/stix121-1.0.0a1.tar.gz/stix121-1.0.0a1/setup.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python
+
+# Copyright (c) 2015 - The MITRE Corporation
+# For license information, see the LICENSE.txt file
+
+from os.path import abspath, dirname, join
+
+
+from setuptools import setup, find_packages
+
+BASE_DIR = dirname(abspath(__file__))
+VERSION_FILE = join(BASE_DIR, 'stix', 'version.py')
+
+def get_version():
+    with open(VERSION_FILE) as f:
+        for line in f.readlines():
+            if line.startswith("__version__"):
+                version = line.split()[-1].strip('"')
+                return version
+        raise AttributeError("Package does not have a __version__")
+
+
+with open('README.rst') as f:
+    readme = f.read()
+
+extras_require = {
+    'docs': [
+        'Sphinx==1.3.1',
+        'sphinx_rtd_theme==0.1.8',
+    ],
+    'test': [
+        'nose==1.3.0',
+        'tox==1.6.1',
+        'maec411',
+    ],
+}
+
+
+install_requires = [
+    'lxml>=2.3',
+    'python-dateutil',
+    'cybox211',
+    'mixbox>=1.0.1',
+]
+
+
+setup(
+    name="stix121",
+    version=get_version(),
+    author="STIX Project, MITRE Corporation",
+    author_email="stix@mitre.org",
+    description="An API for parsing and generating STIX content.",
+    long_description=readme,
+    url="http://stix.mitre.org",
+    packages=find_packages(),
+    install_requires=install_requires,
+    extras_require=extras_require,
+    classifiers=[
+        "Programming Language :: Python",
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+    ]
+)

530a44c7f20a654a587c1325fd145be0988bcd0b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:32:04 +0000

    Add package kev/0.7.0/kev-0.7.0-py2-none-any.whl

---
+++ packages/kev/kev-0.7.0-py2-none-any.whl/kev/exceptions.py
@@ -0,0 +1,31 @@
+class ValidationException(Exception):
+
+    def __init__(self, msg):
+        self.error_msg = msg
+
+    def __str__(self):
+        return self.error_msg
+
+
+class DocNotFoundError(Exception):
+    pass
+
+
+class QueryIndexError(Exception):
+    pass
+
+
+class QueryError(Exception):
+    pass
+
+
+class ResourceNotFound(Exception):
+    pass
+
+
+class DocSaveError(Exception):
+    pass
+
+
+class ResourceError(Exception):
+    pass

7be3c8db8cff10660611aca09dbedc70bab9416e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:32:30 +0000

    Add package Serocom/1.2.0/Serocom-1.2.0.zip

---
+++ packages/Serocom/Serocom-1.2.0.zip/Serocom-1.2.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name         = 'Serocom',
+    version      = '1.2.0',
+    py_modules   = ['nester'],
+    author       = 'serocom',
+    author_email = 'serocom13@gmail.com',
+    url          = 'http://www.serocom.webs.com',
+    description  = 'A simple print function',
+    )

ab9b40dc8c215fbe2ef06e5aa9441580cb4731c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:11:25 +0000

    Add package lens-cli/0.1.1/lens-cli-0.1.1.tar.gz

---
+++ packages/lens-cli/lens-cli-0.1.1.tar.gz/lens-cli-0.1.1/setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+import os
+from setuptools import setup, find_packages
+
+setup(
+    name='lens-cli',
+    author='Veit Heller',
+    version='0.1.1',
+    license='MIT',
+    url='https://github.com/port-zero/lens',
+    description='Extensible data structure traversal in the command line',
+    download_url = 'https://github.com/port-zero/lens/tarball/0.1.1',
+    packages=find_packages('.'),
+    install_requires=[
+        "pygments",
+    ],
+    entry_points={
+        'console_scripts': [
+            'lens = lens:run',
+        ]
+    },
+)
+

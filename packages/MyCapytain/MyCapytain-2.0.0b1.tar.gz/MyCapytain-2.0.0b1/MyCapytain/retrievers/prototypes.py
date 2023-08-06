d87ea04379c008698fbe79953b856b4d512cf68c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:19:41 +0000

    Add package MyCapytain/2.0.0b1/MyCapytain-2.0.0b1.tar.gz

---
+++ packages/MyCapytain/MyCapytain-2.0.0b1.tar.gz/MyCapytain-2.0.0b1/MyCapytain/retrievers/prototypes.py
@@ -0,0 +1,158 @@
+# -*- coding: utf-8 -*-
+"""
+.. module:: MyCapytain.retrievers.protoypes
+   :synopsis: Prototypes of APIs endpoint
+
+.. moduleauthor:: Thibault Clérice <leponteineptique@gmail.com>
+
+
+"""
+
+
+class API(object):
+    """
+    API Prototype object
+
+    :param self: Object
+    :type self: API
+    :param endpoint: URL of the API
+    :type endpoint: text
+
+    :ivar endpoint: Url of the endpoint
+    """
+
+    def __init__(self, endpoint):
+        """ Instantiate an API class
+        """
+        self.endpoint = endpoint
+
+
+class CitableTextServiceRetriever(API):
+    """ Citable Text Service retrievers should have at least have some of the following properties
+
+    """
+
+    def getMetadata(self, objectId=None, **filters):
+        """ Request metadata about a text or a collection
+
+        :param objectId: Text Identifier
+        :param filters: Kwargs parameters. URN and Inv are available
+        :return: Metadata of text from an API or the likes as bytes
+        """
+        raise NotImplementedError
+
+    def getText(self, textId, reference=None, prevnext=False, metadata=False):
+        """ Retrieve a text node from the API
+
+        :param textId: Text Identifier
+        :param reference: Passage Reference
+        :param prevnext: Retrieve graph representing previous and next passage
+        :param metadata: Retrieve metadata about the passage and the text
+        :return: Text of a Passage from an API or the likes as bytes
+        """
+        raise NotImplementedError
+
+    def getSiblings(self, textId, reference):
+        """ Retrieve the siblings of a textual node
+
+        :param textId: Text Identifier
+        :param reference: Passage Reference
+        :return: Siblings references from an API or the likes as bytes
+        """
+        raise NotImplementedError
+
+    def getChildren(self, textId, reference=None, depth=None):
+        """ Retrieve the siblings of a textual node
+
+        :param textId: Text Identifier
+        :param reference: Passage Reference
+        :return: Children references from an API or the likes as bytes
+        """
+        raise NotImplementedError
+
+
+class CTS(CitableTextServiceRetriever):
+    """ CTS API Endpoint Prototype
+    """
+    def getCapabilities(self, inventory):
+        """ Retrieve the inventory information of an API
+
+        :param inventory: Name of the inventory
+        :type inventory: text
+        :rtype: str
+        """
+        raise NotImplementedError()
+
+    def getValidReff(self, urn, inventory, level=1):
+        """ Retrieve valid urn-references for a text
+
+        :param urn: URN identifying the text
+        :type urn: text
+        :param inventory: Name of the inventory
+        :type inventory: text
+        :param level: Depth of references expected
+        :type level: int
+        :rtype: str
+        """
+        raise NotImplementedError()
+
+    def getFirstUrn(self, urn, inventory):
+        """ Retrieve the first passage urn of a text
+
+        :param urn: URN identifying the text
+        :type urn: text
+        :param inventory: Name of the inventory
+        :type inventory: text
+        :rtype: str
+        """
+        raise NotImplementedError()
+
+    def getPrevNextUrn(self, urn, inventory):
+        """ Retrieve the previous and next passage urn of one passage
+
+        :param urn: URN identifying the text's passage (Minimum depth : 1)
+        :type urn: text
+        :param inventory: Name of the inventory
+        :type inventory: text
+        :rtype: str
+        """
+        raise NotImplementedError()
+
+    def getLabel(self, urn, inventory):
+        """ Retrieve informations about a CTS Urn
+
+        :param urn: URN identifying the text's passage (Minimum depth : 1)
+        :type urn: text
+        :param inventory: Name of the inventory
+        :type inventory: text
+        :rtype: str
+        """
+        raise NotImplementedError()
+
+    def getPassage(self, urn, inventory, context=None):
+        """ Retrieve a passage
+
+        :param urn: URN identifying the text's passage (Minimum depth : 1)
+        :type urn: text
+        :param inventory: Name of the inventory
+        :type inventory: text
+        :param context: Number of citation units at the same level of the citation hierarchy as the requested urn, \
+         immediately preceding and immediately following the requested urn to include in the reply
+        :type context: int
+        :rtype: str
+        """
+        raise NotImplementedError()
+
+    def getPassagePlus(self, urn, inventory, context=None):
+        """ Retrieve a passage and informations about it
+
+        :param urn: URN identifying the text's passage (Minimum depth : 1)
+        :type urn: text
+        :param inventory: Name of the inventory
+        :type inventory: text
+        :param context: Number of citation units at the same level of the citation hierarchy as the requested urn,\
+         immediately preceding and immediately following the requested urn to include in the reply
+        :type context: int
+        :rtype: str
+        """
+        raise NotImplementedError()
---
+++ packages/MyCapytain/MyCapytain-2.0.0b1.tar.gz/MyCapytain-2.0.0b1/README.rst
@@ -0,0 +1,39 @@
+.. image:: https://travis-ci.org/Capitains/MyCapytain.svg?branch=master 
+   :target: https://travis-ci.org/Capitains/MyCapytain
+.. image:: https://coveralls.io/repos/Capitains/MyCapytain/badge.svg?branch=master 
+   :target: https://coveralls.io/r/Capitains/MyCapytain?branch=master
+.. image:: https://gemnasium.com/Capitains/MyCapytain.svg 
+   :target: https://gemnasium.com/Capitains/MyCapytain
+.. image:: https://badge.fury.io/py/MyCapytain.svg 
+   :target: http://badge.fury.io/py/MyCapytain
+.. image:: https://readthedocs.org/projects/mycapytain/badge/?version=latest
+   :target: http://mycapytain.readthedocs.org
+.. image:: https://zenodo.org/badge/3923/Capitains/MyCapytain.svg
+   :target: https://zenodo.org/badge/latestdoi/3923/Capitains/MyCapytain
+.. image:: https://img.shields.io/pypi/dm/MyCapytain.svg
+   :target: https://pypi.python.org/pypi/MyCapytain
+.. image:: https://api.codacy.com/project/badge/grade/8e63e69a94274422865e4f275dbf08ea
+   :target: https://www.codacy.com/app/leponteineptique/MyCapytain
+
+MyCapytain is a python package which provides a large set of tools to deal with CTS and Capitains Guidelines in Python.
+
+Installation and Requirements
+#############################
+
+The best way to install MyCapytain is to use pip. MyCapytain tries to support Python over 3.4.
+
+The work needed for supporting Python 2.7 is mostly done, however, since 2.0.0, we are giving up on ensuring that \
+MyCapytain will be compatible with Python < 3 while accepting PR which would help doing so.
+
+.. code-block:: shell
+
+   pip install MyCapytain
+
+If you prefer to use setup.py, you should clone and use the following
+
+.. code-block:: shell
+
+   git clone https://github.com/Capitains/MyCapytain.git
+   cd MyCapytain
+   python setup.py install
+

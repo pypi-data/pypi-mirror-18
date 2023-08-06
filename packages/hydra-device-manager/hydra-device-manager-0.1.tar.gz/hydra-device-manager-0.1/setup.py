3f6eb67d7fa375afd12d5f6297edb74d0e2e5804
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:45:20 +0000

    Add package hydra-device-manager/0.1/hydra-device-manager-0.1.tar.gz

---
+++ packages/hydra-device-manager/hydra-device-manager-0.1.tar.gz/hydra-device-manager-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='hydra-device-manager',
+      version='0.1',
+      description='The funniest joke in the world',
+      url='http://github.com/lordoftheflies/hydra-device-manager',
+      author='lordoftheflies',
+      author_email='heglas11@gmail.com',
+      license='MIT',
+      packages=['devicemanager'],
+      zip_safe=False)

28e2e2a6e6192f0a45708784659b9f340c5f3498
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:20:15 +0000

    Add package framewalker/0.1/framewalker-0.1.tar.gz

---
+++ packages/framewalker/framewalker-0.1.tar.gz/framewalker-0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='framewalker',
+    version='0.1',
+    url='https://github.com/stefanbie/FrameWalker',
+    install_requires=[
+        'peewee'
+    ],
+    )

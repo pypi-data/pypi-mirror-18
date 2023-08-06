b9a3aa809f355c0bd43ec597d91fc5ff1f9494b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:13:21 +0000

    Add package yanotme_nester/1.2.0/yanotme_nester-1.2.0.tar.gz

---
+++ packages/yanotme_nester/yanotme_nester-1.2.0.tar.gz/yanotme_nester-1.2.0/yanotme_nester.py
@@ -0,0 +1,10 @@
+"""this is the standard way to include a mutilple-line comment in your code."""
+def print_lol(the_list,level):
+    """this is a locale param ,called "the_list" ,this can be any python list."""
+    for item_list in the_list:
+        if isinstance(item_list,list):
+            print_lol(item_list,level+1)
+        else:
+            for num in range(level):
+                print("\t",end='')
+            print(item_list);

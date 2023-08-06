71f476dcce9ba96aa7212ecc9d802dfe49ef150b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:21:06 +0000

    Add package workflows/0.16/workflows-0.16.tar.gz

---
+++ packages/workflows/workflows-0.16.tar.gz/workflows-0.16/workflows/services/sample_producer.py
@@ -0,0 +1,36 @@
+from __future__ import absolute_import, division
+from workflows.services.common_service import CommonService
+import logging
+import time
+
+class SampleProducer(CommonService):
+  '''An example service building on top of the workflow.services architecture,
+     demonstrating how this architecture can be used.
+     This service generates messages into a queue.'''
+
+  # Human readable service name
+  _service_name = "Message Producer"
+
+  counter = 0
+
+  def initializing(self):
+    '''Service initialization. This function is run before any commands are
+       received from the frontend. This is the place to request channel
+       subscriptions with the messaging layer, and register callbacks.
+       This function can be overridden by specific service implementations.'''
+    self._register_idle(1, self.create_message)
+    self.log = logging.getLogger(self.__module__ + ':' + __name__)
+    self.log.setLevel(logging.DEBUG)
+    print "Starting"
+    self.log.debug('Initializing')
+
+  def create_message(self):
+    '''Create and send a unique message for this service.'''
+    self.counter += 1
+    print "Sending message #%d" % self.counter
+    self.log.critical('Ping %d', self.counter)
+    self._transport.send("transient.destination",
+                         "Message #%d\n++++++++Produced@ %f" % (
+                           self.counter,
+                           (time.time() % 1000) * 1000
+                         ))

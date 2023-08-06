ce88a28a8262b7dc524483410306bb05180f63fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:17:29 +0000

    Add package Carousel/0.3.1a1/Carousel-0.3.1a1.tar.gz

---
+++ packages/Carousel/Carousel-0.3.1a1.tar.gz/Carousel-0.3.1a1/carousel/__init__.py
@@ -0,0 +1,13 @@
+"""
+Carousel Python Model Simulation Framework
+
+Mark Mikofski (c) 2015
+"""
+
+from carousel.release_robot import get_current_version
+
+__author__ = u'Mark Mikofski'
+__email__ = u'mark.mikofski@sunpowercorp.com'
+__url__ = u'https://github.com/SunPower/Carousel'
+__version__ = get_current_version()
+__release__ = u'Caramel Corn'

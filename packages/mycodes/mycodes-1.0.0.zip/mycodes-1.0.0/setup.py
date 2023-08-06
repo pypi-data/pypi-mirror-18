c700e01b7ca4c9e466f3d16b1b667cd08d674f7f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:10:27 +0000

    Add package mycodes/1.0.0/mycodes-1.0.0.zip

---
+++ packages/mycodes/mycodes-1.0.0.zip/mycodes-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup  # 从发布工具导入setup函数
+
+setup(
+    name='mycodes',
+    version='1.0.0',
+    py_modules=['nester'],
+    author='sbc',
+    author_email='15271909262@163.com',
+    url='http://www.headfirstlabs.com',
+    description='A simple printer of nested lists',
+)

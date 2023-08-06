6fd0b8c3948c491538e7c1aec8cd34abff727387
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:20:29 +0000

    Add package simplenester/1.3.0/simplenester-1.3.0.tar.gz

---
+++ packages/simplenester/simplenester-1.3.0.tar.gz/simplenester-1.3.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name = 'simplenester',
+    version = '1.3.0',
+    py_modules = ['simplenester'],
+    author = 'doni setyawan',
+    author_email = 'donnisoft@gmail.com',
+    url = 'http://www.donnisoft.com',
+    description = 'A simple printer of nested lists'
+    )    

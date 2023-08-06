f05719af516a01cc24be84bb951a14783a39b627
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:20:14 +0000

    Add package objdict/0.3.6/ObjDict-0.3.6.tar.gz

---
+++ packages/objdict/ObjDict-0.3.6.tar.gz/ObjDict-0.3.6/objdict/__init__.py
@@ -0,0 +1,6 @@
+from objdict.objDict import dumps, loads, from_json, to_json
+from objdict.objDict import ClassRegistry, JsonEncoder, ObjDict
+from objdict.dualUrlJson import combiParse,unParse
+from objdict.fred import fred2
+from objdict import objDict as objDicter
+#extra line

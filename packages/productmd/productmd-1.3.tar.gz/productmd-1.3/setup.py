bea4a3baf8125de65ee6a71ddabf7d329bd66216
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:45:38 +0000

    Add package productmd/1.3/productmd-1.3.tar.gz

---
+++ packages/productmd/productmd-1.3.tar.gz/productmd-1.3/setup.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+
+import os
+
+import distutils.command.sdist
+from setuptools import setup
+
+
+# override default tarball format with bzip2
+distutils.command.sdist.sdist.default_format = {"posix": "bztar"}
+
+
+# recursively scan for python modules to be included
+package_root_dirs = ["productmd"]
+packages = set()
+for package_root_dir in package_root_dirs:
+    for root, dirs, files in os.walk(package_root_dir):
+        if "__init__.py" in files:
+            packages.add(root.replace("/", "."))
+packages = sorted(packages)
+
+# Manage dependencies in requirements.txt
+with open('requirements.txt') as f:
+    reqs = f.read().splitlines()
+
+
+setup(
+    name            = "productmd",
+    version         = "1.3",
+    description     = "Product, compose and installation media metadata library",
+    url             = "https://github.com/release-engineering/productmd",
+    author          = "Daniel Mach",
+    author_email    = "dmach@redhat.com",
+    license         = "LGPLv2.1",
+
+    packages        = packages,
+    scripts         = [],
+    test_suite      = "tests",
+    install_requires=reqs,
+)

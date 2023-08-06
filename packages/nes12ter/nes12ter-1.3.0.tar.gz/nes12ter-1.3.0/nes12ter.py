04e0d61d15c4c052f2592dcde1411b748c533507
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:21:11 +0000

    Add package nes12ter/1.3.0/nes12ter-1.3.0.tar.gz

---
+++ packages/nes12ter/nes12ter-1.3.0.tar.gz/nes12ter-1.3.0/nes12ter.py
@@ -0,0 +1,12 @@
+"""This is nester.py includes a function to print list in list,
+    use "for" to repeat"""
+def print_lol(the_list,intent=False,level):
+    """print the data,every element gets a line"""
+        for each_item in the_list:
+            if isinstance(each_item,list):
+                print_lol(each_item,intent,level+1)
+                else:
+                    if intent:
+                        for tab_stop in range(level):
+                            print("\t" *level,end='')
+                        print(each_item)

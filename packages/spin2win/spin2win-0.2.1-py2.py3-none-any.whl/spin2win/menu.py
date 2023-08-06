06327fc80000bfff4036c2ce7b853f3514475461
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:45:18 +0000

    Add package spin2win/0.2.1/spin2win-0.2.1-py2.py3-none-any.whl

---
+++ packages/spin2win/spin2win-0.2.1-py2.py3-none-any.whl/spin2win/menu.py
@@ -0,0 +1,73 @@
+import spin2win.game
+import pygame
+import time
+import random
+from spin2win.arena import Arena
+from spin2win.text import text_objects
+
+pygame.init()
+display_width = 800
+display_height = 600
+ 
+#color
+black = (0,0,0)
+white = (255,255,255)
+red = (200,0,0)
+green = (0, 200, 0)
+bright_red = (255,0,0)
+bright_green = (0,255,0)
+ 
+block_color = (53,115,255)
+ 
+gameDisplay = pygame.display.set_mode((display_width,display_height))
+pygame.display.set_caption('Spin2Win!')
+clock = pygame.time.Clock()
+    
+
+def game_intro():     
+    intro = True
+
+    while intro:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                quit()
+                
+        gameDisplay.fill(white)
+        largeText = pygame.font.SysFont("comicsansms",115)
+        TextSurf, TextRect = text_objects("Spin2Win", largeText, black)
+        TextRect.center = ((display_width/2),(display_height/2))
+        gameDisplay.blit(TextSurf, TextRect)
+
+        button("Começar",150,450,100,50,green,bright_green, game_loop)
+        button("Sair",550,450,100,50,red,bright_red,quitgame)
+   
+        pygame.display.update()
+        clock.tick(15)
+		
+def button(msg,x,y,w,h,ic,ac, action=None):
+    mouse = pygame.mouse.get_pos()
+    click = pygame.mouse.get_pressed()
+    if x+w > mouse[0] > x and y+h > mouse[1] > y:
+        pygame.draw.rect(gameDisplay, ac,(x,y,w,h))
+
+        if click[0] == 1 and action != None:
+            action()         
+    else:
+        pygame.draw.rect(gameDisplay, ic,(x,y,w,h))
+
+    smallText = pygame.font.SysFont("comicsansms",20)
+    textSurf, textRect = text_objects(msg, smallText, black)
+    textRect.center = ( (x+(w/2)), (y+(h/2)) )
+    gameDisplay.blit(textSurf, textRect)
+	
+def game_loop():
+    spin2win.game.run()
+
+def quitgame():
+	pygame.quit()
+
+game_intro()
+game_loop()
+
+quit()

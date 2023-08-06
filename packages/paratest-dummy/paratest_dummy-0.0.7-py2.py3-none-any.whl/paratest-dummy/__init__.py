a609f566af367c96d922a377688e1a1bfe51b69e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:45 +0000

    Add package paratest-dummy/0.0.7/paratest_dummy-0.0.7-py2.py3-none-any.whl

---
+++ packages/paratest-dummy/paratest_dummy-0.0.7-py2.py3-none-any.whl/paratest-dummy/__init__.py
@@ -0,0 +1,4 @@
+def find(self, path, test_pattern, file_pattern, output_path):
+    items = {'foo': 'echo foo', 'bar': 'echo bar', 'bazz': 'echo bazz'}
+    for item in items.items():
+        yield item

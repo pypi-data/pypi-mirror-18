96394766861445892e40271a2cab2006eb5a857d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:02 +0000

    Add package news/1.0/news-1.0.zip

---
+++ packages/news/news-1.0.zip/news-1.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(
+	name 			= 'news',
+	version 		= '1.0',
+	py_modules  	= ['news_module'],
+	author			= 'sang',
+	author_email	= '1975001828@qq.com',
+	url				= '',
+	description		= 'my first python module',
+)

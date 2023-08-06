f20ca70ee8c3d84cb20e145fd05eb9b0abce096c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:45:59 +0000

    Add package copycheck/1.0.0/copycheck-1.0.0-py3-none-any.whl

---
+++ packages/copycheck/copycheck-1.0.0-py3-none-any.whl/copycheck/__about__.py
@@ -0,0 +1,24 @@
+# Copyright 2016 Janus Friis Nielsen. All rights reserved.
+
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+'''
+   Information about the package.
+'''
+
+__title__ = 'copycheck'
+__description__ = 'Check if files have a proper copyright header.'
+__url__ = 'https://github.com/aicis/copycheck'
+__version_info__ = ('1', '0', '0')
+__version__ = '.'.join(__version_info__)
+__author__ = 'Janus Friis Nielsen'
+__author_email__ = 'jfn@fagidiot.dk'
+__maintainer__ = 'Janus Friis Nielsen'
+__maintainer_email__ = 'jfn@sepior.com'
+__created__ = "2016-12-07"
+__updated__ = "2016-12-07"
+__license__ = 'MPL 2.0'
+__copyright__ = 'Copyright 2016 Janus Friis Nielsen'
+__keywords__ = 'Copyright check'

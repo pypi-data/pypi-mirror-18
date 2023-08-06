87aa41ab6c91b2424922b3fedee5ac95a856f835
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:45:59 +0000

    Add package raining-man/0.1.50/raining-man-0.1.50.tar.gz

---
+++ packages/raining-man/raining-man-0.1.50.tar.gz/raining-man-0.1.50/src/raining_man/raining_man/world.py
@@ -0,0 +1,14 @@
+"""
+	NOT USED YET.
+"""
+
+from FGAme import *
+import pygame
+from pygame.locals import *
+
+class World:
+
+	def __init__(self):
+		self.main_character = list(objects)
+		self.list_objects = list(objects)
+		self.gravity = world.gravity(0, 30)

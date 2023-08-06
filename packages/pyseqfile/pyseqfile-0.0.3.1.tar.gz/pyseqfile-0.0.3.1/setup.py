6bb1833b8b49c1bfd895c3e4e4dc2c7fa696afa1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:22:10 +0000

    Add package pyseqfile/0.0.3.1/pyseqfile-0.0.3.1.tar.gz

---
+++ packages/pyseqfile/pyseqfile-0.0.3.1.tar.gz/pyseqfile-0.0.3.1/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup
+from setuptools import Extension
+from Cython.Build import cythonize
+from distutils.command.sdist import sdist as _sdist
+
+cmdclass = {}
+
+
+class sdist(_sdist):
+
+    def run(self):
+        # Make sure the compiled Cython files in the distribution are
+        # up-to-date
+        from Cython.Build import cythonize
+        cythonize(['pyseqfile/seqfile.pyx'])
+        _sdist.run(self)
+cmdclass['sdist'] = sdist
+
+setup(
+    name='pyseqfile',
+    version='0.0.3.1',
+    packages=[
+        'pyseqfile'
+    ],
+    license='MIT',
+    url='http://github.com/phelimb/remcdbg',
+    description='Pyseqfile is a python wrapper on top of [seq_file](https://github.com/noporpoise/seq_file) which allow for reading multiple sequence file formats.',
+    ext_modules=cythonize(
+        [Extension("pyseqfile/seqfile", ["pyseqfile/seqfile.pyx"])]),
+    install_requires=[
+        'cython'],
+    cmdclass=cmdclass
+
+)

0b89921ac6818620b932c79e958eb0bc1c7a4c1f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:21:59 +0000

    Add package httplog/1.1.3/httplog-1.1.3.zip

---
+++ packages/httplog/httplog-1.1.3.zip/httplog-1.1.3/httplog/middleware.py
@@ -0,0 +1,71 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import
+
+import json
+
+from django.conf import settings
+from django.utils.functional import SimpleLazyObject
+from importlib import import_module
+
+from constance import config
+
+from .services import get_httplog_data, save_httplog_data, get_es_log_data, get_user, \
+    get_response_data, save_response_data, get_request_user
+from .tasks import task_http_log
+
+
+class HttpLogExtractMiddleware(object):
+
+    def __init__(self):
+        self.engine = import_module(settings.SESSION_ENGINE)
+
+    def __call__(self):
+        self.http_log = None
+        self.http_log_data = None
+
+    def set_session(self, request):
+        session_key = request.COOKIES.get(settings.SESSION_COOKIE_NAME)
+        request.session = self.engine.SessionStore(session_key)
+        request.user = SimpleLazyObject(lambda: get_user(request))
+
+    def is_valid_method(self, request):
+        return request.method in self.http_log_config.get('METHODS', [])
+
+    def is_valid_url(self, request):
+        return request.path in self.http_log_config.get('PERMITTED_URLS', [])
+
+    def is_valid_user(self, request):
+        return hasattr(request, 'user') and request.user.id
+
+    def is_valid_config(self):
+        return hasattr(settings, 'HTTP_LOG_CONFIG')
+
+    def is_log_to_db(self):
+        return self.http_log_config.get('LOG_TO_DB', False)
+
+    def is_log_to_es(self):
+        return self.http_log_config.get('LOG_TO_ES', False)
+
+    def process_request(self, request):
+        self.set_session(request)
+        self.http_log_config = json.loads(config.HTTP_LOG_CONFIG)
+
+        if self.is_valid_method(request) and self.is_valid_url(request) and self.is_valid_user(request):
+            http_log_data = get_httplog_data(request)
+            self.http_log_data = http_log_data
+
+            if self.is_valid_config() and self.is_log_to_db():
+                self.http_log = save_httplog_data(self.http_log_data, request)
+
+    def process_response(self, request, response):
+        if self.is_valid_method(request) and self.is_valid_url(request) and self.is_valid_user(request):
+            response_data = get_response_data(response)
+
+            if self.is_valid_config() and self.is_log_to_db() and getattr(self, 'http_log', None):
+                save_response_data(self.http_log, response_data)
+
+            if self.is_valid_config() and self.is_log_to_es():
+                es_log_data = get_es_log_data(self.http_log_data, response_data, get_request_user(request))
+                task_http_log(es_log_data)
+
+        return response
---
+++ packages/httplog/httplog-1.1.3.zip/httplog-1.1.3/httplog/models/httplog.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import
+
+from django.contrib.auth.models import User
+from django.db import models
+from jsonfield import JSONField
+
+
+class HttpLog(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True, null=True)
+    updated_at = models.DateTimeField(auto_now=True, null=True)
+    client = JSONField(default={}, null=True, blank=True)
+    server = JSONField(default={}, null=True, blank=True)
+    request = JSONField(default={}, null=True, blank=True)
+    response = JSONField(default={}, null=True, blank=True)
+    name = models.CharField(max_length=256, null=True, blank=True)
+    user = models.ForeignKey(
+        User, related_name='httplogs', db_constraint=False, null=True)
+
+    class Meta:
+        db_table = 'httplog'

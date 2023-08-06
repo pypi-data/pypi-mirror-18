ec347edb1bb7acb398342517bec9ea45d0b88103
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:11:50 +0000

    Add package spintax/1.0/spintax-1.0.zip

---
+++ packages/spintax/spintax-1.0.zip/spintax-1.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='spintax',
+      version='1.0',
+      description='A python module for parsing spintax',
+      url='http://github.com/AceLewis/spintax',
+      author='AceLewis',
+      author_email='AceLewis@users.noreply.github.com',
+      license='GPLv3',
+      packages=['spintax'],
+      zip_safe=False)

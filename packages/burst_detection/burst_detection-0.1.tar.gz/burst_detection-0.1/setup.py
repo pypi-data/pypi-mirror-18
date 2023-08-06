6992206f593292692c145ee5d836845248c75cdb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:32:32 +0000

    Add package burst_detection/0.1/burst_detection-0.1.tar.gz

---
+++ packages/burst_detection/burst_detection-0.1.tar.gz/burst_detection-0.1/setup.py
@@ -0,0 +1,12 @@
+
+from setuptools import setup
+
+setup(name='burst_detection',
+      version='0.1',
+      description='Detect bursts in batched data using Kleinberg''s (2002) algorithm.',
+      url='http://github.com/nmarinsek/burst_detection',
+      author='Nikki Marinsek',
+      author_email='nikki.marinsek@gmail.com',
+      license='',
+      packages=['burst_detection'],
+      zip_safe=False)

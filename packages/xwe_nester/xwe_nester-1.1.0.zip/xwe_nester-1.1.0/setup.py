556473b3602dd495b5675dec72717e0dac9daaff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:45:08 +0000

    Add package xwe_nester/1.1.0/xwe_nester-1.1.0.zip

---
+++ packages/xwe_nester/xwe_nester-1.1.0.zip/xwe_nester-1.1.0/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup( 
+        name         = 'xwe_nester', 
+        version      = '1.1.0', 
+        ## TODO: be sure to change these next few lines to match your details!
+        py_modules   = ['nester'],
+        author       = 'xwePython',
+        author_email = 'wei4android@gmail.com',
+        url          = 'http://www.headfirstlabs.com',
+        description  = 'A simple printer of nested lists',
+     )

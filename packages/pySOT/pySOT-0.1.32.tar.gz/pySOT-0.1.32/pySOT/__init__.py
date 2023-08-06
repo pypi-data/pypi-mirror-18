8c22fe0ad9d610fdba2b11d403b066a33166ee9e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:14:26 +0000

    Add package pySOT/0.1.32/pySOT-0.1.32.tar.gz

---
+++ packages/pySOT/pySOT-0.1.32.tar.gz/pySOT-0.1.32/pySOT/__init__.py
@@ -0,0 +1,24 @@
+try:
+    from StringIO import StringIO
+except ImportError:
+    from io import StringIO
+
+from pySOT.experimental_design import *
+from pySOT.rbf import *
+from pySOT.rs_wrappers import RSCapped, RSUnitbox
+from pySOT.adaptive_sampling import *
+from pySOT.test_problems import *
+from pySOT.sot_sync_strategies import *
+from pySOT.ensemble_surrogate import EnsembleSurrogate
+from pySOT.poly_regression import *
+from pySOT.merit_functions import *
+from pySOT.utils import *
+
+try:
+    from pySOT.mars_interpolant import MARSInterpolant
+    __with_mars__ = True
+except ImportError as err:
+    __with_mars__ = False
+
+__version__ = '0.1.32'
+__author__ = 'David Eriksson, David Bindel, Christine Shoemaker'

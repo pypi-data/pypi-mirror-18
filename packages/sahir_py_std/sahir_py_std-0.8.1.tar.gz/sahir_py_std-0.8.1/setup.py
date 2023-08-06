3e2009b6b8f18b47ad857530513d5c59c445b8eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:51:09 +0000

    Add package sahir_py_std/0.8.1/sahir_py_std-0.8.1.tar.gz

---
+++ packages/sahir_py_std/sahir_py_std-0.8.1.tar.gz/sahir_py_std-0.8.1/setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name = "sahir_py_std",
+    version = "0.8.1",
+    packages=['sahirPyStd'],
+    license='Creative Commons Attribution-Noncommercial-Share Alike license',
+    long_description=open('README.txt').read()
+)

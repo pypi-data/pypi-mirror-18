b7816b0a713c69b2ec0e9859813854690b2d842a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:32:22 +0000

    Add package fortigate-config-parser/0.0.6/fortigate_config_parser-0.0.6-py2.py3-none-any.whl

---
+++ packages/fortigate-config-parser/fortigate_config_parser-0.0.6-py2.py3-none-any.whl/fortigate_parser/main.py
@@ -0,0 +1,32 @@
+"""A temporary easy startpoint."""
+import os, sys
+
+from parser import ConfigParser
+
+try:
+    filename = sys.argv[1]
+except:
+    print("No filename specified, using default example.conf")
+    filename = os.path.join('tests', 'unit_tests', 'fortigate_parser', 'config_examples', 'example.conf')
+
+with open(filename) as f:
+    config = f.read()
+
+config_parser = ConfigParser(config)
+config_parser.parse()
+
+# print config_parser.global_section.config
+# print config_parser.vdom_sections['root']
+
+config_parser.vdom_sections['root'].parse()
+print config_parser.vdom_sections['root'].name
+config_parser.vdom_sections['root'].system_zone_section.parse()
+print config_parser.vdom_sections['root'].system_zone_section.zones
+
+# Stats
+print('\n\nSTATS\n------------------------------')
+print('Lines in global section: {}'.format(len(config_parser.global_section.config_lines())))
+print('Vdoms in vdom sections: {}'.format(len(config_parser.vdom_sections)))
+
+
+
---
+++ packages/fortigate-config-parser/fortigate_config_parser-0.0.6-py2.py3-none-any.whl/fortigate_parser/test.py
@@ -0,0 +1,33 @@
+import os, sys
+
+from parser import ConfigParser
+
+try:
+    filename = sys.argv[1]
+except:
+    print("No filename specified, using default example.conf")
+    filename = os.path.join('..', 'config_examples', 'example.conf')
+
+
+with open(filename) as f:
+    config = f.read()
+
+config_parser = ConfigParser(config)
+config_parser.parse()
+
+# print config_parser.global_section.config
+# print config_parser.vdom_sections['root']
+
+config_parser.vdom_sections['root'].parse()
+print config_parser.vdom_sections['root'].name
+config_parser.vdom_sections['root'].system_zone_section.parse()
+print config_parser.vdom_sections['root'].system_zone_section.zones
+
+
+# Stats
+print('\n\nSTATS\n------------------------------')
+print('Lines in global section: {}'.format(len(config_parser.global_section.config_lines())))
+print('Vdoms in vdom sectiosn: {}'.format(len(config_parser.vdom_sections)))
+
+
+

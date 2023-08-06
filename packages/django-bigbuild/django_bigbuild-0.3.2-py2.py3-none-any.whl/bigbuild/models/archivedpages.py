3abe0a6144e24aecc473a768e52721e5c5bc9bbb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:45:01 +0000

    Add package django-bigbuild/0.3.2/django_bigbuild-0.3.2-py2.py3-none-any.whl

---
+++ packages/django-bigbuild/django_bigbuild-0.3.2-py2.py3-none-any.whl/bigbuild/models/archivedpages.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import os
+from bigbuild.models import BasePage
+
+
+class ArchivedPage(BasePage):
+    """
+    An archived custom page.
+    """
+    @property
+    def frontmatter_path(self):
+        """
+        Returns the metadata.md path where this page will be configured.
+        """
+        return os.path.join(self.archive_static_directory_path, 'metadata.md')

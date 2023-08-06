1280f97b50d2946ec46905ade4ab89553e22927f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:10:11 +0000

    Add package messente-python/1.2.3/messente-python-1.2.3.tar.gz

---
+++ packages/messente-python/messente-python-1.2.3.tar.gz/messente-python-1.2.3/setup.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+
+setup(
+    name="messente-python",
+    version="1.2.3",
+    packages=["messente", "messente.api"],
+    setup_requires=["future", "six", "requests"],
+    install_requires=["future", "six", "requests"],
+    author="Messente.com",
+    author_email="support@messente.com",
+    description="Official Messente.com API library",
+    license="Apache License, Version 2",
+    keywords="messente sms verification 2FA pincode",
+    url="http://messente.com/documentation/",
+    test_suite="test",
+)

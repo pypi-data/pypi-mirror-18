1e281b5653da7235afd799e5133a46d088d777ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:32:36 +0000

    Add package windMailer/0.2.1/windMailer-0.2.1.tar.gz

---
+++ packages/windMailer/windMailer-0.2.1.tar.gz/windMailer-0.2.1/setup.py
@@ -0,0 +1,43 @@
+from setuptools import setup
+
+setup(
+    # Application name:
+    name="windMailer",
+
+    # Version number (initial):
+    version="0.2.1",
+
+    # Application author details:
+    author="dukeman",
+    author_email="gabeduke@gmail.com",
+
+    # Packages
+    packages=["app"],
+
+    scripts=['bin/windMailer'],
+
+    # Include additional files into the package
+    include_package_data=True,
+
+    # Details
+     url="http://pypi.python.org/pypi/windMailer_v021/",
+
+    #
+    # license="LICENSE.txt",
+    description="Useful towel-related stuff. ",
+
+    # entry_points={
+    #     'console_scripts': [
+    #         'windMailer = app.__main__:run'
+    #     ]
+    # },
+
+    # long_description=open("README.txt").read(),
+
+    # Dependent packages (distributions)
+    install_requires=[
+        "slackweb", "python-forecastio", "ConfigParser"
+
+    ],
+
+)

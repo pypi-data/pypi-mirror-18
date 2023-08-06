82fe6d59f0c141becda10586fa6e5f53ebe8ed21
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:43:53 +0000

    Add package aserve/0.0.11/aserve-0.0.11.tar.gz

---
+++ packages/aserve/aserve-0.0.11.tar.gz/aserve-0.0.11/aserve/__init__.py
@@ -0,0 +1,6 @@
+""" remember - Catchy catchphrase """
+
+__project__ = "aserve"
+__version__ = "0.0.11"
+
+from aserve.aserve import main
---
+++ packages/aserve/aserve-0.0.11.tar.gz/aserve-0.0.11/setup.py
@@ -0,0 +1,43 @@
+from setuptools import find_packages
+from setuptools import setup
+
+MAJOR_VERSION = '0'
+MINOR_VERSION = '0'
+MICRO_VERSION = '11'
+VERSION = "{}.{}.{}".format(MAJOR_VERSION, MINOR_VERSION, MICRO_VERSION)
+
+setup(name='aserve',
+      version=VERSION,
+      description="Asynchronously serve APIs",
+      url='https://github.com/kootenpv/aserve',
+      author='Pascal van Kooten',
+      author_email='kootenpv@gmail.com',
+      license='MIT',
+      packages=find_packages(),
+      install_requires=[
+          'aiohttp',
+      ],
+      entry_points={
+          'console_scripts': ['aserve = aserve.aserve:main']
+      },
+
+      classifiers=[
+          'Environment :: Console',
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+          'Operating System :: Microsoft',
+          'Operating System :: MacOS :: MacOS X',
+          'Operating System :: Unix',
+          'Operating System :: POSIX',
+          'Programming Language :: Python :: 3.5',
+          'Topic :: Software Development',
+          'Topic :: Software Development :: Build Tools',
+          'Topic :: Software Development :: Debuggers',
+          'Topic :: Software Development :: Libraries',
+          'Topic :: Software Development :: Libraries :: Python Modules',
+          'Topic :: System :: Software Distribution',
+          'Topic :: System :: Systems Administration',
+          'Topic :: Utilities'
+      ],
+      zip_safe=False,
+      platforms='any')

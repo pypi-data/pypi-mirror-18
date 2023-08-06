c057ef7d49e0470109162b5c7b51b0485c84ca76
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:14:31 +0000

    Add package pygelf/0.2.8/pygelf-0.2.8.tar.gz

---
+++ packages/pygelf/pygelf-0.2.8.tar.gz/pygelf-0.2.8/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+
+setup(
+    name='pygelf',
+    version='0.2.8',
+    packages=['pygelf'],
+    description='Logging handlers with GELF support',
+    keywords='logging udp tcp ssl tls graylog2 graylog gelf',
+    author='Ivan Mukhin',
+    author_email='muhin.ivan@gmail.com',
+    url='https://github.com/keeprocking/pygelf',
+    long_description=open('README.rst').read(),
+    license='MIT'
+)

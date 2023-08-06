87cd3dd7512c166c1339723f03266d7cff66b504
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:32:58 +0000

    Add package money-to-prisoners-common/4.42.0/money_to_prisoners_common-4.42.0-py3-none-any.whl

---
+++ packages/money-to-prisoners-common/money_to_prisoners_common-4.42.0-py3-none-any.whl/mtp_common/assets/javascripts/modules/track-printing.js
@@ -0,0 +1,16 @@
+// Focus on search input field
+
+'use strict';
+
+var analytics = require('./analytics');
+
+exports.TrackPrinting = {
+  init: function () {
+    window.print = (function(printfn) {
+      return function() {
+        analytics.Analytics.send('event', 'print');
+        printfn();
+      };
+    })(window.print);
+  }
+};

932f040c4e702d19a20887747948708e6a976bae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:45:23 +0000

    Add package mpy-repl-tool/0.1/mpy-repl-tool-0.1.tar.gz

---
+++ packages/mpy-repl-tool/mpy-repl-tool-0.1.tar.gz/mpy-repl-tool-0.1/setup.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+#
+# (C) 2016 Chris Liechti <cliechti@gmx.net>
+#
+# SPDX-License-Identifier:    BSD-3-Clause
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+setup(
+    name="mpy-repl-tool",
+    #~ description="",
+    version='0.1',
+    author="Chris Liechti",
+    author_email="cliechti@gmx.net",
+    url="https://github.com/zsquareplusc/mpy-repl-tool",
+    packages=['there'],
+    license="BSD",
+    #~ long_description="""\
+#~ """,
+    #~ classifiers=[
+    #~ ],
+    platforms='any',
+    install_requires=[
+        'pyserial>=3',
+        'colorama',
+    ],
+    extras_require={
+        'mount': ['fusepy'],
+    },
+    entry_points={
+        'console_scripts': [
+            'there = there.__main__:main',
+        ],
+    },
+)
+

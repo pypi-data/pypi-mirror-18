a60e21980a6a08854fef23eb6d85c3dd8304d156
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:14:28 +0000

    Add package lim/1.2.0.dev5/lim-1.2.0.dev5.tar.gz

---
+++ packages/lim/lim-1.2.0.dev5.tar.gz/lim-1.2.0.dev5/lim/genetics/phenotype/binomial.py
@@ -0,0 +1,28 @@
+from __future__ import absolute_import, division, unicode_literals
+
+from numpy import ascontiguousarray
+
+from numpy_sugar import (is_all_equal, is_all_finite)
+
+class BinomialPhenotype(object):
+    def __init__(self, nsuccesses, ntrials):
+        self.nsuccesses = ascontiguousarray(nsuccesses, dtype=float)
+        self.ntrials = ascontiguousarray(ntrials, dtype=float)
+        self.likelihood_name = 'Binomial'
+
+        if is_all_equal(nsuccesses):
+            raise ValueError("The phenotype array has a single unique value" +
+                             " only.")
+
+        if not is_all_finite(nsuccesses):
+            raise ValueError("There are non-finite numbers in phenotype.")
+
+    @property
+    def sample_size(self):
+        return len(self.nsuccesses)
+
+    def to_normal(self):
+        y = self.nsuccesses / self.ntrials
+        y = y / y.std()
+        y -= y.mean()
+        return y

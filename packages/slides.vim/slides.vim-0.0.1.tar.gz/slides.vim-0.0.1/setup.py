18afbb5da477148ea05b3c618fae86ef0e818ed8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:32:05 +0000

    Add package slides.vim/0.0.1/slides.vim-0.0.1.tar.gz

---
+++ packages/slides.vim/slides.vim-0.0.1.tar.gz/slides.vim-0.0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+setup(
+   name='slides.vim',
+   version='0.0.1',
+   description = 'Do presentation in vim by markdown',
+   author = 'gabber12',
+   author_email = 'shubham.sha12@gmail.com',
+   scripts=['bin/vimslides']
+)

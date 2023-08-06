f7d4a00404df356e088c90e161a4bd2eb14a2c5c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:07:18 +0000

    Add package bpnotify/0.45/bpnotify-0.45.tar.gz

---
+++ packages/bpnotify/bpnotify-0.45.tar.gz/bpnotify-0.45/beproud/django/notify/models.py
@@ -0,0 +1,69 @@
+#:coding=utf8:
+
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.contenttypes import generic
+from django.utils.translation import ugettext_lazy as _
+from django.db import models
+
+import jsonfield
+
+from beproud.django.notify.api import _get_media_map
+
+__all__ = (
+    'Notification',
+    'NotifySetting',
+)
+
+
+class MediaChoices(object):
+    def __iter__(self):
+        media_map = _get_media_map()
+        return iter((name, data['verbose_name']) for name, data in media_map.iteritems())
+
+
+class NotificationManager(models.Manager):
+    def get_for_target(self, target):
+        return self.filter(
+            target_content_type=ContentType.objects.get_for_model(target),
+            target_object_id=target.pk,
+        )
+
+
+class Notification(models.Model):
+    target_content_type = models.ForeignKey(ContentType, verbose_name=_('content type id'),
+                                            db_index=True, null=True, blank=True)
+    target_object_id = models.PositiveIntegerField(_('target id'),
+                                                   db_index=True, null=True, blank=True)
+    target = generic.GenericForeignKey('target_content_type', 'target_object_id')
+
+    notify_type = models.CharField(_('notify type'), max_length=100, db_index=True)
+    media = models.CharField(_('media'), max_length=100, choices=MediaChoices(), db_index=True)
+
+    extra_data = jsonfield.JSONField(_('extra data'), null=True, blank=True)
+
+    ctime = models.DateTimeField(_('created'), auto_now_add=True, db_index=True)
+
+    objects = NotificationManager()
+
+    def __unicode__(self):
+        return u"%s (%s, %s)" % (self.target, self.notify_type, self.media)
+
+    class Meta:
+        ordering = ('-ctime',)
+
+
+class NotifySetting(models.Model):
+    target_content_type = models.ForeignKey(ContentType, verbose_name=_('content type id'))
+    target_object_id = models.PositiveIntegerField(_('target id'))
+    target = generic.GenericForeignKey('target_content_type', 'target_object_id')
+
+    notify_type = models.CharField(_('notify type'), max_length=100, db_index=True)
+    media = models.CharField(_('media'), max_length=100, choices=MediaChoices(), db_index=True)
+    send = models.BooleanField(_('send?'), default=False)
+
+    def __unicode__(self):
+        return u"%s (%s, %s, %s)" % (self.target, self.notify_type, self.media,
+                                     self.send and 'send' or 'no send')
+
+    class Meta:
+        unique_together = ('target_content_type', 'target_object_id', 'notify_type', 'media')
---
+++ packages/bpnotify/bpnotify-0.45.tar.gz/bpnotify-0.45/setup.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+#:coding=utf-8:
+
+from setuptools import setup, find_packages
+
+setup(
+    name='bpnotify',
+    version='0.45',
+    description='Notification routing for Django',
+    author='BeProud',
+    author_email='project@beproud.jp',
+    url='https://github.com/beproud/bpnotify/',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Plugins',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+    include_package_data=True,
+    packages=find_packages(),
+    namespace_packages=['beproud', 'beproud.django'],
+    test_suite='tests.main',
+    install_requires=[
+        'Django>=1.2',
+        'django-jsonfield>=0.8.7',
+    ],
+    tests_require=[
+        'celery>=2.2.7',
+        'mock>=0.7.2',
+    ],
+    zip_safe=False,
+)

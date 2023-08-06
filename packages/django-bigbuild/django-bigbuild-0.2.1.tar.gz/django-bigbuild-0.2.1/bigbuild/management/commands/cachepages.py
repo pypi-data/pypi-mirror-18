fa659824be639f33c56d86260e3e266e9b33fc75
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:11:02 +0000

    Add package django-bigbuild/0.2.1/django-bigbuild-0.2.1.tar.gz

---
+++ packages/django-bigbuild/django-bigbuild-0.2.1.tar.gz/django-bigbuild-0.2.1/bigbuild/management/commands/cachepages.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import os
+import json
+from bigbuild.models import PageList
+from bigbuild import get_retired_directory
+from django.core.management.base import BaseCommand
+
+
+class Command(BaseCommand):
+    help = "Cache page metadata to increase the application speed"
+
+    def handle(self, *args, **options):
+        # Set the cache path for retired pages
+        retired_cache_path = os.path.join(get_retired_directory(), '.cache')
+
+        # Delete it if it already exists
+        if os.path.exists(retired_cache_path):
+            os.remove(retired_cache_path)
+
+        # Pull the live PageList from the YAML files
+        page_list = PageList()
+
+        # Save the retired pages out to a new cache
+        with open(retired_cache_path, 'wb') as f:
+            json.dump(
+                dict(retired_pages=[p.to_json() for p in page_list.retired_pages]),
+                f
+            )

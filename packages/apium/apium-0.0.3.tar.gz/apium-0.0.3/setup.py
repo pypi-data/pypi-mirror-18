7b45b8d206a844c1d876fdf649af4dadbbeb8ba3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:12:26 +0000

    Add package apium/0.0.3/apium-0.0.3.tar.gz

---
+++ packages/apium/apium-0.0.3.tar.gz/apium-0.0.3/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='apium',
+    version='0.0.3',
+    author='Christian Boelsen',
+    author_email='christianboelsen+github@gmail.com',
+    packages=find_packages(exclude=["tests"]),
+    entry_points={
+        'console_scripts': [
+            'apium-worker = apium.command:start_workers',
+            'apium-inspect = apium.command:inspect',
+        ],
+    },
+    license='LICENSE',
+    description=open('README').read(),
+    install_requires=[],
+    keywords=['celery', 'task', 'queue'],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU Lesser General Public License v3 or later (LGPLv3+)',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.5',
+    ],
+)

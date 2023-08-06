849417b28f701066b6626db77d1fef370077a9fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:10:04 +0000

    Add package plot/0.1.3/plot-0.1.3.zip

---
+++ packages/plot/plot-0.1.3.zip/plot-0.1.3/plot/canvas/subplots/create.py
@@ -0,0 +1,41 @@
+"""
+Create a figure object
+"""
+from typing import Dict
+import copy
+import matplotlib.pyplot
+from .subplot_spacing import subplot_spacing
+from .global_axis import global_axis
+from ...tk.fnTK import compose
+from ...tk.listTK import upgrade_dimension
+import numpy
+
+
+def create(params):
+    # type: (Dict) -> Dict
+    """Create a new figure object
+
+    Args:
+        params (dict): plotting parameters
+
+    Returns:
+        a parameter dict with a new field "figure"
+        which contains the new figure object
+    """
+    # axes is a numpy.ndarray
+    # axes[0,0] is an instance of
+    #   <class 'matplotlib.axes._subplots.AxesSubplot'>
+    fig, axes = matplotlib.pyplot.subplots(
+                nrows=params['global']['figure']['rows'],
+                ncols=params['global']['figure']['columns'],
+                figsize=(params['global']['figure']['width'],
+                         params['global']['figure']['height']),
+                sharex=params['global']['figure']['axis']['share']['x'],
+                sharey=params['global']['figure']['axis']['share']['y'],
+                squeeze=False
+            )
+    params['internal']['canvas']['figure'] = fig
+    params['internal']['canvas']['axes'] = numpy.array(
+        upgrade_dimension(axes.tolist(), 4))
+    tweek = compose([subplot_spacing, global_axis])
+    return tweek(params)
---
+++ packages/plot/plot-0.1.3.zip/plot-0.1.3/plot/finetune/local_axis/main.py
@@ -0,0 +1,26 @@
+"""
+Adjust the local figure axis
+"""
+from typing import Dict
+from ...tk.fnTK import compose
+from .add_grid_lines import add_grid_lines
+from .refine_legend import refine_legend
+from .tighten_panel_axis_range import tighten_panel_axis_range
+
+
+def main(params):
+    # type: (Dict) -> Dict
+    """Adjust local axis parameters
+
+    Args:
+        params (dict): plotting parameter dictionary
+
+    Returns:
+        same as input
+    """
+    aux = compose([
+        add_grid_lines,
+        refine_legend,
+        tighten_panel_axis_range
+        ])
+    return aux(params)

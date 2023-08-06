7e177b1cb157baaa97291df0b5c90a520512cfc7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:12:59 +0000

    Add package ultracup_nester/1.3.0/ultracup_nester-1.3.0.tar.gz

---
+++ packages/ultracup_nester/ultracup_nester-1.3.0.tar.gz/ultracup_nester-1.3.0/nester.py
@@ -0,0 +1,17 @@
+""" This is the nested.py module that allows to print any level
+of nested lists, if you have a list within a list within a list
+this is your module
+"""
+def print_lol (the_list,indent=False,level=0):
+    """ This function takes a positional argument called "the_list", which
+    is any Python list (of - possibly - nested lists). Each data item in the
+    provided list is (recursively) printed to the screen on it's own line.
+    And a level argument to indent the nested elements level times"""
+    for each_item in the_list:
+        if isinstance(each_item, list):
+            print_lol(each_item,indent,level+1)
+        else:
+            if indent:
+                for tab_stop in range(level):
+                    print("\t", end='')
+            print(each_item)

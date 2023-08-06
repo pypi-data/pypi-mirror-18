91d8cc6482911608c0eaa9295d4096a6a5e24bd2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:43:57 +0000

    Add package eti-django-page-cms/1.8.6/eti-django-page-cms-1.8.6.tar.gz

---
+++ packages/eti-django-page-cms/eti-django-page-cms-1.8.6.tar.gz/eti-django-page-cms-1.8.6/pages/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""Django page CMS module."""
+VERSION = (1, 8, 6)
+__version__ = '.'.join(map(str, VERSION))
+__author__ = "Thomas Saunders"
+__contact__ = "saun0063@umn.edu"
+__homepage__ = "https://github.com/localhuman/django-page-cms/"
+__docformat__ = "restructuredtext"
+__doc__ = 'A tree based Django CMS application'
+__license__ = 'BSD'

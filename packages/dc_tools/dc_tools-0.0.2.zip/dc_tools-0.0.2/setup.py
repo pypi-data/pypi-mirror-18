d3dcecf080163d6fa3e7e69a9be1a95ebd4e3935
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:09:14 +0000

    Add package dc_tools/0.0.2/dc_tools-0.0.2.zip

---
+++ packages/dc_tools/dc_tools-0.0.2.zip/dc_tools-0.0.2/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name = 'dc_tools',
+    version = '0.0.2',
+    py_modules = ['dc_tools'],
+    author = 'vaniff',
+    author_email = 'zhouyong0206@163.com',
+    description = 'dc tools',
+	url='http://pypi.python.org/pypi/dc_type/',
+    long_description=open('README.txt').read(),
+    )

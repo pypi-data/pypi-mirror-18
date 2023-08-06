925cb681d40fac5313c05728ed71c37ca17aa35d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:45:20 +0000

    Add package Lango/0.20.0/Lango-0.20.0.tar.gz

---
+++ packages/Lango/Lango-0.20.0.tar.gz/Lango-0.20.0/setup.py
@@ -0,0 +1,16 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='Lango',
+    version='0.20.0',
+    description='Natural Language Framework for Matching Parse Trees and Modeling Conversation',
+    packages=find_packages(),
+    author='Michael Young',
+    author_email='michaelyoung1995@gmail.com',
+    url='https://github.com/ayoungprogrammer/lango',
+    scripts=[],
+    install_requires=[
+        'nltk',
+        'pycorenlp'
+    ],
+)

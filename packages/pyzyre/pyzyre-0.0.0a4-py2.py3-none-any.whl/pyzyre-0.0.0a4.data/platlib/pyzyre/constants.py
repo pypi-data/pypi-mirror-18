f3b43c06c6a341b38f2d5eda5a6a6bbc926c313b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:20:25 +0000

    Add package pyzyre/0.0.0a4/pyzyre-0.0.0a4-py2.py3-none-any.whl

---
+++ packages/pyzyre/pyzyre-0.0.0a4-py2.py3-none-any.whl/pyzyre-0.0.0a4.data/platlib/pyzyre/constants.py
@@ -0,0 +1,15 @@
+import os
+import sys
+
+PYVERSION = 2
+if sys.version_info > (3,):
+    PYVERSION = 3
+
+
+ZYRE_GROUP = os.environ.get('ZYRE_GROUP', 'ZYRE')
+SERVICE_PORT = os.environ.get('ZYRE_SERVICE_PORT', '49155')
+
+GOSSIP_PORT = os.environ.get('ZYRE_SERVICE_PORT', '49154')
+GOSSIP_ENDPOINT = os.environ.get('ZYRE_GOSSIP_ENDPOINT', 'zyre.local')
+
+LOG_FORMAT = '%(asctime)s - %(levelname)s - %(name)s[%(lineno)s] - %(message)s'

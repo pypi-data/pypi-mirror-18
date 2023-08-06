00e760cf97f29bdc5e7503fc1d69a0193d62677b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:51:51 +0000

    Add package blua_nester/1.1.0/blua_nester-1.1.0.zip

---
+++ packages/blua_nester/blua_nester-1.1.0.zip/blua_nester-1.1.0/nester.py
@@ -0,0 +1,14 @@
+""" This "nester.py " module, it include print_lol() funcion
+this is my first learn python file"""
+
+def print_lol(the_list,indent=False,level=0):
+    """ function description"""
+    for each_item in the_list:
+        if isinstance(each_item, list):
+            print_lol(each_item,indent,level+1)
+        else:
+            if indent:
+                for tab_stop in range(level):
+                    print("\t",end = "")
+            print(each_item)
+            
---
+++ packages/blua_nester/blua_nester-1.1.0.zip/blua_nester-1.1.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name        = 'blua_nester',
+    version     = '1.1.0',
+    py_modules  = ['nester'],
+    author      = 'blua',
+    author_email    = ' ',
+    url             = ' ',
+    description     = 'A simple printer of nested lists, Learning',
+    )

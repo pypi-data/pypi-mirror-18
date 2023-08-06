3e0c18b2854a20a97adf9f1b41313ea2e24ad042
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:42:30 +0000

    Add package yadage/0.9.1/yadage-0.9.1.tar.gz

---
+++ packages/yadage/yadage-0.9.1.tar.gz/yadage-0.9.1/yadage/workflow_loader.py
@@ -0,0 +1,6 @@
+import capschemas
+
+
+def workflow(source, toplevel, schema_name='yadage/workflow-schema', schemadir=None, validate=True):
+    data = capschemas.load(source, toplevel, schema_name, schemadir, validate)
+    return data

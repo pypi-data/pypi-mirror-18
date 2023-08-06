2edd22f4558b1381f882ee0fe371bf5cda065226
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:20 +0000

    Add package hostlist/1.0.2/hostlist-1.0.2-py3-none-any.whl

---
+++ packages/hostlist/hostlist-1.0.2-py3-none-any.whl/hostlist/__init__.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"Sync hostlist and builds config files for services."
+
+from hostlist import buildfiles
+
+__version__ = '1.0.2'
+
+if __name__ == "__main__":
+    buildfiles.main()

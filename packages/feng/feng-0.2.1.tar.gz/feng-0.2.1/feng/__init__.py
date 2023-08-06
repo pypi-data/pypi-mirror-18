db16ce19846ce990b57c263e6e803391db950440
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:45:33 +0000

    Add package feng/0.2.1/feng-0.2.1.tar.gz

---
+++ packages/feng/feng-0.2.1.tar.gz/feng-0.2.1/feng/__init__.py
@@ -0,0 +1,5 @@
+__title__ = 'feng'
+__version__ = '0.2.1'
+__author__ = 'Alexander Svanevik, Anders Aagard, Mats Julian Olsen'
+__license__ = 'MIT'
+__copyright__ = 'Copyright 2016 Alexander Svanevik, Anders Aagard, Mats Julian Olsen'

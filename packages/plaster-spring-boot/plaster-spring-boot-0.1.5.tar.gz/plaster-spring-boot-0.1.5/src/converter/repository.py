a116a8857719b7ba4d7ef04d9aa853259393e26d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:14:29 +0000

    Add package plaster-spring-boot/0.1.5/plaster-spring-boot-0.1.5.tar.gz

---
+++ packages/plaster-spring-boot/plaster-spring-boot-0.1.5.tar.gz/plaster-spring-boot-0.1.5/src/converter/repository.py
@@ -0,0 +1,20 @@
+import pattern.text.en as pattern
+
+import src.data.settings as settings
+from src.data.types import REPOSITORY as GEN_TYPE
+
+
+def gen_package_name():
+    return settings.MAVEN_GROUP_ID + '.' + settings.RELATIVE_PACKAGES[GEN_TYPE]
+
+
+def gen_class_name(root_name):
+    return pattern.singularize(root_name) + 'Repository'
+
+
+def gen_file_path():
+    return settings.REL_PATH + settings.RELATIVE_PACKAGES[GEN_TYPE]
+
+
+def gen_file_name(root_name):
+    return gen_class_name(root_name) + '.java'

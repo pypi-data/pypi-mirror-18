7e229cfcfec9bf340a34e84b18687f0b4539982d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:30:15 +0000

    Add package rabbithole/0.2.0/rabbithole-0.2.0-py2.py3-none-any.whl

---
+++ packages/rabbithole/rabbithole-0.2.0-py2.py3-none-any.whl/rabbithole/consumer.py
@@ -0,0 +1,101 @@
+# -*- coding: utf-8 -*-
+
+"""Consumer: get messages from rabbitmq.
+
+The strategy to get messages is:
+    - connect to the rabbitmq server
+    - bind a queue to the desired exchanges
+
+Note that it's assumed that the exchanges will have `fanout` type and that the
+routing key isn't relevant in this case.
+
+"""
+
+import json
+import logging
+
+import blinker
+import pika
+
+LOGGER = logging.getLogger(__name__)
+
+
+class Consumer(object):
+
+    """Rabbitmq message consumer.
+
+    :param server: Rabbitmq server IP address
+    :type server: str
+    :param exchange_names: Exchange names to bind to
+    :type exchange_names: list(str)
+
+    """
+
+    def __init__(self, server, exchange_names):
+        """Configure exchanges and queue."""
+        LOGGER.info('Connecting to %r...', server)
+        parameters = pika.ConnectionParameters(server)
+        connection = pika.BlockingConnection(parameters)
+        channel = connection.channel()
+
+        # Use a single queue to process messages from all exchanges
+        result = channel.queue_declare(auto_delete=True)
+        queue_name = result.method.queue
+        LOGGER.debug('Declared queue %r', queue_name)
+
+        for exchange_name in exchange_names:
+            channel.exchange_declare(
+                exchange=exchange_name,
+                exchange_type='fanout',
+            )
+            channel.queue_bind(
+                exchange=exchange_name,
+                queue=queue_name,
+            )
+            LOGGER.debug(
+                'Queue %r bound to exchange %r', queue_name, exchange_name)
+
+        channel.basic_consume(self.message_received_cb, queue=queue_name)
+
+        self.channel = channel
+        self.message_received = blinker.Signal()
+
+    def run(self):
+        """Run ioloop and consume messages."""
+        logging.info('Waiting for messages...')
+        self.channel.start_consuming()
+
+    def message_received_cb(self, channel, method_frame, header_frame, body):
+        """Handle message received.
+
+        :param channel: Connection channel with rabbitmq server
+        :type channel: pika.channel.Channel
+        :param method_frame: AMPQ method related data
+        :type method_frame: pika.spec.Deliver
+        :param body: Message body
+        :type body: str
+
+        """
+        exchange_name = method_frame.exchange
+        LOGGER.debug('Message received from %r: %s', exchange_name, body)
+
+        # Only accept json messages
+        if header_frame.content_type != 'application/json':
+            LOGGER.warning(
+                'Message discarded. Unexpected content type: %r',
+                header_frame.content_type,
+            )
+            channel.basic_nack(method_frame.delivery_tag, requeue=False)
+            return
+
+        channel.basic_ack(delivery_tag=method_frame.delivery_tag)
+        try:
+            payload = json.loads(body)
+        except ValueError:
+            LOGGER.warning('Body decoding error: %r', body)
+        else:
+            self.message_received.send(
+                self,
+                exchange_name=exchange_name,
+                payload=payload,
+            )
---
+++ packages/rabbithole/rabbithole-0.2.0-py2.py3-none-any.whl/rabbithole/db.py
@@ -0,0 +1,65 @@
+# -*- coding: utf-8 -*-
+
+"""Database: run queries with batches of rows per exchange."""
+
+import logging
+
+from sqlalchemy import (
+    create_engine,
+    text,
+)
+from sqlalchemy.exc import SQLAlchemyError
+
+LOGGER = logging.getLogger(__name__)
+
+
+class Database(object):
+
+    """Database writer.
+
+    :param url: Database connection string
+    :type url: str
+    :param queries: Insert query to execute for each exchange name
+    :type queries: dict(str, str)
+
+    """
+
+    def __init__(self, url, queries):
+        """Connect to database."""
+        self.engine = create_engine(url)
+        self.connection = None
+
+        self.queries = {
+            exchange_name: text(query)
+            for exchange_name, query
+            in queries.items()
+        }
+
+    def connect(self):
+        """Connect to the database."""
+        self.connection = self.engine.connect()
+        LOGGER.debug('Connected to: %r', self.engine.url)
+        return self
+
+    def batch_ready_cb(self, sender, exchange_name, batch):
+        """Execute insert query for the batch that is ready.
+
+        :param sender: The batcher who sent the batch_ready signal
+        :type sender: rabbithole.batcher.Batcher
+        :param exchange_name: Key used to determine which query to execute
+        :type exchange_name: str
+        :param batch: Batch of rows to insert
+        :type rows: list(dict(str))
+
+        """
+        if exchange_name not in self.queries:
+            LOGGER.warning('No query found for %r', exchange_name)
+            return
+
+        query = self.queries[exchange_name]
+        try:
+            self.connection.execute(query, batch)
+        except SQLAlchemyError as exception:
+            LOGGER.error(exception)
+        else:
+            LOGGER.debug('Inserted %d rows', len(batch))

7ba1112b7a35b0b9df47a54d50b3c0006cc03b0a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:42:49 +0000

    Add package darkdemon007_nester/1.3.0/darkdemon007_nester-1.3.0.zip

---
+++ packages/darkdemon007_nester/darkdemon007_nester-1.3.0.zip/darkdemon007_nester-1.3.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(
+	name = 'darkdemon007_nester',
+	version = '1.3.0',
+	py_modules = ['darkdemon007_nester'],
+	author = 'darkdemon007',
+	author_email = 'adilswarcraft@gmail.com',
+	url = 'http://www.codingspider.com',
+	description = 'A simple printer of nested lists',
+)

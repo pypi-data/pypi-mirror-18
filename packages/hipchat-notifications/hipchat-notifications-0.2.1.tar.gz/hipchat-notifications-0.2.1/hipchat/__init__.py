84db77f1e7fc91af8f2f1a7c60bbf446cd8a1fff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:12:06 +0000

    Add package hipchat-notifications/0.2.1/hipchat-notifications-0.2.1.tar.gz

---
+++ packages/hipchat-notifications/hipchat-notifications-0.2.1.tar.gz/hipchat-notifications-0.2.1/hipchat/__init__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+from exceptions import HipChatError
+from logger import HipChatHandler
+from notifications import (
+    gray,
+    green,
+    grey,
+    purple,
+    red,
+    yellow,
+    notify_room,
+    notify_user
+)

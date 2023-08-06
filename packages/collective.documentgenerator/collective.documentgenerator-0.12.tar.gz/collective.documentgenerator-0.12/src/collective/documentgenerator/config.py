629e0d2a3296eb0b6d1fd7c7b6c4e6c3e9e26c53
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:45:38 +0000

    Add package collective.documentgenerator/0.12/collective.documentgenerator-0.12.tar.gz

---
+++ packages/collective.documentgenerator/collective.documentgenerator-0.12.tar.gz/collective.documentgenerator-0.12/src/collective/documentgenerator/config.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+
+from plone import api
+
+ODS_FORMATS = (('ods', 'LibreOffice Calc (.ods)'),
+               ('xls', 'Microsoft Excel (.xls)'),)
+
+ODT_FORMATS = (('odt', 'LibreOffice Writer (.odt)'),
+               ('doc', 'Microsoft Word (.doc)'),
+               ('rtf', 'Rich Text Format (.RTF)'),)
+
+NEUTRAL_FORMATS = (('pdf', 'Adobe PDF (.pdf)'),)
+
+POD_FORMATS = ODS_FORMATS + ODT_FORMATS + NEUTRAL_FORMATS
+
+DEFAULT_PYTHON_UNO_PATH = '/usr/bin/python'
+
+VIEWLET_TYPES = ['PODTemplate', 'ConfigurablePODTemplate']
+
+
+def get_uno_path():
+    return api.portal.get_registry_record(
+        'collective.documentgenerator.browser.controlpanel.IDocumentGeneratorControlPanelSchema.uno_path'
+    )
+
+
+def get_oo_port():
+    return api.portal.get_registry_record(
+        'collective.documentgenerator.browser.controlpanel.IDocumentGeneratorControlPanelSchema.oo_port'
+    )

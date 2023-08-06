5f9460cf4e4a23851e21222e9894ac991aa39eca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:10:23 +0000

    Add package text2img/0.0.32/text2img-0.0.32.tar.gz

---
+++ packages/text2img/text2img-0.0.32.tar.gz/text2img-0.0.32/setup.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+setup(
+    name='text2img',
+    version='0.0.32',
+    url='https://github.com/tuaplicacionpropia/text2img',
+    download_url='https://github.com/tuaplicacionpropia/text2img/archive/master.zip',
+    author=u'tuaplicacionpropia.com',
+    author_email='tuaplicacionpropia@gmail.com',
+    description='Python library for generate images from text.',
+    long_description='Python library for generate images svg from text and svg templates.',
+    keywords='svg, text, json, generator',
+    classifiers=[
+      'Development Status :: 4 - Beta',
+      'License :: OSI Approved :: MIT License',
+      'Programming Language :: Python', 
+      'Programming Language :: Python :: 2.7', 
+      'Intended Audience :: Developers', 
+      'Topic :: Multimedia :: Graphics',
+    ],
+    packages=find_packages(exclude=['tests']),
+    zip_safe=False,
+    include_package_data=True,
+    platforms='any',
+    license='MIT',
+    install_requires=[
+        'lxml==3.3.3',
+        'hjson==2.0.2',
+    ],
+)
+

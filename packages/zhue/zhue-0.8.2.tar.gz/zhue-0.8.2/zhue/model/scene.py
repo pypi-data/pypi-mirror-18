fa53fcf996128f69c684c3fcdea61a421315fa2c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:11:59 +0000

    Add package zhue/0.8.2/zhue-0.8.2.tar.gz

---
+++ packages/zhue/zhue-0.8.2.tar.gz/zhue-0.8.2/zhue/model/scene.py
@@ -0,0 +1,45 @@
+from __future__ import unicode_literals
+import basemodel
+import group
+
+
+class Scene(basemodel.BaseGroup):
+    def __init__(self, bridge, hue_id, json):
+        super(Scene, self).__init__(bridge, 'scenes', hue_id, json)
+
+    @property
+    def owner(self):
+        return self._bridge.user(username=self._json['owner'])
+
+    @property
+    def version(self):
+        return self._json['version']
+
+    @property
+    def picture(self):
+        return self._json['picture']
+
+    @property
+    def locked(self):
+        return self._json['locked']
+
+    @property
+    def last_updated(self):
+        return self._json['last_updated']
+
+    @property
+    def appdata(self):
+        return self._json['appdata']
+
+    @property
+    def recycle(self):
+        return self._json['recycle']
+
+    def recall(self, target_group=None):
+        if target_group is None:
+            target_grp = group.MasterGroup(self._bridge)
+        else:
+            target_grp = target_group
+        assert isinstance(target_grp, group.Group), \
+            'Provided object is not a group'
+        return target_grp.set_scene(self)

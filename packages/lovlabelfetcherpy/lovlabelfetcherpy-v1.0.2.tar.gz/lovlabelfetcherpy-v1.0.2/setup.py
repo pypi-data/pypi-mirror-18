8d6389803e25352ded72a3327a58cdf1ea873db0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:09:31 +0000

    Add package lovlabelfetcherpy/v1.0.2/lovlabelfetcherpy-v1.0.2.tar.gz

---
+++ packages/lovlabelfetcherpy/lovlabelfetcherpy-v1.0.2.tar.gz/lovlabelfetcherpy-v1.0.2/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'lovlabelfetcherpy',
+  packages = ['lovlabelfetcherpy'], # this must be the same as the name above
+  version = 'v1.0.2',
+  description = 'LOV Label Fetcher',
+  author = 'Ivan Ermilov',
+  author_email = 'ivan.s.ermilov@gmail.com',
+  url = 'https://github.com/earthquakesan/lovlabelfetcher-py', # use the URL to the github repo
+  download_url = 'https://github.com/earthquakesan/lovlabelfetcher-py/archive/v1.0.2.tar.gz', 
+  keywords = ['aksw', 'semantic-web'], # arbitrary keywords
+  classifiers = [],
+)

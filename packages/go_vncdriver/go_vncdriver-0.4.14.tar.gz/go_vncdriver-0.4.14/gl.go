1ec5df906668c92dea47e05cfb7a37d6e7b5dea4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:11:32 +0000

    Add package go_vncdriver/0.4.14/go_vncdriver-0.4.14.tar.gz

---
+++ packages/go_vncdriver/go_vncdriver-0.4.14.tar.gz/go_vncdriver-0.4.14/gl.go
@@ -0,0 +1,18 @@
+// +build !no_gl
+
+package main
+
+import "github.com/openai/go-vncdriver/vncgl"
+
+const compiledWithGL = true
+
+func (b *sessionInfo) initRenderer(name string) error {
+	if b.rendererSet {
+		return nil
+	}
+	b.rendererSet = true
+
+	// Make sure we've initialized glfw
+	vncgl.SetupRendering()
+	return b.batch.SetRenderer(name, &vncgl.VNCGL{})
+}

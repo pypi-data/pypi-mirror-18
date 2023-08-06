77d1dfbbbbf3babb478803f965835c4dc8182e54
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:51:28 +0000

    Add package mymath-ghajba/0.0.4/mymath_ghajba-0.0.4-py2.py3-none-any.whl

---
+++ packages/mymath-ghajba/mymath_ghajba-0.0.4-py2.py3-none-any.whl/mymath_ghajba.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+"""A simple mathematics module to demonstrate the usage of flit"""
+__version__ = '0.0.4'
+__author__ = 'GHajba'
+
+
+def add(a, b):
+    return a + b
+
+
+def sub(a, b):
+    return a - b
+
+
+def mult(a, b):
+    return a * b
+
+
+def div(a, b):
+    return a / b

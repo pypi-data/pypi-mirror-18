7672f2dfa46fd51b37c2bb1b7894a6cbac9f7c99
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:10:42 +0000

    Add package wafer/0.4.9/wafer-0.4.9.tar.gz

---
+++ packages/wafer/wafer-0.4.9.tar.gz/wafer-0.4.9/wafer/management/commands/wafer_speaker_contact_details.py
@@ -0,0 +1,43 @@
+import sys
+import csv
+from optparse import make_option
+
+from django.core.management.base import BaseCommand
+
+from django.contrib.auth import get_user_model
+from wafer.talks.models import ACCEPTED
+
+
+class Command(BaseCommand):
+    help = "List contact details for the speakers."
+
+    option_list = BaseCommand.option_list + tuple([
+        make_option('--speakers', action="store_true", default=False,
+                    help='List speaker email addresses'
+                    ' (for accepted talks)'),
+        make_option('--allspeakers', action="store_true", default=False,
+                    help='List speaker email addresses'
+                    ' (for all talks)'),
+    ])
+
+    def _speaker_emails(self, options):
+        people = get_user_model().objects.filter(
+            talks__isnull=False).distinct()
+
+        csv_file = csv.writer(sys.stdout)
+        for person in people:
+            if options['allspeakers']:
+                titles = [x.title for x in person.talks.all()]
+            else:
+                titles = [x.title for x in
+                          person.talks.filter(status=ACCEPTED)]
+                if not titles:
+                    continue
+            row = [x.encode("utf-8")
+                   for x in (person.userprofile.display_name(), person.email,
+                   person.userprofile.contact_number or 'NO CONTACT INFO',
+                   ';'.join(titles))]
+            csv_file.writerow(row)
+
+    def handle(self, *args, **options):
+        self._speaker_emails(options)

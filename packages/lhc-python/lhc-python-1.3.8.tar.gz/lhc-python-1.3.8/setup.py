0ca3aafc1276facc609ef79fd7006eb62deda9df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:45:41 +0000

    Add package lhc-python/1.3.8/lhc-python-1.3.8.tar.gz

---
+++ packages/lhc-python/lhc-python-1.3.8.tar.gz/lhc-python-1.3.8/setup.py
@@ -0,0 +1,25 @@
+import os
+
+from setuptools import setup, find_packages
+
+with open('README.rst', encoding='utf-8') if os.path.exists('README.rst') else \
+        open('README.md', encoding='utf-8') as fileobj:
+    long_description = fileobj.read()
+
+setup(
+    name='lhc-python',
+    version='1.3.8',
+    author='Liam H. Childs',
+    author_email='liam.h.childs@gmail.com',
+    packages=find_packages(exclude=['docs', 'lhc.test*']),
+    scripts=[],
+    url='https://github.com/childsish/lhc-python',
+    license='LICENSE.txt',
+    description='My python library of classes and functions that help me work',
+    long_description=long_description,
+    install_requires=['sortedcontainers == 1.5.3'],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Science/Research',
+        'Topic :: Scientific/Engineering :: Bio-Informatics']
+)

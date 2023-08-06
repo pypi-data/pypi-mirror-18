2b2495dec190b72882ea364f9ccd658b528b54c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:17:42 +0000

    Add package slackbot-wems/0.2.481235459/slackbot_wems-0.2.481235459.tar.gz

---
+++ packages/slackbot-wems/slackbot_wems-0.2.481235459.tar.gz/slackbot_wems-0.2.481235459/slackbot_wems/wems/viktor/slacklib.py
@@ -0,0 +1,25 @@
+# Put your commands here
+COMMAND1 = "haha"
+COMMAND2 = "pfs"
+COMMAND3 = "egg"
+
+def haha_loop():
+    for i in range(15):
+        print("@wemmy haha")
+
+# Your handling code goes in this function
+def handle_command(command):
+    """
+        Determine if the command is valid. If so, take action and return
+        a response, if necessary.
+    """
+    response = ""
+    if command.find(COMMAND1) >= 0:
+        response = haha_loop()
+    elif command.find(COMMAND2) >=0:
+        response = "you found an easter egg! PRO_FuZion_SlIME made this easter egg! the next easter egg command is egg"
+
+    elif command.find(COMMAND3) >=0:
+        response = "that was the last egg! i might make more!"
+
+    return response

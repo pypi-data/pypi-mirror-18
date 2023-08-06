471d6765c9d15a3629be658ef97e9b33937e5744
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:21:09 +0000

    Add package enable/4.6.1/enable-4.6.1.zip

---
+++ packages/enable/enable-4.6.1.zip/enable-4.6.1/enable/_version.py
@@ -0,0 +1,8 @@
+# THIS FILE IS GENERATED FROM ENABLE SETUP.PY
+version = '4.6.1'
+full_version = '4.6.1'
+git_revision = 'Unknown'
+is_released = True
+
+if not is_released:
+    version = full_version

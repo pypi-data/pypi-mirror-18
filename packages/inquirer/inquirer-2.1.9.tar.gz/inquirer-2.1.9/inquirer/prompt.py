9293e99c0fecc145e39f89003afa32c1ebfb0556
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:51 +0000

    Add package inquirer/2.1.9/inquirer-2.1.9.tar.gz

---
+++ packages/inquirer/inquirer-2.1.9.tar.gz/inquirer-2.1.9/inquirer/prompt.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+from .render.console import ConsoleRender
+
+
+def prompt(questions, render=None, answers=None, raise_keyboard_interrupt=False):
+    render = render or ConsoleRender()
+    answers = answers or {}
+
+    try:
+        for question in questions:
+            answers[question.name] = render.render(question, answers)
+        return answers
+    except KeyboardInterrupt:
+        if raise_keyboard_interrupt:
+            raise
+        print('')
+        print('Cancelled by user')
+        print('')

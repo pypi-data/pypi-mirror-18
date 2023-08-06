5848d052188e92ed3e52282b7f778c48eae80dd6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:45:01 +0000

    Add package graphql-relay/0.4.5/graphql-relay-0.4.5.tar.gz

---
+++ packages/graphql-relay/graphql-relay-0.4.5.tar.gz/graphql-relay-0.4.5/setup.py
@@ -0,0 +1,66 @@
+import sys
+
+from setuptools import setup, find_packages
+from setuptools.command.test import test as TestCommand
+
+
+class PyTest(TestCommand):
+    user_options = [('pytest-args=', 'a', "Arguments to pass to py.test")]
+
+    def initialize_options(self):
+        TestCommand.initialize_options(self)
+        self.pytest_args = []
+
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+        self.test_args = []
+        self.test_suite = True
+
+    def run_tests(self):
+        # import here, cause outside the eggs aren't loaded
+        import pytest
+        errno = pytest.main(self.pytest_args)
+        sys.exit(errno)
+
+setup(
+    name='graphql-relay',
+    version='0.4.5',
+
+    description='Relay implementation for Python',
+    long_description=open('README.rst').read(),
+
+    url='https://github.com/graphql-python/graphql-relay-py',
+
+    author='Syrus Akbary',
+    author_email='me@syrusakbary.com',
+
+    license='MIT',
+
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Libraries',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: Implementation :: PyPy',
+    ],
+
+    keywords='api graphql protocol rest relay',
+
+    packages=find_packages(exclude=['tests']),
+
+    install_requires=[
+        'six>=1.10.0',
+        'graphql-core>=0.5.0',
+        'promise>=0.4.0'
+    ],
+    tests_require=['pytest>=2.7.2'],
+    extras_require={
+    },
+
+    cmdclass={'test': PyTest},
+)

e23cfc1b3c2882fc7196e1e763ae0ded84e72ab1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:42 +0000

    Add package bdss-client/1.0.1b2/bdss_client-1.0.1b2-py3-none-any.whl

---
+++ packages/bdss-client/bdss_client-1.0.1b2-py3-none-any.whl/client/config.py
@@ -0,0 +1,29 @@
+# Big Data Smart Socket
+# Copyright (C) 2016 Clemson University
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+#
+
+import configparser
+import os
+from pkg_resources import resource_string
+
+config = configparser.ConfigParser()
+config.read_string(resource_string(__name__, "defaults.cfg").decode("utf-8"))
+config.read([os.path.expanduser("~/.bdss.cfg"), "bdss.cfg"])
+
+metadata_repository_url = config.get("metadata_repository", "url").rstrip("/")
+
+client_destination = config.get("client", "location", fallback=None)

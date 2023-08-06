b17f4f3b5dc1ff447d6545b5ad9cf637198e044a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:10:15 +0000

    Add package artifactory-lib/0.0.1/artifactory-lib-0.0.1.tar.gz

---
+++ packages/artifactory-lib/artifactory-lib-0.0.1.tar.gz/artifactory-lib-0.0.1/setup.py
@@ -0,0 +1,56 @@
+"""A setuptools based setup module.
+See:
+https://packaging.python.org/en/latest/distributing.html
+https://github.com/pypa/sampleproject
+"""
+
+# Always prefer setuptools over distutils
+from setuptools import setup, find_packages
+# To use a consistent encoding
+from codecs import open
+from os import path
+
+setup(
+    name='artifactory-lib',
+
+    # Versions should comply with PEP440.  For a discussion on single-sourcing
+    # the version across setup.py and the project code, see
+    # https://packaging.python.org/en/latest/single_source_version.html
+    # Step 7: use from SCM https://pypi.python.org/pypi/setuptools_scm
+    use_scm_version=True,
+    setup_requires=['setuptools_scm'],
+
+    description='Python library for interacting with Artifactory API',
+    url='https://github.com/gugahoi/artifactory_python',
+    author='Gustavo Hoirisch',
+    author_email='gugahoi@gmail.com',
+    license='MIT',
+
+    # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
+        # How mature is this project? Common values are
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+        'Development Status :: 3 - Alpha',
+
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+
+        'License :: OSI Approved :: MIT License',
+
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+
+    # What does your project relate to?
+    keywords='artifactory management library',
+    py_modules=["Artifactory"],
+    install_requires=['requests']
+)

bc1c3e15397e7cffb9f925badcdd608a8ae7ea60
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:20:33 +0000

    Add package suger_nester/1.0.0/suger_nester-1.0.0.tar.gz

---
+++ packages/suger_nester/suger_nester-1.0.0.tar.gz/suger_nester-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+name = 'suger_nester',
+version = '1.0.0',
+py_modules = ['suger_nester'],
+author = 'Suger',
+author_email = 'suger.lim0211@gmail.com',
+url = 'http://www.headfirstlabs.com',
+description = 'A simple printer of nested lists',
+)

505c7440a05697d88e8d467e10f66237ae236e2e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:18:53 +0000

    Add package unzipbomb/1.1/unzipbomb-1.1.tar.gz

---
+++ packages/unzipbomb/unzipbomb-1.1.tar.gz/unzipbomb-1.1/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'unzipbomb',
+  packages = ['unzipbomb'],
+  version = '1.1',
+  description = 'A python module which defuses the zipbomb also can extract all the zip files that are contained in a specific root folder, please read usage.pdf from github repo',
+  author = 'Ankitesh Gupta, Saif Vazir',
+  author_email = 'ankiteshguptas@gmail.com,ssv.saif@gmail.com',
+  url = 'https://github.com/ankitesh97/unzipbomb',
+  download_url = 'https://github.com/ankitesh97/unzipbomb/tarball/v1.1',
+  keywords = ['unzipbomb', 'zipbomb', 'extract', 'multiple zip files', 'zip bomb','unzip','unzip','zip','compress','decompress','extract multiple zip files'],
+  classifiers = [],
+)

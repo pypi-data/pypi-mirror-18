3449e3296b871b0a574d4f56b294c00401f00081
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:09:19 +0000

    Add package tlslite-ng/0.7.0-alpha2/tlslite-ng-0.7.0-alpha2.tar.gz

---
+++ packages/tlslite-ng/tlslite-ng-0.7.0-alpha2.tar.gz/tlslite-ng-0.7.0-alpha2/tlslite/utils/dns_utils.py
@@ -0,0 +1,15 @@
+import re
+
+def is_valid_hostname(hostname):
+    if hostname[-1] == ".":
+        # strip exactly one dot from the right, if present
+        hostname = hostname[:-1]
+    if len(hostname) > 253:
+        return False
+
+    # must not be all-numeric, so that it can't be confused with an ip-address
+    if re.match(r"[\d.]+$", hostname):
+        return False
+
+    allowed = re.compile("(?!-)[A-Z\d-]{1,63}(?<!-)$", re.IGNORECASE)
+    return all(allowed.match(x) for x in hostname.split("."))

e80190b3eecd11525341f4746bae85df2c6eea5d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:17:01 +0000

    Add package mdexparser/0.0.2/mdexparser-0.0.2.tar.gz

---
+++ packages/mdexparser/mdexparser-0.0.2.tar.gz/mdexparser-0.0.2/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+		name = 'mdexparser',
+		version = '0.0.2',
+		py_modules = ['dexparser', 'disassembler'],
+		author = 'Marco Montagna', # Original author is bunseokbot
+		author_email = 'marco@marcomontagna.com',
+		url = 'https://github.com/mmontagna/dexparser',
+		description = 'DEX Parser for Android Analyst'
+	 )

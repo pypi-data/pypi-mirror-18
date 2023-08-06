6b724794cab6e69d0b389e1e75447c4cf5b0d804
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:09:31 +0000

    Add package streamshift/0.0.6/streamshift-0.0.6.tar.gz

---
+++ packages/streamshift/streamshift-0.0.6.tar.gz/streamshift-0.0.6/setup.py
@@ -0,0 +1,16 @@
+import os
+from setuptools import setup
+
+setup(
+    name="streamshift",
+    version="0.0.6",
+    author="Gosha Sawicka",
+    author_email="mal.galazka@gmail.com",
+    scripts=['bin/streamshift.py'],
+    description=("Buffer audio streams and serve them back with a user-defined offset"),
+    license="MIT",
+    keywords="radio audio stream",
+    url="https://github.com/patyk/streamshift",
+    packages=['streamshift', 'tests'],
+    classifiers=[],
+)

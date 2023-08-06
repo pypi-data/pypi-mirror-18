5843c4b89c2012e0f575a0e80acd389bfb8dfbe6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:06:33 +0000

    Add package oandapyV20/0.2.1/oandapyV20-0.2.1.tar.gz

---
+++ packages/oandapyV20/oandapyV20-0.2.1.tar.gz/oandapyV20-0.2.1/oandapyV20/types/__init__.py
@@ -0,0 +1,26 @@
+
+__all__ = [
+    'OrderID',
+    'TradeID',
+    'AccountUnits',
+    'PriceValue',
+    'Units',
+    'ClientID',
+    'ClientTag',
+    'ClientComment',
+    'OrderIdentifier',
+    'OrderSpecifier'
+]
+
+from .types import (
+    OrderID,
+    TradeID,
+    AccountUnits,
+    PriceValue,
+    Units,
+    ClientID,
+    ClientTag,
+    ClientComment,
+    OrderIdentifier,
+    OrderSpecifier
+)

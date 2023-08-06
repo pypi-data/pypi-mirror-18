e88e70205cd6b4cab6f503470215333b4375a679
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:13:24 +0000

    Add package infobip-api-python-client/1.1.1/infobip-api-python-client-1.1.1.zip

---
+++ packages/infobip-api-python-client/infobip-api-python-client-1.1.1.zip/infobip-api-python-client-1.1.1/infobip/util/configuration.py
@@ -0,0 +1,12 @@
+__author__ = 'mstipanov'
+
+
+class Configuration:
+    def __init__(self, username = None, password = None, api_key = None, token = None, base_url = None):
+        self.base_url = "https://api.infobip.com"
+        if base_url:
+            self.base_url = base_url
+        self.username = username
+        self.password = password
+        self.api_key = api_key
+        self.token = token

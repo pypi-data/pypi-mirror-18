a0a2f1adedbb9e377f05547d62bbd664f6c399c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:20:00 +0000

    Add package staticbs/0.1/staticbs-0.1.zip

---
+++ packages/staticbs/staticbs-0.1.zip/staticbs-0.1/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+setup(name='staticbs',
+      version='0.1',
+      description='A simple 3D electro-magnetostatic biot-savart solving simulator',
+      classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2.7',
+        'Intended Audience :: Science/Research',
+        'Topic :: Scientific/Engineering :: Physics',
+        'Topic :: Scientific/Engineering :: Visualization',
+      ],
+      url='https://github.com/grungy/staticbs',
+      author='Josh Marks and Andrea Waite',
+      author_email='jmarks@udel.edu',
+      license='MIT',
+      packages=['staticbs'],
+      install_requires=['numpy', 'scipy'],
+      zip_safe=False)

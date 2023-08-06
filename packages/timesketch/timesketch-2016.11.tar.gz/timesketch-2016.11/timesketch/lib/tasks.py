aa5e48508775eb2bd824f3caeb1784d1f44d7f98
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:09:15 +0000

    Add package timesketch/2016.11/timesketch-2016.11.tar.gz

---
+++ packages/timesketch/timesketch-2016.11.tar.gz/timesketch-2016.11/timesketch/lib/tasks.py
@@ -0,0 +1,82 @@
+# Copyright 2015 Google Inc. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""Celery task for processing Plaso storage files."""
+
+import os
+import sys
+
+from flask import current_app
+# We currently don't have plaso in our Travis setup. This is a workaround
+# for that until we fix the Travis environment.
+# TODO: Add Plaso to our Travis environment we are running our tests in.
+try:
+    from plaso.frontend import psort
+except ImportError:
+    pass
+
+from timesketch import create_celery_app
+
+celery = create_celery_app()
+
+
+def get_data_location():
+    """Path to the plaso data directory.
+
+    Returns:
+        The path to where the plaso data directory is or None if not existing.
+    """
+    data_location = current_app.config.get(u'PLASO_DATA_LOCATION', None)
+    if not data_location:
+        data_location = os.path.join(sys.prefix, u'share', u'plaso')
+    if not os.path.exists(data_location):
+        data_location = None
+    return data_location
+
+
+@celery.task(track_started=True)
+def run_plaso(source_file_path, timeline_name, index_name, username=None):
+    """Create a Celery task for processing Plaso storage file.
+
+    Args:
+        source_file_path: Path to plaso storage file.
+        timeline_name: Name of the Timesketch timeline.
+        index_name: Name of the datastore index.
+        username: Username of the user who will own the timeline.
+
+    Returns:
+        Dictionary with count of processed events.
+    """
+    plaso_data_location = get_data_location()
+    flush_interval = 1000  # events to queue before bulk index
+    doc_type = u'plaso_event'  # Document type for Elasticsearch
+
+    # Use Plaso psort frontend tool.
+    frontend = psort.PsortFrontend()
+    frontend.SetDataLocation(plaso_data_location)
+    storage_reader = frontend.CreateStorageReader(source_file_path)
+
+    # Setup the Timesketch output module.
+    output_module = frontend.CreateOutputModule(u'timesketch')
+    output_module.SetIndexName(index_name)
+    output_module.SetTimelineName(timeline_name)
+    output_module.SetFlushInterval(flush_interval)
+    output_module.SetDocType(doc_type)
+    if username:
+        output_module.SetUserName(username)
+
+    # Start process the Plaso storage file.
+
+    counter = frontend.ExportEvents(storage_reader, output_module)
+
+    return dict(counter)
---
+++ packages/timesketch/timesketch-2016.11.tar.gz/timesketch-2016.11/timesketch/ui/views/home.py
@@ -0,0 +1,86 @@
+# Copyright 2015 Google Inc. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""This module implements HTTP request handler."""
+
+from flask import Blueprint
+from flask import current_app
+from flask import render_template
+from flask import redirect
+from flask import request
+from flask import url_for
+from flask_login import current_user
+from flask_login import login_required
+from sqlalchemy import not_
+
+from timesketch.models.sketch import Sketch
+from timesketch.models.sketch import View
+from timesketch.lib.forms import HiddenNameDescriptionForm
+from timesketch.models import db_session
+
+
+# Register flask blueprint
+home_views = Blueprint(u'home_views', __name__)
+
+
+@home_views.route(u'/', methods=[u'GET', u'POST'])
+@home_views.route(u'/sketch/', methods=[u'GET', u'POST'])
+@login_required
+def home():
+    """Generates the home page view template.
+
+    Returns:
+        Template with context.
+    """
+    form = HiddenNameDescriptionForm()
+    sketches = Sketch.all_with_acl().filter(
+        not_(Sketch.Status.status == u'deleted'),
+        Sketch.Status.parent).order_by(Sketch.updated_at.desc())
+    query_filter = request.args.get(u'filter', u'')
+    query = request.args.get(u'q', u'')
+    # Only render upload button if it is configured.
+    upload_enabled = current_app.config[u'UPLOAD_ENABLED']
+    last_sketch = View.query.filter_by(
+        user=current_user, name=u'').order_by(
+            View.updated_at.desc()).first()
+
+    if query_filter:
+        if query_filter == u'user':
+            sketches = sketches.filter(Sketch.user == current_user)
+        elif query_filter == u'shared':
+            sketches = sketches.filter(not_(Sketch.user == current_user))
+
+    # TODO: Figure out a better way to handle this.
+    if query:
+        if query.startswith(u'*'):
+            query = u''
+        else:
+            sketches = sketches.filter(Sketch.name.contains(query)).limit(100)
+
+    # Handle form for creating a new sketch.
+    if form.validate_on_submit():
+        sketch = Sketch(
+            name=form.name.data, description=form.description.data,
+            user=current_user)
+        sketch.status.append(sketch.Status(user=None, status=u'new'))
+        # Give the requesting user permissions on the new sketch.
+        sketch.grant_permission(permission=u'read', user=current_user)
+        sketch.grant_permission(permission=u'write', user=current_user)
+        sketch.grant_permission(permission=u'delete', user=current_user)
+        db_session.add(sketch)
+        db_session.commit()
+        return redirect(url_for(u'sketch_views.overview', sketch_id=sketch.id))
+
+    return render_template(
+        u'home/home.html', sketches=sketches, form=form, query=query,
+        upload_enabled=upload_enabled, last_sketch=last_sketch)

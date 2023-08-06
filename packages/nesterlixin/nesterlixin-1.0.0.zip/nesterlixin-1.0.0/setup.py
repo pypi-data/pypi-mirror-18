7f864367c3a94bff079a5ba696e355fefde65658
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:18:47 +0000

    Add package nesterlixin/1.0.0/nesterlixin-1.0.0.zip

---
+++ packages/nesterlixin/nesterlixin-1.0.0.zip/nesterlixin-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name='nesterlixin',
+    version='1.0.0',
+    py_modules=['nester'],
+    author='hfpython',
+    author_email='wanglixin112417@pwrd.com',
+    url='http://www.headfirstlabs.com',
+    description='A simple printer of nested lists',
+)

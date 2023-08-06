f37e987c7260545d3709f607ba1713e34a38f56e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:44:52 +0000

    Add package rptk/0.1.0.dev14/rptk-0.1.0.dev14.tar.gz

---
+++ packages/rptk/rptk-0.1.0.dev14.tar.gz/rptk-0.1.0.dev14/setup.py
@@ -0,0 +1,43 @@
+from setuptools import setup, find_packages
+from codecs import open
+
+version = open('packaging/VERSION').read().strip()
+requirements = open('packaging/requirements.txt').read().split("\n")
+test_requirements = open('packaging/requirements-test.txt').read().split("\n")
+with open('README.rst', encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='rptk',
+    version=version,
+    author='Workonline Communications',
+    author_email='communications@workonkonline.co.za',
+    description='Python tools for prefix filter list management operations',
+    long_description=long_description,
+    license='LICENSE',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Internet',
+    ],
+    packages=find_packages(
+        include=[
+            'rptk',
+            'rptk.*'
+        ],
+        exclude=[]
+    ),
+    entry_points={
+        'console_scripts': ['rptk=rptk.command_line:main']
+    },
+    include_package_data=True,
+
+    url='https://github.com/wolcomm/rptk',
+    download_url='https://github.com/wolcomm/rptk/%s' % version,
+
+    install_requires=requirements,
+    tests_require=test_requirements,
+    test_suite='nose.collector'
+)

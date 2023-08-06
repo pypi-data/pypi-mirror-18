9b8d11ac78aaae18d0f23eb4bd1b9c3a57788be8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:22:34 +0000

    Add package imps/0.2.1/imps-0.2.1.tar.gz

---
+++ packages/imps/imps-0.2.1.tar.gz/imps-0.2.1/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+setup(
+    name='imps',
+    version='0.2.1',
+    description='Python utility to sort Python imports',
+    author='Andy Boot',
+    url='https://github.com/bootandy/imps',
+    license="Apache",
+    install_requires=['flake8-import-order', 'configparser'],
+    packages=['imps'],
+    entry_points={
+        'console_scripts': [
+            'imps = imps.shell:main',
+        ],
+    },
+    keywords=['Refactoring', 'Imports'],
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'Environment :: Console',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Utilities',
+    ]
+)

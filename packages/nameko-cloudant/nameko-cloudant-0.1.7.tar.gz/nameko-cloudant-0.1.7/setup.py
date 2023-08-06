4e3003b4d0606de9b4061dd935c1adb9cf5a82ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:11:32 +0000

    Add package nameko-cloudant/0.1.7/nameko-cloudant-0.1.7.tar.gz

---
+++ packages/nameko-cloudant/nameko-cloudant-0.1.7.tar.gz/nameko-cloudant-0.1.7/setup.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+from setuptools import setup
+
+setup(
+    name='nameko-cloudant',
+    version='0.1.7',
+    description='A dependency for cloudant',
+    author='frisellcpl',
+    author_email='johan@trell.se',
+    url='http://github.com/frisellcpl/nameko-cloudant',
+    packages=['nameko_cloudant'],
+    install_requires=[
+        "nameko==2.4.2",
+        "cloudant==2.3.0"
+    ],
+    zip_safe=True,
+    license='Apache License, Version 2.0',
+    classifiers=[
+        "Programming Language :: Python",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: POSIX",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4",
+        "Topic :: Internet",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Intended Audience :: Developers",
+    ]
+)

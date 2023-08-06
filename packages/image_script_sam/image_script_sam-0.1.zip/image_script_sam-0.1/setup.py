1236b2234ea8ab06e3b1dea30197959bddc618db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:09:56 +0000

    Add package image_script_sam/0.1/image_script_sam-0.1.zip

---
+++ packages/image_script_sam/image_script_sam-0.1.zip/image_script_sam-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+setup(
+    name='image_script_sam',    # This is the name of your PyPI-package.
+    version='0.1',                          # Update the version number for new releases
+    scripts=['image_script.py'],
+    author = 'Sambridhi'                  # The name of your scipt, and also the command you'll be using for calling it
+)

0273cbe32bb5b3673854394a401625f3d8fab63e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:44:56 +0000

    Add package Products.Archetypes/1.12/Products.Archetypes-1.12.tar.gz

---
+++ packages/Products.Archetypes/Products.Archetypes-1.12.tar.gz/Products.Archetypes-1.12/Products/Archetypes/setuphandlers.py
@@ -0,0 +1,84 @@
+"""
+Archetypes setup handlers.
+"""
+from Products.Archetypes.config import REFERENCE_CATALOG
+from Products.Archetypes.config import TOOL_NAME
+from Products.Archetypes.config import UID_CATALOG
+from Products.CMFCore.utils import getToolByName
+from Products.CMFPlone.interfaces import INonInstallable
+from zope.interface import implementer
+
+
+@implementer(INonInstallable)
+class HiddenProfiles(object):
+
+    def getNonInstallableProfiles(self):
+        """Prevents uninstall profile from showing up in the profile list
+        when creating a Plone site.
+
+        """
+        return [
+            u'Products.Archetypes:uninstall',
+        ]
+
+
+def install_uidcatalog(out, site, rebuild=False):
+    catalog = getToolByName(site, UID_CATALOG)
+
+    index_defs = (('UID', 'FieldIndex'),
+                  ('Type', 'FieldIndex'),
+                  ('id', 'FieldIndex'),
+                  ('Title', 'FieldIndex'),  # used for sorting
+                  ('portal_type', 'FieldIndex'),)
+    metadata_defs = ('UID', 'Type', 'id', 'Title', 'portal_type', 'meta_type')
+    reindex = False
+
+    for indexName, indexType in index_defs:
+        if indexName not in catalog.indexes():
+            catalog.addIndex(indexName, indexType, extra=None)
+            reindex = True
+
+    for metadata in metadata_defs:
+        if indexName not in catalog.schema():
+            catalog.addColumn(metadata)
+            reindex = True
+    if reindex:
+        catalog.manage_reindexIndex()
+
+
+def install_referenceCatalog(out, site, rebuild=False):
+    catalog = getToolByName(site, REFERENCE_CATALOG)
+    reindex = False
+
+    for indexName, indexType in (('UID', 'FieldIndex'),
+                                 ('sourceUID', 'FieldIndex'),
+                                 ('targetUID', 'FieldIndex'),
+                                 ('relationship', 'FieldIndex'),
+                                 ('targetId', 'FieldIndex'),):
+        if indexName not in catalog.indexes():
+            catalog.addIndex(indexName, indexType, extra=None)
+            reindex = True
+        if indexName not in catalog.schema():
+            catalog.addColumn(indexName)
+            reindex = True
+    if reindex:
+        catalog.manage_reindexIndex()
+
+
+def install_templates(out, site):
+    at = getToolByName(site, TOOL_NAME)
+    at.registerTemplate('base_view', 'Base View')
+
+
+def setupArchetypes(context):
+    """
+    Setup Archetypes step.
+    """
+    # Only run step if a flag file is present (e.g. not an extension profile)
+    if context.readDataFile('archetypes-various.txt') is None:
+        return
+    out = []
+    site = context.getSite()
+    install_uidcatalog(out, site)
+    install_referenceCatalog(out, site)
+    install_templates(out, site)

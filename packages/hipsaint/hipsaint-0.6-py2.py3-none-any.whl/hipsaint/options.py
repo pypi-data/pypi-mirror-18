77881a35afe04bbcf0a2a9298addc1a37894a288
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:07:54 +0000

    Add package hipsaint/0.6/hipsaint-0.6-py2.py3-none-any.whl

---
+++ packages/hipsaint/hipsaint-0.6-py2.py3-none-any.whl/hipsaint/options.py
@@ -0,0 +1,14 @@
+COLORS = {
+    'PROBLEM': 'red',
+    'RECOVERY': 'green',
+    'ACKNOWLEDGEMENT': 'purple',
+    'FLAPPINGSTART': 'yellow',
+    'WARNING': 'yellow',
+    'UNKNOWN': 'gray',
+    'CRITICAL': 'red',
+    'FLAPPINGEND': 'green',
+    'FLAPPINGSTOP': 'green',
+    'FLAPPINGDISABLED': 'purple',
+    'DOWNTIMESTART': 'red',
+    'DOWNTIMESTOP': 'green'
+}

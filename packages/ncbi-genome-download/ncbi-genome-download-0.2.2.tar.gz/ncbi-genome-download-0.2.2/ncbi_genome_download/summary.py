dfa52ed4c71d2a026c1f808142f073f55c4c0259
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:10:15 +0000

    Add package ncbi-genome-download/0.2.2/ncbi-genome-download-0.2.2.tar.gz

---
+++ packages/ncbi-genome-download/ncbi-genome-download-0.2.2.tar.gz/ncbi-genome-download-0.2.2/ncbi_genome_download/summary.py
@@ -0,0 +1,32 @@
+'''Parse NCBI RefSeq/GenBank summary files'''
+
+
+# pylint: disable=too-few-public-methods
+class SummaryReader(object):
+    '''An Iterator-like class for assembly summary files'''
+    def __init__(self, infile):
+        self._file = infile
+        line = ''
+        while 'assembly_accession' not in line:
+            line = self._file.readline().rstrip('\n')
+
+        if line.startswith('# '):
+            line = line[2:]
+
+        self._fields = line.split('\t')
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        entry = {}
+        line = self._file.readline().rstrip('\n')
+        if line == '':
+            raise StopIteration
+        parts = line.split('\t')
+        for i, val in enumerate(parts):
+            entry[self._fields[i]] = val
+        return entry
+
+    next = __next__
+# pylint: enable=too-few-public-methods

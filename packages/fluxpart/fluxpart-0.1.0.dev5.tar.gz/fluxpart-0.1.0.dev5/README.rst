35482b8a04a51c27793eace31989c96207215374
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:20:00 +0000

    Add package fluxpart/0.1.0.dev5/fluxpart-0.1.0.dev5.tar.gz

---
+++ packages/fluxpart/fluxpart-0.1.0.dev5.tar.gz/fluxpart-0.1.0.dev5/README.rst
@@ -0,0 +1,39 @@
+========
+Fluxpart
+========
+
+-----------------------------------------------------------------------
+Python 3 module for partitioning water vapor and carbon dioxide fluxes.
+-----------------------------------------------------------------------
+
+.. image:: http://www.usda.gov/img/headers/USDA-Logo.png
+        :target: https://www.usda.gov/
+        :alt: U.S. Department of Agriculture
+
+`Agricultural Research Service <https://www.ars.usda.gov>`_
+
+`U.S. Salinity Laboratory <https://www.ars.usda.gov/main/site_main.htm?modecode=20-36-05-00>`_
+
+
+Links
+=====
+
+* Home page: TODO
+* Documentation: https://fluxpart.readthedocs.io
+* Source code: https://github.com/usda-ars-ussl/fluxpart
+
+
+Features
+========
+
+* Implements the Scanlon and Sahu (2008) procedure for partitioning measured
+  water vapor and carbon dioxide fluxes into stomatal (transpiration,
+  photosynthesis) and nonstomatal (evaporation, respiration) components.
+
+
+Citing fluxpart
+===============
+
+If you use fluxpart in your research or project, please cite 
+
+* TODO
---
+++ packages/fluxpart/fluxpart-0.1.0.dev5.tar.gz/fluxpart-0.1.0.dev5/setup.py
@@ -0,0 +1,39 @@
+import os
+from setuptools import setup
+
+# with open('README.rst') as readme_file:
+#     README = readme_file.read()
+
+# with open('HISTORY.rst') as history_file:
+#     HISTORY = history_file.read()
+
+VERSION = '0.1.0dev5'
+
+LONG = ("Python 3 module implementing the Scanlon and Sahu (2008) procedure "
+        "for partitioning measured water vapor and carbon dioxide fluxes into "
+        "stomatal (transpiration, photosynthesis) and nonstomatal "
+        "(evaporation, respiration) components.")
+
+setup(
+    name='fluxpart',
+    version=VERSION,
+    description='Partition water vapor and carbon dioxide fluxes',
+    long_description=LONG,
+    url='https://github.com/usda-ars-ussl/fluxpart',
+    author='Todd Skaggs',
+    author_email='todd.skaggs@ars.usda.gov',
+    license='Public Domain CC0',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Science/Research',
+        'Topic :: Scientific/Engineering',
+        'License :: CC0 1.0 Universal (CC0 1.0) Public Domain Dedication',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3.5',
+    ],
+    packages=['fluxpart'],
+    install_requires=['numpy', 'scipy', 'matplotlib', 'pywavelets'],
+    zip_safe=False,
+    tests_require=['pytest'],
+    test_suite='tests',
+)

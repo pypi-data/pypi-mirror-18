f6bc6545996a9897f2256da862b359a54b925b5a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:07:47 +0000

    Add package gym_ram/0.2/gym_ram-0.2.tar.gz

---
+++ packages/gym_ram/gym_ram-0.2.tar.gz/gym_ram-0.2/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='gym_ram',
+      version='0.2',
+      install_requires=['gym','keras'],  # And any other dependencies foo needs
+      packages=['gym_ram']
+)  

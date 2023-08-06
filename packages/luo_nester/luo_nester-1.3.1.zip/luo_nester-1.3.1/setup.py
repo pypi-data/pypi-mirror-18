a3199668fcb54722c25711b898cd130fc380671b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:09:06 +0000

    Add package luo_nester/1.3.1/luo_nester-1.3.1.zip

---
+++ packages/luo_nester/luo_nester-1.3.1.zip/luo_nester-1.3.1/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+		name = 'luo_nester',
+		version = '1.3.1',
+		py_modules = ['nester'],
+		author = 'Luo',
+		author_email='13390583227@163.com',
+		url = 'https://mail.qq.com/cgi-bin/frame_html?sid=Pjx32NOH4lB4A2kx&r=7b74b2130705806e8883e2cac83f536c',
+		description = 'The function of looping output',
+	)

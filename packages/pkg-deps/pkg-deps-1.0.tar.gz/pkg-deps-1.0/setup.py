349db173425e2c0186f8399b73d44e9735dc7afe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:17:03 +0000

    Add package pkg-deps/1.0/pkg-deps-1.0.tar.gz

---
+++ packages/pkg-deps/pkg-deps-1.0.tar.gz/pkg-deps-1.0/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+import sys
+
+
+tests_require = ['virtualenv']
+
+if sys.version_info < (3, 2):
+    tests_require.append('subprocess32')
+
+setup(
+    name="pkg-deps",
+    version="1.0",
+    license="Public Domain",
+    packages=find_packages(),
+    install_requires=['pydotplus', 'networkx', 'click'],
+    tests_require=tests_require,
+    # entry_points based script is really slow (0.5 seconds)
+    # might want to switch to normal script, since windows
+    # version isn't a concern right now
+    entry_points={
+        'console_scripts': [
+            'pkg-deps = pkg_deps.main:main',
+        ],
+    },
+    extras_require={
+        'teamcity': ['teamcity-messages'],
+    },
+    test_suite='tests.tests',
+    zip_safe=False,
+)

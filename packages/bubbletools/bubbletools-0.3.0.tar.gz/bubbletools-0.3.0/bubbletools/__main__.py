6e3b3e2e77601223153ce7c37e61d0b72240c205
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:10:54 +0000

    Add package bubbletools/0.3.0/bubbletools-0.3.0.tar.gz

---
+++ packages/bubbletools/bubbletools-0.3.0.tar.gz/bubbletools-0.3.0/bubbletools/__main__.py
@@ -0,0 +1,38 @@
+"""Bubble format related tools
+
+usage:
+    bubble-tool.py validate <bblfile> [--profiling]
+    bubble-tool.py dot <bblfile> [<dotfile>] [--render]
+    bubble-tool.py gexf <bblfile> [<gexffile>]
+
+"""
+
+
+import docopt
+
+from bubbletools import validator
+from bubbletools import converter
+from bubbletools import utils
+
+
+if __name__ == "__main__":
+    args = docopt.docopt(__doc__)
+
+    if args['validate']:
+        logs = validator.validate(utils.file_lines(args['<bblfile>']),
+                                  profiling=args['--profiling'])
+        for log in logs:
+            print(log)
+
+    if args['dot']:
+        print('Output file:', converter.bubble_to_dot(
+            args['<bblfile>'],
+            args['<dotfile>'],
+            render=args['--render']
+        ))
+
+    if args['gexf']:
+        print('Output file:', converter.bubble_to_gexf(
+            args['<bblfile>'],
+            args['<gexffile>'],
+        ))

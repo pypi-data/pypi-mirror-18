fdb7f46da1cb6a1d74b38bf7d3c40bd2376b17fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:12:32 +0000

    Add package pyagrum/0.10.0.1/pyagrum-0.10.0.1-cp35-cp35m-win_amd64.whl

---
+++ packages/pyagrum/pyagrum-0.10.0.1-cp35-cp35m-win_amd64.whl/pyAgrum/functions.py
@@ -0,0 +1,166 @@
+#- * - coding : utf - 8 - * -
+"""
+Helping functions and consts for pyAgrum
+"""
+
+#aGrum Licence( GPL )
+#-- -- -- -- -- -- -- -- -- -
+#* This program is free software; you can redistribute it and / or modify *
+#* it under the terms of the GNU General Public License as published by *
+#* the Free Software Foundation; either version 2 of the License, or *
+#*( at your option ) any later version.*
+#* *
+#* This program is distributed in the hope that it will be useful, *
+#* but WITHOUT ANY WARRANTY; without even the implied warranty of *
+#* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the *
+#* GNU General Public License for more details.*
+#* *
+#* You should have received a copy of the GNU General Public License *
+#* along with this program; if not, write to the *
+#* Free Software Foundation, Inc., *
+#* 59 Temple Place - Suite 330, Boston, MA 02111 - 1307, USA.
+
+from .pyAgrum import BayesNet
+from .pyAgrum import Potential
+from .pyAgrum import InfluenceDiagram
+from .pyAgrum import LazyPropagation
+
+def about():
+  """
+  about() for pyAgrum
+
+  """
+  print("pyAgrum version {0}".format('0.10.0'))
+  print("(c) Pierre-Henri Wuillemin, Christophe Gonzales, Lionel Torti")
+  print("    UPMC 2015")
+  print("""
+    This is free software; see the source code for copying conditions.
+    There is ABSOLUTELY NO WARRANTY; not even for MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  For details, see 'pyAgrum.warranty'.
+    """)
+
+def availableBNExts():
+    """
+    :return: a string which lists all suffixes for supported BN file formats.
+    """
+    return "bif|dsl|net|bifxml|o3prm|uai"
+
+def loadBN(filename,listeners=None,verbose=True,**opts):
+    """
+    :param filename: the name of file
+    :return: a BN from a file using one of the availableBNExts() suffixes.
+    """
+    bn=BayesNet()
+
+    extension=filename.split('.')[-1].upper()
+    if extension=="BIF":
+        warns=bn.loadBIF(filename,listeners)
+    elif extension=="BIFXML":
+        warns=bn.loadBIFXML(filename,listeners)
+    elif extension=="DSL":
+        warns=bn.loadDSL(filename,listeners)
+    elif extension=="NET":
+        warns=bn.loadNET(filename,listeners)
+    elif extension=="O3PRM":
+        warns=bn.loadPRM(filename,opts.get('system',''),opts.get('classpath',''),listeners)
+    elif extension=="UAI":
+        warns=bn.loadUAI(filename,listeners)
+    else:
+        raise Exception("extension "+filename.split('.')[-1]+" unknown. Please use among "+availableBNExts())
+
+    if verbose:
+      print(warns)
+
+    bn.setProperty("name",filename)
+    return bn
+
+def saveBN(bn,filename):
+    """
+    save a BN into a file using the format corresponding to one of the availableBNExts() suffixes.
+    """
+    extension=filename.split('.')[-1].upper()
+    if extension=="BIF":
+        bn.saveBIF(filename)
+    elif extension=="BIFXML":
+        bn.saveBIFXML(filename)
+    elif extension=="DSL":
+        bn.saveDSL(filename)
+    elif extension=="NET":
+        bn.saveNET(filename)
+    elif extension=="UAI":
+        bn.saveUAI(filename)
+    else:
+        raise Exception("extension "+filename.split('.')[-1]+" unknown. Please use "+availableBNExts())
+
+
+
+def loadID(filename):
+  """
+  read a gum.InfluenceDiagram from a bifxml file
+
+  :param filename: the name of file
+  :return: an InfluenceDiagram
+  """
+
+  extension=filename.split('.')[-1].upper()
+  if extension!="BIFXML":
+    raise Exception("extension "+extension+" unknown. Please use bifxml.")
+
+  diag=InfluenceDiagram()
+  res=diag.loadBIFXML(filename)
+
+  if not res:
+    raise Exception("Error(s) in "+filename)
+
+  diag.setProperty("name",filename)
+  return diag
+
+
+def fastBN(arcs,domain_size=2):
+  """
+  rapid prototyping of BN.
+
+  :param arcs: dot-like simple list of arcs ("a->b->c;a->c->d" for instance)
+  :param domain_size: number of modalities for each created variable.
+  :return: the created pyAgrum.BayesNet
+  """
+  def _getId(bn,a):
+    try:
+      ia=bn.idFromName(a)
+    except IndexError:
+      ia=bn.add(a,domain_size)
+    return ia
+
+  bn=BayesNet()
+  for arc in arcs.split(";"):
+    if arc!="":
+      l=arc.split("->")
+      if len(l)==1: # cannot be 0
+        _getId(bn,l[0])
+      else:
+        for a, b in zip(l[:-1], l[1:]):
+          ia=_getId(bn,a)
+          ib=_getId(bn,b)
+          bn.addArc(ia,ib)
+  bn.generateCPTs()
+  return bn
+
+def getPosterior(bn, evs, target):
+  """
+  Compute the posterior of a single target (variable) in a BN given evidence using Lazy Propagation (for now).
+
+  :param bn:
+  :type bn: pyAgrum.BayesNet
+  :param evs: events map {
+  name;
+  val, name : [ val1, val2 ], ...}
+  :type evs: dictionary
+  :param target: variable name
+  :type target: str
+  :return: posterior Potential
+  """
+  inf = LazyPropagation(bn)
+  inf.setEvidence(evs)
+  inf.makeInference()
+  return Potential(inf.posterior(bn.idFromName(target)))
+#creating a new Potential from posterior( will disappear with ie )

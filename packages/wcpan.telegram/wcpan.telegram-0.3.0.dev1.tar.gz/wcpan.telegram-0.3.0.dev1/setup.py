e6312ab25c444bd58b1996396ddd17ceaeeb676c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:18:42 +0000

    Add package wcpan.telegram/0.3.0.dev1/wcpan.telegram-0.3.0.dev1.tar.gz

---
+++ packages/wcpan.telegram/wcpan.telegram-0.3.0.dev1.tar.gz/wcpan.telegram-0.3.0.dev1/setup.py
@@ -0,0 +1,42 @@
+import os.path as op
+
+from setuptools import setup, find_packages
+
+
+with open(op.join(op.dirname(__file__), './README.rst')) as fin:
+    long_description = fin.read()
+
+
+setup(
+    name='wcpan.telegram',
+
+    version='0.3.0.dev1',
+
+    description='Telegram Bot API with Tornado',
+    long_description=long_description,
+
+    url='https://github.com/legnaleurc/wcpan.telegram',
+
+    author='Wei-Cheng Pan',
+    author_email='legnaleurc@gmail.com',
+
+    license='MIT',
+
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+
+        'Intended Audience :: Developers',
+        'Topic :: Communications :: Chat',
+
+        'License :: OSI Approved :: MIT License',
+
+        'Programming Language :: Python :: 3 :: Only',
+        'Programming Language :: Python :: 3.5',
+    ],
+
+    keywords='telegram bot tornado',
+
+    packages=find_packages(exclude=['contrib', 'docs', 'tests*']),
+
+    install_requires=['tornado >= 4'],
+)

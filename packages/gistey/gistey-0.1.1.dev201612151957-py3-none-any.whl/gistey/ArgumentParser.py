a5d0b6beadd2b42560eec7a9ce64c24b20013706
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:20:17 +0000

    Add package gistey/0.1.1.dev201612151957/gistey-0.1.1.dev201612151957-py3-none-any.whl

---
+++ packages/gistey/gistey-0.1.1.dev201612151957-py3-none-any.whl/gistey/ArgumentParser.py
@@ -0,0 +1,19 @@
+import argparse
+
+parser = argparse.ArgumentParser(description="Create github gists from cmdline",
+                                 prog="gistey")
+
+parser.add_argument('-s', '--secret',
+                    default=False,
+                    help="To create a secret gist",
+                    action='store_true')
+
+parser.add_argument('-f', '--files',
+                    required=True,
+                    nargs='+',
+                    help="Files to be uploaded in the gist",
+                    action='store')
+
+parser.add_argument('-d', '--description',
+                    help='Description for the gist',
+                    action='store')

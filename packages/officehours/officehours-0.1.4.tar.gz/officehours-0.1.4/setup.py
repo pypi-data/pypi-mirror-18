6e5e3b3cb29653e7918384dd025e86e5de960b08
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:46:11 +0000

    Add package officehours/0.1.4/officehours-0.1.4.tar.gz

---
+++ packages/officehours/officehours-0.1.4.tar.gz/officehours-0.1.4/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+
+setup(
+    name = 'officehours',
+    packages = ['officehours'],
+    version = '0.1.4',
+    description = 'Utility to calculate time intervals in working hours',
+    author = 'Guillermo Guirao Aguilar',
+    author_email = 'contact@guillermoguiraoaguilar.com',
+    url = 'https://github.com/Funk66/officehours.git',
+    keywords = ['time', 'hours', 'office', 'business', 'work'],
+)

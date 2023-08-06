25d8254221977c0a160c0360840ff565e4d1f493
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:30:59 +0000

    Add package slackbot-wems/0.2.480266113/slackbot_wems-0.2.480266113.tar.gz

---
+++ packages/slackbot-wems/slackbot_wems-0.2.480266113.tar.gz/slackbot_wems-0.2.480266113/slackbot_wems/wems/kent/slacklib.py
@@ -0,0 +1,33 @@
+# Put your commands here
+COMMAND1 = "hi"
+COMMAND2 = "am i a hacker?"
+COMMAND3 = "who is the master"
+COMMAND4 = "why do you have so many commands?"
+COMMAND5 = "kecko"
+COMMAND6 = "time to eat the cake!"
+COMMAND7 = "name 4 names of cats"
+
+# Your handling code goes in this function
+def handle_command(command):
+    """
+        Determine if the command is valid. If so, take action and return
+        a response, if necessary.
+    """
+    response = ""
+    if command.find(COMMAND1) >= 0:
+        response = "Hi Minecraftman"
+    
+    elif command.find(COMMAND2) >= 0:
+        response = "yes, you are destined to be a hackerF"
+    elif command.find(COMMAND3) >= 0:
+        response = "Kent is the master, all hail Kent"
+    elif command.find(COMMAND4) >= 0:
+        response = "the WEMS students are giving them to me"
+    elif command.find(COMMAND5) >= 0:
+        response = "happy birthday! @kecko made it your birthday! the next birthday command is: time to eat the cake!"
+    elif command.find(COMMAND6) >= 0:
+        response = "someone stole the cake, it was Dean!"
+    elif command.find(COMMAND7) >= 0:
+        response = "Caspian and Luna -- who are Kent's cats -- and Huckle and Nugget -- who are Emerson's cats"
+        
+    return response

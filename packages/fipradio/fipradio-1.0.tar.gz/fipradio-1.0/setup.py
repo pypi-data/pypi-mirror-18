45b4b22e1eec7bca0bdfd0201e6b9d981d3cfd5c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:49:13 +0000

    Add package fipradio/1.0/fipradio-1.0.tar.gz

---
+++ packages/fipradio/fipradio-1.0.tar.gz/fipradio-1.0/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(name="fipradio",
+      version="1.0",
+      author="Alexandre Macabies",
+      description="Command line tool to start & stop the stream and mute "
+                  "unwanted songs from FIP radio",
+      url="https://github.com/zopieux/fipradio",
+      license="MIT",
+      classifiers=["Topic :: Multimedia :: Sound/Audio :: Players",
+                   "Environment :: Console",
+                   "Development Status :: 4 - Beta",
+                   "License :: OSI Approved :: MIT License",
+                   "Programming Language :: Python :: 3",
+                   "Programming Language :: Python :: 3 :: Only"],
+      install_requires=['aiohttp'],
+      py_modules=['fipradio'],
+      scripts=['fip'])

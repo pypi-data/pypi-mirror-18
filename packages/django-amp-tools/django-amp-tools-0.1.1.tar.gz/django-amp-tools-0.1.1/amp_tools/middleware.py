e93878909cb9649756995132a68256a74c5aa72c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:19:11 +0000

    Add package django-amp-tools/0.1.1/django-amp-tools-0.1.1.tar.gz

---
+++ packages/django-amp-tools/django-amp-tools-0.1.1.tar.gz/django-amp-tools-0.1.1/amp_tools/middleware.py
@@ -0,0 +1,13 @@
+
+from amp_tools.settings import settings
+from amp_tools import set_amp_detect
+
+
+class AMPDetectionMiddleware(object):
+
+    def process_request(self, request):
+        if settings.AMP_TOOLS_GET_PARAMETER in request.GET:
+            if request.GET[settings.AMP_TOOLS_GET_PARAMETER] == settings.AMP_TOOLS_GET_VALUE:
+                set_amp_detect(is_amp_detect=True, request=request)
+        else:
+            set_amp_detect(is_amp_detect=False, request=request)

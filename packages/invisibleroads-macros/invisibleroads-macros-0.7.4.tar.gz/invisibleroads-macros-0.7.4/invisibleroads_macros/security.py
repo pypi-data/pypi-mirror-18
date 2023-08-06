2ae45476d2306c3962cd5666462c97efee13e694
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:21:49 +0000

    Add package invisibleroads-macros/0.7.4/invisibleroads-macros-0.7.4.tar.gz

---
+++ packages/invisibleroads-macros/invisibleroads-macros-0.7.4.tar.gz/invisibleroads-macros-0.7.4/invisibleroads_macros/security.py
@@ -0,0 +1,14 @@
+import random
+import string
+
+
+ALPHABET = string.digits + string.letters
+
+
+def make_random_string(
+        length, with_punctuation=False, with_spaces=False, alphabet=ALPHABET):
+    if with_punctuation:
+        alphabet += string.punctuation
+    if with_spaces:
+        alphabet += ' '
+    return ''.join(random.choice(alphabet) for x in range(length))

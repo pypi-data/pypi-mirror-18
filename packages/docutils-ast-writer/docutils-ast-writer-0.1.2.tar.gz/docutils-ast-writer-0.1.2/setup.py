bc4aa0aabb5fd21b61170982d403a6c657be3fe1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:43:42 +0000

    Add package docutils-ast-writer/0.1.2/docutils-ast-writer-0.1.2.tar.gz

---
+++ packages/docutils-ast-writer/docutils-ast-writer-0.1.2.tar.gz/docutils-ast-writer-0.1.2/setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+setup(
+    name='docutils-ast-writer',
+    description='AST Writer for docutils',
+    version='0.1.2',
+    author='jimo1001',
+    author_email='jimo1001@gmail.com',
+    license='MIT',
+    url='https://github.com/jimo1001/docutils-ast-writer',
+    packages=find_packages(),
+    install_requires=[
+        'docutils>=0.12'
+    ],
+    entry_points="""
+        [console_scripts]
+        rst2ast = rst2ast.cmd:run
+    """,
+    use_2to3 = True
+)

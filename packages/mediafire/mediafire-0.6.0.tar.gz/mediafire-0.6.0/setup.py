461aff6cf38b1606ed0b680c28dbb8abf2eda545
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:07:45 +0000

    Add package mediafire/0.6.0/mediafire-0.6.0.tar.gz

---
+++ packages/mediafire/mediafire-0.6.0.tar.gz/mediafire-0.6.0/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup
+from pip.req import parse_requirements
+import uuid
+
+requirements = parse_requirements('requirements.txt', session=uuid.uuid1())
+install_requires = [str(r.req) for r in requirements]
+
+setup(
+    name='mediafire',
+    version='0.6.0',
+    author='Roman Yepishev',
+    author_email='rye@keypressure.com',
+    packages=['mediafire', 'mediafire.media'],
+    url='https://github.com/MediaFire/mediafire-python-open-sdk',
+    license='BSD',
+    description='Python MediaFire client library',
+    long_description=open('README.rst').read(),
+    install_requires=install_requires,
+    keywords="mediafire cloud files sdk storage api upload",
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'License :: OSI Approved :: BSD License'
+    ]
+)

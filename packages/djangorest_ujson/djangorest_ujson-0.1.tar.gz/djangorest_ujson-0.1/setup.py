e2f2837e837c44ddd316f157fd9603f9c8cd92da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:31:03 +0000

    Add package djangorest_ujson/0.1/djangorest_ujson-0.1.tar.gz

---
+++ packages/djangorest_ujson/djangorest_ujson-0.1.tar.gz/djangorest_ujson-0.1/setup.py
@@ -0,0 +1,3 @@
+from setuptools import setup
+setup(name='djangorest_ujson', version='0.1', description='JSON Renderer and Parser for Django Rest Framework using the ultra fast json (in C).', url='https://github.com/akshaysharma096/drf_ujson',
+      author='Akshay Sharma', author_email='mailbag.akshay@gmail.com', license='MIT', classifiers=['Framework :: Django', 'Programming Language :: Python', 'Intended Audience :: Developers', 'Topic :: Software Development :: Libraries :: Python Modules'], keywords='django djangorestframework restframework djangorest parser renderer', packages=['drf_ujson'], zip=False, install_requires=['ujson', 'django', 'djangorestframework'])

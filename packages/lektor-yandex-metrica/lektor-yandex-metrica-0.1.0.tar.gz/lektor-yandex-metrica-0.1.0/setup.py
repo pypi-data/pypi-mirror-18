df040ed259ea0c335d85377234914aa21f534a51
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:30:08 +0000

    Add package lektor-yandex-metrica/0.1.0/lektor-yandex-metrica-0.1.0.tar.gz

---
+++ packages/lektor-yandex-metrica/lektor-yandex-metrica-0.1.0.tar.gz/lektor-yandex-metrica-0.1.0/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+
+setup(
+    name='lektor-yandex-metrica',
+    version='0.1.0',
+    author='Alex Khomchenko',
+    author_email='akhomchenko@gmail.com',
+    url='https://github.com/gagoman/lektor-yandex-metrica',
+    license='MIT',
+    description='Adds support for Yandex Metrica to Lektor CMS',
+    long_description=open('README.rst').read(),
+    py_modules=['lektor_yandex_metrica'],
+    entry_points={
+        'lektor.plugins': [
+            'yandex-metrica = lektor_yandex_metrica:YandexMetricaPlugin',
+        ]
+    },
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3'
+    ]
+)

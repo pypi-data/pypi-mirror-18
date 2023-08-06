a7bacd74342a7ee443899644e72745c8a979ebb7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:08:49 +0000

    Add package betfairlightweight/0.8.2/betfairlightweight-0.8.2.tar.gz

---
+++ packages/betfairlightweight/betfairlightweight-0.8.2.tar.gz/betfairlightweight-0.8.2/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+from betfairlightweight.__init__ import __version__
+
+
+INSTALL_REQUIRES = [
+    'requests == 2.10.0',
+]
+TEST_REQUIRES = [
+    'mock'
+]
+
+setup(
+        name='betfairlightweight',
+        version=__version__,
+        packages=['betfairlightweight', 'betfairlightweight.endpoints',
+                  'betfairlightweight.resources', 'betfairlightweight.streaming'],
+        package_dir={'betfairlightweight': 'betfairlightweight'},
+        install_requires=INSTALL_REQUIRES,
+        requires=['requests'],
+        url='https://github.com/liampauling/betfairlightweight',
+        license='MIT',
+        author='liampauling',
+        author_email='',
+        description='Lightweight python wrapper for Betfair API-NG',
+        test_suite='tests'
+)

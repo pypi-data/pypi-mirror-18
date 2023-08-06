9b32c8d088e4df9c7c2090fb746e87b6efb7231a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:12:02 +0000

    Add package nester_161203/1.3.9/nester_161203-1.3.9.tar.gz

---
+++ packages/nester_161203/nester_161203-1.3.9.tar.gz/nester_161203-1.3.9/setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name        = 'nester_161203',
+    version     = '1.3.9',
+    py_modules  = ['nester'],
+    author      = 'Shawn Kim',
+    author_email= 'sgreen22@gmail.com'
+)

d6729e7e15f2744dd4eea66989ef2d8d12c9ba2d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:12:05 +0000

    Add package ym-impyla/0.14.0/ym-impyla-0.14.0.tar.gz

---
+++ packages/ym-impyla/ym-impyla-0.14.0.tar.gz/ym-impyla-0.14.0/setup.py
@@ -0,0 +1,77 @@
+# Copyright 2013 Cloudera Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from __future__ import absolute_import
+
+import re
+import sys
+
+import ez_setup
+
+ez_setup.use_setuptools()
+
+from setuptools import setup, find_packages
+
+
+def readme():
+    with open('README.md', 'r') as ip:
+        return ip.read()
+
+
+PY2 = sys.version_info[0] == 2
+PY3 = sys.version_info[0] == 3
+
+# Apache Thrift does not yet support Python 3 (see THRIFT-1857).  We use
+# thriftpy as a stopgap replacement
+reqs = ['six', 'bitarray']
+if PY2:
+    packages = find_packages()
+    reqs.append('thrift')
+elif PY3:
+    packages = find_packages(exclude=['impala._thrift_gen',
+                                      'impala._thrift_gen.*'])
+    reqs.append('thriftpy>=0.3.5')
+
+with open('impala/__init__.py', 'r') as fd:
+    version = re.search(r'^__version__\s*=\s*[\'"]([^\'"]*)[\'"]',
+                        fd.read(), re.MULTILINE).group(1)
+
+setup(
+    name='ym-impyla',
+    version=version,
+    description='Python client for the Impala distributed query engine',
+    long_description=readme(),
+    maintainer='Wes McKinney',
+    maintainer_email='wes.mckinney@twosigma.com',
+    author='Uri Laserson',
+    author_email='laserson@cloudera.com',
+    url='https://github.com/yimian/impyla',
+    packages=packages,
+    install_package_data=True,
+    package_data={'impala.thrift': ['*.thrift']},
+    install_requires=reqs,
+    keywords=('cloudera impala python hadoop sql hdfs mpp spark pydata '
+              'pandas distributed db api pep 249 hive hiveserver2 hs2'),
+    license='Apache License, Version 2.0',
+    classifiers=[
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5'
+    ],
+    entry_points={
+        'sqlalchemy.dialects': ['impala = impala.sqlalchemy:ImpalaDialect']},
+    zip_safe=False)

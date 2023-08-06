f30f8c2e7fe2d9e147ef344743f73c64fb9ed88f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:45:22 +0000

    Add package dis/1.0.0/dis-1.0.0.tar.gz

---
+++ packages/dis/dis-1.0.0.tar.gz/dis-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name = 'dis',
+    version ='1.0.0',
+    py_modules = ['nester'],
+    author = 'sandy',
+    author_email = 'sandyzhao@wisehandy.com',
+    url = 'http://www.wisehandy.com',
+    description = 'A simple test',
+    )

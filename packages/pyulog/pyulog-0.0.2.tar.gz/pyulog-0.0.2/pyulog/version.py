fa92254af3e0cf5fac0505e08baf3c8d0007d8d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:13:41 +0000

    Add package pyulog/0.0.2/pyulog-0.0.2.tar.gz

---
+++ packages/pyulog/pyulog-0.0.2.tar.gz/pyulog-0.0.2/pyulog/version.py
@@ -0,0 +1,10 @@
+
+# THIS FILE IS GENERATED FROM SETUP.PY
+short_version = '0.0.2'
+version = '0.0.2'
+full_version = '0.0.2'
+git_revision = 'c19295bfc664df2925bacdc22ac6695c29d7c9be'
+release = True
+
+if not release:
+    version = full_version

1c881e1a23d46fcb18a55274ddcec6bd41ad6a47
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:08:20 +0000

    Add package Koptool/2.8/Koptool-2.8-py2-none-any.whl

---
+++ packages/Koptool/Koptool-2.8-py2-none-any.whl/koptool/offline_api.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+# coding = utf-8
+import requests
+import json
+import sys
+from list_api import __list_api__
+
+def __offline_api__(service_name,api_name):
+    username = 'CHENJIAXIONG'
+    password = '!chen1994'
+    flag = 1
+    res_api = __list_api__(service_name)
+    for api in res_api:
+        if api['action'] == api_name:
+            id = api['id']
+            flag = 2
+            url = 'http://inner.kop.com/v2/api/'+str(id)+'/offline'
+            res = requests.get(url,auth=(username,password))
+            return res
+    if flag == 1:
+        print 'there is no such api'
+
+#if __name__ == '__main__':
+#    result = __offline_api__(sys.argv[1],sys.argv[2])
+ #   print result
+

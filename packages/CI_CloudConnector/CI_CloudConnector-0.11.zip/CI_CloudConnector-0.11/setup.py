237c9b0071664a33163af9812f7874e024bec7cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:18:37 +0000

    Add package CI_CloudConnector/0.11/CI_CloudConnector-0.11.zip

---
+++ packages/CI_CloudConnector/CI_CloudConnector-0.11.zip/CI_CloudConnector-0.11/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(name='CI_CloudConnector',
+      version='0.11',
+      py_modules=['CI_CloudConnector' , 'CI_LC_BL'],
+	  description="IOT application that collect data from PLC (ModBus or AnB Ethernet/IP) and send to cloud using https",
+	  author="Ido Peles",
+	  author_email="idop@contel.co.il",
+          install_requires=['pymodbus' , 'cpppo', 'logging', 'pip'],
+      )
+	  

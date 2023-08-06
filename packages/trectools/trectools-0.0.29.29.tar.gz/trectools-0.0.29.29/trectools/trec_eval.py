83ddf08050f1cd3846dd3923c2ea71f58b6a7a2d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:17:49 +0000

    Add package trectools/0.0.29.29/trectools-0.0.29.29.tar.gz

---
+++ packages/trectools/trectools-0.0.29.29.tar.gz/trectools-0.0.29.29/trectools/trec_eval.py
@@ -0,0 +1,21 @@
+from trectools import TrecRun
+
+class TrecEval:
+    def __init__(self, run, qrel):
+        pass
+
+    def evaluateAll():
+        pass
+
+    def getReturnedDocuments():
+        pass
+
+    def getRelevantDocuments():
+        pass
+
+    def getRunId():
+        pass
+
+    def getPrecisionAtX():
+        pass
+

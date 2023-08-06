b6f52d48706f55bee71f37aac77fa3f367f105e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:18:48 +0000

    Add package data-importer/3.0.2/data-importer-3.0.2.tar.gz

---
+++ packages/data-importer/data-importer-3.0.2.tar.gz/data-importer-3.0.2/data_importer/__init__.py
@@ -0,0 +1,4 @@
+# encoding: utf-8
+__doc__ = 'Data Importer'
+__version__ = '3.0.2'
+__author__ = 'Valder Gallo <valdergallo@gmail.com>'
---
+++ packages/data-importer/data-importer-3.0.2.tar.gz/data-importer-3.0.2/setup.py
@@ -0,0 +1,71 @@
+# encoding: utf-8
+import os
+import sys
+from setuptools import setup, find_packages
+from setuptools.command.test import test as TestCommand
+import data_importer
+
+
+def readme():
+    try:
+        os.system('pandoc --from=markdown --to=rst README.md -o README.rst')
+        with open('README.rst') as f:
+            return f.read()
+    except Exception:
+        return '''**Django Data Importer** is a tool which allow you to transform easily a CSV, XML, XLS and XLSX file into a python object or a django model instance. It is based on the django-style declarative model.'''
+
+
+class PyTest(TestCommand):
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+        self.test_args = ['data_importer', 'tests', '--cov=data_importer', '-vrsx']
+        self.test_suite = True
+
+    def run_tests(self):
+        # import here, cause outside the eggs aren't loaded
+        import pytest
+        errno = pytest.main(self.test_args)
+        sys.exit(errno)
+
+
+setup(
+    name='data-importer',
+    url='https://github.com/valdergallo/data-importer',
+    download_url='https://github.com/valdergallo/data-importer/tarball/{0!s}/'.format(data_importer.__version__),
+    author="valdergallo",
+    author_email='valdergallo@gmail.com',
+    keywords='Django Data Importer XLS XLSX CSV XML',
+    description='Simple library to easily import data with Django',
+    license='BSD',
+    long_description=readme(),
+    classifiers=[
+      'Framework :: Django',
+      'Operating System :: OS Independent',
+      'Topic :: Utilities'
+    ],
+    version=data_importer.__version__,
+    install_requires=[
+        'django>=1.4',
+        'openpyxl==2.4.0',
+        'xlrd==1.0.0',
+        'six==1.10.0',
+    ],
+    tests_require=[
+        'pytest>=3.0.0',
+        'pytest-django==2.9.1',
+        'pytest-cov==2.3.1',
+        'openpyxl==2.4.0',
+        'xlrd>=1.0.0',
+        'django>=1.4',
+        'six==1.10.0',
+        'mock==2.0.0',
+    ],
+    cmdclass={'test': PyTest},
+    zip_safe=False,
+    platforms='any',
+    package_dir={'': '.'},
+    packages=find_packages('.', exclude=['tests', '*.tests', 'docs', 'example', 'media']),
+    package_data={
+        '': ['templates/data_importer.html', 'templates/my_upload.html']
+    }
+)

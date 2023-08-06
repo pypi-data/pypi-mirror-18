84bddd365fd72a8b4ac182183055221442f891db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:22:36 +0000

    Add package django-bigbuild/0.1.7/django_bigbuild-0.1.7-py2.py3-none-any.whl

---
+++ packages/django-bigbuild/django_bigbuild-0.1.7-py2.py3-none-any.whl/bigbuild/management/commands/retirepage.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import os
+import shutil
+from django.conf import settings
+from bigbuild.views import PageRetireView
+from bigbuild.models import PageList, Page
+from django.core.management.base import BaseCommand, CommandError
+
+
+class Command(BaseCommand):
+    help = "Retire a page directory by permanently rendering its HTML"
+    args = "<slug>"
+
+    def add_arguments(self, parser):
+        parser.add_argument('slug', nargs='+', type=str)
+        parser.add_argument(
+            '--keep-page',
+            action='store_true',
+            dest='keep_page',
+            default=False,
+            help='Do not delete the page directory as part of the retirement'
+        )
+
+    def handle(self, *args, **options):
+        # Loop through the slugs
+        for slug in options['slug']:
+            # Pull the object
+            try:
+                p = PageList()[slug]
+            except:
+                raise CommandError("Slug provided (%s) does not exist" % slug)
+
+            if not isinstance(p, Page):
+                raise CommandError("Slug (%s) is not a Page object" % slug)
+
+            # Build it
+            PageRetireView().build_object(p)
+
+            # If the retired directory exists, kill it
+            if os.path.exists(p.retired_directory_path):
+                shutil.rmtree(p.retired_directory_path)  # pragma: no cover
+
+            # Save that directory to the retired folder
+            shutil.copytree(
+                os.path.join(settings.BUILD_DIR, p.get_absolute_url()[1:]),
+                p.retired_directory_path,
+            )
+
+            # Save the metadata to the retired folder
+            frontmatter_path = os.path.join(p.retired_directory_path, 'metadata.md')
+            p.write_frontmatter(frontmatter_path)
+
+            # Delete the page folder
+            if not options['keep_page']:
+                shutil.rmtree(p.directory_path)
---
+++ packages/django-bigbuild/django_bigbuild-0.1.7-py2.py3-none-any.whl/bigbuild/urls.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from django.conf.urls import url
+from bigbuild import views, feeds, sitemaps, get_base_url
+
+
+urlpatterns = [
+    # Index redirect
+    url(r'^$', views.IndexRedirectView.as_view(), name="index-redirect"),
+
+    # Pages
+    url(
+        r'^{0}$'.format(get_base_url()[1:]),
+        views.PageListView.as_view(),
+        name='page-list'
+    ),
+    url(
+        r'^{0}(?P<slug>[-\w]+)/$'.format(get_base_url()[1:]),
+        views.PageDetailView.as_view(),
+        name='page-detail'
+    ),
+
+    # Static assets
+    url(
+        r'^{0}(?P<slug>[-\w]+)/static/(?P<path>.*)$'.format(get_base_url()[1:]),
+        views.page_static_serve,
+        name='page-detail-static'
+    ),
+
+    # Machine-readable feeds
+    url(
+        r'^{0}sitemap.xml$'.format(get_base_url()[1:]),
+        sitemaps.SitemapView.as_view(),
+        name='bigbuild-sitemap'
+    ),
+    url(
+        r'^{0}google-news-sitemap.xml$'.format(get_base_url()[1:]),
+        sitemaps.GoogleNewsSitemapView.as_view(),
+        name='bigbuild-google-news-sitemap'
+    ),
+    url(
+        r'^{0}feeds/latest.xml$'.format(get_base_url()[1:]),
+        feeds.LatestPages(),
+        name="bigbuild-feeds-latest"
+    ),
+]

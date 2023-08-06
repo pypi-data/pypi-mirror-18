4f10a73c482d08ec3480bc0da2e4d2716e247af9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:08:16 +0000

    Add package Koptool/2.5/Koptool-2.5.zip

---
+++ packages/Koptool/Koptool-2.5.zip/Koptool-2.5/setup.py
@@ -0,0 +1,30 @@
+#-*- encoding: UTF-8 -*-
+from setuptools import setup, find_packages
+"""
+打包的用的setup必须引入，
+"""
+ 
+VERSION = '2.5'
+ 
+setup(name='Koptool',
+      version=VERSION,
+      description="a tiny and smart cli player of douyutv,ximalayad,anmu based on Python",
+      long_description='just enjoy',
+      classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      keywords='python ksyun tools terminal',
+      author='cjxcloud',
+      author_email='cjxcloud@163.com',
+      #url='https://github.com/cjx1994/utls/tree/master/ksyun.tools',
+      license='MIT',
+      packages=find_packages(),
+      include_package_data=True,
+      zip_safe=True,
+      install_requires=[
+        'requests',
+      ],
+      entry_points={
+        'console_scripts':[
+            'koptool = koptool.kop:main'
+        ]
+      },
+)

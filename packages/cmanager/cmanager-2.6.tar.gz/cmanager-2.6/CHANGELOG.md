0aa726266e6c531837179b1ca246f1f274e035b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:19:33 +0000

    Add package cmanager/2.6/cmanager-2.6.tar.gz

---
+++ packages/cmanager/cmanager-2.6.tar.gz/cmanager-2.6/CHANGELOG.md
@@ -0,0 +1,20 @@
+##2. - 15/12/2016
+
+* Miss download tarbal version
+
+##2.5 - 15/12/2016
+
+* Fixed configure on Pypi installer.
+
+##1.4, 1.5 and 1.6 - 14/12/2016
+
+* Fixed Pypi installer.
+
+##1.3 - 13/12/2016
+
+* Now compatible with Python 3 and support US Standard PEP8 encodings
+* Supported to Pypi installer.
+
+##1.0 - 04/12/2016
+
+* Initial launch
---
+++ packages/cmanager/cmanager-2.6.tar.gz/cmanager-2.6/cmanager/cmanager.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+# Copyright 2016 Fachrizal Oktavian
+# This file is part of CManager.
+#
+# CManager is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# CManager is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with CManager.  If not, see <http://www.gnu.org/licenses/>.
+
+import os
+import sys
+from core.cm_app import Cm_app
+
+
+def main():
+    if os.getuid() != 0:
+        print ("[-] Please Access as root..!")
+        sys.exit()
+
+    if len(sys.argv) > 1:
+        configure = sys.argv[1]
+        if configure.lower() == 'configure':
+            DIR_BASE = os.path.dirname(os.path.realpath(__file__))
+            os.system(DIR_BASE + '/configure.sh')
+            sys.exit()
+
+    cm_app_handler = Cm_app()
+    cm_app_handler.standby()
+
+
+if __name__ == '__main__':
+    main()

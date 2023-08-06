d77269946748276ae91da003f6858a349967f598
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:12:36 +0000

    Add package hahariri_nester/1.22.0/hahariri_nester-1.22.0.tar.gz

---
+++ packages/hahariri_nester/hahariri_nester-1.22.0.tar.gz/hahariri_nester-1.22.0/setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+#-*-coding:utf-8-*-
+from distutils.core import setup
+setup(
+      name            = 'hahariri_nester',
+      version         = '1.22.0',
+      author          = 'hfpython',
+      author_email    = 'hfpython@headfirstlabs.com',
+      py_modules      = ['hahariri_nester'],
+      url             = 'http://www.headfirstlabs.com',
+      description     = 'A simple printer of nested lists',
+
+      )
+

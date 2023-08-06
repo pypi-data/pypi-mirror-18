aa6435cc3d23f027d9dec34e83522951a002648b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:08:46 +0000

    Add package kweisamx_nester/1.3.0/kweisamx_nester-1.3.0.tar.gz

---
+++ packages/kweisamx_nester/kweisamx_nester-1.3.0.tar.gz/kweisamx_nester-1.3.0/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+
+setup(
+        name        = 'kweisamx_nester',
+        version     = '1.3.0',
+        py_modules  = ['kweisamx_nester'],
+        author      = 'sam',
+        author_email= 'sam@gmail.com',
+        url         = 'http://www.headfirstlabs.com',
+        description = 'A simple printer of nested lists'
+     )

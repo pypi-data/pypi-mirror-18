4ecc65e280f11fe531de6c69bf344e53a2bc2033
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:10:42 +0000

    Add package bookScraper/2.2/bookScraper-2.2.tar.gz

---
+++ packages/bookScraper/bookScraper-2.2.tar.gz/bookScraper-2.2/bookScraper/main.py
@@ -0,0 +1,19 @@
+
+# import 
+from scrapy import cmdline
+
+class RunnerClass:
+    def __init__(self):
+        # os.system('scrapy runspider sastobook_scrape')
+        # os.system('scrapy runspider azbook_scrape')
+
+        cmdline.execute('scrapy crawl sastobook'.split())
+        cmdline.execute('scrapy crawl azbook'.split())
+
+        sasto = open('sasto.txt', 'r')
+        sasto_price = float(sasto.read())
+
+        az = open('az.txt', 'r')
+        az_price = float(az.read())
+
+        self.mini = min(sasto_price, az_price)

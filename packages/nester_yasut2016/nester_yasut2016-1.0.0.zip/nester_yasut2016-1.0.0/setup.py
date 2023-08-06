ce50243366299171da7c1402e700b1332ebd11bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:10:30 +0000

    Add package nester_yasut2016/1.0.0/nester_yasut2016-1.0.0.zip

---
+++ packages/nester_yasut2016/nester_yasut2016-1.0.0.zip/nester_yasut2016-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name = 'nester_yasut2016',
+    version = '1.0.0',
+    py_modules = ['nester'],
+    author = 'yasu',
+    author_email = 'yasutaka404@gmail.com',
+    url = 'http://www.yasutaka404.com',
+    description = 'A simple printer of nested lists',
+    )

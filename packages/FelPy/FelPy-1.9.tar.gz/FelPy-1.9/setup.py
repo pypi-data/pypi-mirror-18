8c93efdee36a07224fd20ec7431772ac978a07cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:09:01 +0000

    Add package FelPy/1.9/FelPy-1.9.tar.gz

---
+++ packages/FelPy/FelPy-1.9.tar.gz/FelPy-1.9/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'FelPy',
+  py_modules = ['listFormat',"confReturn"],
+  version = '1.9',
+  description = 'An assortment of tools from Felix - includes a list formatter, leaderboard file manager, and advanced user input reciever',
+  author = 'Felix Johnson',
+  author_email = 'him@felixhasa.website',
+  url = 'https://github.com/12johnsonf/FelPy', # use the URL to the github repo
+  download_url = 'https://github.com/12johnsonf/FelPy/tarball/1.9', # I'll explain this in a second
+  keywords = ['tools', 'custom', 'first'], # arbitrary keywords
+  classifiers = [],
+)

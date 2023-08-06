9733ccb51e0687471d399f08e5e6ab02238861e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:17:48 +0000

    Add package dee1024/0.0.3/dee1024-0.0.3.tar.gz

---
+++ packages/dee1024/dee1024-0.0.3.tar.gz/dee1024-0.0.3/setup.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(
+    name = 'dee1024',
+    version = '0.0.3',
+    keywords = ('dee', 'egg'),
+    description = 'a simple dee 1024',
+    license = 'MIT License',
+
+    url = 'http://xxxx.org',
+    author = 'dee1024',
+    author_email = 'coolcooldee@gmail.com',
+
+    packages = find_packages(),
+    include_package_data = True,
+    platforms = 'any',
+    install_requires = [],
+)

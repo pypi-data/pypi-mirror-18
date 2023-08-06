083644c0d627bda8278db798bcaad7f4be7f9c32
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:10:19 +0000

    Add package nesteraron/1.0.0/nesteraron-1.0.0.zip

---
+++ packages/nesteraron/nesteraron-1.0.0.zip/nesteraron-1.0.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(
+        name = 'nesteraron',
+        version = '1.0.0',
+        py_modules = ['nesteraaron'],
+        author = 'wjwcan10482',
+        author_email ='wjwcan10482@163.com',
+        url = 'http://www.shareyou.wang/blog',
+        description ='A test',
+      )

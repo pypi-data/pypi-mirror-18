dea7f9dc5ec5a71b3699cb9c1a8002f01c7370b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:30:24 +0000

    Add package lollipop/1.0.1/lollipop-1.0.1-py2.py3-none-any.whl

---
+++ packages/lollipop/lollipop-1.0.1-py2.py3-none-any.whl/lollipop/utils.py
@@ -0,0 +1,52 @@
+import inspect
+import re
+
+
+def identity(value):
+    """Function that returns its argument."""
+    return value
+
+def constant(value):
+    """Returns function that takes any arguments and always returns given value."""
+    def func(*args, **kwargs):
+        return value
+    return func
+
+
+def is_list(value):
+    """Returns True if value supports list interface; False - otherwise"""
+    return isinstance(value, list)
+
+
+def is_dict(value):
+    """Returns True if value supports dict interface; False - otherwise"""
+    return isinstance(value, dict)
+
+
+def call_with_context(func, context, *args):
+    """
+    Check if given function has more arguments than given. Call it with context
+    as last argument or without it.
+    """
+    if inspect.ismethod(func):
+        arg_count = len(inspect.getargspec(func).args) - 1
+    elif inspect.isfunction(func):
+        arg_count = len(inspect.getargspec(func).args)
+    else:
+        arg_count = len(inspect.getargspec(func.__call__).args) - 1
+
+    if len(args) < arg_count:
+        args = list(args)
+        args.append(context)
+
+    return func(*args)
+
+
+def to_snake_case(s):
+    """Converts camel-case identifiers to snake-case."""
+    return re.sub('([^_A-Z])([A-Z])', lambda m: m.group(1) + '_' + m.group(2).lower(), s)
+
+
+def to_camel_case(s):
+    """Converts snake-case identifiers to camel-case."""
+    return re.sub('_([a-z])', lambda m: m.group(1).upper(), s)

a26769497488753573e74217c6c5dfd836cff351
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:14:34 +0000

    Add package toralarm/0.2/toralarm-0.2.tar.gz

---
+++ packages/toralarm/toralarm-0.2.tar.gz/toralarm-0.2/setup.py
@@ -0,0 +1,19 @@
+'''
+Created on 07.12.2016
+
+@author: LukeCodewalker92
+'''
+
+from distutils.core import setup
+setup(
+  name = 'toralarm',
+  packages = ['toralarm'], # this must be the same as the name above
+  version = '0.2',
+  description = 'Toralarm Python API',
+  author = 'LukeCodewalker92',
+  author_email = 'LukeCodewalker92@gmail.com',
+  url = 'https://github.com/lukeskywalker92/toralarm', # use the URL to the github repo
+  download_url = 'https://github.com/lukeskywalker92/toralarm/0.2', # I'll explain this in a second
+  keywords = ['toralarm', 'soccer', 'livescores'], # arbitrary keywords
+  classifiers = [],
+)

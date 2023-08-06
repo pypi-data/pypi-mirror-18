0f06bee5940b4589a91b3943eacd5afa6a1e166b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:30:55 +0000

    Add package py-rtprio/1.0/py-rtprio-1.0.tar.gz

---
+++ packages/py-rtprio/py-rtprio-1.0.tar.gz/py-rtprio-1.0/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup(
+	name='py-rtprio',
+	version='1.0',
+	packages=['rtprio'],
+	author='Kyle Evans',
+	author_email='self@kyle-evans.net',
+	license='BSD',
+	url='https://github.com/kevans91/py-rtprio',
+	classifiers=[
+		'Development Status :: 4 - Beta',
+		'Intended Audience :: Developers',
+		'License :: OSI Approved :: BSD License',
+		'Operating System :: POSIX :: BSD :: FreeBSD',
+		'Programming Language :: Python',
+		'Programming Language :: Python :: 3',
+	]
+)

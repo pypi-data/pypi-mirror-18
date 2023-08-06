9774d7e2f52d0f1165f50d93e9ae3750eefaf64f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:20:50 +0000

    Add package symsynd/1.3.0/symsynd-1.3.0-cp33-none-manylinux1_i686.whl

---
+++ packages/symsynd/symsynd-1.3.0-cp33-none-manylinux1_i686.whl/symsynd/libsymbolizer.py
@@ -0,0 +1,79 @@
+import os
+from threading import Lock
+
+from symsynd.exceptions import SymbolicationError
+from symsynd._symbolizer import ffi
+from symsynd._compat import to_bytes
+
+
+lib = ffi.dlopen(os.path.join(os.path.dirname(__file__), '_libsymbolizer.so'))
+
+
+_lib_lock = Lock()
+_initialized = False
+
+
+def _init_lib():
+    global _initialized
+    if _initialized:
+        return
+    with _lib_lock:
+        if _initialized:
+            return
+        lib.llvm_symbolizer_lib_init()
+        _initialized = True
+
+
+def _symstr(ptr):
+    if ptr == ffi.NULL:
+        return None
+    val = ffi.string(ptr)
+    if val == '<invalid>':
+        return None
+    return val.decode('utf-8', 'replace')
+
+
+class Symbolizer(object):
+
+    def __init__(self):
+        _init_lib()
+        self._ptr = lib.llvm_symbolizer_new()
+
+    def close(self):
+        if self._ptr is not None:
+            lib.llvm_symbolizer_free(self._ptr)
+            self._ptr = None
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_value, tb):
+        self.close()
+
+    def __del__(self):
+        try:
+            self.close()
+        except Exception:
+            pass
+
+    def symbolize(self, module, offset, arch, is_data=False):
+        if self._ptr is None:
+            raise RuntimeError('Symbolizer closed')
+
+        if arch is not None:
+            module += ':' + arch
+
+        rv = lib.llvm_symbolizer_symbolize(
+            self._ptr, to_bytes(module), offset, is_data and 1 or 0)
+        try:
+            if rv.error:
+                raise SymbolicationError(_symstr(rv.error))
+
+            return (
+                _symstr(rv.name),
+                _symstr(rv.filename),
+                rv.lineno,
+                rv.column,
+            )
+        finally:
+            lib.llvm_symbol_free(rv)

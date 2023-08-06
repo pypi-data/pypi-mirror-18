9246bca98b8063055c268e1fce528d4ff54a38f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:32:04 +0000

    Add package printlistbaozi/1.1.0/printlistbaozi-1.1.0.zip

---
+++ packages/printlistbaozi/printlistbaozi-1.1.0.zip/printlistbaozi-1.1.0/printlistbaozi.py
@@ -0,0 +1,10 @@
+def print_lol(the_list,indent=False,level=0):
+    for i in the_list:
+        if isinstance(i,list):
+            print_lol(i,indent,level+1)
+        else:
+            if indent:
+                for tab_stop in range(level):
+                    print("\t",end='')
+            print(i)
+
---
+++ packages/printlistbaozi/printlistbaozi-1.1.0.zip/printlistbaozi-1.1.0/setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+
+setup(name='printlistbaozi',
+      version='1.1.0',
+      py_modules=['printlistbaozi'],
+      author='baozi',
+      author_email='476041626@qq.com',
+      description='A simple printer of nested lists')

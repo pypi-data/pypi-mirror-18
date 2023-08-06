f4c16ffc9e137244273ad394960dccb25485d6f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:45:08 +0000

    Add package trustar/0.0.1/trustar-0.0.1.tar.gz

---
+++ packages/trustar/trustar-0.0.1.tar.gz/trustar-0.0.1/setup.py
@@ -0,0 +1,23 @@
+# python setup.py register -r pypitest
+# python setup.py sdist upload -r pypitest
+from setuptools import setup, find_packages
+
+setup(name='trustar',
+      version='0.0.1',
+      author='TruSTAR Technology Inc.',
+      url='https://github.com/trustar/trustar-python',
+      description='Python SDK for the TruSTAR REST API',
+      author_email='support@trustar.co',
+      license='MIT',
+      install_requires=['future',
+                        'pandas',
+                        'python-dateutil',
+                        'pytz',
+                        'requests',
+                        'configparser'],
+      # package source directory
+      # package_dir={'': 'trustar'},
+      # packages=find_packages('trustar'),
+      packages=find_packages(),
+      use_2to3=True
+      )

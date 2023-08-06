df7f3633eb8d993bb289acacdbbe35b9fc78fefe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:19:57 +0000

    Add package napa/0.88/napa-0.88.tar.gz

---
+++ packages/napa/napa-0.88.tar.gz/napa-0.88/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup;
+from setuptools import find_packages;
+
+setup(name='napa',
+      version='0.88',
+      description='Network Analysis of Protein Adaptation (intra-protein residue coevolution network construction and analysis)',
+      url='http://karchinlab.org/napa',
+      author='Violeta Beleva-Guthrie',
+      author_email='vbeleva@gmail.com',
+      license='',
+      packages=find_packages(exclude=['tests']),
+      install_requires=[
+          'dill',
+          'joblib',
+          'numpy',
+          'scipy',
+          'networkx',
+          'ete2'
+      ],
+      classifiers=[
+      'Development Status :: 4 - Beta',
+      'Intended Audience :: Developers',
+      'Topic :: Scientific/Engineering :: Bio-Informatics'
+      ],
+      zip_safe=True)

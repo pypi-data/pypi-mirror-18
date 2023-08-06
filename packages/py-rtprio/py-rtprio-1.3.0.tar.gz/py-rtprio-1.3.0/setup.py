c2d2037ace990f728261609208b4f6974a05d208
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:33:11 +0000

    Add package py-rtprio/1.3.0/py-rtprio-1.3.0.tar.gz

---
+++ packages/py-rtprio/py-rtprio-1.3.0.tar.gz/py-rtprio-1.3.0/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup
+
+setup(
+	name='py-rtprio',
+	version='1.3.0',
+	packages=['rtprio'],
+	description='rtprio(2) interface for altering process priority',
+	author='Kyle Evans',
+	author_email='self@kyle-evans.net',
+	license='BSD',
+	url='https://github.com/kevans91/py-rtprio',
+	classifiers=[
+		'Development Status :: 4 - Beta',
+		'Intended Audience :: Developers',
+		'License :: OSI Approved',
+		'License :: OSI Approved :: BSD License',
+		'Operating System :: POSIX',
+		'Operating System :: POSIX :: BSD',
+		'Operating System :: POSIX :: BSD :: FreeBSD',
+		'Programming Language :: Python',
+		'Programming Language :: Python :: 2',
+		'Programming Language :: Python :: 2.7',
+		'Programming Language :: Python :: 3',
+		'Programming Language :: Python :: 3.4',
+		'Programming Language :: Python :: 3.5',
+		'Programming Language :: Python :: 3.6',
+	]
+)

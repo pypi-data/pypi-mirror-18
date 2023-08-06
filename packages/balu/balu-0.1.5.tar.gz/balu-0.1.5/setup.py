7acab575a508d1331e194b0606104a04b352105d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:18:25 +0000

    Add package balu/0.1.5/balu-0.1.5.tar.gz

---
+++ packages/balu/balu-0.1.5.tar.gz/balu-0.1.5/setup.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+setup(
+  name = 'balu',
+  packages=find_packages(),
+  version = '0.1.5',
+  description = 'Python implementation of Balu Toolbox by Domingo Mery (http://dmery.ing.puc.cl/index.php/balu/)',
+  author = 'Diego Patiño',
+  author_email = 'dapatinoco@unal.edu.com',
+  url = 'https://github.com/dipaco/balu-python',
+  download_url = 'https://github.com/dipaco/balu-python/releases/tag/v0.1.0',
+  keywords = ['Computer Vision', 'Machine Learning', 'Image Processing'],
+  classifiers = [],
+  install_requires = [
+      'numpy',
+  ],
+)

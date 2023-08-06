c0159dbc6ea11743cfc42efab69aaae74d7c7bbd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:15:35 +0000

    Add package deel/0.0.4.4/deel-0.0.4.4.tar.gz

---
+++ packages/deel/deel-0.0.4.4.tar.gz/deel-0.0.4.4/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+install_requires = [
+    'chainer>=1.16.0',
+    'filelock',
+    'nose',
+    'pillow',
+    'h5py',
+    'numpy>=1.9.0',
+    'protobuf',
+    'six>=1.9.0',
+]
+setup(
+    name = "deel",
+    version = "0.0.4.4",
+    author = "UEI corporation",
+    author_email="info@uei.co.jp",
+    description = ("Deel; A High level deep learning description language"),
+    license = "MIT",
+    keywords = "chainer",
+    url = "https://github.com/uei/deel",
+    install_requires=install_requires,
+    packages=[  'deel',
+                'deel.network',
+                'deel.model',
+                ],
+)

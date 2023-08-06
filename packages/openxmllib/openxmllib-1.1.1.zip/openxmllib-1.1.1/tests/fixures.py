d90bdf5ca81d8094643b30d6c7763b1cdaa3234a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:19:21 +0000

    Add package openxmllib/1.1.1/openxmllib-1.1.1.zip

---
+++ packages/openxmllib/openxmllib-1.1.1.zip/openxmllib-1.1.1/tests/fixures.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+"""
+Environment fixures (stubs, data, ...) for unit testing
+"""
+# $Id: fixures.py 6796 2007-12-04 10:52:51Z glenfant $
+
+import os
+import sys
+
+# Fixing the sys.path such "openxmllib" is on top
+this_dir = os.path.dirname(os.path.abspath(__file__))
+parent_dir = os.path.dirname(this_dir)
+if parent_dir not in sys.path:
+    sys.path.insert(0, parent_dir)
+
+# Unit tests notification
+if not os.getenv('PYTHON_UNIT_TEST', False):
+    os.putenv('PYTHON_UNIT_TEST', 'OK')
+
+# Directories with test files
+TEST_FILES_IN = os.path.join(this_dir, 'in')
+ALL_IN_FILES = ('wordprocessing1.docx', 'spreadsheet1.xlsx', 'presentation1.pptx')
+ALL_IN_COVER_FILES = ('wordprocessing1_cover.docx', 'spreadsheet1_cover.xlsx', 'presentation1_cover.pptx')
+ALL_IN_TEMPLATE_FILES = ('wordprocessing1.dotx', 'spreadsheet1.xltx', 'presentation1.potx')
+HOST_NAME = '127.0.0.1'
+PORT = 8088
+DOCX_URL = 'http://%s:%s/word.docx' % (HOST_NAME, PORT)
+
+__all__ = (
+    'TEST_FILES_IN',
+    'ALL_IN_FILES',
+    'ALL_IN_COVER_FILES',
+    'ALL_IN_TEMPLATE_FILES',
+    'DOCX_URL',
+    'HOST_NAME',
+    'PORT'
+)
---
+++ packages/openxmllib/openxmllib-1.1.1.zip/openxmllib-1.1.1/tests/test_presentation.py
@@ -0,0 +1,65 @@
+# -*- coding: utf-8 -*-
+"""
+Testing PresentationDocument
+"""
+# $Id$
+
+import unittest
+import os
+from fixures import *
+
+import openxmllib
+
+class PresentationTest(unittest.TestCase):
+    """Testing querying properties from a document"""
+
+    def setUp(self):
+        test_file_path = os.path.join(TEST_FILES_IN, ALL_IN_FILES[2])
+        self.doc = openxmllib.openXmlDocument(path=test_file_path)
+        test_template_file_path = os.path.join(TEST_FILES_IN, ALL_IN_TEMPLATE_FILES[2])
+        self.template = openxmllib.openXmlDocument(path=test_template_file_path)
+        return
+
+
+    def test_indexableText(self):
+        """Indexable text with properties"""
+
+        itext = self.doc.indexableText().split()
+        some_words = (u'Chapter', u'presentation', u'proud', u'three', u'two',
+             u'four', u'item', u'one', u'My')
+        some_words += (u'false',)
+        for word in some_words:
+            self.failUnless(word in itext, "%s was expected in %s" % (word, itext))
+        return
+
+
+    def test_indexableTextNoprop(self):
+        """Indexable text without properties"""
+
+        itext = self.doc.indexableText(include_properties=False).split()
+        some_words = (u'Chapter', u'presentation', u'proud', u'three', u'two',
+             u'four', u'item', u'one', u'My')
+        for word in some_words:
+            self.failUnless(word in itext, "%s was expected in %s" % (word, itext))
+        return
+
+
+    def test_templateFile(self):
+        """Template file (potx)"""
+        itext = self.template.indexableText().split()
+        some_words = (u'Chapter', u'presentation', u'proud', u'three', u'two',
+             u'four', u'item', u'one', u'My')
+        some_words += (u'false',)
+        for word in some_words:
+            self.failUnless(word in itext, "%s was expected in %s" % (word, itext))
+        return
+# /class WordProcessingTest
+
+
+def test_suite():
+    suite = unittest.TestSuite()
+    suite.addTest(unittest.makeSuite(PresentationTest))
+    return suite
+
+if __name__ == '__main__':
+    unittest.TextTestRunner().run(test_suite())

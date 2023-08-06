881eb7cf20a4aef168908f5832971e5c8ce22d54
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:22:52 +0000

    Add package xjson/0.1.4/xjson-0.1.4.zip

---
+++ packages/xjson/xjson-0.1.4.zip/xjson-0.1.4/setup.py
@@ -0,0 +1,20 @@
+﻿#coding=utf-8
+
+from setuptools import setup,find_packages
+
+setup(
+	name = "xjson",
+	version = "0.1.4",
+	keywords = ("json","parse","xpath","syntax"),
+	description = "allow you parse a json file with xpath syntax",
+	license = "MIT License",
+	install_requires = [],
+	author = "zhangTian",
+	author_email = "hhczy1003@gmail.com",
+	packages = find_packages(),
+	#packages = ["xjson","ReadMe\\.rst"],
+	platforms = "any",
+	#github 或read_the_docs上的链接
+	url = "https://github.com/hhczy/xjson",
+	long_description = open("./xjson/ReadMe.rst").read()
+	)

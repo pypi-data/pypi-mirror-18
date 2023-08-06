5158f4046ce7c1fe36759757a6bd033cd0a0f6ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:21:18 +0000

    Add package majorityvoting/0.1.3/majorityvoting-0.1.3.tar.gz

---
+++ packages/majorityvoting/majorityvoting-0.1.3.tar.gz/majorityvoting-0.1.3/setup.py
@@ -0,0 +1,16 @@
+from distutils.core import setup
+setup(
+    name = 'majorityvoting',
+    packages = ['majorityvoting'],
+    version = '0.1.3',
+    description = 'Majority Judgement Voting tool (fit for sklearn, etc.)',
+    author = 'Tao Peter Wang',
+    author_email = 'peterwangtao0@hotmail.com',
+    url = 'https://github.com/TPeterW/Probabilistic-Majority-Voting',
+    download_url = 'https://github.com/TPeterW/Probabilistic-Majority-Voting/tarball/0.1.3',
+    keywords = ['majority voting', 'sklearn', 'machine learning', 'probability', 'majority judgement'],
+    install_requires=[
+          'numpy',
+    ],
+    classifiers = [],
+)

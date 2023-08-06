ee1684119ca690976f6f5a2778a19a73cb51d46c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:45:02 +0000

    Add package tfwrapper/0.0.1a15/tfwrapper-0.0.1a15-py2-none-any.whl

---
+++ packages/tfwrapper/tfwrapper-0.0.1a15-py2-none-any.whl/tfwrapper/__init__.py
@@ -0,0 +1,16 @@
+from .lock import TerraformS3Lock
+from .run import TerraformRun
+from .exceptions import (
+    TerraformLockedException, TerraformUnlockException, TerraformRunException)
+
+
+# Set default logging handler to avoid "No handler found" warnings.
+import logging
+try:  # Python 2.7+
+    from logging import NullHandler
+except ImportError:
+    class NullHandler(logging.Handler):
+        def emit(self, record):
+            pass
+
+logging.getLogger(__name__).addHandler(NullHandler())

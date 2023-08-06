4d668b35ba4c811e24e5807d68cbefba17656bea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:12:16 +0000

    Add package dredh.nester/1.0.0/dredh.nester-1.0.0.tar.gz

---
+++ packages/dredh.nester/dredh.nester-1.0.0.tar.gz/dredh.nester-1.0.0/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+		name = 'dredh.nester',
+		version = '1.0.0',
+		py_modules = ['nester'],
+		author = 'dredh',
+		author_email = '527219336@qq.com',
+		url = 'http://www.headfirstlabs.com',
+		description = 'A simple printer of nester lists',
+
+	)

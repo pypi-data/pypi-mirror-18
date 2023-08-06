f0c9519cb64e5e50b31dfdb5d4c32686d06636c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:45:55 +0000

    Add package attrs/16.3.0/attrs-16.3.0.tar.gz

---
+++ packages/attrs/attrs-16.3.0.tar.gz/attrs-16.3.0/conftest.py
@@ -0,0 +1,21 @@
+from __future__ import absolute_import, division, print_function
+
+import os
+
+import pytest
+from hypothesis import settings
+
+
+@pytest.fixture(scope="session")
+def C():
+    """
+    Return a simple but fully features attrs class with an x and a y attribute.
+    """
+    from attr import attributes, attr
+
+    @attributes
+    class C(object):
+        x = attr()
+        y = attr()
+
+    return C

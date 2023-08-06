2f407e25dd7fd32b30b1d90b944eabe9d6fbb454
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:51:22 +0000

    Add package mymath-ghajba/0.0.1/mymath_ghajba-0.0.1.tar.gz

---
+++ packages/mymath-ghajba/mymath_ghajba-0.0.1.tar.gz/mymath_ghajba-0.0.1/setup.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+__author__ = 'GHajba'
+
+setup(
+    name="mymath_ghajba",
+    version='0.0.1',
+    url='http://www.discoversdk.com/',
+    author='GHajba',
+    author_email='ghajba@gmail.com',
+)

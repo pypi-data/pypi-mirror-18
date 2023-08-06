5e9885bb25029d50cf60c960250b0be3fbc0a534
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:44:28 +0000

    Add package ovp-core/1.0.10/ovp-core-1.0.10.tar.gz

---
+++ packages/ovp-core/ovp-core-1.0.10.tar.gz/ovp-core-1.0.10/ovp_core/serializers.py
@@ -0,0 +1,25 @@
+from ovp_core import models
+
+from rest_framework import serializers
+
+
+class GoogleAddressSerializer(serializers.ModelSerializer):
+  class Meta:
+    model = models.GoogleAddress
+    fields = ['typed_address', 'typed_address2', 'address_line', 'city_state']
+    read_only_fields = ['address_line', 'city_state']
+
+class GoogleAddressCityStateSerializer(serializers.ModelSerializer):
+  class Meta:
+    model = models.GoogleAddress
+    fields = ['city_state']
+
+class SkillSerializer(serializers.ModelSerializer):
+  class Meta:
+    fields = ['name']
+    model = models.Skill
+
+class CauseSerializer(serializers.ModelSerializer):
+  class Meta:
+    fields = ['name']
+    model = models.Cause

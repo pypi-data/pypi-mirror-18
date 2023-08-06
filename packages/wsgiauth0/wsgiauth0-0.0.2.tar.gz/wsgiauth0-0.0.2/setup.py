af8e0df5aa3849d911511495b16a84ccf862f6b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:09:21 +0000

    Add package wsgiauth0/0.0.2/wsgiauth0-0.0.2.tar.gz

---
+++ packages/wsgiauth0/wsgiauth0-0.0.2.tar.gz/wsgiauth0-0.0.2/setup.py
@@ -0,0 +1,28 @@
+from os import path
+from setuptools import setup
+
+here = path.abspath(path.dirname(__file__))
+
+with open(path.join(here, 'README.rst')) as f:
+    long_description = f.read()
+
+with open(path.join(here, 'VERSION')) as f:
+    version = f.read().strip()
+
+with open('requirements.txt') as f:
+    requirements = f.read().split()
+
+with open(path.join(here, 'requirements-tests.txt')) as f:
+    test_requirements = f.read()
+
+setup(
+    version=version,
+    name='wsgiauth0',
+    description='Auth0 middleware for multiple client configurations',
+    long_description=long_description,
+    py_modules=['wsgiauth0'],
+    install_requires=requirements,
+    tests_require=test_requirements,
+    url='https://gitlab.com/hadrien/',
+    licence='MIT',
+)

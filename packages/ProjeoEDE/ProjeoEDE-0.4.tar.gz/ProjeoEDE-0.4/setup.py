1980b4e72948b11eb932967956c7604de745b32b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:17:35 +0000

    Add package ProjeoEDE/0.4/ProjeoEDE-0.4.tar.gz

---
+++ packages/ProjeoEDE/ProjeoEDE-0.4.tar.gz/ProjeoEDE-0.4/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+with open('README.rst') as f:
+    readme = f.read()
+setup(name='ProjeoEDE',
+      version='0.4',
+      author='Jhonnatha de Andrade Monteiro',
+      author_email='jhonnatha.am@gmail.com',
+      license='MIT',
+      description='Example package that calculate thermodynamic data',
+      long_description=readme,
+      packages=find_packages())

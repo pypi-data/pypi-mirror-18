b6f19481950e2dda1ac999828dc29bacc87d4dc0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:18:55 +0000

    Add package time2relax/0.1.0/time2relax-0.1.0-py2.py3-none-any.whl

---
+++ packages/time2relax/time2relax-0.1.0-py2.py3-none-any.whl/time2relax/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+#             __
+# _|_ o __  _  _) __ _  |  _
+#  |_ | |||(/_/__ | (/_ | (_|><
+#
+
+__version__ = '0.1.0'
+
+from .time2relax import HTTPClient, Server, Database  # noqa

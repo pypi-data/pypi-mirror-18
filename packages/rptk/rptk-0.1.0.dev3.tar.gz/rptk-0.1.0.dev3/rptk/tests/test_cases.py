381834c171a82460fc503dd1374dc91cf98c834e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:10:38 +0000

    Add package rptk/0.1.0.dev3/rptk-0.1.0.dev3.tar.gz

---
+++ packages/rptk/rptk-0.1.0.dev3.tar.gz/rptk-0.1.0.dev3/rptk/tests/test_cases.py
@@ -0,0 +1,26 @@
+import os
+from unittest import TestCase
+from whichcraft import which
+from rptk import configuration, dispatch
+
+
+class TestRptk(TestCase):
+    config_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'tests.conf')
+    argv = ['--config_path', config_path]
+
+    def test_01_dependency_bgpq3(self):
+        path = which('bgpq3')
+        self.assertIsInstance(path, str, msg="bgpq3 executable not found in PATH")
+
+    def test_02_configure(self):
+        config = configuration.Config(argv=self.argv)
+        self.assertIsInstance(config, configuration.Config, msg="config object invalid")
+        self.config = config
+
+    def test_03_dummy_query(self):
+        config = configuration.Config(argv=self.argv)
+        dispatcher = dispatch.Dispatcher(config=config)
+        result = dispatcher.dispatch()
+        self.assertIsInstance(result, dict)
+        self.assertIsInstance(result["test"], dict)
+        self.assertEqual(result["test"]["object"], config.args.object)

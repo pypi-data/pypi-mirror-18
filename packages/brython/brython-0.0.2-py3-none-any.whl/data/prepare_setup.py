48a95fb1d8b60434668ce3da2adc0184377880ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:20:05 +0000

    Add package brython/0.0.2/brython-0.0.2-py3-none-any.whl

---
+++ packages/brython/brython-0.0.2-py3-none-any.whl/data/prepare_setup.py
@@ -0,0 +1,33 @@
+import os
+import shutil
+import json
+
+from tools import make_bundle
+
+# prepare script with standard distribution
+www = os.path.join(os.path.dirname(os.path.dirname(os.getcwd())), 'www')
+folders = [
+    os.path.join(www, 'src', 'Lib'),
+    os.path.join(www, 'src', 'libs')
+]
+res = {}
+for folder in folders:
+    res.update(make_bundle.bundle(folder))
+
+# copy stdlib in dist/brython_modules.js
+# and initialise brython_modules.js with the same content
+lib_dir = os.path.join(os.getcwd(), 'dist')
+if not os.path.exists(lib_dir):
+    os.mkdir(lib_dir)
+
+bundle_names = ['brython_stdlib.js', 'brython_modules.js']
+for name in bundle_names:
+    bundle_path = os.path.join(lib_dir, name)
+    with open(bundle_path, 'w', encoding='utf-8') as out:
+        out.write('__BRYTHON__.use_VFS = true;\n')
+        out.write('__BRYTHON__.VFS = {}\n'.format(json.dumps(res)))
+
+# copy brython.js
+shutil.copyfile(os.path.join(www, 'src', 'brython.js'),
+    os.path.join(lib_dir, 'brython.js'))
+    

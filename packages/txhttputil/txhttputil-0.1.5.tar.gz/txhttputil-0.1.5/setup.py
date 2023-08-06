f7d24b13d7d95a49a7bb4505b3d6ce5643df504c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:16:41 +0000

    Add package txhttputil/0.1.5/txhttputil-0.1.5.tar.gz

---
+++ packages/txhttputil/txhttputil-0.1.5.tar.gz/txhttputil-0.1.5/setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+
+setup(
+    name='txhttputil',
+    packages=['txhttputil', 'txhttputil.downloader', 'txhttputil.login_page', 'txhttputil.site', 'txhttputil.util'],
+    package_data={'txhttputil.login_page': ['*.xml']},
+    version='0.1.5',
+    description='Synerty utility classes for serving a static site with twisted.web with user permissions.',
+    author='Synerty',
+    author_email='contact@synerty.com',
+    url='https://github.com/Synerty/txhttputil',
+    download_url='https://github.com/Synerty/txhttputil/tarball/0.1.5',
+    keywords=['twisted', 'resource', 'file', 'download', 'synerty'],
+    classifiers=[],
+)

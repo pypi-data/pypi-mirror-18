ae2432d2c88c4ff70a3d2b3421e52d38063be2aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:10:44 +0000

    Add package cfio/0.1.2/cfio-0.1.2.zip

---
+++ packages/cfio/cfio-0.1.2.zip/cfio-0.1.2/setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+from distutils.core import setup
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
+setup(
+    name='cfio',
+    version='0.1.2',
+    packages=['cfio'],
+    url='',
+    license='GPL',
+    author='Marco Bartel',
+    author_email='bsimpson888@gmail.com',
+    description='compatible file io',
+    install_requires=requirements,
+    data_files=[('', ['requirements.txt'])]
+)

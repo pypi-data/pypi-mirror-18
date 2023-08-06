897db17e863336633eecedb20de7b35e283cb1f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:45:37 +0000

    Add package ORG.asm/1.0.00-alpha2/ORG.asm-1.0.00-alpha2.tar.gz

---
+++ packages/ORG.asm/ORG.asm-1.0.00-alpha2.tar.gz/ORG.asm-1.0.00-alpha2/python/orgasm/assembler/__init__.py
@@ -0,0 +1,18 @@
+"""
+.. module:: orgasm.assembler
+   :platform: Unix
+   :synopsis: The :py:mod:`orgasm.assembler` python package provide the :class:`Assembler` class .
+
+The :py:mod:`orgasm.assembler` package
+======================================
+
+:author:  Eric Coissac
+:contact: eric.coissac@inria.fr
+
+The :py:mod:`orgasm.assembler` python package provide the :class:`Assembler` class 
+which manage the assembling process.
+"""
+
+from ._assembler import Assembler
+from ._assembler import buildstem
+from ._tango import tango

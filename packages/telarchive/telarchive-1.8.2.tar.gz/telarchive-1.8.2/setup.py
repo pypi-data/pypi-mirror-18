fc7292f95cf63a19986a6824dd0c4665b7ca6874
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:09:58 +0000

    Add package telarchive/1.8.2/telarchive-1.8.2.tar.gz

---
+++ packages/telarchive/telarchive-1.8.2.tar.gz/telarchive-1.8.2/setup.py
@@ -0,0 +1,31 @@
+# Always prefer setuptools over distutils
+from setuptools import setup, find_packages
+
+setup(name="telarchive",
+	version="1.8.2",
+	description="Script for automated searches of several major astronomical telescope archives",
+	# [ ] long_description
+	url="http://www.mpe.mpg.de/~erwin/code/",
+	author="Peter Erwin",
+	author_email="erwin@mpe.mpg.de",
+	license="GPL",
+
+	classifiers=[
+		# How mature is this project?
+		"Development Status :: 5 - Production/Stable",
+		
+		# [ ] Intended audience
+		
+		# [ ] License
+		
+		# Specify supported Python versions
+		"Programming Language :: Python :: 2",
+		"Programming Language :: Python :: 2.6",
+		"Programming Language :: Python :: 2.7",
+	],
+	
+	keywords=['astronomy', 'science'],
+	packages=['telarchive'],
+	scripts=['dosearch.py', 'do_fetchsdss.py', 'do_fetchsdss_spectra.py']
+	)
+	

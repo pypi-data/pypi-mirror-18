eb0658860958f5755a41d17475b2c745446d48b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:07:50 +0000

    Add package micropython-cloudmanager/0.0.102/micropython-cloudmanager-0.0.102.tar.gz

---
+++ packages/micropython-cloudmanager/micropython-cloudmanager-0.0.102.tar.gz/micropython-cloudmanager-0.0.102/setup.py
@@ -0,0 +1,24 @@
+import sys
+# Remove current dir from sys.path, otherwise setuptools will peek up our
+# module instead of system.
+sys.path.pop(0)
+from setuptools import setup
+
+
+setup(
+    name='micropython-cloudmanager',
+    description='Micropython client that allows network attached boards to be controlled from a central redis server',
+    long_description="""Provides a client that allows one a network capable micropython board to
+interface with a central redis server.""",
+    url='https://github.com/dhubbard/micropython-cloudmanager',
+    author='Dwight Hubbard',
+    author_email="dwight@dwighthubbard.com",
+    install_requires=['hostlists', 'redislite', 'python-daemon', 'pip>=8.1.2', 'netifaces', 'watchdog'],
+    license='MIT',
+    maintainer='Dwight Hubbard',
+    maintainer_email='dwight@dwighthubbard.com',
+    packages=['cloudmanager'],
+    scripts=['scripts/micropython_board_manager', 'scripts/mbm'],
+    version='0.0.102',
+    zip_safe=True,
+)

cc1d4a51a5ad7b3d3d76c44f84d240a23c8962ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:21:56 +0000

    Add package askfm.py/0.6.5/askfm.py-0.6.5.tar.gz

---
+++ packages/askfm.py/askfm.py-0.6.5.tar.gz/askfm.py-0.6.5/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(name='askfm.py',
+      version='0.6.5',
+      description='Ask.fm crawler',
+      author='utgwkk',
+      author_email='utagawa.kiki@gmail.com',
+      url='https://github.com/utgwkk/askfm.py',
+      packages=find_packages(exclude=['tests']),
+      install_requires=['requests', 'beautifulsoup4'],
+      license='MIT',
+      classifiers=[
+          'Development Status :: 4 - Beta',
+          'License :: OSI Approved :: MIT License',
+          'Operating System :: OS Independent',
+          'Programming Language :: Python',
+          'Programming Language :: Python :: 2.7',
+          'Programming Language :: Python :: 3',
+          'Programming Language :: Python :: 3.3',
+          'Programming Language :: Python :: 3.4',
+          'Programming Language :: Python :: 3.5',
+          'Topic :: Software Development :: Libraries :: Python Modules'
+      ]
+      )

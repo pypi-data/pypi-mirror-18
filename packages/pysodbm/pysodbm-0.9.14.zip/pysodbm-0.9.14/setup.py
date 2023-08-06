b07f615e9d1823403dbab76d3f1f2076fb983a80
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:46:25 +0000

    Add package pysodbm/0.9.14/pysodbm-0.9.14.zip

---
+++ packages/pysodbm/pysodbm-0.9.14.zip/pysodbm-0.9.14/setup.py
@@ -0,0 +1,16 @@
+from distutils.core import setup
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
+setup(
+    name='pysodbm',
+    version='0.9.14',
+    packages=['pysodbm'],
+    url='https://github.com/bsimpson888/pysodbm',
+    license='GPL',
+    author='Marco Bartel',
+    author_email='bsimpson888@gmail.com',
+    description='A ORM Database layer',
+    install_requires=requirements,
+    data_files=[('', ['requirements.txt'])]
+)

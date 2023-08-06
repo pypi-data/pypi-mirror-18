8cf5b59378d484566f73e88ae0be77d8dfe20a88
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:45:08 +0000

    Add package zhue/0.5.0/zhue-0.5.0.tar.gz

---
+++ packages/zhue/zhue-0.5.0.tar.gz/zhue-0.5.0/zhue/model/light.py
@@ -0,0 +1,60 @@
+from __future__ import unicode_literals
+import hueobject
+
+
+class Light(hueobject.LightDevice):
+    def __init__(self, *args, **kwargs):
+        super(Light, self).__init__('lights', *args, **kwargs)
+
+    @property
+    def is_on(self):
+        return self.state.on
+
+    @property
+    def state(self):
+        return LightState(self._json['state'])
+
+    def __str__(self):
+        return super(Light, self).__str__ + ' (*)' if self.is_on else ''
+
+
+class LightState(hueobject.HueObject):
+    @property
+    def alert(self):
+        return self._json['alert']
+
+    @property
+    def on(self):
+        return self._json['on']
+
+    @property
+    def brightness(self):
+        return self._json['bri']
+
+    @property
+    def colormode(self):
+        return self._json['colormode']
+
+    @property
+    def color_temperature(self):
+        return self._json['ct']
+
+    @property
+    def effect(self):
+        return self._json['effect']
+
+    @property
+    def hue(self):
+        return self._json['hue']
+
+    @property
+    def reachable(self):
+        return self._json['reachable']
+
+    @property
+    def saturation(self):
+        return self._json['sat']
+
+    @property
+    def xy(self):
+        return self._json['xy']

551002ad235ab1d540f272185307aef0da118b46
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:09:01 +0000

    Add package HT_nester/1.0.0/HT_nester-1.0.0.zip

---
+++ packages/HT_nester/HT_nester-1.0.0.zip/HT_nester-1.0.0/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+		name = 'HT_nester',
+		version = '1.0.0',
+		py_modules = ['HT_nester'],
+		author = 'hfpython',
+		author_email = 'hfpython@headfirstlabs.com',
+		url = 'http://www.headfirstlabs.com'
+		
+
+   	 )

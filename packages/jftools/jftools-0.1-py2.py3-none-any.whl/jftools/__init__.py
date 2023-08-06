3c74521ceaf05a3945f16e0f6bbc772f87a89601
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:31:41 +0000

    Add package jftools/0.1/jftools-0.1-py2.py3-none-any.whl

---
+++ packages/jftools/jftools-0.1-py2.py3-none-any.whl/jftools/__init__.py
@@ -0,0 +1,9 @@
+"""Collection of small useful helper tools for Python by Johannes Feist."""
+
+__version__ = '0.1'
+
+__all__ = ['shade_color','tic','toc','ipynbimport_install']
+
+from .shade_color import shade_color
+from .tictoc import tic, toc
+from .ipynbimport import install as ipynbimport_install

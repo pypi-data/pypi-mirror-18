23ec6ffc821d69a0d3086555d5f38147f443ca5a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:07:01 +0000

    Add package plot/0.1.1/plot-0.1.1-py3-none-any.whl

---
+++ packages/plot/plot-0.1.1-py3-none-any.whl/plot/io/input/parse.py
@@ -0,0 +1,45 @@
+"""
+Parse the user input configuration file,
+update the default configuration dictionary
+with the user-defined values and
+return the updated dictionary.
+"""
+from typing import Dict, AnyStr
+from .parser import parser
+from .readAll import readAll
+from .create_parameters import create_parameters
+from ... import tk
+
+
+def parse(user_config_file, default_config_file):
+    # type: (AnyStr, AnyStr) -> (Dict)
+    """Return an updated configuration file
+
+    Read a user configuration file and
+    a default configuration file.
+    Use the values in the user configuration
+    file to update the default configuration file.
+    Finally return an updated configuration dictionary.
+
+    Args:
+        user_config_file (str): user configuration file
+
+    Returns:
+        an updated configuration dictionary
+    """
+    fn_parser1 = parser(user_config_file)
+    fn_parser2 = parser(default_config_file)
+    user_dict = fn_parser1(readAll(user_config_file))
+    default_dict = tk.dictTK.default(fn_parser2(readAll(default_config_file)))
+    ooo = {}
+
+    for k in ['global', 'local', 'data']:
+        if k in user_dict:
+            if isinstance(user_dict[k], list):
+                ooo[k] = [create_parameters(user_dict[k][i], default_dict[k])
+                          for i in range(len(user_dict[k]))]
+            else:
+                ooo[k] = create_parameters(user_dict[k], default_dict[k])
+        else:
+            ooo[k] = create_parameters(dict(), default_dict[k])
+    return ooo

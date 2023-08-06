fc9c092c956dc7681ea8d9de8220102d2fb33c31
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:09:46 +0000

    Add package django-gipsy/2.1/django-gipsy-2.1.tar.gz

---
+++ packages/django-gipsy/django-gipsy-2.1.tar.gz/django-gipsy-2.1/setup.py
@@ -0,0 +1,44 @@
+#! /usr/bin/env python
+from distutils.core import setup
+from setuptools import find_packages
+import sys
+
+# python2 and python3 support
+try:
+    reload
+except NameError:
+    # py3k has unicode by default
+    pass
+else:
+    reload(sys).setdefaultencoding('utf-8')
+
+
+setup(
+    name='django-gipsy',
+    version='2.1',
+    author='Guillaume Pousseo',
+    author_email='guillaumepousseo@revsquare.com',
+    description='A set of fancy tools for django.',
+    long_description=open('README.rst').read(),
+    url='http://www.revsquare.com',
+    license='BSD License',
+    platforms=['OS Independent'],
+    packages=find_packages(),
+    include_package_data=True,
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+    ],
+    install_requires=[
+        'google-api-python-client==1.3.1',
+        'django-grappelli>=2.7'
+    ],
+)

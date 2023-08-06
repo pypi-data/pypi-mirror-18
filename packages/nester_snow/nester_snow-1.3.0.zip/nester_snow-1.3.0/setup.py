4e5a30273c027032bde0f9e354585ba0db3c9a93
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:08:24 +0000

    Add package nester_snow/1.3.0/nester_snow-1.3.0.zip

---
+++ packages/nester_snow/nester_snow-1.3.0.zip/nester_snow-1.3.0/setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='nester_snow',
+    version='1.3.0',
+    py_modeles=['nester_snow'],
+    author='hfpython',
+    description='A simple printer of nested lists',
+      )

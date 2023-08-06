d21ce57d21e1098a5b619ec73f464b07dcc9bf6c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:12:23 +0000

    Add package hpcc-i-spark/0.1.1/hpcc_i_spark-0.1.1-py2.py3-none-any.whl

---
+++ packages/hpcc-i-spark/hpcc_i_spark-0.1.1-py2.py3-none-any.whl/hpcc_i_spark/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+
+__author__ = 'Vivek Nair'
+__email__ = 'vivekaxl@gmail.com'
+__version__ = '0.1.1'
+
+from .cli import get_content, find_record_structure,hello, say_hello

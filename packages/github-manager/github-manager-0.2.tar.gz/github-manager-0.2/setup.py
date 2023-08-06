a8900e48bf95a5e360286f85014588a514f9a0f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:59 +0000

    Add package github-manager/0.2/github-manager-0.2.tar.gz

---
+++ packages/github-manager/github-manager-0.2.tar.gz/github-manager-0.2/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup
+
+setup(
+    name='github-manager',
+    version='0.2',
+    description='Gitman - Simple CLI Github Repository Manager',
+    url='https://github.com/Nelthorim/gitman',
+    author='Eduardo Coello Rodriguez',
+    author_email='ecoerod@gmail.com',
+    license="MIT",
+    keywords='git github repository manager',
+
+    packages=['gitman'],
+
+    install_requires=['requests>=2.12'],
+
+    entry_points={
+        'console_scripts': [
+            'gitman = gitman.__main__:main'
+        ]
+    },
+
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+    ]
+
+)

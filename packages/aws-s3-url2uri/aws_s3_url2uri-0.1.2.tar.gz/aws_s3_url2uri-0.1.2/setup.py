5ff597a0ec77ca7491e8b37063aa2d437496fd29
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:11:00 +0000

    Add package aws-s3-url2uri/0.1.2/aws_s3_url2uri-0.1.2.tar.gz

---
+++ packages/aws-s3-url2uri/aws_s3_url2uri-0.1.2.tar.gz/aws_s3_url2uri-0.1.2/setup.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+from pip.req import parse_requirements
+
+with open('README.md') as f:
+    long_description = f.read()
+
+install_requires = [
+    str(pr.req) for pr in parse_requirements('requirements.txt', session='hack')
+]
+
+
+setup(
+    name='aws_s3_url2uri',
+    description='Make awscli s3 commands work with URL',
+    long_description=long_description,
+    version='0.1.2',
+    author='NAKATA Keisuke',
+    author_email='keisuke.nakata.919@gmail.com',
+    url='https://github.com/keisuke-nakata/aws-cli-s3-url2uri',
+    license='Apache License 2.0',
+    packages=find_packages(),
+    install_requires=install_requires,
+    test_suite='tests',
+    entry_points={
+        'console_scripts': [
+            'aws_s3_url2uri=aws_s3_url2uri.main:aws_s3_url2uri'
+        ]
+    }
+)

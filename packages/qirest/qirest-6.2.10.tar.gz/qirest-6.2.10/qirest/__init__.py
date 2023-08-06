42bdbf5995ad5d90784cd837fa37838efdc63d6f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:20:21 +0000

    Add package qirest/6.2.10/qirest-6.2.10.tar.gz

---
+++ packages/qirest/qirest-6.2.10.tar.gz/qirest-6.2.10/qirest/__init__.py
@@ -0,0 +1,7 @@
+"""The Quantitative Imaging Profile (*QiPr*) REST server."""
+
+__version__ = '6.2.10'
+"""
+The one-based major.minor.patch version. Minor and patch
+version numbers begin at 1.
+"""

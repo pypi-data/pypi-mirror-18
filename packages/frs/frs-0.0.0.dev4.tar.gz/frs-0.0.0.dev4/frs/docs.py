eaa90c590f7534c11802dbc56165e774ea9a4395
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:51:20 +0000

    Add package frs/0.0.0.dev4/frs-0.0.0.dev4.tar.gz

---
+++ packages/frs/frs-0.0.0.dev4.tar.gz/frs-0.0.0.dev4/frs/docs.py
@@ -0,0 +1,20 @@
+from flask import url_for, redirect, Response
+
+from flask import Blueprint
+
+
+def make_blueprint(api):
+    bp = Blueprint(api._config_prefix, __name__, static_folder='static')
+
+    @bp.route('/')
+    def get_root():
+        return redirect(url_for(
+            '.static',
+            filename='index.html',
+            url=url_for('.get_swagger')))
+
+    @bp.route('/swagger.yaml')
+    def get_swagger():
+        return Response(api.get_spec_text())
+
+    return bp

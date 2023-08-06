a3b14589e76d6dd362ca3102e495e2d626e42286
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:51:11 +0000

    Add package sahir_py_std/0.8.4/sahir_py_std-0.8.4.tar.gz

---
+++ packages/sahir_py_std/sahir_py_std-0.8.4.tar.gz/sahir_py_std-0.8.4/sahirPyStd/standard_deviation.py
@@ -0,0 +1,23 @@
+import numpy as np
+import itertools
+
+def standard_deviation(x):
+    
+    if len(x) == 0:
+        raise ValueError('No Values')
+    elif len(x) == 1 and type(x[0]) != list:
+        raise ValueError('No standard deviation of one number')
+    else:
+        list_flat = []
+        for element in x:
+            if type(element) != list:
+                list_flat = list_flat + [[element]]
+            else:
+                list_flat = list_flat + [element]
+
+    list_flat = list(itertools.chain(*list_flat))
+    n = len(list_flat)
+    mean = sum(list_flat) / n
+    ssq = sum((x_i-mean)**2 for x_i in list_flat)
+    stdev = (ssq/n)**0.5
+    return(stdev)

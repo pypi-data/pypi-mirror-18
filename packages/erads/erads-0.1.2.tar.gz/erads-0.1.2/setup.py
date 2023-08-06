d0a186785aeef08f188a429c744fff5ac81b891c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:08:48 +0000

    Add package erads/0.1.2/erads-0.1.2.tar.gz

---
+++ packages/erads/erads-0.1.2.tar.gz/erads-0.1.2/setup.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+with open('README.rst') as readme_file:
+    readme = readme_file.read()
+
+with open('HISTORY.rst') as history_file:
+    history = history_file.read()
+
+requirements = [
+    # TODO: put package requirements here
+]
+
+test_requirements = [
+    # TODO: put package test requirements here
+]
+
+setup(
+    name='erads',
+    version='0.1.2',
+    description="fetch data, a platform for era group",
+    long_description=readme + '\n\n' + history,
+    author="gongshijun",
+    author_email='gongshijun@ict.ac.cn',
+    url='https://github.com/gongshijun/erads',
+    packages=find_packages('src', exclude=["*.tests", "*.tests.*", "tests.*", "tests"]),
+    # exclude_package=
+    package_dir={'':
+                 'src'},
+    package_data={
+        # If any package contains *.rst files, include them:
+        '':['*.txt', '*.rst'],
+    },
+    exclude_package_data={'': ['README.txt']},
+    include_package_data=True,
+    install_requires=requirements,
+    scripts=['src/scripts/test'],
+    # entry_points
+    license="MIT license",
+    zip_safe=False,
+    keywords='erads',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        "Programming Language :: Python :: 2",
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+    test_suite='tests',
+    tests_require=test_requirements
+)
---
+++ packages/erads/erads-0.1.2.tar.gz/erads-0.1.2/tests/test_erads.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+test_erads
+----------------------------------
+
+Tests for `erads` module.
+"""
+
+
+import sys
+import unittest
+
+from erads import erads
+
+
+
+class TestErads(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_000_something(self):
+        pass

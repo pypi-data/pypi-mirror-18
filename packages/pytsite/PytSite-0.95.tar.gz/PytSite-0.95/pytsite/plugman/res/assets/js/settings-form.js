1bfcf4c2de05fcd538039c4e198844b7025cb3eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:45:30 +0000

    Add package pytsite/0.95/PytSite-0.95.tar.gz

---
+++ packages/pytsite/PytSite-0.95.tar.gz/PytSite-0.95/pytsite/plugman/res/assets/js/settings-form.js
@@ -0,0 +1,36 @@
+$(function () {
+    var form = $('.pytsite-form.setting-uid-plugman');
+
+    form.on('pytsite.form.forward', function () {
+        var actionBtn = form.find('.action-btn');
+
+        actionBtn.click(function (e) {
+            e.preventDefault();
+
+            var btn = $(this);
+            var actionBtns = form.find('.action-btn');
+            var icon = btn.find('i.fa');
+            var endpoint = btn.attr('data-ep');
+            var iconClass = icon.attr('class');
+
+            // Disable all action buttons
+            actionBtns.attr('disabled', true);
+
+            // Add spinner to the clicked button
+            icon.attr('class', 'fa fa-spin fa-spinner');
+
+            pytsite.httpApi.post(endpoint, {name: btn.data('name')}).done(function () {
+                // Wait some time while application finishes reloading
+                setTimeout(function () {
+                    location.reload();
+                }, 5000);
+            }).fail(function (r) {
+                actionBtns.attr('disabled', false);
+                icon.attr('class', iconClass);
+
+                if ('error' in r.responseJSON)
+                    alert(r.responseJSON.error);
+            });
+        });
+    });
+});

c633662e83064d8ce4095ace8343e85c2b7a8fd7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:17:04 +0000

    Add package pyoa/0.106/pyoa-0.106.tar.gz

---
+++ packages/pyoa/pyoa-0.106.tar.gz/pyoa-0.106/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='pyoa',
+      version='0.106',
+      description='A set of functions for processing and analyzing classical Arabic texts in OpenArabic',
+      url='http://github.com/openarabic/pyoa',
+      author='Maxim Romanov',
+      author_email='maxim.romanov@uni-leipzig.de',
+      license='MIT',
+      packages=['pyoa'],
+      zip_safe=False)

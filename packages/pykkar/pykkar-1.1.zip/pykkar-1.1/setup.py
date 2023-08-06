1e6bdb660c24da7751358c4e95c6b00d5be62dc6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:10:39 +0000

    Add package pykkar/1.1/pykkar-1.1.zip

---
+++ packages/pykkar/pykkar-1.1.zip/pykkar-1.1/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+setup(
+      name="pykkar",
+      version=1.1,
+      description="Virtual robot library for teaching programming",
+      url="https://bitbucket.org/plas/pykkar",
+      license="MIT",
+      classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Education",
+        "License :: Freeware",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 3",
+        "Topic :: Education",
+      ],
+      keywords="robot pykkar education karel",
+      py_modules=["pykkar"],
+)

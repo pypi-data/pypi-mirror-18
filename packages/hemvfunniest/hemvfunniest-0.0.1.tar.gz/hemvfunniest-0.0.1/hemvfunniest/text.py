b5b1e81f2241300ee3b5d587e7b5f3c3d289bc03
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:44:35 +0000

    Add package hemvfunniest/0.0.1/hemvfunniest-0.0.1.tar.gz

---
+++ packages/hemvfunniest/hemvfunniest-0.0.1.tar.gz/hemvfunniest-0.0.1/hemvfunniest/text.py
@@ -0,0 +1,4 @@
+from markdown import markdown
+def joke():
+    return (u'Hello! Wenn ist das Nunst\u00fcck git und Slotermeyer? Ja! ... '
+            u'Beiherhund das Oder die Flipperwaldt gersput.')

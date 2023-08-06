64f727236d43cf9f95676a760e2e4f2e57cc5361
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:50:47 +0000

    Add package sahir_py_std/0.4/sahir_py_std-0.4.tar.gz

---
+++ packages/sahir_py_std/sahir_py_std-0.4.tar.gz/sahir_py_std-0.4/sahirPyStd/standard_deviation.py
@@ -0,0 +1,12 @@
+## This is just to practice creating packages
+
+import numpy as np
+
+def standard_deviation(x):
+    ary = np.array(x)
+    ary = ary.flatten(ary)
+    n = len(ary)
+    mean = sum(ary) / n
+    ssq = sum((x_i-mean)**2 for x_i in ary)
+    stdev = (ssq/n)**0.5
+    return(stdev)

e72226c822c203bbc6dd7546e362121c44fe7692
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:08:13 +0000

    Add package user_behavior/1.3.1/user_behavior-1.3.1.zip

---
+++ packages/user_behavior/user_behavior-1.3.1.zip/user_behavior-1.3.1/user_behavior/middleware.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import
+
+import json
+from django.forms import model_to_dict
+from datetime import date
+
+from django.contrib.auth.models import User
+
+from .serializers import ApiInfoSerializer
+from .services import get_request_data, set_response_data
+from .tasks import task_user_behavior
+from django.conf import settings
+
+
+class UserBehaviorExtractMiddleware(object):
+
+    def __init__(self):
+        self._api_info = None
+
+    def __call__(self, *args, **kwargs):
+        self._api_info = None
+
+    def process_request(self, request):
+        if hasattr(request, 'user') and request.user.id and request.method == 'POST':
+            request_data = get_request_data(request)
+
+            serializer = ApiInfoSerializer(data=request_data, partial=True)
+
+            if serializer.is_valid():
+                user = User.objects.get(id=request.user.id)
+                api_info = serializer.save(user=user)
+                self._api_info = api_info
+
+    def process_response(self, request, response):
+        if request.method == 'POST':
+            api_info = set_response_data(self._api_info, response)
+            task_user_behavior(api_info)
+        return response

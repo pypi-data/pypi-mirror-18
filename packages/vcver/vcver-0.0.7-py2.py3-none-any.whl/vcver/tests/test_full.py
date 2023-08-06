4573930a358195960d9eee5a474c87b50ebe583d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:18:40 +0000

    Add package vcver/0.0.7/vcver-0.0.7-py2.py3-none-any.whl

---
+++ packages/vcver/vcver-0.0.7-py2.py3-none-any.whl/vcver/tests/test_full.py
@@ -0,0 +1,23 @@
+import vcver
+
+
+def test_happy_case():
+    """
+    instead of setting up a git repo,
+    just testing that vcver's own
+    version string matches the
+    expected format.
+    """
+    v = vcver.get_version()
+    version, scm = v.split("+")
+    major, minor, patch, dev = version.split(".")
+    branch, change_id = scm.split(".")
+    # scm_change_id should be a git hash.
+    assert set(change_id) & set("abcdef1234567890") == set(change_id)
+    # major / minor should be an int
+    int(major)
+    int(minor)
+    int(patch)
+    assert dev.startswith("dev")
+    # commitcount should be an int
+    int(dev[3:])

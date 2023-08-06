3906834995c4fa445b9d07fe66be4a82ff8f0d43
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:50:45 +0000

    Add package iplocate/2.2.3/iplocate-2.2.3.tar.gz

---
+++ packages/iplocate/iplocate-2.2.3.tar.gz/iplocate-2.2.3/setup.py
@@ -0,0 +1,39 @@
+# coding=utf8
+from setuptools import setup
+__author__ = 'hellflame'
+
+
+setup(
+    name='iplocate',
+    version='2.2.3',
+    keywords=('ip', 'ipv4', 'ipv6', 'location', 'ip2location', 'ip to location'),
+    description="在终端获取本机或者指定ip地址",
+    license="MIT",
+    author='hellflame',
+    author_email="hellflamedly@gmail.com",
+    url="https://github.com/hellflame/iplocate",
+    packages=['IpLocate'],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        "Environment :: Console",
+        "Operating System :: MacOS",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: POSIX :: Linux",
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.4'
+    ],
+    install_requires=[
+        "paramSeeker"
+    ],
+    platforms="any",
+    entry_points={
+        'console_scripts': [
+            'iplocate=IpLocate.run:main'
+        ]
+    }
+)
+
+

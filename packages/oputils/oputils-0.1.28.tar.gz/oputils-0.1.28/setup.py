dd2c43a28bc486c28b8ad890ed714c79e9e6f0c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:20:29 +0000

    Add package oputils/0.1.28/oputils-0.1.28.tar.gz

---
+++ packages/oputils/oputils-0.1.28.tar.gz/oputils-0.1.28/setup.py
@@ -0,0 +1,32 @@
+from distutils.core import setup
+
+setup(
+    # Application name:
+    name="oputils",
+
+    # Version number (initial):
+    version="0.1.28",
+
+    # Application author details:
+    author="James Raby",
+    author_email="james.raby@lonelyplanet.com",
+
+    # Packages
+    packages=["oputils"],
+
+    # Include additional files into the package
+    include_package_data=True,
+
+    # Details
+    url="http://pypi.python.org/pypi/oputils_v0128/",
+
+    #
+    # license="LICENSE.txt",
+    description="Open planet python utilities",
+
+    # long_description=open("README.txt").read(),
+
+    # Dependent packages (distributions)
+    install_requires=[
+    ]
+)

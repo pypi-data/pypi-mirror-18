566210cf2ddc29f249bea4cbba23b035c015198c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:20:19 +0000

    Add package python-watcher/0.33.0/python-watcher-0.33.0.tar.gz

---
+++ packages/python-watcher/python-watcher-0.33.0.tar.gz/python-watcher-0.33.0/watcher/applier/workflow_engine/default.py
@@ -0,0 +1,157 @@
+# -*- encoding: utf-8 -*-
+# Copyright (c) 2016 b<>com
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+# implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+from oslo_log import log
+from taskflow import engines
+from taskflow.patterns import graph_flow as gf
+from taskflow import task
+
+from watcher._i18n import _LE, _LW, _LC
+from watcher.applier.workflow_engine import base
+from watcher.common import exception
+from watcher import objects
+
+LOG = log.getLogger(__name__)
+
+
+class DefaultWorkFlowEngine(base.BaseWorkFlowEngine):
+    """Taskflow as a workflow engine for Watcher
+
+    Full documentation on taskflow at
+    http://docs.openstack.org/developer/taskflow/
+    """
+
+    def decider(self, history):
+        # FIXME(jed) not possible with the current Watcher Planner
+        #
+        # decider – A callback function that will be expected to
+        # decide at runtime whether v should be allowed to execute
+        # (or whether the execution of v should be ignored,
+        # and therefore not executed). It is expected to take as single
+        # keyword argument history which will be the execution results of
+        # all u decideable links that have v as a target. It is expected
+        # to return a single boolean
+        # (True to allow v execution or False to not).
+        return True
+
+    def execute(self, actions):
+        try:
+            # NOTE(jed) We want to have a strong separation of concern
+            # between the Watcher planner and the Watcher Applier in order
+            # to us the possibility to support several workflow engine.
+            # We want to provide the 'taskflow' engine by
+            # default although we still want to leave the possibility for
+            # the users to change it.
+            # todo(jed) we need to change the way the actions are stored.
+            # The current implementation only use a linked list of actions.
+            # todo(jed) add olso conf for retry and name
+            flow = gf.Flow("watcher_flow")
+            previous = None
+            for a in actions:
+                task = TaskFlowActionContainer(a, self)
+                flow.add(task)
+                if previous is None:
+                    previous = task
+                    # we have only one Action in the Action Plan
+                    if len(actions) == 1:
+                        nop = TaskFlowNop()
+                        flow.add(nop)
+                        flow.link(previous, nop)
+                else:
+                    # decider == guard (UML)
+                    flow.link(previous, task, decider=self.decider)
+                    previous = task
+
+            e = engines.load(flow)
+            e.run()
+
+        except Exception as e:
+            raise exception.WorkflowExecutionException(error=e)
+
+
+class TaskFlowActionContainer(task.Task):
+    def __init__(self, db_action, engine):
+        name = "action_type:{0} uuid:{1}".format(db_action.action_type,
+                                                 db_action.uuid)
+        super(TaskFlowActionContainer, self).__init__(name=name)
+        self._db_action = db_action
+        self._engine = engine
+        self.loaded_action = None
+
+    @property
+    def action(self):
+        if self.loaded_action is None:
+            action = self.engine.action_factory.make_action(
+                self._db_action,
+                osc=self._engine.osc)
+            self.loaded_action = action
+        return self.loaded_action
+
+    @property
+    def engine(self):
+        return self._engine
+
+    def pre_execute(self):
+        try:
+            self.engine.notify(self._db_action, objects.action.State.ONGOING)
+            LOG.debug("Pre-condition action: %s", self.name)
+            self.action.pre_condition()
+        except Exception as e:
+            LOG.exception(e)
+            self.engine.notify(self._db_action, objects.action.State.FAILED)
+            raise
+
+    def execute(self, *args, **kwargs):
+        try:
+            LOG.debug("Running action: %s", self.name)
+
+            self.action.execute()
+            self.engine.notify(self._db_action, objects.action.State.SUCCEEDED)
+        except Exception as e:
+            LOG.exception(e)
+            LOG.error(_LE('The workflow engine has failed '
+                          'to execute the action: %s'), self.name)
+
+            self.engine.notify(self._db_action, objects.action.State.FAILED)
+            raise
+
+    def post_execute(self):
+        try:
+            LOG.debug("Post-condition action: %s", self.name)
+            self.action.post_condition()
+        except Exception as e:
+            LOG.exception(e)
+            self.engine.notify(self._db_action, objects.action.State.FAILED)
+            raise
+
+    def revert(self, *args, **kwargs):
+        LOG.warning(_LW("Revert action: %s"), self.name)
+        try:
+            # TODO(jed): do we need to update the states in case of failure?
+            self.action.revert()
+        except Exception as e:
+            LOG.exception(e)
+            LOG.critical(_LC("Oops! We need a disaster recover plan."))
+
+
+class TaskFlowNop(task.Task):
+    """This class is used in case of the workflow have only one Action.
+
+    We need at least two atoms to create a link.
+    """
+    def execute(self):
+        pass
---
+++ packages/python-watcher/python-watcher-0.33.0.tar.gz/python-watcher-0.33.0/watcher/tests/applier/workflow_engine/test_default_workflow_engine.py
@@ -0,0 +1,184 @@
+# -*- encoding: utf-8 -*-
+# Copyright (c) 2015 b<>com
+#
+# Authors: Jean-Emile DARTOIS <jean-emile.dartois@b-com.com>
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+# implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+import abc
+import mock
+
+import six
+
+from watcher.applier.actions import base as abase
+from watcher.applier.actions import factory
+from watcher.applier.workflow_engine import default as tflow
+from watcher.common import exception
+from watcher.common import utils
+from watcher import objects
+from watcher.tests.db import base
+
+
+class ExpectedException(Exception):
+    pass
+
+
+@six.add_metaclass(abc.ABCMeta)
+class FakeAction(abase.BaseAction):
+    def schema(self):
+        pass
+
+    def post_condition(self):
+        pass
+
+    def pre_condition(self):
+        pass
+
+    def revert(self):
+        pass
+
+    def execute(self):
+        raise ExpectedException()
+
+
+class TestDefaultWorkFlowEngine(base.DbTestCase):
+    def setUp(self):
+        super(TestDefaultWorkFlowEngine, self).setUp()
+        self.engine = tflow.DefaultWorkFlowEngine(
+            config=mock.Mock(),
+            context=self.context,
+            applier_manager=mock.MagicMock())
+
+    @mock.patch('taskflow.engines.load')
+    @mock.patch('taskflow.patterns.graph_flow.Flow.link')
+    def test_execute(self, graph_flow, engines):
+        actions = mock.MagicMock()
+        try:
+            self.engine.execute(actions)
+            self.assertTrue(engines.called)
+        except Exception as exc:
+            self.fail(exc)
+
+    def create_action(self, action_type, parameters, next):
+        action = {
+            'uuid': utils.generate_uuid(),
+            'action_plan_id': 0,
+            'action_type': action_type,
+            'input_parameters': parameters,
+            'state': objects.action.State.PENDING,
+            'next': next,
+        }
+        new_action = objects.Action(self.context, **action)
+        new_action.create()
+        new_action.save()
+
+        return new_action
+
+    def check_action_state(self, action, expected_state):
+        to_check = objects.Action.get_by_uuid(self.context, action.uuid)
+        self.assertEqual(expected_state, to_check.state)
+
+    def check_actions_state(self, actions, expected_state):
+        for a in actions:
+            self.check_action_state(a, expected_state)
+
+    @mock.patch('taskflow.engines.load')
+    @mock.patch('taskflow.patterns.graph_flow.Flow.link')
+    def test_execute_with_no_actions(self, graph_flow, engines):
+        actions = []
+        try:
+            self.engine.execute(actions)
+            self.assertFalse(graph_flow.called)
+            self.assertTrue(engines.called)
+        except Exception as exc:
+            self.fail(exc)
+
+    def test_execute_with_one_action(self):
+        actions = [self.create_action("nop", {'message': 'test'}, None)]
+        try:
+            self.engine.execute(actions)
+            self.check_actions_state(actions, objects.action.State.SUCCEEDED)
+
+        except Exception as exc:
+            self.fail(exc)
+
+    def test_execute_with_two_actions(self):
+        actions = []
+        second = self.create_action("sleep", {'duration': 0.0}, None)
+        first = self.create_action("nop", {'message': 'test'}, second.id)
+
+        actions.append(first)
+        actions.append(second)
+
+        try:
+            self.engine.execute(actions)
+            self.check_actions_state(actions, objects.action.State.SUCCEEDED)
+
+        except Exception as exc:
+            self.fail(exc)
+
+    def test_execute_with_three_actions(self):
+        actions = []
+
+        third = self.create_action("nop", {'message': 'next'}, None)
+        second = self.create_action("sleep", {'duration': 0.0}, third.id)
+        first = self.create_action("nop", {'message': 'hello'}, second.id)
+
+        self.check_action_state(first, objects.action.State.PENDING)
+        self.check_action_state(second, objects.action.State.PENDING)
+        self.check_action_state(third, objects.action.State.PENDING)
+
+        actions.append(first)
+        actions.append(second)
+        actions.append(third)
+
+        try:
+            self.engine.execute(actions)
+            self.check_actions_state(actions, objects.action.State.SUCCEEDED)
+
+        except Exception as exc:
+            self.fail(exc)
+
+    def test_execute_with_exception(self):
+        actions = []
+
+        third = self.create_action("no_exist", {'message': 'next'}, None)
+        second = self.create_action("sleep", {'duration': 0.0}, third.id)
+        first = self.create_action("nop", {'message': 'hello'}, second.id)
+
+        self.check_action_state(first, objects.action.State.PENDING)
+        self.check_action_state(second, objects.action.State.PENDING)
+        self.check_action_state(third, objects.action.State.PENDING)
+
+        actions.append(first)
+        actions.append(second)
+        actions.append(third)
+
+        self.assertRaises(exception.WorkflowExecutionException,
+                          self.engine.execute, actions)
+
+        self.check_action_state(first, objects.action.State.SUCCEEDED)
+        self.check_action_state(second, objects.action.State.SUCCEEDED)
+        self.check_action_state(third, objects.action.State.FAILED)
+
+    @mock.patch.object(factory.ActionFactory, "make_action")
+    def test_execute_with_action_exception(self, m_make_action):
+        actions = [self.create_action("fake_action", {}, None)]
+        m_make_action.return_value = FakeAction(mock.Mock())
+
+        exc = self.assertRaises(exception.WorkflowExecutionException,
+                                self.engine.execute, actions)
+
+        self.assertIsInstance(exc.kwargs['error'], ExpectedException)
+        self.check_action_state(actions[0], objects.action.State.FAILED)

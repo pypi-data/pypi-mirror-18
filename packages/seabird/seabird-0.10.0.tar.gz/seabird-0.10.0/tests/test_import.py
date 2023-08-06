2c49e1a1e53e426106785c4dd83134cdd24fe0e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:12:47 +0000

    Add package seabird/0.10.0/seabird-0.10.0.tar.gz

---
+++ packages/seabird/seabird-0.10.0.tar.gz/seabird-0.10.0/tests/test_import.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+def test_answer():
+    import numpy
+    import seabird
+    import seabird.cnv
+    from seabird.cnv import CNV
+    from seabird.cnv import fCNV

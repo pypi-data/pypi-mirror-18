358fcade56dd842579e14850e8e2eed02f39a751
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:10:59 +0000

    Add package nester_redbrush/1.0.1/nester_redbrush-1.0.1.zip

---
+++ packages/nester_redbrush/nester_redbrush-1.0.1.zip/nester_redbrush-1.0.1/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup (
+        name = 'nester_redbrush',
+        version = '1.0.1',
+        py_modules=['nester'],
+        author='hfpython',
+        author_email='redbrush@163.com',
+        url='http://www.headfirstlabs.com',
+        description='just a test',
+        )
+        

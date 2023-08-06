ecb2f15a0f491fa77d71fdbff3d6450a11738306
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:10:32 +0000

    Add package django-mixinvideos/0.1.2/django-mixinvideos-0.1.2.tar.gz

---
+++ packages/django-mixinvideos/django-mixinvideos-0.1.2.tar.gz/django-mixinvideos-0.1.2/setup.py
@@ -0,0 +1,15 @@
+# coding: utf-8
+from setuptools import setup
+
+
+setup(
+    name='django-mixinvideos',
+    version='0.1.2',
+    url='https://github.com/edussilva/django-mixinvideos',
+    author='Eduardo Silva',
+    author_email='edussilva@live.com',
+    keywords='django mixins mixin video videos',
+    description='Mixin for videos',
+    packages=['mixin_videos'],
+    isntall_requires=['requests'],
+)

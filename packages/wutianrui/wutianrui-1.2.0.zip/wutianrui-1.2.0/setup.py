cd9842fb4406dd0e74a4ca6345b056006347912b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:18:44 +0000

    Add package wutianrui/1.2.0/wutianrui-1.2.0.zip

---
+++ packages/wutianrui/wutianrui-1.2.0.zip/wutianrui-1.2.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup (
+    name = 'wutianrui',
+    version = '1.2.0',
+    py_modules = ['nester'],
+    author = 'wutianrui',
+    author_email = '2396402057@qq.com',
+    url = 'http://www.woolinux.pub',
+    description = 'A simple printer of nested lists',
+    )

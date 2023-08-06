8a0d2d2da0184d82743b2c77975efc7385011ddd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:19:12 +0000

    Add package animop-ep/0.1/animop_ep.tar.gz

---
+++ packages/animop-ep/animop_ep.tar.gz/animop/animop_ep/__init__.py
@@ -0,0 +1,29 @@
+import urllib.request as urllib
+from bs4 import BeautifulSoup
+import re
+import unshortenit
+
+class AnimOp:
+
+    def __init__(self):
+        self.url = ''
+        self.first = ''
+        self.last = ''
+        self.fname = ''
+
+    def link(self):
+        resp = urllib.urlopen(self.url)
+        soup = BeautifulSoup(resp.read())
+
+        link = soup.find_all('a', href=re.compile(r'http:\/\/adf.ly\/[\s\S].....'), text=re.compile(r'\d'))
+        i = 0
+        for value in link:
+            i += 1
+            if (i > self.first) and (i < self.last):
+                unshortened_uri, status = unshortenit.unshorten_only(value['href'])
+                print(unshortened_uri, value.contents[0])
+                with open(self.fname + ".txt", "a") as myfile:
+                    myfile.write(unshortened_uri + " - " + value.contents[0] + "\n")
+            else:
+                pass
+        return "Done!"

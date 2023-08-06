2d4dc9a3f0510012032ac04d1349b3550231bf9b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:09:39 +0000

    Add package fs_extra/0.0.3/fs_extra-0.0.3.zip

---
+++ packages/fs_extra/fs_extra-0.0.3.zip/fs_extra-0.0.3/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+  name = 'fs_extra',
+  packages = ['fs_extra'],
+  version = '0.0.3',
+  description = 'One-liners for filesystem manipulations',
+  author = 'robert dobre',
+  author_email = 'dobre_robert_marius@yahoo.com',
+  url = 'https://github.com/dobre-robert-marius/fs_extra',
+  download_url = 'https://github.com/dobre-robert-marius/fs_extra/tarball/0.0.1',
+  keywords = ['filesystem', 'file manipulation'],
+  classifiers = []
+)

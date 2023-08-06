2d1c1ebf360eb5af8a8af41d89393bdbad306d46
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:22:53 +0000

    Add package enhance/1.1/enhance-1.1.tar.gz

---
+++ packages/enhance/enhance-1.1.tar.gz/enhance-1.1/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+
+setup(
+    name = "enhance",
+    packages = ["enhance"],
+    entry_points = {
+                "console_scripts": ['enhance = enhance.srcnn:main']
+    },
+    version = '1.1',
+    description = "ENHANCE",
+    long_description = """ command-line utility for enhancing images. seriously.
+
+    See full readme at https://github.com/rueberger/enhance
+    """,
+    author = "Andrew Berger",
+    author_email = "andbberger@gmail.co"
+    )

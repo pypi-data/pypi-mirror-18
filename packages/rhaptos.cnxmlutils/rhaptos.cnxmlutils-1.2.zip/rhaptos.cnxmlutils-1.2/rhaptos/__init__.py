78943772cc50b9f577e8e316db6597b10c81cdc3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:20:12 +0000

    Add package rhaptos.cnxmlutils/1.2/rhaptos.cnxmlutils-1.2.zip

---
+++ packages/rhaptos.cnxmlutils/rhaptos.cnxmlutils-1.2.zip/rhaptos.cnxmlutils-1.2/rhaptos/__init__.py
@@ -0,0 +1,12 @@
+# See http://peak.telecommunity.com/DevCenter/setuptools#namespace-packages
+"""
+Copyright (C) 2013 Rice University
+
+This software is subject to the provisions of the GNU AFFERO GENERAL PUBLIC LICENSE Version 3.0 (AGPL).  
+See LICENSE.txt for details.
+"""
+try:
+    __import__('pkg_resources').declare_namespace(__name__)
+except ImportError:
+    from pkgutil import extend_path
+    __path__ = extend_path(__path__, __name__)

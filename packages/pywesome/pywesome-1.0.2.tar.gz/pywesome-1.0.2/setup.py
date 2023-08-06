70cfd30b0cde9a2ac31323395860601f7ca8e525
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:19:11 +0000

    Add package pywesome/1.0.2/pywesome-1.0.2.tar.gz

---
+++ packages/pywesome/pywesome-1.0.2.tar.gz/pywesome-1.0.2/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name = 'pywesome',
+    version = '1.0.2',
+    description = 'Python lists on steroidss',
+    packages = ['pywesome'],
+    author = 'Lucas Bedout',
+	author_email = 'roadtostark@gmail.com',
+	url = 'https://github.com/lucasbedout/pywesome', # use the URL to the github repo
+	download_url = 'https://github.com/lucasbedout/pywesome/tarball/v1.0.2', # I'll explain this in a second
+	keywords = ['list', 'collection', 'map', 'reduce', 'filter'],
+)

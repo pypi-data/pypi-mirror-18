ba7019d4b83ae17c70102304a6ad74462db0b38a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:10:33 +0000

    Add package pygments-ccl-lexer/0.7.1/pygments-ccl-lexer-0.7.1.tar.gz

---
+++ packages/pygments-ccl-lexer/pygments-ccl-lexer-0.7.1.tar.gz/pygments-ccl-lexer-0.7.1/ccl_lexer/__init__.py
@@ -0,0 +1,34 @@
+from pygments.lexers.sql import PostgresLexer
+from pygments.token import Keyword, Name, Operator
+
+__all__ = ['CCL_Lexer']
+
+
+CCL_KEYWORDS = ['select', 'from', 'where', 'with', 'order by']
+CCL_OPERATOR_WORDS = ['in', 'between', 'and', 'or', 'distinct']
+CCL_FUNCTIONS = ['substring', 'piece','cnvtint','cnvtdatetime', 'uar_get_code_display']
+CCL_CONSTANTS = ['curdate']
+class CCL_Lexer(PostgresLexer):
+    name = 'CCL'
+    aliases = ['ccl', 'prg']
+    filenames = ['*.ccl']
+
+    def get_tokens_unprocessed(self, text):
+        extra_content = [
+            (CCL_FUNCTIONS, Name.Function),
+            (CCL_KEYWORDS, Keyword),
+            (CCL_OPERATOR_WORDS, Operator.Word),
+            (CCL_CONSTANTS, Name.Constant)
+        ]
+
+        for index, token, value in PostgresLexer.get_tokens_unprocessed(self, text):
+            if token is Name:
+                for i in extra_content:
+                    if value in i[0]:
+                        yield index, i[1], value
+                        break;
+                else:
+                    yield index, token, value
+            else:
+                yield index, token, value
+

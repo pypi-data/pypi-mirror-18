df1b0b9e39e6d21c01d850126a50e5bbfeb7da80
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:17:32 +0000

    Add package sayhello/0.1.1/sayhello-0.1.1.tar.gz

---
+++ packages/sayhello/sayhello-0.1.1.tar.gz/sayhello-0.1.1/setup.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+#encoding=utf-8
+
+from setuptools import setup, find_packages
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+with open(path.join(here, 'README.rst')) as f:
+    readme = f.read()
+
+setup(
+    name = 'sayhello',
+    version = '0.1.1',
+    author = 'catplanet',
+    description = 'say text on the command-line',
+    long_description = readme,
+    author_email='catplanet@qq.com',
+    url='https://github.com/catplant/say-hello',
+    packages=find_packages(exclude=['test*']),
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: Chinese (Simplified)',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+    #install_requires = ['doctop', 'requests'],
+    license = 'MIT',
+    keywords='say command text',
+    entry_points={
+        'console_scripts': ['say=sayhello:run']
+    }
+)

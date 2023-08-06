1515dfa51b47285ef7c8fdd8dc56444a20d0a8bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:43:26 +0000

    Add package nester_liuciuse/1.3.0/nester_liuciuse-1.3.0.zip

---
+++ packages/nester_liuciuse/nester_liuciuse-1.3.0.zip/nester_liuciuse-1.3.0/setup.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Dec 05 15:45:02 2016
+
+@author: Administrator
+"""
+
+from distutils.core import setup
+
+setup(
+      name = 'nester_liuciuse',
+      version = '1.3.0',
+      py_modules = ['nester'],
+      author = 'Leikai',
+      author_email = 'leikaikailkk@163.com',
+      url = 'https://github.com/liuciuse',
+      description = 'test',
+      )

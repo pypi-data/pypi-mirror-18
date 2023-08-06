b5c2a100b580bc9dac16b12ee0ef47d5e510230d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:44:59 +0000

    Add package twitterscraper/0.2.4/twitterscraper-0.2.4.zip

---
+++ packages/twitterscraper/twitterscraper-0.2.4.zip/twitterscraper-0.2.4/setup.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+from setuptools import setup, find_packages
+
+
+setup(
+    name='twitterscraper',
+    version='0.2.4',
+    description='Tool for scraping Tweets',
+    url='https://github.com/taspinar/twitterscraper',
+    author=['Ahmet Taspinar', 'Lasse Schuirmann'],
+    author_email='taspinar@gmail.com',
+    license='MIT',
+    packages=find_packages(exclude=["build.*", "tests", "tests.*"]),
+    install_requires=[
+        'bs4', 'lxml', 'fake_useragent'
+    ],
+    entry_points={
+        "console_scripts": [
+            "TwitterScraper = twitterscraper.main:main"
+        ]
+    })

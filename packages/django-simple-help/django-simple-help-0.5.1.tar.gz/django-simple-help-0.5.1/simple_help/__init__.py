2e369e0818bb741351311bcf01363441db7031a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:10:07 +0000

    Add package django-simple-help/0.5.1/django-simple-help-0.5.1.tar.gz

---
+++ packages/django-simple-help/django-simple-help-0.5.1.tar.gz/django-simple-help-0.5.1/simple_help/__init__.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+
+# django-simple-help
+# simple_help/__init__.py
+
+from __future__ import unicode_literals
+
+
+__all__ = [
+    "models",
+    "templatetags",
+    "translation",
+    "settings",
+    "admin",
+    "forms",
+    "migrations",
+    "south_migrations",
+    "apps",
+    "utils",
+    "default_app_config",
+]
+
+
+default_app_config = "simple_help.apps.Config"

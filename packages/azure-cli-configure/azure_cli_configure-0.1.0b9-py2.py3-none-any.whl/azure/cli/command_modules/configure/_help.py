50f36b4d4ec38926ea0cc46016213627c0d103a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:09:08 +0000

    Add package azure-cli-configure/0.1.0b9/azure_cli_configure-0.1.0b9-py2.py3-none-any.whl

---
+++ packages/azure-cli-configure/azure_cli_configure-0.1.0b9-py2.py3-none-any.whl/azure/cli/command_modules/configure/_help.py
@@ -0,0 +1,13 @@
+#---------------------------------------------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License. See License.txt in the project root for license information.
+#---------------------------------------------------------------------------------------------
+
+from azure.cli.core.help_files import helps #pylint: disable=unused-import
+
+#pylint: disable=line-too-long
+
+helps['configure'] = """
+            type: command
+            short-summary: Interactive experience for setting up the Azure CLI
+"""

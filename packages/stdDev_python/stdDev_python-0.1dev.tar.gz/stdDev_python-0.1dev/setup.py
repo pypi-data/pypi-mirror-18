36b7ac64a6f7ee15e2ce967269643dde06c98824
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:20:57 +0000

    Add package stdDev_python/0.1dev/stdDev_python-0.1dev.tar.gz

---
+++ packages/stdDev_python/stdDev_python-0.1dev.tar.gz/stdDev_python-0.1dev/setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='stdDev_python',
+    version='0.1dev',
+    packages=['stdDev',],
+    license='Creative Commons Attribution-Noncommercial-Share Alike license',
+    long_description=open('README.txt').read(),
+)
---
+++ packages/stdDev_python/stdDev_python-0.1dev.tar.gz/stdDev_python-0.1dev/stdDev/standard_deviation.py
@@ -0,0 +1,6 @@
+def standard_deviation(x):
+    n = len(x)
+    mean = sum(x) / n
+    ssq = sum((x_i-mean)**2 for x_i in x)
+    stdev = (ssq/n)**0.5
+    return(stdev)

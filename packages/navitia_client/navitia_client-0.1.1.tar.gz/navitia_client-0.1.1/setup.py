b7ef7f9e0deecb004b5d065f73289900c057af23
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:19:30 +0000

    Add package navitia_client/0.1.1/navitia_client-0.1.1.tar.gz

---
+++ packages/navitia_client/navitia_client-0.1.1.tar.gz/navitia_client-0.1.1/setup.py
@@ -0,0 +1,17 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+
+setup(
+    name='navitia_client',
+    packages=['navitia_client'],  # this must be the same as the name above
+    version='0.1.1',
+    description='(unofficial) navitia client for python',
+    author='Leonard Binet',
+    author_email='leonardbinet@gmail.com',
+    license='MIT',
+    url='https://github.com/leonardbinet/navitia-python',
+    classifiers=[],
+)

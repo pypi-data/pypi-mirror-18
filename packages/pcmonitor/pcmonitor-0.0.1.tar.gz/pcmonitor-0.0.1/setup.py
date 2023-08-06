0aea034163b86993fefb80bd21ed243dd4da4250
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:11:52 +0000

    Add package pcmonitor/0.0.1/pcmonitor-0.0.1.tar.gz

---
+++ packages/pcmonitor/pcmonitor-0.0.1.tar.gz/pcmonitor-0.0.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='pcmonitor',
+    version='0.0.1',
+    description='HTOP Python package',
+    author='suchbeat',
+    author_email='s.u.chbeat@gmail.com',
+    url='https://github.com/suchbeat/pcmonitor.git',
+    py_modules=['pcmonitor'],
+    install_requires=['psutil'],
+)

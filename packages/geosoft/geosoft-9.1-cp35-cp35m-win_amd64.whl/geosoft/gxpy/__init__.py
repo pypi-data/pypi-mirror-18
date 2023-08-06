e7e1d158ebdc18e38312269e084eea58edb2a1c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:51:30 +0000

    Add package geosoft/9.1/geosoft-9.1-cp35-cp35m-win_amd64.whl

---
+++ packages/geosoft/geosoft-9.1-cp35-cp35m-win_amd64.whl/geosoft/gxpy/__init__.py
@@ -0,0 +1,10 @@
+__all__ = ['system', 'gx', 'om', 'utility', 'vv', 'va', 'ipj', 'grd', 'gdb']
+from . import system
+from . import gx
+from . import om
+from . import utility
+from . import vv
+from . import va
+from . import ipj
+from . import grd
+from . import gdb

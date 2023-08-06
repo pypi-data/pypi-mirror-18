02e36fc93f4aabfe83015ed37fe4ef35171ba82b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:45:50 +0000

    Add package psycopg-postgis2/0.0.1/psycopg-postgis2-0.0.1.tar.gz

---
+++ packages/psycopg-postgis2/psycopg-postgis2-0.0.1.tar.gz/psycopg-postgis2-0.0.1/postgis/__meta__.py
@@ -0,0 +1,8 @@
+"""Pyscopg helpers to work with PostGIS."""
+
+VERSION = (0, 0, 1)
+
+__author__ = 'Yohan Boniface, Zhou Qi'
+__contact__ = "yohan.boniface@data.gouv.fr, bitwolaiye@gmail.com"
+__homepage__ = "https://github.com/bitwolaiye/psycopg-postgis"
+__version__ = ".".join(map(str, VERSION))

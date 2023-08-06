d29301824aa050107c2feeab89546ecb9f15c9c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:32:49 +0000

    Add package mkdocs-jwplayer/0.0.2/mkdocs-jwplayer-0.0.2.tar.gz

---
+++ packages/mkdocs-jwplayer/mkdocs-jwplayer-0.0.2.tar.gz/mkdocs-jwplayer-0.0.2/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+  name='mkdocs-jwplayer',
+  version='0.0.2',
+  url='https://github.com/jwplayer/mkdocs-jwplayer',
+  license='',
+  description='JW Player theme for MkDocs',
+  author='Matthew Martindale',
+  author_email='mmartindale@jwplayer.com',
+  packages=find_packages(),
+  include_package_data=True,
+  entry_points={
+    'mkdocs.themes': [
+      'jwplayer = jwplayer',
+    ]
+  },
+  zip_safe=False
+)

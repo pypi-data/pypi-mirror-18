94106797dba90c0bbf21c011d66f8a303e4bb515
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:10:29 +0000

    Add package funniest1/0.1/funniest1-0.1.tar.gz

---
+++ packages/funniest1/funniest1-0.1.tar.gz/funniest1-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='funniest1',
+      version='0.1',
+      description='The funniest joke in the world',
+      author='Flying Circus',
+      license='MIT',
+      packages=['funniest1'],
+      zip_safe=False)

38532e1d0e414ab4f34a6369202495d7a45b1682
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:31:07 +0000

    Add package pygments-ccl-lexer/0.7.63/pygments_ccl_lexer-0.7.63-py2-none-any.whl

---
+++ packages/pygments-ccl-lexer/pygments_ccl_lexer-0.7.63-py2-none-any.whl/ccl_lexer/ccl_definition.py
@@ -0,0 +1,31 @@
+from pygments.token import Keyword, Name, Operator, Text, Comment, Number, String, Punctuation, Token
+
+from definitions import FUNCTIONS, DATATYPES, KEYWORDS, PSEUDO_TYPES, NAMESPACE, OPERATOR_WORDS
+
+TOKEN_ASSIGNMENTS = {
+    'keywords': Keyword,
+    'datatypes': Keyword.Types,
+    'operator_words': Operator.Word,
+    'functions': Token.Name,
+    'namespace': Keyword.Namespace,
+    'pseudo_types': Keyword.Pseudo
+}
+
+_keywords = KEYWORDS
+_functions = FUNCTIONS
+_datatypes = DATATYPES
+_operator_words = OPERATOR_WORDS
+_namespace = NAMESPACE
+_pseudo_types = PSEUDO_TYPES
+
+
+def create_dictionary(t):
+    ccl_tokens = {}
+    for k, v in t:
+        if "__" not in k:
+            if k[0] == '_':
+                ccl_tokens[k[1:]] = [word.replace(' ', '\s') for word in v]
+    return ccl_tokens
+
+tokens = locals().items()
+CCL_TOKENS = create_dictionary(tokens)

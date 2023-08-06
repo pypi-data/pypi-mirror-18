7c4d4f0ae7a4f2e3fc1ac8397e943c8d062232f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:10:47 +0000

    Add package gallery-dl/0.6.1/gallery_dl-0.6.1-py3-none-any.whl

---
+++ packages/gallery-dl/gallery_dl-0.6.1-py3-none-any.whl/gallery_dl/cloudflare.py
@@ -0,0 +1,93 @@
+# -*- coding: utf-8 -*-
+
+# Copyright 2015, 2016 Mike Fährmann
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License version 2 as
+# published by the Free Software Foundation.
+
+"""Methods to access sites behind Cloudflare protection"""
+
+import time
+import operator
+import urllib.parse
+from . import text
+from .cache import cache
+
+def bypass(url, maxage):
+    def decorator(func):
+        solve = cache(maxage=maxage, keyarg=1)(solve_challenge)
+        def wrap(self, *args):
+            self.session.cookies = solve(self.session, url)
+            return func(self, *args)
+        return wrap
+    return decorator
+
+def bypass_ddos_protection(session, url):
+    """Prepare a requests.session to access 'url' behind Cloudflare protection"""
+    session.cookies = solve_challenge(session, url)
+    return session
+
+def solve_challenge(session, url):
+    session.headers["Referer"] = url
+    page = session.get(url).text
+    params = text.extract_all(page, (
+        ('jschl_vc', 'name="jschl_vc" value="', '"'),
+        ('pass'    , 'name="pass" value="', '"'),
+    ))[0]
+    params["jschl_answer"] = solve_jschl(url, page)
+    time.sleep(4)
+    url = urllib.parse.urljoin(url, "/cdn-cgi/l/chk_jschl")
+    session.get(url, params=params)
+    return session.cookies
+
+def solve_jschl(url, page):
+    """Solve challenge to get 'jschl_answer' value"""
+    data, pos = text.extract_all(page, (
+        ('var' , ',f, ', '='),
+        ('key' , '"', '"'),
+        ('expr', ':', '}')
+    ))
+    solution = evaluate_expression(data["expr"])
+    variable = "{}.{}".format(data["var"], data["key"])
+    vlength = len(variable)
+    expressions = text.extract(page, "'challenge-form');", "f.submit();", pos)[0]
+    for expr in expressions.split(";")[1:]:
+        if expr.startswith(variable):
+            func = operator_functions[expr[vlength]]
+            value = evaluate_expression(expr[vlength+2:])
+            solution = func(solution, value)
+        elif expr.startswith("a.value"):
+            return solution + len(urllib.parse.urlsplit(url).netloc)
+
+def evaluate_expression(expr):
+    """Evaluate a Javascript expression for the challange and return its value"""
+    stack = []
+    ranges = []
+    value = ""
+    for index, char in enumerate(expr):
+        if char == "(":
+            stack.append(index+1)
+        elif char == ")":
+            begin = stack.pop()
+            if stack:
+                ranges.append((begin, index))
+    for subexpr in [expr[begin:end] for begin, end in ranges] or (expr,):
+        num = 0
+        for part in subexpr.split("[]"):
+            num += expression_values[part]
+        value += str(num)
+    return int(value)
+
+operator_functions = {
+    "+": operator.add,
+    "-": operator.sub,
+    "*": operator.mul,
+}
+
+expression_values = {
+    "": 0,
+    "+": 0,
+    "!+": 1,
+    "+!!": 1,
+}
---
+++ packages/gallery-dl/gallery_dl-0.6.1-py3-none-any.whl/gallery_dl/extractor/8chan.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+
+# Copyright 2014-2016 Mike Fährmann
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License version 2 as
+# published by the Free Software Foundation.
+
+"""Extract images and videos from https://8ch.net/"""
+
+from . import chan
+
+class InfinitychanThreadExtractor(chan.ChanThreadExtractor):
+    """Extractor for images from threads from 8ch.net"""
+    category = "8chan"
+    filename_fmt = "{time}-{filename}{ext}"
+    pattern = [r"(?:https?://)?(?:www\.)?8ch\.net/([^/]+)/res/(\d+)"]
+    test = [("https://8ch.net/tg/res/175887.html", {
+        "url": "646d4230b40f9cff3f8674e3efe44bba3af4924b",
+        "keyword": "d313fa77f689ddcd45736f1e86bf63cf58614786",
+        "content": "81e21a3cc87f64f224a966f207e8e1731216c345",
+    })]
+    api_url = "https://8ch.net/{board}/res/{thread}.json"
+    file_url = "https://media.8ch.net/{board}/src/{tim}{ext}"
+    file_url_v2 = "https://media.8ch.net/file_store/{tim}{ext}"
+
+    def build_url(self, post):
+        fmt = self.file_url if len(post["tim"]) < 64 else self.file_url_v2
+        return fmt.format_map(post)

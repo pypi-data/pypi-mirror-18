031c5dada4cdd41fccbeba3b91c5042897ffd412
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:12:50 +0000

    Add package bids/0.0/bids-0.0.zip

---
+++ packages/bids/bids-0.0.zip/bids-0.0/setup.py
@@ -0,0 +1,20 @@
+# encoding: utf8
+
+from setuptools import setup
+
+
+setup(
+    author='Yaroslav Halchenko',
+    author_email='debian@onerussian.com',
+    description='bids: a template for small scientific Python projects',
+    install_requires=['pybids'],
+    long_description='''Use `pybids <https://pypi.python.org/pypi/pybids/>`_ instead.''',
+    maintainer='Yaroslav Halchenko',
+    maintainer_email='debian@onerussian.com',
+    name='bids',
+    platforms=['all'],
+    py_modules=['wheel-platform-tag-is-broken-on-empty-wheels-see-issue-141'],
+    url='https://pypi.python.org/pypi/pybids/',
+    version="0.0",
+    zip_safe=False,
+)

d1d67b4f924cc9546851c7d7c15f60eaa8585e18
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:19:20 +0000

    Add package plex_download/1.1.0/plex_download-1.1.0.tar.gz

---
+++ packages/plex_download/plex_download-1.1.0.tar.gz/plex_download-1.1.0/plex_download/__init__.py
@@ -0,0 +1,8 @@
+from __future__ import absolute_import
+
+from plex_download.client import DownloadClient
+from plex_download.version import DownloadablePlexVersion
+
+__version__ = '1.1.0'
+
+__all__ = ('DownloadClient', 'DownloadablePlexVersion')

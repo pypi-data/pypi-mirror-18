fd97aa23ef506749061b9492f44c3365c692e3d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:20:04 +0000

    Add package django-safespace/0.2.0/django-safespace-0.2.0.tar.gz

---
+++ packages/django-safespace/django-safespace-0.2.0.tar.gz/django-safespace-0.2.0/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+dev_dependencies = [
+    'flake8',
+    'isort',
+    'pydocstyle',
+    'pytest-cov',
+    'pytest-django>=3.0.0',
+]
+
+if __name__ == '__main__':
+    setuptools.setup(
+        name='django-safespace',
+        description='Exception catching and handling middleware for Django',
+        version='0.2.0',
+        url='https://github.com/valohai/django-safespace',
+        author='Valohai',
+        maintainer='Aarni Koskela',
+        maintainer_email='akx@iki.fi',
+        license='MIT',
+        install_requires=['Django'],
+        tests_require=dev_dependencies,
+        extras_require={'dev': dev_dependencies},
+        packages=setuptools.find_packages('.', exclude=('safespace_tests',)),
+        include_package_data=True,
+    )

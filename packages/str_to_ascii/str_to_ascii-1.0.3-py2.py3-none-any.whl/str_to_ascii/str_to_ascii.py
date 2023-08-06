9d07cbf2d7f4300e0a61330430a76c8a81f38468
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:31:21 +0000

    Add package str_to_ascii/1.0.3/str_to_ascii-1.0.3-py2.py3-none-any.whl

---
+++ packages/str_to_ascii/str_to_ascii-1.0.3-py2.py3-none-any.whl/str_to_ascii/str_to_ascii.py
@@ -0,0 +1,15 @@
+import string
+
+
+def asciify(line: str) -> str:
+    """
+    Return printable ascii characters + TAB.
+    """
+
+    good_chars = set(string.digits +
+                     string.ascii_letters +
+                     string.punctuation +
+                     '\t' +
+                     ' ')
+
+    return ''.join(c for c in line if c in good_chars)

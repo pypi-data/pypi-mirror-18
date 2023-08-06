98e2e55fb0093a5061bdeaa9ad20635cb0084579
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:10:03 +0000

    Add package dependency-injector/3.1.4/dependency-injector-3.1.4.tar.gz

---
+++ packages/dependency-injector/dependency-injector-3.1.4.tar.gz/dependency-injector-3.1.4/CONTRIBUTORS.rst
@@ -0,0 +1,5 @@
+Dependency Injector Contributors
+================================
+
++ Roman Mogilatov
++ Konstantin vz'One Enchant
---
+++ packages/dependency-injector/dependency-injector-3.1.4.tar.gz/dependency-injector-3.1.4/src/dependency_injector/__init__.py
@@ -0,0 +1,7 @@
+"""Dependency injector top-level package."""
+
+VERSION = '3.1.4'
+"""Version number that follows semantic versioning.
+
+:type: str
+"""

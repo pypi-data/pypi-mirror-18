e14ea2ccb30133c2757731b43788ca242d8dfc3e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:45:24 +0000

    Add package flotils/0.3.2b0/flotils-0.3.2b0-py2.7.egg

---
+++ packages/flotils/flotils-0.3.2b0-py2.7.egg/flotils/__init__.py
@@ -0,0 +1,21 @@
+# -*- coding: UTF-8 -*-
+"""
+An utility package
+"""
+
+__author__ = "the01"
+__email__ = "jungflor@gmail.com"
+__copyright__ = "Copyright (C) 2013-16, Florian JUNG"
+__license__ = "MIT"
+__version__ = "0.3.2b0"
+__date__ = "2016-11-25"
+
+import logging
+
+from .loadable import Loadable, loadJSON, saveJSON, loadJSONFile, saveJSONFile
+from .logable import Logable, ModuleLogable
+from .runable import StartStopable, Startable, Stopable, StartException
+
+
+logger = logging.getLogger(__name__)
+__all__ = ["logable", "loadable", "runable"]

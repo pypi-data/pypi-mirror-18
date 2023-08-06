a77618bbe5bd312f95678a90708b44bc530f10f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:20:18 +0000

    Add package xiaodnester/1.0.0/xiaodnester-1.0.0.zip

---
+++ packages/xiaodnester/xiaodnester-1.0.0.zip/xiaodnester-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+            name                  = 'xiaodnester',
+            version                 = '1.0.0',
+            py_modules        = ['xiaodnester'],
+            author                = 'xiaoD',
+            author_email      = '313381017@qq.com',
+            url                      = 'http://www.headfirstlabs.com',
+            description         = 'A simple printer of nested lists',
+            )

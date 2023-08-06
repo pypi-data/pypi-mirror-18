16c4ad04e63b99344ceccaa96757ce60db4641eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:18:03 +0000

    Add package datacore-python-client/0.2.16/datacore_python_client-0.2.16-py3-none-any.whl

---
+++ packages/datacore-python-client/datacore_python_client-0.2.16-py3-none-any.whl/datacore/__init__.py
@@ -0,0 +1,15 @@
+__all__ = [
+    "datacore_restlike",
+    "types"
+    ]
+
+
+version = "0.2.6"
+
+
+from datacore.datacore_restlike import Access as Access
+from datacore.datacore_restlike import DatacoreException as DatacoreException
+
+
+Access = Access
+DatacoreException = DatacoreException

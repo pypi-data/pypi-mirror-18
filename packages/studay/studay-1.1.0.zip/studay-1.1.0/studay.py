37df25e330a7441e2d332e66c11adfc2d2936a58
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:30:49 +0000

    Add package studay/1.1.0/studay-1.1.0.zip

---
+++ packages/studay/studay-1.1.0.zip/studay-1.1.0/studay.py
@@ -0,0 +1,10 @@
+﻿''' 这里是python模块的描述注解'''
+def print_lol(the_list,level):
+	''' 这里是函数的注释'''
+	for each_item in the_list:
+		if isinstance(each_item,list):
+			print_lol(each_item,level+1);
+		else :
+			for each_i in range(level):
+				print("\t",end='');
+			print(each_item);

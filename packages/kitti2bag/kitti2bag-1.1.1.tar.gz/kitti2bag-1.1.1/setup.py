4b76059a4705a28bbdb200cac6c76b97b1f1b6e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:30:19 +0000

    Add package kitti2bag/1.1.1/kitti2bag-1.1.1.tar.gz

---
+++ packages/kitti2bag/kitti2bag-1.1.1.tar.gz/kitti2bag-1.1.1/setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='kitti2bag',
+      version='1.1.1',
+      description='Convert KITTI dataset to ROS bag file the easy way!',
+      author='Tomas Krejci',
+      author_email='tomas789@gmail.com',
+      url='https://github.com/tomas789/kitti2bag/',
+      download_url = 'https://github.com/tomas789/kitti2bag/archive/1.1.1.zip',
+      keywords = ['dataset', 'ros', 'rosbag', 'kitti'],
+      scripts=['bin/kitti2bag']
+      )

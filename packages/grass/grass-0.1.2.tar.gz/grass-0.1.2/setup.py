a4cd63711c47cfc2d50d5f2638b3dadf3ecaf375
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:45:15 +0000

    Add package grass/0.1.2/grass-0.1.2.tar.gz

---
+++ packages/grass/grass-0.1.2.tar.gz/grass-0.1.2/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+setup(name='grass',
+      version='0.1.2',
+      scripts=['bin/grass'],
+      description='Annotation of de novo assemblies using semi-supervised learning on graphs',
+      url='https://github.com/COMBINE-lab/GRASS',
+      author='Laraib Malik, Shravya Thatipally, Nikhil Junneti, Rob Patro',
+      author_email='rob.patro@cs.stonybrook.edu',
+      license='BSD with attribution',
+      packages=['grass'],
+      install_requires=[
+          'PyYAML',
+          'coloredlogs',
+          'click',
+          'networkx',
+          'numpy',
+          'pandas',
+          'tqdm',
+          'rapclust',
+          'statistics'
+      ],
+      zip_safe=False)

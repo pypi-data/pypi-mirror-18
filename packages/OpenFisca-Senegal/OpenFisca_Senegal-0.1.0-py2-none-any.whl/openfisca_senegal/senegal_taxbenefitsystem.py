8ac7ab2345f0f76d71bc689ca5530741d8500172
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:16:02 +0000

    Add package OpenFisca-Senegal/0.1.0/OpenFisca_Senegal-0.1.0-py2-none-any.whl

---
+++ packages/OpenFisca-Senegal/OpenFisca_Senegal-0.1.0-py2-none-any.whl/openfisca_senegal/senegal_taxbenefitsystem.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+
+import os
+import xml.etree.ElementTree
+
+from openfisca_core import conv, legislationsxml
+from openfisca_core.taxbenefitsystems import TaxBenefitSystem
+
+from . import entities, scenarios
+
+
+COUNTRY_DIR = os.path.dirname(os.path.abspath(__file__))
+MODEL_FILE = os.path.join(COUNTRY_DIR, 'model.py')
+PARAM_FILE = os.path.join(COUNTRY_DIR, 'parameters.xml')
+
+class SenegalTaxBenefitSystem(TaxBenefitSystem):
+    """Senegalese tax and benefit system"""
+    CURRENCY = u"FCFA"
+
+    def __init__(self):
+        super(SenegalTaxBenefitSystem, self).__init__(entities.entities)
+        self.Scenario = scenarios.Scenario
+        self.add_variables_from_file(MODEL_FILE)
+        self.add_legislation_params(PARAM_FILE)

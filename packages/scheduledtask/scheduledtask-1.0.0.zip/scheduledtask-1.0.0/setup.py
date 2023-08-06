75bf3e2ca0f126e0151bd9597c57809367650cf6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:00 +0000

    Add package scheduledtask/1.0.0/scheduledtask-1.0.0.zip

---
+++ packages/scheduledtask/scheduledtask-1.0.0.zip/scheduledtask-1.0.0/setup.py
@@ -0,0 +1,16 @@
+from distutils.core import setup
+
+setup(
+    name='scheduledtask',
+    version='1.0.0',
+    packages=['scheduledtask'],
+    url='https://github.com/leonidumanskiy/scheduledtask',
+    license='MIT',
+    author='Leonid Umanskiy',
+    author_email='me@leonidumanskiy.com',
+    description='scheduled tasks for Python 3',
+    keywords=['scheduled', 'tasks', 'cron'],
+    classifiers=[
+        'Programming Language :: Python :: 3.5',
+    ]
+)

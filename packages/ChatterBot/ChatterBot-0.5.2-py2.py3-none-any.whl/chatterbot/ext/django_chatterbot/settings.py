928b081252982467407104f164e7fa1660dcf480
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:49:50 +0000

    Add package ChatterBot/0.5.2/ChatterBot-0.5.2-py2.py3-none-any.whl

---
+++ packages/ChatterBot/ChatterBot-0.5.2-py2.py3-none-any.whl/chatterbot/ext/django_chatterbot/settings.py
@@ -0,0 +1,18 @@
+"""
+Default ChatterBot settings for Django.
+"""
+from django.conf import settings
+
+
+CHATTERBOT_SETTINGS = getattr(settings, 'CHATTERBOT', {})
+
+CHATTERBOT_DEFAULTS = {
+    'name': 'ChatterBot',
+    'storage_adapter': 'chatterbot.storage.DjangoStorageAdapter',
+    'input_adapter': 'chatterbot.input.VariableInputTypeAdapter',
+    'output_adapter': 'chatterbot.output.OutputFormatAdapter',
+    'output_format': 'json'
+}
+
+CHATTERBOT = CHATTERBOT_DEFAULTS.copy()
+CHATTERBOT.update(CHATTERBOT_SETTINGS)

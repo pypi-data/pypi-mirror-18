526d052a44d41cc20bce0549577f10c20978059e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:09:52 +0000

    Add package karatsuba/0.1/karatsuba-0.1.tar.gz

---
+++ packages/karatsuba/karatsuba-0.1.tar.gz/karatsuba-0.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+        name='karatsuba',
+        packages=['karatsuba'],
+        description='Fast convolution algorithms with Python types',
+        license='MIT',
+        author='Thomas Baruchel',
+        author_email = 'baruchel@gmx.com',
+        version='0.1',
+        url='https://github.com/baruchel/karatsuba'
+)

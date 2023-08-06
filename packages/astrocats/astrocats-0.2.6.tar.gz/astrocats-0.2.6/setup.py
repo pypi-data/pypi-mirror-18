9db3a04d46d8fb79a25e5e2611a339dd1ad87922
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:49:26 +0000

    Add package astrocats/0.2.6/astrocats-0.2.6.tar.gz

---
+++ packages/astrocats/astrocats-0.2.6.tar.gz/astrocats-0.2.6/setup.py
@@ -0,0 +1,48 @@
+"""WARNING : THIS SCRIPT IS NOT CURRENTLY OPERATIONAL.
+"""
+
+import os
+
+from setuptools import find_packages, setup
+
+from astrocats.catalog.utils import logger
+from astrocats.main import setup_user_config
+
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
+
+setup_user_config(logger.get_logger())
+
+dir_path = os.path.dirname(os.path.realpath(__file__))
+exec(open(os.path.join(dir_path, 'astrocats', '__init__.py')).read())
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
+setup(
+    name="astrocats",
+    packages=find_packages(exclude=('*supernovae*', '*tidaldisruptions*',
+                                    '*novae*')),
+    include_package_data=True,
+    version=__version__,  # noqa
+    description=("Package for downloading, analyzing, and constructing open "
+                 "astronomy catalogs."),
+    license=__license__,  # noqa
+    author=__author__,  # noqa
+    author_email="guillochon@gmail.com",
+    install_requires=required,
+    url="https://github.com/astrocatalogs/astrocats",
+    download_url=(
+        'https://github.com/astrocatalogs/astrocats/tarball/' +
+        __version__  # noqa
+    ),
+    keywords="astronomy",
+    long_description=read('README.md'),
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.5"
+    ],
+    zip_safe=True)

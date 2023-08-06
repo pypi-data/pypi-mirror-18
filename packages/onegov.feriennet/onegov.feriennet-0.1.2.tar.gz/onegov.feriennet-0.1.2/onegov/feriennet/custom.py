6027c07e3d9c7b5e7126a3617ff57b9958f1c2ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:11:50 +0000

    Add package onegov.feriennet/0.1.2/onegov.feriennet-0.1.2.tar.gz

---
+++ packages/onegov.feriennet/onegov.feriennet-0.1.2.tar.gz/onegov.feriennet-0.1.2/onegov/feriennet/custom.py
@@ -0,0 +1,50 @@
+from itertools import chain
+from onegov.activity import BookingCollection, PeriodCollection
+from onegov.feriennet import _, FeriennetApp
+from onegov.feriennet.collections import VacationActivityCollection
+from onegov.feriennet.layout import DefaultLayout
+from onegov.org.elements import Link
+
+
+@FeriennetApp.template_variables()
+def get_template_variables(request):
+
+    front = []
+
+    # inject an activites link in front of all top navigation links
+    front.append(Link(
+        text=_("Activities"),
+        url=request.class_link(VacationActivityCollection)
+    ))
+
+    # for logged-in users show the number of open bookings
+    if request.is_logged_in:
+        period = PeriodCollection(request.app.session()).active()
+        bookings = BookingCollection(request.app.session())
+
+        if period:
+            count = bookings.booking_count(request.current_username)
+
+            if count:
+                attributes = {'data-count': str(count)}
+            else:
+                attributes = {}
+
+            front.append(Link(
+                text=period.confirmed and _("My Bookings") or _("My Wishlist"),
+                url=request.link(bookings),
+                classes=('count', period.confirmed and 'success' or 'alert'),
+                attributes=attributes
+            ))
+        else:
+            front.append(Link(
+                text=_("My Bookings"),
+                url=request.link(bookings)
+            ))
+
+    layout = DefaultLayout(request.app.org, request)
+    links = chain(front, layout.top_navigation)
+
+    return {
+        'top_navigation': links
+    }
---
+++ packages/onegov.feriennet/onegov.feriennet-0.1.2.tar.gz/onegov.feriennet-0.1.2/onegov/feriennet/security.py
@@ -0,0 +1,186 @@
+from morepath.authentication import NO_IDENTITY
+from onegov.activity import Activity, ActivityCollection, Booking, Occasion
+from onegov.core.security import Public, Private, Personal
+from onegov.core.security.rules import has_permission_logged_in
+from onegov.feriennet import FeriennetApp
+from onegov.org.models import ImageFileCollection, SiteCollection
+from sqlalchemy import or_
+
+
+#: Describes the states which are visible to the given role (not taking
+# ownership in account!)
+VISIBLE_ACTIVITY_STATES = {
+    'admin': (
+        'preview',
+        'proposed',
+        'accepted',
+        'denied',
+        'archived'
+    ),
+    'editor': (
+        'accepted',
+    ),
+    'member': (
+        'accepted',
+    ),
+    'anonymous': (
+        'accepted',
+    )
+}
+
+
+def is_owner(username, activity):
+    """ Returns true if the given username is the owner of the given
+    activity.
+
+    """
+    if not username:
+        return False
+
+    return username == activity.username
+
+
+class ActivityQueryPolicy(object):
+    """ Limits activity queries depending on the current user. """
+
+    def __init__(self, username, role):
+        self.username = username
+        self.role = role
+
+    @classmethod
+    def for_identity(cls, identity):
+        if identity is None or identity is NO_IDENTITY:
+            return cls(None, None)
+        else:
+            return cls(identity.userid, identity.role)
+
+    def granted_subset(self, query):
+        """ Limits the given activites query for the given user. """
+
+        if self.username is None or self.role not in ('admin', 'editor'):
+            return self.public_subset(query)
+        else:
+            return self.private_subset(query)
+
+    def public_subset(self, query):
+        """ Limits the given query to activites meant for the public. """
+        return query.filter(
+            Activity.state.in_(VISIBLE_ACTIVITY_STATES['anonymous'])
+        )
+
+    def private_subset(self, query):
+        """ Limits the given query to activites meant for admins/owners.
+
+        Admins see all the states and owners see the states of their own.
+        """
+
+        assert self.role and self.username
+
+        return query.filter(or_(
+            Activity.state.in_(VISIBLE_ACTIVITY_STATES[self.role]),
+            Activity.username == self.username
+        ))
+
+
+@FeriennetApp.permission_rule(model=object, permission=Private)
+def has_private_permission_logged_in(app, identity, model, permission):
+    """ Take away private permission for editors. For exceptions see
+    the permission rules below.
+
+    """
+
+    if identity.role != 'editor':
+        return has_permission_logged_in(app, identity, model, permission)
+
+    return False
+
+
+@FeriennetApp.permission_rule(model=SiteCollection, permission=Private)
+def has_private_permission_site_collection(app, identity, model, permission):
+    """ Give editors the ability to access the site collection. """
+
+    if identity.role != 'editor':
+        return has_permission_logged_in(app, identity, model, permission)
+
+    return True
+
+
+@FeriennetApp.permission_rule(model=ImageFileCollection, permission=Private)
+def has_private_permission_image_collection(app, identity, model, permission):
+    """ Give editors the ability to access the image file collection (but not
+    the file collection!).
+
+    """
+
+    if identity.role != 'editor':
+        return has_permission_logged_in(app, identity, model, permission)
+
+    return True
+
+
+@FeriennetApp.permission_rule(model=ActivityCollection, permission=Private)
+def has_private_permission_activity_collections(
+        app, identity, model, permission):
+    """ Give the editor private permission for activity collections (needed
+    to create new activites).
+
+    """
+
+    # only overries the editor role
+    if identity.role != 'editor':
+        return has_permission_logged_in(app, identity, model, permission)
+
+    return True
+
+
+@FeriennetApp.permission_rule(model=Activity, permission=Private)
+def has_private_permission_activities(app, identity, model, permission):
+    """ Give the editor private permission for activities. """
+
+    # only overries the editor role
+    if identity.role != 'editor':
+        return has_permission_logged_in(app, identity, model, permission)
+
+    return is_owner(identity.userid, model)
+
+
+@FeriennetApp.permission_rule(model=Occasion, permission=Private)
+def has_private_permission_occasions(app, identity, model, permission):
+    """ Give the editor private permission for occasions. """
+
+    # only overries the editor role
+    if identity.role != 'editor':
+        return has_permission_logged_in(app, identity, model, permission)
+
+    return is_owner(identity.userid, model.activity)
+
+
+@FeriennetApp.permission_rule(model=Activity, permission=Public, identity=None)
+def has_public_permission_not_logged_in(app, identity, model, permission):
+    """ Only make activites anonymously accessible with certain states. """
+
+    return model.state in VISIBLE_ACTIVITY_STATES['anonymous']
+
+
+@FeriennetApp.permission_rule(model=Booking, permission=Personal)
+def has_personal_permission_booking(app, identity, model, permission):
+    """ Ensure that logged in users may only change their own bookings. """
+
+    if identity.role == 'admin':
+        return True
+
+    return model.username == identity.userid
+
+
+@FeriennetApp.permission_rule(model=Activity, permission=Public)
+def has_public_permission_logged_in(app, identity, model, permission):
+    """ Only make activites accessible with certain states (or if owner). """
+
+    # roles other than admin/editor are basically treated as anonymous,
+    # so fallback in this case
+    if identity.role not in ('admin', 'editor'):
+        return has_public_permission_not_logged_in(
+            app, None, model, permission)
+
+    return is_owner(identity.userid, model) \
+        or model.state in VISIBLE_ACTIVITY_STATES[identity.role]

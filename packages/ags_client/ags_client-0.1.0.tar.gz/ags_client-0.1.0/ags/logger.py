d0f2cabaa45f92482cf5ebd70f43f6d8abe8bc3d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:19:54 +0000

    Add package ags_client/0.1.0/ags_client-0.1.0.tar.gz

---
+++ packages/ags_client/ags_client-0.1.0.tar.gz/ags_client-0.1.0/ags/logger.py
@@ -0,0 +1,39 @@
+import logging.config
+import os
+
+
+config = {
+    'version': 1,
+    'formatters': {
+        'default': {
+            'format': '%(name)s [%(levelname)s] %(message)s'
+        }
+    },
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+            'formatter': 'default',
+            'level': 'DEBUG'
+        }
+    },
+    'loggers': {
+        'ags': {
+            'handlers': ['console'],
+            'level': 'DEBUG'
+        }
+    },
+}
+
+if 'AGS_CLIENT_LOG_PATH' in os.environ:
+    config['handlers']['file'] = {
+        'class': 'logging.FileHandler',
+        'formatter': 'default',
+        'level': 'DEBUG',
+        'filename': os.environ['AGS_CLIENT_LOG_PATH']
+    }
+    config['loggers']['ags']['handlers'].append('file')
+
+
+def get_logger(name):
+    logging.config.dictConfig(config)
+    return logging.getLogger(name)

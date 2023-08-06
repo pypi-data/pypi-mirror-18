a1a097167476baf0eb98f656c11b7c05a972105d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:29:44 +0000

    Add package booksnake/0.1.0/booksnake-0.1.0.tar.gz

---
+++ packages/booksnake/booksnake-0.1.0.tar.gz/booksnake-0.1.0/setup.py
@@ -0,0 +1,39 @@
+import os
+from distutils.core import setup
+
+"""
+git tag {VERSION}
+git push --tags
+python setup.py sdist upload -r pypi
+"""
+
+VERSION = "0.1.0"
+
+# https://pythonhosted.org/an_example_pypi_project/setuptools.html
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name = "booksnake",
+    version = VERSION,
+    author = "Jordan Matelsky",
+    author_email = "j6k4m8@gmail.com",
+    description = ("Download books from the internet and send to kindle"),
+    license = "BSD",
+    keywords = [
+        "kindle",
+        "ebook",
+        "download"
+    ],
+    url = "https://github.com/j6k4m8/booksnake/tarball/" + VERSION,
+    packages=['booksnake'],
+    long_description = read('README.md'),
+    scripts = [
+        'scripts/booksnake'
+    ],
+    classifiers=[],
+    install_requires=[
+        'getpass',
+        'BeautifulSoup'
+    ],
+)

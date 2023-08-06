41fa2b9b3be0d8e41eb3960f936b7c0621217d70
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:10:42 +0000

    Add package yate_lhwd/1.0.0/yate_lhwd-1.0.0.tar.gz

---
+++ packages/yate_lhwd/yate_lhwd-1.0.0.tar.gz/yate_lhwd-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name= 'yate_lhwd',
+    version= '1.0.0',
+    py_modules = ['yate'],
+    author= 'lihang',
+    author_email= '418045274@qq.com',
+    url = 'http://www.cutelhwd.cn',
+    description= 'yate by lhwd',
+)

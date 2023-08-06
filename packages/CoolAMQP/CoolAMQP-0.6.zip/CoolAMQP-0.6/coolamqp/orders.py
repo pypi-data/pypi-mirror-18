6ae41968db20c1c1c616d85375a172c11cd3c3ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:17:02 +0000

    Add package CoolAMQP/0.6/CoolAMQP-0.6.zip

---
+++ packages/CoolAMQP/CoolAMQP-0.6.zip/CoolAMQP-0.6/coolamqp/orders.py
@@ -0,0 +1,122 @@
+#coding=UTF-8
+"""
+Orders that can be dispatched to ClusterHandlerThread
+"""
+from threading import Lock
+
+
+class Order(object):
+    """Base class for orders dispatched to ClusterHandlerThread"""
+    def __init__(self, on_completed=None, on_failed=None):
+        self.on_completed = on_completed
+        self.on_failed = on_failed
+        self._result = None  # None on non-completed
+                            # True on completed OK
+                            # exception instance on failed
+        self.lock = Lock()
+        self.lock.acquire()
+        self.cancelled = False
+
+    def has_finished(self):
+        """Return if this task has either completed or failed"""
+        return self._result is not None
+
+    def cancel(self):
+        """Cancel this order"""
+        self.cancelled = True
+
+    def completed(self):
+        self._result = True
+        self.lock.release()
+
+        if self.on_completed is not None:
+            self.on_completed()
+
+    def failed(self, e):
+        """
+        :param e: AMQPError instance or Cancelled instance
+        """
+        self._result = e
+        self.lock.release()
+
+        if self.on_failed is not None:
+            self.on_failed(e)
+
+    def result(self):
+        """Wait until this is completed and return a response"""
+        self.lock.acquire()
+        return self._result
+
+
+class SendMessage(Order):
+    """Send a message"""
+    def __init__(self, message, exchange, routing_key, on_completed=None, on_failed=None):
+        Order.__init__(self, on_completed=on_completed, on_failed=on_failed)
+        self.message = message
+        self.exchange = exchange
+        self.routing_key = routing_key
+
+
+class DeclareExchange(Order):
+    """Declare an exchange"""
+    def __init__(self, exchange, on_completed=None, on_failed=None):
+        Order.__init__(self, on_completed=on_completed, on_failed=on_failed)
+        self.exchange = exchange
+
+
+class DeleteExchange(Order):
+    """Delete an exchange"""
+    def __init__(self, exchange, on_completed=None, on_failed=None):
+        Order.__init__(self, on_completed=on_completed, on_failed=on_failed)
+        self.exchange = exchange
+
+
+class DeclareQueue(Order):
+    """Declare a a queue"""
+    def __init__(self, queue, on_completed=None, on_failed=None):
+        Order.__init__(self, on_completed=on_completed, on_failed=on_failed)
+        self.queue = queue
+
+
+class ConsumeQueue(Order):
+    """Declare and consume from a queue"""
+    def __init__(self, queue, on_completed=None, on_failed=None):
+        Order.__init__(self, on_completed=on_completed, on_failed=on_failed)
+        self.queue = queue
+
+
+class DeleteQueue(Order):
+    """Delete a queue"""
+    def __init__(self, queue, on_completed=None, on_failed=None):
+        Order.__init__(self, on_completed=on_completed, on_failed=on_failed)
+        self.queue = queue
+
+
+class SetQoS(Order):
+    """Set QoS"""
+    def __init__(self, prefetch_window, prefetch_count, on_completed=None, on_failed=None):
+        Order.__init__(self, on_completed=on_completed, on_failed=on_failed)
+        self.qos = (prefetch_window, prefetch_count)
+
+
+class CancelQueue(Order):
+    """Cancel consuming from a queue"""
+    def __init__(self, queue, on_completed=None, on_failed=None):
+        Order.__init__(self, on_completed=on_completed, on_failed=on_failed)
+        self.queue = queue
+
+
+class AcknowledgeMessage(Order):
+    """ACK a message"""
+    def __init__(self, connect_id, delivery_tag, on_completed):
+        Order.__init__(self, on_completed=on_completed)
+        self.connect_id = connect_id
+        self.delivery_tag = delivery_tag
+
+
+class NAcknowledgeMessage(Order):
+    """NACK a message"""
+    def __init__(self, connect_id, delivery_tag, on_completed):
+        Order.__init__(self, on_completed=on_completed)
+        self.connect_id = connect_id
+        self.delivery_tag = delivery_tag
---
+++ packages/CoolAMQP/CoolAMQP-0.6.zip/CoolAMQP-0.6/setup.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+#coding=UTF-8
+from distutils.core import setup
+
+setup(name='CoolAMQP',
+      version='0.6',
+      description='The AMQP client library',
+      author=u'DMS Serwis s.c.',
+      author_email='piotrm@smok.co',
+      url='https://github.com/smok-serwis/coolamqp',
+      download_url='https://github.com/smok-serwis/coolamqp/archive/master.zip',
+      keywords=['amqp', 'pyamqp', 'rabbitmq', 'client', 'network', 'ha', 'high availability'],
+      packages=['coolamqp', 'coolamqp.backends'],
+      license='MIT License',
+      long_description='''The Python AMQP client library that makes you forget about all the nasty corner cases about AMQP reconnection''',
+      requires=[
+            "amqp",
+            "six"
+      ]
+     )

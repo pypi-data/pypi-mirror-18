8c33ecf4360ca2802e84ddfc235200ea91972c0e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:20:07 +0000

    Add package myFirstPythonModule/1.2.1/myFirstPythonModule-1.2.1.tar.gz

---
+++ packages/myFirstPythonModule/myFirstPythonModule-1.2.1.tar.gz/myFirstPythonModule-1.2.1/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+	name		= 'myFirstPythonModule',
+	version		= '1.2.1',
+	py_modules	= ['myFirstPythonModule'],
+	author		= 'hfpython',
+	author_email	= 'hfpython@headfirstlabs.com',
+	url		= 'http://www.headfirstlabs.com',
+	description	= 'A simple printer of nested lists',
+     )

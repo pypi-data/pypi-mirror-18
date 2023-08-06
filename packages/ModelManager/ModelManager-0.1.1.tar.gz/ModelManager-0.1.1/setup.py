c82f7ecd072e360431a35edb23a53093676973eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:10:13 +0000

    Add package ModelManager/0.1.1/ModelManager-0.1.1.tar.gz

---
+++ packages/ModelManager/ModelManager-0.1.1.tar.gz/ModelManager-0.1.1/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name='ModelManager',
+    packages=['model_manager'],
+    version='0.1.1',
+    description='Crud based model manager that sits on top of orm models to allow for a data access layer and a separate service layer. (all with minimal effort)',
+    author='Brian S Morgan',
+    author_email='brian.s.morgan@gmail.com',
+    url='https://github.com/bmorgan21/model-manager',
+    install_requires=[
+    ]
+)

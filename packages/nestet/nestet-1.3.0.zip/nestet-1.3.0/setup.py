5730433e9f455fb297b7cca874b211f2c016c10a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:45:27 +0000

    Add package nestet/1.3.0/nestet-1.3.0.zip

---
+++ packages/nestet/nestet-1.3.0.zip/nestet-1.3.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+        name = 'nestet',
+        version = '1.3.0',
+        py_modules = ['nester'],
+        author = 'blackstar',
+        author_email = 'zhongliyuxi@hotmail.com',
+        url = 'http://www.hotmail.com',
+        description = 'A simple printer of nested lists',
+    )

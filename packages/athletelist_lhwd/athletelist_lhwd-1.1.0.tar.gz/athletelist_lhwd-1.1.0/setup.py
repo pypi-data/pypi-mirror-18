d72ee0bd49de7ae69db1cdda4b8db77177cb4a2b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:10:42 +0000

    Add package athletelist_lhwd/1.1.0/athletelist_lhwd-1.1.0.tar.gz

---
+++ packages/athletelist_lhwd/athletelist_lhwd-1.1.0.tar.gz/athletelist_lhwd-1.1.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name= 'athletelist_lhwd',
+    version= '1.1.0',
+    py_modules = ['athletelist'],
+    author= 'lihang',
+    author_email= '418045274@qq.com',
+    url = 'http://www.cutelhwd.cn',
+    description= 'athlete list by lhwd',
+)

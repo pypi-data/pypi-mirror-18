58bea11c76544fdd6ceaaf8506ae0317ebd1b9a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:08:47 +0000

    Add package songye/1.0.0/songye-1.0.0.tar.gz

---
+++ packages/songye/songye-1.0.0.tar.gz/songye-1.0.0/setup.py
@@ -0,0 +1,12 @@
+
+from distutils.core import setup
+
+setup(
+        name = 'songye',
+        version = '1.0.0',
+        py_modules = ['nest_list'],
+        author = 'hfpython',
+        author_email = '260837024@qq.com',
+        url = 'http://www.headfirstlabs.com',
+        description = 'A simple printer of nested lists',
+    )

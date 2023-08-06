2fabea0b5a01b3201b730fdd7b8f13af05f4cc73
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:10:33 +0000

    Add package rptk/0.1.0.dev2/rptk-0.1.0.dev2.tar.gz

---
+++ packages/rptk/rptk-0.1.0.dev2.tar.gz/rptk-0.1.0.dev2/rptk/command_line.py
@@ -0,0 +1,15 @@
+import sys
+from rptk import configuration, dispatch
+
+
+def main():
+    argv = sys.argv[1:]
+    config = configuration.Config(argv=argv)
+    dispatcher = dispatch.Dispatcher(config=config)
+    result = dispatcher.dispatch()
+    sys.stdout.write("%s\n" % result)
+    return
+
+
+if __name__ == "__main__":
+    main()

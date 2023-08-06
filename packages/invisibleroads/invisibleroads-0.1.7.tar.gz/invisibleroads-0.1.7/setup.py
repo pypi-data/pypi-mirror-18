3afe95da7a67350cb5c8df7f8dbb059f0f7a699f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:21:49 +0000

    Add package invisibleroads/0.1.7/invisibleroads-0.1.7.tar.gz

---
+++ packages/invisibleroads/invisibleroads-0.1.7.tar.gz/invisibleroads-0.1.7/setup.py
@@ -0,0 +1,36 @@
+from os.path import abspath, dirname, join
+from setuptools import find_packages, setup
+
+
+ENTRY_POINTS = """
+[console_scripts]
+invisibleroads = invisibleroads.scripts:launch
+"""
+FOLDER = dirname(abspath(__file__))
+DESCRIPTION = '\n\n'.join(open(join(FOLDER, x)).read().strip() for x in [
+    'README.rst', 'CHANGES.rst'])
+setup(
+    name='invisibleroads',
+    version='0.1.7',
+    description='Simple framework for extensible command line scripts',
+    long_description=DESCRIPTION,
+    classifiers=[
+        'Programming Language :: Python',
+        'Framework :: Pyramid',
+        'Topic :: Internet :: WWW/HTTP :: WSGI :: Application',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    author='Roy Hyunjin Han',
+    author_email='rhh@crosscompute.com',
+    url='http://invisibleroads.com',
+    keywords='web pyramid pylons invisibleroads',
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'invisibleroads-macros>=0.7.4',
+        'six',
+        'stevedore',
+    ],
+    entry_points=ENTRY_POINTS)

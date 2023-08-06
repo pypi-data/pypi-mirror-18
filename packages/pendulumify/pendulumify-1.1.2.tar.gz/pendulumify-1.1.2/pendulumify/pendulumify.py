fb7bf8e0e7395e6d3792a69c9be04e57ea9073b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:12:11 +0000

    Add package pendulumify/1.1.2/pendulumify-1.1.2.tar.gz

---
+++ packages/pendulumify/pendulumify-1.1.2.tar.gz/pendulumify-1.1.2/pendulumify/pendulumify.py
@@ -0,0 +1,70 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import (
+    absolute_import,
+    division,
+    unicode_literals,
+    print_function,
+)
+
+from functools import wraps
+
+import inspect
+import numbers
+import pendulum
+
+try:
+    unicode = unicode
+except NameError:
+    basestring = (str, bytes)
+
+PASSTHROUGH_TYPES = (basestring, numbers.Number)
+
+
+class WrappedGenerator(object):
+    def __init__(self, wrapped):
+        self.wrapped = wrapped
+
+    def __iter__(self):
+        return self
+
+    def __next__(self, *args, **kwargs):
+        return self.next(*args, **kwargs)
+
+    def next(self):
+        return pendulumify(next(self.wrapped))
+
+
+def pendulumify(obj):
+    '''
+
+    Recursively turns all datetime objects in a container type iterable (think
+    dictionaries, lists, sets, etc.) into a Pendulum object, including those
+    returned by generator objects
+
+    '''
+
+    if isinstance(obj, PASSTHROUGH_TYPES):
+        return obj
+
+    if callable(obj):
+        @wraps(obj)
+        def wrapped(*args, **kwargs):
+            return pendulumify(obj(*args, **kwargs))
+
+        return wrapped
+
+    if inspect.isgenerator(obj):
+        return WrappedGenerator(obj)
+
+    if isinstance(obj, dict):
+        return {k: pendulumify(v) for k, v in obj.items()}
+
+    try:
+        return pendulum.instance(obj)
+    except AttributeError:
+        pass
+
+    constructor = type(obj)
+
+    return constructor(map(pendulumify, obj))

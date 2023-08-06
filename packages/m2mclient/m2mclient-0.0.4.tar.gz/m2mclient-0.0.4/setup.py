7df8ed8d4542639926881dfd00b23becf56ded57
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:43:46 +0000

    Add package m2mclient/0.0.4/m2mclient-0.0.4.tar.gz

---
+++ packages/m2mclient/m2mclient-0.0.4.tar.gz/m2mclient-0.0.4/setup.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+classifiers = [
+    'Development Status :: 3 - Alpha',
+    'Intended Audience :: Developers',
+    'Programming Language :: Python',
+]
+
+# http://stackoverflow.com/questions/2058802/how-can-i-get-the-version-defined-in-setup-py-setuptools-in-my-package
+with open('m2mclient/_version.py') as f:
+    exec(f.read())
+
+long_desc = "Official Dataplicity M2M Client"
+setup(
+    name='m2mclient',
+    version=VERSION,
+    description="A client for the Dataplicity M2M server",
+    long_description=long_desc,
+    author='WildFoundry',
+    author_email='support@dataplicity.com',
+    url='https://www.dataplicity.com',
+    platforms=['any'],
+    packages=find_packages(),
+    include_package_data=True,
+    exclude_package_data={'': ['_*', 'docs/*']},
+    classifiers=classifiers,
+    install_requires=[
+        'enum34',
+        'six',
+        'websocket-client==0.37.0'  # opcode nameerror bug in 0.39.0
+    ]
+)

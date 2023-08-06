f07bee956e374f0cbc0cc00072bd5ca5cb26cbf2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:10:45 +0000

    Add package assignmentsptyma/0.2/assignmentsptyma-0.2.tar.gz

---
+++ packages/assignmentsptyma/assignmentsptyma-0.2.tar.gz/assignmentsptyma-0.2/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='assignmentsptyma',
+      version='0.2',
+      description='The funniest joke in the world',
+      packages=['assignmentsptyma'],
+      zip_safe=False)

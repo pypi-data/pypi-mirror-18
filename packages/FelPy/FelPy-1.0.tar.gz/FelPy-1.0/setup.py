cbed8372f363349cfca6a2b2a4b9bfa67e26d4f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:08:43 +0000

    Add package FelPy/1.0/FelPy-1.0.tar.gz

---
+++ packages/FelPy/FelPy-1.0.tar.gz/FelPy-1.0/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'FelPy',
+  py_modules = ['ArrayFormat'],
+  version = '1.0',
+  description = 'An assortment of tools from Felix - currently just an array manipulation module',
+  author = 'Felix Johnson',
+  author_email = 'him@felixhasa.website',
+  url = 'https://github.com/12johnsonf/FelPy', # use the URL to the github repo
+  download_url = 'https://github.com/12johnsonf/FelPy/tarball/1.0', # I'll explain this in a second
+  keywords = ['tools', 'custom', 'first'], # arbitrary keywords
+  classifiers = [],
+)

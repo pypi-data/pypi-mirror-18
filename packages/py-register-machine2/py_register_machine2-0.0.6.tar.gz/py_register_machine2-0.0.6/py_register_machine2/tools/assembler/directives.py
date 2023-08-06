0af4e24ab84af05bca1fc4aca9336866e4aadf83
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:21:42 +0000

    Add package py-register-machine2/0.0.6/py_register_machine2-0.0.6.tar.gz

---
+++ packages/py-register-machine2/py_register_machine2-0.0.6.tar.gz/py_register_machine2-0.0.6/py_register_machine2/tools/assembler/directives.py
@@ -0,0 +1,76 @@
+#!/usr/bin/python3
+
+"""
+Directives for the assembler.
+"""
+
+class BaseDirective(object):
+	"""
+	Every Directive has to provide the following Attributes/Methods:
+
+	* ``name`` (like ``.set``)
+	* ``get_words(line)``: return the data to store
+	* ``isstatic()`` returns True, if the reference should be static
+	"""
+
+	def __init__(self, name):
+		self.name = name
+	def get_words(self, line):
+		return [int(line[0])]
+	def isstatic(self):
+		return False
+
+
+class ConvertingDirective(BaseDirective):
+	"""
+	The function ``function`` will have to take the rest of the line (as a list)
+	and convert it to an iterable of ``int`` objects
+
+	**Example**: The ``.string`` directive::
+
+		# usage: .string name string
+		# ie.: .string foo this is a test
+
+		def string_function(line):
+			line = " ".join(line)
+			res = []
+			for char in line:
+				res.append(ord(char))
+			return res
+	"""
+	def __init__(self, name, function):
+		BaseDirective.__init__(self, name)
+		self.function = function
+
+	def get_words(self, line):
+		return self.function(line)
+
+class Zeros(BaseDirective):
+	"""
+	Usage: 
+	::
+
+		.zeros n	
+
+	Fills the next n words with zeros.
+	"""
+	def __init__(self, name = ".zeros"):
+		self.name = name
+	def get_words(self, line):
+		return [0] * int(line[0])
+
+class Padding(BaseDirective):
+	"""
+	Usage:
+	::
+
+		.padd n v
+
+	Fills the next n words with v.
+	"""
+	def __init__(self, name = ".padd"):
+		self.name = name
+	def get_words(self, line):
+		number, value = line
+		return [int(value)] * int(number)
+	

73e4855e529c6583bf097234193b9518973a71c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:10:24 +0000

    Add package plandf/0.1.129/plandf-0.1.129.tar.gz

---
+++ packages/plandf/plandf-0.1.129.tar.gz/plandf-0.1.129/plandf/settings.py
@@ -0,0 +1,14 @@
+# These are only needed, if you are using custom conversion rates.
+# If you are valuating everything in terms of work hour value,
+# then you don't need to enter this information.
+
+# Currency value ( get one at openexchangerates.org )
+OPENEXCHANGE_APP_ID = ''
+OPENEXCHANGE_BASE_CURRENCY = 'USD'
+OPENEXCHANGE_URL = 'https://openexchangerates.org/api/latest.json'
+
+# [Optional] Hour value ( get one at api.stlouisfed.org )
+FRED_KEY = ''
+FRED_SERIES = 'CES0500000003'
+DEFAULT_HOUR_VALUE_IN_USD = 25.0
+

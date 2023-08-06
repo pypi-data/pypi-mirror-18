9d97f19137420507296d6e7efeb077d06dd68fbb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:21:32 +0000

    Add package pyspectral/0.3.0/pyspectral-0.3.0.tar.gz

---
+++ packages/pyspectral/pyspectral-0.3.0.tar.gz/pyspectral-0.3.0/pyspectral/__init__.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Copyright (c) 2013, 2015, 2016 Adam.Dybbroe
+
+# Author(s):
+
+#   Adam.Dybbroe <adam.dybbroe@smhi.se>
+#   Panu Lahtinen <panu.lahtinen@fmi.fi>
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+"""Main module"""
+
+from pyspectral.version import __version__
+import logging
+import os
+
+LOG = logging.getLogger(__name__)
+
+BASE_PATH = os.path.sep.join(os.path.dirname(
+    os.path.realpath(__file__)).split(os.path.sep)[:-1])
+# FIXME: Use package_resources?
+PACKAGE_CONFIG_PATH = os.path.join(BASE_PATH, 'etc')
+BUILTIN_CONFIG_FILE = os.path.join(PACKAGE_CONFIG_PATH, 'pyspectral.cfg')
+
+CONFIG_FILE = os.environ.get('PSP_CONFIG_FILE')
+
+if CONFIG_FILE is not None and (not os.path.exists(CONFIG_FILE) or
+                                not os.path.isfile(CONFIG_FILE)):
+    raise IOError(str(CONFIG_FILE) + " pointed to by the environment " +
+                  "variable PSP_CONFIG_FILE is not a file or does not exist!")
+
+
+def get_config():
+    """Get config from file"""
+
+    import ConfigParser
+
+    conf = ConfigParser.ConfigParser()
+    conf.read(BUILTIN_CONFIG_FILE)
+    if CONFIG_FILE is not None:
+        try:
+            conf.read(CONFIG_FILE)
+        except ConfigParser.NoSectionError:
+            LOG.info('Failed reading configuration file: %s',
+                     str(CONFIG_FILE))
+
+    return conf

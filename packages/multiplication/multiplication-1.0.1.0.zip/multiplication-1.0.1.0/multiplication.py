f45bbeb0b9bc18940af525b4e6be9b843c7d6e0a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:46:29 +0000

    Add package multiplication/1.0.1.0/multiplication-1.0.1.0.zip

---
+++ packages/multiplication/multiplication-1.0.1.0.zip/multiplication-1.0.1.0/multiplication.py
@@ -0,0 +1,8 @@
+def multiplication():
+    for i in range(1,10):
+        for j in range(1,10):
+            work_ot= i*j
+            print('%d*%d=%d\t'%(i,j,work_ot),end='')
+        print()
+
+#work_ot()
---
+++ packages/multiplication/multiplication-1.0.1.0.zip/multiplication-1.0.1.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+
+    name         = 'multiplication',
+    version      = '1.0.1.0',
+    py_modules    = ['multiplication'],
+    author       = 'Qiang',
+    author_email = '12919379@qq.com',
+    url          = 'http://www.qiang.com',
+    description  = 'A simple multiplication table',)

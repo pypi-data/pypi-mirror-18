f30df97e538cbf54c4208a0dcc8c051362706b54
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:10:16 +0000

    Add package gpcharts/v1.3.2/gpcharts-v1.3.2.tar.gz

---
+++ packages/gpcharts/gpcharts-v1.3.2.tar.gz/gpcharts-v1.3.2/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+   name = 'gpcharts',
+   py_modules = ['gpcharts'], # this must be the same as the name above
+   version = 'v1.3.2',
+   description = 'A Google Charts API for Python and Jupyter',
+   author = 'Sagnik Ghosh',
+   author_email = 'sghosh84@gmail.com',
+   url = 'https://github.com/Dfenestrator/GooPyCharts',
+   download_url = 'https://github.com/Dfenestrator/GooPyCharts/archive/v1.3.1.tar.gz', 
+   keywords = ['plots', 'matplotlib', 'Bokeh', 'charts', 'Google charts', 'graphs'], # arbitrary keywords
+   classifiers = [],
+)

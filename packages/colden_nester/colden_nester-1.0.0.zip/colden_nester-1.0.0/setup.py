701972afa29739a79b098587775621dedd7ce217
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:08:46 +0000

    Add package colden_nester/1.0.0/colden_nester-1.0.0.zip

---
+++ packages/colden_nester/colden_nester-1.0.0.zip/colden_nester-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name         = 'colden_nester',
+    version      ='1.0.0',
+    py_modules   =['nester'],
+    author       ='colden',
+    author_email ='654562715@qq.com',
+    url          ='http://www.ahu.edu.cn',
+    description  ='A Simple printer of nessted lists',
+    )

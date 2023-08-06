287c5ae45c597d0af2d6c4fb4495bd024bad87a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:33:09 +0000

    Add package signalfx-detector-syncer/0.1/signalfx_detector_syncer-0.1-py2.py3-none-any.whl

---
+++ packages/signalfx-detector-syncer/signalfx_detector_syncer-0.1-py2.py3-none-any.whl/signalfx_detector_syncer/__main__.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+
+# Copyright (C) 2016 SignalFx, Inc. All rights reserved.
+
+import argparse
+import logging
+import signalfx
+import sys
+
+from . import syncer
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-t', '--token', required=True,
+                        help='Authentication token')
+    parser.add_argument('--team', help='An optional team identifier')
+    parser.add_argument('-a', '--api-endpoint',
+                        help='SignalFx API endpoint')
+    parser.add_argument('-n', '--dry-run', action='store_true',
+                        help='Dry-run mode, do not update detectors')
+    parser.add_argument('-D', '--debug', action='store_const',
+                        dest='loglevel',
+                        const=logging.DEBUG, default=logging.WARNING,
+                        help='Enable debug logging')
+    parser.add_argument('-v', '--verbose', action='store_const',
+                        dest='loglevel',
+                        const=logging.INFO,
+                        help='Enable verbose logging')
+    parser.add_argument('directory', default='.', help='Source directory')
+
+    options = parser.parse_args()
+    logging.basicConfig(stream=sys.stderr, level=options.loglevel)
+
+    sfx = signalfx.SignalFx(api_endpoint=options.api_endpoint)
+    client = syncer.Syncer(sfx.rest(options.token),
+                           options.team,
+                           options.dry_run)
+    client.sync(options.directory)
+
+
+if __name__ == '__main__':
+    sys.exit(main())

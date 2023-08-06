d35af23801e09baae8fc808041fe567badd46760
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:43:56 +0000

    Add package testing-tuf-warehouse/0.1/testing-tuf-warehouse-0.1.tar.gz

---
+++ packages/testing-tuf-warehouse/testing-tuf-warehouse-0.1.tar.gz/testing-tuf-warehouse-0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='testing-tuf-warehouse',
+      version='0.1',
+      description='TUF and warehouse',
+      author='Ree',
+      author_email='ree@example.com',
+      license='MIT',
+      packages=['testing-tuf-warehouse'],
+      zip_safe=False)

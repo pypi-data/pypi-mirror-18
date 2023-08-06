ebf187f11ddba06bec32d7b92ab57b64d225c800
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:12:55 +0000

    Add package sugertest/1.1.0/sugertest-1.1.0.zip

---
+++ packages/sugertest/sugertest-1.1.0.zip/sugertest-1.1.0/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+        name         = 'sugertest',
+        version      = '1.1.0',
+        py_modules   = ['nester'],
+        author       = 'suger',
+        author_email = 'suger@gmail.com',
+        url          = 'http://www.suger.com',
+        description  = 'this is my test',
+
+        )

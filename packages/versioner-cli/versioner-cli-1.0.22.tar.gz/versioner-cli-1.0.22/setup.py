0b294a0db7dd3fa177c2b5b8c8202963bcd864e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:10:42 +0000

    Add package versioner-cli/1.0.22/versioner-cli-1.0.22.tar.gz

---
+++ packages/versioner-cli/versioner-cli-1.0.22.tar.gz/versioner-cli-1.0.22/setup.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+requires = [
+    'docker-py',
+    'enum34',
+    'future',
+    'six',
+    'PyYaml',
+    'requests'
+]
+
+setup(name='versioner-cli',
+      version='1.0.22',
+      description='cli tool to interact with versioner service',
+      author='Ben Waters',
+      author_email='ben@book-md.com',
+      package_dir={'': 'lib'},
+      packages=find_packages('lib'),
+      data_files=[('/etc/default', ['config/versioner'])],
+      license='MIT',
+      url="https://github.com/bookmd/bookmd-versioner-cli",
+      scripts=['bin/versioner-cli'],
+      install_requires=requires,
+      )

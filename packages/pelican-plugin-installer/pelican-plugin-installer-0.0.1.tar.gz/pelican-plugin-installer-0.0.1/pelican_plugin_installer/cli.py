dfe0eb16c7b0ece0ed7fa574076c6c4bc8554fcb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:09:58 +0000

    Add package pelican-plugin-installer/0.0.1/pelican-plugin-installer-0.0.1.tar.gz

---
+++ packages/pelican-plugin-installer/pelican-plugin-installer-0.0.1.tar.gz/pelican-plugin-installer-0.0.1/pelican_plugin_installer/cli.py
@@ -0,0 +1,35 @@
+import click
+
+from . import (
+    delete_plugin, discover_plugins_path, install_plugin, update_plugin)
+from .exceptions import BaseException
+
+operations = {
+    'install': (install_plugin, ('Plugin installed!')),
+    'delete': (delete_plugin, ('Plugin removed!')),
+    'update': (update_plugin, ('Plugin updated!')),
+}
+
+
+@click.command()
+@click.option('--config', '-c', 'config_file', default='pelicanconf.py', help='The path to Pelican configuration file.')
+@click.option('--delete', '-d', 'operation', flag_value='delete', help='Delete a specific plugin.')
+@click.option('--install', '-i', 'operation', flag_value='install', help='Install a specific plugin.')
+@click.option('--update', '-u', 'operation', flag_value='update', help='Update a specific plugin.')
+@click.argument('plugin_name', required=False)
+def main(plugin_name, operation, config_file):
+    """Installs Pelican Plugins in an easy way"""
+
+    plugins_path = discover_plugins_path(config_file)
+
+    fn = operations[operation][0]
+
+    try:
+        fn(plugin_name, plugins_path)
+    except BaseException as e:
+        click.echo(e.msg)
+    else:
+        success_msg = operations[operation][1]
+
+        click.echo(success_msg)
+        click.echo("Don't forget to update the PLUGINS variable.")
---
+++ packages/pelican-plugin-installer/pelican-plugin-installer-0.0.1.tar.gz/pelican-plugin-installer-0.0.1/setup.py
@@ -0,0 +1,50 @@
+"""
+Installs Pelican Plugins in an easy way
+"""
+from setuptools import find_packages, setup
+
+dependencies = ['click']
+test_dependencies = ['pytest', 'pytest-runner', 'pytest-mock']
+
+setup(
+    name='pelican-plugin-installer',
+    version='0.0.1',
+    url='https://github.com/kplaube/pelican-plugin-installer',
+    license='GPL3',
+    author='Klaus Laube',
+    author_email='kplaube@gmail.com',
+    description='Installs Pelican Plugins in an easy way',
+    long_description=__doc__,
+    packages=find_packages(exclude=['tests']),
+    include_package_data=True,
+    zip_safe=False,
+    platforms='any',
+    install_requires=dependencies,
+    tests_require=test_dependencies,
+    entry_points={
+        'console_scripts': [
+            'pelican-plugin-installer = pelican_plugin_installer.cli:main',
+        ],
+    },
+    classifiers=[
+        # As from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+        # 'Development Status :: 1 - Planning',
+        # 'Development Status :: 2 - Pre-Alpha',
+        'Development Status :: 3 - Alpha',
+        # 'Development Status :: 4 - Beta',
+        # 'Development Status :: 5 - Production/Stable',
+        # 'Development Status :: 6 - Mature',
+        # 'Development Status :: 7 - Inactive',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Operating System :: POSIX',
+        'Operating System :: MacOS',
+        'Operating System :: Unix',
+        # 'Operating System :: Microsoft :: Windows',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ]
+)

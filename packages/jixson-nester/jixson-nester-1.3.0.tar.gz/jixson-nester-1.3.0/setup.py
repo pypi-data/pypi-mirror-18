1d863d505fc9e445cc87c8ef4355cf6d05f63989
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:10:17 +0000

    Add package jixson-nester/1.3.0/jixson-nester-1.3.0.tar.gz

---
+++ packages/jixson-nester/jixson-nester-1.3.0.tar.gz/jixson-nester-1.3.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup	(
+	name		= 'jixson-nester',
+	version		= '1.3.0',
+	py_modules	= ['jixson-nester'],
+	author		= 'jixson',
+	author_email	= 'jixson@foxmail.com',
+	url		= 'http://www.wangww.com',
+	description	= 'a simple printer of nested lists',
+	)

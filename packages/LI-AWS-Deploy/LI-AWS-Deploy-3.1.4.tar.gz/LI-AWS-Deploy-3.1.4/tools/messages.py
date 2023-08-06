7978a133e75deb6d89559fac3dd00a0ee46b6c2e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:20:32 +0000

    Add package LI-AWS-Deploy/3.1.4/LI-AWS-Deploy-3.1.4.tar.gz

---
+++ packages/LI-AWS-Deploy/LI-AWS-Deploy-3.1.4.tar.gz/LI-AWS-Deploy-3.1.4/tools/messages.py
@@ -0,0 +1,106 @@
+# -*- coding: utf-8 -*-
+from __future__ import print_function, unicode_literals, with_statement, nested_scopes
+import datadog
+import platform
+import slackweb
+from tools import settings
+from tools.utils import run_command
+
+
+class Message():
+    def __init__(
+            self,
+            config,
+            branch,
+            commit,
+            repo,
+            action,
+            test=False,
+            *args,
+            **kwargs):
+        self.config = config
+        self.branch = branch
+        self.commit = commit
+        self.repo = repo
+        self.test = test
+        self.action = action
+        print("\033[1m\033[93m\n>> Enviando mensagens")
+        print("*********************\033[0m")
+
+    def send(self, alert_type="info"):
+        if settings.USE_DATADOG:
+            self.send_datadog(alert_type)
+        if settings.USE_SLACK:
+            self.send_slack()
+
+    def send_datadog(self, alert_type):
+
+        options = {
+            'api_key': self.config['datadog_api_key'],
+            'app_key': self.config['datadog_app_key']
+        }
+
+        title = "DEPLOY {}: {}/{}".format(
+            self.action, self.repo, self.branch.name)
+        text = self.commit
+        tags = ["deploy"]
+
+        if not self.test:
+            datadog.initialize(**options)
+            datadog.api.Event.create(
+                title=title,
+                text=text,
+                tags=tags,
+                alert_type=alert_type if not self.test else "info"
+            )
+            print("Datadog OK")
+        else:
+            return "{}\n{}".format(title, text)
+
+    def send_slack(self):
+
+        text = "DEPLOY {}: {}/{}\n{}".format(
+            self.action,
+            self.repo,
+            self.branch.name,
+            self.commit
+        )
+
+        if not self.test:
+            slack = slackweb.Slack(
+                url=self.config['slack_url']
+            )
+
+            slack.notify(
+                text=text,
+                channel="#{}".format(
+                    self.config['slack_channel']) if not self.test else settings.TEST_CHANNEL,
+                username=self.config['slack_user'],
+                icon_emoji=":{}:".format(
+                    self.config['slack_icon']))
+            print("Slack OK")
+        else:
+            return text
+
+
+def notify(title, msg):
+    if platform.system().lower() == 'linux':  # Linux
+        run_command(
+            command_list=[
+                {
+                    'command': 'notify-send -i gsd-xrandr {title} {msg}'.format(
+                        title='"{}"'.format(title),
+                        msg='"{}"'.format(msg)),
+                    'run_stdout': False}],
+            get_stdout=False,
+            title=None)
+    elif platform.system().lower() != "windows":  # Mac
+        run_command(
+            command_list=[
+                {
+                    'command': "osascript -e 'display notification {msg} with title {title}'".format(
+                        title='"{}"'.format(title),
+                        msg='"{}"'.format(msg)),
+                    'run_stdout': False}],
+            get_stdout=False,
+            title=None)

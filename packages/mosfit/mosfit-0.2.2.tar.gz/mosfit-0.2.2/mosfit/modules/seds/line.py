bf3770d3ad350ff433822a907b0eb730faa7c730
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:52:06 +0000

    Add package mosfit/0.2.2/mosfit-0.2.2.tar.gz

---
+++ packages/mosfit/mosfit-0.2.2.tar.gz/mosfit-0.2.2/mosfit/modules/seds/line.py
@@ -0,0 +1,33 @@
+from mosfit.modules.seds.sed import SED
+
+CLASS_NAME = 'Line'
+
+
+class Line(SED):
+    """Line spectral energy distribution, modifies existing SED.
+    """
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+    def process(self, **kwargs):
+        self._seds = kwargs['seds']
+        self._bands = kwargs['all_bands']
+        self._band_indices = kwargs['all_band_indices']
+        self._sample_wavelengths = kwargs['sample_wavelengths']
+        self._luminosities = kwargs['luminosities']
+        zp1 = 1.0 + kwargs['redshift']
+
+        seds = []
+        for li, lum in enumerate(self._luminosities):
+            cur_band = self._bands[li]
+            bi = self._band_indices[li]
+            rest_freqs = [x * zp1 for x in self._sample_frequencies[bi]]
+
+            # Dummy function for now, needs implementation
+            sed = [0.0 for x in rest_freqs]
+            seds.append(list(sed))
+
+        seds = self.add_to_existing_seds(seds, **kwargs)
+
+        return {'sample_wavelengths': self._sample_wavelengths, 'seds': seds}

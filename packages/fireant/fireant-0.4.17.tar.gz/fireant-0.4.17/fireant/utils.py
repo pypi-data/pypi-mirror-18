0bda7ed0fb84c80986cb0cfa110b3af140d5fe37
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:44:16 +0000

    Add package fireant/0.4.17/fireant-0.4.17.tar.gz

---
+++ packages/fireant/fireant-0.4.17.tar.gz/fireant-0.4.17/fireant/utils.py
@@ -0,0 +1,34 @@
+# coding: utf-8
+import itertools
+
+import pandas as pd
+
+
+def dimension_levels(dimension_key, dimension):
+    if 'display_field' in dimension:
+        return [dimension_key, dimension['display_field']]
+    return [dimension_key]
+
+
+def wrap_list(value):
+    return value if isinstance(value, (tuple, list)) else [value]
+
+
+def slice_first(item):
+    if isinstance(item, (tuple, list)):
+        return item[0]
+    return item
+
+
+def filter_duplicates(iterable):
+    filtered_list, seen = [], set()
+    for item in iterable:
+        key = slice_first(item)
+
+        if key in seen:
+            continue
+
+        seen.add(key)
+        filtered_list.append(item)
+
+    return filtered_list

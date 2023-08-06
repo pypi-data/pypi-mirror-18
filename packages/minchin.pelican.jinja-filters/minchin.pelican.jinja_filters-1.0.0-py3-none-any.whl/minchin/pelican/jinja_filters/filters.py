075f73e8a3371c10dbf2b41a50645fcd8124b9d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:10:40 +0000

    Add package minchin.pelican.jinja-filters/1.0.0/minchin.pelican.jinja_filters-1.0.0-py3-none-any.whl

---
+++ packages/minchin.pelican.jinja-filters/minchin.pelican.jinja_filters-1.0.0-py3-none-any.whl/minchin/pelican/jinja_filters/filters.py
@@ -0,0 +1,38 @@
+"""
+Various filters for Jinja.
+"""
+
+from titlecase import titlecase as _titlecase
+
+__all__ = ['datetime',
+           'article_date',
+           'breaking_spaces',
+           'titlecase']
+
+
+def datetime(value, format='%Y/%m/%d %H:%M'):
+    """
+    Convert a datetime to a different format.
+
+    The default format looks like --> 2016/11/25 12:34
+    """
+    return value.strftime(format)
+
+
+def article_date(value):
+    """
+    Converts a date to the format we want it displayed on the article template.
+
+    Format looks like --> Friday, November 4, 2016
+    """
+    return value.strftime('%A, %B %-d, %Y')
+
+
+def breaking_spaces(value):
+    """Converts non-breaking spaces to regular spaces."""
+    return value.replace('\u00A0', ' ')
+
+
+def titlecase(value):
+    """ Returns the titlecased version of the supplied text."""
+    return _titlecase(value)

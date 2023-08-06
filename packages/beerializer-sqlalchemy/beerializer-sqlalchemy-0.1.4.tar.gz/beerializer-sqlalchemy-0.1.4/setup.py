83eb191c94071f566fc0b380702c5c616bcb6129
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:09:03 +0000

    Add package beerializer-sqlalchemy/0.1.4/beerializer-sqlalchemy-0.1.4.tar.gz

---
+++ packages/beerializer-sqlalchemy/beerializer-sqlalchemy-0.1.4.tar.gz/beerializer-sqlalchemy-0.1.4/setup.py
@@ -0,0 +1,33 @@
+"""
+Package to convert arbitrary python objects into DTOs ready
+for serialization and validation.
+"""
+
+__version__ = "0.1.4"
+
+from setuptools import setup
+
+
+setup(
+    name="beerializer-sqlalchemy",
+    author="Songbee Team",
+    author_email="hi@songbee.net",
+    url="https://github.com/Songbee/beerializer-sqlalchemy",
+    version=__version__,
+    description=__doc__.replace("\n", " "),
+    long_description=open("README.rst").read(),
+    keywords="dto serializer serialize REST marshal JSON",
+    packages=["beerializer_sqlalchemy"],
+    install_requires=[
+        "SQLAlchemy",
+    ],
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "License :: Public Domain",
+        "Programming Language :: Python",
+        "Topic :: Internet :: WWW/HTTP :: Dynamic Content",
+        "Topic :: Internet :: WWW/HTTP :: Dynamic Content :: CGI Tools/Libraries",
+        "Topic :: Software Development",
+    ]
+)

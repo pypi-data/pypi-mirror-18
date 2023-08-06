53b96e0622194bc2e9952dea1c13e560f81ea3a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:30:56 +0000

    Add package sphinx-foundation-theme/0.3.0/sphinx_foundation_theme-0.3.0-py2.py3-none-any.whl

---
+++ packages/sphinx-foundation-theme/sphinx_foundation_theme-0.3.0-py2.py3-none-any.whl/sphinx_foundation_theme/__init__.py
@@ -0,0 +1,11 @@
+"""Sphinx Foundation Theme"""
+
+__version__ = '0.3.0'
+
+import os
+
+def get_html_theme_path():
+    """Return the `html_theme_path` in a list for use in a project's Sphinx
+    conf.py."""
+    cur_dir = os.path.abspath(os.path.dirname(__file__))
+    return [cur_dir]

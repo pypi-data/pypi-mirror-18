649590b063c18c6714982698787af5ecac8825f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:19:53 +0000

    Add package mycommons/1.0/mycommons-1.0.tar.gz

---
+++ packages/mycommons/mycommons-1.0.tar.gz/mycommons-1.0/setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+setup(name='mycommons',
+      version='1.0',
+      author="irene cho",
+      author_email="irene.cho@google.com",
+      license="MIT License",
+      url="https://pypi.python.org/pypi/satang500/mycommons",
+      py_modules=['mycommons'],
+)

24923afdc066770cb5fadf7430d5ad21a21cffb8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:06:18 +0000

    Add package gitmachine/0.1.3/gitmachine-0.1.3.tar.gz

---
+++ packages/gitmachine/gitmachine-0.1.3.tar.gz/gitmachine-0.1.3/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+  name = 'gitmachine',
+  packages = ['gitmachine'],
+  version = '0.1.3',
+  description = 'Git Machine',
+  author = 'Daniel Firth',
+  author_email = 'locallycompact@gmail.com',
+  url = 'https://gitlab.com/baserock/gitmachine',
+  keywords = [],
+  classifiers = [],
+  package_data={}
+)

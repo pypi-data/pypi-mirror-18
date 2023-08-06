1fb4735ebed57f2c7c1f9bf88ead4963c2967565
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:19:10 +0000

    Add package trydiffoscope/64/trydiffoscope-64.tar.gz

---
+++ packages/trydiffoscope/trydiffoscope-64.tar.gz/trydiffoscope-64/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name='trydiffoscope',
+    version='64',
+    author="Chris Lamb",
+    author_email="lamby@debian.org",
+    scripts=(
+        'trydiffoscope',
+    ),
+)

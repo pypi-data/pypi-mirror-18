5ce7eed9c77c9b8b7a58b5dfe4768f288e3d3fdc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:10:22 +0000

    Add package Flask-Diamond/0.5.0rc1/Flask-Diamond-0.5.0rc1.tar.gz

---
+++ packages/Flask-Diamond/Flask-Diamond-0.5.0rc1.tar.gz/Flask-Diamond-0.5.0rc1/flask_diamond/facets/accounts.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+# Flask-Diamond (c) Ian Dennis Miller
+
+from flask.ext.security import SQLAlchemyUserDatastore
+from flask.ext.security import Security
+
+security = Security()
+
+
+def init_accounts(self, user=None, role=None, confirm_register_form=None):
+    """
+    Initialize Security for application.
+
+    :param kwargs: parameters that will be passed through to Flask-Security
+    :type kwargs: dict
+    :returns: None
+
+    A number of common User account operations are provided by `Flask-
+    Security <http://pythonhosted.org/Flask-Security/>`_. This function is
+    responsible for associating User models in the database with the
+    Security object.
+
+    In case you need to override a Flask-Security form (as is the case
+    with implementing CAPTCHA) then you must use super() from within your
+    application and provide any arguments destined for Flask-Security.
+
+    >>> result = self.super("accounts", user=User, role=Role,
+    >>>    confirm_register_form=ExtendedRegisterForm)
+    """
+
+    # import database
+    from .. import db
+
+    if not user or not role:
+        raise Exception
+
+    # create datastore
+    user_datastore = SQLAlchemyUserDatastore(db, user, role)
+    setattr(Security, "user_datastore", user_datastore)
+
+    if confirm_register_form:
+        security.init_app(self.app, datastore=user_datastore,
+            confirm_register_form=confirm_register_form)
+    else:
+        security.init_app(self.app, datastore=user_datastore)

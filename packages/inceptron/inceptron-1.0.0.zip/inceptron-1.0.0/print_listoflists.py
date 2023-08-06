b2f5588589ef84009aeba44ef22f2bb32605b740
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:20:33 +0000

    Add package inceptron/1.0.0/inceptron-1.0.0.zip

---
+++ packages/inceptron/inceptron-1.0.0.zip/inceptron-1.0.0/print_listoflists.py
@@ -0,0 +1,24 @@
+
+"""
+This is the “inceptron.py" module, and it provides one function called
+print_listoflists() which prints lists that may or may not include nested lists.
+"""
+
+def print_list(llist, indent=False, level=0):
+    """
+    This function takes a positional argument called “llist", which is any
+    Python list (of, possibly, nested lists). Each data item in the provided list
+    is (recursively) printed to the screen on its own line.
+    :param llist:
+    :param indent:
+    :param level:
+    :return:
+    """
+    for each_item in llist:
+        if isinstance(each_item, list):
+            print_list(each_item, indent, level+1)
+        else:
+            if indent:
+                for tab_stop in range(level):
+                    print("\t", end='')
+            print(each_item)

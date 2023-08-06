d4f067c62662795b78a91beca25470b9de745976
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:06:25 +0000

    Add package reverseEcology/0.2/reverseEcology-0.2.tar.gz

---
+++ packages/reverseEcology/reverseEcology-0.2.tar.gz/reverseEcology-0.2/README.md
@@ -0,0 +1,7 @@
+## ReverseEcology
+### Copyright (c) 2016, Joshua J. Hamilton
+### [https://github.com/joshamilton](https://github.com/joshamilton)
+
+***
+
+This package is under active development. For examples of how to use it, please see the [GitHub repo](https://github.com/joshamilton/revEcolFWActinos) for our forthcoming manuscript, "Reverse Ecology of Uncultivated Freshwater Actinobacteria."
---
+++ packages/reverseEcology/reverseEcology-0.2.tar.gz/reverseEcology-0.2/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+def readme():
+    with open('README.md') as f:
+        return f.read()
+    
+setup(name='reverseEcology',
+      version='0.2',
+      description='Reverse ecology analysis of metabolic network reconstructions',
+       classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2.7',
+      ],
+      url='https://github.com/joshamilton/reverseEcology',
+      author='Joshua J. Hamilton',
+      author_email='joshamilton@gmail.com',
+      license='MIT',
+      packages=find_packages(),
+      include_package_data=True)

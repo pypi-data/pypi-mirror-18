2083cfd88c24075cb13f491378a861c58c7a2caf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:19:53 +0000

    Add package marionette_client/3.3.0/marionette_client-3.3.0.tar.gz

---
+++ packages/marionette_client/marionette_client-3.3.0.tar.gz/marionette_client-3.3.0/setup.py
@@ -0,0 +1,53 @@
+import os
+import re
+
+from setuptools import setup, find_packages
+
+
+THIS_DIR = os.path.dirname(os.path.realpath(__name__))
+
+
+def read(*parts):
+    with open(os.path.join(THIS_DIR, *parts)) as f:
+        return f.read()
+
+
+def get_version():
+    return re.findall("__version__ = '([\d\.]+)'",
+                      read('marionette', '__init__.py'), re.M)[0]
+
+
+setup(name='marionette_client',
+      version=get_version(),
+      description="Marionette test automation client",
+      long_description=open('README.rst').read(),
+      # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+          'Development Status :: 7 - Inactive',
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: Mozilla Public License 2.0 (MPL 2.0)',
+          'Operating System :: MacOS :: MacOS X',
+          'Operating System :: Microsoft :: Windows',
+          'Operating System :: POSIX',
+          'Topic :: Software Development :: Quality Assurance',
+          'Topic :: Software Development :: Testing',
+          'Topic :: Utilities',
+          'Programming Language :: Python',
+          'Programming Language :: Python :: 2.7',
+      ],
+      keywords='mozilla',
+      author='Auto-tools',
+      author_email='tools-marionette@lists.mozilla.org',
+      url='https://wiki.mozilla.org/Auto-tools/Projects/Marionette',
+      license='Mozilla Public License 2.0 (MPL 2.0)',
+      packages=['marionette'],
+      package_data={'marionette': ['touch/*.js']},
+      include_package_data=True,
+      zip_safe=False,
+      entry_points="""
+      # -*- Entry points: -*-
+      [console_scripts]
+      marionette = marionette.runtests:cli
+      """,
+      install_requires=read('requirements.txt').splitlines(),
+      )

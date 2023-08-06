79156663f357a6ba5a2f12aca1403e5777e9da65
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:22:29 +0000

    Add package github-dl/0.1a9/github-dl-0.1a9.tar.gz

---
+++ packages/github-dl/github-dl-0.1a9.tar.gz/github-dl-0.1a9/download/download_repos.py
@@ -0,0 +1,23 @@
+import git
+
+
+def download_repos(dst, repos):
+    """
+    handles downloading paginated gists
+
+    Arguments
+    ---------
+    dst : string
+        folder to write repositories to
+
+    repos : array-like of git.repo.base.Repo
+        repositories to download
+    """
+    for repo in repos:
+        try:
+            # we can clone the projects, but we only want to download small projects
+            git.repo.base.Repo.clone_from(repo.git_url, dst + '/' + repo.full_name)
+        except git.exc.GitCommandError as e:
+            # if the repositority already exists we want to move on
+            if not e.stderr.endswith('already exists and is not an empty directory.\n\''):
+                raise e

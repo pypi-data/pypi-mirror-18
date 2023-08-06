2b09efc1b15cebd9acd4fb1c341e9f421569ce5a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:30:26 +0000

    Add package wile/0.3.1/wile-0.3.1.tar.gz

---
+++ packages/wile/wile-0.3.1.tar.gz/wile-0.3.1/setup.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python
+import os
+from setuptools import setup
+
+
+def read(filename):
+    return open(os.path.join(os.path.dirname(__file__), filename)).read()
+
+setup(
+    name="wile",
+    use_scm_version=True,
+    setup_requires=['setuptools_scm'],
+    author="Leo Antunes",
+    author_email="leo@costela.net",
+    description=("A stripped down Let's Encrypt (ACME) client"),
+    license="GPLv3",
+    keywords="letsencrypt acme ssl",
+    url="https://github.com/costela/wile",
+    py_modules=['wile', 'reg', 'cert', 'argtypes'],
+    install_requires=[
+        'six',
+        'acme >= 0.6',
+        'click >= 6.0',
+        'pyOpenSSL',
+        'cryptography',
+        'setuptools_scm',  # for run-time version-detect
+    ],
+    entry_points={
+        'console_scripts': [
+            'wile = wile:main',
+        ],
+    },
+    long_description=read('README.md'),
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python',
+        'Topic :: Internet :: Name Service (DNS)',
+        'Topic :: Security :: Cryptography',
+        'Topic :: Utilities',
+    ],
+)

c433e58c10a75b727997721ceae5f91ae6964155
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:09:32 +0000

    Add package pubmarine/0.2/pubmarine-0.2.tar.gz

---
+++ packages/pubmarine/pubmarine-0.2.tar.gz/pubmarine-0.2/setup.py
@@ -0,0 +1,24 @@
+from distutils.core import setup
+
+setup(name="pubmarine",
+        version="0.2",
+        description="PubSub for Python-3.5's asyncio",
+        author="Toshio Kuratomi",
+        author_email="toshio@fedoraproject.org",
+        maintainer="Toshio Kuratomi",
+        maintainer_email="toshio@fedoraproject.org",
+        url="https://github.com/abadger/pubmarine",
+        license="Lesser GNU Public License v3+",
+        keywords='pubsub events',
+        classifiers=[
+            'Development Status :: 3 - Alpha',
+            'Intended Audience :: Developers',
+            'License :: OSI Approved :: GNU Lesser General Public License v3 or later (LGPLv3+)',
+            'Operating System :: OS Independent',
+            'Programming Language :: Python :: 3 :: Only',
+            'Programming Language :: Python :: 3.5',
+            'Programming Language :: Python :: 3.6',
+            'Topic :: Software Development :: Libraries :: Python Modules',
+        ],
+        packages=['pubmarine'],
+    )

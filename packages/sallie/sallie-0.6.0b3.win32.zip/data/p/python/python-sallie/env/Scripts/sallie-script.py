911ab2933e8e549db5ee162dc25ab0cde766e0f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:45:31 +0000

    Add package sallie/0.6.0b3/sallie-0.6.0b3.win32.zip

---
+++ packages/sallie/sallie-0.6.0b3.win32.zip/data/p/python/python-sallie/env/Scripts/sallie-script.py
@@ -0,0 +1,10 @@
+#!C:\data\p\python\python-sallie\env\Scripts\python.exe
+# EASY-INSTALL-ENTRY-SCRIPT: 'sallie==0.6.0b3','console_scripts','sallie'
+__requires__ = 'sallie==0.6.0b3'
+import sys
+from pkg_resources import load_entry_point
+
+if __name__ == '__main__':
+    sys.exit(
+        load_entry_point('sallie==0.6.0b3', 'console_scripts', 'sallie')()
+    )

fec41039387db119c49b11dfb285c2bb8f93f4b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:45:51 +0000

    Add package qcloudcli/1.3/qcloudcli-1.3.zip

---
+++ packages/qcloudcli/qcloudcli-1.3.zip/qcloudcli-1.3/qcloudsdkcvm/RenewInstanceRequest.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+from qcloudsdkcore.request import Request
+class RenewInstanceRequest(Request):
+
+	def __init__(self):
+		Request.__init__(self, 'cvm', 'qcloudcliV1', 'RenewInstance', 'cvm.api.qcloud.com')
+
+	def get_instanceId(self):
+		return self.get_params().get('instanceId')
+
+	def set_instanceId(self, instanceId):
+		self.add_param('instanceId', instanceId)
+		
+	def get_period(self):
+		return self.get_params().get('period')
+
+	def set_period(self, period):
+		self.add_param('period', period)
+

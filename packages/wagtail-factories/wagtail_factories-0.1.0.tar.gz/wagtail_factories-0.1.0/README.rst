c4db1a46e3417b303ac9137cf4da3903630a2659
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:13:52 +0000

    Add package wagtail-factories/0.1.0/wagtail_factories-0.1.0.tar.gz

---
+++ packages/wagtail-factories/wagtail_factories-0.1.0.tar.gz/wagtail_factories-0.1.0/README.rst
@@ -0,0 +1,23 @@
+=================
+wagtail-factories
+=================
+
+Factory boy classes for Wagtail CMS
+
+.. start-no-pypi
+
+Status
+------
+
+.. image:: https://readthedocs.org/projects/wagtail-factories/badge/?version=latest
+    :target: https://readthedocs.org/projects/wagtail-factories/
+   
+.. image:: https://travis-ci.org/mvantellingen/wagtail-factories.svg?branch=master
+    :target: https://travis-ci.org/mvantellingen/wagtail-factories
+
+
+.. image:: https://img.shields.io/pypi/v/wagtail-factories.svg
+    :target: https://pypi.python.org/pypi/wagtail-factories/
+
+.. end-no-pypi
+
---
+++ packages/wagtail-factories/wagtail_factories-0.1.0.tar.gz/wagtail_factories-0.1.0/src/wagtail_factories/factories.py
@@ -0,0 +1,76 @@
+import factory
+from factory.utils import extract_dict
+from django.utils.text import slugify
+from wagtail.wagtailcore.models import Collection, Page, Site
+from wagtail.wagtailimages.models import get_image_model
+
+__all__ = [
+    'CollectionFactory',
+    'ImageFactory',
+    'PageFactory',
+    'SiteFactory',
+]
+
+
+class MP_NodeFactory(factory.DjangoModelFactory):
+
+    @classmethod
+    def _create(cls, model_class, *args, **kwargs):
+        parent_passed = 'parent' in kwargs
+        parent = kwargs.pop('parent', None)
+        parent_kwargs = extract_dict('parent', kwargs)
+
+        if parent and parent_kwargs:
+            raise ValueError('Cant pass a parent instance and attributes')
+
+        if parent_kwargs:
+            parent = cls(**parent_kwargs)
+
+        if not parent and not parent_passed:
+            raise ValueError(
+                "`parent` is a required kwarg. If you want to create a root " +
+                "object then pass `parent=None`")
+
+        if parent:
+            instance = model_class(**kwargs)
+            return parent.add_child(instance=instance)
+        else:
+            return model_class.add_root(**kwargs)
+
+
+class CollectionFactory(MP_NodeFactory):
+    name = 'Test collection'
+
+    class Meta:
+        model = Collection
+
+
+class PageFactory(MP_NodeFactory):
+    title = 'Test page'
+    slug = factory.LazyAttribute(lambda obj: slugify(obj.title))
+
+    class Meta:
+        model = Page
+
+
+class CollectionMemberFactory(factory.DjangoModelFactory):
+    collection = factory.SubFactory(CollectionFactory, parent=None)
+
+
+class ImageFactory(CollectionMemberFactory):
+
+    class Meta:
+        model = get_image_model()
+
+    file = factory.django.ImageField()
+
+
+class SiteFactory(factory.DjangoModelFactory):
+    hostname = 'localhost'
+    port = factory.Sequence(lambda n: 81 + n)
+    site_name = 'Test site'
+    root_page = factory.SubFactory(PageFactory, parent=None)
+    is_default_site = False
+
+    class Meta:
+        model = Site

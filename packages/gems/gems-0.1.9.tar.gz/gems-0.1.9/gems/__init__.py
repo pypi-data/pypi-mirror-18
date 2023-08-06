292c0e35a9b9ad984d29a15a1f252c06524bb3d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:50:03 +0000

    Add package gems/0.1.9/gems-0.1.9.tar.gz

---
+++ packages/gems/gems-0.1.9.tar.gz/gems-0.1.9/gems/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+__author__ = 'Blake Printy'
+__email__ = 'bprinty@gmail.com'
+__version__ = '0.1.9'
+
+
+from .datatypes import composite
+from .datatypes import filetree
+

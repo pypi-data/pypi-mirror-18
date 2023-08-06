f0293dd246236a151a36e14e41f89d8586857ea8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:12:50 +0000

    Add package noweb.py/1.0.0/noweb.py-1.0.0.tar.gz

---
+++ packages/noweb.py/noweb.py-1.0.0.tar.gz/noweb.py-1.0.0/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+  name = 'noweb.py',
+  version = '1.0.0',
+  py_modules = ['noweb'],
+  author = 'dragontortoise',
+  author_email = 'dragontortoise888@gmail.com',
+  url = 'https://twitter.com/DragonTortoise',
+  description = 'Literate Programming with noweb implementation in Python'
+)
+

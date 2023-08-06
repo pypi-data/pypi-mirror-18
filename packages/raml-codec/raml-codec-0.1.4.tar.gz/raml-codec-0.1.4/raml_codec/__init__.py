8e201c93dda06e63e9905af081793962056f1fc3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:13:18 +0000

    Add package raml-codec/0.1.4/raml-codec-0.1.4.tar.gz

---
+++ packages/raml-codec/raml-codec-0.1.4.tar.gz/raml-codec-0.1.4/raml_codec/__init__.py
@@ -0,0 +1,19 @@
+from coreapi.codecs import BaseCodec
+from raml_codec.decode import decode_raml
+from raml_codec.encode import encode_raml
+
+
+__version__ = "0.1.4"
+
+
+class RAMLCodec(BaseCodec):
+    media_type = 'application/raml+yaml'
+    format = 'raml'
+
+    def decode(self, bytestring, **options):
+        base_url = options.get('base_url', None)
+        return decode_raml(bytestring, base_url)
+
+    def encode(self, document, **options):
+        base_url = options.get('base_url', None)
+        return encode_raml(document, base_url)

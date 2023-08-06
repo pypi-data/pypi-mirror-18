7e4066d29d86d77f1682fb4418a285330a8a4c22
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:09:38 +0000

    Add package sacad/1.8.2/sacad-1.8.2.tar.gz

---
+++ packages/sacad/sacad-1.8.2.tar.gz/sacad-1.8.2/tests/__init__.py
@@ -0,0 +1,174 @@
+#!/usr/bin/env python3
+
+import contextlib
+import logging
+import math
+import socket
+import unittest
+import unittest.mock
+import urllib.parse
+
+import PIL.Image
+import requests
+import web_cache
+
+import sacad
+
+
+web_cache.DISABLE_PERSISTENT_CACHING = True
+
+
+def is_internet_reachable():
+  try:
+    # open TCP socket to Google DNS server
+    with socket.create_connection(("8.8.8.8", 53)):
+      pass
+  except OSError as e:
+    if e.errno == 101:
+      return False
+    raise
+  return True
+
+
+def download(url, filepath):
+  with contextlib.closing(requests.get(url, timeout=5, verify=False, stream=True)) as response:
+    response.raise_for_status()
+    with open(filepath, "wb") as f:
+      for chunk in response.iter_content(2 ** 14):
+        f.write(chunk)
+
+
+@unittest.skipUnless(is_internet_reachable(), "Need Internet access")
+class TestSacad(unittest.TestCase):
+
+  @staticmethod
+  def getImgInfo(img_filepath):
+    with open(img_filepath, "rb") as img_file:
+      img = PIL.Image.open(img_file)
+      format = img.format.lower()
+      format = sacad.SUPPORTED_IMG_FORMATS[format]
+      width, height = img.size
+    return format, width, height
+
+  def test_getMasterOfPuppetsCover(self):
+    """ Search and download cover for 'Master of Puppets' with different parameters. """
+    for format in sacad.cover.CoverImageFormat:
+      for size in (300, 600, 1200):
+        for size_tolerance in (0, 25, 50):
+          with sacad.mkstemp_ctx.mkstemp(prefix="sacad_test_",
+                                         suffix=".%s" % (format.name.lower())) as tmp_filepath:
+            sacad.search_and_download("Master of Puppets",
+                                      "Metallica",
+                                      format,
+                                      size,
+                                      size_tolerance,
+                                      (),
+                                      False,
+                                      tmp_filepath)
+            out_format, out_width, out_height = __class__.getImgInfo(tmp_filepath)
+            self.assertEqual(out_format, format)
+            self.assertLessEqual(out_width, size * (100 + size_tolerance) / 100)
+            self.assertGreaterEqual(out_width, size * (100 - size_tolerance) / 100)
+            self.assertLessEqual(out_height, size * (100 + size_tolerance) / 100)
+            self.assertGreaterEqual(out_height, size * (100 - size_tolerance) / 100)
+
+  def test_getImageUrlMetadata(self):
+    """ Download the beginning of image files to guess their format and resolution. """
+    refs = {"https://www.nuclearblast.de/static/articles/152/152118.jpg/1000x1000.jpg": (sacad.cover.CoverImageFormat.JPEG,
+                                                                                         (700, 700),
+                                                                                         math.ceil(18000 / sacad.CoverSourceResult.METADATA_PEEK_SIZE_INCREMENT)),
+            "http://img2-ak.lst.fm/i/u/55ad95c53e6043e3b150ba8a0a3b20a1.png": (sacad.cover.CoverImageFormat.PNG,
+                                                                               (600, 600),
+                                                                               1)}
+    for url, (ref_fmt, ref_size, block_read) in refs.items():
+      sacad.CoverSourceResult.getImageMetadata = unittest.mock.Mock(wraps=sacad.CoverSourceResult.getImageMetadata)
+      source = unittest.mock.Mock()
+      source.http = sacad.http_helpers.Http(allow_session_cookies=False,
+                                            min_delay_between_accesses=1 / 3)
+      cover = sacad.CoverSourceResult(url,
+                                      None,
+                                      None,
+                                      source=source,
+                                      thumbnail_url=None,
+                                      source_quality=None,
+                                      check_metadata=sacad.cover.CoverImageMetadata.ALL)
+      cover.updateImageMetadata()
+      self.assertEqual(cover.size, ref_size)
+      self.assertEqual(cover.format, ref_fmt)
+      self.assertGreaterEqual(sacad.CoverSourceResult.getImageMetadata.call_count, 0)
+      self.assertLessEqual(sacad.CoverSourceResult.getImageMetadata.call_count, block_read)
+
+  def test_compareImageSignatures(self):
+    """ Compare images using their signatures. """
+    urls = ("http://wac.450f.edgecastcdn.net/80450F/kool1017.com/files/2013/09/cover_highway_to_hell_500x500.jpg",
+            "http://www.jesus-is-savior.com/Evils%20in%20America/Rock-n-Roll/highway_to_hell-large.jpg",
+            "http://i158.photobucket.com/albums/t113/gatershanks/Glee%20Alternative%20Song%20Covers/1x14%20Hell%20O/1x14Hell-O-HighwayToHell.jpg")
+    with sacad.mkstemp_ctx.mkstemp(suffix="jpg") as temp_filepath1, \
+            sacad.mkstemp_ctx.mkstemp(suffix="jpg") as temp_filepath2, \
+            sacad.mkstemp_ctx.mkstemp(suffix="jpg") as temp_filepath3:
+      img_sig = {}
+      for i, (url, filepath) in enumerate(zip(urls, (temp_filepath1, temp_filepath2, temp_filepath3))):
+        download(url, filepath)
+        with open(filepath, "rb") as img_file:
+          img_data = img_file.read()
+          img_sig[i] = sacad.CoverSourceResult.computeImgSignature(img_data)
+      self.assertTrue(sacad.CoverSourceResult.areImageSigsSimilar(img_sig[0], img_sig[1]))
+      self.assertTrue(sacad.CoverSourceResult.areImageSigsSimilar(img_sig[1], img_sig[0]))
+      self.assertFalse(sacad.CoverSourceResult.areImageSigsSimilar(img_sig[0], img_sig[2]))
+      self.assertFalse(sacad.CoverSourceResult.areImageSigsSimilar(img_sig[1], img_sig[2]))
+      self.assertFalse(sacad.CoverSourceResult.areImageSigsSimilar(img_sig[2], img_sig[0]))
+      self.assertFalse(sacad.CoverSourceResult.areImageSigsSimilar(img_sig[2], img_sig[1]))
+
+  def test_coverSources(self):
+    """ Check all sources return valid results with different parameters. """
+    for size in range(300, 1200 + 1, 300):
+      source_args = (size, 0)
+      sources = [sacad.sources.LastFmCoverSource(*source_args),
+                 sacad.sources.GoogleImagesWebScrapeCoverSource(*source_args),
+                 sacad.sources.AmazonDigitalCoverSource(*source_args)]
+      sources.extend(sacad.sources.AmazonCdCoverSource(*source_args, tld=tld) for tld in sacad.sources.AmazonCdCoverSource.TLDS)
+      for source in sources:
+        for artist, album in zip(("Michael Jackson", "Björk"), ("Thriller", "Vespertine")):
+          results = source.search(album, artist)
+          results = sacad.CoverSourceResult.preProcessForComparison(results, size, 0)
+          if not (((size > 500) and isinstance(source, sacad.sources.AmazonCdCoverSource)) or
+                  ((size > 500) and isinstance(source, sacad.sources.LastFmCoverSource)) or
+                  (isinstance(source, sacad.sources.AmazonCdCoverSource) and (artist == "Björk") and
+                   (urllib.parse.urlsplit(source.base_url).netloc.rsplit(".", 1)[-1] == "cn"))):
+            self.assertGreaterEqual(len(results), 1, "%s %s %s %u" % (source.__class__.__name__,
+                                                                      artist,
+                                                                      album,
+                                                                      size))
+          for result in results:
+            self.assertTrue(result.urls)
+            self.assertIn(result.format, sacad.cover.CoverImageFormat)
+            self.assertGreaterEqual(result.size[0], size)
+
+    # test for specific cover not available on amazon.com, but on amazon.de
+    size = 290
+    source = sacad.sources.AmazonCdCoverSource(size, 0, tld="de")
+    results = source.search("Dream Dance 5", "Various")
+    self.assertGreaterEqual(len(results), 1)
+    for result in results:
+      self.assertTrue(result.urls)
+      self.assertIn(result.format, sacad.cover.CoverImageFormat)
+      self.assertGreaterEqual(result.size[0], size)
+
+  def test_unaccentuate(self):
+    self.assertEqual(sacad.sources.base.CoverSource.unaccentuate("EéeAàaOöoIïi"), "EeeAaaOooIii")
+
+  def test_is_square(self):
+    for x in range(1, 100):
+      if x in (1, 4, 9, 16, 25, 36, 49, 64, 81):
+        self.assertTrue(sacad.cover.is_square(x), x)
+      else:
+        self.assertFalse(sacad.cover.is_square(x), x)
+
+
+if __name__ == "__main__":
+  # logging
+  #logging.getLogger().setLevel(logging.DEBUG)
+  logging.getLogger().setLevel(logging.CRITICAL + 1)
+
+  # run tests
+  unittest.main()
---
+++ packages/sacad/sacad-1.8.2.tar.gz/sacad-1.8.2/tests/recurse_test.py
@@ -0,0 +1,249 @@
+#!/usr/bin/env python3
+
+import collections
+import contextlib
+import functools
+import itertools
+import os
+import shutil
+import tempfile
+import unittest
+import urllib.parse
+
+import mutagen
+import requests
+
+import sacad.recurse as recurse
+
+
+try:
+  redirect_stdout = contextlib.redirect_stdout
+except AttributeError:
+  # contextlib.redirect_stdout is not available (Python 3.3), build our own
+  import sys
+  @contextlib.contextmanager
+  def redirect_stdout(s):
+    original_stdout, sys.stdout = sys.stdout, s
+    try:
+      yield
+    finally:
+      sys.stdout = original_stdout
+
+
+def download(url, filepath):
+  cache_dir = os.getenv("TEST_DL_CACHE_DIR")
+  if cache_dir is not None:
+    os.makedirs(cache_dir, exist_ok=True)
+    cache_filepath = os.path.join(cache_dir,
+                                  os.path.basename(urllib.parse.urlsplit(url).path))
+    if os.path.isfile(cache_filepath):
+      shutil.copyfile(cache_filepath, filepath)
+      return
+  with contextlib.closing(requests.get(url, stream=True)) as response:
+    response.raise_for_status()
+    with open(filepath, "wb") as f:
+      for chunk in response.iter_content(2 ** 14):
+        f.write(chunk)
+  if cache_dir is not None:
+    shutil.copyfile(filepath, cache_filepath)
+
+
+class TestRecursive(unittest.TestCase):
+
+  @classmethod
+  def setUpClass(cls):
+    cls.temp_dir = tempfile.TemporaryDirectory()
+    cls.album1_dir = os.path.join(cls.temp_dir.name, "album1")
+    os.mkdir(cls.album1_dir)
+    url = "https://upload.wikimedia.org/wikipedia/en/4/45/ACDC_-_Back_In_Black-sample.ogg"
+    filepath1 = os.path.join(cls.album1_dir, "2 track.ogg")
+    download(url, filepath1)
+    mf = mutagen.File(filepath1)
+    mf["artist"] = "ARTIST1"
+    mf["album"] = "ALBUM1"
+    mf.save()
+
+    cls.album2_dir = os.path.join(cls.temp_dir.name, "album2")
+    os.mkdir(cls.album2_dir)
+    filepath2 = os.path.join(cls.album2_dir, "1.dat")
+    with open(filepath2, "wb") as f:
+      f.write(b"\x00" * 8)
+    filepath3 = os.path.join(cls.album2_dir, "2 track.ogg")
+    shutil.copyfile(filepath1, filepath3)
+    mf = mutagen.File(filepath3)
+    mf["artist"] = "ARTIST2"
+    mf["album"] = "ALBUM2"
+    mf.save()
+
+    cls.album3_dir = os.path.join(cls.temp_dir.name, "album3")
+    os.mkdir(cls.album3_dir)
+    url = "http://www.stephaniequinn.com/Music/Vivaldi%20-%20Spring%20from%20Four%20Seasons.mp3"
+    download(url, os.path.join(cls.album3_dir, "1 track.mp3"))
+
+    cls.album4_dir = os.path.join(cls.temp_dir.name, "album4")
+    os.mkdir(cls.album4_dir)
+    url = "https://auphonic.com/media/audio-examples/01.auphonic-demo-unprocessed.m4a"
+    download(url, os.path.join(cls.album4_dir, "1 track.m4a"))
+
+    cls.not_album_dir = os.path.join(cls.temp_dir.name, "not an album")
+    os.mkdir(cls.not_album_dir)
+    shutil.copyfile(filepath2, os.path.join(cls.not_album_dir, "a.dat"))
+
+    cls.invalid_album_dir = os.path.join(cls.temp_dir.name, "invalid album")
+    os.mkdir(cls.invalid_album_dir)
+    shutil.copyfile(filepath2, os.path.join(cls.invalid_album_dir, "2 track.ogg"))
+    filepath4 = os.path.join(cls.invalid_album_dir, "3 track.ogg")
+    shutil.copyfile(filepath1, filepath4)
+    mf = mutagen.File(filepath4)
+    del mf["album"]
+    mf.save()
+
+  @classmethod
+  def tearDownClass(cls):
+    cls.temp_dir.cleanup()
+
+  def test_analyze_lib(self):
+    with open(os.devnull, "wt") as dn, redirect_stdout(dn):
+      work = recurse.analyze_lib(__class__.temp_dir.name, "a.jpg")
+      self.assertEqual(len(work), 4)
+      self.assertIn(__class__.album1_dir, work)
+      self.assertEqual(work[__class__.album1_dir], ("ARTIST1", "ALBUM1"))
+      self.assertIn(__class__.album2_dir, work)
+      self.assertEqual(work[__class__.album2_dir], ("ARTIST2", "ALBUM2"))
+      self.assertIn(__class__.album3_dir, work)
+      self.assertEqual(work[__class__.album3_dir], ("Quinn String Quartet", "Israeli Concertino"))
+      self.assertIn(__class__.album4_dir, work)
+      self.assertEqual(work[__class__.album4_dir], ("Auphonic", "Auphonic Demonstration"))
+
+      work = recurse.analyze_lib(__class__.temp_dir.name, "1.dat")
+      self.assertEqual(len(work), 3)
+      self.assertIn(__class__.album1_dir, work)
+      self.assertEqual(work[__class__.album1_dir], ("ARTIST1", "ALBUM1"))
+      self.assertIn(__class__.album3_dir, work)
+      self.assertEqual(work[__class__.album3_dir], ("Quinn String Quartet", "Israeli Concertino"))
+      self.assertIn(__class__.album4_dir, work)
+      self.assertEqual(work[__class__.album4_dir], ("Auphonic", "Auphonic Demonstration"))
+
+  def test_get_metadata(self):
+    self.assertEqual(recurse.get_metadata(map(functools.partial(os.path.join,
+                                                                __class__.album1_dir),
+                                              os.listdir(__class__.album1_dir))),
+                     ("ARTIST1", "ALBUM1"))
+    self.assertEqual(recurse.get_metadata(map(functools.partial(os.path.join,
+                                                                __class__.album2_dir),
+                                              os.listdir(__class__.album2_dir))),
+                     ("ARTIST2", "ALBUM2"))
+    self.assertEqual(recurse.get_metadata(map(functools.partial(os.path.join,
+                                                                __class__.album3_dir),
+                                              os.listdir(__class__.album3_dir))),
+                     ("Quinn String Quartet", "Israeli Concertino"))
+    self.assertEqual(recurse.get_metadata(map(functools.partial(os.path.join,
+                                                                __class__.album4_dir),
+                                              os.listdir(__class__.album4_dir))),
+                     ("Auphonic", "Auphonic Demonstration"))
+    self.assertEqual(recurse.get_metadata(map(functools.partial(os.path.join,
+                                                                __class__.not_album_dir),
+                                              os.listdir(__class__.not_album_dir))),
+                     (None, None))
+    self.assertEqual(recurse.get_metadata(map(functools.partial(os.path.join,
+                                                                __class__.invalid_album_dir),
+                                              os.listdir(__class__.invalid_album_dir))),
+                     ("ARTIST1", None))
+
+  def test_analyze_dir(self):
+    scrobbler = itertools.cycle("|/-\\")
+    with open(os.devnull, "wt") as dn, redirect_stdout(dn):
+      stats = collections.defaultdict(int)
+      failed_dirs = []
+      r = recurse.analyze_dir(stats,
+                              __class__.album1_dir,
+                              os.listdir(__class__.album1_dir),
+                              "1.jpg",
+                              failed_dirs,
+                              scrobbler,
+                              0)
+      self.assertIn("files", stats)
+      self.assertEqual(stats["files"], 1)
+      self.assertIn("albums", stats)
+      self.assertEqual(stats["albums"], 1)
+      self.assertIn("missing covers", stats)
+      self.assertEqual(stats["missing covers"], 1)
+      self.assertNotIn("errors", stats)
+      self.assertEqual(len(failed_dirs), 0)
+      self.assertEqual(r[0], ("ARTIST1", "ALBUM1"))
+
+      stats.clear()
+      r = recurse.analyze_dir(stats,
+                              __class__.album2_dir,
+                              os.listdir(__class__.album2_dir),
+                              "1.jpg",
+                              failed_dirs,
+                              scrobbler,
+                              0)
+      self.assertIn("files", stats)
+      self.assertEqual(stats["files"], 2)
+      self.assertIn("albums", stats)
+      self.assertEqual(stats["albums"], 1)
+      self.assertIn("missing covers", stats)
+      self.assertEqual(stats["missing covers"], 1)
+      self.assertNotIn("errors", stats)
+      self.assertEqual(len(failed_dirs), 0)
+      self.assertEqual(r[0], ("ARTIST2", "ALBUM2"))
+      stats.clear()
+      r = recurse.analyze_dir(stats,
+                              __class__.album2_dir,
+                              os.listdir(__class__.album2_dir),
+                              "1.dat",
+                              failed_dirs,
+                              scrobbler,
+                              0)
+      self.assertIn("files", stats)
+      self.assertEqual(stats["files"], 2)
+      self.assertIn("albums", stats)
+      self.assertEqual(stats["albums"], 1)
+      self.assertNotIn("missing covers", stats)
+      self.assertNotIn("errors", stats)
+      self.assertEqual(len(failed_dirs), 0)
+      self.assertEqual(r[0], (None, None))
+
+      stats.clear()
+      failed_dirs.clear()
+      r = recurse.analyze_dir(stats,
+                              __class__.not_album_dir,
+                              os.listdir(__class__.not_album_dir),
+                              "1.jpg",
+                              failed_dirs,
+                              scrobbler,
+                              0)
+      self.assertIn("files", stats)
+      self.assertEqual(stats["files"], 1)
+      self.assertNotIn("albums", stats)
+      self.assertNotIn("missing covers", stats)
+      self.assertNotIn("errors", stats)
+      self.assertEqual(len(failed_dirs), 0)
+      self.assertEqual(r[0], (None, None))
+
+      stats.clear()
+      failed_dirs.clear()
+      r = recurse.analyze_dir(stats,
+                              __class__.invalid_album_dir,
+                              os.listdir(__class__.invalid_album_dir),
+                              "1.jpg",
+                              failed_dirs,
+                              scrobbler,
+                              0)
+      self.assertIn("files", stats)
+      self.assertEqual(stats["files"], 2)
+      self.assertIn("albums", stats)
+      self.assertEqual(stats["albums"], 1)
+      self.assertIn("missing covers", stats)
+      self.assertEqual(stats["missing covers"], 1)
+      self.assertIn("errors", stats)
+      self.assertEqual(stats["errors"], 1)
+      self.assertSequenceEqual(failed_dirs, (__class__.invalid_album_dir,))
+      self.assertEqual(r[0], ("ARTIST1", None))
+
+
+if __name__ == "__main__":
+  # run tests
+  unittest.main()

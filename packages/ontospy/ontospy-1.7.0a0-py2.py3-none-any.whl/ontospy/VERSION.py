59c6c39dc0ca131d69a4be4e68885c5f08b9e1ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:22:22 +0000

    Add package ontospy/1.7.0a0/ontospy-1.7.0a0-py2.py3-none-any.whl

---
+++ packages/ontospy/ontospy-1.7.0a0-py2.py3-none-any.whl/ontospy/VERSION.py
@@ -0,0 +1,25 @@
+# !/usr/bin/env python
+#  -*- coding: UTF-8 -*-
+
+
+##################
+#
+#  metadata
+#
+##################
+
+
+
+__version__ = "1.7.0-alpha"
+__copyright__ = "CopyRight (C) 2015-2016 by Michele Pasin"
+__license__ = "GNU"
+__author__ = "Michele Pasin"
+__author_email__ = "michele dot pasin at gmail dot com"
+
+USAGE = "ontospy [options] [uri]"
+VERSION = "v" + __version__
+
+AGENT = "%s/%s" % (__name__, __version__)
+
+
+

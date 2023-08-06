8193fce6baccd57d4fbdf00aa2884b487586ddf0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:32:23 +0000

    Add package pook/0.1.0rc1/pook-0.1.0rc1.tar.gz

---
+++ packages/pook/pook-0.1.0rc1.tar.gz/pook-0.1.0rc1/History.rst
@@ -0,0 +1,7 @@
+History
+=======
+
+0.1.0-rc.1 (2016-11-27)
+------------------
+
+- First version (still beta)
---
+++ packages/pook/pook-0.1.0rc1.tar.gz/pook-0.1.0rc1/pook/__init__.py
@@ -0,0 +1,11 @@
+from .api import *  # noqa
+
+# Explicit symbols export
+__all__ = api_exports  # noqa
+
+# Package metadata
+__author__ = 'Tomas Aparicio'
+__license__ = 'MIT'
+
+# Current version
+__version__ = '0.1.0-rc.1'

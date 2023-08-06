d3c79368f9982c6867af12f3633723ebcf059a65
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:09:59 +0000

    Add package git-up/1.4.4/git-up-1.4.4.zip

---
+++ packages/git-up/git-up-1.4.4.zip/git-up-1.4.4/setup.py
@@ -0,0 +1,49 @@
+# coding=utf-8
+from setuptools import setup, find_packages
+
+setup(
+    name="git-up",
+    version="1.4.4",
+    packages=find_packages(),
+    scripts=['PyGitUp/gitup.py'],
+    install_requires=['GitPython>2.0.0', 'colorama>=0.3.7',
+                      'termcolor>=1.1.0', 'click>=6.0.0',
+                      'six>=1.10.0'],
+
+    # Tests
+    test_suite="nose.collector",
+    tests_require='nose',
+
+    # Executable
+    entry_points={
+        'console_scripts': [
+            'git-up = PyGitUp.gitup:run'
+        ]
+    },
+
+    # Additional data
+    package_data={
+        'PyGitUp': ['check-bundler.rb'],
+        '': ['README.rst', 'LICENCE']
+    },
+
+    zip_safe=False,
+
+    # Metadata
+    author="Markus Siemens",
+    author_email="markus@m-siemens.de",
+    description="A python implementation of 'git up'",
+    license="MIT",
+    keywords="git git-up",
+    url="https://github.com/msiemens/PyGitUp",
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Topic :: Software Development :: Version Control",
+        "Topic :: Utilities"
+    ],
+
+    long_description=open('README.rst').read()
+)

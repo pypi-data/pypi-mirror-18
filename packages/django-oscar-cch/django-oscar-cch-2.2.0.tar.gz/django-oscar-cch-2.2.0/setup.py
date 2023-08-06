f44ac0c47daa55c76fa198db0946a003f0116081
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:19:56 +0000

    Add package django-oscar-cch/2.2.0/django-oscar-cch-2.2.0.tar.gz

---
+++ packages/django-oscar-cch/django-oscar-cch-2.2.0.tar.gz/django-oscar-cch-2.2.0/setup.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python
+import codecs
+import os.path
+from setuptools import setup
+from versiontag import get_version, cache_git_tag
+
+
+packages = [
+    'oscarcch',
+    'oscarcch.migrations',
+    'oscarcch.tests',
+]
+
+setup_requires = [
+    'versiontag>=1.1.1',
+]
+
+requires = [
+    'Django>=1.9.12',
+    'django-statsd-mozilla>=0.3.16',
+    'django-oscar>=1.3',
+    'instrumented-soap>=1.1.0',
+]
+
+extras_require = {
+    'raven':  ["raven>=5.12.0"],
+}
+
+def fpath(name):
+    return os.path.join(os.path.dirname(__file__), name)
+
+def read(fname):
+    return codecs.open(fpath(fname), encoding='utf-8').read()
+
+cache_git_tag()
+
+setup(
+    name='django-oscar-cch',
+    description="Integration between django-oscar and the CCH Sales Tax Office SOAP API.",
+    version=get_version(pypi=True),
+    long_description=open('README.rst').read(),
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Framework :: Django :: 1.8',
+        'Framework :: Django :: 1.9',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: ISC License (ISCL)',
+        'Operating System :: Unix',
+        'Operating System :: MacOS :: MacOS X',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+    author='Craig Weber',
+    author_email='crgwbr@gmail.com',
+    url='https://gitlab.com/thelabnyc/django-oscar-cch',
+    license='ISC',
+    packages=packages,
+    install_requires=requires,
+    extras_require=extras_require,
+    setup_requires=setup_requires
+)

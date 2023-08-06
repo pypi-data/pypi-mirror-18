f1e4abc17260476bc10537e21686d452e82ca121
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:20:35 +0000

    Add package libarchive-c/2.7/libarchive-c-2.7.tar.gz

---
+++ packages/libarchive-c/libarchive-c-2.7.tar.gz/libarchive-c-2.7/libarchive/entry.py
@@ -0,0 +1,139 @@
+from __future__ import division, print_function, unicode_literals
+
+from contextlib import contextmanager
+from ctypes import c_char_p, create_string_buffer
+
+from . import ffi
+
+
+@contextmanager
+def new_archive_entry():
+    entry_p = ffi.entry_new()
+    try:
+        yield entry_p
+    finally:
+        ffi.entry_free(entry_p)
+
+
+class ArchiveEntry(object):
+
+    def __init__(self, archive_p, entry_p):
+        self._archive_p = archive_p
+        self._entry_p = entry_p
+
+    def __str__(self):
+        return self.pathname
+
+    @property
+    def filetype(self):
+        return ffi.entry_filetype(self._entry_p)
+
+    def get_blocks(self, block_size=ffi.page_size):
+        archive_p = self._archive_p
+        buf = create_string_buffer(block_size)
+        read = ffi.read_data
+        while 1:
+            r = read(archive_p, buf, block_size)
+            if r == 0:
+                break
+            yield buf.raw[0:r]
+
+    @property
+    def isblk(self):
+        return self.filetype & 0o170000 == 0o060000
+
+    @property
+    def ischr(self):
+        return self.filetype & 0o170000 == 0o020000
+
+    @property
+    def isdir(self):
+        return self.filetype & 0o170000 == 0o040000
+
+    @property
+    def isfifo(self):
+        return self.filetype & 0o170000 == 0o010000
+
+    @property
+    def islnk(self):
+        return bool(ffi.entry_hardlink_w(self._entry_p) or
+                    ffi.entry_hardlink(self._entry_p))
+
+    @property
+    def issym(self):
+        return self.filetype & 0o170000 == 0o120000
+
+    def _linkpath(self):
+        return (ffi.entry_symlink_w(self._entry_p) or
+                ffi.entry_hardlink_w(self._entry_p) or
+                ffi.entry_symlink(self._entry_p) or
+                ffi.entry_hardlink(self._entry_p))
+
+    # aliases to get the same api as tarfile
+    linkpath = property(_linkpath)
+    linkname = property(_linkpath)
+
+    @property
+    def isreg(self):
+        return self.filetype & 0o170000 == 0o100000
+
+    @property
+    def isfile(self):
+        return self.isreg
+
+    @property
+    def issock(self):
+        return self.filetype & 0o170000 == 0o140000
+
+    @property
+    def isdev(self):
+        return self.ischr or self.isblk or self.isfifo or self.issock
+
+    @property
+    def atime(self):
+        return ffi.entry_atime(self._entry_p)
+
+    @property
+    def mtime(self):
+        return ffi.entry_mtime(self._entry_p)
+
+    @property
+    def ctime(self):
+        return ffi.entry_ctime(self._entry_p)
+
+    def _getpathname(self):
+        return (ffi.entry_pathname_w(self._entry_p) or
+                ffi.entry_pathname(self._entry_p))
+
+    def _setpathname(self, value):
+        if not isinstance(value, bytes):
+            value = value.encode('utf8')
+        ffi.entry_update_pathname_utf8(self._entry_p, c_char_p(value))
+
+    pathname = property(_getpathname, _setpathname)
+    # aliases to get the same api as tarfile
+    path = property(_getpathname, _setpathname)
+    name = property(_getpathname, _setpathname)
+
+    @property
+    def size(self):
+        if ffi.entry_size_is_set(self._entry_p):
+            return ffi.entry_size(self._entry_p)
+
+    @property
+    def mode(self):
+        return ffi.entry_mode(self._entry_p)
+
+    @property
+    def strmode(self):
+        # note we strip the mode because archive_entry_strmode
+        # returns a trailing space: strcpy(bp, "?rwxrwxrwx ");
+        return ffi.entry_strmode(self._entry_p).strip()
+
+    @property
+    def rdevmajor(self):
+        return ffi.entry_rdevmajor(self._entry_p)
+
+    @property
+    def rdevminor(self):
+        return ffi.entry_rdevminor(self._entry_p)
---
+++ packages/libarchive-c/libarchive-c-2.7.tar.gz/libarchive-c-2.7/tests/test_rwx.py
@@ -0,0 +1,122 @@
+"""Test reading, writing and extracting archives."""
+
+from __future__ import division, print_function, unicode_literals
+
+import libarchive
+from libarchive.extract import EXTRACT_OWNER, EXTRACT_PERM, EXTRACT_TIME
+
+from . import check_archive, in_dir, treestat
+
+
+def test_buffers(tmpdir):
+
+    # Collect information on what should be in the archive
+    tree = treestat('libarchive')
+
+    # Create an archive of our libarchive/ directory
+    buf = bytes(bytearray(1000000))
+    with libarchive.memory_writer(buf, 'gnutar', 'xz') as archive:
+        archive.add_files('libarchive/')
+
+    # Read the archive and check that the data is correct
+    with libarchive.memory_reader(buf) as archive:
+        check_archive(archive, tree)
+
+    # Extract the archive in tmpdir and check that the data is intact
+    with in_dir(tmpdir.strpath):
+        flags = EXTRACT_OWNER | EXTRACT_PERM | EXTRACT_TIME
+        libarchive.extract_memory(buf, flags)
+        tree2 = treestat('libarchive')
+        assert tree2 == tree
+
+
+def test_fd(tmpdir):
+    archive_file = open(tmpdir.strpath+'/test.tar.bz2', 'w+b')
+    fd = archive_file.fileno()
+
+    # Collect information on what should be in the archive
+    tree = treestat('libarchive')
+
+    # Create an archive of our libarchive/ directory
+    with libarchive.fd_writer(fd, 'gnutar', 'bzip2') as archive:
+        archive.add_files('libarchive/')
+
+    # Read the archive and check that the data is correct
+    archive_file.seek(0)
+    with libarchive.fd_reader(fd) as archive:
+        check_archive(archive, tree)
+
+    # Extract the archive in tmpdir and check that the data is intact
+    archive_file.seek(0)
+    with in_dir(tmpdir.strpath):
+        flags = EXTRACT_OWNER | EXTRACT_PERM | EXTRACT_TIME
+        libarchive.extract_fd(fd, flags)
+        tree2 = treestat('libarchive')
+        assert tree2 == tree
+
+
+def test_files(tmpdir):
+    archive_path = tmpdir.strpath+'/test.tar.gz'
+
+    # Collect information on what should be in the archive
+    tree = treestat('libarchive')
+
+    # Create an archive of our libarchive/ directory
+    with libarchive.file_writer(archive_path, 'ustar', 'gzip') as archive:
+        archive.add_files('libarchive/')
+
+    # Read the archive and check that the data is correct
+    with libarchive.file_reader(archive_path) as archive:
+        check_archive(archive, tree)
+
+    # Extract the archive in tmpdir and check that the data is intact
+    with in_dir(tmpdir.strpath):
+        flags = EXTRACT_OWNER | EXTRACT_PERM | EXTRACT_TIME
+        libarchive.extract_file(archive_path, flags)
+        tree2 = treestat('libarchive')
+        assert tree2 == tree
+
+
+def test_custom_writer():
+
+    # Collect information on what should be in the archive
+    tree = treestat('libarchive')
+
+    # Create an archive of our libarchive/ directory
+    blocks = []
+
+    def write_cb(data):
+        blocks.append(data[:])
+        return len(data)
+
+    with libarchive.custom_writer(write_cb, 'zip') as archive:
+        archive.add_files('libarchive/')
+        pass
+
+    # Read the archive and check that the data is correct
+    buf = b''.join(blocks)
+    with libarchive.memory_reader(buf) as archive:
+        check_archive(archive, tree)
+
+
+def test_adding_entry_from_memory():
+    entry_path = 'this is path'
+    entry_data = 'content'
+    entry_size = len(entry_data)
+
+    blocks = []
+
+    def write_callback(data):
+        blocks.append(data[:])
+        return len(data)
+
+    with libarchive.custom_writer(write_callback, 'zip') as archive:
+        archive.add_file_from_memory(entry_path, entry_size, entry_data)
+
+    buf = b''.join(blocks)
+    with libarchive.memory_reader(buf) as memory_archive:
+        for archive_entry in memory_archive:
+            assert entry_data.encode() == b''.join(
+                archive_entry.get_blocks()
+            )
+            assert archive_entry.path == entry_path

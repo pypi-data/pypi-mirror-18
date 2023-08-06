4c1bfe8b4af28e272fb109cd75ad8fe226ca6cd4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:10:30 +0000

    Add package pygments-ccl-lexer/0.6.6/pygments-ccl-lexer-0.6.6.tar.gz

---
+++ packages/pygments-ccl-lexer/pygments-ccl-lexer-0.6.6.tar.gz/pygments-ccl-lexer-0.6.6/setup.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+version = '0.6.6'
+
+setup(name='pygments-ccl-lexer',
+    version=version,
+    description='Pygments lexer for CCL',
+    long_description=open('README.rst').read(),
+    keywords='pygments ccl lexer',
+    author='Thomas J. Perry',
+    author_email='admin@daftscience.com',
+    url='https://github.com/daftscience/pygments-ccl-lexer',
+    license='GPL',
+    packages=find_packages(),
+    install_requires=[
+        'pygments >= 1.4',
+        # -*- Extra requirements: -*-
+    ],
+    entry_points="""
+        [pygments.lexers]
+        ccl=ccl_lexer:CCL_Lexer
+    """,
+)

c27d0d985ab756516b528041fd06a6e34d31ea33
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:20:28 +0000

    Add package cloudspecs/0.1.0/cloudspecs-0.1.0.tar.gz

---
+++ packages/cloudspecs/cloudspecs-0.1.0.tar.gz/cloudspecs-0.1.0/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+import os
+
+
+def load_data_files(directory='data'):
+    return [(root, [os.path.join(root, f) for f in files])
+            for root, dirs, files in os.walk(directory)]
+
+setup(
+    name='cloudspecs',
+    version='0.1.0',
+    author='Mihir Singh (@citruspi)',
+    author_email='hello@mihirsingh.com',
+    packages=find_packages(),
+    zip_safe=False,
+    include_package_data=True,
+    platforms='any',
+    data_files=load_data_files()
+)

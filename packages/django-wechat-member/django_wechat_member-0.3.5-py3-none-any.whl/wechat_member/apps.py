161488e0f137e505ddb49757050b4224cf887b2f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:07:46 +0000

    Add package django-wechat-member/0.3.5/django_wechat_member-0.3.5-py3-none-any.whl

---
+++ packages/django-wechat-member/django_wechat_member-0.3.5-py3-none-any.whl/wechat_member/apps.py
@@ -0,0 +1,6 @@
+from django.utils.translation import ugettext_lazy as _
+from django.apps import AppConfig
+
+class WechatMemberConfig(AppConfig):
+    name = 'wechat_member'
+    verbose_name = _('wechat member')

c717b8184e1a4c6a69bccc4d8b31c05539bff2e4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:20:30 +0000

    Add package PixivPy/3.2.2/PixivPy-3.2.2.tar.gz

---
+++ packages/PixivPy/PixivPy-3.2.2.tar.gz/PixivPy-3.2.2/pixivpy3/__init__.py
@@ -0,0 +1,10 @@
+"""
+Pixiv API library
+"""
+__version__ = '3.2.2'
+
+from .papi import PixivAPI
+from .aapi import AppPixivAPI
+from .utils import PixivError
+
+__all__ = ("PixivAPI", "AppPixivAPI", "PixivError")

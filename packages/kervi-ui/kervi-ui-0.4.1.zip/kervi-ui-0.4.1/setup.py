fd5ac6ada819a65924eeb0d50f42cc3e44b36f42
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:46:09 +0000

    Add package kervi-ui/0.4.1/kervi-ui-0.4.1.zip

---
+++ packages/kervi-ui/kervi-ui-0.4.1.zip/kervi-ui-0.4.1/setup.py
@@ -0,0 +1,18 @@
+from distutils.core import setup
+from distutils import dir_util
+from distutils import sysconfig
+import os
+
+setup(
+    name='kervi-ui',
+    packages=['kervi-ui',"kervi-ui/web"],
+    version='0.4.1',
+    description='UI module for the kervi framework. It is included as dependency when kervi in installed.',
+    author='Tim Wentzlau',
+    author_email='tim.wentzlau@gmail.com',
+    url='https://github.com/kervi/kervi-ui',
+    download_url='https://github.com/wentzlau/kervi-ui/archive/v1.0-alpha.tar.gz',
+    keywords=['ui', 'robotic', 'automation'],
+    classifiers=[],
+)
+

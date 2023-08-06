9321bb42e27ea71877a0d2c9b29cfb5b6da18764
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:32:32 +0000

    Add package shellsnmp/0.0.2/shellsnmp-0.0.2.tar.gz

---
+++ packages/shellsnmp/shellsnmp-0.0.2.tar.gz/shellsnmp-0.0.2/README.rst
@@ -0,0 +1,26 @@
+=========
+shellsnmp
+=========
+
+| Shamelessly (ab)using NET-SNMP commands for a quick-and-dirty Python SNMP lib
+
+Usage
+-----
+
+Be sure you have MIB files loaded on your system.::
+
+    from shellsnmp.Poller import SNMP
+
+    snmp = SNMP(community='public', host='172.16.1.3')
+    status, time = snmp.bulkwalk(mibfile='/path/to/IF-MIB.my', 
+        oidspec='ifOperStatus')
+
+
+Note that only `bulkwalk()` has been implemented at this time.
+
+License and Copyright
+---------------------
+
+Licensed MIT
+
+Copyright 2016 - David Michael Pennington (mike /|at|\ pennington.net)
---
+++ packages/shellsnmp/shellsnmp-0.0.2.tar.gz/shellsnmp-0.0.2/shellsnmp/Poller.py
@@ -0,0 +1,45 @@
+from subprocess import Popen, PIPE
+import re
+
+import arrow
+
+class SNMP(object):
+    def __init__(self, host='', community='public'):
+        assert host
+        self.host = host
+        self.community = community
+
+    def _cast(self, text):
+        if re.search(r'^\s*\d+\s*$', text):
+            # Integer
+            return int(text)
+        elif re.search(r'^\s*\d+\.\d+\.\d+\.\d+\s*$', text):
+            # IP Address
+            return str(text)
+        else:
+            return str(text)
+
+    def _parse(self, text):
+        """Parse the output of bulkwalk()"""
+        retval = dict()
+        for line in text.splitlines():
+            vals = re.split('\s+', line)
+            retval[self._cast(vals[0].split('.')[-1])] = self._cast(vals[1])
+        return retval
+
+    def pivot_table(self, vals1, vals2):
+        """Use the common oid index values to combine into one table"""
+        assert vals1
+        assert vals2
+        retval = dict()
+        for idx, val1 in vals1.items():
+            retval[val1] =vals2.get(idx)
+        return retval
+
+    def bulkwalk(self, mibfile="", oidspec=""):
+        assert mibfile
+        assert oidspec
+        cmd = 'snmpbulkwalk -v 2c -m ./{0} -c {1} -Oxsq {2} {3}'.format(
+            mibfile, self.community, self.host, oidspec)
+        proc = Popen(cmd, shell=True, stdout=PIPE)
+        return self._parse(proc.stdout.read()), arrow.now()

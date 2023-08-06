3da8ab3a8f05a9271ddef4953e1dfbba5710f1e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:07:41 +0000

    Add package pypathway/0.2.4.2/pypathway-0.2.4.2.tar.gz

---
+++ packages/pypathway/pypathway-0.2.4.2.tar.gz/pypathway-0.2.4.2/setup.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+import sys
+
+# with open('README.rst') as readme_file:
+#     readme = readme_file.read()
+
+# with open('HISTORY.rst') as history_file:
+#     history = history_file.read()
+
+try:
+   import pypandoc
+   readme = pypandoc.convert('README.md', 'rst')
+except (IOError, ImportError):
+   readme = ''
+
+
+requirements = [
+    'requests>=1.0',
+    'ipython',
+    'jupyter',
+    'echarts-python',
+    'numpy',
+]
+
+test_requirements = [
+]
+
+setup(
+    name='pypathway',
+    version='0.2.4.2',
+    description="A Python package for playing with pathways",
+    long_description=readme,
+    author="sheep",
+    author_email='sss3barry@gmail.com',
+    url='https://github.com/iseekwonderful/pypathway',
+    packages=[
+        'pypathway',
+    ],
+    package_dir={'pypathway':
+                 'pypathway'},
+    include_package_data=True,
+    install_requires=requirements,
+    license="GNU General Public License v3 (GPLv3)",
+    zip_safe=False,
+    keywords='pypathway',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Natural Language :: English',
+        "Programming Language :: Python :: 2",
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+    ],
+    test_suite='tests',
+    tests_require=test_requirements
+)

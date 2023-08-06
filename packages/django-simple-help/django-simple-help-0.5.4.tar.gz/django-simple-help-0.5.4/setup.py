3670640dac5c2a67da2dbe0de71cfd798ecf9b3c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:09:29 +0000

    Add package django-simple-help/0.5.4/django-simple-help-0.5.4.tar.gz

---
+++ packages/django-simple-help/django-simple-help-0.5.4.tar.gz/django-simple-help-0.5.4/setup.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# django-simple-help
+# setup.py
+
+from setuptools import (
+    setup,
+    find_packages,
+)
+
+
+# metadata
+VERSION = (0, 5, 4)
+__version__ = ".".join(map(str, VERSION))
+setup(
+    name="django-simple-help",
+    version=__version__,
+    packages=find_packages(),
+    install_requires=[
+        "django-annoying==0.10.3",
+        "django-wysiwyg-redactor==0.4.9.1",
+    ],
+    extras_require={
+        "modeltranslation": ["django-modeltranslation==0.12", ],
+    },
+    author="Alexei Andrushievich",
+    author_email="vint21h@vint21h.pp.ua",
+    description="Django reusable application providing page help",
+    license="MIT",
+    url="https://github.com/DCOD-OpenSource/django-simple-help/",
+    download_url="https://github.com/DCOD-OpenSource/django-simple-help/archive/{version}.tar.gz".format(**{"version": __version__, }),
+    zip_safe=False,
+    include_package_data=True,
+    classifiers=[
+        "Environment :: Plugins",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: Unix",
+        "Programming Language :: Python :: 2.6",
+        "Programming Language :: Python :: 2.7",
+        "Topic :: Utilities",
+    ]
+)
---
+++ packages/django-simple-help/django-simple-help-0.5.4.tar.gz/django-simple-help-0.5.4/simple_help/templatetags/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+# django-simple-help
+# simple_help/templatetags/__init__.py
+
+from __future__ import unicode_literals
+
+
+__all__ = [
+    "help_tags",
+]

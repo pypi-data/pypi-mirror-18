94437a5bad308fb8b8d0fcb7aa0cd7434e74e90e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:44:50 +0000

    Add package yagocd/0.4.3/yagocd-0.4.3.tar.gz

---
+++ packages/yagocd/yagocd-0.4.3.tar.gz/yagocd-0.4.3/yagocd/resources/artifact.py
@@ -0,0 +1,415 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+###############################################################################
+# The MIT License
+#
+# Copyright (c) 2016 Grigory Chernyshev
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+#
+###############################################################################
+
+import time
+
+from yagocd.resources import Base, BaseManager
+from yagocd.util import since
+
+
+@since('14.3.0')
+class ArtifactManager(BaseManager):
+    """
+    The artifacts API allows users to query and create artifacts of a job.
+
+    `Official documentation. <https://api.go.cd/current/#artifacts>`_
+
+    :versionadded: 14.3.0.
+    """
+
+    def __init__(
+        self,
+        session,
+        pipeline_name=None,
+        pipeline_counter=None,
+        stage_name=None,
+        stage_counter=None,
+        job_name=None
+    ):
+        """
+        Constructs instance of ``ArtifactManager``.
+        Parameters to the constructor and methods of the class could be duplicated. That is because of two use cases
+        of this class:
+            1. When the class being instantiated from :class:`yagocd.client.Client`, we don't know all the necessary
+             parameters yet, but we need an instance to work with. So we skip parameters instantiation in constructor,
+             but require them for each method.
+            2. When the class being used from :class:`yagocd.resources.job.JobInstance` - in this case we already
+            know all required parameters, so we can instantiate `ArtifactManager` with them.
+
+        :param session: session object from client.
+        :type session: yagocd.session.Session.
+        :param pipeline_name: name of the pipeline.
+        :param pipeline_counter: pipeline counter.
+        :param stage_name: name of the stage.
+        :param stage_counter: stage counter.
+        :param job_name: name of the job.
+        """
+        super(ArtifactManager, self).__init__(session)
+
+        self.base_api = self._session.base_api(api_path='')
+
+        self._pipeline_name = pipeline_name
+        self._pipeline_counter = pipeline_counter
+        self._stage_name = stage_name
+        self._stage_counter = stage_counter
+        self._job_name = job_name
+
+    def __iter__(self):
+        """
+        Method for iterating over all artifacts.
+
+        :return: An array of :class:`yagocd.resources.artifact.Artifact`.
+        :rtype: list of yagocd.resources.artifact.Artifact
+        """
+        return iter(self.list())
+
+    def __getitem__(self, path):
+        """
+        Method for downloading artifact or directory zip by given path.
+
+        :param path: path to the file or directory zip
+        :return: the contents of the file you requested or
+        directory contents in the form of a zip file.
+        """
+        return self.directory_wait(path=path)
+
+    def list(
+        self,
+        pipeline_name=None,
+        pipeline_counter=None,
+        stage_name=None,
+        stage_counter=None,
+        job_name=None
+    ):
+        """
+        Lists all available artifacts in a job.
+
+        :versionadded: 14.3.0.
+
+        :param pipeline_name: name of the pipeline.
+        :param pipeline_counter: pipeline counter.
+        :param stage_name: name of the stage.
+        :param stage_counter: stage counter.
+        :param job_name: name of the job.
+        :return: An array of :class:`yagocd.resources.artifact.Artifact`.
+        :rtype: list of yagocd.resources.artifact.Artifact
+        """
+        assert self._pipeline_name or pipeline_name
+        assert self._pipeline_counter or pipeline_counter
+        assert self._stage_name or stage_name
+        assert self._stage_counter or stage_counter
+        assert self._job_name or job_name
+
+        response = self._session.get(
+            path=(
+                '{base_api}'
+                '/files'
+                '/{pipeline_name}'
+                '/{pipeline_counter}'
+                '/{stage_name}'
+                '/{stage_counter}'
+                '/{job_name}.json'
+            ).format(
+                base_api=self.base_api,
+                pipeline_name=self._pipeline_name or pipeline_name,
+                pipeline_counter=self._pipeline_counter or pipeline_counter,
+                stage_name=self._stage_name or stage_name,
+                stage_counter=self._stage_counter or stage_counter,
+                job_name=self._job_name or job_name
+            ),
+        )
+        artifacts = list()
+        for data in response.json():
+            artifacts.append(Artifact(session=self._session, data=data))
+
+        return artifacts
+
+    def file(
+        self,
+        path,
+        pipeline_name=None,
+        pipeline_counter=None,
+        stage_name=None,
+        stage_counter=None,
+        job_name=None,
+    ):
+        """
+        Gets an artifact file by its path.
+
+        :versionadded: 14.3.0.
+
+        :note: The `path_to_file` can be a nested file for e.g. `dist/foobar-widgets-1.2.0.jar`.
+
+        :param path: path to the file.
+        :param pipeline_name: name of the pipeline.
+        :param pipeline_counter: pipeline counter.
+        :param stage_name: name of the stage.
+        :param stage_counter: stage counter.
+        :param job_name: name of the job.
+        :return: the contents of the file you requested.
+        """
+        return self.directory(
+            path=path,
+            pipeline_name=pipeline_name,
+            pipeline_counter=pipeline_counter,
+            stage_name=stage_name,
+            stage_counter=stage_counter,
+            job_name=job_name
+        )
+
+    def directory(
+        self,
+        path,
+        pipeline_name=None,
+        pipeline_counter=None,
+        stage_name=None,
+        stage_counter=None,
+        job_name=None,
+    ):
+        """
+        Gets an artifact directory by its path.
+
+        :versionadded: 14.3.0.
+
+        :note: The `path` can be a nested directory for e.g. target/dist.zip
+
+        :warning: Since it may take an undetermined amount of time to compress \
+        a directory, the server may return a `202 Accepted` code to indicate that \
+        it is compressing the requested directory. \
+        Users are expected to poll the url every few seconds to check if the \
+        directory is available.
+
+        :param path: path to directory.
+        :param pipeline_name: name of the pipeline.
+        :param pipeline_counter: pipeline counter.
+        :param stage_name: name of the stage.
+        :param stage_counter: stage counter.
+        :param job_name: name of the job.
+        :return:
+            * A status code `202 Accepted` to acknowledge your request to compress
+              the contents of the requested directory.
+            * The requested directory contents in the form of a zip file.
+        """
+        assert self._pipeline_name or pipeline_name
+        assert self._pipeline_counter or pipeline_counter
+        assert self._stage_name or stage_name
+        assert self._stage_counter or stage_counter
+        assert self._job_name or job_name
+
+        response = self._session.get(
+            path=(
+                '{base_api}'
+                '/files'
+                '/{pipeline_name}'
+                '/{pipeline_counter}'
+                '/{stage_name}'
+                '/{stage_counter}'
+                '/{job_name}'
+                '/{path_to_folder}').format(
+                base_api=self.base_api,
+                pipeline_name=self._pipeline_name or pipeline_name,
+                pipeline_counter=self._pipeline_counter or pipeline_counter,
+                stage_name=self._stage_name or stage_name,
+                stage_counter=self._stage_counter or stage_counter,
+                job_name=self._job_name or job_name,
+                path_to_folder=path
+            ),
+        )
+
+        if response.status_code == 202:
+            return None
+
+        return response.content
+
+    def directory_wait(
+        self,
+        path,
+        timeout=60,
+        backoff=0.4,
+        max_wait=4,
+        pipeline_name=None,
+        pipeline_counter=None,
+        stage_name=None,
+        stage_counter=None,
+        job_name=None,
+    ):
+        """
+        Gets an artifact directory by its path.
+        This method wraps original `directory` method, adding
+        timeout to wait  for directory to be available.
+
+        :versionadded: 14.3.0.
+
+        :param path: path to directory.
+        :param timeout: timeout in seconds to wait for directory.
+        :param backoff: backoff value.
+        :param max_wait: maximum wait amount.
+        :param pipeline_name: name of the pipeline.
+        :param pipeline_counter: pipeline counter.
+        :param stage_name: name of the stage.
+        :param stage_counter: stage counter.
+        :param job_name: name of the job.
+        :return: The requested directory contents in the form of a zip file.
+        """
+        start_time = time.time()
+        time_elapsed = 0
+        counter = 0
+        directory_zip = None
+
+        while time_elapsed < timeout:
+            directory_zip = self.directory(path, pipeline_name, pipeline_counter, stage_name, stage_counter, job_name)
+            if directory_zip is not None:
+                break
+
+            time.sleep(min(backoff * (2 ** counter), max_wait))
+            counter += 1
+            time_elapsed = time.time() - start_time
+
+        return directory_zip
+
+    def create(
+        self,
+        path,
+        filename,
+        pipeline_name=None,
+        pipeline_counter=None,
+        stage_name=None,
+        stage_counter=None,
+        job_name=None,
+    ):
+        """
+        Uploads a local file as an artifact.
+
+        :versionadded: 14.3.0.
+
+        :param path: path to the file within job directory.
+        :param filename: the contents file to be uploaded.
+        :param pipeline_name: name of the pipeline.
+        :param pipeline_counter: pipeline counter.
+        :param stage_name: name of the stage.
+        :param stage_counter: stage counter.
+        :param job_name: name of the job.
+        :return: an acknowledgement that the file was created.
+        """
+        assert self._pipeline_name or pipeline_name
+        assert self._pipeline_counter or pipeline_counter
+        assert self._stage_name or stage_name
+        assert self._stage_counter or stage_counter
+        assert self._job_name or job_name
+
+        response = self._session.post(
+            path=(
+                '{base_api}'
+                '/files'
+                '/{pipeline_name}'
+                '/{pipeline_counter}'
+                '/{stage_name}'
+                '/{stage_counter}'
+                '/{job_name}'
+                '/{path_to_file}'
+            ).format(
+                base_api=self.base_api,
+                pipeline_name=self._pipeline_name or pipeline_name,
+                pipeline_counter=self._pipeline_counter or pipeline_counter,
+                stage_name=self._stage_name or stage_name,
+                stage_counter=self._stage_counter or stage_counter,
+                job_name=self._job_name or job_name,
+                path_to_file=path
+            ),
+            files={'file': open(filename, 'rb')},
+            headers={
+                'Confirm': 'true'
+            },
+        )
+
+        return response.text
+
+    def append(
+        self,
+        path,
+        filename,
+        pipeline_name=None,
+        pipeline_counter=None,
+        stage_name=None,
+        stage_counter=None,
+        job_name=None,
+    ):
+        """
+        Appends a local file to an existing artifact.
+
+        :versionadded: 14.3.0.
+
+        :param path: path to the file within job directory.
+        :param filename: the contents file to be uploaded.
+        :param pipeline_name: name of the pipeline.
+        :param pipeline_counter: pipeline counter.
+        :param stage_name: name of the stage.
+        :param stage_counter: stage counter.
+        :param job_name: name of the job.
+        :return: an acknowledgement that the file was created.
+        """
+        assert self._pipeline_name or pipeline_name
+        assert self._pipeline_counter or pipeline_counter
+        assert self._stage_name or stage_name
+        assert self._stage_counter or stage_counter
+        assert self._job_name or job_name
+
+        response = self._session.put(
+            path=(
+                '{base_api}'
+                '/files'
+                '/{pipeline_name}'
+                '/{pipeline_counter}'
+                '/{stage_name}'
+                '/{stage_counter}'
+                '/{job_name}'
+                '/{path_to_file}'
+            ).format(
+                base_api=self.base_api,
+                pipeline_name=self._pipeline_name or pipeline_name,
+                pipeline_counter=self._pipeline_counter or pipeline_counter,
+                stage_name=self._stage_name or stage_name,
+                stage_counter=self._stage_counter or stage_counter,
+                job_name=self._job_name or job_name,
+                path_to_file=path
+            ),
+            files={'file': open(filename, 'rb')}
+        )
+
+        return response.text
+
+
+class Artifact(Base):
+    def files(self):
+        return [ArtifactFile(session=self._session, data=data) for data in self.data.files]
+
+
+class ArtifactFile(Base):
+    def fetch(self):
+        response = self._session.get(self.data.url)
+        return response.content

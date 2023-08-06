79157f8761ce153d30645850d512e534bb3ee875
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:19:52 +0000

    Add package colorcet/0.9.1/colorcet-0.9.1.tar.gz

---
+++ packages/colorcet/colorcet-0.9.1.tar.gz/colorcet-0.9.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='colorcet',
+      version='0.9.1',
+      description='Collection of perceptually uniform colormaps',
+      url='http://github.com/bokeh/colorcet',
+      maintainer='James A. Bednar',
+      maintainer_email='jbednar@continuum.io',
+      packages=['colorcet'])

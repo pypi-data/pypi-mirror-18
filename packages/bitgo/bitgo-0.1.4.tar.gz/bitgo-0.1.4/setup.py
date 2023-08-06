e444a8a7ece7cc56074c3d907d7c9a46bae8bbac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:46:14 +0000

    Add package bitgo/0.1.4/bitgo-0.1.4.tar.gz

---
+++ packages/bitgo/bitgo-0.1.4.tar.gz/bitgo-0.1.4/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+
+version = "0.1.4"
+
+setup(
+    name='bitgo',
+    packages=['bitgo'],
+    version=version,
+    description='alpha version of a bitgo python library',
+    author='Sebastian Serrano',
+    author_email='sebastian@bitpagos.com',
+    entry_points={
+        'console_scripts':
+            [
+                'bitgo = bitgo.cmd:main',
+            ]
+    },
+    url='https://github.com/sserrano44/pybitgo',
+    download_url='https://github.com/sserrano44/pybitgo/tarball/%s' % version,
+    keywords=['bitcoin', 'bitgo'],
+    classifiers=[
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Internet',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+    install_requires=["pycryptodome", "requests", "pycoin"]
+)

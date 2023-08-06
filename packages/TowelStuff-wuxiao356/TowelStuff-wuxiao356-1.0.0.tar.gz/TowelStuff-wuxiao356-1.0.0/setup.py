1cb9e1910425f8deea862fe03f34a5aa0379c740
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:07:41 +0000

    Add package TowelStuff-wuxiao356/1.0.0/TowelStuff-wuxiao356-1.0.0.tar.gz

---
+++ packages/TowelStuff-wuxiao356/TowelStuff-wuxiao356-1.0.0.tar.gz/TowelStuff-wuxiao356-1.0.0/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name="TowelStuff-wuxiao356",
+    version="1.0.0",
+    packages=["towelstuff"],
+    license="Creative Commons Attribution-Noncommercial-Share Alike license",
+    long_description=open("README.txt").read(),
+    url='http://www.wuxiao356.com',
+    author='Yu Wu',
+    author_email='wuxiao356@gmail.com'
+)

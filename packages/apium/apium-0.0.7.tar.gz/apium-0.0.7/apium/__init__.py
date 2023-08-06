c48bd81b16cd9d98fe3b2bc15563270d79687663
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:50:36 +0000

    Add package apium/0.0.7/apium-0.0.7.tar.gz

---
+++ packages/apium/apium-0.0.7.tar.gz/apium-0.0.7/apium/__init__.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+
+"""
+.. module: apium
+    :synopsis: User facing imports.
+"""
+
+
+from concurrent.futures import (
+    wait,
+    as_completed,
+    FIRST_COMPLETED,
+    FIRST_EXCEPTION,
+    ALL_COMPLETED,
+)
+from .exceptions import *   # pylint: disable=W0401
+from .executor import TaskExecutor, Future
+from .worker import register_task, schedule_task
+
+# TODO:
+# - Authentication
+# - Encryption
+# - Private tasks
+# - More error checking:
+#   - Problems unpickling task on the server.
+#   - Problems unpickling results on the client.
+#   - Chaining task to non existent parent.
+# - Possible to backport a better chaining implementation to concurrent.futures??
+#   - Probably not. Would need a reference to the executor in the future.

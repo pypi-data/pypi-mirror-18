b0ef250ba86e9fb75a876b405ef68ef25aefede3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:18:36 +0000

    Add package nester_elmotionfitte/1.4.1/nester_elmotionfitte-1.4.1.tar.gz

---
+++ packages/nester_elmotionfitte/nester_elmotionfitte-1.4.1.tar.gz/nester_elmotionfitte-1.4.1/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup (
+		name 			= 'nester_elmotionfitte',
+		version 		= '1.4.1',
+		py_modules		= ['nester'],
+		author			= 'elmotionfitte',
+		author_email	= 'taparaidhamkih@gmail.com',
+		url				= '',
+		description		= 'a simple printer of nested lists with 4 arguments',
+	  )

94633c7bd31f07f98cd43ba35abea48f377c2fc9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:21:05 +0000

    Add package kallankallan_nester/1.2.0/kallankallan_nester-1.2.0.zip

---
+++ packages/kallankallan_nester/kallankallan_nester-1.2.0.zip/kallankallan_nester-1.2.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(
+            name               = 'kallankallan_nester',
+            version            = '1.2.0',
+            py_modules   = ['nester'], 
+            author             = 'wing',
+            author_email = 'xxx@qq.com',
+            url                    = 'http://www.qq.com',
+            description    = 'A simple printer of nested lists',
+      )

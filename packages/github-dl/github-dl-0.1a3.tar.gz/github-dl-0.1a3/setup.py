37590aca449778007b8fb0b85466a570144477dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:00 +0000

    Add package github-dl/0.1a3/github-dl-0.1a3.tar.gz

---
+++ packages/github-dl/github-dl-0.1a3.tar.gz/github-dl-0.1a3/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup
+
+setup(
+    name='github-dl',
+    version='0.1a3',
+    description='a command line tool for downloading repositories and gists',
+    url='https://github.com/cameres/github-dl',
+    author='Connor Ameres',
+    author_email='connorameres@gmail.com',
+    license='Mozilla Public License 2.0 (MPL 2.0)',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: Mozilla Public License 2.0 (MPL 2.0)',
+        'Topic :: Software Development',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.5'
+    ],
+    keywords='github gists download',
+    # packages=['download'],
+    install_requires=[
+        'click',
+        'pygithub',
+        'gitpython',
+        'requests'
+    ],
+    entry_points='''
+        [console_scripts]
+        github-dl=github_dl:github_dl
+        gist-dl=github_dl:gist_dl
+    '''
+)

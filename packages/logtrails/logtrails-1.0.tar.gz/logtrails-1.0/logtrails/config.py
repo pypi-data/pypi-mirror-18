808410a922fed0fb3856a681a6af86784c57cf77
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:45:24 +0000

    Add package logtrails/1.0/logtrails-1.0.tar.gz

---
+++ packages/logtrails/logtrails-1.0.tar.gz/logtrails-1.0/logtrails/config.py
@@ -0,0 +1,200 @@
+"""
+Configuration files for logtrails parsers
+"""
+
+import os
+import re
+import sre_constants
+
+from collections import OrderedDict
+from configobj import ConfigObj, ConfigObjError
+
+from logtrails.logreader import LogTrailReaderProcess
+from logtrails.queue import RedisConnection
+
+DEFAULT_CONFIGURATION_FILE = '~/.logtrails.cfg'
+
+DEFAULT_PARSER_CONFIG = {
+    'channel': 'logtrails',
+}
+
+class ConfigurationError(Exception):
+    pass
+
+
+class QueueConfiguration(object):
+    """Queue configuration
+
+    """
+    def __init__(self, hostname, port, channels=[]):
+        self.hostname = hostname
+        self.port = int(port)
+        if isinstance(channels, str):
+            channels = [channels]
+        self.channels = channels
+
+    def __repr__(self):
+        return '{0}:{1}'.format(self.hostname, self.port)
+
+
+class LogPatternConfiguration(object):
+    """Log pattern
+
+    Parse and regexp matcher to object for log readers
+    """
+    def __init__(self, group, name, pattern):
+        self.group = group
+        self.name = name
+        self.regexp = re.compile(pattern)
+
+    def __getattr__(self, attr):
+        return getattr(self.regexp, attr)
+
+    @property
+    def channel(self):
+        """Redis pubsub channel
+
+        """
+        return self.group.parser.settings.get('channel', DEFAULT_PARSER_CONFIG['channel'])
+
+
+class LogPatternGroupConfiguration(object):
+    """Pattern group configuration
+
+    Group of patterns for a log parser
+    """
+    def __init__(self, parser, name, patterns):
+        self.parser = parser
+        self.name = name
+
+        self.pattern_map = OrderedDict()
+        for key, pattern in patterns.items():
+            if key not in self.pattern_map:
+                self.pattern_map[key] = []
+            try:
+                self.pattern_map[key].append(LogPatternConfiguration(self, key, pattern))
+            except sre_constants.error as e:
+                print 'WARNING: {0} {1} invalid regexp pattern {2}: {3}'.format(self.parser, name, pattern, e)
+
+    def __repr__(self):
+        return '{0}'.format(self.name)
+
+    @property
+    def patterns(self):
+        """Return all patterns
+
+        """
+        patterns = []
+        for key in self.pattern_map:
+            patterns.extend(self.pattern_map[key])
+        return patterns
+
+
+class LogParserConfiguration(object):
+    """Patterns configuration
+
+    """
+    def __init__(self, path, patterns_config):
+        self.path = path
+        self.settings = {}
+        self.patterngroups = OrderedDict()
+
+        try:
+            config = ConfigObj(patterns_config, list_values=False)
+        except ConfigObjError as e:
+            raise ConfigurationError('Error reading {0}: {1}'.format(patterns_config, e))
+
+        if 'settings' in config:
+            for key in DEFAULT_PARSER_CONFIG:
+                self.settings[key] = config['settings'].get(key, DEFAULT_PARSER_CONFIG[key])
+        else:
+            self.settings = DEFAULT_PARSER_CONFIG.copy()
+
+        for key, patterns in config.items():
+            if key in ( 'settings', ):
+                continue
+            self.patterngroups[key] = LogPatternGroupConfiguration(self, key, patterns)
+
+    def __repr__(self):
+        return '{0}'.format(self.path)
+
+    @property
+    def patterns(self):
+        """Return configured patterns
+
+        """
+        patterns = []
+        for name in self.patterngroups:
+            patterns.extend(self.patterngroups[name].patterns)
+        return patterns
+
+
+class Configuration(object):
+    """Logtrails configuration
+
+    """
+
+    def __init__(self, path=DEFAULT_CONFIGURATION_FILE):
+        self.path = os.path.expandvars(os.path.expanduser(path))
+        self.queue_configuration = None
+        self.handlers = []
+
+    @property
+    def patterns(self):
+        """All configured pattern groups
+
+        Return all known pattern groups, regardless of associated logfile
+        """
+        patterns = []
+        for handler in self.handlers:
+            patterns.extend(handler.patterns)
+        return patterns
+
+    def load(self):
+        """Load configuration
+
+        """
+        if not os.path.isfile(self.path):
+            raise ConfigurationError('No such file: {0}'.format(self.path))
+
+        if not os.access(self.path, os.R_OK):
+            raise ConfigurationError('Permission denied: {0}'.format(self.path))
+
+        try:
+            config = ConfigObj(self.path)
+        except Exception as e:
+            raise ConfigurationError('Error parsing {0}: {1}'.format(path, e))
+
+        try:
+            self.queue_configuration = QueueConfiguration(**config['queue'])
+        except KeyError:
+            raise ConfigurationError('Missing queue configuration: {0}'.format(path))
+
+        if 'parsers' in config:
+            for key in config['parsers']:
+                if not isinstance(config['parsers'][key], list):
+                    config['parsers'][key] = [config['parsers'][key]]
+                for patterns_config in config['parsers'][key]:
+                    self.handlers.append(LogParserConfiguration(key, patterns_config))
+        else:
+            raise ConfigurationError('Missing log parser configuration: {0}'.format(path))
+
+    def get_redis_connection(self):
+        """Return configured redis connection
+
+        """
+        return RedisConnection(
+            hostname=self.queue_configuration.hostname,
+            port=self.queue_configuration.port,
+            channels=self.queue_configuration.channels,
+        )
+
+    def configure_readers(self, callback):
+        """Configure log readers processes
+
+        Return log reader processes for configured parsers
+        """
+        processes = []
+        for handler in self.handlers:
+            processes.append(LogTrailReaderProcess(handler.path, handler.patterns, callback))
+        return processes
---
+++ packages/logtrails/logtrails-1.0.tar.gz/logtrails-1.0/logtrails/logreader.py
@@ -0,0 +1,154 @@
+"""
+Log file readers
+
+Implements classes to read logs
+"""
+
+import json
+import re
+
+from datetime import datetime
+from multiprocessing import Process
+from systematic.log import LogEntry, LogFile, LogfileTailReader, LogFileError
+
+LOGFILE_DATE_PREFIX_PARSERS = (
+    ( re.compile('^(?P<datetime>[^\s]+\s+\d+\s+\d+:\d+:\d+) (?P<line>.*)'), '%b %d %H:%M:%S', ),
+    ( re.compile('^(?P<datetime>\d+/[^/]+/\d+:\d+:\d+:\d+) (?P<tz>\+\d+) (?P<line>.*)'), '%d/%b/%Y:%H:%M:%S', ),
+)
+
+class JSONDataEncoder(json.JSONEncoder):
+    """JSON encoder
+
+    JSON encoder with datetime to isoformat() conversion
+    """
+    def default(self, o):
+        if isinstance(o, datetime):
+            return o.isoformat()
+        return json.JSONEncoder.default(self, o)
+
+
+class LogEntryParser(dict):
+    """Log entry
+
+    Log entry processor for LogTrailReader and LogTrailLog
+    """
+    def __init__(self, reader, line, year):
+
+        line = line.rstrip()
+        self.reader = reader
+        self.line = line
+        self.message_fields = {}
+
+        self['time'], line = self.__parse_date__(line)
+        self['filename'] = self.reader.path
+        self.message = line
+
+        for pattern in self.reader.patterns:
+            m = pattern.match(line)
+            if m:
+                self.channel = pattern.channel
+                self['group'] = pattern.group.name
+                self['label'] = pattern.name
+                self.update(m.groupdict().items())
+                return
+
+        raise LogFileError('Entry does not match patterns: {0}'.format(line))
+
+    def __parse_date__(self, line):
+        """Parse datetime prefix
+
+        Parse known date/time prefix formats from line
+        """
+        for pattern, datefmt in LOGFILE_DATE_PREFIX_PARSERS:
+            m = pattern.match(line)
+            if m:
+                value = datetime.strptime(m.groupdict()['datetime'], datefmt)
+                return value, m.groupdict()['line']
+
+        raise LogFileError('Error parsing entry time from line: {0}'.format(self.line))
+
+    def append(self, message):
+        self.message = '{0}\n{1}'.format(self.message, message.rstrip())
+
+    def to_json(self):
+        return json.dumps(self, indent=2, cls=JSONDataEncoder)
+
+
+class LogTrailLogFile(LogFile):
+    """Logfile for trails
+
+    Parser for offline logs
+    """
+    lineloader = LogEntryParser
+
+    def __init__(self, path, patterns):
+        super(LogTrailLogFile, self).__init__(path)
+        self.patterns = patterns
+
+    def readline(self):
+        """Read line from log
+
+        Parse entry from logfile
+        """
+
+        if self.fd is None:
+            raise LogFileError('File is not loaded')
+
+        try:
+            line = self.fd.readline()
+            if line == '':
+                self.__loaded = True
+                return None
+
+            # Multiline log entry
+            if line[:1] in [' ', '\t'] and len(self):
+                self[-1].append(line)
+                return self.readline()
+
+            else:
+                entry = self.lineloader(self, line, year=self.mtime.year)
+                self.append(entry)
+                return entry
+
+        except OSError as e:
+            raise LogFileError('Error reading file {0}: {1}'.format(self.path, e))
+
+
+class LogTrailReader(LogfileTailReader):
+    """Logfile parser
+
+    Tail reader for one logfile with parser patterns as specified in config
+    """
+    lineparser = LogEntryParser
+
+    def __init__(self, path, patterns):
+        super(LogTrailReader, self).__init__(path)
+        self.patterns = patterns
+
+    def __format_line__(self, line):
+        """Format line
+
+        Formats line as log entry. Returns None if entry is not supported
+        """
+        return self.lineparser(self, line, self.year)
+
+
+class LogTrailReaderProcess(Process):
+    """Python multiprocessing process for LogTrailReader
+
+    Runs with LogTrailReader, processing new log entries and runs callback
+    with result entry as JSON
+    """
+    def __init__(self, path, patterns, callback, **kwargs):
+        super(LogTrailReaderProcess, self).__init__(**kwargs)
+        self.reader = LogTrailReader(path, patterns)
+        self.callback = callback
+
+    def run(self, *args, **kwargs):
+        """Run log reader
+
+        Seek to end of input file and start processing new entries
+        """
+        self.reader.seek_to_end()
+        while True:
+            self.callback(self.reader.next())

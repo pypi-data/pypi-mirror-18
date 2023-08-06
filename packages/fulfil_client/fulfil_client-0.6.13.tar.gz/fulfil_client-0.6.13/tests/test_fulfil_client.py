f032fa4c663111d82959f6a62ac113e99ce283dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:09:30 +0000

    Add package fulfil_client/0.6.13/fulfil_client-0.6.13.tar.gz

---
+++ packages/fulfil_client/fulfil_client-0.6.13.tar.gz/fulfil_client-0.6.13/tests/test_fulfil_client.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+test_fulfil_client
+----------------------------------
+
+Tests for `fulfil_client` module.
+"""
+import pytest
+from fulfil_client import ServerError
+
+
+def test_find(client):
+    IRModel = client.model('ir.model')
+    ir_models = IRModel.find([])
+    assert len(ir_models) > 0
+    assert ir_models[0]['id']
+    assert ir_models[0]['rec_name']
+
+
+def test_find_no_filter(client):
+    IRModel = client.model('ir.model')
+    ir_models = IRModel.find()
+    assert len(ir_models) > 0
+    assert ir_models[0]['id']
+    assert ir_models[0]['rec_name']
+
+
+def test_raises_server_error(client):
+    Model = client.model('ir.model')
+    with pytest.raises(ServerError):
+        Model.search(1)

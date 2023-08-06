4d6ad8a23297e381e3181afaa6afccc234fe7571
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:14:29 +0000

    Add package t77-date/0.5.0/t77-date-0.5.0.tar.gz

---
+++ packages/t77-date/t77-date-0.5.0.tar.gz/t77-date-0.5.0/setup.py
@@ -0,0 +1,35 @@
+from codecs import open
+
+from setuptools import setup, find_packages
+
+
+setup(
+    name="t77-date",
+    version='0.5.0',
+    author="Tomasz Jakub Rup",
+    author_email="tomasz.rup@gmail.com",
+    url='https://github.com/tomi77/python-t77-date',
+    description='A set of functions related with dates',
+    long_description=open("README.rst").read(),
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Topic :: Software Development',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Utilities',
+    ],
+    license='MIT',
+    packages=find_packages(exclude=('tests',)),
+    install_requires=['six'],
+    test_suite='tests'
+)
---
+++ packages/t77-date/t77-date-0.5.0.tar.gz/t77-date-0.5.0/t77_date/datetime.py
@@ -0,0 +1,99 @@
+"""datetime.datetime and datetime.date related functions"""
+from __future__ import absolute_import
+from datetime import timedelta, date, datetime
+
+
+def start_of_day(val):
+    """
+    Return a new datetime with values that represent a start of a day.
+    :param val: Date to ...
+    :type val: datetime.datetime | datetime.date
+    :rtype: datetime.datetime
+    """
+    if type(val) == date:
+        val = datetime.fromordinal(val.toordinal())
+    return val.replace(hour=0, minute=0, second=0, microsecond=0)
+
+
+def end_of_day(val):
+    """
+    Return a new datetime with values that represent a end of a day.
+    :param val: Date to ...
+    :type val: datetime.datetime | datetime.date
+    :rtype: datetime.datetime
+    """
+    if type(val) == date:
+        val = datetime.fromordinal(val.toordinal())
+    return start_of_day(val) + timedelta(days=1, microseconds=-1)
+
+
+def start_of_month(val):
+    """
+    Return a new datetime with values that represent a start of a month.
+    :param val: Date to ...
+    :type val: datetime.datetime | datetime.date
+    :rtype: datetime.datetime
+    """
+    if type(val) == date:
+        val = datetime.fromordinal(val.toordinal())
+    return start_of_day(val).replace(day=1)
+
+
+def end_of_month(val):
+    """
+    Return a new datetime with values that represent a end of a month.
+    :param val: Date to ...
+    :type val: datetime.datetime | datetime.date
+    :rtype: datetime.datetime
+    """
+    if type(val) == date:
+        val = datetime.fromordinal(val.toordinal())
+    if val.month == 12:
+        return start_of_month(val).replace(year=val.year + 1, month=1) \
+               - timedelta(microseconds=1)
+    else:
+        return start_of_month(val).replace(month=val.month + 1) \
+               - timedelta(microseconds=1)
+
+
+def _set_week_day(val, week_day, val_weekday, sign):
+    if val_weekday == week_day:
+        return val
+
+    diff = sign * (val_weekday - week_day)
+    diff = -diff if diff < 0 else 7 - diff
+    val += sign * timedelta(days=diff)
+
+    return val
+
+
+def set_next_week_day(val, week_day, iso=False):
+    """
+    Set week day.
+    New date will be greater or equal than input date.
+    :param val: datetime or date
+    :type val: datetime.datetime | datetime.date
+    :param week_day: Week day to set
+    :type week_day: int
+    :param iso: week_day in ISO format, or not
+    :type iso: bool
+    :return: datetime.datetime | datetime.date
+    """
+    return _set_week_day(val, week_day,
+                         val.isoweekday() if iso else val.weekday(), sign=1)
+
+
+def set_prev_week_day(val, week_day, iso=False):
+    """
+    Set week day.
+    New date will be less or equal than input date.
+    :param val: datetime or date
+    :type val: datetime.datetime | datetime.date
+    :param week_day: Week day to set
+    :type week_day: int
+    :param iso: week_day in ISO format, or not
+    :type iso: bool
+    :return: datetime.datetime | datetime.date
+    """
+    return _set_week_day(val, week_day,
+                         val.isoweekday() if iso else val.weekday(), sign=-1)

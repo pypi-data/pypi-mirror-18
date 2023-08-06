d2d259272c537e40fe9dbd7e6b90cfaf122460cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:51:34 +0000

    Add package hhic/0.9.5/hhic-0.9.5.tar.gz

---
+++ packages/hhic/hhic-0.9.5.tar.gz/hhic-0.9.5/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(name='hhic',
+      version='0.9.5',
+      description='(homomorphic hash integrity checker) integrity checker for homomorphic ciphers with homomorphic hashes',
+      url='',
+      author='Samim Faiz',
+      author_email='mhsamim.faiz@gmail.com',
+      packages=['hhic'],
+      install_requires=[
+          'gensafeprime==1.5', 'rsa==3.4.2', 'phe==1.2.3',
+      ],
+      zip_safe=False)

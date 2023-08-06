cc4c6d21c86aed9ff0bc8c816e4ac60a6d126017
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:06:28 +0000

    Add package quiver_engine/0.1.3.2.2/quiver_engine-0.1.3.2.2.tar.gz

---
+++ packages/quiver_engine/quiver_engine-0.1.3.2.2.tar.gz/quiver_engine-0.1.3.2.2/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='quiver_engine',
+    version="0.1.3.2.2",
+    author="Jake Bian",
+    author_email="jake@keplr.io",
+    description=("Interactive per-layer visualization for convents in keras"),
+    license='mit',
+    packages=find_packages(),
+    include_package_data=True,
+    package_data={'quiver_engine': 'quiverboard/dist'},
+    install_requires=[
+        'keras',
+        'flask',
+        'flask_cors',
+        'gevent',
+        'numpy',
+        'pillow'
+    ]
+)

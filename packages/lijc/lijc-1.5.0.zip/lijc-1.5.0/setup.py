2260ef6117bfc63b9c9bbc4b332d54cb997cb04d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:21:57 +0000

    Add package lijc/1.5.0/lijc-1.5.0.zip

---
+++ packages/lijc/lijc-1.5.0.zip/lijc-1.5.0/setup.py
@@ -0,0 +1,11 @@
+﻿from distutils.core import setup
+
+setup(
+		name         = 'lijc',
+		version			 = '1.5.0',
+		py_modules   = ['lijc'],
+		author       = 'lijc',
+		author_email = 'lijc@headfirstlabs.com',
+		url          = 'http://www.headfirstlabs.com',
+		description  = 'A simple printer of nested lists',
+	)

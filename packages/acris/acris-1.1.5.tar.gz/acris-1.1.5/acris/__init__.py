7ca247b45d540aa2edb9847c62e5b01620ce5603
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:12:22 +0000

    Add package acris/1.1.5/acris-1.1.5.tar.gz

---
+++ packages/acris/acris-1.1.5.tar.gz/acris-1.1.5/acris/__init__.py
@@ -0,0 +1,8 @@
+__version__ = "1.1.5"
+
+from .threaded import threaded
+from .singleton import Singleton
+from .sequence import Sequence
+from .timed_sized_logging_handler import TimedSizedRotatingHandler
+from .mplogger import MpLogger
+from .data_types import MergedChainedDict

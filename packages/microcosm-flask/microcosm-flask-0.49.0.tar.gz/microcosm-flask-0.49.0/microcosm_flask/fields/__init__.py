33ad6a74a7f6979cde11d6d1433848ea16e4a370
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:19:54 +0000

    Add package microcosm-flask/0.49.0/microcosm-flask-0.49.0.tar.gz

---
+++ packages/microcosm-flask/microcosm-flask-0.49.0.tar.gz/microcosm-flask-0.49.0/microcosm_flask/fields/__init__.py
@@ -0,0 +1,8 @@
+"""
+Custom fields.
+
+"""
+from microcosm_flask.fields.enum_field import EnumField  # noqa: F401
+from microcosm_flask.fields.query_string_list import QueryStringList  # noqa: F401
+from microcosm_flask.fields.timestamp_field import TimestampField  # noqa: F401
+from microcosm_flask.fields.uri_field import URIField  # noqa: F401

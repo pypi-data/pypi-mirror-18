ea96c9cd56fd2bdb1b12020ac27e21a04789da67
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:45:23 +0000

    Add package mpy-repl-tool/0.1/mpy_repl_tool-0.1-py3-none-any.whl

---
+++ packages/mpy-repl-tool/mpy_repl_tool-0.1-py3-none-any.whl/there/__main__.py
@@ -0,0 +1,289 @@
+#! /usr/bin/env python3
+# encoding: utf-8
+#
+# (C) 2016 Chris Liechti <cliechti@gmx.net>
+#
+# SPDX-License-Identifier:    BSD-3-Clause
+import posixpath
+import os
+import sys
+import stat
+import time
+import glob
+import serial.tools.list_ports
+from .speaking import nice_bytes, mode_to_chars
+from .string_escape import escaped
+from . import repl_connection
+from . import miniterm_mpy
+
+
+def make_connection(args, port=None):
+    """make a conenction, port overrides args.port"""
+    m = repl_connection.MicroPythonRepl(port or args.port,
+                                        args.baudrate,
+                                        user=args.user,
+                                        password=args.password)
+    m.protocol.verbose = args.verbose > 1
+    return m
+
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+def command_detect(m, args):
+    """\
+    Help finding MicroPython boards.
+
+    By default it simply lists all serial ports. If --test is used, each of
+    the ports is opened (with the given --baudrate) and tested for a Python
+    prompt. If there is no response it runs in a timeout, so this option is
+    quite a bit slower that just listing the ports.
+    """
+    # list all serial ports
+    for info in serial.tools.list_ports.comports():
+        if args.test:
+            try:
+                m = make_connection(args, port=info.device)
+                try:
+                    mpy_info = m.exec('import sys; print(sys.implementation)').strip()
+                finally:
+                    m.close()
+            except Exception as e:
+                mpy_info = str(e)
+            sys.stdout.write('{!s}: {}\n'.format(info, mpy_info))
+        else:
+            sys.stdout.write('{!s}\n'.format(info))
+
+
+def command_run(m, args):
+    """\
+    Execute the contents of a (small) file on the target, without saving it to
+    the targets file system.
+    """
+    # XXX set timeout / as argument?
+    sys.stdout.write(m.exec(open(args.FILE).read()))
+
+
+def command_ls(m, args):
+    """\
+    List files on the targets file system.
+    """
+    for path in args.PATH:
+        if args.long:
+            files = m.ls(path)
+            files.sort()
+            for filename in files:
+                st = m.stat(posixpath.join(path, filename))
+                sys.stdout.write('{} {:4} {:4} {:>7} {} {}\n'.format(
+                    mode_to_chars(st.st_mode),
+                    st.st_uid if st.st_uid is not None else 'NONE',
+                    st.st_gid if st.st_gid is not None else 'NONE',
+                    nice_bytes(st.st_size),
+                    time.strftime('%Y-%m-%d %02H:%02M:%02S', time.localtime(st.st_mtime)),
+                    escaped(filename)))
+        else:
+            sys.stdout.write(' '.join(m.ls(path)))
+            sys.stdout.write('\n')
+
+
+def command_cat(m, args):
+    """\
+    Print the contents of a file from the target to stdout.
+    """
+    sys.stdout.buffer.write(m.read_file(args.PATH))
+    sys.stdout.buffer.write(b'\n')
+
+
+def command_rm(m, args):
+    """\
+    Remove files on target.
+    """
+    # XXX use glob, --force option, --recursive option
+    for path in args.PATH:
+        m.remove(path)
+
+
+EXCLUDE_DIRS = ['__pycache__']
+
+def ensure_dir(m, path):
+    try:
+        st = m.stat(path)
+    except FileNotFoundError:
+        m.mkdir(path)
+    else:
+        if (st.st_mode & stat.S_IFDIR) == 0:
+            raise FileExistsError('there is a file in the way: {}'.format(path))
+
+
+def command_put(m, args):
+    """\
+    Copy a file from here to there.
+    """
+    dst = args.DST[0]
+    try:
+        dst_dir = (m.stat(dst).st_mode & stat.S_IFDIR) != 0
+        dst_exists = True
+    except FileNotFoundError:
+        dst_dir = False
+        dst_exists = False
+    # expand the patterns for our windows users ;-)
+    paths = sum((glob.glob(src) for src in args.SRC), [])
+    if len(paths) > 1:
+        if not dst_dir:
+            raise ValueError('destination must be a directory')
+        if not dst_exists:
+            raise ValueError('destination directory must exist')
+    for path in paths:
+        if os.path.isdir(path):
+            if os.path.basename(path) in EXCLUDE_DIRS:
+                continue
+            if args.recursive:
+                root = os.path.dirname(path)
+                for dirpath, dirnames, filenames in os.walk(path):
+                    relpath = os.path.relpath(dirpath, root)
+                    if not args.dry_run:
+                        ensure_dir(m, posixpath.join(dst, relpath))
+                    for dir in EXCLUDE_DIRS:
+                        try:
+                            dirnames.remove(dir)
+                        except ValueError:
+                            pass
+                    for filename in filenames:
+                        if args.verbose:
+                            sys.stderr.write('{} -> {}\n'.format(
+                                os.path.join(dirpath, filename),
+                                posixpath.join(dst, relpath, filename)))
+                        if not args.dry_run:
+                            m.write_file(os.path.join(dirpath, filename),
+                                         posixpath.join(dst, relpath, filename))
+            else:
+                sys.stderr.write('skiping directory {}\n'.format(path))
+        else:
+            if dst_dir:
+                if args.verbose:
+                    sys.stderr.write('{} -> {}\n'.format(path, posixpath.join(dst, os.path.basename(path))))
+                if not args.dry_run:
+                    m.write_file(path, posixpath.join(dst, os.path.basename(path)))
+            else:
+                if args.verbose:
+                    sys.stderr.write('{} -> {}\n'.format(path, dst))
+                if not args.dry_run:
+                    m.write_file(path, dst)
+
+
+def command_mount(m, args):
+    """\
+    Mount the target as file system via FUSE.
+    """
+    from . import fuse_drive
+    import subprocess
+    if args.explore:
+        # XXX this is for linux, use os.startfile() on windows
+        subprocess.call(["xdg-open", args.MOUNTPOINT])
+    try:
+        fuse_drive.mount(m, args.MOUNTPOINT, args.verbose)
+    except RuntimeError:
+        sys.stderr.write('ERROR: Could not mount - note: directory must exist\n')
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+def main():
+    import argparse
+
+    global_options = argparse.ArgumentParser(add_help=False)
+    global_options.add_argument('-p', '--port',
+        default=os.environ.get('MPY_PORT', 'hwgrep://USB'),
+        help='set the serial port')
+    global_options.add_argument('-b', '--baudrate', type=int,
+        default=os.environ.get('MPY_BAUDRATE', '115200'),
+        help='set the baud rate')
+    global_options.add_argument('-c', '--command',
+        help='execute given code on target')
+    global_options.add_argument('-i', '--interactive', action='store_true',
+        help='drop to interactive shell at the end')
+    global_options.add_argument('-u', '--user',
+        default=os.environ.get('MPY_USER'),
+        help='response to login prompt')
+    global_options.add_argument('-w', '--password',
+        default=os.environ.get('MPY_PASSWORD'),
+        help='response to password prompt')
+    global_options.add_argument('-v', '--verbose', action='count', default=0,
+        help='show diagnostic messages')
+    global_options.add_argument('--develop', action='store_true',
+        help='show tracebacks on errors (development of this tool)')
+
+    parser = argparse.ArgumentParser(
+        description='Do stuff via the MicroPython REPL',
+        parents=[global_options])
+    parser.set_defaults(connect=False, func=lambda m, args: 0)
+
+    subparsers = parser.add_subparsers(help='sub-command help')
+
+    parser_detect = subparsers.add_parser('detect', help='help locating a board', parents=[global_options])
+    parser_detect.add_argument('-t', '--test', action='store_true', help='open and test each port')
+    parser_detect.set_defaults(func=command_detect)
+
+    parser_run = subparsers.add_parser('run', help='execute file contents on target', parents=[global_options])
+    parser_run.add_argument('FILE', nargs='?', help='load this file contents')
+    parser_run.set_defaults(func=command_run, connect=True)
+
+    parser_ls = subparsers.add_parser('ls', help='list files', parents=[global_options])
+    parser_ls.add_argument('PATH', nargs='*', default='/', help='paths to list')
+    parser_ls.add_argument('-l', '--long', action='store_true', help='show more info')
+    parser_ls.set_defaults(func=command_ls, connect=True)
+
+    parser_cat = subparsers.add_parser('cat', help='print contents of one file', parents=[global_options])
+    parser_cat.add_argument('PATH', help='filename on target')
+    parser_cat.set_defaults(func=command_cat, connect=True)
+
+
+    parser_put = subparsers.add_parser('put', help='file(s) to copy onto target', parents=[global_options])
+    parser_put.add_argument('SRC', nargs='+', help='one or more source files/directories')
+    parser_put.add_argument('DST', nargs=1, help='destination directory')
+    parser_put.add_argument('-r', '--recursive', action='store_true', help='copy recursively')
+    parser_put.add_argument('--dry-run', action='store_true', help='do not actually create anything on target')
+    parser_put.set_defaults(func=command_put, connect=True)
+
+    parser_rm = subparsers.add_parser('rm', help='remove files on target', parents=[global_options])
+    parser_rm.add_argument('PATH', nargs='+', help='filename on target')
+    parser_rm.set_defaults(func=command_rm, connect=True)
+
+    parser_mount = subparsers.add_parser('mount', help='Make target files accessible via FUSE', parents=[global_options])
+    parser_mount.add_argument('MOUNTPOINT', help='local mount point, directory must exist')
+    parser_mount.add_argument('-e', '--explore', action='store_true', help='auto open file explorer at mount point')
+    parser_mount.set_defaults(func=command_mount, connect=True)
+
+    args = parser.parse_args()
+
+    if args.command or args.interactive:
+        args.connect = True
+
+    if args.connect:
+        m = make_connection(args)
+    else:
+        m = None
+
+    exitcode = 0
+    try:
+        if args.func:
+            args.func(m, args)
+        if args.command:
+            sys.stdout.write(m.exec(args.command))
+    except Exception as e:
+        if args.develop:
+            raise
+        sys.stderr.write('ERROR: action or command failed: {}\n'.format(e))
+        exitcode = 1
+
+    if args.interactive:
+        if m:
+            m.stop(interrupt=False)
+        sys.argv = ['n/a', '-f', 'direct']
+        miniterm_mpy.main(serial_instance=m.serial)
+    elif m:
+        m.close(interrupt=False)
+
+    sys.exit(exitcode)
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+if __name__ == '__main__':
+    main()
---
+++ packages/mpy-repl-tool/mpy_repl_tool-0.1-py3-none-any.whl/there/repl_connection.py
@@ -0,0 +1,215 @@
+#! /usr/bin/env python3
+# encoding: utf-8
+#
+# (C) 2016 Chris Liechti <cliechti@gmx.net>
+#
+# SPDX-License-Identifier:    BSD-3-Clause
+import ast
+import posixpath
+import queue
+import os
+import re
+import stat
+import sys
+import time
+import traceback
+import serial
+import serial.threaded
+
+# match "OSError: [Errno 2] ENOENT" and "OSError: 2"
+re_oserror = re.compile(b'OSError: (\[Errno )?(\d+)(\] )?')
+
+
+def prefix(text, prefix):
+    return ''.join('{}{}\n'.format(prefix, line) for line in text.splitlines())
+
+
+class MicroPythonReplProtocol(serial.threaded.Packetizer):
+
+    TERMINATOR = b'\x04>'
+
+    def  __init__(self, *args, **kwargs):
+         super().__init__(*args, **kwargs)
+         self.response = queue.Queue()
+         self.verbose = False
+
+    def connection_made(self, transport):
+        super().connection_made(transport)
+        #~ sys.stderr.write('port opened\n')
+
+    def handle_packet(self, data):
+        #~ sys.stderr.write('response received: {!r}\n'.format(data))
+        self.response.put(data)
+
+    def connection_lost(self, exc):
+        if exc:
+            traceback.print_exc(exc)
+        #~ sys.stderr.write('port closed\n')
+
+    def _parse_error(self, text):
+        """Read the error message and convert exceptions"""
+        lines = text.splitlines()
+        if lines[0].startswith(b'Traceback'):
+            m = re_oserror.match(lines[-1])
+            if m:
+                err_num = int(m.group(2))
+                if err_num == 2:
+                    raise FileNotFoundError(2)
+                elif err_num == 13:
+                    raise PermissionError(13)
+                elif err_num == 17:
+                    raise FileExistsError(17)
+                elif err_num:
+                    raise OSError(err_num)
+
+    def exec(self, string, timeout=3):
+        if self.verbose:
+            sys.stderr.write(prefix(string, 'I: '))
+        self.transport.write(string.encode('utf-8'))
+        if self.response.qsize():
+            self.response.get_nowait()
+        self.transport.write(b'\x04')
+        try:
+            data = self.response.get(timeout=timeout)
+        except queue.Empty:
+            raise IOError('timeout')
+        else:
+            out, err = data.split(b'\x04')
+            if not out.startswith(b'OK'):
+                raise IOError('data was not accepted: {}: {}'.format(out, err))
+            if self.verbose:
+                sys.stderr.write(prefix(out[2:].decode('utf-8'), 'O: '))
+            if err:
+                if self.verbose:
+                    sys.stderr.write(prefix(err.decode('utf-8'), 'E: '))
+                self._parse_error(err)
+                raise IOError('execution failed: {}: {}'.format(out, err))
+            return out[2:].decode('utf-8')
+
+
+class MicroPythonRepl(object):
+    def __init__(self, port='hwgrep://VID:PID=1A86:7523', baudrate=115200, user=None, password=None):
+        self.serial = None
+        self.serial = serial.serial_for_url(port, baudrate=baudrate, timeout=1)
+        if user is not None:
+            time.sleep(0.1)
+            self.serial.read_until(b'Login as: ')
+            time.sleep(0.1)
+            self.serial.write(user.encode('utf-8'))
+            self.serial.write(b'\r\n')
+        if password is not None:
+            self.serial.read_until(b'Password: ')
+            time.sleep(0.1)
+            self.serial.write(password.encode('utf-8'))
+            self.serial.write(b'\r\n')
+            time.sleep(0.1)
+        self.serial.write(b'\x03\x02')  # CTRL+C, exit raw repl
+        time.sleep(0.2)
+        self.serial.write(b'\x03\x01')  # CTRL+C, enter raw repl mode
+        time.sleep(0.2)
+        if port.startswith('socket://'):
+            # hack as reset_input_buffer does not clear anything on socket connections as of pySerial 3.1
+            self.serial._socket.recv(10000)  # clear input, use timeout
+        else:
+            self.serial.reset_input_buffer()
+
+        self._thread = serial.threaded.ReaderThread(self.serial, MicroPythonReplProtocol)
+        self._thread.daemon = True
+        self._thread.start()
+        self.transport, self.protocol = self._thread.connect()
+
+    def stop(self, interrupt=True):
+        if interrupt:
+            self.serial.write(b'\x03\x02')  # exit raw repl mode, and interrupt
+        else:
+            self.serial.write(b'\x02')  # exit raw repl mode
+        self._thread.stop()
+
+    def close(self, interrupt=True):
+        if interrupt:
+            self.serial.write(b'\x03\x02')  # exit raw repl mode, and interrupt
+        else:
+            self.serial.write(b'\x02')  # exit raw repl mode
+        self._thread.close()
+
+    def exec(self, string):
+        """execute the string on the target and return its output"""
+        return self.protocol.exec(string)
+
+    def evaluate(self, string):
+        """execute string on the target and return its output parsed as python literal"""
+        return ast.literal_eval(self.exec(string))
+
+    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+    def statvfs(self, path):
+        st = self.evaluate('import os; print(os.statvfs({!r}))'.format(str(path)))
+        return os.statvfs_result(st)
+        #~ f_bsize, f_frsize, f_blocks, f_bfree, f_bavail, f_files, f_ffree, f_favail, f_flag, f_namemax
+
+    def stat(self, path, fake_attrs=False):
+        st = self.evaluate('import os; print(os.stat({!r}))'.format(str(path)))
+        if fake_attrs:
+            # XXX fake some attributes: rw, uid/gid
+            st = list(st)
+            st[stat.ST_MODE] |= 0o660
+            try:
+                st[stat.ST_GID] = os.getgid()
+                st[stat.ST_UID] = os.getuid()
+            except AttributeError:
+                pass  # Windwos
+        return os.stat_result(st)
+
+    def remove(self, path):
+        return self.evaluate('import os; print(os.remove({!r}))'.format(str(path)))
+
+    def rename(self, path, path_to):
+        return self.evaluate('import os; print(os.rename({!r}, {!r}))'.format(str(path), str(path_to)))
+
+    def mkdir(self, path):
+        return self.evaluate('import os; print(os.mkdir({!r}))'.format(str(path)))
+
+    def rmdir(self, path):
+        return self.evaluate('import os; print(os.rmdir({!r}))'.format(str(path)))
+
+    def read_file(self, path):
+        # use the fact that Python joins adjacent consecutive strings
+        # for the snippet here as well as for the data returned from target
+        return b''.join(self.evaluate(
+            '_f = open({!r}, "rb")\n'
+            'print("[")\n'
+            'while True:\n'
+            '    _b = _f.read()\n'
+            '    if not _b: break\n'
+            '    print(_b, ",")\n'
+            'print("]")\n'
+            '_f.close(); del _f; del _b'.format(str(path))))
+
+    def write_file(self, local_filename, path=None):
+        if path is None:
+            path = local_filename
+        with open(local_filename, 'rb') as f:
+            self.write_to_file(path, f.read())
+
+    def write_to_file(self, path, contents):
+        """\
+        Write contents (expected to be bytes) to path on the target.
+        """
+        if not isinstance(contents, (bytes, bytearray)):
+            raise TypeError('contents must be bytes/bytearray, got {} instead'.format(type(contents)))
+        blocksize = 128
+        self.exec('_f = open({!r}, "wb")'.format(str(path)))
+        for i in range(0, len(contents), blocksize):
+            self.exec('_f.write({!r})'.format(contents[i:i+blocksize]))
+        self.exec('_f.close(); del _f;')
+
+    def truncate(self, path):
+        return self.evaluate(
+            '_f = open({!r}, "rw")\n'
+            '_f.seek({})\n'
+            'print(_f.truncate())\n'
+            '_f.close(); del _f; del _b'.format(str(path), int(length)))
+
+    def ls(self, path):
+        return self.evaluate('import os; print(os.listdir({path!r}))'.format(path=path))
+

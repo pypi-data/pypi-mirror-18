1891d3b8536b3171f9a5c1dfa599b9ffe24cd849
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:19:01 +0000

    Add package nirvanachow/1.2.0/nirvanachow-1.2.0.zip

---
+++ packages/nirvanachow/nirvanachow-1.2.0.zip/nirvanachow-1.2.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+        name         = 'nirvanachow',
+        version      = '1.2.0',
+        py_modules   = ['nester'],
+        author       = 'nast lee',
+        author_email = 'tjulyzhou@163.com',
+ #       url          = 'null',
+        description  = 'A simple printer of nested lists',
+    )

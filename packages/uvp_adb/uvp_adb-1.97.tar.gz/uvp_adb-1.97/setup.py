ffa78f55f70926fa20fc61dc8cec8ce251d87bca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:30:46 +0000

    Add package uvp_adb/1.97/uvp_adb-1.97.tar.gz

---
+++ packages/uvp_adb/uvp_adb-1.97.tar.gz/uvp_adb-1.97/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='uvp_adb',
+      version='1.97',
+      description='adb functions for uvp phone',
+      url='https://github.com/inforfang/uvp_adb',
+      download_url = 'https://github.com/inforfang/uvp_adb/tarball/1.97',
+      author='inforfang',
+      author_email='szaforward2@gmail.com',
+      license='MIT',
+      packages=['uvp_adb'],
+      zip_safe=False)

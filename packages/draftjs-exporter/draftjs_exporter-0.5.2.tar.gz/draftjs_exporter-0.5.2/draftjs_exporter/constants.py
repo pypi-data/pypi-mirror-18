c0d98f69227024984fbfc346262916a2815c9744
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:18:29 +0000

    Add package draftjs-exporter/0.5.2/draftjs_exporter-0.5.2.tar.gz

---
+++ packages/draftjs-exporter/draftjs_exporter-0.5.2.tar.gz/draftjs_exporter-0.5.2/draftjs_exporter/constants.py
@@ -0,0 +1,36 @@
+from __future__ import absolute_import, unicode_literals
+
+
+# http://stackoverflow.com/a/22723724/1798491
+class Enum(object):
+    def __init__(self, *elements):
+        self.elements = tuple(elements)
+
+    def __getattr__(self, name):
+        if name not in self.elements:
+            raise AttributeError("'Enum' has no attribute '{}'".format(name))
+
+        return name
+
+
+# https://github.com/draft-js-utils/draft-js-utils/blob/master/src/Constants.js
+class BLOCK_TYPES:
+    UNSTYLED = 'unstyled'
+    HEADER_ONE = 'header-one'
+    HEADER_TWO = 'header-two'
+    HEADER_THREE = 'header-three'
+    HEADER_FOUR = 'header-four'
+    HEADER_FIVE = 'header-five'
+    HEADER_SIX = 'header-six'
+    UNORDERED_LIST_ITEM = 'unordered-list-item'
+    ORDERED_LIST_ITEM = 'ordered-list-item'
+    BLOCKQUOTE = 'blockquote'
+    PULLQUOTE = 'pullquote'
+    CODE = 'code-block'
+    ATOMIC = 'atomic'
+    HORIZONTAL_RULE = 'horizontal-rule'
+
+
+ENTITY_TYPES = Enum('LINK', 'IMAGE', 'TOKEN')
+
+INLINE_STYLES = Enum('BOLD', 'CODE', 'ITALIC', 'STRIKETHROUGH', 'UNDERLINE')

3e46935a50b512e704dc2d541100f5ceaaabc214
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:20:12 +0000

    Add package mastercard-installments-non-fi/0.0.1/mastercard_installments_non_fi-0.0.1-py2.py3-none-any.whl

---
+++ packages/mastercard-installments-non-fi/mastercard_installments_non_fi-0.0.1-py2.py3-none-any.whl/mastercardinstallmentsnonfi/__init__.py
@@ -0,0 +1,4 @@
+from mastercardapicore import *
+from mastercardinstallmentsnonfi.resourceconfig import *
+from mastercardinstallmentsnonfi.createinstallmentplan import *
+from mastercardinstallmentsnonfi.calculateinstallmentdata import *

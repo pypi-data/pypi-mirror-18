4178b835830308d2a89a19b730a5ca7c1877e621
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:08:58 +0000

    Add package url2word/0.1.3/url2word-0.1.3.tar.gz

---
+++ packages/url2word/url2word-0.1.3.tar.gz/url2word-0.1.3/setup.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+import sys
+
+import url2word
+
+
+install_requires = [
+    "beautifulsoup4"
+]
+
+
+setup(
+    name=url2word.__title__,
+    version=url2word.__version__,
+    description=url2word.__summary__,
+    license=url2word.__license__,
+    author=url2word.__author__,
+    packages=["url2word"],
+    install_requires=install_requires,
+    long_description=open("README.md").read(),
+    entry_points={
+        'console_scripts': [
+            'url2word=url2word:main',
+        ],
+    },
+    classifiers=[
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.5",
+    ]
+)

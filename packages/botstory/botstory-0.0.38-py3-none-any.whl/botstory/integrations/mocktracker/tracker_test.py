c2313e72e1adf124ca78abc53923a825024b5e8a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:33:05 +0000

    Add package botstory/0.0.38/botstory-0.0.38-py3-none-any.whl

---
+++ packages/botstory/botstory-0.0.38-py3-none-any.whl/botstory/integrations/mocktracker/tracker_test.py
@@ -0,0 +1,42 @@
+import pytest
+
+from . import tracker
+from .. import mocktracker
+from ... import di, story
+
+
+def teardown_function(function):
+    story.clear()
+
+
+def test_event():
+    t = tracker.MockTracker()
+    t.event()
+
+
+def test_new_message():
+    t = tracker.MockTracker()
+    t.new_message()
+
+
+def test_new_user():
+    t = tracker.MockTracker()
+    t.new_user()
+
+
+def test_story():
+    t = tracker.MockTracker()
+    t.story()
+
+
+def test_get_mock_tracker_as_dep():
+    story.use(mocktracker.MockTracker())
+
+    with di.child_scope():
+        @di.desc()
+        class OneClass:
+            @di.inject()
+            def deps(self, tracker):
+                self.tracker = tracker
+
+        assert isinstance(di.injector.get('one_class').tracker, mocktracker.MockTracker)

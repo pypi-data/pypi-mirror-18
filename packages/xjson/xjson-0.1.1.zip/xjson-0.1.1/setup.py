a1d4e852efec5e4ee849e5f53d715b9db8424ffd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:22:44 +0000

    Add package xjson/0.1.1/xjson-0.1.1.zip

---
+++ packages/xjson/xjson-0.1.1.zip/xjson-0.1.1/setup.py
@@ -0,0 +1,17 @@
+#coding=utf-8
+
+from setuptools import setup,find_packages
+
+setup(
+	name = "xjson",
+	version = "0.1.1",
+	keywords = ("json","parse","xpath","syntax"),
+	description = "allow you parse a json file with xpath syntax",
+	license = "MIT License",
+	install_requires = [],
+	author = "zhangTian",
+	author_email = "hhczy1003@gmail.com",
+	#packages = find_packages(),
+	packages = ["xjson"],
+	platforms = "any"
+	)

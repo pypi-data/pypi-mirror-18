3e21d61d0350415ba5403376c47fa2b1dbf0ba96
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:21:33 +0000

    Add package TelegramBotAPI/0.3.2/TelegramBotAPI-0.3.2.tar.gz

---
+++ packages/TelegramBotAPI/TelegramBotAPI-0.3.2.tar.gz/TelegramBotAPI-0.3.2/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="TelegramBotAPI",
+    version="0.3.2",
+    description="Telegram Bot API",
+    author="Source Simian",
+    author_email='sourcesimian@users.noreply.github.com',
+    url='https://github.com/sourcesimian/pyTelegramBotAPI',
+    download_url="https://github.com/sourcesimian/pyPlugin/tarball/v0.3.2",
+    license='MIT',
+    packages=find_packages(exclude=['tests',]),
+    install_requires=[
+        'pyOpenSSL',
+        'service_identity',
+        'requests',
+    ],
+)

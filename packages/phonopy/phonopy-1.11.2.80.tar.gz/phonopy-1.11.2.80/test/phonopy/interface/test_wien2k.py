2d4b7fd81cf2292ba8dde212aca9b2df423aa37a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:33:04 +0000

    Add package phonopy/1.11.2.80/phonopy-1.11.2.80.tar.gz

---
+++ packages/phonopy/phonopy-1.11.2.80.tar.gz/phonopy-1.11.2.80/test/phonopy/interface/test_wien2k.py
@@ -0,0 +1,37 @@
+import unittest
+
+import numpy as np
+from phonopy.structure.atoms import PhonopyAtoms
+from phonopy.structure.symmetry import Symmetry
+from phonopy.interface.wien2k import parse_wien2k_struct
+from phonopy.file_IO import parse_disp_yaml
+import os
+data_dir=os.path.dirname(os.path.abspath(__file__))
+
+class TestWien2k(unittest.TestCase):
+
+    def setUp(self):
+        pass
+    
+    def tearDown(self):
+        pass
+    
+    def test_parse_wien2k_struct(self):
+        cell, npts, r0s, rmts = parse_wien2k_struct(os.path.join(data_dir,"BaGa2.struct"))
+        lattice = cell.get_cell().T
+        displacements, supercell = parse_disp_yaml(os.path.join(data_dir,"disp_BaGa2.yaml"),
+                                                   return_cell=True)
+        symmetry = Symmetry(cell)
+        print(PhonopyAtoms(atoms=cell))
+        sym_op = symmetry.get_symmetry_operations()
+        print(symmetry.get_international_table())
+        for i, (r, t) in enumerate(
+                zip(sym_op['rotations'], sym_op['translations'])):
+            print("--- %d ---" % (i + 1))
+            print(r)
+            print(t)
+
+if __name__ == '__main__':
+    suite = unittest.TestLoader().loadTestsFromTestCase(TestWien2k)
+    unittest.TextTestRunner(verbosity=2).run(suite)
+    # unittest.main()

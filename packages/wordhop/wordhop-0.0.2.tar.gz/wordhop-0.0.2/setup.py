fa2c5b89e12037771f3581622a65fdcb82085968
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:14:53 +0000

    Add package wordhop/0.0.2/wordhop-0.0.2.tar.gz

---
+++ packages/wordhop/wordhop-0.0.2.tar.gz/wordhop-0.0.2/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+      name = 'wordhop',
+      packages = ['wordhop'],
+      version = '0.0.2',
+      description = 'A toolkit for creating a hybrid Chatbot + Human UX',
+      author = 'Michael Nathanson',
+      author_email = 'mike@wordhop.io',
+      url = 'https://github.com/wordhop-io/wordhop-python',
+      download_url = 'https://github.com/wordhop-io/wordhop-python/tarball/0.0.2',
+      keywords = ['chatbot', 'bot', 'opitmization', 'bot', 'analytics'],
+      classifiers = [],
+      )

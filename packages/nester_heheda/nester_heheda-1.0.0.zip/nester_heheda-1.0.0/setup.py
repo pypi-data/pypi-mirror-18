9d45d66c4bd6b4d061fdb3edb24a5d59297a18cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:50:04 +0000

    Add package nester_heheda/1.0.0/nester_heheda-1.0.0.zip

---
+++ packages/nester_heheda/nester_heheda-1.0.0.zip/nester_heheda-1.0.0/setup.py
@@ -0,0 +1,15 @@
+#coding = utf-8
+
+#元数据相关
+from distutils.core import setup
+
+setup(
+    name            = 'nester_heheda',
+    version         = '1.0.0',
+    py_modules      = ['nester_heheda'],
+    author          = 'aceLinpython',
+    author_email    = 'echislin123@live.com',
+    url             = 'http://blog.csdn.net/qq_29757283/article/details/53571115',
+    description     = 'a simple printer of nested lists',
+    )
+

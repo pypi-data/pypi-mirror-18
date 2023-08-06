e8c76a4b22f3db1690032498a671db1efdda9766
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:08:12 +0000

    Add package kafkatos3/0.1.4/kafkatos3-0.1.4.tar.gz

---
+++ packages/kafkatos3/kafkatos3-0.1.4.tar.gz/kafkatos3-0.1.4/kafkatos3/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""Archive kafka messages to S3"""
+
+# Not sure why the below doesn't work. But oh well
+# from kafkatos3 import metadata
+import metadata
+
+__version__ = metadata.version
+__author__ = metadata.authors[0]
+__license__ = metadata.license
+__copyright__ = metadata.copyright

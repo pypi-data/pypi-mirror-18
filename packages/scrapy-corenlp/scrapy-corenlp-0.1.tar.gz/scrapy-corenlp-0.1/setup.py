663da0fa74752bbcddb5bcb6cad6afbaa76b9899
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:10:20 +0000

    Add package scrapy-corenlp/0.1/scrapy-corenlp-0.1.tar.gz

---
+++ packages/scrapy-corenlp/scrapy-corenlp-0.1.tar.gz/scrapy-corenlp-0.1/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+
+setup(
+    name='scrapy-corenlp',
+    version='0.1',
+    description='Scrapy spider middleware :: Stanford CoreNLP Named Entity Recognition',
+    url='https://github.com/vu3jej/scrapy-corenlp',
+    author='Jithesh E J',
+    author_email='mail@jithesh.net',
+    license='BSD-2-Clause',
+    packages=['scrapy_corenlp'],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Programming Language :: Python :: 3.4',
+        'Topic :: Text Processing :: Linguistic',
+    ]
+)

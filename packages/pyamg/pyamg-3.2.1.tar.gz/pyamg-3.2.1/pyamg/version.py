d716ef860321399d3fbd87999ffbe273702b3bef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:17:55 +0000

    Add package pyamg/3.2.1/pyamg-3.2.1.tar.gz

---
+++ packages/pyamg/pyamg-3.2.1.tar.gz/pyamg-3.2.1/pyamg/version.py
@@ -0,0 +1,9 @@
+
+# THIS FILE IS GENERATED FROM SETUP.PY
+short_version = '3.2.1'
+version = '3.2.1'
+full_version = '3.2.1'
+git_revision = '19847f885e08318350ce2d94b3d94ab82b6f04eb'
+release = True
+if not release:
+    version = full_version

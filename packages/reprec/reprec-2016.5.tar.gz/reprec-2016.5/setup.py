063ffdb64900de5646349632c7283515fb3a8d46
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:08:33 +0000

    Add package reprec/2016.5/reprec-2016.5.tar.gz

---
+++ packages/reprec/reprec-2016.5.tar.gz/reprec-2016.5/setup.py
@@ -0,0 +1,18 @@
+import setuptools
+
+setuptools.setup(
+    name='reprec',
+    version='2016.5',
+    license='BSD',
+    url='https://github.com/guettli/reprec',
+    long_description=open('README.rst').read(),
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+
+    entry_points={
+        'console_scripts': [
+            'reprec=reprec.reprec:main',
+            'setops=setops:main',
+        ],
+    }
+)

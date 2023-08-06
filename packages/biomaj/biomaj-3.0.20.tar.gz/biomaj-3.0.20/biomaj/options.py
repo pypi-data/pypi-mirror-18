ae9608fbfe1085f58f367bacac65d10546c506e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:11:14 +0000

    Add package biomaj/3.0.20/biomaj-3.0.20.tar.gz

---
+++ packages/biomaj/biomaj-3.0.20.tar.gz/biomaj-3.0.20/biomaj/options.py
@@ -0,0 +1,36 @@
+from builtins import object
+
+
+class Options(object):
+    """
+    Available options
+    """
+
+    def __init__(self, options=None):
+        self.options = options
+
+    def get_option(self, option):
+        """
+        Gets an option if present, else return None
+        """
+        #if self.options is None:
+        #  return None
+
+        #if hasattr(self.options, option):
+        #  return getattr(self.options, option)
+
+        if hasattr(self, option):
+            return getattr(self, option)
+        #if option in self.options:
+        #  return self.options[option]
+
+        return None
+
+    UPDATE = 'update'
+    REMOVE = 'remove'
+    PUBLISH = 'publish'
+    FROM_TASK = 'from_task'
+    PROCESS = 'process'
+    STOP_BEFORE = 'stop_before'
+    STOP_AFTER = 'stop_after'
+    FROMSCRATCH = 'fromscratch'

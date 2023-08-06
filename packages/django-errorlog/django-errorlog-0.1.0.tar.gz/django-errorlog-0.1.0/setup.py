ab16e99e9912ab476be0f45cb1125c26d49076dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:12:52 +0000

    Add package django-errorlog/0.1.0/django-errorlog-0.1.0.tar.gz

---
+++ packages/django-errorlog/django-errorlog-0.1.0.tar.gz/django-errorlog-0.1.0/setup.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+import sys
+
+if 'sdist' in sys.argv:
+    from pypandoc import convert
+    long_description = convert('README.md', 'rst')
+else:
+    long_description = ""
+VERSION = "0.1.0"
+
+setup(
+    name='django-errorlog',
+    version=VERSION,
+    description='Django reuseable app to collect the unexpcted exception then generate comprehansive report just like what you get in debug mode and store in database',
+    url="https://github.com/yjmade/django-errorlog",
+    long_description=long_description,
+    author='Jay Young(yjmade)',
+    author_email='dev@yjmade.net',
+    packages=find_packages(),
+    install_requires=["django>1.9"],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: ISC License (ISCL)',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+    license='MIT',
+    keywords='django error log report',
+
+)

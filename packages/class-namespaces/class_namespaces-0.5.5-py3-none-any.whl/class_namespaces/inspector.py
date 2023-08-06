1ce23288c0ec039ec7c8c52be48105085aa286ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:33:07 +0000

    Add package class-namespaces/0.5.5/class_namespaces-0.5.5-py3-none-any.whl

---
+++ packages/class-namespaces/class_namespaces-0.5.5-py3-none-any.whl/class_namespaces/inspector.py
@@ -0,0 +1,27 @@
+"""Inspector.
+
+Wrapper around objects, helps expose protocols.
+
+"""
+
+import collections
+
+
+class _Inspector(collections.namedtuple('_Inspector', ['object', 'dict'])):
+
+    """Wrapper around objects. Provides access to protocold."""
+
+    __slots__ = ()
+
+    def __new__(cls, obj, *, mro):
+        dct = collections.ChainMap(*[vars(cls) for cls in mro])
+        return super().__new__(cls, obj, dct)
+
+    def get_as_attribute(self, key):
+        """Return attribute with the given name, or raise AttributeError."""
+        try:
+            return self.dict[key]
+        # These lines will execute if an incomplete data descriptor is used for
+        # the operation it doesn't define.
+        except KeyError:
+            raise AttributeError(key)

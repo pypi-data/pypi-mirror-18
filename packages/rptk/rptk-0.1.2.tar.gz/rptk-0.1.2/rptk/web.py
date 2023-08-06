09328aaf45ff06b5cd9b6dbfdb331a022b94fc8e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:11:03 +0000

    Add package rptk/0.1.2/rptk-0.1.2.tar.gz

---
+++ packages/rptk/rptk-0.1.2.tar.gz/rptk-0.1.2/rptk/web.py
@@ -0,0 +1,26 @@
+from flask import Flask, request, make_response
+from rptk.api import Rptk
+
+app = Flask(__name__)
+
+
+@app.route("/formats")
+def get_formats():
+    opts = request.args.to_dict()
+    rptk = Rptk(**opts)
+    formats = rptk.available_formats()
+    response = make_response("\n".join(formats))
+    response.headers['Content-Type'] = "text/plain"
+    return response
+
+@app.route("/<string:fmt>/<string:obj>")
+def get_prefix_list(fmt=None, obj=None):
+    opts = request.args.to_dict()
+    rptk = Rptk(object=obj, format=fmt, **opts)
+    result = rptk.query()
+    response = make_response(result)
+    response.headers['Content-Type'] = "text/plain"
+    return response
+
+if __name__ == "__main__":
+    app.run(host='::', port=8080)

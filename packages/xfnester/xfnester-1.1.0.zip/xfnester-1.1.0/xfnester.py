4300a4d8360ebbd521775863a0ee61ce793a41aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:30:25 +0000

    Add package xfnester/1.1.0/xfnester-1.1.0.zip

---
+++ packages/xfnester/xfnester-1.1.0.zip/xfnester-1.1.0/xfnester.py
@@ -0,0 +1,8 @@
+﻿def print_lol(the_list,the_tab):
+       for each_item in the_list:
+                if isinstance(each_item,list):
+                         print_lol(each_item,the_tab+1)
+                else:
+                         for num in range(the_tab):
+                             print("\t",end='')
+                         print(each_item)

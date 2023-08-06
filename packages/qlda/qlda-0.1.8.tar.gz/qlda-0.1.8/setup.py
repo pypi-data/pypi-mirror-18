9a579a83c6e214fc0993a0635d7943a45c9e8a61
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:10:16 +0000

    Add package qlda/0.1.8/qlda-0.1.8.tar.gz

---
+++ packages/qlda/qlda-0.1.8.tar.gz/qlda-0.1.8/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(name='qlda',
+      version='0.1.8',
+      description='Lda-MLOPE',
+      url='https://github.com/Quangbd/qlda',
+      author='Quang Bui Duc',
+      author_email='quangbd.hust@gmail.com',
+      license='NONE',
+      packages=['qlda'],
+      install_requires=[
+          'numpy', 'scipy',
+      ],
+      zip_safe=False)

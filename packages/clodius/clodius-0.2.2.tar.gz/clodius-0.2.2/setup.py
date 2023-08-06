ecac9c10602700a1eb2f297f58052668925ba293
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:45:09 +0000

    Add package clodius/0.2.2/clodius-0.2.2.tar.gz

---
+++ packages/clodius/clodius-0.2.2.tar.gz/clodius-0.2.2/setup.py
@@ -0,0 +1,21 @@
+from distutils.core import setup
+from distutils.extension import Extension
+
+from Cython.Build import cythonize
+
+import numpy 
+
+setup(name='clodius',
+    ext_modules=[Extension("clodius.fast", ["clodius/fast.pyx"])],
+     include_dirs=[numpy.get_include()],
+      version='0.2.2',
+      description='Tile generation of big data',
+      author='Peter Kerpedjiev',
+      author_email='pkerpedjiev@gmail.com',
+      url='',
+      packages=['clodius'],
+      install_requires=[
+                    'cython',
+                    'numpy' ],
+      scripts=['scripts/make_single_threaded_tiles.py']
+     )

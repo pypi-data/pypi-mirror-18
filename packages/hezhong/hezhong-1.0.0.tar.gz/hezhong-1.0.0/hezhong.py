dbcc2ec21ac2524c540172060dd6c4ef12b5b9e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:20:29 +0000

    Add package hezhong/1.0.0/hezhong-1.0.0.tar.gz

---
+++ packages/hezhong/hezhong-1.0.0.tar.gz/hezhong-1.0.0/hezhong.py
@@ -0,0 +1,2 @@
+def print_version():
+    print ("version 1.0.0")
---
+++ packages/hezhong/hezhong-1.0.0.tar.gz/hezhong-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+        name = 'hezhong',
+        version = '1.0.0',
+        py_modules = ['hezhong'],
+        author  = 'Answer',
+        author_email = 'hezhong0711@gmail.com',
+        url = 'http://www.hezhong0711.cn',
+        description = 'A simple printer of hezhong module version ',
+    )

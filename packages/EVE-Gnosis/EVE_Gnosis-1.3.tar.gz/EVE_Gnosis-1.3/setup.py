531ff238fab1a4ee093a0cfcfdd28250b115221b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:10:27 +0000

    Add package EVE-Gnosis/1.3/EVE_Gnosis-1.3.tar.gz

---
+++ packages/EVE-Gnosis/EVE_Gnosis-1.3.tar.gz/EVE_Gnosis-1.3/setup.py
@@ -0,0 +1,14 @@
+from pip.req import parse_requirements
+from setuptools import setup, find_packages
+
+parsed_reqs = parse_requirements('requirements.txt', session=False)
+install_requires = [str(ir.req) for ir in parsed_reqs]
+
+setup(name='EVE_Gnosis',
+      version='1.3',
+      description='EVE Online Formulas and Simulations',
+      author='Ebag Trescientas',
+      author_email='ebagola@gmail.com',
+      url='https://github.com/Ebag333/EVE_Gnosis',
+      packages=find_packages(exclude='tests'),
+      )

105244b930600a928714c93bbf7dbb0f49ee89f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:09:52 +0000

    Add package hello_samu/0.0/hello_samu-0.0.zip

---
+++ packages/hello_samu/hello_samu-0.0.zip/hello_samu-0.0/setup.py
@@ -0,0 +1,6 @@
+from setuptools import setup
+setup(
+    name='hello_samu',    # This is the name of your PyPI-package.
+    version='0.0',                          # Update the version number for new releases
+    scripts=['hello_samu.py']                  # The name of your scipt, and also the command you'll be using for calling it
+)

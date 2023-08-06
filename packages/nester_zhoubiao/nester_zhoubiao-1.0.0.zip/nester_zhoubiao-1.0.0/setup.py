5bead9f6d9401744c90046c4bb81b1fb7f6eb68d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:08:15 +0000

    Add package nester_zhoubiao/1.0.0/nester_zhoubiao-1.0.0.zip

---
+++ packages/nester_zhoubiao/nester_zhoubiao-1.0.0.zip/nester_zhoubiao-1.0.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+	name = 'nester_zhoubiao',
+	version = '1.0.0',
+	py_moudules = ['nester'],
+	author = '伪代码',
+	author_email = 'zhoubiao2016@163.com',
+	description = 'A simple printer of nested lists'
+)

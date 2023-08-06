9ea03bbc7da855bd40d4607d7b255edcdb66ee9d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:10:11 +0000

    Add package test_tljing/1.0.0/test_tljing-1.0.0.zip

---
+++ packages/test_tljing/test_tljing-1.0.0.zip/test_tljing-1.0.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(
+    name='test_tljing',
+    version='1.0.0',
+    py_modules=['test_tljing'],
+    author='tljing',
+    author_email='zj1986@126.com',
+    url='http://www.headfirstlabs.com',
+    description='A example',
+    )

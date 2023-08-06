5bdbafa228c2f25df6d791bc67e087443fd1ae8c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:44:48 +0000

    Add package hulkify/0.0.9/hulkify-0.0.9.tar.gz

---
+++ packages/hulkify/hulkify-0.0.9.tar.gz/hulkify-0.0.9/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+import json
+import os
+
+setup(name='hulkify',
+    version='0.0.9',
+    description='Turn normal English into HULK-SPEAK',
+    url='https://github.com/sjml/hulkify',
+    author='Shane Liesegang',
+    author_email='shane@techie.net',
+    license='MIT',
+    keywords='hulk english text filter',
+    packages=['hulkify'],
+    package_data={'hulkify': ['grammar.json']},
+    classifiers = [
+      'Development Status :: 3 - Alpha',
+      'Intended Audience :: Developers',
+      'License :: OSI Approved :: MIT License',
+      'Natural Language :: English',
+      'Programming Language :: Python :: 2 :: Only',
+      'Programming Language :: Python :: 2.7',
+      'Topic :: Text Processing :: Filters',
+    ],
+    install_requires=["Pattern>=2.6"],
+)

df1ee0f70346c5b8948551ae4aa5106a847e1794
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:43:37 +0000

    Add package vimddr/0.1.8/vimddr-0.1.8.tar.gz

---
+++ packages/vimddr/vimddr-0.1.8.tar.gz/vimddr-0.1.8/setup.py
@@ -0,0 +1,43 @@
+from setuptools import setup, find_packages
+from os import path
+import platform
+
+if (platform.system() == 'Windows'):
+    getch = 'msvcrt'
+else:
+    getch = 'getch'
+
+setup(
+    name='vimddr',
+    version='0.1.8',
+    description='Improve your vim reflexes!',
+    url='https://github.com/BlitzKraft/vimDDR',
+    author='BK Bolisetty',
+    author_email='bk@blitzkraft.me',
+    license='MIT',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Games/Entertainment',
+        'Environment :: Console',
+        'Topic :: Terminals',
+        'Topic :: Text Editors',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+
+    keywords=['vim', 'commandline', 'games'],
+    packages=find_packages('game', '__main__'),
+
+    install_requires=[getch],
+    extras_require={
+    },
+    entry_points={
+        'console_scripts': [
+            'vimddr = __main__:main',
+        ],
+    },
+)

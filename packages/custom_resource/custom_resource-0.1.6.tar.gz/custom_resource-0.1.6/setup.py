6896d3c2905f26fef12ee575e08a8b439c506a52
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:08:53 +0000

    Add package custom_resource/0.1.6/custom_resource-0.1.6.tar.gz

---
+++ packages/custom_resource/custom_resource-0.1.6.tar.gz/custom_resource-0.1.6/setup.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+
+import os.path
+
+from setuptools import setup
+
+ROOT = os.path.dirname(__file__)
+
+setup(
+    version="0.1.6",
+    url="https://github.com/nathforge/custom_resource",
+    name="custom_resource",
+    description="Helps you implement custom CloudFormation resources via Python Lambda functions.",
+    long_description=open(os.path.join(ROOT, "README.rst")).read(),
+    author="Nathan Reynolds",
+    author_email="email@nreynolds.co.uk",
+    packages=["custom_resource"],
+    package_dir={"": os.path.join(ROOT, "src")},
+    install_requires=[
+        line.strip()
+        for line in open(os.path.join(ROOT, "requirements.txt"))
+        if not line.startswith("#")
+        and line.strip() != ""
+    ],
+    test_suite="tests",
+    tests_require=["mock"],
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 2.7"
+    ]
+)

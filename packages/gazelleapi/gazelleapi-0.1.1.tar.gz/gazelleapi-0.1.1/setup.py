5f7f6d3e36bd76d82026282734b3fc4a2d545e07
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:45:10 +0000

    Add package gazelleapi/0.1.1/gazelleapi-0.1.1.tar.gz

---
+++ packages/gazelleapi/gazelleapi-0.1.1.tar.gz/gazelleapi-0.1.1/setup.py
@@ -0,0 +1,38 @@
+from setuptools import setup, find_packages
+from gazelleapi import __version__, __author__, __author_email__, __url__
+
+with open('README.md') as f:
+    readme_file = f.read()
+
+with open('LICENSE.md') as f:
+    license_file = f.read()
+
+setup(
+    name='gazelleapi',
+    version=__version__,
+    description='Gazelle API',
+    long_description=readme_file,
+    author=__author__,
+    author_email=__author_email__,
+    url=__url__,
+    license=license_file,
+    install_requires=[
+        "future",
+        "requests"
+    ],
+    packages=find_packages(exclude=('tests', 'docs')),
+    package_data={
+        '': ['*.txt']
+    },
+    zip_safe=True,
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3'
+    ]
+)

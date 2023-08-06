00106e41b96df21cf01c3f103af8dc5457ee6572
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:12:04 +0000

    Add package py-ugs3client/0.1/py-ugs3client-0.1.tar.gz

---
+++ packages/py-ugs3client/py-ugs3client-0.1.tar.gz/py-ugs3client-0.1/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(name='py-ugs3client',
+      version='0.1',
+      description='UGS3 Python client',
+      url='https://github.com/vsevolod-kolchinsky/py-ugs3client',
+      author='Vsevolod Kolchinsky',
+      author_email='vsevolod.kolchinsky@gmail.com',
+      license='MIT',
+      packages=['ugs3client'],
+      install_requires=[
+          'requests','cached-property',
+      ],
+      )

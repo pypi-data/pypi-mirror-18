0d9eecc21373bd30e6ec568ae16c148d24454dff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:46:08 +0000

    Add package roulier/0.0.2rc2/roulier-0.0.2rc2.tar.gz

---
+++ packages/roulier/roulier-0.0.2rc2.tar.gz/roulier-0.0.2rc2/setup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+version = open('VERSION').read().strip()
+
+setup(
+    name="roulier",
+    version=version,
+    packages=find_packages(),
+    install_requires=['lxml', 'Jinja2', 'requests', 'cerberus'],
+    author="Hparfr <https://github.com/hparfr>",
+    author_email="roulier@hpar.fr",
+    description="Label parcels without pain",
+    include_package_data=True,
+    package_data={'roulier': ['*.xml', '*.xsl', '*.zpl']},
+    url="https://github.com/akretion/roulier",
+    download_url="https://github.com/akretion/roulier/tarball/0.1.0",
+    keywords=['carrier', 'logistics', 'delivery'],
+)

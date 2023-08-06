e1d1db5d19c661cbf8dd7e6070184be328a37a2e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:18:39 +0000

    Add package hpimon/0.12/hpimon-0.12.tar.gz

---
+++ packages/hpimon/hpimon-0.12.tar.gz/hpimon-0.12/setup.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Nov 14 15:50:55 2016
+
+@author: jussi
+"""
+
+from setuptools import setup
+
+
+setup(name='hpimon',
+      version='0.12',
+      description='Continuous HPI monitor for Elekta MEG systems',
+      author='Jussi Nurminen',
+      author_email='jnu@iki.fi',
+      license='MIT',
+      url='https://github.com/jjnurminen/hpimon',
+      include_package_data=True,
+      entry_points={
+          'gui_scripts': ['hpimon = hpimon.hpimon:main']
+      })

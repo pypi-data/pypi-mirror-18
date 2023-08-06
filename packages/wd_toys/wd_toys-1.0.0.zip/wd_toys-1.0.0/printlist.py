ce5b4817c16c36142ecf032de27873093f2b18d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:19:36 +0000

    Add package wd_toys/1.0.0/wd_toys-1.0.0.zip

---
+++ packages/wd_toys/wd_toys-1.0.0.zip/wd_toys-1.0.0/printlist.py
@@ -0,0 +1,8 @@
+"""用于简单的列表显示"""
+
+def print_lol(the_list):
+    for each_item in the_list:
+        if isinstance(each_item,list):
+            print_lol(each_item)
+        else:
+            print(each_item)            
---
+++ packages/wd_toys/wd_toys-1.0.0.zip/wd_toys-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+                name                 ='wd_toys',
+                version              ='1.0.0',
+                py_modules     =['printlist'],
+                author               ='wayde',
+                author_email    ='micwayde@163.com',
+                url                      ='http://nowebsite',
+                description      ='A simple printer of lists',
+            )

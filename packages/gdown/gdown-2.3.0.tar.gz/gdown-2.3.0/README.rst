660aad1b35d86bfe9ddd3efc1d10f40fbfba8ed8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:09:52 +0000

    Add package gdown/2.3.0/gdown-2.3.0.tar.gz

---
+++ packages/gdown/gdown-2.3.0.tar.gz/gdown-2.3.0/README.rst
@@ -0,0 +1,47 @@
+gdown
+=====
+
+.. image:: https://travis-ci.org/wkentaro/gdown.svg?branch=master
+    :target: https://travis-ci.org/wkentaro/gdown
+.. image:: https://badge.fury.io/py/gdown.svg
+    :target: https://badge.fury.io/py/gdown
+
+
+Google Drive direct download of big files.
+
+
+Usage
+-----
+
+.. code-block:: bash
+
+  gdown "[URL]" -O [FILENAME]
+
+
+**Example**
+
+.. code-block:: bash
+
+  gdown "https://drive.google.com/uc?id=0B9P1L--7Wd2vVlpzRkwzYUladms" -O hoge.txt
+
+
+Installation
+------------
+
+.. code-block:: bash
+
+  pip install gdown
+
+
+License
+-------
+This is originally created with Perl, and this is Python version Command Line Tool.
+
+::
+
+  gdown.pl
+
+  by circulosmeos 04-2014.
+
+  Released under GPL v3
+  http://www.gnu.org/copyleft/gpl.html
---
+++ packages/gdown/gdown-2.3.0.tar.gz/gdown-2.3.0/setup.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+
+from __future__ import print_function
+import sys
+import subprocess
+from setuptools import setup, find_packages
+
+
+version = '2.3.0'
+
+
+# publish helper
+if sys.argv[-1] == 'publish':
+    for cmd in [
+            'python setup.py sdist upload',
+            'git tag {}'.format(version),
+            'git push origin master --tag']:
+        subprocess.check_call(cmd, shell=True)
+    sys.exit(0)
+
+setup(
+    name='gdown',
+    version=version,
+    packages=find_packages(),
+    description='Google Drive direct download of big files.',
+    long_description=open('README.rst').read(),
+    author='Kentaro Wada',
+    author_email='www.kentaro.wada@gmail.com',
+    url='http://github.com/wkentaro/gdown',
+    license='MIT',
+    keywords='utility',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: POSIX',
+        'Topic :: Internet :: WWW/HTTP',
+        ],
+    entry_points={'console_scripts': ['gdown=gdown:main']},
+)

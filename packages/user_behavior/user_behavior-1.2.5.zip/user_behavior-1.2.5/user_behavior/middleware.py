9fb44b858b1d1b561910982a2438d6cba44b31c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:08:02 +0000

    Add package user_behavior/1.2.5/user_behavior-1.2.5.zip

---
+++ packages/user_behavior/user_behavior-1.2.5.zip/user_behavior-1.2.5/user_behavior/middleware.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import
+
+import json
+from django.forms import model_to_dict
+from datetime import date
+
+from django.contrib.auth.models import User
+
+from .serializers import ApiInfoSerializer
+from .services import get_request_data, set_response_data
+from .tasks import task_user_behavior
+from django.conf import settings
+
+
+class UserBehaviorExtractMiddleWare(object):
+
+    def __init__(self):
+        self._api_info = None
+
+    def process_request(self, request):
+        request_data = get_request_data(request)
+
+        serializer = ApiInfoSerializer(data=request_data, partial=True)
+
+        if serializer.is_valid():
+            if request.user.id:
+                user = User.objects.get(id=request.user.id)
+                api_info = serializer.save(user=user)
+                self._api_info = api_info
+
+    def process_response(self, request, response):
+        if self._api_info:
+            api_info = set_response_data(self._api_info, response)
+            task_user_behavior(api_info)
+        return response
---
+++ packages/user_behavior/user_behavior-1.2.5.zip/user_behavior-1.2.5/user_behavior/urls.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from django.conf.urls import url, include
+
+from rest_framework import routers
+
+from .views import ApiInfoViewSet, UserBehaviorViewSet
+
+router = routers.DefaultRouter(trailing_slash=False)
+
+router.register(r'api_info', ApiInfoViewSet)
+router.register(r'user_behavior', UserBehaviorViewSet)
+
+url_patterns = [
+    url(r'', include(router.urls)),
+]

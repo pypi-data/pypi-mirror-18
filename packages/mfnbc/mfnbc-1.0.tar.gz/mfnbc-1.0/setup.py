906d077100365f24bdec35e84331ef9ede059c58
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:20:52 +0000

    Add package mfnbc/1.0/mfnbc-1.0.tar.gz

---
+++ packages/mfnbc/mfnbc-1.0.tar.gz/mfnbc-1.0/setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+from setuptools import setup
+setup(
+    name='mfnbc',
+    version='1.00',
+    license='The MIT License (MIT)',
+    author="Shawn",
+    author_email='shawnzam@gmail.com',
+    url='https://github.com/shawnzam/mfnbc',
+    packages=['mfnbc'],
+    install_requires=['nltk>=3.2'],
+    keywords=['bayes'],
+)

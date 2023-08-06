02ce0ef64d5a91bddf2fe6d7eeb8b79bb34e3c1a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:09:06 +0000

    Add package oct2py/3.6.0/oct2py-3.6.0.tar.gz

---
+++ packages/oct2py/oct2py-3.6.0.tar.gz/oct2py-3.6.0/oct2py/__pycache__/_make_figs.m
@@ -0,0 +1,19 @@
+
+function _make_figs(plot_dir, plot_name, plot_format, plot_res)
+% Get all the figures that are currently open.
+figHandles = get(0, 'children');
+
+for _fig=1:length(_figHandles),
+    _handle = _figHandles(_fig);
+    _filename = sprintf('%s/%s%03d.%s', plot_dir, plot_name, _fig + %s, plot_format);
+    try
+       _image = double(get(get(get(_handle,'children'),'children'),'cdata'));
+       _clim = get(get(_handle,'children'),'clim');
+       _image = _image - _clim(1);
+       _image = _image ./ (_clim(2) - _clim(1));
+       imwrite(uint8(_image*255), _filename);
+    catch
+        print(_handle, _filename, sprintf('-r%s', plot_res));
+    close(_handle);
+end;
+

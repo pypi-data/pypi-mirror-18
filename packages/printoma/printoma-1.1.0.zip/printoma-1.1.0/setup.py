341f183511bb0130f014aaee4f0e1a4ef2325ecf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:09:02 +0000

    Add package printoma/1.1.0/printoma-1.1.0.zip

---
+++ packages/printoma/printoma-1.1.0.zip/printoma-1.1.0/setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+
+
+setup (
+
+
+            name = 'printoma',
+            version = '1.1.0',
+            py_modules =['printoma'],
+            author = 'Ademola Oduguwa',
+            author_email='ademola.oduguwa@gmail.com',
+            url = ' ',
+            description ='A simple nested list printer2',
+
+      )

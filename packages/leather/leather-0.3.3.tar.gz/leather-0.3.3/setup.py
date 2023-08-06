6f2cd9d34a52076d08b0e0c17a3bb201d5ee4218
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:11:31 +0000

    Add package leather/0.3.3/leather-0.3.3.tar.gz

---
+++ packages/leather/leather-0.3.3.tar.gz/leather-0.3.3/setup.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+install_requires = [
+    'six>=1.6.1'
+]
+
+setup(
+    name='leather',
+    version='0.3.3',
+    description='Python charting for 80% of humans.',
+    long_description=open('README.rst').read(),
+    author='Christopher Groskopf',
+    author_email='chrisgroskopf@gmail.com',
+    url='http://leather.readthedocs.io/',
+    license='MIT',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Framework :: IPython',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: MIT License',
+        'Topic :: Multimedia :: Graphics',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy',
+        'Topic :: Scientific/Engineering :: Information Analysis',
+        'Topic :: Scientific/Engineering :: Visualization',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+    packages=[
+        'leather',
+        'leather.scales',
+        'leather.series',
+        'leather.shapes',
+        'leather.ticks'
+    ],
+    install_requires=install_requires
+)

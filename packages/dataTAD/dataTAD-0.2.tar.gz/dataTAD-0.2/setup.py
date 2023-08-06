468eb3b41de94b52eb6c0ab1a4eb37aeaa5ad540
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:06:31 +0000

    Add package dataTAD/0.2/dataTAD-0.2.tar.gz

---
+++ packages/dataTAD/dataTAD-0.2.tar.gz/dataTAD-0.2/setup.py
@@ -0,0 +1,18 @@
+
+from setuptools import setup
+
+def readme():
+    with open('README.rst') as f:
+        return f.read()
+
+setup(name='dataTAD',
+      version='0.2',
+      description='A module with sample data to learn Text as Data for Social Science Research',
+      url='http://andreucasas.com/text_as_data/',
+      author='Andreu Casas',
+      author_email='acasas2@uw.edu',
+      license='MIT',
+      packages=['dataTAD'],
+      install_requires=[],
+      zip_safe=False,
+      include_package_data=True)

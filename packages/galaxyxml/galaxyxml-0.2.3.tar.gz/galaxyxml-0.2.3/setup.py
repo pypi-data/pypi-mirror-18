b93386e983100a363b0eba510a8b8472e9ceb9a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:09:27 +0000

    Add package galaxyxml/0.2.3/galaxyxml-0.2.3.tar.gz

---
+++ packages/galaxyxml/galaxyxml-0.2.3.tar.gz/galaxyxml-0.2.3/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+from pip.req import parse_requirements
+import sys, os
+
+if sys.argv[-1] == 'publish':
+    os.system("python setup.py sdist bdist_wheel upload; git push")
+    sys.exit()
+
+setup(name="galaxyxml",
+        version='0.2.3',
+        description='Galaxy XML generation library',
+        author='Eric Rasche',
+        author_email='esr@tamu.edu',
+        install_requires=['lxml'],
+        packages=["galaxyxml", "galaxyxml.tool", "galaxyxml.tool.parameters"],
+        classifiers=[
+            'Development Status :: 3 - Alpha',
+            'Operating System :: OS Independent',
+            'Intended Audience :: Developers',
+            'Environment :: Console',
+            'License :: OSI Approved :: Apache Software License',
+            ],
+        )

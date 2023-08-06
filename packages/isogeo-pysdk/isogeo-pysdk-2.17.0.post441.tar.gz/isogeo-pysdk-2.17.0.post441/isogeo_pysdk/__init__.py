daaf894a4f53f682132a04bde0113e3ab53fd23c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:11:14 +0000

    Add package isogeo-pysdk/2.17.0.post441/isogeo-pysdk-2.17.0.post441.tar.gz

---
+++ packages/isogeo-pysdk/isogeo-pysdk-2.17.0.post441.tar.gz/isogeo-pysdk-2.17.0.post441/isogeo_pysdk/__init__.py
@@ -0,0 +1,11 @@
+﻿#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+    This module is an abstraction calls about the Isogeo REST API.
+    http://www.isogeo.com/
+"""
+
+__version__ = "2.17.0-441"
+
+from isogeo_sdk import Isogeo

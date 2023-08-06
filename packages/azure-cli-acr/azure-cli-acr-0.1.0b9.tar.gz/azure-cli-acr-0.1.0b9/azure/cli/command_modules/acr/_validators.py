df874a2ddd54ff98f3d02b7cd16b98e01addae87
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:09:06 +0000

    Add package azure-cli-acr/0.1.0b9/azure-cli-acr-0.1.0b9.tar.gz

---
+++ packages/azure-cli-acr/azure-cli-acr-0.1.0b9.tar.gz/azure-cli-acr-0.1.0b9/azure/cli/command_modules/acr/_validators.py
@@ -0,0 +1,23 @@
+#---------------------------------------------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License. See License.txt in the project root for license information.
+#---------------------------------------------------------------------------------------------
+
+from azure.cli.core._util import CLIError
+
+from ._factory import get_arm_service_client
+
+import azure.cli.core._logging as _logging
+logger = _logging.get_az_logger(__name__)
+
+def validate_resource_group_name(namespace):
+    if namespace.resource_group_name:
+        client = get_arm_service_client()
+        resource_group_name = namespace.resource_group_name
+
+        if not client.resource_groups.check_existence(resource_group_name):
+            logger.warning('Command to create a resource group:')
+            logger.warning('  az resource group create -n <name> -l <location>')
+            raise CLIError(
+                'The resource group {} does not exist in the current subscription.'\
+                .format(resource_group_name))

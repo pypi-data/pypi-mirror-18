482e2f35b6924c3d9f4fbb47e9a3c4a735b3dc54
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:07:27 +0000

    Add package quickstartup/0.12.3/quickstartup-0.12.3.tar.gz

---
+++ packages/quickstartup/quickstartup-0.12.3.tar.gz/quickstartup-0.12.3/quickstartup/qs_website/middleware.py
@@ -0,0 +1,26 @@
+# coding: utf-8
+
+
+from django.conf import settings
+from django.http import Http404
+
+from .views import website_page
+
+
+def website_page_middleware(get_response):
+    def middleware(request):
+        response = get_response(request)
+        if response.status_code != 404:
+            return response
+
+        # noinspection PyBroadException
+        try:
+            return website_page(request, request.path_info)
+        except Http404:
+            return response
+        except Exception:
+            if settings.DEBUG:
+                raise
+            return response
+
+    return middleware

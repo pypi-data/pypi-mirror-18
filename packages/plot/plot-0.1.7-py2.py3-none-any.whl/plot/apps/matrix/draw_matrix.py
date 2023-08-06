bb3621bf457053ee116772ce64caa41b38fc6bc5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:12:51 +0000

    Add package plot/0.1.7/plot-0.1.7-py2.py3-none-any.whl

---
+++ packages/plot/plot-0.1.7-py2.py3-none-any.whl/plot/apps/matrix/draw_matrix.py
@@ -0,0 +1,37 @@
+"""
+Draw a matrix
+"""
+from typing import Dict
+from numpy import ndarray
+import matplotlib.pyplot as pyplot
+from .set_aspect_ratio import set_aspect_ratio
+from .set_axis_extent import set_axis_extent
+
+
+def draw_matrix(
+        obj_axis,         # type: object
+        data,             # type: ndarray
+        p                 # type: Dict
+        ):
+    # type: (...) -> object
+    """Draw a matrix
+
+    Args:
+        obj_axis (object): a matplotlib axis object
+        data (ndarray): a numpy ndarray object
+        p (dict): a python dictionary
+
+    Returns:
+        a `matplotlib.image.AxesImage` object
+    """
+    obj = obj_axis.matshow(
+        data,
+        vmin=p['matrix']['vertical']['min'],
+        vmax=p['matrix']['vertical']['max'],
+        interpolation=p['matrix']['interpolation'],
+        origin=p['matrix']['origin'],
+        cmap=pyplot.get_cmap(p['matrix']['color_map']))
+    set_aspect_ratio(obj_axis)
+    set_axis_extent(obj, p)
+    obj_axis.xaxis.set_ticks_position('bottom')
+    return obj
---
+++ packages/plot/plot-0.1.7-py2.py3-none-any.whl/plot/io/output/saveFigure.py
@@ -0,0 +1,26 @@
+"""
+Save the matplotlib 'Figure' object into
+an image file.
+"""
+from matplotlib.figure import Figure
+
+
+def saveFigure(params):
+    # type:(Dict) -> bool
+    """Save figure
+
+    Args:
+        params (dict): plotting parameter dictionary
+
+    Returns:
+        same as input
+    """
+    obj_figure = params['internal']['canvas']['figure']
+    obj_figure.savefig(
+            params['global']['figure']['output'],
+            bbox_inches='tight',
+            dpi=params['global']['figure']['dpi'],
+            pad_inches=params['global']['figure']['padding'],
+            transparent=params['global']['figure']['transparent'],
+        )
+    return params

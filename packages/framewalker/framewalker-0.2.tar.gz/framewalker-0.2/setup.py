ccc2aa1d1eedc974785cc9643d5d67134cb9372a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:21:07 +0000

    Add package framewalker/0.2/framewalker-0.2.tar.gz

---
+++ packages/framewalker/framewalker-0.2.tar.gz/framewalker-0.2/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='framewalker',
+    version='0.2',
+    url='https://github.com/stefanbie/FrameWalker',
+    install_requires=[
+        'peewee',
+        'PyMySQL',
+    ],
+    )

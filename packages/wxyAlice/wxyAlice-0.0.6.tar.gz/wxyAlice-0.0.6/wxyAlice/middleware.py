b6c674aec1db4aa9fc211cbb5bbedbad705641c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:10:36 +0000

    Add package wxyAlice/0.0.6/wxyAlice-0.0.6.tar.gz

---
+++ packages/wxyAlice/wxyAlice-0.0.6.tar.gz/wxyAlice-0.0.6/wxyAlice/middleware.py
@@ -0,0 +1,37 @@
+import falcon
+import json
+
+
+class Head(object):
+    def process_request(self, req, resp):
+        if 'application/json' not in req.content_type:
+            raise falcon.HTTPUnsupportedMediaType('111', href='')
+            if not req.client_accepts_json:
+                raise falcon.HTTPNotAcceptable('222', href='')
+        return
+
+
+class Wrap(object):
+    def process_request(self, req, resp):
+        if req.content_length in (None, 0):
+            req.context['doc'] = None
+            return
+        try:
+            body = req.stream.read()
+            req.context['doc'] = json.loads(body.decode('utf-8'))
+        except:
+            raise falcon.HTTPBadRequest('can not decode body', 'bbb')
+
+    def process_response(self, req, resp, resource):
+        if 'ret' not in req.context:
+            return
+        resp.body = json.dumps(req.context['ret'])
+
+
+class Cors(object):
+    def process_request(self, req, resp):
+        if req.method == 'OPTIONS':
+            resp.set_header('access-control-allow-origin', '*')
+            resp.set_header('access-control-allow-methods', '*')
+            resp.set_header('access-control-allow-credentials', 'true')
+        return

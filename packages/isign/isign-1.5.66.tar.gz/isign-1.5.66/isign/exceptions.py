5eedd2ce632cfcdfc825315dc58688b31af9161d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:21:18 +0000

    Add package isign/1.5.66/isign-1.5.66.tar.gz

---
+++ packages/isign/isign-1.5.66.tar.gz/isign-1.5.66/isign/exceptions.py
@@ -0,0 +1,28 @@
+""" Some common exceptions """
+
+
+class NotSignable(Exception):
+    """ superclass for any reason why app shouldn't be
+        signable """
+    pass
+
+
+class NotMatched(NotSignable):
+    """ thrown if we can't find any app class for
+        this file path """
+    pass
+
+
+class MissingCredentials(Exception):
+    """ thrown if credentials are missing """
+    pass
+
+
+class ImproperCredentials(Exception):
+    """ thrown if something looks fishy about credentials """
+    pass
+
+
+class OpenSslFailure(Exception):
+    """ something is wrong with openssl output """
+    pass

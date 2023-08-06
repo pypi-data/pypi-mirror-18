8922c008d195012ef4cd20a2498f81a3e98163ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:09:27 +0000

    Add package Flask-RESTy/0.10.6/Flask_RESTy-0.10.6-py2.py3-none-any.whl

---
+++ packages/Flask-RESTy/Flask_RESTy-0.10.6-py2.py3-none-any.whl/flask_resty/compat.py
@@ -0,0 +1,11 @@
+import sys
+import itertools
+
+PY2 = int(sys.version_info[0]) == 2
+
+if PY2:
+    zip_longest = itertools.izip_longest
+    basestring = basestring  # noqa
+else:
+    zip_longest = itertools.zip_longest
+    basestring = (str, bytes)

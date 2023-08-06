be212b0e25104cefe06b34a7b3cdfc89967779af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:45:00 +0000

    Add package nester_paryzowy/1.0.0/nester_paryzowy-1.0.0.tar.gz

---
+++ packages/nester_paryzowy/nester_paryzowy-1.0.0.tar.gz/nester_paryzowy-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name='nester_paryzowy',
+    version='1.0.0',
+    py_modules=['nester'],
+    author='me',
+    author_email='test@sss.pl',
+    url='http://www.www.www',
+    description='A simple printer of nested lists',
+)

30bab0c303d0a70862292cb2fc000961a67cf771
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:06:26 +0000

    Add package django-http-cat/0.5/django-http-cat-0.5.zip

---
+++ packages/django-http-cat/django-http-cat-0.5.zip/django-http-cat-0.5/httpcat/middleware.py
@@ -0,0 +1,11 @@
+from django.http import HttpResponse
+from django.conf import settings
+
+OK_CODES = getattr(settings, "HTTP_OK_CODES", [200, 304])
+
+class HttpCatErrorHandler(object):
+    def process_response(self, request, response):
+        if response.status_code not in OK_CODES:
+            url = "https://http.cat/" + str(response.status_code)
+            return HttpResponse('<img src="{0}">'.format(url), status=response.status_code)
+        return response

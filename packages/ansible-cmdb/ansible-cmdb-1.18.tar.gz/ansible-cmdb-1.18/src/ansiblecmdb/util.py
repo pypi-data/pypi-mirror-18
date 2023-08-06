1e11d7fcde62bc3bb6a44f702f6e87c97762d392
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:20:39 +0000

    Add package ansible-cmdb/1.18/ansible-cmdb-1.18.tar.gz

---
+++ packages/ansible-cmdb/ansible-cmdb-1.18.tar.gz/ansible-cmdb-1.18/src/ansiblecmdb/util.py
@@ -0,0 +1,34 @@
+import copy
+
+
+def deepupdate(target, src):
+    """Deep update target dict with src
+    For each k,v in src: if k doesn't exist in target, it is deep copied from
+    src to target. Otherwise, if v is a list, target[k] is extended with
+    src[k]. If v is a set, target[k] is updated with v, If v is a dict,
+    recursively deep-update it.
+
+    Examples:
+    >>> t = {'name': 'Ferry', 'hobbies': ['programming', 'sci-fi']}
+    >>> deepupdate(t, {'hobbies': ['gaming']})
+    >>> print t
+    {'name': 'Ferry', 'hobbies': ['programming', 'sci-fi', 'gaming']}
+    """
+    for k, v in src.items():
+        if type(v) == list:
+            if not k in target:
+                target[k] = copy.deepcopy(v)
+            else:
+                target[k].extend(v)
+        elif type(v) == dict:
+            if not k in target:
+                target[k] = copy.deepcopy(v)
+            else:
+                deepupdate(target[k], v)
+        elif type(v) == set:
+            if not k in target:
+                target[k] = v.copy()
+            else:
+                target[k].update(v.copy())
+        else:
+            target[k] = copy.copy(v)

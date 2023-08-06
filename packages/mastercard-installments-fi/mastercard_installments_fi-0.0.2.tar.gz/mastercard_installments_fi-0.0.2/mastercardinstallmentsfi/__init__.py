922b4988d1199f7d25a6b125b4003ee511851693
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:19:16 +0000

    Add package mastercard-installments-fi/0.0.2/mastercard_installments_fi-0.0.2.tar.gz

---
+++ packages/mastercard-installments-fi/mastercard_installments_fi-0.0.2.tar.gz/mastercard_installments_fi-0.0.2/mastercardinstallmentsfi/__init__.py
@@ -0,0 +1,5 @@
+from mastercardapicore import *
+from mastercardinstallmentsfi.eligibilityandinstallmentplan import *
+from mastercardinstallmentsfi.eligibilityandcalculator import *
+from mastercardinstallmentsfi.backgroundconfiguration import *
+from mastercardinstallmentsfi.resourceconfig import *

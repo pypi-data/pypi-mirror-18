2807a82110e1362e6382f39dfaaf38883ad94ad1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:10:15 +0000

    Add package linhanqiu/1.2.0/linhanqiu-1.2.0.zip

---
+++ packages/linhanqiu/linhanqiu-1.2.0.zip/linhanqiu-1.2.0/linhanqiu.py
@@ -0,0 +1,10 @@
+'''this is a way to print long_levels_list
+    hehe'''
+def print_list(the_list,level=0):
+    for each_item in the_list:
+        if isinstance(each_item,list):
+            print_list(each_item,level+1)
+        else:
+            for tab_Zstop in range(level):
+                print ('\t')
+            print each_item
---
+++ packages/linhanqiu/linhanqiu-1.2.0.zip/linhanqiu-1.2.0/setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+setup(
+    name = 'linhanqiu',
+    version = '1.2.0',
+    py_modules = ['linhanqiu'],
+    author = 'linhanqiu',
+    author_email = 'linhanqiu1123@163.com',
+
+)

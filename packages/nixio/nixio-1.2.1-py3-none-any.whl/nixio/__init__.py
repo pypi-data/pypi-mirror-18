56a1acfa1680ec08e52f5698be26f8bb22dad91a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:10:59 +0000

    Add package nixio/1.2.1/nixio-1.2.1-py3-none-any.whl

---
+++ packages/nixio/nixio-1.2.1-py3-none-any.whl/nixio/__init__.py
@@ -0,0 +1,26 @@
+# Copyright (c) 2014, German Neuroinformatics Node (G-Node)
+#
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted under the terms of the BSD License. See
+# LICENSE file in the root of the Project.
+
+from __future__ import (absolute_import, division, print_function)#, unicode_literals)
+
+from nixio.pycore.file import File, FileMode
+from nixio.value import Value, DataType
+from nixio.dimension_type import DimensionType
+from nixio.link_type import LinkType
+
+from nixio.section import S
+
+try:
+    import nixio.util.inject
+except ImportError:
+    pass
+
+__all__ = ("File", "FileMode", "DataType", "Value", "LinkType", "DimensionType")
+
+__author__ = ('Christian Kellner, Adrian Stoewer, Andrey Sobolev, Jan Grewe,'
+              ' Balint Morvai')

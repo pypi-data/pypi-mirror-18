e3080eef5f642a3041c3e638671efc472a3cb99c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:20:05 +0000

    Add package musicnow/2.3/musicnow-2.3.tar.gz

---
+++ packages/musicnow/musicnow-2.3.tar.gz/musicnow-2.3/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+setup(name='musicnow',
+      version='2.3',
+      description='Lets you download music with album art and metadata',
+      url='https://github.com/lakshaykalbhor/MusicNow',
+      author='Lakshay Kalbhor',
+      author_email='lakshaykalbhor@gmail.com',
+      license='MIT',
+      packages =['musicnow'],
+      install_requires=[
+          'youtube-dl',
+          'bs4',
+          'mutagen',
+          'requests',
+          'spotipy',
+          'six',
+          'colorama',
+      ],
+      entry_points={
+        'console_scripts': ['musicnow=musicnow.command_line:main'],
+      }
+      )

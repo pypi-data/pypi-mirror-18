d7e038b5c4bbcc5fbf9f69b00f2f7dce3e6e3cfd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:07:21 +0000

    Add package cgcloud-toil/1.6.0a1.dev403/cgcloud-toil-1.6.0a1.dev403.tar.gz

---
+++ packages/cgcloud-toil/cgcloud-toil-1.6.0a1.dev403.tar.gz/cgcloud-toil-1.6.0a1.dev403/src/cgcloud/toil/__init__.py
@@ -0,0 +1,12 @@
+def roles( ):
+    from cgcloud.toil.toil_box import (ToilLegacyBox,
+                                       ToilBox,
+                                       ToilLatestBox,
+                                       ToilLeader,
+                                       ToilWorker)
+    return sorted( locals( ).values( ), key=lambda cls: cls.__name__ )
+
+
+def cluster_types( ):
+    from cgcloud.toil.toil_cluster import ToilCluster
+    return sorted( locals( ).values( ), key=lambda cls: cls.__name__ )

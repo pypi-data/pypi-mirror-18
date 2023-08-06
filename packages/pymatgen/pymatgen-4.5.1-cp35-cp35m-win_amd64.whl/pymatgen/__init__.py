3ec3b24317241b5253986bbd6c320de751c8309d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:07:55 +0000

    Add package pymatgen/4.5.1/pymatgen-4.5.1-cp35-cp35m-win_amd64.whl

---
+++ packages/pymatgen/pymatgen-4.5.1-cp35-cp35m-win_amd64.whl/pymatgen/__init__.py
@@ -0,0 +1,49 @@
+from __future__ import unicode_literals
+
+import os
+import warnings
+
+__author__ = "Pymatgen Development Team"
+__email__ ="pymatgen@googlegroups.com"
+__maintainer__ = "Shyue Ping Ong"
+__maintainer_email__ ="shyuep@gmail.com"
+__date__ = "Nov 15 2016"
+__version__ = "4.5.1"
+
+
+SETTINGS_FILE = os.path.join(os.path.expanduser("~"), ".pmgrc.yaml")
+
+
+def _load_pmg_settings():
+    if os.path.exists(SETTINGS_FILE):
+        try:
+            import yaml
+            with open(SETTINGS_FILE, "rt") as f:
+                return yaml.load(f)
+        except:
+            # If there are any errors, default to using environment variables
+            # if present.
+            pass
+    d = {}
+    for k in ["VASP_PSP_DIR", "MAPI_KEY"]:
+        if k in os.environ:
+            warnings.warn("You have %s set in the env. From pmg 5, all "
+                          "settings should be in the .pmgrc.yaml file." % k)
+        d[k] = os.environ.get(k)
+    return d
+
+SETTINGS = _load_pmg_settings()
+
+# Order of imports is important on some systems to avoid
+# failures when loading shared libraries.
+# import spglib
+# from . import optimization, util
+# del(spglib, optimization, util)
+
+# Useful aliases for commonly used objects and modules.
+# Allows from pymatgen import <class> for quick usage.
+
+from pymatgen.core import *
+from .electronic_structure.core import Spin, Orbital
+from .matproj.rest import MPRester
+from monty.json import MontyEncoder, MontyDecoder, MSONable

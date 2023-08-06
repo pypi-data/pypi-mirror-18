6fcac5f41ad792133670cd34161842255bec021b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:13:41 +0000

    Add package toil-scripts/2.1.0a1.dev692/toil_scripts-2.1.0a1.dev692-py2.7.egg

---
+++ packages/toil-scripts/toil_scripts-2.1.0a1.dev692-py2.7.egg/toil_scripts/__init__.py
@@ -0,0 +1,21 @@
+import logging
+
+_log = logging.getLogger(__name__)
+
+# TODO: replace this with toil_lib.urls._download_s3_url
+
+def download_from_s3_url(file_path, url):
+    from urlparse import urlparse
+    from boto.s3.connection import S3Connection
+    s3 = S3Connection()
+    try:
+        parsed_url = urlparse(url)
+        if not parsed_url.netloc or not parsed_url.path.startswith('/'):
+            raise RuntimeError("An S3 URL must be of the form s3:/BUCKET/ or "
+                               "s3://BUCKET/KEY. '%s' is not." % url)
+        bucket = s3.get_bucket(parsed_url.netloc)
+        key = bucket.get_key(parsed_url.path[1:])
+        _log.info("From URL %s, parsed --> %s, bucket %s, key %s", url, parsed_url, bucket, key)
+        key.get_contents_to_filename(file_path)
+    finally:
+        s3.close()

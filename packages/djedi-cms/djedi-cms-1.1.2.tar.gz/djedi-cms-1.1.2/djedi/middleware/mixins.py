f35ad61080ad9763205e45f23a362dce27fe3ba4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:21:04 +0000

    Add package djedi-cms/1.1.2/djedi-cms-1.1.2.tar.gz

---
+++ packages/djedi-cms/djedi-cms-1.1.2.tar.gz/djedi-cms-1.1.2/djedi/middleware/mixins.py
@@ -0,0 +1,73 @@
+import json
+
+import cio
+
+from django.core.exceptions import ImproperlyConfigured
+from django.core.urlresolvers import reverse, NoReverseMatch
+from django.utils import translation
+
+from cio.pipeline import pipeline
+from djedi.auth import has_permission
+from djedi.compat import render_to_string
+
+
+class TranslationMixin(object):
+
+    def activate_language(self):
+        # Activate current django translation
+        language = translation.get_language()
+        cio.env.push_state(i18n=language)
+
+
+class AdminPanelMixin(object):
+
+    def inject_admin_panel(self, request, response):
+        user = getattr(request, 'user', None)
+
+        # Validate user permissions
+        if not has_permission(user):
+            return
+
+        # Do not inject admin panel in admin
+        try:
+            djedi_cms_url = reverse('admin:djedi:cms')
+        except NoReverseMatch:
+            raise ImproperlyConfigured('Could not find djedi in your url conf, '
+                                       'enable django admin or include '
+                                       'djedi.urls within the admin namespace.')
+        else:
+            admin_prefix = djedi_cms_url.strip('/').split('/')[0]
+            if request.path.startswith('/' + admin_prefix):
+                return
+
+        # Do not inject admin panel on gzipped responses
+        if 'gzip' in response.get('Content-Encoding', ''):
+            return
+
+        # Only inject admin panel in html pages
+        content_type = response.get('Content-Type', '').split(';')[0]
+        if content_type not in ('text/html', 'application/xhtml+xml'):
+            return
+
+        embed = self.render_cms()
+        self.body_append(response, embed)
+
+    def render_cms(self):
+        defaults = dict(
+            (node.namespace_uri.clone(version=None), node.initial)
+            for node in pipeline.history.list('get')
+        )
+        return render_to_string('djedi/cms/embed.html', {
+            'json_nodes': json.dumps(defaults).replace('</', '\\x3C/'),
+        })
+
+    def body_append(self, response, html):
+        idx = response.content.lower().rfind(b'</body>')
+
+        if idx >= 0:
+            response.content = b''.join((response.content[:idx],
+                                         html.encode('utf8'),
+                                         response.content[idx:]))
+
+            if response.get('Content-Length', None):
+                response['Content-Length'] = len(response.content)

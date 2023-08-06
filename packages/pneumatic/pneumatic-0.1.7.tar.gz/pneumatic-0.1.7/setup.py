26b9e1e80efbb002564e4704ca1524867b7fa617
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:45:51 +0000

    Add package pneumatic/0.1.7/pneumatic-0.1.7.tar.gz

---
+++ packages/pneumatic/pneumatic-0.1.7.tar.gz/pneumatic-0.1.7/setup.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+
+def readme():
+    with open('README.rst') as f:
+        return f.read()
+
+setup(
+    name='pneumatic',
+    version='0.1.7',
+    description='A bulk upload library for DocumentCloud.',
+    long_description=readme(),
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+    url='http://pneumatic.readthedocs.io/en/latest/',
+    author='Anthony DeBarros',
+    author_email='adebarros@gmail.com',
+    license='MIT',
+    packages=['pneumatic'],
+    install_requires=[
+        'requests',
+        'colorama'
+    ],
+    zip_safe=False
+)

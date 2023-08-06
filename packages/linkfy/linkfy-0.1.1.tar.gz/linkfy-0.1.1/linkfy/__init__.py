f73f246d3df0a941fca0bd7c740ea90c012908a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:22:50 +0000

    Add package linkfy/0.1.1/linkfy-0.1.1.tar.gz

---
+++ packages/linkfy/linkfy-0.1.1.tar.gz/linkfy-0.1.1/linkfy/__init__.py
@@ -0,0 +1,9 @@
+""":mod:`linky` --- linky
+~~~~~~~~~~~~~~~~~~~~~~~~~
+
+"""
+from .sub import linkfy
+
+__all__ = 'linkfy',
+__version_info__ = 0, 1, 1
+__version__ = '.'.join(str(v) for v in __version_info__)

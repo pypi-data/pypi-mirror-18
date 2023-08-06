fdebcedf5b249f360cc6b97585392f1755d7adbe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:09:53 +0000

    Add package zodbtools/0.0.0.dev1/zodbtools-0.0.0.dev1.tar.gz

---
+++ packages/zodbtools/zodbtools-0.0.0.dev1.tar.gz/zodbtools-0.0.0.dev1/setup.py
@@ -0,0 +1,47 @@
+# zodbtools | pythonic package setup
+from setuptools import setup, find_packages
+
+# read file content
+def readfile(path):
+    with open(path, 'r') as f:
+        return f.read()
+
+setup(
+    name        = 'zodbtools',
+    version     = '0.0.0.dev1',
+    description = 'ZODB-related utilities',
+    long_description = readfile('README.rst'),
+    url         = 'https://lab.nexedi.com/kirr/zodbtools',
+    license     = 'GPLv2+',
+    author      = 'Kirill Smelkov',
+    author_email= 'kirr@nexedi.com',
+
+    keywords    = 'zodb utility tool',
+
+    package_dir = {'wendelin': ''},
+    packages    = find_packages(),
+    install_requires = ['ZODB'],
+
+    # TODO have only one console program "zodb" and then it is
+    # zodb cmd ...
+    # zodb dump ...
+    entry_points= {'console_scripts': [
+                        'zodbcmp    = zodbtool.zodbcmp:main',
+                        'zodbdump   = zodbtool.zodbdump:main',
+                      ]
+                  },
+
+    classifiers = [_.strip() for _ in """\
+        Development Status :: 3 - Alpha
+        Intended Audience :: Developers
+        Operating System :: POSIX :: Linux
+        Programming Language :: Python :: 2
+        Programming Language :: Python :: 2.7
+        Programming Language :: Python :: 3
+        Programming Language :: Python :: 3.4
+        Programming Language :: Python :: 3.5
+        Topic :: Database
+        Topic :: Utilities
+        Framework :: ZODB\
+    """.splitlines()]
+)

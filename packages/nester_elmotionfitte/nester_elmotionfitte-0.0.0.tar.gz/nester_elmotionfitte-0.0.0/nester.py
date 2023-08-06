f952ba24661c0a4415527adaa6b7965b382384fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:18:31 +0000

    Add package nester_elmotionfitte/0.0.0/nester_elmotionfitte-0.0.0.tar.gz

---
+++ packages/nester_elmotionfitte/nester_elmotionfitte-0.0.0.tar.gz/nester_elmotionfitte-0.0.0/nester.py
@@ -0,0 +1,17 @@
+"""This is the "nester.py" module and it provides one function called print_lol()
+   which prints lists that may or may not include nested lists."""
+
+#import sys
+
+def print_lol(the_list, indent=False, level=0, write=sys.stdout):
+	"""
+	Prints a list of (possibly) nested lists with indentation.
+	                                                       """
+	for each_item in the_list:
+		if isinstance(each_item, list):
+			print_lol(each_item, indent, level+1, write)
+		else:
+			if indent:
+				for x in range(level):
+					print("\t", end='', file=write)
+			print (each_item, file=write)
---
+++ packages/nester_elmotionfitte/nester_elmotionfitte-0.0.0.tar.gz/nester_elmotionfitte-0.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup (
+		name 			= 'nester_elmotionfitte',
+		verison 		= '1.4.0',
+		py_modules		= ['nester'],
+		author			= 'elmotionfitte',
+		author_email	= 'taparaidhamkih@gmail.com',
+		url				= '',
+		description		= 'a simple printer of nested lists with 4 arguments',
+	  )

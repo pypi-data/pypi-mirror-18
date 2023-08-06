da32323212b4e6087c5c0237fccef1a7d1c4d662
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:19:56 +0000

    Add package duckduckpy/0.2/duckduckpy-0.2.tar.bz2

---
+++ packages/duckduckpy/duckduckpy-0.2.tar.bz2/duckduckpy-0.2/setup.py
@@ -0,0 +1,34 @@
+import duckduckpy
+from setuptools import setup
+
+with open('README.rst', 'r') as f:
+    long_description = f.read()
+
+setup(name='duckduckpy',
+      version=duckduckpy.__version__,
+      packages=['duckduckpy'],
+      description=duckduckpy.__doc__,
+      author=duckduckpy.__author__,
+      author_email=duckduckpy.__email__,
+      license=duckduckpy.__license__,
+      url=duckduckpy.__url__,
+      download_url='https://github.com/ivankliuk/duckduckpy/tarball/0.2',
+      long_description=long_description,
+      platforms=['any'],
+      keywords=["duckduckgo"],
+      classifiers=[
+          "Development Status :: 2 - Pre-Alpha",
+          "Intended Audience :: Developers",
+          "License :: OSI Approved :: MIT License",
+          "Operating System :: OS Independent",
+          'Programming Language :: Python',
+          'Programming Language :: Python :: 2',
+          'Programming Language :: Python :: 2.6',
+          'Programming Language :: Python :: 2.7',
+          'Programming Language :: Python :: 3',
+          'Programming Language :: Python :: 3.3',
+          'Programming Language :: Python :: 3.4',
+          'Programming Language :: Python :: 3.5',
+          "Topic :: Internet :: WWW/HTTP :: Indexing/Search"],
+      zip_safe=True,
+      test_suite="tests")

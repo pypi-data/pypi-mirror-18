3fa4662e1dcb67f7408174e79c6c4e69b92ca686
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:44:53 +0000

    Add package wharf/1.0.2/wharf-1.0.2-py3-none-any.whl

---
+++ packages/wharf/wharf-1.0.2-py3-none-any.whl/wharf/cli.py
@@ -0,0 +1,46 @@
+import click
+from . import settings, api
+
+
+@click.group()
+@click.pass_context
+def cli(ctx):
+    pass
+
+
+@cli.command()
+@click.argument('config_path')
+@click.argument('command', default='dev')
+def run(config_path, command='dev'):
+    exit_code = api.run(config_path, command, click.echo)
+    exit(exit_code)
+
+
+@cli.command()
+@click.argument("config_path")
+@click.argument("template")
+def create_configuration(config_path, template):
+    config = api.create_configuration(config_path, template)
+    click.echo(config)
+
+
+@cli.command()
+@click.argument("config_path")
+@click.option('--format', type=click.Choice(['systemd']), default='systemd')
+def create_init_file(config_path, format):
+    config = api.create_init_file(config_path, format)
+    click.echo(config)
+
+
+@cli.command()
+def version():
+    major, minor, patch = settings.VERSION
+
+    click.echo("Wharf version {}.{}.{} compiled on {}".format(
+        major,
+        minor,
+        patch,
+        settings.COMPILATION_DATE.strftime('%Y-%m-%d %H:%M:%S')
+    ))
+
+
---
+++ packages/wharf/wharf-1.0.2-py3-none-any.whl/wharf/settings.py
@@ -0,0 +1,4 @@
+import datetime
+
+VERSION=(1, 0, 2)
+COMPILATION_DATE = datetime.datetime.fromtimestamp(1481058061.0409334)

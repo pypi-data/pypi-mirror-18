1f4fb7e45cfdfc831277cc1d98e681e85c0a177e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:19:01 +0000

    Add package nirvanachow/1.1.0/nirvanachow-1.1.0.zip

---
+++ packages/nirvanachow/nirvanachow-1.1.0.zip/nirvanachow-1.1.0/nester.py
@@ -0,0 +1,13 @@
+'''这是“nster.py”模块，提供了一个名为print_lol()的函数，这个函数的作用是
+   打印列表，其中有可能包含（也可能不包含）嵌套列表。'''
+def print_lol(the_list, length):
+    '''这个函数有一个位置参数，名为the_list，这可以是任何python列表（可以嵌套列表）。
+       所指定的列表中的每个数据项都会（递归地）输出到屏幕上，各数据项各占一行。'''
+    
+    for each_item in the_list:
+        if isinstance(each_item, list):
+            print_lol(each_item, length*2)
+        else:
+            for num in range(length-1):
+                print('\t', end='')
+            print(each_item)

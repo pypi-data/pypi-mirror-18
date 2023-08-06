c7bdd786a294ad55f5d3db37f22401cc9af42221
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:31:20 +0000

    Add package ish-parser/0.0.20/ish_parser-0.0.20.tar.gz

---
+++ packages/ish-parser/ish_parser-0.0.20.tar.gz/ish_parser-0.0.20/tests/ComponentTest.py
@@ -0,0 +1,53 @@
+import unittest
+from src import CloudCoverage, Minutes, Irradiance
+from src.Components import *
+
+class SkyCoverSummationComponentTest(unittest.TestCase):
+
+  def test_string(self):
+    simple_string = '4991+0061059'
+    sd = SkyCoverSummationComponent()
+    sd.loads(simple_string)
+    self.assertEqual(sd.sky_cover_summation, {'coverage': 'OVERCAST - 8/8 coverage',
+                                               'secondary_coverage': 'Missing',
+                                               'height': 610,
+                                               'characteristic': 'Missing'})
+
+class SnowDepthComponentTest(unittest.TestCase):
+
+  def test_string(self):
+    simple_string = '00089500007694'
+    sd = SnowDepthComponent()
+    sd.loads(simple_string)
+    self.assertEqual(sd.snow_depth, {'depth': 8, 'quality': '5', 'condition': '9'})
+
+class SkyCoverComponentTest(unittest.TestCase):
+
+  def test_string(self):
+    sample_string = '005+999999999'
+    sky = SkyCoverComponent()
+    sky.loads(sample_string)
+    self.assertEqual(sky.sky_cover['coverage'], CloudCoverage('00', CloudCoverage.OKTA, '5'))
+
+class SolarIrradianceComponentTest(unittest.TestCase):
+  
+  def test_string(self):
+    sample_string = '006010140690989029013102999999'
+    sol = SolarIrradianceComponent()
+    sol.loads(sample_string)
+    self.assertEqual(sol.solar_irradiance['time_period'], Minutes(60))
+    self.assertEqual(sol.solar_irradiance['global_irradiance'], Irradiance('1014'))
+    self.assertEqual(sol.solar_irradiance['irradiance_data_flag'], 'Value estimated; passes all pertinent SERI_QC tests')
+    self.assertEqual(sol.solar_irradiance['direct_beam_irradiance'], Irradiance('0989'))
+    self.assertEqual(sol.solar_irradiance['uvb_global_irradiance'], Irradiance('9999'))
+
+class SkyConditionObservationComponentTest(unittest.TestCase):
+
+  def test_string(self):
+    sample_string = '08991999999999999999999'
+    sky = SkyConditionObservationComponent()
+    sky.loads(sample_string)
+    self.assertEqual(sky.sky_condition_observation['total_coverage'],
+                      CloudCoverage('08', CloudCoverage.OKTA, '1'))
+    self.assertEqual(sky.sky_condition_observation['total_lowest_coverage'],
+                      CloudCoverage('99', CloudCoverage.OKTA, '9'))
---
+++ packages/ish-parser/ish_parser-0.0.20.tar.gz/ish_parser-0.0.20/tests/ish_parser_test.py
@@ -0,0 +1,86 @@
+import unittest
+import logging
+from src.ish_parser import ish_parser
+from src.ish_report import ish_report
+
+LOG = logging.basicConfig(level=logging.ERROR)
+
+class ish_parser_test(unittest.TestCase):
+
+  ORD_FILE = 'tests/725300.txt'
+  AUS_FILE = 'tests/722540-13904-2014'
+  OTHER_RANDOM = 'tests/010060-99999-2014'
+  RECURSIONBUG = 'tests/035480-99999-1943'
+  OTHER_BUG = 'tests/723030-13714-1973'
+  OLDRANDOMFILE = 'tests/725300-94846-1983'
+  AT1_ERROR = 'tests/726430-14920-2015'
+
+  def test_from_file(self):
+    ''' test that we can load a weather file from a file '''
+    with open(self.ORD_FILE) as fp:
+      content = fp.read()
+    wf = ish_parser()
+    wf.loads(content)
+    self.assertEqual(len(wf.get_reports()), 4262)
+    self.assertEqual(type(wf.get_reports()[10]), ish_report)
+    self.assertEqual(len(wf.get_observations()), 3135)
+
+  def test_issues_with_missing_at1(self):
+    with open(self.AT1_ERROR) as fp:
+      content = fp.read()
+    wf = ish_parser()
+    wf.loads(content)
+    self.assertEqual(len(wf.get_reports()), 154)
+
+  def test_random_old_file(self):
+    ''' test that we can load another random old file with no problems 
+    from 30 years ago '''
+    with open(self.OLDRANDOMFILE) as fp:
+      content = fp.read()
+    wf = ish_parser()
+    wf.loads(content)
+    self.assertEqual(len(wf.get_reports()), 8760)
+    self.assertEqual(type(wf.get_reports()[10]), ish_report)
+    self.assertEqual(len(wf.get_observations()), 7466)
+
+  def test_file_throwing_problems(self):
+    ''' test a file that was getting stuck in crazy infinite recursion '''
+    with open(self.RECURSIONBUG) as fp:
+      content = fp.read()
+    wf = ish_parser()
+    wf.loads(content)
+    self.assertEqual(len(wf.get_reports()), 4410)
+    self.assertEqual(type(wf.get_reports()[10]), ish_report)
+    
+    one_report = wf.get_reports()[22]
+    self.assertEqual(one_report.air_temperature.get_fahrenheit(), 'MISSING')
+
+  def test_random_other_file(self):
+    ''' test that we can load another random old file with no problems 
+    from 30 years ago '''
+    with open(self.OTHER_RANDOM) as fp:
+      content = fp.read()
+    wf = ish_parser()
+    wf.loads(content)
+    self.assertEqual(len(wf.get_reports()), 2816)
+    self.assertEqual(type(wf.get_reports()[10]), ish_report)
+
+  def test_another_weird_file(self):
+    ''' test that we can load another random old file with no problems 
+    from 30 years ago '''
+    with open(self.OTHER_BUG) as fp:
+      content = fp.read()
+    wf = ish_parser()
+    wf.loads(content)
+    self.assertEqual(len(wf.get_reports()), 8580)
+    one_report = wf.get_reports()[22]
+    self.assertEqual(one_report.air_temperature.get_fahrenheit(), 64.4)
+
+  def test_other_airport(self):
+    with open(self.AUS_FILE) as fp:
+      content = fp.read()
+    wf = ish_parser()
+    wf.loads(content)
+    self.assertEqual(len(wf.get_reports()), 4237)
+    self.assertEqual(type(wf.get_reports()[10]), ish_report)
+    self.assertEqual(len(wf.get_observations()), 3333)

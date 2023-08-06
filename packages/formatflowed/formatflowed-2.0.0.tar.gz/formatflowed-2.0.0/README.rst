e05cb71170f16568becb7e5314046b59721cb43d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:12:03 +0000

    Add package formatflowed/2.0.0/formatflowed-2.0.0.tar.gz

---
+++ packages/formatflowed/formatflowed-2.0.0.tar.gz/formatflowed-2.0.0/README.rst
@@ -0,0 +1,48 @@
+===============
+formatflowed.py
+===============
+
+Introduction
+------------
+
+The formatflowed.py python library provides en- and decoding functionality for 
+`RFC 2646`_ and `RFC 3676`_ text, also called format=flowed text. The 
+development of this library was generously sponsored by `Logicalware`_, and
+was written by `Martijn Pieters <mj@zopatista.com>`_.
+
+The latest version can be downloaded from the `pypi page`_; the code repository 
+is hosted `GitHub`_.
+
+.. image:: https://travis-ci.org/mjpieters/formatflowed.svg?branch=master
+    :target: https://travis-ci.org/mjpieters/formatflowed
+
+.. _RFC 2646: http://www.faqs.org/rfcs/rfc2646.html
+.. _RFC 3676: http://www.faqs.org/rfcs/rfc3676.html
+.. _Logicalware: http://www.logicalware.com/
+.. _pypi page: http://pypi.python.org/pypi/formatflowed
+.. _GitHub: https://github.com/mjpieters/formatflowed
+
+
+Requirements
+------------
+
+formatflowed.py has been tested with python versions 2.6 - 2.7, 3.3 - 3.6 and
+pypy and pypy3. Installation requires setuptools.
+
+
+Installation
+------------
+
+Use the standard setuptools installation script provided::
+
+ python setup.py install
+
+or install the package as an egg::
+
+ pip install formatflowed 
+ 
+
+Usage
+-----
+
+Further documentation is embedded in the docstrings of the module.
---
+++ packages/formatflowed/formatflowed-2.0.0.tar.gz/formatflowed-2.0.0/setup.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python
+"""Distutils setup for formatflowed.py"""
+
+# Copyright (C) 2005-2012 Martijn Pieters
+# Written by Martijn Pieters <mj@zopatista.com>
+# Development was sponsored by Logicalware (http://www.logicalware.org/)
+# Licensed as Open Source under the same terms as the Python 2.4.1 license,
+# as available at http://www.python.org/2.4.1/license.html
+
+import os
+from setuptools import setup
+
+
+version = '2.0.0'
+
+install_requires = [
+    'setuptools',
+]
+
+
+def read(*rnames):
+    with open(os.path.join(os.path.dirname(__file__), *rnames)) as f:
+        return f.read()
+
+
+setup(
+    name='formatflowed',
+    version=version,
+    description='RFC 3676 format=flowed text processing',
+    long_description='\n\n'.join((read('README.rst'), read('CHANGES.txt'))),
+    license='Python Software Foundation License',
+    platforms='OS Independent',
+    author='Martijn Pieters',
+    author_email='mj@zopatista.com',
+    url='http://pypi.python.org/pypi/formatflowed',
+    download_url='http://www.zopatista.com/projects/formatflowed/'
+                 'releases/1.0.0/formatflowed-1.0.0.tar.gz',
+    keywords=('email', 'mime', 'format', 'flowed', 'rfc2646', 'rfc3676'),
+    py_modules=['formatflowed'],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Python Software Foundation License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: Implementation :: PyPy',
+        'Topic :: Communications :: Email',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Text Processing :: Markup'
+    ],
+    install_requires=install_requires,
+    test_suite='formatflowed',
+)

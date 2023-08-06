6a486e72e5c4a263fde5705884d784139d6c3500
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:09:31 +0000

    Add package kuoliang_nester/1.0.0/kuoliang_nester-1.0.0.zip

---
+++ packages/kuoliang_nester/kuoliang_nester-1.0.0.zip/kuoliang_nester-1.0.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+    name ='kuoliang_nester',
+    version ='1.0.0',
+    py_modules =['nester'],
+    author ='kuoliang',
+    author_email ='chienkuoliang@gmail.com',
+    description ='A simple printer of nested lists',
+    )

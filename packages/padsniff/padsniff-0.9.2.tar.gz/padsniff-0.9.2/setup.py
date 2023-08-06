f2ee335d75710f75522cfb832f780865458954b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:12:22 +0000

    Add package padsniff/0.9.2/padsniff-0.9.2.tar.gz

---
+++ packages/padsniff/padsniff-0.9.2.tar.gz/padsniff-0.9.2/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup
+
+setup(
+    name='padsniff',
+    version='0.9.2',
+    description='Command-line tool to sniff Puzzle & Dragons data.',
+    url='https://bitbucket.org/necromanteion/padsniff',
+    author='Will Medlar',
+    author_email='wmmedlar@gmail.com',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3 :: Only',
+        'Topic :: Games/Entertainment :: Puzzle Games',
+    ],
+    keywords=[
+        'puzzle & dragons',
+        'man-in-the-middle',
+        'reverse engineering',
+    ],
+    packages=['padsniff'],
+    entry_points={
+        'console_scripts': [
+            'padsniff = padsniff.cli:cli'
+        ]
+    }
+)

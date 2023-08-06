526431f5406eff892d3e13a1934c42ad06f95904
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:46:26 +0000

    Add package beval/1.0.3/beval-1.0.3.tar.gz

---
+++ packages/beval/beval-1.0.3.tar.gz/beval-1.0.3/beval/schema.py
@@ -0,0 +1,20 @@
+
+
+class Provider(object):
+    pass
+
+
+class ValueGroupProvider(Provider):
+    pass
+
+
+class MappingDataProvider(Provider):
+    pass
+
+
+class Schema(object):
+
+    def __init__(self, name):
+        self._name = name
+
+    pass

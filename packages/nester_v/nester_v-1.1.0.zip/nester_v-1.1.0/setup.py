4eac23d512faf5cd58a4597a091d859ea0b603a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:51:57 +0000

    Add package nester_v/1.1.0/nester_v-1.1.0.zip

---
+++ packages/nester_v/nester_v-1.1.0.zip/nester_v-1.1.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+	name = 'nester_v',
+	version = '1.1.0',
+	py_modules = ['nester_v'],
+	author = 'vivipython',
+	author_email = '797564@qq.com',
+	url = 'http://www.headfirstlabs.com',
+	description = '打印包含嵌套列表的列表',
+	)

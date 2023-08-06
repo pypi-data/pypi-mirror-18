ec9077b9c00c873f47a682835c7d10a086dbadb3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:07:49 +0000

    Add package hupper/0.3.2/hupper-0.3.2.tar.gz

---
+++ packages/hupper/hupper-0.3.2.tar.gz/hupper-0.3.2/setup.py
@@ -0,0 +1,58 @@
+from setuptools import setup, find_packages
+
+def readfile(name):
+    with open(name) as f:
+        return f.read()
+
+readme = readfile('README.rst')
+changes = readfile('CHANGES.rst')
+
+setup_requires = [
+    'setuptools-git',
+]
+
+docs_require = [
+    'watchdog',
+    'Sphinx',
+    'pylons-sphinx-themes',
+]
+
+tests_require = [
+    'watchdog',
+    'pytest',
+    'pytest-cov',
+    'mock',
+]
+
+setup(
+    name='hupper',
+    version='0.3.2',
+    description='Integrated process monitor for developing servers.',
+    long_description=readme + '\n\n' + changes,
+    author='Michael Merickel',
+    author_email='michael@merickel.org',
+    url='https://github.com/Pylons/hupper',
+    packages=find_packages('src', exclude=['tests']),
+    package_dir={'': 'src'},
+    include_package_data=True,
+    setup_requires=setup_requires,
+    extras_require={
+        'docs': docs_require,
+        'testing': tests_require,
+    },
+    zip_safe=False,
+    keywords='server daemon autoreload reloader hup file watch process',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy',
+    ],
+)

805626770e3aeba3222437613389080d1703db46
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:08:38 +0000

    Add package raining-man/0.1.43/raining-man-0.1.43.tar.gz

---
+++ packages/raining-man/raining-man-0.1.43.tar.gz/raining-man-0.1.43/src/raining_man/boy.py
@@ -0,0 +1,19 @@
+from FGAme import *
+import pygame
+from raining_man.images import *
+
+class Boy:
+
+	def __init__(self):
+		self.body = world.add.aabb(shape=(30,80), pos=(400, 100), mass=500)
+		self.body_pygame = pygame.image.load('1.png')
+		self.rect = [0, 0, 60, 60]
+		self.position = [400, 100]
+		self.k = 1.05
+
+	def update(self):
+		pass
+
+	def move_p1(self, dx):
+		self.position = [self.position[0] + dx, 100]
+		self.body.pos = self.position

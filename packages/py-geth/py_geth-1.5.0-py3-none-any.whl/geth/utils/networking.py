6883908c39f09102f1e370a48d72c6849fbf2d2b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:44:55 +0000

    Add package py-geth/1.5.0/py_geth-1.5.0-py3-none-any.whl

---
+++ packages/py-geth/py_geth-1.5.0-py3-none-any.whl/geth/utils/networking.py
@@ -0,0 +1,34 @@
+import contextlib
+
+from gevent import socket
+
+
+def is_port_open(port):
+    sock = socket.socket()
+    try:
+        sock.bind(('127.0.0.1', port))
+    except socket.error:
+        return False
+    else:
+        return True
+    finally:
+        sock.close()
+
+
+def get_open_port():
+    sock = socket.socket()
+    sock.bind(('127.0.0.1', 0))
+    port = sock.getsockname()[1]
+    sock.close()
+    return str(port)
+
+
+@contextlib.contextmanager
+def get_ipc_socket(ipc_path, timeout=0.1):
+    sock = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
+    sock.connect(ipc_path)
+    sock.settimeout(timeout)
+
+    yield sock
+
+    sock.close()

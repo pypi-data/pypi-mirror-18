6cba7b14638792c02d61bee3b7bb4fceb51b29ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:21:02 +0000

    Add package nester_Frank/1.0.0/nester_Frank-1.0.0.zip

---
+++ packages/nester_Frank/nester_Frank-1.0.0.zip/nester_Frank-1.0.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+    name = 'nester_Frank',
+    version = '1.0.0',
+    py_modules = ['nester'],
+    author ='FrankLaurance',
+    author_email = 'lz0116@mail.ustc.edu.cn',
+    description = 'A testing python',
+    )

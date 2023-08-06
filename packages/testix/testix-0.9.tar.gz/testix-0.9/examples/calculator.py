f37d7bb9886956acf9a7789a96d2ffcd06cfa3a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:19:07 +0000

    Add package testix/0.9/testix-0.9.tar.gz

---
+++ packages/testix/testix-0.9.tar.gz/testix-0.9/examples/calculator.py
@@ -0,0 +1,15 @@
+import multiplier
+
+class Calculator( object ):
+	def __init__( self, initialValue ):
+		self._value = initialValue
+
+	def add( self, addend ):
+		self._value += addend
+
+	def result( self ):
+		return self._value
+
+	def multiply( self, factor ):
+		multiplier.xxx( 'a', 'b')
+		self._value = multiplier.multiply( first = self._value, second = factor )

83ef31049a09583621795576b5612a0a2ceac6dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:10:38 +0000

    Add package bookScraper/1.1/bookScraper-1.1.tar.gz

---
+++ packages/bookScraper/bookScraper-1.1.tar.gz/bookScraper-1.1/bookScraper/main.py
@@ -0,0 +1,18 @@
+
+import os
+
+class RunnerClass:
+    def __init__(self):
+        os.system('scrapy runspider sastobook_scrape.py')
+        os.system('scrapy runspider azbook_scrape.py')
+
+        sasto = open('sasto.txt', 'r')
+        sasto_price = float(sasto.read())
+
+        az = open('az.txt', 'r')
+        az_price = float(az.read())
+
+        self.mini = min(sasto_price, az_price)
+
+if __name__ == '__main__':
+    print '\n\nMinimum price is: ', RunnerClass().mini, '\n'

9d24cd4f2f38fc8ec909dd92f9e9f03ddfe087ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:09:14 +0000

    Add package nester-rhidus/1.2.0/nester-rhidus-1.2.0.zip

---
+++ packages/nester-rhidus/nester-rhidus-1.2.0.zip/nester-rhidus-1.2.0/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+        name         = "nester-rhidus",
+        version      = "1.2.0",
+        py_modules   = ["nester"],
+        author       = "Rainer Kartmann",
+        author_email = "rainer.kartmann@student.kit.edu",
+        url          = "",
+        description  = "A simple printer of nested lists.",
+    )
+
+        

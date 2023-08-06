d49a290764eca76f6bfe9846adbecf5c81c38944
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:21:16 +0000

    Add package slipstream-cli/0.8.1/slipstream_cli-0.8.1-py2.py3-none-any.whl

---
+++ packages/slipstream-cli/slipstream_cli-0.8.1-py2.py3-none-any.whl/slipstream/api/models.py
@@ -0,0 +1,55 @@
+from __future__ import unicode_literals
+
+import collections
+
+App = collections.namedtuple('App', [
+    'name',
+    'type',
+    'version',
+    'path',
+])
+
+Deployment = collections.namedtuple('Deployment', [
+    'id',
+    'module',
+    'status',
+    'started_at',
+    'last_state_change',
+    'cloud',
+])
+
+VirtualMachine = collections.namedtuple('VirtualMachine', [
+    'id',
+    'cloud',
+    'status',
+    'deployment_id',
+    'deployment_owner',
+    'ip',
+    'cpu',
+    'ram',
+    'disk',
+    'instance_type',
+    'is_usable',
+])
+
+Usage = collections.namedtuple('Usage', [
+    'cloud',
+    'run_usage',
+    'vm_usage',
+    'inactive_vm_usage',
+    'others_vm_usage',
+    'pending_vm_usage',
+    'unknown_vm_usage',
+    'quota',
+])
+
+Module = collections.namedtuple('Module', [
+    'name',
+    'type',
+    'created',
+    'modified',
+    'description',
+    'version',
+    'path',
+])
+

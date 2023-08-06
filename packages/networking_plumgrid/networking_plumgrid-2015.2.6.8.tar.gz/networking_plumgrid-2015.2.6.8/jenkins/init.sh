896ddcd606e3955469a6735463bf9ef9b843c408
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:09:26 +0000

    Add package networking_plumgrid/2015.2.6.8/networking_plumgrid-2015.2.6.8.tar.gz

---
+++ packages/networking_plumgrid/networking_plumgrid-2015.2.6.8.tar.gz/networking_plumgrid-2015.2.6.8/jenkins/init.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# This is not the final version and it's a stop gap implementation
+# to get things up and running on gate
+echo "networking-plumgrid unit tests.."
+cd ../ ; ./run_tests.sh -V

2a9df193656adbda49bcd1bcf23ea97dab86296d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:19:48 +0000

    Add package pika-commons/1.0/pika-commons-1.0.tar.gz

---
+++ packages/pika-commons/pika-commons-1.0.tar.gz/pika-commons-1.0/pika-commons.py
@@ -0,0 +1,7 @@
+class Test:
+	def test1(self):
+		print "dir1/test1.py --> Test class --> test1()"
+
+
+def test2():
+	print "dir1/test1.py --> test()2"
---
+++ packages/pika-commons/pika-commons-1.0.tar.gz/pika-commons-1.0/setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+setup(name='pika-commons',
+      version='1.0',
+      author="irene cho",
+      author_email="irene.cho@google.com",
+      license="MIT License",
+      url="https://testpypi.python.org/pypi/satang500/pika-commons",
+      py_modules=['pika-commons'],
+)

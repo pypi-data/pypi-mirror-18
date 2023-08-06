a4602ff8f4a46ac98635c7fe7f424d9ba8a016a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:08:46 +0000

    Add package the_big_username_blacklist/1.3.4/the_big_username_blacklist-1.3.4.tar.gz

---
+++ packages/the_big_username_blacklist/the_big_username_blacklist-1.3.4.tar.gz/the_big_username_blacklist-1.3.4/the_big_username_blacklist/__init__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+"""
+the-big-username-blacklist
+--------------------------
+A opinionated username blacklist
+"""
+
+__title__ = "the_big_username_blacklist"
+__version__ = "1.3.4"
+__build__ = 134
+__author__ = "Martin Sandström"
+__license__ = "MIT"
+__copyright__ = "Copyright 2015-2016 Martin Sandström"
+
+
+from .validator import validate, get_blacklist  # NOQA

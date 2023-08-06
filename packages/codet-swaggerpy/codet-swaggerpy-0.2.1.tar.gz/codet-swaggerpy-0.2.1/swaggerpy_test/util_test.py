148abbf5d9f29fcf7ba47ba16df917bb08aad276
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:20:40 +0000

    Add package codet-swaggerpy/0.2.1/codet-swaggerpy-0.2.1.tar.gz

---
+++ packages/codet-swaggerpy/codet-swaggerpy-0.2.1.tar.gz/codet-swaggerpy-0.2.1/swaggerpy_test/util_test.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+#
+# Copyright (c) 2013, Digium, Inc.
+#
+
+import unittest
+
+
+class UtilTest(unittest.TestCase):
+    pass
+
+if __name__ == '__main__':
+    unittest.main()

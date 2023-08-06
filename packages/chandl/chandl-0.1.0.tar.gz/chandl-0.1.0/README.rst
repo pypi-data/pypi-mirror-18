054f9650928637153e2b5de85e926eef449cd372
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:29:44 +0000

    Add package chandl/0.1.0/chandl-0.1.0.tar.gz

---
+++ packages/chandl/chandl-0.1.0.tar.gz/chandl-0.1.0/README.rst
@@ -0,0 +1,58 @@
+chandl
+======
+
+.. image:: https://img.shields.io/pypi/v/chandl.svg
+   :target: https://pypi.python.org/pypi/chandl
+.. image:: https://img.shields.io/pypi/pyversions/chandl.svg
+.. image:: https://travis-ci.org/gebn/chandl.svg?branch=master
+   :target: https://travis-ci.org/gebn/chandl
+.. image:: https://coveralls.io/repos/github/gebn/chandl/badge.svg?branch=master
+   :target: https://coveralls.io/github/gebn/chandl?branch=master
+
+A lightweight tool for parsing and downloading 4chan threads.
+
+Features
+--------
+
+-  An API for programmatically analysing 4chan content.
+-  Customise the each file name using a lambda function.
+-  Filter out images or videos.
+-  Concurrent downloading, with parallelism linked to the number of
+   cores.
+
+Installation
+------------
+
+To install ``chandl``, simply run:
+
+::
+
+    $ pip install chandl
+
+Usage
+-----
+
+::
+
+    $ chandl -h
+    usage: chandl [-h] [-c] [-t THREADS] [-v] url [url ...]
+
+    A lightweight tool for parsing and downloading 4chan threads.
+
+    positional arguments:
+      url                   the URL(s) of the thread(s) whose files to download
+
+    optional arguments:
+      -h, --help            show this help message and exit
+      -c, --cwd             download to the working directory
+      -t THREADS, --threads THREADS
+                            the maximum number of download threads to use per core
+      -v, --verbosity       increase output verbosity
+
+Roadmap
+-------
+
+-  Implement tracking of threads until they are deleted
+-  File exclusion
+-  Improve test coverage
+-  Pylint or flake8 integration
---
+++ packages/chandl/chandl-0.1.0.tar.gz/chandl-0.1.0/chandl/util.py
@@ -0,0 +1,100 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+import logging
+import sys
+import hashlib
+import unidecode
+import six
+
+
+def bytes_fmt(num, suffix='B'):
+    """
+    Turn a number of bytes into a more friendly representation, e.g. 2.5MiB.
+
+    :param num: The number of bytes to convert.
+    :param suffix: The unit suffix (defaults to 'B').
+    :return: The human-readable equivalent of the input size.
+    :raises ValueError: If num is not an integer.
+    """
+
+    if not isinstance(num, six.integer_types):
+        raise ValueError('Byte count must be an integer')
+
+    num = abs(num)
+    for unit in ['', 'Ki', 'Mi', 'Gi', 'Ti', 'Pi', 'Ei', 'Zi']:
+        if num < 1024.0:
+            return '{0:.1f} {1}{2}'.format(num, unit, suffix)
+        num /= 1024.0
+    return '{0:.1f} {1}{2}'.format(num, 'Yi', suffix)
+
+
+def decode_cli_arg(arg):
+    """
+    Turn a bytestring provided by `argparse` into unicode.
+
+    :param arg: The bytestring to decode.
+    :return: The argument as a unicode object.
+    :raises ValueError: If arg is None.
+    """
+
+    if arg is None:
+        raise ValueError('Argument cannot be None')
+
+    return arg.decode(sys.getfilesystemencoding())
+
+
+def make_filename(string):
+    """
+    Turn a string into something that can be safely used as a file or directory
+    name.
+
+    :param string: The string to convert.
+    :return: The sanitised string.
+    :raises ValueError: If string is None.
+    """
+
+    if string is None:
+        raise ValueError('String cannot be None')
+
+    safe = [' ', '.', '_', ':']
+    joined = ''.join([c for c in unidecode.unidecode(string)
+                      if c.isalnum() or c in safe]).strip()
+    if not joined:
+        raise ValueError('Filename would be empty')
+    return joined
+
+
+def md5_file(path):
+    """
+    Get the MD5 hash of a file.
+
+    :param path: The path of the file.
+    :return: The 32-character long lowercase hex representation of the
+             checksum.
+    :raises ValueError: If path is invalid.
+    """
+
+    if not path:
+        raise ValueError('Path cannot be empty or None')
+
+    hash_ = hashlib.md5()
+    with open(path, 'rb') as fd:
+        for chunk in iter(lambda: fd.read(4096), b''):
+            hash_.update(chunk)
+    return hash_.hexdigest()
+
+
+def log_level_from_vebosity(verbosity):
+    """
+    Get the `logging` module log level from a verbosity.
+
+    :param verbosity: The number of times the `-v` option was specified.
+    :return: The corresponding log level.
+    """
+
+    if verbosity == 0:
+        return logging.WARNING
+    if verbosity == 1:
+        return logging.INFO
+    return logging.DEBUG

095998b923eb19b217f731c158399ebe8c8ee887
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:14:53 +0000

    Add package newsFeedClassifier-CS331/1.0.1/newsFeedClassifier-CS331-1.0.1.tar.gz

---
+++ packages/newsFeedClassifier-CS331/newsFeedClassifier-CS331-1.0.1.tar.gz/newsFeedClassifier-CS331-1.0.1/setup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+try:
+    from setuptools.core import setup
+except ImportError:
+    from distutils.core import setup
+
+
+setup(name='newsFeedClassifier-CS331',
+      version='1.0.1',
+      license='LUMS',
+      description='news feed classifier using naive bayes algorithm',
+      long_description=open('README.md').read(),
+      author='CS331 Group: Dot Slash',
+      author_email='artificialintelligence@pern.edu.pk',
+      reference= 'https://github.com/muatik/naive-bayes-classifier',
+      packages=['newsFeedClassifier-CS331'],
+      platforms='any')

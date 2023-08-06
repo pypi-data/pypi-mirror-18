977ef3a18a6e294fe036d4e6be289d6de37ca040
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:09:17 +0000

    Add package pageobject/0.0.34/pageobject-0.0.34.tar.gz

---
+++ packages/pageobject/pageobject-0.0.34.tar.gz/pageobject-0.0.34/pageobject/pageobjectbase.py
@@ -0,0 +1,113 @@
+import logging
+from selenium.webdriver import Remote as WebDriver
+
+
+class PageObjectBase(object):
+
+    DEFAULT_ROOT_NAME = 'root'
+    NAME_SEPARATOR = '.'
+    DEFAULT_WAIT_TIMEOUT = 60
+    DEFAULT_POLL_INTERVAL = 0.25
+
+
+    def __nonzero__(self):      # pragma: no cover
+        return self.__bool__()  # Python 2 throwback
+
+
+    def __repr__(self):
+        my_class = self.__class__.__name__
+        base_class = self.__class__.__bases__[0].__name__
+        try:
+            locator = self.locator
+        except: # pragma: no cover
+            locator = 'INVALID LOCATOR'
+        return '<{}({}) (locator="{}")>'.format(
+                my_class, base_class, locator)
+
+
+    def __setattr__(self, attr_name, attr_value):
+        object.__setattr__(self, attr_name, attr_value)
+        if attr_name is not '_parent' and isinstance(attr_value, PageObjectBase):
+            child = attr_value
+            child.__dict__['_parent'] = self
+
+
+    @property
+    def parent(self):
+        return self._parent
+
+
+    @property
+    def default_locator(self):
+        return None
+
+
+    @property
+    def _parent_locator(self):
+        try:
+            return self.parent.locator
+        except AttributeError:
+            return ''
+
+
+    @property
+    def locator(self):
+        if self.default_locator:
+            return self.default_locator
+        elif self._chain:
+            return '{}{}'.format(self._parent_locator, self._locator)
+        else:
+            return self._locator
+
+
+    @property
+    def webdriver(self):
+        try:
+            return self.parent.webdriver
+        except AttributeError:
+            error_msg = ('webdriver should be an instance of selenium'
+                        + ' WebDriver, instead is "{}"').format(self._webdriver)
+            assert isinstance(self._webdriver, WebDriver), error_msg
+            return self._webdriver
+
+
+    @property
+    def logger(self):
+        try:
+            return self.parent.logger
+        except AttributeError:
+            if self._logger is None:
+                return logging
+            else:
+                return self._logger
+
+
+    @property
+    def name(self):
+        try:
+            return self.parent._get_child_name(self)
+        except AttributeError:
+            if self._name:
+                return self._name
+            else:
+                return PageObjectBase.DEFAULT_ROOT_NAME
+
+
+    @property
+    def full_name(self):
+        try:
+            return self.parent._get_child_full_name(self)
+        except AttributeError:
+            return self.name
+
+
+    @property
+    def _log_id_short(self): # pragma: no cover
+        return 'page object "{}"'.format(self.name)
+
+
+    @property
+    def _log_id_long(self): # pragma: no cover
+        return 'full name path: "{}", element: "{}"'.format(
+                self.full_name, self.locator)
+
---
+++ packages/pageobject/pageobject-0.0.34.tar.gz/pageobject-0.0.34/pageobject/pageobjectlist.py
@@ -0,0 +1,95 @@
+from .pageobjectbase import PageObjectBase
+from .pageobject import PageObject
+from . import commands
+
+
+class PageObjectList(PageObjectBase):
+
+    def __init__(self, locator, chain=True, children_class=None, children_locator=None, count_locator=None):
+        self._locator = locator
+        self._chain = chain
+        self._children_class = children_class
+        self._children_locator = children_locator
+        self._count_locator = count_locator
+        self._parent = None
+
+
+    def __bool__(self):
+        return bool(len(self))
+
+
+    def __getitem__(self, slice):
+        return self.children[slice]
+
+
+    def __len__(self):
+        return len(self.children)
+
+
+    @property
+    def _children_count(self):
+        return len(self.webdriver.find_elements_by_xpath(self.count_locator))
+
+
+    @property
+    def children(self):
+        children = []
+        ChildrenClass = self.children_class
+        for i in range(self._children_count):
+            locator = self.children_locator.format(i+1)
+            child = ChildrenClass(locator, chain=False)
+            child.index = i
+            child._parent = self
+            children.append(child)
+        return children
+
+
+    def _get_child_name(self, child_po):
+        return '{}[{}]'.format(self.name, child_po.index)
+
+
+    def _get_child_full_name(self, child_po):
+        return '{}[{}]'.format(self.full_name, child_po.index)
+
+
+    @property
+    def children_class(self):
+        if self._children_class:
+            return self._children_class
+        else:
+            return PageObject
+
+
+    @property
+    def default_children_locator(self):
+        return None
+
+
+    @property
+    def children_locator(self):
+        if self.default_children_locator:
+            return self.default_children_locator
+        elif self._children_locator:
+            return self._children_locator
+        else:
+            return '({})[{}]'.format(self.locator, '{}')
+
+
+    @property
+    def default_count_locator(self):
+        return None
+
+
+    @property
+    def count_locator(self):
+        if self.default_count_locator:
+            return self.default_count_locator
+        elif self._count_locator:
+            return self._count_locator
+        else:
+            return self.locator
+
+
+    # commands
+    index = commands.index
+

b80e46566197867ee698139dbd743fcc6bdd2b85
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:08:53 +0000

    Add package sentenceslug/1.0/sentenceslug-1.0.tar.gz

---
+++ packages/sentenceslug/sentenceslug-1.0.tar.gz/sentenceslug-1.0/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'sentenceslug',
+  packages = ['sentenceslug'], # this must be the same as the name above
+  version = '1.0',
+  description = 'Generate random slugs using basic sentence structure',
+  author = 'jef79m',
+  author_email = 'jeff@fuzzknuckle.com',
+  url = 'https://github.com/jef79m/sentenceslug', # use the URL to the github repo
+  download_url = 'https://github.com/jef79m/sentenceslug/tarball/1.0', # I'll explain this in a second
+  keywords = ['random', 'slug', 'sentence'], # arbitrary keywords
+  classifiers = [],
+)

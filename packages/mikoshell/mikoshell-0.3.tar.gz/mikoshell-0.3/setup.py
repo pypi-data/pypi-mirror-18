02075ee43574fee965e39cc4871d3b46a06d36df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:50:10 +0000

    Add package mikoshell/0.3/mikoshell-0.3.tar.gz

---
+++ packages/mikoshell/mikoshell-0.3.tar.gz/mikoshell-0.3/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name = 'mikoshell',
+    packages = ['mikoshell'],
+    requires = ['paramiko'],
+    version = '0.3',
+    description = 'Simple Shell for Paramiko',
+    author = 'Matt Ryan',
+    author_email = 'inetuid@gmail.com',
+    url = 'https://github.com/inetuid/mikoshell',
+    download_url = 'https://github.com/inetuid/mikoshell/tarball/0.3',
+    classifiers = [],
+)

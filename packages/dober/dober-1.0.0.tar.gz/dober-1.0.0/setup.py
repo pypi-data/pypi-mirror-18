52db3524b8887be90a4e8d8773b126c3ed9647f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:44:56 +0000

    Add package dober/1.0.0/dober-1.0.0.tar.gz

---
+++ packages/dober/dober-1.0.0.tar.gz/dober-1.0.0/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+
+setup(
+	name = 		'dober',
+	version = 	'1.0.0',
+	py_modules = 	['dober'],
+	author = 	'hfpython',
+	author_email = 	'hfpython@headfirstlabs.com',
+	url = 		'http://www.headfirstlabs.com',
+	description = 	'A simple printer of nested lists',
+)

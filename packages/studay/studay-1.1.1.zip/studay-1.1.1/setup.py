a3a3ab669b1f4282f44b33a7a468cf28cbcca43d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:30:50 +0000

    Add package studay/1.1.1/studay-1.1.1.zip

---
+++ packages/studay/studay-1.1.1.zip/studay-1.1.1/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+		name = 'studay',
+		version = '1.1.1',
+		py_modules = ['studay'],
+		author = 'hammer',
+		author_email = 'fhammer@qq.com',
+		url = 'http://www.headfirstlabs.com',
+		description = 'sjasjkajckdskdkd',
+	)
